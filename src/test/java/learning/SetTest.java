a4fce41ed7fad7f38305a548cf09dfa70de842fb
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-11-10 14:09:12 +0900

    test(SetTest): learn about set contains method
    - Set의 내장된 contains 메서드를 통해 특정 요소가 존재하는지 확인하는 학습 테스트를 진행했습니다.
    - 제약 조건을 지키기 위해 반복문 없이 구현했습니다.

--- src/test/java/learning/SetTest.java
+++ src/test/java/learning/SetTest.java
@@ -3,6 +3,8 @@ package learning;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -27,5 +29,12 @@ public class SetTest {
         int expectedSize = 3;
 
         assertThat(numbers.size()).isEqualTo(expectedSize);
+    }
+
+    @DisplayName("Set의 내장 메서드로 특정 요소의 포함 여부를 확인할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsTest(int expectedResult) {
+        assertThat(numbers).contains(expectedResult);
     }
 }
