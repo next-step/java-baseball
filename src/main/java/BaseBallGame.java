a3243abe39a55b15441e9087c8aa70fdddd23cb8
FRABUCHI <alphasuno214@gmail.com>
2020-11-10 21:57:46 +0900

    게임 진행 로직 구현

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -6,9 +6,13 @@ public class BaseBallGame {
 
     public void main(String[] args) throws Exception {
         int[] answer = new int[cnt];
-        int status = 1; //1 : (재)시작, 2 : 종료
-        setNumber();
-        inputNumber();
+        int status = 1; //1 : (재)시작, 2 : 종료, 3 : 진행중
+
+        while (status != 2) {
+            if (status == 1) answer = setNumber();
+            status = execution(inputNumber(), answer);
+            if (status == 2) status = isExit();
+        }
     }
 
     /**
@@ -58,4 +62,83 @@ public class BaseBallGame {
         return true;
     }
 
+    /**
+     * 유저의 답과 정답을 비교 후 결과 반환. (2 : 종료, 3 : 진행중)
+     * @param inputNumbers
+     * @param answer
+     * @return int
+     */
+    public int execution(int[] inputNumbers, int[] answer) {
+        int strikeCnt = 0;
+        int ballCnt = 0;
+        int nothingCnt = 0;
+
+        for (int i = 0; i < cnt; i++) {
+            System.out.println("num : " + inputNumbers[i]);
+            strikeCnt += isStrike(i, inputNumbers[i], answer);
+            ballCnt += isBall(i, inputNumbers[i], answer);
+            nothingCnt += isNothing(inputNumbers[i], answer);
+        }
+
+        if (strikeCnt != 0) System.out.printf("%d 스트라이크 ", strikeCnt);
+        if (ballCnt != 0) System.out.printf("%d 볼 ", ballCnt);
+        if (nothingCnt == cnt) System.out.print("4 볼");
+        System.out.println();
+
+        return (strikeCnt == cnt) ? 2 : 3;
+    }
+
+    /**
+     * Strike 확인 (0 : Ball or Nothing, 1 : Strike)
+     *
+     * @param index
+     * @param num
+     * @param answer
+     * @return int
+     */
+    public int isStrike(int index, int num, int[] answer) {
+        if (num == answer[index]) return 1;
+        return 0;
+    }
+
+    /**
+     * Ball 확인 (0 : Strike or Nothing, 1 : Ball)
+     *
+     * @param index
+     * @param num
+     * @param answer
+     * @return int
+     */
+    public int isBall(int index, int num, int[] answer) {
+        boolean result = false;
+        for (int i = 0; i < cnt; i++) {
+            if (i != index && num == answer[i]) result = true;
+        }
+        return result ? 1 : 0;
+    }
+
+    /**
+     * Nothing 확인 (0 : Strike or Ball, 1 : Nothing)
+     *
+     * @param num
+     * @param answer
+     * @return int
+     */
+    public int isNothing(int num, int[] answer) {
+        boolean result = true;
+        for (int i = 0; i < cnt; i++) {
+            if (num == answer[i]) result = false;
+        }
+        return result ? 1 : 0;
+    }
+
+    /**
+     * 게임 종료 확인 (1 : (재)시작, 2 : 종료)
+     * @return int
+     */
+    private static int isExit() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료.");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return 2;
+    }
 }
--- src/test/java/BaseBallGameTest.java
+++ src/test/java/BaseBallGameTest.java
@@ -29,4 +29,67 @@ class BaseBallGameTest {
         assertFalse(game.checkNumber("qwef"));
         assertFalse(game.checkNumber("12f"));
     }
+
+    @Test
+    void isStrikeTest() {
+        assertEquals(1, game.isStrike(0, 3, new int[] {3,6,8}));
+        assertEquals(0, game.isStrike(1, 3, new int[] {3,6,8}));
+        assertEquals(0, game.isStrike(2, 3, new int[] {3,6,8}));
+
+        assertEquals(0, game.isStrike(0, 6, new int[] {3,6,8}));
+        assertEquals(1, game.isStrike(1, 6, new int[] {3,6,8}));
+        assertEquals(0, game.isStrike(2, 8, new int[] {3,6,8}));
+
+        assertEquals(0, game.isStrike(0, 8, new int[] {3,6,8}));
+        assertEquals(0, game.isStrike(1, 8, new int[] {3,6,8}));
+        assertEquals(1, game.isStrike(2, 8, new int[] {3,6,8}));
+
+        assertEquals(0, game.isStrike(0, 5, new int[] {3,6,8}));
+        assertEquals(0, game.isStrike(1, 5, new int[] {3,6,8}));
+        assertEquals(0, game.isStrike(2, 5, new int[] {3,6,8}));
+    }
+
+    @Test
+    void isBallTest() {
+        assertEquals(0, game.isBall(0, 3, new int[] {3,6,8}));
+        assertEquals(1, game.isBall(1, 3, new int[] {3,6,8}));
+        assertEquals(1, game.isBall(2, 3, new int[] {3,6,8}));
+
+        assertEquals(1, game.isBall(0, 6, new int[] {3,6,8}));
+        assertEquals(0, game.isBall(1, 6, new int[] {3,6,8}));
+        assertEquals(1, game.isBall(2, 8, new int[] {3,6,8}));
+
+        assertEquals(1, game.isBall(0, 8, new int[] {3,6,8}));
+        assertEquals(1, game.isBall(1, 8, new int[] {3,6,8}));
+        assertEquals(0, game.isBall(2, 8, new int[] {3,6,8}));
+
+        assertEquals(0, game.isBall(0, 5, new int[] {3,6,8}));
+        assertEquals(0, game.isBall(1, 5, new int[] {3,6,8}));
+        assertEquals(0, game.isBall(2, 5, new int[] {3,6,8}));
+    }
+
+    @Test
+    void isNothingTest() {
+        assertEquals(0, game.isNothing(3, new int[] {3,6,8}));
+        assertEquals(0, game.isNothing(6, new int[] {3,6,8}));
+        assertEquals(0, game.isNothing(8, new int[] {3,6,8}));
+
+        assertEquals(1, game.isNothing(1, new int[] {3,6,8}));
+        assertEquals(1, game.isNothing(4, new int[] {3,6,8}));
+        assertEquals(1, game.isNothing(7, new int[] {3,6,8}));
+    }
+
+    @Test
+    void executionTest() {
+        assertEquals(3, game.execution(new int[] {1,2,3}, new int[] {2,3,4}));
+        assertEquals(3, game.execution(new int[] {3,6,8}, new int[] {8,6,3}));
+        assertEquals(3, game.execution(new int[] {3,6,8}, new int[] {6,3,1}));
+        assertEquals(3, game.execution(new int[] {1,2,3}, new int[] {4,5,6}));
+        assertEquals(2, game.execution(new int[] {1,2,3}, new int[] {1,2,3}));
+        assertEquals(2, game.execution(new int[] {3,6,8}, new int[] {3,6,8}));
+    }
+
+    @Test
+    void isExitTest() {
+    }
 }
