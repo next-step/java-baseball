6c92d596e7e4ee5ca2fff62933bf96cad85c77cb
FRABUCHI <alphasuno214@gmail.com>
2020-11-10 21:39:40 +0900

    유저 임의의 수 선택 기능 구현);

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -1,14 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class BaseBallGame {
-    private int cnt;
+    private int cnt = 3;
 
     public void main(String[] args) throws Exception {
         int[] answer = new int[cnt];
         int status = 1; //1 : (재)시작, 2 : 종료
         setNumber();
+        inputNumber();
     }
 
+    /**
+     * 컴퓨터가 서로 다른 임의의 수 3개 선택.
+     * @return int[]
+     */
     public int[] setNumber() {
         int[] answer = new int[cnt];
         Random random = new Random();
@@ -21,5 +27,35 @@ public class BaseBallGame {
         return answer;
     }
 
+    /**
+     * 유저가 임의의 수 3개 선택.
+     * @return int[]
+     */
+    public int[] inputNumber() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        int[] inputNumbers = new int[cnt];
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            input = sc.nextLine();
+            if (checkNumber(input)) break;
+        }
+        for (int i = 0; i < cnt; i++) { inputNumbers[i] = input.charAt(i) - '0'; }
+        return inputNumbers;
+    }
+
+    /**
+     * 입력값 확인
+     * @param input
+     * @return boolean
+     */
+    public boolean checkNumber(String input) {
+        if (input.length() != cnt) return false;
+        char[] charArr = input.toCharArray();
+        for (char c : charArr) {
+            if (c > '9' || c < '0') return false;
+        }
+        return true;
+    }
 
 }
--- src/test/java/BaseBallGameTest.java
+++ src/test/java/BaseBallGameTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BaseBallGameTest {
 
@@ -9,7 +10,23 @@ class BaseBallGameTest {
     @Test
     void setNumberTest() {
         for (int i = 0; i < game.setNumber().length; i++) {
-            assertThat(game.setNumber()[i]);
+            assertThat("컴퓨터 입력" + game.setNumber()[i]);
         }
+    }
+
+    @Test
+    void inputNumberTest() throws Exception {
+        for (int i = 0; i < game.inputNumber().length; i++) {
+            assertThat("유저 입력 : " + game.inputNumber()[i]);
+        }
+    }
+
+    @Test
+    void checkNumberTest() {
+        assertTrue(game.checkNumber("123"));
+        assertTrue(game.checkNumber("456"));
+        assertFalse(game.checkNumber("12345"));
+        assertFalse(game.checkNumber("qwef"));
+        assertFalse(game.checkNumber("12f"));
     }
 }
