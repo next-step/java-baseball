94f0f3442e0cad5268fe1ec9283deba2a75ae4fc
himj131@gmail.com <himj131@gmail.com>
2020-11-11 06:59:03 +0900

    test: Set 학습 테스트 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("set 크기 확인")
+    void set_size_test() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("numbers에 1,2,3값이 존재하는지 확인")
+    void set_contain_test(int input) {
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("numbers에 존재하는인 값, 존재하지 않는 값 확인")
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(Integer input, Boolean result) {
+        assertThat(numbers.contains(input)).isEqualTo(result);
+    }
+}
