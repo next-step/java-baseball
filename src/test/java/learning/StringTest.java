1be1e5e407e15d539a5601df3eece440932d74fe
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-11-10 13:21:53 +0900

    refactor(StringTest): remove magic number
    - 테스트의 의도를 좀 더 명확하게 드러내기 위해 매직 넘버를 제거하고 명확한 변수를 인자로 전달하도록 수정했습니다.

--- src/test/java/learning/StringTest.java
+++ src/test/java/learning/StringTest.java
@@ -2,6 +2,8 @@ package learning;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,13 +11,14 @@ public class StringTest {
     private String separator = ",";
 
     @DisplayName("String 클래스의 split 메서드가 구분자로 구분된 복수의 요소를 잘 반환하는지 확인")
-    @Test
-    void splitManyStringTest() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2"})
+    void splitManyStringTest(String expectedResult) {
         String testString = "1,2";
 
         String[] splitResult = testString.split(separator);
 
-        assertThat(splitResult).contains("1", "2");
+        assertThat(splitResult).contains(expectedResult);
     }
 
     @DisplayName("String 클래스의 split 메서드가 구분자로 구분되지 않는 단수 요소를 잘 반환하는지 확인")
@@ -25,6 +28,6 @@ public class StringTest {
 
         String[] splitResult = testString.split(separator);
 
-        assertThat(splitResult).containsExactly("1");
+        assertThat(splitResult).containsExactly(testString);
     }
 }
