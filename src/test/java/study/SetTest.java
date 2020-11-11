d3568d670e8325c4280966c3e0627dfc8695e49a
sang5c <skah321@gmail.com>
2020-11-11 19:48:57 +0900

    test: Set 학습테스트 요구사항 1 size 확인 test 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
 
@@ -16,6 +20,12 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+    }
+
+    @DisplayName("size 확인 테스트")
+    @Test
+    public void size() {
+        assertThat(numbers.size()).isEqualTo(3);
     }
 
 }
