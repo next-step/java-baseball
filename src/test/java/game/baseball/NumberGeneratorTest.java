b2a434a2c809b9804a2f7046328b7199e0026b09
monds <suahnn@gmail.com>
2020-11-11 20:33:50 +0900

    test: 랜덤 숫자 생성기 테스트클래스 생성

---
+++ src/test/java/game/baseball/NumberGeneratorTest.java
@@ -0,0 +1,20 @@
+package game.baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberGeneratorTest {
+
+    @Test
+    @DisplayName("범위 내의 임의의 랜덤 숫자 생성")
+    void generateNumberInRange() {
+        Set<Integer> numbers = NumberGenerator.shuffleAndGet();
+        for (Integer number : numbers) {
+            assertThat(number).isBetween(1, 9);
+        }
+    }
+}
