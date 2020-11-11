904ee605c78d7e527df0cdae81196ab446fe0b27
Yeoseon Yun <devyyskr@gmail.com>
2020-11-11 17:53:10 +0900

    test(BallNumbers): BallNumbers 테스트 코드 추가

---
+++ src/main/java/nextstep/precourse/domain/BallNumbers.java
@@ -0,0 +1,11 @@
+package nextstep.precourse.domain;
+
+import java.util.List;
+
+public class BallNumbers {
+    private List<BallNumber> ballNumbers;
+
+    public BallNumbers(List<BallNumber> ballNumbers) {
+        this.ballNumbers = ballNumbers;
+    }
+}
---
+++ src/test/java/nextstep/precourse/domain/BallNumbersTest.java
@@ -0,0 +1,57 @@
+package nextstep.precourse.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class BallNumbersTest {
+    private static final BallNumber BALLNUMBER_ONE = new BallNumber(1, 1);
+    private static final BallNumber BALLNUMBER_TWO = new BallNumber(2, 2);
+    private static final BallNumber BALLNUMBER_THREE = new BallNumber(3, 3);
+    private static final BallNumber BALLNUMBER_SAME_POSITION = new BallNumber(4, 2);
+    private static final BallNumber BALLNUMBER_SAME_NUMBER = new BallNumber(2, 3);
+
+    @Test
+    @DisplayName("BallNumber는 3개만으로 구성되어야 한다. 그렇지 않으면 Exception을 반환한다.")
+    void assertNumbersCount() {
+        List<BallNumber> ballNumbers= new ArrayList<>();
+        ballNumbers.add(BALLNUMBER_ONE);
+        ballNumbers.add(BALLNUMBER_TWO);
+        ballNumbers.add(BALLNUMBER_THREE);
+        ballNumbers.add(BALLNUMBER_SAME_POSITION);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new BallNumbers(ballNumbers);
+        }).withMessage("숫자는 3개로만 구성되어 있어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("BallNumber는 1,2,3 각각 다른 포지션에 포진되어 있어야 한다. 그렇지 않으면 Exception을 반환한다.")
+    void assertNumbersPosition() {
+        List<BallNumber> ballNumbers = new ArrayList<>();
+        ballNumbers.add(BALLNUMBER_ONE);
+        ballNumbers.add(BALLNUMBER_TWO);
+        ballNumbers.add(BALLNUMBER_SAME_POSITION);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new BallNumbers(ballNumbers);
+        }).withMessage("숫자들은 서로 다른 포지션에 포진되어 있어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("BallNumber 서로 다른 숫자로 구성되어 있어야 한다. 그렇지 않으면 Exception을 반환한다.")
+    void assertNumbersDifference() {
+        List<BallNumber> ballNumbers = new ArrayList<>();
+        ballNumbers.add(BALLNUMBER_ONE);
+        ballNumbers.add(BALLNUMBER_TWO);
+        ballNumbers.add(BALLNUMBER_SAME_NUMBER);
+
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new BallNumbers(ballNumbers);
+        }).withMessage("서로 다른 숫자를 제시해주세요.");
+    }
+}
