68086821b62ded00b1eb14cc9d704671c589747d
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-11-11 18:14:00 +0900

    feat(BaseBall.class): Strike, Ball, Nothing 체크하는 기능(3 Strike가 나올때까지 반복)

--- src/main/java/com/nextstep/baseball/BaseBall.java
+++ src/main/java/com/nextstep/baseball/BaseBall.java
@@ -1,6 +1,5 @@
 package com.nextstep.baseball;
 
-import java.io.IOException;
 import java.util.*;
 
 public class BaseBall {
@@ -10,12 +9,67 @@ public class BaseBall {
 	Scanner sc = new Scanner(System.in);
 
 	public boolean play() {
+		//1. 1~9까지 서로 다른 임의의 수 3개를 선택하여 리턴하는 기능
 		List<Integer> answerList = selectNonoverlapThreeNumber();
+
+		//2. 사용자로부터 1~9까지 서로 다른 임의의 수 3개를 입력 받는 기능
 		printMessage("숫자를 입력해주세요 : ");
 		//todo 입력에 대한 예외처리해야함.
 		List<Integer> inputNumberList = readNonoverThreeNumber();
-//		System.out.println(inputNumberList);
+
+		//3. Strike, Ball 체크하는 기능
+		List<Boolean> strikePosition = new ArrayList<>();
+		do {
+			strikePosition = detectStrike(answerList, inputNumberList);
+			removeStrike(strikePosition, answerList, inputNumberList);
+			int ballCount = detectBall(answerList, inputNumberList);
+			boolean isNothing = isNothing(answerList);
+		} while (strikePosition.size() == 0);
+
 		return true;
+	}
+
+	private boolean isNothing(List<Integer> answerList) {
+		if (answerList.size() == 3) {
+			return true;
+		}
+		return false;
+	}
+
+	private int detectBall(List<Integer> answerList, List<Integer> inputNumberList) {
+		answerList.retainAll(inputNumberList);
+		return answerList.size();
+	}
+
+	private void removeStrike(List<Boolean> strikePosition, List<Integer> answerList, List<Integer> inputNumberList) {
+		for (int i = 0; i < strikePosition.size(); i++) {
+			removeStrike(strikePosition.get(i), answerList, inputNumberList, i);
+		}
+	}
+
+	private void removeStrike(Boolean isStrike, List<Integer> answerList, List<Integer> inputNumberList, int index) {
+		if (isStrike) {
+			answerList.remove(index);
+			inputNumberList.remove(index);
+		}
+	}
+
+	private List<Boolean> detectStrike(List<Integer> answerList, List<Integer> inputNumberList) {
+		List<Boolean> strikePosition = new ArrayList<>(Arrays.asList(Boolean.FALSE, Boolean.FALSE, Boolean.FALSE));
+		for (int i = 0; i < answerList.size(); i++) {
+			Integer answer = answerList.get(i);
+			Integer input = inputNumberList.get(i);
+
+			detectStrike(answer, input, strikePosition, i);
+		}
+
+		return strikePosition;
+	}
+
+	private void detectStrike(Integer answer, Integer input, List<Boolean> strikePosition, int index) {
+		if (answer == input) {
+			strikePosition.add(index, Boolean.TRUE);
+		}
 	}
 
 	private void collectDigits(int input, List<Integer> digits) {
