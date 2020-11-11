a3d1d1ae89b7b684f53b3195fcc543954e6aad07
parchano <chano22@naver.com>
2020-11-11 16:44:25 +0900

    build(input) 입력에 대한 기능개발 및 테스트 완료
    Please enter the commit message for your changes. Lines starting

--- src/main/java/woo/baseball/domain/GameUser.java
+++ src/main/java/woo/baseball/domain/GameUser.java
@@ -1,8 +1,28 @@
 package woo.baseball.domain;
 
+/**
+ * @author : chano
+ * @Date   : 2020. 11. 11.
+ * @Description : 게임을 진행할 유저에대한 필요한 공통필드
+ */
 public class GameUser {
+	
+	/**
+	 * @Description : 야구게임을 하기위한 digit 이며 3자리수이다.
+	 * @Field : baseBallNumberSize
+	 */
 	private final static int baseBallNumberSize = 3;
+	
+	/**
+	 * @Description : 게임을 즐기기위한 번호이다. 예를들어 231 or 233 or 924...
+	 * @Field : baseBallNumber
+	 */
 	private int baseBallNumber;
+	
+	/**
+	 * @Description : 실질적으로 비즈니스로직에 사용될 유저의 번호이다. split기능을 위해 String형으로 선언되었다.
+	 * @Field : strNumber 
+	 */
 	private String[] strNumber = new String[baseBallNumberSize];
 
 	public int getBaseBallNumber() {
