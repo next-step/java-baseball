600fd2047a75f52b618f13007c54560a986d0e6f
sang5c <skah321@gmail.com>
2020-11-11 19:25:09 +0900

    test: 요구사항 3-1 특정 위치 문자 가져오기

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,6 +28,15 @@ public class StringTest {
     public void substring() {
         String str = "(1,2)";
         assertThat(str.substring(1, str.length() - 1)).isEqualTo("1,2");
+
+    }
+
+    @DisplayName("특정 위치의 문자 가져오기")
+    @ParameterizedTest
+    @CsvSource({"0, a", "1, b", "2, c"})
+    public void charAt(int index, char target) {
+        String str = "abc";
+        assertThat(str.charAt(index)).isEqualTo(target);
     }
 
 }
