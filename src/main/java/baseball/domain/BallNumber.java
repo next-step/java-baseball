5ab681bf1738f8b7494233a81a81280d1a9e232b
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-12 22:45:39 +0900

    feat : 1~9 사이의 숫자 validate 추가
    - 1~9 이외의 숫자를 받으면 예외처리

--- src/main/java/baseball/domain/BallNumber.java
+++ src/main/java/baseball/domain/BallNumber.java
@@ -5,6 +5,13 @@ public class BallNumber {
     private int number;
 
     public BallNumber(int number) {
+        validate(number);
         this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number <= 0 || number > 9) {
+            throw new IllegalArgumentException();
+        }
     }
 }
