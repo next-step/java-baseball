6fe2441d7d90166c08cf3fb8408c343c816c734c
Eugene Kim <eugene70kim@gmail.com>
2020-11-12 18:37:09 +0900

    feat: translate judge to String
    - translate judge to human readable String

--- src/main/java/Judge.java
+++ src/main/java/Judge.java
@@ -1,9 +1,15 @@
 /**
  * 숫자야구게임 심판 유틸리티 클래스.
  * - 판정을 한다.
- * - TODO: 판정 결과를 말한다(인간의 말로 변환한다).
+ * - 판정을 말한다.
  */
 public final class Judge {
+
+    static final String STRIKE_OUT = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    static final String NOTHING = "낫싱";
+    static final String STRIKE = "스트라이크";
+    static final String BALL = "볼";
+    static final String EMPTY = "";
 
     /**
      * 숫자야구게임 판정 메서드. 두개의 int[3] 배열을 받아서 각 자리의 원소가 일치하는 경우 스트라이크, 자리는 다르지만 같은 수인
@@ -27,6 +33,22 @@ public final class Judge {
         result[1] += numbers1[2] == numbers2[0] ? 1 : 0;
         result[1] += numbers1[2] == numbers2[1] ? 1 : 0;
         return result;
+    }
+
+    /**
+     * 심판이 판정을 말하다. 판정결과 숫자 배열을 사용자에게 보여줄 문자열로 변환한다.
+     *
+     * @param judge 판정결과 정수 배열. [0]번째 원소는 스트라이크 수, [1]번째 원소는 볼의 수이다.
+     * @return 판정결과 문자열
+     */
+    public static String speak(int[] judge) {
+        if (judge[0] == 3) {
+            return STRIKE_OUT;
+        }
+        if (judge[0] == 0 && judge[1] == 0) {
+            return NOTHING;
+        }
+        return (judge[0] == 0 ? EMPTY : judge[0] + STRIKE) + (judge[1] == 0 ? EMPTY : judge[1] + BALL);
     }
 
     private Judge() {}
--- src/test/java/JudgeTest.java
+++ src/test/java/JudgeTest.java
@@ -12,4 +12,17 @@ class JudgeTest {
         assertThat(Judge.judge(new int[]{1, 2, 3}, new int[]{7, 8, 9})).isEqualTo(new int[]{0, 0});
         assertThat(Judge.judge(new int[]{9, 5, 1}, new int[]{9, 4, 1})).isEqualTo(new int[]{2, 0});
     }
+
+    @Test
+    public void testJudgeSpeak() {
+        assertThat(Judge.speak(new int[]{3, 0})).isEqualTo(Judge.STRIKE_OUT);
+        assertThat(Judge.speak(new int[]{0, 0})).isEqualTo(Judge.NOTHING);
+        assertThat(Judge.speak(new int[]{1, 0})).isEqualTo(1 + Judge.STRIKE);
+        assertThat(Judge.speak(new int[]{2, 0})).isEqualTo(2 + Judge.STRIKE);
+        assertThat(Judge.speak(new int[]{0, 1})).isEqualTo(1 + Judge.BALL);
+        assertThat(Judge.speak(new int[]{0, 2})).isEqualTo(2 + Judge.BALL);
+        assertThat(Judge.speak(new int[]{0, 3})).isEqualTo(3 + Judge.BALL);
+        assertThat(Judge.speak(new int[]{1, 1})).isEqualTo(1 + Judge.STRIKE + 1 + Judge.BALL);
+        assertThat(Judge.speak(new int[]{1, 2})).isEqualTo(1 + Judge.STRIKE + 2 + Judge.BALL);
+    }
 }
