85521a1554aa9aa25998c55f3031ec6d3aa2c3f4
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-11-12 21:37:31 +0900

    feat(BaseballGameRunner): can contiune game
    - 플레이어가 야구게임을 한판한 뒤 계속할지 그만둘지 여부를 선택할 수 있도록 기능을 구현했습니다.

--- src/main/java/ui/BaseballGameRunner.java
+++ src/main/java/ui/BaseballGameRunner.java
@@ -7,22 +7,31 @@ import domain.RandomBaseballNumbersGenerator;
 import java.util.Scanner;
 
 public class BaseballGameRunner {
-    private static final String GAME_ENDING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_ENDING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         BaseballGameTemplate.runBaseballGame(() -> {
-            BaseballGame baseballGame = BaseballGame.init(new RandomBaseballNumbersGenerator());
+            boolean isContinued = true;
+            while (isContinued) {
+                BaseballGame baseballGame = BaseballGame.init(new RandomBaseballNumbersGenerator());
 
-            playOneRound(baseballGame);
+                playOneRound(baseballGame);
 
-            System.out.println(GAME_ENDING);
+                System.out.println(GAME_ENDING);
+
+                String flag = scanner.nextLine();
+                if (flag.equals("2")) {
+                    isContinued = false;
+                }
+            }
         });
     }
 
     private static void playOneRound(BaseballGame baseballGame) {
         while(!baseballGame.isFinished()) {
-            System.out.println("숫자를 입력해주세요: ");
+            System.out.print("숫자를 입력해주세요: ");
             UserInput userInput = new UserInput(scanner.nextLine());
 
             BaseballResult baseballResult = baseballGame.play(userInput.convertToBaseballNumbers());
