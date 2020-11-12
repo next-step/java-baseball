f1ac915fc70382b876dc10f346c009364de1ccbf
Eugene Kim <eugene70kim@gmail.com>
2020-11-12 17:57:49 +0900

    docs: add javadoc comment to NumberGenerator
    - for class and public method

--- src/main/java/NumberGenerator.java
+++ src/main/java/NumberGenerator.java
@@ -1,9 +1,17 @@
 import java.util.Random;
 
+/**
+ * 숫자야구게임을 위한 숫자 생성기. 규칙에 맞게 숫자배열을 생성하여 반환한다.
+ */
 public final class NumberGenerator {
 
     private static final Random random = new Random(System.nanoTime());
 
+    /**
+     * 숫자 생성 메서드. 1부터 9까지의 숫자를 3개 생성하여 정수형 배열로 반환한다.
+     *
+     * @return int[3] 생성된 숫자를 배열에 담아 반환한다.
+     */
     public static int[] generate() {
         return new int[] { random(), random(), random()};
     }
