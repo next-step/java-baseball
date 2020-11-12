9f6e81cf2cde4fb76ddbf5236f4a49144e65f066
kimchanghyeondev <kimchanghyeon1201@gmail.com>
2020-11-12 23:02:06 +0900

    사용자 정상여부 입력 예외처리

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,8 +1,12 @@
 package baseball;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     private Referee referee;
     private NumberGenerator numberGenerator;
+    private Scanner sc = new Scanner(System.in);
 
     private Main() {//prevent Default Constructor
 
@@ -17,5 +21,21 @@ public class Main {
 
     public void start() {
         numberGenerator.getNotDuplicatedEachHundredNumber();
+        int inputedNumberByPlayer = getInputNumberByPlayer();
+
+    }
+
+    private int getInputNumberByPlayer() throws InputMismatchException {
+        int number = 0;
+        try {
+            System.out.println("세자리 숫자를 입력해주세요 =>");
+            number = sc.nextInt();
+        } catch (InputMismatchException inputMismatchException) {
+            sc = new Scanner(System.in);
+            getInputNumberByPlayer();
+        } finally {
+            number = number > 99 && number < 1000 ? number : getInputNumberByPlayer();
+        }
+        return number;
     }
 }
