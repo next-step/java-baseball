6217a4bdcee5dc66bf594c4561cdabb06f82ec97
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-12 22:44:15 +0900

    test : 1~9 사이의 숫자만 생성 하는지 테스트
    - 1~9 이외의 숫자를 받으면 예외처리

--- src/test/java/baseball/domain/BallNumberTest.java
+++ src/test/java/baseball/domain/BallNumberTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class BallNumberTest {
 
     @DisplayName("공 생성")
@@ -11,5 +13,14 @@ class BallNumberTest {
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9})
     public void constructor_success(int input) {
         new BallNumber(input);
+    }
+
+    @DisplayName("1~9 이의 공 번호만 생성 가능")
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -50, -1, 0, 10, 20, 30, 100})
+    public void constructor_fail(int input) {
+        assertThatThrownBy(
+                () -> new BallNumber(input)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
