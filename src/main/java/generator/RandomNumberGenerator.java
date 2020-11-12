5bb946d3acc0aaa91283a0bd3677425bf3ebeaf1
shk3029 <shk30290@naver.com>
2020-11-13 01:35:33 +0900

    fix: 난수 생성 리턴 타입을 Set<Integer> -> int[]로 변경

--- src/main/java/generator/RandomNumberGenerator.java
+++ src/main/java/generator/RandomNumberGenerator.java
@@ -1,18 +1,29 @@
 package generator;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
     @Override
-    public Set<Integer> generate(int size) {
+    public int[] generate(int size) {
         Set<Integer> numbers = new HashSet<>();
 
         while (numbers.size() < size) {
             int random = (int)(Math.random() * 8) + 1;
             numbers.add(random);
         }
-        return numbers;
+        return setToArray(numbers);
+    }
+
+    private int[] setToArray(Set<Integer> set) {
+        int[] result = new int[set.size()];
+        Iterator<Integer> iterator = set.iterator();
+        int index=0;
+        while (iterator.hasNext()) {
+            result[index++] = iterator.next();
+        }
+        return result;
     }
 }
