4f4efb12d4fbefcc034211981b2d95c41f772887
somi <somidevelop@gmail.com>
2020-11-12 16:21:40 +0900

    style: todo 주석추가

--- src/main/java/NumberBaseballGame.java
+++ src/main/java/NumberBaseballGame.java
@@ -6,12 +6,23 @@ public class NumberBaseballGame {
 		
 		Scanner scanner = new Scanner(System.in);
 		
+			
 		System.out.print("숫자를 입력해주세요 : ");
 		
 		String str = scanner.next();
 		
 		System.out.println(str);
-
+		
+		
+		// 랜덤 세자리 정수 생성
+		
+		
+		// 숫자 입력 받아서 스트라이크/ 볼 / 게임종료 판정
+		
+		
+		// 게임을 새로 시작할지 ? 게임을 종료할지 여부 받기
+			
+		
 		scanner.close();
 		
 	}
