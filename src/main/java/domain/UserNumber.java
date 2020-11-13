8299765eba2e19a0f7f924d940130cbba1e9a911
ohjuntaek <201211734@pukyong.ac.kr>
2020-11-13 22:57:23 +0900

    feat: 각 자리 수에 같은 숫자가 있으면 예외를 발생시킨다.

--- src/main/java/domain/UserNumber.java
+++ src/main/java/domain/UserNumber.java
@@ -1,5 +1,9 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UserNumber {
     private final Integer number;
 
@@ -9,13 +13,23 @@ public class UserNumber {
     }
 
     private void validate(int number) {
-        validateThreeLengthNumber(number);
+        validateThreeLength(number);
+        validateDuplication(number);
     }
 
-    protected static void validateThreeLengthNumber(int randomNumber) {
+    protected static void validateThreeLength(int randomNumber) {
         String numberString = String.valueOf(randomNumber);
         if (numberString.length() != 3) {
             throw new IllegalArgumentException("입력 숫자는 3자리 수여야 합니다.");
+        }
+    }
+
+    protected static void validateDuplication(int number) {
+        String numberString = String.valueOf(number);
+        String[] split = numberString.split("");
+        Set<String> set = new HashSet<>(Arrays.asList(split));
+        if (set.size() != 3) {
+            throw new IllegalArgumentException("중복되지 않은 숫자를 입력하세요.");
         }
     }
 }
