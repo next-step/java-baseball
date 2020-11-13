7339aa27bd51e0b97258eccd1d69e288a223f101
mook1594 <wjdanr89@gmail.com>
2020-11-14 01:42:20 +0900

    feat: 볼카운트 계산 로직 구현
    - 볼카운트 계산 로직 구현
    - 볼카운트 계산 로직 테스트 코드 통과

---
+++ src/main/java/baseball/BallCount.java
@@ -0,0 +1,40 @@
+package baseball;
+
+public class BallCount {
+
+    public static final int BALL_COUNT_SIZE = 3;
+
+    private int strike;
+    private int ball;
+
+    public BallCount(){
+        this.strike = 0;
+        this.ball = 0;
+    }
+
+    public boolean isNoting(){
+        return strike == 0 && ball == 0 ? true : false;
+    }
+
+    public boolean isEndGame(){
+        return strike == 3 ? true : false;
+    }
+
+    public void addStrike(){
+        this.strike++;
+    }
+
+    public void addBall(){
+        this.ball++;
+    }
+
+    @Override
+    public String toString(){
+        return new StringBuilder()
+                .append(strike)
+                .append("S")
+                .append(ball)
+                .append("B")
+                .toString();
+    }
+}
--- src/main/java/baseball/BallCountChecker.java
+++ src/main/java/baseball/BallCountChecker.java
@@ -1,7 +1,31 @@
 package baseball;
 
 public class BallCountChecker {
-    public static String check(String number, String answer) {
-        return null;
+    public static String check(final String input, final String answer) {
+        BallCount ballCount = new BallCount();
+
+        for(int i = 0; i < BallCount.BALL_COUNT_SIZE; i++) {
+            if(isStrike(input.charAt(i), answer.charAt(i))){
+                ballCount.addStrike();
+            } else if(isBall(input.charAt(i), answer)){
+                ballCount.addBall();
+            }
+        }
+
+        return ballCount.toString();
+    }
+
+    private static boolean isStrike(final char input, final char answer) {
+        if(input == answer){
+            return  true;
+        }
+        return false;
+    }
+
+    private static boolean isBall(final char input, final String answer) {
+        if(answer.contains(Character.toString(input))){
+            return true;
+        }
+        return false;
     }
 }
