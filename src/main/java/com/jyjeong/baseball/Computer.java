eaba62031fd53e3aaa65d8a21426b35e3311d26a
jyjeong620 <jyjeong620@naver.com>
2020-11-13 11:55:10 +0900

    feat : 난수의 중복 체크 로직 추가

--- src/main/java/com/jyjeong/baseball/Computer.java
+++ src/main/java/com/jyjeong/baseball/Computer.java
@@ -2,12 +2,22 @@ package com.jyjeong.baseball;
 
 public class Computer {
 
+    NumberCheck uniqueNumber = new NumberCheck();
+
     public int[] createRandom(){
         int[] randomNumber = new int[3];
         for(int i = 0 ; i < randomNumber.length ; i++){
             randomNumber[i] = (int)((Math.random()*9)+1);
         }
-
         return randomNumber;
+    }
+
+    public int[] computerNumber(){
+        int[] computerNumber;
+        do{
+            computerNumber = createRandom();
+        }while (!uniqueNumber.uniqueCheck(computerNumber));
+
+        return computerNumber;
     }
 }
