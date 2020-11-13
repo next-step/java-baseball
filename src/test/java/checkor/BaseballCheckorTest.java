f64d46ddaeddeeeed533fd26ab8eebe85f5880c9
shk3029 <shk30290@naver.com>
2020-11-13 11:47:00 +0900

    test: strike, ball, nothing 기능 및 통합 테스트

--- src/test/java/checkor/BaseballCheckorTest.java
+++ src/test/java/checkor/BaseballCheckorTest.java
@@ -2,7 +2,13 @@ package checkor;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BaseballCheckorTest {
 
@@ -14,22 +20,43 @@ class BaseballCheckorTest {
         baseballCheckor = new BaseballCheckor(numbers);
     }
 
-    @Test
-    @DisplayName("숫자를 입력하면 strike 개수, ball 개수 또는 낫싱을 알려주는 테스트")
-    public void check() {
+    @ParameterizedTest
+    @MethodSource("generateCheckData")
+    @DisplayName("숫자를 입력하면 strike 개수, ball 개수 또는 낫싱을 알려주는 통합 테스트")
+    public void check(int[] input, String expected) {
+        assertThat(baseballCheckor.check(input)).isEqualTo(expected);
     }
 
-
-    @Test
-    @DisplayName("strike 개수 맞추기 테스트")
-    public void countStrike() {
-
+    @ParameterizedTest
+    @MethodSource("generateStrikeAndBallData")
+    @DisplayName("strike, ball 개수 맞추기 기능 테스트")
+    public void countStrike(int[] input, int strikeExpected, int ballExpected) {
+        baseballCheckor.setInput(input);
+        int strike = baseballCheckor.strike(input);
+        int ball = baseballCheckor.ball(input, strike);
+        assertThat(strike).isEqualTo(strikeExpected);
+        assertThat(ball).isEqualTo(ball);
     }
-    @Test
-    @DisplayName("ball 개수 맞추기 테스트")
-    public void countBall() {
 
+    static Stream<Arguments> generateCheckData() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,3}, "1스트라이크 "),
+                Arguments.of(new int[]{1,7,9}, "3스트라이크 "),
+                Arguments.of(new int[]{1,9,7}, "1스트라이크 2볼 "),
+                Arguments.of(new int[]{1,7,2}, "2스트라이크 "),
+                Arguments.of(new int[]{7,9,1}, "3볼 "),
+                Arguments.of(new int[]{2,5,8}, "낫싱")
+        );
     }
 
-
+    static Stream<Arguments> generateStrikeAndBallData() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,3}, 1, 0),
+                Arguments.of(new int[]{1,7,9}, 3, 0),
+                Arguments.of(new int[]{1,9,7}, 1, 2),
+                Arguments.of(new int[]{1,7,2}, 2, 1),
+                Arguments.of(new int[]{7,9,1}, 0, 3),
+                Arguments.of(new int[]{2,5,8}, 0, 0)
+        );
+    }
 }
