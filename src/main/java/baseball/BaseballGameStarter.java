5d40be29dd50f8b6a98bffb852ce5e0cb289e407
star-no <nxsung@naver.com>
2020-11-13 23:21:57 +0900

    feat(#5) BaseballGameScanner 클래스 생성
    * scan, checkValidate 메소드 작성

--- src/main/java/baseball/BaseballGameStarter.java
+++ src/main/java/baseball/BaseballGameStarter.java
@@ -2,13 +2,17 @@ package baseball;
 
 public class BaseballGameStarter {
 
+    private BaseballGameScanner baseballGameScanner = new BaseballGameScanner();
+
     public void start() {
         do {
             sayHello();
+            String inputData = baseballGameScanner.scan();
+
         } while (false); // todo 게임 종료 판단로직이 나오면 변경할 것
     }
 
     private void sayHello() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 }
