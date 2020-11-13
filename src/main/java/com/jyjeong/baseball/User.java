0197982f11bb5a3960f8ee80cb290b22314c397e
jyjeong620 <jyjeong620@naver.com>
2020-11-13 11:56:08 +0900

    feat : 사용자가 입력한 숫자 정합성 체크로직 추가

--- src/main/java/com/jyjeong/baseball/User.java
+++ src/main/java/com/jyjeong/baseball/User.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class User {
 
+    NumberCheck numberCheck = new NumberCheck();
+
     /**
      * 사용자의 값 입력받는 함수
      */
@@ -24,12 +26,27 @@ public class User {
     public int[] validate(String data){
         int[] number = new int[3];
 
-        //TODO 숫자 여부 체크
+        if(!numberCheck.numberCheck(data)){
+            throw new RuntimeException("입력된 값이 숫자가 아닙니다");
+        }
 
-        //TODO 숫자의 범위 체크
+        if(!numberCheck.lengthCheck(data)){
+            throw new RuntimeException("입력된 값의 범위가 초과 되었습니다.");
+        }
 
-        //TODO 숫자의 중복여부 체크
+        number = stringToIntArray(data);
+
+        if(!numberCheck.uniqueCheck(number)){
+            throw new RuntimeException("입력된 값중 중복된 값이 있습니다");
+        }
         return number;
     }
 
+    public int[] stringToIntArray(String data){
+        int[] userNumber = new int[3];
+        for(int i = 0 ; i < data.length() ; i++){
+            userNumber[i] = Character.digit(data.charAt(i),10);
+        }
+        return userNumber;
+    }
 }
