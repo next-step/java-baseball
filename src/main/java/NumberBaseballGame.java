c674e03e8a847e77fa855727a1046247d57a776e
somi <somidevelop@gmail.com>
2020-11-13 12:59:17 +0900

    refactor: 입력받는 문자 예외처리 추가

--- src/main/java/NumberBaseballGame.java
+++ src/main/java/NumberBaseballGame.java
@@ -44,11 +44,9 @@ public class NumberBaseballGame {
 	 */
 	private static void playBaseball(int randomNum) {
 		boolean isKeep = true;
+		String showMsg = "숫자를 입력해주세요 : ";
 		do {
-			System.out.print("숫자를 입력해주세요 : ");
-
-			String inputNumStr = scanner.next();
-			
+			String inputNumStr = inputNumber(RANDOM_DIGIT, showMsg);
 			int inputNum = Integer.parseInt(inputNumStr);
 			int strikeCount = countStrike(randomNum, inputNum);
 			int ballCount = countBall(randomNum, inputNum) - strikeCount;
@@ -186,14 +184,104 @@ public class NumberBaseballGame {
 	 * 
 	 * @param strikeCount 스트라이크
 	 * @return true(게임지속), false(게임종료)
-	 * @author somi
 	 * 
 	 */
 	private static boolean isKeepGame() {
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-		String str = scanner.next();
-		//str 예외처리 함수 TODO
+		String showMsg = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : ";
+		String str = inputNumber(1, showMsg);
 		return (str.equals("1")) ? true : false;
+	}
+	
+	
+	/**
+	 * 
+	 * 숫자 입력받는 메서드
+	 * 
+	 * @param digit 입력받아야할 숫자 자릿수, showMsg 숫자받을때 출력할 메시지
+	 * @return inputNum 입력받은 숫자
+	 * 
+	 */
+	private static String inputNumber(int digit, String showMsg) {
+		boolean isCorrect = false;
+		String inputNum = "";
+		
+		while (!isCorrect) {
+			System.out.print(showMsg);
+			inputNum = scanner.next();
+			isCorrect = inputNumberException(inputNum, digit);
+		}
+		
+		return inputNum;
+	}
+	
+	
+	/**
+	 * 
+	 * 입력받은 숫자 예외처리
+	 * 
+	 * @param inputNum 입력받은 숫자, digit 입력받아야할 숫자 자릿수
+	 * @return true(게임지속), false(게임종료)
+	 * 
+	 */
+	private static boolean inputNumberException(String inputNum, int digit) {
+		if (!inputNumberIntegerException(inputNum) || !inputNumberZeroException(inputNum) 
+				|| !inputNumberLengthException(inputNum, digit)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * 
+	 * 입력받은 문자가 정수인지 체크
+	 * 
+	 * @param inputNum 입력받은 문자
+	 * @return true (정상), false(비정상)
+	 * 
+	 */
+	private static boolean inputNumberIntegerException(String inputNum) {
+		try {
+			Integer.parseInt(inputNum);
+		} catch (NumberFormatException e) {
+			System.out.println("양의 정수만 입력가능합니다.");
+			return false;
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * 
+	 * 입력받은 문자에 0이 포함되어 있는지 체크
+	 * 
+	 * @param inputNum 입력받은 문자
+	 * @return true (정상), false(비정상)
+	 * 
+	 */
+	private static boolean inputNumberZeroException(String inputNum) {
+		if (inputNum.contains("0")) {
+			System.out.println("0이아닌 양의 정수만 입력가능합니다.");
+			return false;
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * 
+	 * 입력받은 문자 자릿수가 맞는지 체크
+	 * 
+	 * @param inputNum 입력받은 문자, digit (입력받아야할 정수자릿수)
+	 * @return true (정상), false(비정상)
+	 * 
+	 */
+	private static boolean inputNumberLengthException(String inputNum, int digit) {
+		if (inputNum.length() != digit) {
+			System.out.println("입력문자의 자릿수를 확인해주세요.");
+			return false;
+		}
+		return true;
 	}
 	
 	
