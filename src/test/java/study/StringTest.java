6b67d6ee7d216a0877bbb916a957148ecae293d5
mook1594 <wjdanr89@gmail.com>
2020-11-14 00:52:09 +0900

    test: 요구사항3 테스트
    - assertThatThrownBy 함수 사용 익히기

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -32,5 +33,25 @@ public class StringTest {
         String result = input.substring(1, input.length() - 1);
 
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt() 테스트")
+    public void charAt(){
+        String input = "abc";
+
+        assertThat(input.charAt(0)).isEqualTo('a');
+        assertThat(input.charAt(1)).isEqualTo('b');
+        assertThat(input.charAt(2)).isEqualTo('c');
+
+        assertThatThrownBy(() -> {
+            input.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+
+        assertThatThrownBy(() -> {
+            input.charAt(-1);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: -1");
     }
 }
