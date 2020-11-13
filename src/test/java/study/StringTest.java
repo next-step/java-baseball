d9af1c6f43063ec1c8a934dbfbbcad48be3d40d5
parkeeseul <eeseul.p@gmail.com>
2020-11-13 22:38:27 +0900

    test: StringTest 추가

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,25 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+
+        assertThat(actual).hasSize(2)
+                .contains("1", "2")
+                .containsExactly("1", "2");
+    }
+
+    @Test
+    void split_just_one() {
+        String[] result = "1".split(",");
+
+        assertThat(result).hasSize(1)
+                .contains("1")
+                .containsExactly("1");
+    }
+}
