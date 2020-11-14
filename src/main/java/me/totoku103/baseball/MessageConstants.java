7716ff4701e1c62724ae4977960df8c82645b4f5
zkimsuh <shkim@j2stechno.com>
2020-11-14 17:27:56 +0900

    사용자 입력 기능
    불필요 문구 삭제
    정규식 자리 재수정

--- src/main/java/me/totoku103/baseball/MessageConstants.java
+++ src/main/java/me/totoku103/baseball/MessageConstants.java
@@ -5,8 +5,6 @@ public enum MessageConstants {
     INVALID_INPUT("다시 입력해주세요"),
     STRIKE("스트라이크"),
     BALL("볼"),
-    NO_MATCH("낫씽"),
-    GAME_END("숫자를 모두 맞히셨습니다! 게임 종료"),
     GMAE_MODE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
 
     private String message;
