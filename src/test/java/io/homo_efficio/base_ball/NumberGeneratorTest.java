a4dc770e8a845c14f2169b83988e17ad81ed20f8
Homo Efficio <homo.efficio@gmail.com>
2020-11-14 19:05:49 +0900

    feat(길이 일반화): 문자열 길이 3~9까지 수용하는 일반화 로직 실패 테스트 추가
    - 3이 매직 넘버처럼 사용되어 나중에 길이 변경 등 기능 확장 시 불편

--- src/test/java/io/homo_efficio/base_ball/NumberGeneratorTest.java
+++ src/test/java/io/homo_efficio/base_ball/NumberGeneratorTest.java
@@ -1,9 +1,14 @@
 package io.homo_efficio.base_ball;
 
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +27,25 @@ class NumberGeneratorTest {
 
         assertThat(number.length()).isEqualTo(3);
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "숫자 문자열 길이: {0}")
+    @MethodSource("len")
+    void check_arbitrary_len_digit_unique(int len) {
+        String number = NumberGenerator.generate(len);
+
+        Set<String> numbers = new HashSet<>(len);
+        String[] split = number.split("");
+        for (String num : split) {
+            numbers.add(num);
+        }
+
+        assertThat(number.length()).isEqualTo(len);
+        assertThat(numbers.size()).isEqualTo(len);
+    }
+
+    private static Stream<Arguments> len() {
+        return IntStream.range(3, 99)
+                .mapToObj(len -> Arguments.of(len));
     }
 }
