fa875f107534eb06af9ad6d8b1d34c2b89d20c86
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-14 12:08:30 +0900

    feat : 랜덤 숫자 생성 전략 추가
    - 숫자 3개 생성 전략 추가
    - 룰이 변경될경우 번호 생성 전략 교체할 수 있도록 interface 추가

---
+++ src/main/java/baseball/domain/ThreeBallNumberStrategy.java
@@ -0,0 +1,33 @@
+package baseball.domain;
+
+import baseball.util.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreeBallNumberStrategy implements RandomNumberStrategy {
+
+    private static final int BALL_NUMBER_SIZE = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    private List<Integer> numbers = new ArrayList<>();
+
+    @Override
+    public List<Integer> generate() {
+        while (numbers.size() < BALL_NUMBER_SIZE) {
+            addNumber();
+        }
+
+        return Collections.unmodifiableList(numbers);
+    }
+
+    private void addNumber() {
+        int randomNumber = RandomUtils.getRandomNumber(MIN_NUMBER, MAX_NUMBER);
+
+        if (!numbers.contains(randomNumber)) {
+            numbers.add(randomNumber);
+        }
+    }
+}
