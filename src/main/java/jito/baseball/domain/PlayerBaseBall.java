d0557533f0748cb65989cfc97b02e09f0bda8e15
jiho <jhhj424@naver.com>
2020-11-15 01:33:38 +0900

    feat: 플레이어한테 입력받을 숫자 클래스 PlayerBaseBall 클래스 구현
    플레이어가 입력한 값을 생성자 매개변수로 받아서, validation check를 진행하고 숫자를 생성함

---
+++ src/main/java/jito/baseball/domain/PlayerBaseBall.java
@@ -0,0 +1,53 @@
+package jito.baseball.domain;
+
+import jito.baseball.message.ErrorMessage;
+import jito.baseball.util.BaseballException;
+
+public class PlayerBaseBall implements NumberBaseBall {
+
+    private final int[] playerNumbers;
+
+    public PlayerBaseBall(String input) {
+        String[] playerInput = input.split("");
+        playerNumbers = new int[playerInput.length];
+        for (int i=0; i<playerInput.length; i++) {
+            playerNumbers[i] = Integer.parseInt(playerInput[i]);
+        }
+        checkValidation();
+    }
+
+    public int[] getPlayerNumbers() {
+        return playerNumbers;
+    }
+
+    @Override
+    public void isNotOneToNineNumber() {
+        for (int number : playerNumbers) {
+            if (number < 1 || number > 9) {
+                BaseballException.throwException(ErrorMessage.EACH_NUMBER_RANGE_ERROR);
+            }
+        }
+    }
+
+    @Override
+    public void isNotThreeDigits() {
+        if (playerNumbers.length != 3) {
+            System.out.println(playerNumbers.length);
+            BaseballException.throwException(ErrorMessage.DIGITS_ERROR);
+        }
+    }
+
+    @Override
+    public void isDuplicate() {
+        if ((playerNumbers[0] == playerNumbers[1] || playerNumbers[0] == playerNumbers[2]) || playerNumbers[1] == playerNumbers[2]) {
+            BaseballException.throwException(ErrorMessage.DUPLICATE_ERROR);
+        }
+    }
+
+    @Override
+    public void checkValidation() {
+        isNotOneToNineNumber();
+        isNotThreeDigits();
+        isDuplicate();
+    }
+}
