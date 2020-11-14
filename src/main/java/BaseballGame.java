c973c775424a3a986d8746316e199f2443f7e73f
minjikim <carrykim1126@gmail.com>
2020-11-14 23:58:57 +0900

    Refactor random number for computer

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class BaseballGame {
-    public static void main(String[] args) {
+    public String num(){
         Random r = new Random();
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int tmp = 0;
@@ -11,8 +11,12 @@ public class BaseballGame {
             arr[0] = arr[j];
             arr[j] = tmp;
         }
-        String com = ""+arr[0]+arr[1]+arr[2];
-        System.out.print("컴퓨터 수 : " + com);
+        String num = ""+arr[0]+arr[1]+arr[2];
+        return num;
+    }
 
+    public static void main(String[] args) {
+        String com = num();
+        System.out.print("컴퓨터 수 : " + com);
     }
 }
