8951203c0ef830868023c939f3cba27a8aff3b19
Lee kang-yong <yebgi83@gmail.com>
2020-11-15 03:22:10 +0900

    refactor : 패턴 분석 단위 테스트에 대한 메소드 이름 수정
    초기에 기능 정의시에 지은 메소드 이름이어서, 의미 있는 이름으로 변경

--- src/test/java/PatternDisplayFormatterTest.java
+++ src/test/java/PatternDisplayFormatterTest.java
@@ -8,56 +8,56 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PatternDisplayFormatterTest {
     @Test
     @DisplayName("1개의 스트라이크가 있으면, '1 스트라이크' 리턴")
-    void case1() {
+    void formatOneStrike() {
         PatternResult patternResult = new PatternResult(1, 0);
         assertEquals(PatternDisplayFormatter.format(patternResult), "1 스트라이크");
     }
 
     @Test
     @DisplayName("2개의 스트라이크가 있으면, '2 스트라이크' 리턴")
-    void case2() {
+    void formatTwoStrikes() {
         PatternResult patternResult = new PatternResult(2, 0);
         assertEquals(PatternDisplayFormatter.format(patternResult), "2 스트라이크");
     }
 
     @Test
     @DisplayName("3개의 스트라이크가 있으면, '3 스트라이크' 리턴")
-    void case3() {
+    void formatThreeStrikes() {
         PatternResult patternResult = new PatternResult(3, 0);
         assertEquals(PatternDisplayFormatter.format(patternResult), "3 스트라이크");
     }
 
     @Test
     @DisplayName("1개의 볼이 있으면, '1 볼' 리턴")
-    void case4() {
+    void formatOneBall() {
         PatternResult patternResult = new PatternResult(0, 1);
         assertEquals(PatternDisplayFormatter.format(patternResult), "1 볼");
     }
 
     @Test
     @DisplayName("2개의 볼이 있으면, '2 볼' 리턴")
-    void case5() {
+    void formatTwoBalls() {
         PatternResult patternResult = new PatternResult(0, 2);
         assertEquals(PatternDisplayFormatter.format(patternResult), "2 볼");
     }
 
     @Test
     @DisplayName("3개의 볼이 있으면, '3 볼' 리턴")
-    void case6() {
+    void formatThreeBalls() {
         PatternResult patternResult = new PatternResult(0, 3);
         assertEquals(PatternDisplayFormatter.format(patternResult), "3 볼");
     }
 
     @Test
     @DisplayName("1개의 스트라이크 및 2개의 볼이 있으면, '2 스트라이크, 1 볼' 리턴")
-    void case7() {
+    void formatTwoStrikesAndOneBall() {
         PatternResult patternResult = new PatternResult(2, 1);
         assertEquals(PatternDisplayFormatter.format(patternResult), "2 스트라이크, 1 볼");
     }
 
     @Test
     @DisplayName("스트라이크 또는 볼이 전혀 없으면, '낫싱' 리턴")
-    void case8() {
+    void formatNoStrikeAndBall() {
         PatternResult patternResult = new PatternResult(0, 0);
         assertEquals(PatternDisplayFormatter.format(patternResult), "낫싱");
     }
