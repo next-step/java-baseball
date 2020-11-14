6b33a0c891c9673e2bc89e58944e81648a1aff78
Lee kang-yong <yebgi83@gmail.com>
2020-11-15 01:35:41 +0900

    feat : 정답인지 체크하기 위한 기능에 대한 단위 테스트
    정답인지 체크하기 위한 기능에 대한 기능 정의 및 단위 테스트 추가

---
+++ src/test/java/AnswerCheckerTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AnswerCheckerTest {
+    @Test
+    @DisplayName("3 스트라이크는 정답이어야 한다.")
+    void checkTrue_IfThreeStrikesShouldBeAnswer() {
+    }
+
+    @Test
+    @DisplayName("3 스트라이크 이외에는 정답이 아니어야 한다.")
+    void checkTrue_IfAllCasesExceptForThreeStrikesShouldNotBeAnswer() {
+    }
+}
