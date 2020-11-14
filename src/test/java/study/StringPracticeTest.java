300a4fa71467b72263184af33be663dfc57c56c4
“nhs0912” <nhs0912@nate.com>
2020-11-14 16:09:22 +0900

    fix:class Name 수정

--- src/test/java/study/StringPracticeTest.java
+++ src/test/java/study/StringPracticeTest.java
@@ -1,23 +1,21 @@
 package study;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-class StringTestTest {
+class StringTest {
 
 
     @Test
     @DisplayName("2개이상 문자열 comma(,) 분리 테스트")
     void divideTwoMoreCommaTest(){
         //given
-        StringTest strTest = new StringTest();
-        String givenText ="1,2";
+        String strTest = new String();
+        java.lang.String givenText ="1,2";
         //when
-        String[] strings = strTest.divideComma(givenText);
+        java.lang.String[] strings = strTest.divideComma(givenText);
         //then
         assertThat(strings).as("문자열 \"1,2\"를 comma로 분리하기").contains("1","2");
     }
@@ -26,10 +24,10 @@ class StringTestTest {
     @DisplayName("1개 문자열 comma(,) 분리 테스트")
     void divideOnlyOneCommaTest(){
         //given
-        StringTest strTest = new StringTest();
-        String givenText ="1";
+        String strTest = new String();
+        java.lang.String givenText ="1";
         //when
-        String[] strings = strTest.divideComma(givenText);
+        java.lang.String[] strings = strTest.divideComma(givenText);
         //then
         assertThat(strings).as("문자열 \"1\"를 comma로 분리하기").contains("1");
     }
@@ -39,10 +37,10 @@ class StringTestTest {
     @DisplayName("(1,2)에서 ()괄호 없애기")
     void removeParenthesisTest(){
         //given
-        StringTest strTest = new StringTest();
-        String givenText = "(1,2)";
+        String strTest = new String();
+        java.lang.String givenText = "(1,2)";
         //when
-        String result = strTest.removeParenthesis(givenText);
+        java.lang.String result = strTest.removeParenthesis(givenText);
         //then
         assertThat(result).as("(1,2) -> 1,2로 변환").isEqualTo("1,2");
     }
