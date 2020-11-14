40cb745ccafd95e56fbc5b71f9a5db2755c66602
Lee Dong Wook <sh95119@gmail.com>
2020-11-14 17:48:42 +0900

    feat: BaseballGame 클래스 생성 및 필드 추가
    BaseballGame 클래스 생성 및 문제와 정답 필드 생성 및 초기화

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,12 @@
+import java.util.LinkedHashSet;
+
+public class BaseballGame {
+
+	private LinkedHashSet<Integer> question;
+	private LinkedHashSet<Integer> answer;
+
+	BaseballGame() {
+		question = new LinkedHashSet<>(3);
+		answer = new LinkedHashSet<>(3);
+	}
+}
