8fde821e3f2c93d0ba75e52e20b189cd020bf569
somi <somidevelop@gmail.com>
2020-11-14 22:48:08 +0900

    feat: String요구사항3-1, 3-3완료
    "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
    JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.

--- src/main/java/JunitString.java
+++ src/main/java/JunitString.java
@@ -27,5 +27,11 @@ public class JunitString {
 
 		return str;
 	}
+	
+	// String 요구사항3
+	// "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+	public char charAtStrReturn(String str, int index) {
+		return str.charAt(index);
+	}
 
 }
--- src/test/java/JunitStringTest.java
+++ src/test/java/JunitStringTest.java
@@ -1,8 +1,10 @@
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("String Test")
 public class JunitStringTest {
 
 	@Test
@@ -45,6 +47,17 @@ public class JunitStringTest {
 		JunitString junitStudy = new JunitString();
 		String actual = junitStudy.subStringParenthesis("(1,2)");
 		assertThat(actual).isEqualTo("1,2");
+	}
+
+	// String 요구사항3-1, 3-3
+	// "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+	// JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+	@Test
+	@DisplayName("abc 문자중 지정한 위치 문자 리턴 하는지 여부")
+	public void testCharAtStrReturn() {
+		JunitString junitStudy = new JunitString();
+		char actual = junitStudy.charAtStrReturn("abc", 1);
+		assertThat(actual).isEqualTo('b');
 	}
 
 }
