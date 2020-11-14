e5fe4c8ce590b40036af5fa69e232071221475fa
bbbnam <bbbnam@naver.com>
2020-11-15 03:41:40 +0900

    feat : 랜덤 숫자 생성 객체 및 인터페이스 생성 & 구현

---
+++ src/main/java/AutoChoiceStrategy.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AutoChoiceStrategy implements ChoiceStrategy {
+    private static final int NUMBER_MIN = 1;
+    private static final int NUMBER_MAX = 9;
+
+    private static final List<Number> NUMBERS;
+
+    static {
+        NUMBERS = new ArrayList<>();
+        for (int i = NUMBER_MIN; i <= NUMBER_MAX; i++) {
+            NUMBERS.add(Number.valueOf(i));
+        }
+    }
+
+    @Override
+    public Numbers generate() {
+        Collections.shuffle(NUMBERS);
+        return Numbers.valueOf(NUMBERS.subList(0, 3));
+    }
+}
