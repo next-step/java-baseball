2db9977da86318592d759c5a13f83cced23ee101
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-14 14:32:26 +0900

    test : 3개 공을 생성할때 주입된 전략에 대한 테스트
    - 주입된 전략이 null일 경우

--- src/test/java/baseball/domain/ThreeBallTest.java
+++ src/test/java/baseball/domain/ThreeBallTest.java
@@ -4,8 +4,11 @@ import baseball.domain.strategy.BallGenerateStrategy;
 import baseball.domain.strategy.RandomNumberGenerateStrategy;
 import baseball.domain.strategy.RandomNumberStrategy;
 import baseball.domain.strategy.ThreeBallGenerateStrategy;
+import baseball.exception.BallException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ThreeBallTest {
 
@@ -18,6 +21,16 @@ class ThreeBallTest {
 
         //when
         new ThreeBall(ballStrategy);
+    }
+
+    @DisplayName("공 생성 전략 - 주입 테스트")
+    @Test
+    public void strategy_fail() throws Exception {
+        //then
+        assertThatThrownBy(
+                () -> new ThreeBall(null)
+        ).isInstanceOf(BallException.class)
+                .hasMessage("공 생성에 실패 하였습니다.");
     }
 
 }
