d38b5a1a46b668eb2b0991419c04a109d3beae25
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-14 13:41:11 +0900

    test : 미중복 랜덤 숫자 3개가 생성 안됐을 때 체크
    - 미 중복 숫자가 3개 안나왔을때 exception 테스트

--- src/test/java/baseball/domain/ThreeBallGenerateStrategyTest.java
+++ src/test/java/baseball/domain/ThreeBallGenerateStrategyTest.java
@@ -1,10 +1,13 @@
 package baseball.domain;
 
+import baseball.exception.IllegalBallNumberException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ThreeBallGenerateStrategyTest {
 
@@ -12,7 +15,7 @@ class ThreeBallGenerateStrategyTest {
 
     @DisplayName("생성된 숫자가 3가지 인치 체크")
     @Test
-    public void generate() throws Exception {
+    public void generate_success() throws Exception {
         //given
         BallGenerateStrategy generator = new ThreeBallGenerateStrategy(new RandomNumberGenerateStrategy());
 
@@ -21,6 +24,19 @@ class ThreeBallGenerateStrategyTest {
 
         //then
         Assertions.assertTrue(result.size() == BALL_GENERATOR_RESULT_SIZE);
+    }
+
+    @DisplayName("미 중복 랜덤 숫자가 3개가 안나왔을 때")
+    @Test
+    public void generate_fail() throws Exception {
+        //given
+        BallGenerateStrategy generator = new ThreeBallGenerateStrategy(() -> 1);
+
+        //then
+        assertThatThrownBy(
+                () -> generator.generate()
+        ).isInstanceOf(IllegalBallNumberException.class)
+                .hasMessage("랜덤 숫자 3개가 생성 되어야 합니다.");
     }
 
 }
