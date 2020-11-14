b8c69f93292f0b66ed927c31044c4d41cab0fa9b
egjeon <dmsrhkd132@gmail.com>
2020-11-15 02:36:14 +0900

    test : MessageExceptionTest클래스 생성 후, 기능 테스트. - 예외처리 util 기능 테스트

---
+++ src/test/java/exception/MessageExceptionTest.java
@@ -0,0 +1,56 @@
+package exception;
+
+
+import org.assertj.core.api.ThrowableTypeAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MessageExceptionTest {
+
+    private Set<Integer> numbers;
+    private String input;
+    @BeforeEach
+    public void setUp(){
+        input = "123";
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+    @DisplayName("중복된 숫자 체크")
+    @Test
+     void duplicateCheck() {
+        numbers = new HashSet<>();
+        for(int i = 0; i < input.length(); i++){
+            System.out.println(input.charAt(i)-'0');
+            numbers.add(input.charAt(i)-'1');
+        }
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("숫자 길이 체크")
+    @Test
+    void lengthCheck(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("숫자만으로 이루어져 있는지 체크")
+    @Test
+    public void numberCheck() {
+        boolean exceptionCatched = false;
+        try{
+            Integer.parseInt(input);
+        }catch (NumberFormatException e){
+            exceptionCatched = true;
+        }
+        assertThat(exceptionCatched).isFalse();
+    }
+
+}
