4951e0407b7ac5e9a3b497e345a229b94f8c105d
10M <tenmillionwon@gmail.com>
2020-11-14 13:17:35 +0900

    feat : baseball game ui
    Added new feature:
     - get number from player
     - can replay game if a user type 1 after ending one game

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,8 @@
 package baseball;
 
-import static baseball.GenerateComputerNumber.*;
-import static baseball.GetPlayerNumber.*;
+import static baseball.util.CompareComputerPlayerNumber.*;
+import static baseball.util.GenerateComputerNumber.*;
+import static baseball.util.GetPlayerNumber.*;
 
 import java.util.Scanner;
 import java.util.Set;
@@ -12,31 +13,69 @@ public class BaseballGame {
 	}
 
 	private static void start() {
-
 		boolean isPlaying = true;
 		while (isPlaying) {
 			Set<Integer> computerNumber = generateComputerNumber();
 			playingBaseball(computerNumber);
-			isPlaying = false;
+			isPlaying = askReplayingGame();
 		}
 	}
+
+	private static boolean askReplayingGame() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String command = getInputPlayerNumber();
+		if (command.equals("1")) {
+			return true;
+		}
+		return false;
+	}
+
 
 	private static void playingBaseball(Set<Integer> computerNumber) {
-		Scanner sc = new Scanner(System.in);
+		Boolean eachGamePlaying = true;
 
-		while (true) {
+		while (eachGamePlaying) {
 			System.out.print("숫자를 입력해주세요 : ");
-			String input = sc.next();
-			Set<Integer> playerNumber = getPlayerNumber(input);
-			checkPlayerNumber(computerNumber, playerNumber);
+			Set<Integer> playerNumber = getPlayerNumber(getInputPlayerNumber());
+			eachGamePlaying = checkPlayerNumber(computerNumber, playerNumber);
 		}
 
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
 
-	private static void checkPlayerNumber(Set<Integer> computerNumber, Set<Integer> playerNumber) {
+	private static String getInputPlayerNumber() {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		return input;
+	}
+	
+	private static Boolean checkPlayerNumber(Set<Integer> computerNumber, Set<Integer> playerNumber) {
 		if (playerNumber == null) {
 			System.out.println("1~9 사이의 서로 다른 3자리 숫자를 입력해주세요.");
+			return true;
+		}
+
+		Integer[] score = compareStrikeAndBall(computerNumber, playerNumber);
+		printResult(score);
+
+		return (score[0] == 3) ? false : true;
+	}
+
+	private static void printResult(Integer[] score) {
+		if (score[0] == 0 && score[1] == 0) {
+			System.out.println("낫싱");
 			return;
 		}
+		makeResultState(score);
+	}
+
+	private static void makeResultState(Integer[] score) {
+		if (score[0] > 0) {
+			System.out.print(score[0] + " 스트라이크 ");
+		}
+		if (score[1] > 0) {
+			System.out.print(score[1] + " 볼 ");
+		}
+		System.out.println("");
 	}
 }
