10b32afdc0114f25095063b0b3d86583b118f37f
limdingdong <goldbat9554@naver.com>
2020-11-14 15:25:50 +0900

    feat: main application 클래스, process 처리 클래스 등록
    App main 클래스 및 로직처리 클래스 신규생성
    
    Authored by Dongsub.Lim

---
+++ src/main/java/BaseballApp.java
@@ -0,0 +1,9 @@
+import baseball.process.BaseballGame;
+
+public class BaseballApp {
+
+    public static void main(String[] args) {
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.executeGame();
+    }
+}
---
+++ src/main/java/baseball/process/BaseballGame.java
@@ -0,0 +1,8 @@
+package baseball.process;
+
+public class BaseballGame {
+
+    public void executeGame() {
+        //TODO: 게임실행 로직 작성
+    }
+}
