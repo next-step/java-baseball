29cc1e6f3388a2f297f8d14a24de5f3001465f70
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-14 14:45:25 +0900

    feat : 공의 개수가 3개 인지 체크 로직 추가
    - 공이 3개가 안될때 exception

--- src/main/java/baseball/domain/ThreeBall.java
+++ src/main/java/baseball/domain/ThreeBall.java
@@ -10,7 +10,9 @@ import java.util.Objects;
 
 public class ThreeBall {
 
+    private static final int BALL_SIZE = 3;
     private static final String CONSTRUCT_ERROR_MESSAGE = "공 생성에 실패 하였습니다.";
+    private static final String SIZE_ERROR_MESSAGE = "공의 사이즈가 3개가 되어야 합니다.";
 
     private List<BallNumber> balls;
 
@@ -24,7 +26,14 @@ public class ThreeBall {
             result.add(new BallNumber(number));
         }
 
+        validateSize(result);
         this.balls = Collections.unmodifiableList(result);
+    }
+
+    private void validateSize(final List<BallNumber> balls) {
+        if (balls.size() != BALL_SIZE) {
+            throw new BallException(SIZE_ERROR_MESSAGE);
+        }
     }
 
     private void validateStrategy(final BallGenerateStrategy strategy) {
