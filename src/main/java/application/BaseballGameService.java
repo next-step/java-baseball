b48805ef18b08b5be7abd00be4a5582887e9130f
ChoiYunJin <zzdd1558@ogqcorp.com>
2020-11-14 14:06:42 +0900

    #4 Add 컴퓨터의 난수와 , 사용자가 입력한 수를 비교하여 , 볼 스트라이크 , 낫싱 or 포볼을 출력

---
+++ src/main/java/application/BaseballGameService.java
@@ -0,0 +1,57 @@
+package application;
+
+import utils.BaseballValidation;
+import domain.BaseballCountVo;
+
+public class BaseballGameService {
+
+  BaseballGameUserService baseballGameUserService = new BaseballGameUserService();
+
+  public void playGame() {
+    boolean gameStatus = true;
+    String computerNumbers = GenerateRandomNumber.generateRandomNumber();
+    do {
+      String player = baseballGameUserService.playerInputValue();
+
+      BaseballCountVo result = isResult(computerNumbers, player);
+
+      gameStatus = baseballGameUserService.printBallStatus(result);
+
+
+    } while (!gameStatus);
+  }
+
+  public BaseballCountVo isResult(String computer, String player) {
+
+    int ballCount = 0;
+    int strikeCount = 0;
+    String[] playerInput = stringToStringArray(player);
+    for (int i = 0; i< computer.length(); i++) {
+      int findIndex = computer.indexOf(playerInput[i]);
+      BaseballCountVo result = getBaseballCount(findIndex , i);
+
+      ballCount += result.getBall();
+      strikeCount += result.getStrike();
+    }
+
+    return new BaseballCountVo(strikeCount, ballCount);
+  }
+
+  public BaseballCountVo getBaseballCount(int index, int loop) {
+    int ball = 0;
+    int strike = 0;
+
+    if(BaseballValidation.isNothing(index)) {
+      return new BaseballCountVo(0,0);
+    }
+
+    strike += BaseballValidation.isStrike(loop, index);
+    ball += BaseballValidation.isBall(loop, index);
+
+    return new BaseballCountVo(strike , ball);
+  }
+
+  public String[] stringToStringArray(String value) {
+    return value.split("");
+  }
+}
--- src/main/java/application/BaseballGameUserService.java
+++ src/main/java/application/BaseballGameUserService.java
@@ -6,6 +6,14 @@ import domain.BaseballCountVo;
 
 public class BaseballGameUserService {
   private static Scanner scan = new Scanner(System.in);
+  public int BASEBALL_LENGTH = 3;
+
+  public boolean isGameStart() {
+    System.out.println("게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요");
+    String status = scan.nextLine();
+
+    return "1".equals(status);
+  }
 
   public String playerInputValue() {
     String input = "";
@@ -15,5 +23,48 @@ public class BaseballGameUserService {
     }
 
     return input;
+  }
+
+  public boolean printBallStatus(BaseballCountVo result) {
+    int strike = result.getStrike();
+    int ball = result.getBall();
+
+    if (BaseballValidation.isZero(strike) && BaseballValidation.isZero(ball)){
+      printNothing();
+    }
+
+    printStrikeCount(result.getStrike());
+    printBallCount(result.getBall());
+
+    return matchStrike(strike);
+  }
+
+  private boolean matchStrike(int strike) {
+    if (strike == BASEBALL_LENGTH) {
+      System.out.println(strike + "개의 숫자를 모두 맞히셨습니다! 게임종료");
+      return true;
+    }
+
+    return false;
+  }
+
+  private void printNothing() {
+    System.out.print("포볼!");
+  }
+
+  private void printBallCount(int count) {
+    if(BaseballValidation.isZero(count)) {
+      return;
+    }
+
+    System.out.print(count + " 볼");
+  }
+
+  private void printStrikeCount(int count) {
+    if(BaseballValidation.isZero(count)) {
+      return;
+    }
+
+    System.out.print(count + " 스트라이크");
   }
 }
