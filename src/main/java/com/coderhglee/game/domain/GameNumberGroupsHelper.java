27d7ca5c4024eae9ef62e866f08ef79f294b3285
coderhglee <goodna17@gmail.com>
2020-11-14 14:07:29 +0900

    refactor($gameNumbers): 숫자 비교 함수 리펙토링
    - 사이즈 반환 함수로 수정

--- src/main/java/com/coderhglee/game/domain/GameNumberGroupsHelper.java
+++ src/main/java/com/coderhglee/game/domain/GameNumberGroupsHelper.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 public class GameNumberGroupsHelper {
     public static GameGroupsCompareResult compareEachGameGroup(GameNumberGroups criteriaGameNumberGroups, GameNumberGroups targetGameNumberGroups) {
-        List<GameNumber> retainList = getRetainGameNumbers(criteriaGameNumberGroups, targetGameNumberGroups);
-        int numberSameScore = retainList.size();
+        int numberSameScore = getRetainGameNumbersSize(criteriaGameNumberGroups, targetGameNumberGroups);
         int numberAndDigitSameScore = 0;
         for (int compareIndexNumber = 0; compareIndexNumber < GameNumberGroups.MESSAGE_ALLOW_LENGTH_MAX; compareIndexNumber++) {
             numberAndDigitSameScore += calculationDigitSameScore(criteriaGameNumberGroups.getGameNumberByIndex(compareIndexNumber),
                     targetGameNumberGroups.getGameNumberByIndex(compareIndexNumber));
         }
-        return new GameGroupsCompareResult(numberSameScore - numberAndDigitSameScore, numberAndDigitSameScore);
+        numberSameScore -= numberAndDigitSameScore;
+        return new GameGroupsCompareResult(numberSameScore, numberAndDigitSameScore);
     }
 
-    private static List<GameNumber> getRetainGameNumbers(GameNumberGroups criteriaGameNumberGroups, GameNumberGroups targetGameNumberGroups) {
+    private static int getRetainGameNumbersSize(GameNumberGroups criteriaGameNumberGroups, GameNumberGroups targetGameNumberGroups) {
         List<GameNumber> retainList = new ArrayList<>(criteriaGameNumberGroups.getGameNumbers());
         retainList.retainAll(targetGameNumberGroups.getGameNumbers());
-        return retainList;
+        return retainList.size();
     }
 
     private static int calculationDigitSameScore(GameNumber criteriaGameNumber, GameNumber targetGameNumber) {
