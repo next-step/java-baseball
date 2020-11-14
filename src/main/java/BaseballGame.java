ebe6fe2ec010aa4fb1c95935ff60f6058cc706d1
Lee Dong Wook <sh95119@gmail.com>
2020-11-14 18:44:26 +0900

    feat: createGame()이라는 메서드 추가 및 정답을 초기화하는 코드 생성

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,12 +1,34 @@
 import java.util.LinkedHashSet;
+import java.util.Random;
 
 public class BaseballGame {
 
 	private LinkedHashSet<Integer> question;
 	private LinkedHashSet<Integer> answer;
 
+	public LinkedHashSet<Integer> getQuestion() {
+		return question;
+	}
+
+	public LinkedHashSet<Integer> getAnswer() {
+		return answer;
+	}
+
 	BaseballGame() {
 		question = new LinkedHashSet<>(3);
 		answer = new LinkedHashSet<>(3);
 	}
+
+	public void createGame() {
+		createQuestion();
+	}
+
+	private void createQuestion() {
+		Random random = new Random();
+
+		while(question.size() != 3) {
+			question.add(random.nextInt(9) + 1);
+		}
+	}
+
 }
---
+++ src/test/java/BaseballGameTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Iterator;
+
+public class BaseballGameTest {
+
+	@Test
+	public void createGameReturnQuestions() {
+		// Arrange
+		BaseballGame game = new BaseballGame();
+		// Act
+		game.createGame();
+
+		// Assert
+
+		// 1. 값이 NULL이 아닌지 Check
+		assertThat(game.getQuestion())
+			.isNotEmpty()
+			.isNotNull();
+
+		// 2. 숫자 뽑은 숫자의 개수가 3개가 맞는지?
+		assertThat(game.getQuestion().size()).isEqualTo(3);
+
+		// 3. 각각의 숫자가 정상적인 범위에 들어가 있는지?
+		Iterator<Integer> iter = game.getQuestion().iterator();
+		for(int i = 0; i < 3; i++) {
+			iter.hasNext();
+			assertThat(iter.next()).isBetween(1, 9);
+		}
+	}
+
+}
