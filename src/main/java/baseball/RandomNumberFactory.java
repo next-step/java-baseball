1a26d78975fcfb6f91a80792e92c5c5a13600bd9
mook1594 <wjdanr89@gmail.com>
2020-11-14 13:45:29 +0900

    feat: 숫자 랜덤 생성 - 숫자 랜덤 생성 구현
    - 중복 되지않는 Set으로 중복을 체크한다.
    - Set의 size가 3일때까지 생성한다.

--- src/main/java/baseball/RandomNumberFactory.java
+++ src/main/java/baseball/RandomNumberFactory.java
@@ -1,7 +1,35 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class RandomNumberFactory {
+
+    private final Random random;
+    private final Set<Integer> store;
+
+    public RandomNumberFactory(){
+        random = new Random();
+        store = new HashSet<>();
+    }
+
     public String make() {
-        return "111";
+        makeNumberWithoutDuplicate();
+
+        StringBuilder sb = new StringBuilder();
+        store.iterator().forEachRemaining(a -> sb.append(a));
+        return sb.toString();
+    }
+
+    private void makeNumberWithoutDuplicate(){
+        while(store.size() < BallCount.BALL_COUNT_SIZE){
+            int number = make1to9();
+            store.add(number);
+        }
+    }
+
+    private int make1to9(){
+        return random.nextInt(8) + 1;
     }
 }
