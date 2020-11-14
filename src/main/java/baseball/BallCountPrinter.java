fc999ab51e05d70f1ba86ce82a30aa460f1e0262
mook1594 <wjdanr89@gmail.com>
2020-11-14 11:53:19 +0900

    test: 볼카운트 출력 - 출력 테스트 코드 추가
    - 각 볼카운트 상황별 출력 테스트

---
+++ src/main/java/baseball/BallCountPrinter.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class BallCountPrinter {
+
+    private BallCount ballCount;
+
+    public BallCountPrinter(BallCount ballCount) {
+        this.ballCount = ballCount;
+    }
+
+    public void print() {
+    }
+}
---
+++ src/test/java/baseball/BallCountPrinterTest.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallCountPrinterTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1:0:1스트라이크",
+            "2:0:2스트라이크",
+            "3:0:3스트라이크",
+            "0:1:1볼",
+            "0:2:2볼",
+            "1:1:1스트라이크 1볼",
+            "2:1:2스트라이크 1볼",
+            "2:2:2스트라이크 2볼",
+            "0:0:낫싱"
+    }, delimiter = ':')
+    public void makeText(final int strike, final int ball, final String textExpected) {
+        BallCount ballCount = new BallCount(strike, ball);
+        BallCountPrinter printer = new BallCountPrinter(ballCount);
+        printer.print();
+
+        assertThat(outputStreamCaptor.toString().trim()).isEqualTo(textExpected);
+    }
+}
