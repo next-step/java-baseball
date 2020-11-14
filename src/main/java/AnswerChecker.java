8ecf768b7210f373c9f10d5302b4ce1375c1735c
Lee kang-yong <yebgi83@gmail.com>
2020-11-15 01:44:59 +0900

    feat : 정답인지 체크하기 위한 기능 추가 및 단위 테스트 구체화
    정답인지 체크하기 위한 기능을 추가.
    체크는 패턴이 정답에 해당하는 형태인지를 파악하는 방법을 사용함.

---
+++ src/main/java/AnswerChecker.java
@@ -0,0 +1,14 @@
+import model.PatternResult;
+
+public class AnswerChecker {
+    private AnswerChecker() {
+    }
+
+    public static boolean isPatternForAnswer(PatternResult patternResult) {
+        if (patternResult.getBallCount() != 0) {
+            return false;
+        }
+
+        return patternResult.getStrikeCount() == 3;
+    }
+}
--- src/test/java/AnswerCheckerTest.java
+++ src/test/java/AnswerCheckerTest.java
@@ -1,14 +1,46 @@
+import model.PatternResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class AnswerCheckerTest {
     @Test
     @DisplayName("3 스트라이크는 정답이어야 한다.")
     void checkTrue_IfThreeStrikesShouldBeAnswer() {
+        PatternResult patternResult = new PatternResult(3, 0);
+        assertTrue(AnswerChecker.isPatternForAnswer(patternResult));
     }
 
     @Test
-    @DisplayName("3 스트라이크 이외에는 정답이 아니어야 한다.")
-    void checkTrue_IfAllCasesExceptForThreeStrikesShouldNotBeAnswer() {
+    @DisplayName("3 스트라이크 이외에의 다른 스트라이크는 정답이 아니어야 한다.")
+    void checkTrue_IfAnyStrikesExceptForThreeStrikesShouldNotBeAnswer() {
+        for (int strikeCount = 0; strikeCount < 3; strikeCount++) {
+            PatternResult patternResult = new PatternResult(3, 0);
+            assertTrue(AnswerChecker.isPatternForAnswer(patternResult));
+        }
+    }
+
+    @Test
+    @DisplayName("볼이 있는 경우에는 정답이 아니어야 한다.")
+    void checkTrue_IfAnyBallsShouldNotBeAnswer() {
+        for (int strikeCount = 0; strikeCount < 3; strikeCount++) {
+            if (isFoundAnswerByAnyBallCounts(strikeCount)) {
+                fail();
+            }
+        }
+    }
+
+    boolean isFoundAnswerByAnyBallCounts(int strikeCount) {
+        for (int ballCount = 1; ballCount < 3 - strikeCount; ballCount++) {
+            PatternResult patternResult = new PatternResult(strikeCount, ballCount);
+
+            if (AnswerChecker.isPatternForAnswer(patternResult)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
