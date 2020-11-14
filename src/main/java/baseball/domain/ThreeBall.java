a8aa2c5c6345c2c1e1916a61c8b9e5a8562483ee
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-14 14:33:10 +0900

    feat : 공 생성 exception 추가, 공 생성시 validate
    - 공 생성시 예외 처리, exception 추가

--- src/main/java/baseball/domain/ThreeBall.java
+++ src/main/java/baseball/domain/ThreeBall.java
@@ -1,17 +1,23 @@
 package baseball.domain;
 
 import baseball.domain.strategy.BallGenerateStrategy;
+import baseball.exception.BallException;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class ThreeBall {
+
+    private static final String CONSTRUCT_ERROR_MESSAGE = "공 생성에 실패 하였습니다.";
 
     private List<BallNumber> balls;
 
     public ThreeBall(final BallGenerateStrategy strategy) {
         List<BallNumber> result = new ArrayList<>();
+
+        validateStrategy(strategy);
         List<Integer> numbers = strategy.generate();
 
         for (Integer number : numbers) {
@@ -19,6 +25,12 @@ public class ThreeBall {
         }
 
         this.balls = Collections.unmodifiableList(result);
+    }
+
+    private void validateStrategy(final BallGenerateStrategy strategy) {
+        if (Objects.isNull(strategy)) {
+            throw new BallException(CONSTRUCT_ERROR_MESSAGE);
+        }
     }
 
 }
---
+++ src/main/java/baseball/exception/BallException.java
@@ -0,0 +1,8 @@
+package baseball.exception;
+
+public class BallException extends RuntimeException {
+
+    public BallException(final String message) {
+        super(message);
+    }
+}
