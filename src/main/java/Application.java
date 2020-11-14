f80eec910f34e84965d2ec1ab65b67f50add5811
Lee kang-yong <yebgi83@gmail.com>
2020-11-15 02:55:58 +0900

    feat : EntryPoint 생성 및 UI 로직 구현
    Application 클래스를 만들고 EntryPoint를 구성
    그리고 입력 및 게임 진행을 위한 UI 로직을 구현.

---
+++ src/main/java/Application.java
@@ -0,0 +1,53 @@
+import component.*;
+import model.PatternResult;
+import model.ThreeDigits;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        do {
+            ThreeDigits answer = AnswerCreator.createAnswer();
+            doPlay(answer);
+        } while(doRetryQuery());
+    }
+
+    private static void doPlay(ThreeDigits answer) {
+        while (true) {
+            System.out.print("3개의 숫자를 입력해주세요: ");
+
+            ThreeDigits actual = inputThreeDigits();
+            PatternResult patternResult = PatternAnalyzer.analyze(actual, answer);
+
+            System.out.println(PatternDisplayFormatter.format(patternResult));
+
+            if (AnswerChecker.isPatternForAnswer(patternResult)) {
+                break;
+            }
+        }
+
+        System.err.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    private static boolean doRetryQuery() {
+        System.err.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = inputByPattern("[12]");
+        return input.equals("1");
+    }
+
+    private static ThreeDigits inputThreeDigits() {
+        return ThreeDigitsFactory.createFrom(inputByPattern("[1-9]{3}"));
+    }
+
+    private static String inputByPattern(String pattern) {
+        while (true) {
+            try {
+                return new Scanner(System.in)
+                        .next(pattern);
+            } catch (NoSuchElementException e) {
+                System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+            }
+        }
+    }
+}
