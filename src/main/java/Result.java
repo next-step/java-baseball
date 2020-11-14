5a801e5a84ca074864797c6028349f491dbc3f25
bbbnam <bbbnam@naver.com>
2020-11-15 03:43:46 +0900

    feat : 결과값 객체 생성

---
+++ src/main/java/Result.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Result {
+
+    private final List<Score> score;
+
+    public Result(List<Score> score) {
+        this.score = Collections.unmodifiableList(score);
+    }
+
+    public List<Score> getScores() {
+        return new ArrayList<>(score);
+    }
+}
