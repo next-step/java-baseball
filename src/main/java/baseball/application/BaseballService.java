72a621892a2077571d511eca2ad24fe86e8d78d4
yohan <yohan.lee@uneedcomms.com>
2020-11-14 16:24:17 +0900

    refactor: BaseballService 생성자 변경

--- src/main/java/baseball/application/BaseballService.java
+++ src/main/java/baseball/application/BaseballService.java
@@ -6,14 +6,13 @@ import baseball.dto.Request;
 import baseball.dto.Response;
 
 public class BaseballService {
-    private final BaseBallGenerator baseBallGenerator;
+    private final BaseballNumbers target;
 
     public BaseballService(final BaseBallGenerator baseBallGenerator) {
-        this.baseBallGenerator = baseBallGenerator;
+        target = baseBallGenerator.generate();
     }
 
     public Response play(Request request) {
-        BaseballNumbers target = baseBallGenerator.generate();
         BaseballNumbers playerNumber = BaseballNumbers.from(request.getNumber());
         return Response.from(target.match(playerNumber));
     }
