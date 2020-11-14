6ba84e59c13577985d6bd23d511e136ebb8942ca
bluehoped <bluehoped@hanmail.net>
2020-11-15 00:56:26 +0900

    feat : 점수 클래스 추가

---
+++ src/main/java/Score.java
@@ -0,0 +1,30 @@
+public class Score {
+	private static final String BALL_TEXT = " 볼";
+	private static final String STRIKE_TEXT = " 스트라이크";
+	private static final String FOUR_BALL = "포볼";
+
+	private int ballCount;
+	private int strikeCount;
+	private String ballMessage;
+	private String strikeMessage;
+
+	public void addBall() {
+		ballMessage = ++ballCount + BALL_TEXT;
+	}
+
+	public void addStrike() {
+		strikeMessage = ++strikeCount + STRIKE_TEXT;
+	}
+
+	public boolean isAllStrike() {
+		return strikeCount == 3;
+	}
+
+	public String getScoreString() {
+		if (ballCount == 0 && strikeCount == 0) {
+			return FOUR_BALL;
+		}
+
+		return strikeMessage + " " + ballMessage;
+	}
+}
---
+++ src/test/java/ScoreTest.java
@@ -0,0 +1,26 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("점수 클래스테스트")
+class ScoreTest {
+
+	private Score score = new Score();
+
+	@Test
+	@DisplayName("스트라이크 테스트")
+	void isAllStrike() {
+		score.addStrike();
+		score.addStrike();
+		score.addStrike();
+
+		assertThat(score.isAllStrike()).isTrue();
+	}
+
+	@Test
+	@DisplayName("점수 결과 출력 테스트")
+	void getScoreString() {
+		assertThat(score.getScoreString()).isEqualTo("포볼");
+	}
+}
