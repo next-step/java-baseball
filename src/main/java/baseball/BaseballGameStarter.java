59a11b3a76095d3beb7b95a81fed81073ba0000e
star-no <nxsung@naver.com>
2020-11-14 17:52:22 +0900

    feat(#8) BaseballGameStarter - makeComputerNumber 함수 추가
    * 1-9 까지의 숫자로 이루어진 3자리 String
    * 각 자리 숫자가 중복되면 안된다

--- src/main/java/baseball/BaseballGameStarter.java
+++ src/main/java/baseball/BaseballGameStarter.java
@@ -1,10 +1,16 @@
 package baseball;
 
+import java.util.Random;
+
 public class BaseballGameStarter {
 
     private BaseballGameScanner baseballGameScanner = new BaseballGameScanner();
 
+    private String computerNumber;
+
     public void start() {
+        init();
+
         do {
             sayHello();
             String inputData = baseballGameScanner.scan();
@@ -14,5 +20,31 @@ public class BaseballGameStarter {
 
     private void sayHello() {
         System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    private void init() {
+        computerNumber = makeComputerNumber();
+    }
+
+    String makeComputerNumber() {
+        Random random = new Random();
+        boolean[] checked = new boolean[10];
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0 ; i < 3 ; i++) {
+            stringBuilder.append(getDistinctRandomInt(random, checked));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private int getDistinctRandomInt(Random random, boolean[] checked) {
+        int randomInt = random.nextInt(9) + 1; // 1 ~ 9 랜덤한 수
+        if (!checked[randomInt]) {
+            checked[randomInt] = true;
+            return randomInt;
+        }
+
+        return getDistinctRandomInt(random, checked);
     }
 }
