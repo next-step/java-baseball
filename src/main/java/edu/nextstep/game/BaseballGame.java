84c3ac6d66961b40d5f0f753e56ab41507729ea2
Donghwan Noh <namul.bap@gmail.com>
2020-11-15 05:08:13 +0900

    feat(game): sets player's number
    BaseballGame 클래스의 인스턴스 변수로 BaseballGamer 클래스의 player와 computer를 포함.
    
    BREAKING CHANGE: Playground 클래스에서 숫자야구게임 생성 시, player를 생성해서 주입하는 로직이 추가됨.

--- src/main/java/edu/nextstep/game/BaseballGame.java
+++ src/main/java/edu/nextstep/game/BaseballGame.java
@@ -1,4 +1,11 @@
 package edu.nextstep.game;
 
 public class BaseballGame {
+	private final int SELECTED_NUMBER_SIZE = 3;
+	private BaseballGamer player;
+	private BaseballGamer computer;
+
+	public void setPlayer(BaseballGamer player) {
+		this.player = player;
+	}
 }
--- src/main/java/edu/nextstep/playground/Playground.java
+++ src/main/java/edu/nextstep/playground/Playground.java
@@ -1,6 +1,7 @@
 package edu.nextstep.playground;
 
 import edu.nextstep.game.BaseballGame;
+import edu.nextstep.game.BaseballGamer;
 
 public class Playground {
 	public void play() {
@@ -9,5 +10,6 @@ public class Playground {
 
 	private void playBaseballGame() {
 		BaseballGame game = new BaseballGame();
+		game.setPlayer(new BaseballGamer("123")); // todo 숫자를 콘솔입력으로 받을 수 있도록 수정
 	}
 }
