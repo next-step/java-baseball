e4c6a70b6afa37f21d76393596cec04ac90c20f8
Karll <hankikab@gmail.com>
2020-11-15 22:33:24 +0900

    베이스볼게임 구현

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,4 +1,165 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BaseballGame {
+    private static boolean islive = true;
+    private static String message = "";
+    private static int[] right_answer;
+    private static int strike = 0;
+    private static int ball = 0;
+
+    public void start() {
+        while (islive) {
+            this.game_init();
+            String input = this.getInput();
+            this.check(input);
+        }
+    }
+
+    /**
+     * 게임 만들기
+     */
+    private void game_init() {
+        this.strike = 0;
+        this.ball = 0;
+        // todo 정답이 매번바뀌게 구현해야함
+        this.right_answer = new int[]{1, 2, 3};
+    }
+
+    /**
+     * 입력값 int 배열로 만들기
+     * @param input
+     * @return
+     */
+    private int[] makeArray(String input) {
+        int[] result = new int[3];
+
+        for (int i = 0; i < input.length() ; i++) {
+            result[i] = Integer.parseInt(String.valueOf(input.charAt(i)));
+        }
+        return result;
+    }
+
+    /**
+     * 메시지 보내기
+     * @param message
+     */
+    private void message(String message, boolean islive) {
+        System.out.println(message);
+        this.islive = islive;
+    }
+
+    /**
+     * 게임 판정
+     * @param input
+     */
+    private void check(String input) {
+        if(!this.isNumber(input) || !this.checkInputThreeLength(input)) {
+            this.message(message,true);
+        }
+        int[] input_array = this.makeArray(input);
+        this.checkAnswer(input_array);
+    }
+
+    /**
+     * 체크한 스트라이크,볼 메시지 보내기
+     */
+    private void checkStrikeBallMessage() {
+        String message = "";
+        if (strike > 0) {
+            message += strike + " 스트라이크 ";
+        }
+
+        if (ball > 0) {
+            message += ball + " 볼";
+        }
+
+        if (message.length() > 0) {
+            this.message(message, true);
+        }
+    }
+
+    /**
+     * 스트라이크와 볼 체크
+     * @param input_num         입력숫자
+     * @param input_idx         입력숫자 자릿수
+     * @param right_answer_num  정답숫자
+     * @param right_answer_idx  정답숫자 자릿수
+     */
+    private void checkStrikeBall(int input_num, int input_idx, int right_answer_num, int right_answer_idx) {
+        if (input_num == right_answer_num && input_idx == right_answer_idx) {
+            strike++;
+        }
+
+        if (input_num == right_answer_num && input_idx != right_answer_idx) {
+            ball++;
+        }
+    }
+
+    /**
+     * 숫자 검증
+     * @param input_num
+     * @param input_idx
+     */
+    private void checkNumber(int input_num, int input_idx) {
+        for (int i = 0; i < right_answer.length; i++) {
+            this.checkStrikeBall(input_num, input_idx, right_answer[i] , i);
+        }
+    }
+
+    /**
+     * 정답 검증
+     * @param input_array
+     */
+    private void checkAnswer(int[] input_array) {
+        for (int i = 0; i < input_array.length; i++) {
+            this.checkNumber(input_array[i], i);
+        }
+
+        this.checkStrikeBallMessage();
+
+        if (strike == 3) {
+            this.message("3개의 숫자를 모두 맞히셨습니다. 게임종료", false);
+       }
+    }
+
+    /**
+     * 입력값 받아오기
+     * @return
+     */
+    private String getInput() {
+        this.message("숫자를 입력해 주세요 : ", true);
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+
+        return input;
+    }
+
+    /**
+     * 입력값이 3자리인지 검증
+     * @param input 입력값
+     * @return
+     */
+    private boolean checkInputThreeLength(String input) {
+        if (input.length() != 3) {
+            message = "숫자 3자리를 입력해주세요.";
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 입력값 숫자 체크
+     * @param input
+     * @return
+     */
+    private boolean isNumber(String input) {
+        message = "숫자만 입력가능합니다.";
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 }
