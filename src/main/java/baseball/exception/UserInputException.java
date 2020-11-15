48424c77cbc7714de3a23a87c5c246c2d200a43b
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-15 15:58:04 +0900

    feat: 사용자 입력에 대한 exception 추가

---
+++ src/main/java/baseball/exception/UserInputException.java
@@ -0,0 +1,9 @@
+package baseball.exception;
+
+public class UserInputException extends RuntimeException {
+
+    public UserInputException(final String message) {
+        super(message);
+    }
+
+}
