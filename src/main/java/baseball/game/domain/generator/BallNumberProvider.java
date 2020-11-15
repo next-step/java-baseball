fe85e785e1684f1cf68c65619ddc31f75098bcb9
이기승 <keeseung@sixshop.com>
2020-11-15 13:21:13 +0900

    refactor: BallNumberProvider getNumbers 상수 수정

--- src/main/java/baseball/game/domain/generator/BallNumberProvider.java
+++ src/main/java/baseball/game/domain/generator/BallNumberProvider.java
@@ -1,5 +1,6 @@
 package baseball.game.domain.generator;
 
+import baseball.game.domain.Balls;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -13,6 +14,6 @@ public class BallNumberProvider {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         Collections.shuffle(numbers);
 
-        return numbers.subList(0, 3);
+        return numbers.subList(0, Balls.LIMIT_BALL_SIZE);
     }
 }
