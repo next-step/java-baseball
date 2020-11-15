aac72e27506616aea6d64e2ffcb6ab067781432e
JeJe <las9897@gmail.com>
2020-11-15 19:30:39 +0900

    test: 난수 초기화 테스트

--- src/test/java/ComputerTest.java
+++ src/test/java/ComputerTest.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class ComputerTest {
 
@@ -22,6 +25,21 @@ class ComputerTest {
         assertThat(numbers)
                 .hasSize(3)
                 .doesNotHaveDuplicates();
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("난수 초기화 테스트")
+    public void reset_numbers() {
+        //given
+        Computer computer = new Computer();
+        computer.generateNumber();
+
+        //when
+        List<Integer> numbers = computer.getNumbers();
+        computer.generateNumber();
+
+        //then
+        assertThat(computer.getNumbers()).isNotEqualTo(numbers);
     }
 
 }
