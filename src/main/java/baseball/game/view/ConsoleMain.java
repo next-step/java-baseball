0a41157c1adf01282dc9510bedb5d7e52c01e0a2
이기승 <keeseung@sixshop.com>
2020-11-15 13:18:17 +0900

    feat: 게임이 종료되었을 때 게임을 다시 시작하거나 완전히 종료하는 기능

--- src/main/java/baseball/game/view/ConsoleMain.java
+++ src/main/java/baseball/game/view/ConsoleMain.java
@@ -11,26 +11,56 @@ import java.util.Scanner;
 public class ConsoleMain {
 
     public static void main(String[] args) {
-        BaseBallGame baseBallGame = BaseBallGame.createGame(RandomBallsGenerator.generate());
-        Scanner scanner = new Scanner(System.in);
+        boolean gameFlag;
 
-        while(true) {
-            System.out.print("숫자를 입력해주세요 : ");
-            String input = scanner.next();
+        do {
+            BaseBallGame baseBallGame = BaseBallGame.createGame(RandomBallsGenerator.generate());
+            BaseBallGameResultType result;
 
-            List<Integer> numbers = new ArrayList<>();
-            for(String number: input.split("")) {
-                numbers.add(Integer.parseInt(number));
+            Scanner scanner = new Scanner(System.in);
+            do {
+                List<Integer> numbers = getIntegers(scanner);
+                result = baseBallGame.getResult(DirectBallsGenerator.generate(numbers));
+                System.out.println(result.getPrintString());
+
+            } while(result != BaseBallGameResultType.THREE_STRIKE);
+
+            gameFlag = gameEnd(scanner);
+        } while (gameFlag);
+    }
+
+    private static List<Integer> getIntegers(Scanner scanner) {
+        while (true) {
+            try {
+                return inputNumber(scanner);
+            } catch (NumberFormatException e) {
+                System.out.println("숫자를 잘못 입력 했습니다. 다시 입력해 주세요.");
             }
+        }
+    }
 
-            BaseBallGameResultType result = baseBallGame.getResult(DirectBallsGenerator.generate(numbers));
-            System.out.println(result.getPrintString());
+    private static List<Integer> inputNumber(Scanner scanner) {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = scanner.next();
 
-            if(result == BaseBallGameResultType.THREE_STRIKE) {
+        List<Integer> numbers = new ArrayList<>();
+        for(String number: input.split("")) {
+            numbers.add(Integer.parseInt(number));
+        }
+        return numbers;
+    }
+
+    private static boolean gameEnd(Scanner scanner) {
+        while (true) {
+            try {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                return;
-            }
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int input = scanner.nextInt();
 
+                return input == 1;
+            } catch (NumberFormatException e) {
+                System.out.println("잘못 입력 했습니다. 다시 입력해 주세요.");
+            }
         }
     }
 }
