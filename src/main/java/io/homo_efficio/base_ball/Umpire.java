0e8f521ea615691f6e80f7cf40d1876526b3b0b8
Homo Efficio <homo.efficio@gmail.com>
2020-11-15 11:48:07 +0900

    refactor: 판정 관련 파라미터 갯수 축소

--- src/main/java/io/homo_efficio/base_ball/Umpire.java
+++ src/main/java/io/homo_efficio/base_ball/Umpire.java
@@ -6,39 +6,36 @@ package io.homo_efficio.base_ball;
  */
 public class Umpire {
 
-    private String genNumber;
-    private String inputNumber;
+    private final String genNumber;
+    private final String inputNumber;
+    private final String[] genSplit;
+    private final String[] inputSplit;
 
     public Umpire(String genNumber, String inputNumber) {
         this.genNumber = genNumber;
         this.inputNumber = inputNumber;
+        this.genSplit = genNumber.split("");
+        this.inputSplit = inputNumber.split("");
     }
 
     public Result judge() {
         int strikes = 0, balls = 0;
-        String[] genSplit = genNumber.split("");
-        String[] inputSplit = inputNumber.split("");
-
-        return getResult(strikes, balls, genSplit, inputSplit);
-    }
-
-    private Result getResult(int strikes, int balls, String[] genSplit, String[] inputSplit) {
         for (int i = 0; i < inputSplit.length; i++) {
-            strikes = countStrikes(strikes, genSplit, inputSplit, i);
-            balls = countBalls(balls, genSplit, inputSplit, i);
+            strikes = countStrikes(strikes, i);
+            balls = countBalls(balls, i);
         }
 
         return new Result(balls, strikes);
     }
 
-    private int countBalls(int balls, String[] genSplit, String[] inputSplit, int i) {
+    private int countBalls(int balls, int i) {
         if (!genSplit[i].equals(inputSplit[i]) && genNumber.contains(inputSplit[i])) {
             balls++;
         }
         return balls;
     }
 
-    private int countStrikes(int strikes, String[] genSplit, String[] inputSplit, int i) {
+    private int countStrikes(int strikes, int i) {
         if (genSplit[i].equals(inputSplit[i])) {
             strikes++;
         }
