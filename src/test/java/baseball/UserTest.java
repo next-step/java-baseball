39bf3b46ea54824133ca6e2c61878b38e0707534
623nana <6230nana@gmail.com>
2020-11-15 22:12:52 +0900

    test: 사용자에게 서로 다른 3자리 숫자를 입력 받는 기능 테스트

---
+++ src/test/java/baseball/UserTest.java
@@ -0,0 +1,37 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class UserTest {
+
+  @Test
+  @DisplayName("[User] 사용자가 입력한 수를 배열로 바꾸는 기능 테스트")
+  void makeArrTest() {
+    assertThat(User.makeInputToArr(123).length).isEqualTo(3);
+  }
+
+  @ParameterizedTest
+  @MethodSource("validateUserInputData")
+  @DisplayName("[User] 사용자 입력에 대한 validation 기능 테스트")
+  void validateUserInputTest(String input, boolean expected) {
+    assertThat(User.validateUserInput(input)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> validateUserInputData() {
+    return Stream.of(
+        Arguments.of("123", true),
+        Arguments.of("874", true),
+        Arguments.of("1234", false),
+        Arguments.of("12b", false),
+        Arguments.of("1a3", false),
+        Arguments.of("abc", false)
+    );
+  }
+}
