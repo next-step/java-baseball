6b2b898c60892c8fd604fd70e446bc12e0c71217
taemin <taemin@localhost>
2020-11-15 20:16:17 +0900

    <ADD> add getStrikeCount function

--- src/main/java/com/taemin/baseball/BaseballGameRule.java
+++ src/main/java/com/taemin/baseball/BaseballGameRule.java
@@ -1,6 +1,8 @@
 package com.taemin.baseball;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class BaseballGameRule {
 
@@ -17,5 +19,37 @@ public class BaseballGameRule {
 		return random.nextInt(9) + 1;
 	}
 	
+	public int getStrikeCount(LinkedHashSet<Integer> computerNumbers, LinkedHashSet<Integer> userNumbers) throws Exception {
+		int strikeCount = 0;
+		if(computerNumbers == null)
+			throw new Exception("Computer Numbers are null");
+		if(userNumbers == null)
+			throw new Exception("User Numbers are null");
+		int[] computerNumberArray = toIntArrayAtIntSet(computerNumbers);
+		int[] userNumberArray = toIntArrayAtIntSet(userNumbers);
+		
+		for(int index = 0; index < 3; index++)
+			strikeCount += isStrike(computerNumberArray[index], userNumberArray[index]);	
+		return strikeCount;
+	}
+	
+	private int isStrike(int computerNumber, int userNumber) {
+		if(computerNumber == userNumber)
+			return 1;
+		return 0;
+	}
+	
+	private int[] toIntArrayAtIntSet(Set<Integer> set) {
+		int[] intArray = new int[set.size()];
+		int index = 0;
+		Iterator<Integer> setIterator = set.iterator();
+		while(setIterator.hasNext()) {
+			intArray[index] = setIterator.next();
+			index++;
+		}
+			
+		return intArray;
+		
+	}
 	
 }
--- src/test/java/com/taemin/baseball/BaseballGameRuleTest.java
+++ src/test/java/com/taemin/baseball/BaseballGameRuleTest.java
@@ -1,7 +1,6 @@
 package com.taemin.baseball;
-
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.LinkedHashSet;
 import org.junit.jupiter.api.Test;
 
 class BaseballGameRuleTest {
@@ -16,5 +15,21 @@ class BaseballGameRuleTest {
 
 		
 	}
+	
+	@Test 
+	void _getStrikeCount() throws Exception {
+		BaseballGameRule baseballGameRule = new BaseballGameRule();
+		LinkedHashSet<Integer> computerNumbers = baseballGameRule.getComputerRandomNumbers();
+		LinkedHashSet<Integer> userNumbers = baseballGameRule.getComputerRandomNumbers();
+		System.out.println("Computer Numbers" + computerNumbers);
+		System.out.println("User Numbers" + userNumbers);
+		assertNotNull(computerNumbers);
+		assertNotNull(userNumbers);
+		assertSame(3, computerNumbers.size());
+		assertSame(3, userNumbers.size());
+		assertTrue(baseballGameRule.getStrikeCount(computerNumbers, userNumbers) <= 3);
+		System.out.println(baseballGameRule.getStrikeCount(computerNumbers, userNumbers));
+	}
+	
 
 }
