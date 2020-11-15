b972845457ec323525563eeec7b34c23bce4f15e
tictokgirl <tictokgirl@naver.com>
2020-11-15 15:37:02 +0900

    feat: 2. insert entered numbers

--- src/main/java/BaseBall.java
+++ src/main/java/BaseBall.java
@@ -1,11 +1,13 @@
 import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 public class BaseBall {
     final int MAX_NUMBER = 3;
     Set<Integer> numberSet = new HashSet<>();
     int[] computerNumbers = new int[MAX_NUMBER];
+    int[] playerNumbers = new int[MAX_NUMBER];
 
     public int generateRandomNumber() {
         return new Random().nextInt(9) + 1;
@@ -14,6 +16,25 @@ public class BaseBall {
     public void insertDifferentNumbers(int number) {
         if(numberSet.add(number)){
             computerNumbers[numberSet.size() - 1] = number;
+        }
+    }
+
+    public void inputView() {
+        System.out.print("숫자를 입력해주세요 : ");
+        insertInputNumber(inputNumber());
+    }
+
+    public int inputNumber() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public void insertInputNumber(int enteredNumber) {
+        int index = MAX_NUMBER - 1;
+        while(enteredNumber > 0) {
+            playerNumbers[index] = enteredNumber % 10;
+            enteredNumber /= 10;
+            index--;
         }
     }
 }
