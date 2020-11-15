f910475d522f999c851963c3cabda01888379000
mook1594 <wjdanr89@gmail.com>
2020-11-15 13:25:16 +0900

    refactor: 볼카운트 계산 - 안쓰는 코드 정리

--- src/main/java/baseball/BallCountResolver.java
+++
@@ -1,7 +0,0 @@
-package baseball;
-
-public interface BallCountResolver {
-	boolean isMatch(final int index, final String input, final String answer);
-
-	void resolve(final BallCount ballCounts);
-}
