470cee7d4e2ef3d2b24d9467676f9a19f0ee5e46
songseongbin <ppsosqq@gmail.com>
2020-11-15 20:16:26 +0900

    <feat>: 입력한 수에 따른 결과 출력 기능 추가

--- src/main/java/NumberBaseBall.java
+++ src/main/java/NumberBaseBall.java
@@ -1,25 +1,68 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class NumberBaseBall {
 	private static final List<String> NUMBERS =
 		Collections.unmodifiableList(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
 
 	private static final int NUMBERS_MAX_INDEX = 8;
+	private static final int MAX_DIGIT = 3;
 
 	public String makeRandomNumberString() {
-		String numberString = "";
 		List<String> tempNumbers = new ArrayList(NUMBERS);
-
 		Random random = new Random();
-		for(int i = 0; i < 3; i++) {
+		String answer = "";
+		for (int i = 0; i < MAX_DIGIT; i++) {
 			int randomIdx = random.nextInt(NUMBERS_MAX_INDEX - i);
-			numberString += tempNumbers.get(randomIdx);
+			answer += tempNumbers.get(randomIdx);
 			tempNumbers.remove(randomIdx);
 		}
-		return numberString;
+		return answer;
+	}
+
+	public String calculate(String answerNumber, String inputNumber) {
+		List<String> answerNumbers = Arrays.asList(answerNumber.split(""));
+		List<String> inputNumbers = Arrays.asList(inputNumber.split(""));
+		int strikeCnt = 0;
+		int ballAndStrikeCnt = 0;
+
+		for (int i = 0; i < MAX_DIGIT; i++) {
+			strikeCnt += getStrikeCnt(answerNumbers.get(i), inputNumbers.get(i));
+			ballAndStrikeCnt += getBallAndStrikeCnt(answerNumbers, inputNumbers.get(i));
+		}
+		return makeResult(strikeCnt, ballAndStrikeCnt - strikeCnt);
+	}
+
+	private int getBallAndStrikeCnt(List<String> answerNumbers, String inputNumber) {
+		Set<String> answerNumberSet = new HashSet<>(answerNumbers);
+		if (answerNumberSet.contains(inputNumber)) {
+			return 1;
+		}
+		return 0;
+	}
+
+	private int getStrikeCnt(String answerNumber, String inputNumber) {
+		if (answerNumber.equals(inputNumber)) {
+			return 1;
+		}
+		return 0;
+	}
+
+	private String makeResult(int strikeCnt, int ballCnt) {
+		StringBuilder sb = new StringBuilder();
+		String space = " ";
+
+		if (strikeCnt > 0) {
+			sb.append(strikeCnt).append("스트라이크").append(space);
+		}
+		if (ballCnt > 0) {
+			sb.append(ballCnt).append("볼");
+		}
+		return sb.toString();
 	}
 }
--- src/test/java/NumberBaseBallTest.java
+++ src/test/java/NumberBaseBallTest.java
@@ -10,6 +10,19 @@ import org.junit.jupiter.api.Test;
 class NumberBaseBallTest {
 
 	@Test
+	@DisplayName("각 자리가 중복되는 경우, 중복 검사(isEachNumberUniqueAndThreeDigitNumber) 함수 테스트")
+	public void isEachNumberUniqueAndThreeDigitNumberTest() {
+		assertAll(
+			() -> assertFalse(isEachNumberUniqueAndThreeDigitNumber("232")),
+			() -> assertFalse(isEachNumberUniqueAndThreeDigitNumber("554")),
+			() -> assertFalse(isEachNumberUniqueAndThreeDigitNumber("499")),
+			() -> assertTrue(isEachNumberUniqueAndThreeDigitNumber("369")),
+			() -> assertTrue(isEachNumberUniqueAndThreeDigitNumber("519")),
+			() -> assertTrue(isEachNumberUniqueAndThreeDigitNumber("932"))
+		);
+	}
+
+	@Test
 	@DisplayName("각 자리가 중복되지 않는 세자리 수가 만들어지는지 테스트")
 	public void makeRandomNumberStringTest() {
 		NumberBaseBall numberBaseBall = new NumberBaseBall();
@@ -30,16 +43,27 @@ class NumberBaseBallTest {
 	}
 
 	@Test
-	@DisplayName("각 자리가 중복되는 경우, 중복 검사(isEachNumberUniqueAndThreeDigitNumber) 함수 테스트")
-	public void isEachNumberUniqueAndThreeDigitNumberTest() {
-		assertAll(
-			() -> assertFalse(isEachNumberUniqueAndThreeDigitNumber("232")),
-			() -> assertFalse(isEachNumberUniqueAndThreeDigitNumber("554")),
-			() -> assertFalse(isEachNumberUniqueAndThreeDigitNumber("499")),
-			() -> assertTrue(isEachNumberUniqueAndThreeDigitNumber("369")),
-			() -> assertTrue(isEachNumberUniqueAndThreeDigitNumber("519")),
-			() -> assertTrue(isEachNumberUniqueAndThreeDigitNumber("932"))
-		);
+	@DisplayName("3볼 결과 메시지")
+	public void makeResultTest_When_3Ball() {
+		NumberBaseBall numberBaseBall = new NumberBaseBall();
+		String result = numberBaseBall.calculate("352", "235");
+		assertEquals(result, "3볼");
+	}
+
+	@Test
+	@DisplayName("3스트라이크 결과 메시지")
+	public void makeResultTest_When_3Strike() {
+		NumberBaseBall numberBaseBall = new NumberBaseBall();
+		String result = numberBaseBall.calculate("352", "352");
+		assertEquals(result, "3스트라이크 ");
+	}
+
+	@Test
+	@DisplayName("1스트라이크 2볼 결과 메시지")
+	public void makeResultTest_When_1Strike_2Ball() {
+		NumberBaseBall numberBaseBall = new NumberBaseBall();
+		String result = numberBaseBall.calculate("352", "253");
+		assertEquals(result, "1스트라이크 2볼");
 	}
 
 	private boolean isEachNumberUniqueAndThreeDigitNumber(String numberString) {
