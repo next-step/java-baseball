4b146a0001c245f497a2fb3cb07ce9c8e41b6bd0
박상욱 <aristein.park@gmail.com>
2020-11-16 00:07:21 +0900

    test: 빠진 유닛테스트 추가
    reflection을 적용해 private 함수들 test를 만드려고 시도.
    현재 단계에서 너무 큰 부담이라 생각해 함수들을 public으로 만들었음.
    추후 피드백 받고 방향을 정해보는게 좋을 듯 함.
    
    isStrikeOut 함수 테스트 추가.
    getRandomNumberString 함수 테스트 추가.

--- src/test/java/baseballgame/BaseballGameTest.java
+++ src/test/java/baseballgame/BaseballGameTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BaseballGameTest {
 
@@ -15,11 +17,29 @@ class BaseballGameTest {
     @Test
     void gameNumberCheckIsAllNumber() {
         baseballGame.initGame();
-        String gameNumber = baseballGame.getGameNumber();
+        String gameNumber = baseballGame.getBaseballNumber();
         Assertions.assertThat(gameNumber).isNotEmpty();
         Assertions.assertThat(gameNumber.length()).isEqualTo(3);
         for (char c : gameNumber.toCharArray()) {
             assertTrue(Character.isDigit(c));
         }
+    }
+
+    @DisplayName("랜덤 숫자 문자열 생성")
+    @ParameterizedTest
+    @CsvSource(value = {"1", "2", "3", "10"})
+    void getRandomNumberString(int size) {
+        String gameNumber = baseballGame.getRandomNumberString(size);
+        Assertions.assertThat(gameNumber.length()).isEqualTo(size);
+        for (char c : gameNumber.toCharArray()) {
+            assertTrue(Character.isDigit(c));
+        }
+    }
+
+    @DisplayName("스트라이크 아웃 체크")
+    @ParameterizedTest
+    @CsvSource(value = {"1:false", "2:false", "3:true", "4:false"}, delimiter = ':')
+    void isStrikeOut(int strikeCount, boolean expect) {
+        assertEquals(expect, baseballGame.isStrikeOut(strikeCount));
     }
 }
