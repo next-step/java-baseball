f13813bd7297b0398d282055e1b25cac49544bc2
JUNGYI <>
2020-11-16 01:00:48 +0900

    test : 컴퓨터는 입력한 숫자에 대한 결과(같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 포볼 또는 낫싱, 모든 수가 같은 자리에 있으면 성공)를 출력하고 성공할 때까지 게임을 반복

--- src/test/java/game/BaseballTest.java
+++ src/test/java/game/BaseballTest.java
@@ -21,7 +21,6 @@ public class BaseballTest {
     @Test
     @DisplayName("1에서 9까지 서로 다른 임의의 수 3개를 Set 자료구조에 담아서 사이즈를 비교해본다.")
     void prepareComputerNumbers_ShouldReturnNoDuplicateNumbers() {
-
         int[] computerNumbers = baseball.prepareComputerNumbers();
         Set<Integer> numbers = new HashSet<>();
 
@@ -35,7 +34,6 @@ public class BaseballTest {
     @Test
     @DisplayName("게임 플레이어가 입력한 텍스트가 숫자로만 구성되어있는지와 숫자 갯수가 적합한지 테스트")
     void checkInputNumbers_IsNumberAndSuitableCount() {
-
         String inputNumber = "a3p";
         assertFalse(baseball.checkInputNumbers(inputNumber));
 
@@ -46,4 +44,30 @@ public class BaseballTest {
         assertTrue(baseball.checkInputNumbers(inputNumber));
     }
 
+    @Test
+    @DisplayName("스트라이크와 볼을 적합하게 판단하는지 테스트")
+    void baseballJudgment_ShouldReturnSuitableCount() {
+        int[] computerNumbers = new int[] {1, 2, 3};
+        int[] userNumbers = new int[] {4, 2, 1};
+
+        BaseballJudgment baseballJudgment = baseball.baseballJudgment(computerNumbers, userNumbers);
+        assertEquals(baseballJudgment.getStrikeCount(), 1);
+        assertEquals(baseballJudgment.getBallCount(), 1);
+
+        userNumbers = new int[] {2, 3, 1};
+        baseballJudgment = baseball.baseballJudgment(computerNumbers, userNumbers);
+        assertEquals(baseballJudgment.getStrikeCount(), 0);
+        assertEquals(baseballJudgment.getBallCount(), 3);
+
+        userNumbers = new int[] {5, 6, 7};
+        baseballJudgment = baseball.baseballJudgment(computerNumbers, userNumbers);
+        assertEquals(baseballJudgment.getStrikeCount(), 0);
+        assertEquals(baseballJudgment.getBallCount(), 0);
+
+        userNumbers = new int[] {1, 2, 3};
+        baseballJudgment = baseball.baseballJudgment(computerNumbers, userNumbers);
+        assertEquals(baseballJudgment.getStrikeCount(), 3);
+        assertEquals(baseballJudgment.getBallCount(), 0);
+    }
+    
 }
