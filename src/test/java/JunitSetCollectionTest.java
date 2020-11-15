e556807138f7ddd5b3186412982874fd576729c0
somi <somidevelop@gmail.com>
2020-11-15 23:58:44 +0900

    feat: Set Collection 요구사항3완료
    요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
    예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다

--- src/test/java/JunitSetCollectionTest.java
+++ src/test/java/JunitSetCollectionTest.java
@@ -31,12 +31,21 @@ public class JunitSetCollectionTest {
 		System.out.println("numbers size : " + size);
 	}
 
-	// Set Collection 요구사항1
+	// Set Collection 요구사항2
 	// Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
 	// JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
 	@ParameterizedTest
-	@ValueSource(ints = {1,2,3})
+	@ValueSource(ints = {1, 2, 3})
 	void contains(int index) {
+		assertThat(numbers.contains(index)).isTrue();
+	}
+
+	// Set Collection 요구사항3
+	// 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
+	// 예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다
+	@ParameterizedTest
+	@ValueSource(ints = {1,2,3,4,5})
+	void containsFalse(int index) {
 		assertThat(numbers.contains(index)).isTrue();
 	}
 
