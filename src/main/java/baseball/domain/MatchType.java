2979015e27d122f54effdbac31e8aaa3922da5c8
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-15 21:37:58 +0900

    refactor : 미사용 필드 제거

--- src/main/java/baseball/domain/MatchType.java
+++ src/main/java/baseball/domain/MatchType.java
@@ -2,18 +2,6 @@ package baseball.domain;
 
 public enum MatchType {
 
-    STRIKE("스트라이크"),
-    BALL("볼"),
-    NOTHING("낫싱");
-
-    private String message;
-
-    MatchType(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    STRIKE, BALL, NOTHING;
 
 }
