dcb70c837a5b2722a85f5e8d00ea5e25e672201e
Mingyu Lee <leemingyu05@gmail.com>
2020-11-15 18:18:18 +0900

    feat: 게임 재시작

--- src/main/java/service/BaseballGame.java
+++ src/main/java/service/BaseballGame.java
@@ -36,6 +36,13 @@ public class BaseballGame {
 	private void endGame() {
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+		Integer input = scanner.nextInt();
+
+		if (input.equals(1)) {
+			this.setPlayers();
+			return;
+		}
 	}
 
 	private void printResult(BaseballResult baseballResult) {
