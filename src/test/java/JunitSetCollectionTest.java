2d429a88bd4511daf0711d5295bb980d44927ac7
somi <somidevelop@gmail.com>
2020-11-15 23:52:54 +0900

    feat: Set Collection 요구사항2완료
    Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
    JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.

--- src/test/java/JunitSetCollectionTest.java
+++ src/test/java/JunitSetCollectionTest.java
@@ -1,9 +1,13 @@
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Set Collection Test")
 public class JunitSetCollectionTest {
@@ -27,5 +31,13 @@ public class JunitSetCollectionTest {
 		System.out.println("numbers size : " + size);
 	}
 
+	// Set Collection 요구사항1
+	// Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
+	// JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
+	@ParameterizedTest
+	@ValueSource(ints = {1,2,3})
+	void contains(int index) {
+		assertThat(numbers.contains(index)).isTrue();
+	}
 
 }
