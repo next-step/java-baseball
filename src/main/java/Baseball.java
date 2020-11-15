6a971567ad610718cb191af58b501d056b58e955
JeJe <las9897@gmail.com>
2020-11-15 19:44:24 +0900

    feat: 게임 플레이어 서로 다른 3개의 숫자 입력

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Baseball {
     Scanner scanner = new Scanner(System.in);
@@ -7,7 +10,23 @@ public class Baseball {
         Computer computer = new Computer();
         computer.generateNumber();
 
+        userNumberInput();
+
         gameEnd();
+    }
+
+
+    public void userNumberInput() {
+
+        System.out.println("숫자를 입력해주세요: ");
+        String[] inputNumbers = scanner.next().split("");
+        Set<String> numberSet = new HashSet<>(Arrays.asList(inputNumbers));
+
+        while (numberSet.size() != 3) {
+            System.out.println("잘못된 입력입니다. 중복없는 1~9의 3자리 숫자를 입력해주세요: ");
+            inputNumbers = scanner.next().split("");
+            numberSet = new HashSet<>(Arrays.asList(inputNumbers));
+        }
     }
 
     public void gameEnd() {
