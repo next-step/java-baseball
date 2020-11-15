6c29940735804e66729cd2bd6be972bb4276f07e
sanghun <sanghun1984@gmail.com>
2020-11-15 22:03:13 +0900

    feat: 3개의 숫자에 대한 판단(스트라이크, 볼, 낫싱)을 한다.

--- src/main/java/service/BaseballService.java
+++ src/main/java/service/BaseballService.java
@@ -1,14 +1,23 @@
 package service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaseballService {
 
 	public static final int SCORE_MAX_LENGTH = 3;
 	public static final int SCORE_MIN_NUM = 1;
 	public static final int SCORE_MAX_NUM = 9;
+
 	private int[] gameMasterScore = new int[SCORE_MAX_LENGTH];
+	private Map<Enum, Integer> scoreMap = new HashMap<>();
 
 	public int[] getGameMasterScore() {
 		return gameMasterScore;
+	}
+
+	public Map<Enum, Integer> getScoreMap() {
+		return scoreMap;
 	}
 
 	public boolean playUntilEnd(String inputScore) {
@@ -16,8 +25,35 @@ public class BaseballService {
 			return false;
 		}
 		generateGameMasterScore();
+		setScoreResult(inputScore);
+		System.out.println(getScoreResultMessage());
 
-		return true;
+		return getGameResult();
+	}
+
+	public void setScoreResult(String inputScore) {
+		for (int i = 0; i < inputScore.length(); i++) {
+			int inputScoreAt = Integer.parseInt(inputScore.charAt(i) + "");
+			setScoreResult(i, inputScoreAt);
+		}
+	}
+
+	public void setScoreResult(int inputScoreIndex, int inputScoreAt) {
+		for (int i = 0; i < SCORE_MAX_LENGTH; i++) {
+			setScoreResultToMap(i, inputScoreIndex, inputScoreAt);
+		}
+	}
+
+	/**
+	 * 입력된 숫자의 자리와 값이 마스터 스코어와 동일한지 여부에 따라 스크라이크 또는 볼을 결정한다.
+	 */
+	public void setScoreResultToMap(int masterScoreIndex, int inputScoreIndex, int inputScoreAt) {
+		if (gameMasterScore[masterScoreIndex] == inputScoreAt && masterScoreIndex == inputScoreIndex) {
+			scoreMap.put(BaseballCount.STRIKE, scoreMap.getOrDefault(BaseballCount.STRIKE, 0) + 1);
+		}
+		if (gameMasterScore[masterScoreIndex] == inputScoreAt && masterScoreIndex != inputScoreIndex) {
+			scoreMap.put(BaseballCount.BALL, scoreMap.getOrDefault(BaseballCount.BALL, 0) + 1);
+		}
 	}
 
 	public boolean isValidInputScore(String inputScore) {
@@ -25,12 +61,37 @@ public class BaseballService {
 	}
 
 	public void generateGameMasterScore() {
-		for (int i = 0; i< SCORE_MAX_LENGTH; i++) {
+		for (int i = 0; i < SCORE_MAX_LENGTH; i++) {
 			gameMasterScore[i] = getRandomNumberUsingInts();
 		}
 	}
 
 	public int getRandomNumberUsingInts() {
 		return (int)(Math.random() * SCORE_MAX_NUM + SCORE_MIN_NUM);
+	}
+
+	public String getScoreResultMessage() {
+		int strikeCount = scoreMap.getOrDefault(BaseballCount.STRIKE, 0);
+		int ballCount = scoreMap.getOrDefault(BaseballCount.BALL, 0);
+
+		if (strikeCount > 0 || ballCount > 0) {
+			return getValidScoreMessage(strikeCount, ballCount);
+		}
+		return BaseballCount.NOTHING.getName();
+	}
+
+	private boolean getGameResult() {
+		return scoreMap.getOrDefault(BaseballCount.STRIKE, 0) == SCORE_MAX_LENGTH;
+	}
+
+	private String getValidScoreMessage(int strikeCount, int ballCount) {
+		StringBuilder resultMessage = new StringBuilder();
+		if (strikeCount > 0) {
+			resultMessage.append(strikeCount).append(BaseballCount.STRIKE.getName());
+		}
+		if (ballCount > 0) {
+			resultMessage.append(ballCount).append(BaseballCount.BALL.getName());
+		}
+		return resultMessage.toString();
 	}
 }
--- src/test/java/service/BaseballServiceTest.java
+++ src/test/java/service/BaseballServiceTest.java
@@ -2,6 +2,8 @@ package service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,5 +36,27 @@ class BaseballServiceTest {
 		assertTrue(masterScore[0] >= BaseballService.SCORE_MIN_NUM && masterScore[0] <= BaseballService.SCORE_MAX_NUM);
 		assertTrue(masterScore[1] >= BaseballService.SCORE_MIN_NUM && masterScore[1] <= BaseballService.SCORE_MAX_NUM);
 		assertTrue(masterScore[2] >= BaseballService.SCORE_MIN_NUM && masterScore[2] <= BaseballService.SCORE_MAX_NUM);
+	}
+
+	@DisplayName("스코어결과확인")
+	@Test
+	void getScoreResultMessageTest() {
+		Map<Enum, Integer> scoreMap = baseballService.getScoreMap();
+
+		scoreMap.put(BaseballCount.STRIKE, 2);
+		scoreMap.put(BaseballCount.BALL, 1);
+		assertEquals("2스트라이크1볼", baseballService.getScoreResultMessage());
+
+		scoreMap.put(BaseballCount.STRIKE, 3);
+		scoreMap.put(BaseballCount.BALL, 0);
+		assertEquals("3스트라이크", baseballService.getScoreResultMessage());
+
+		scoreMap.put(BaseballCount.STRIKE, 0);
+		scoreMap.put(BaseballCount.BALL, 3);
+		assertEquals("3볼", baseballService.getScoreResultMessage());
+
+		scoreMap.put(BaseballCount.STRIKE, 0);
+		scoreMap.put(BaseballCount.BALL, 0);
+		assertEquals("낫싱", baseballService.getScoreResultMessage());
 	}
 }
