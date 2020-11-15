6e9deca92c52a4a0eef108ff85c3d431cdc3f96e
623nana <6230nana@gmail.com>
2020-11-15 22:06:47 +0900

    refactor: 사용자의 입력을 배열로 바꾸는 메소드 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -16,6 +16,14 @@ public class User {
     return Integer.parseInt(input);
   }
 
+  public static int[] makeInputToArr(int input) {
+    int[] arr = new int[3];
+    arr[0] = input / 100;
+    arr[1] = input % 100 / 10;
+    arr[2] = input % 10;
+    return arr;
+  }
+
   public static boolean validateUserInput(String input) {
     if (input.length() != 3 || !input.matches("^[0-9]*$")) {
       System.out.println("숫자를 바르게 입력하세요!");
@@ -23,4 +31,5 @@ public class User {
     }
     return true;
   }
+
 }
