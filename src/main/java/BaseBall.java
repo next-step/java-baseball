0c61692b7a34156e86e6d6d806e47145b7732449
tictokgirl <tictokgirl@naver.com>
2020-11-15 16:22:19 +0900

    feat: 3. compare numbers

--- src/main/java/BaseBall.java
+++ src/main/java/BaseBall.java
@@ -8,13 +8,15 @@ public class BaseBall {
     Set<Integer> numberSet = new HashSet<>();
     int[] computerNumbers = new int[MAX_NUMBER];
     int[] playerNumbers = new int[MAX_NUMBER];
+    int strike = 0;
+    int ball = 0;
 
     public int generateRandomNumber() {
         return new Random().nextInt(9) + 1;
     }
 
     public void insertDifferentNumbers(int number) {
-        if(numberSet.add(number)){
+        if (numberSet.add(number)) {
             computerNumbers[numberSet.size() - 1] = number;
         }
     }
@@ -31,10 +33,36 @@ public class BaseBall {
 
     public void insertInputNumber(int enteredNumber) {
         int index = MAX_NUMBER - 1;
-        while(enteredNumber > 0) {
+        while (enteredNumber > 0) {
             playerNumbers[index] = enteredNumber % 10;
             enteredNumber /= 10;
             index--;
         }
+    }
+
+    public void compareNumbers(int i, int j) {
+        if (j > 2) {
+            return;
+        }
+        if (i == j && computerNumbers[i] == playerNumbers[j]) {
+            strike++;
+        }
+        if (i != j && computerNumbers[i] == playerNumbers[j]) {
+            ball++;
+        }
+        compareNumbers(i, ++j);
+    }
+
+    public void repeatComparing() {
+        initUser();
+        inputView();
+        for (int i = 0; i < MAX_NUMBER; i++) {
+            compareNumbers(i,0);
+        }
+    }
+
+    public void initUser() {
+        strike = 0;
+        ball = 0;
     }
 }
