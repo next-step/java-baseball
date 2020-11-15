906828b88df804e649148d80b815e3ec6acbe21f
ini8262 <ini8262@naver.com>
2020-11-15 18:29:17 +0900

    feat : 특정한 값이 랜덤값 3자리에 포함여부와 자릿수와 같은지 여부
    - 해쉬를 이용하여 값 자릿수에 값을 키로 이용하고, 자릿수에 인덱스를 값으로 정의하였습니다.
    - README.md 파일 구현기능 목록 수정

--- src/main/java/baseball/core/Round.java
+++ src/main/java/baseball/core/Round.java
@@ -1,6 +1,35 @@
 package baseball.core;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Round {
+	public static final int MAX_DIGITS = 3;
+	private Map<Integer, Integer> numberMap;
+
+	public Round(int value) {
+		if (numberLength(value) != MAX_DIGITS) {
+			throw new IllegalArgumentException(String.format("%d에 자릿수에 해당하는 숫자를 넘겨주시기 바랍니다.", MAX_DIGITS));
+		}
+
+		numberMap = new HashMap<>();
+
+		for (int i = 0; i < MAX_DIGITS; i++) {
+			numberMap.put(getPlaceValue(value, i), i);
+		}
+	}
+
+	protected boolean isExsit(int number) {
+		return numberMap.containsKey(number);
+	}
+
+	protected boolean isPlaceExsit(int number, int index) {
+		if (isExsit(number)) {
+			return numberMap.get(number) == index;
+		}
+
+		return false;
+	}
 
 	protected int numberLength(int number) {
 		return (int)(Math.log10(number) + 1);
--- src/test/java/baseball/core/RoundTest.java
+++ src/test/java/baseball/core/RoundTest.java
@@ -1,19 +1,20 @@
 package baseball.core;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RoundTest {
 	private static Round round;
 
 	@BeforeAll
 	public static void beforeAll() {
-		round = new Round();
+		round = new Round(123);
 	}
 
 	@Test
@@ -64,5 +65,80 @@ public class RoundTest {
 	public void round_get_throw() {
 		Assertions.assertThatIllegalArgumentException().isThrownBy(() -> round.getPlaceValue(1, 10));
 		Assertions.assertThatIllegalArgumentException().isThrownBy(() -> round.getPlaceValue(123, 3));
+	}
+
+	@Test
+	@DisplayName("특정한 값이 랜덤값 3자리에 포함하는지 여부")
+	public void isExsit() {
+		Round round = new Round(123);
+
+		assertTrue(round.isExsit(1));
+		assertTrue(round.isExsit(2));
+		assertTrue(round.isExsit(3));
+		assertFalse(round.isExsit(4));
+		assertFalse(round.isExsit(5));
+		assertFalse(round.isExsit(6));
+		assertFalse(round.isExsit(7));
+		assertFalse(round.isExsit(8));
+		assertFalse(round.isExsit(9));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {0})
+	@DisplayName("첫번째 자릿값이 특정한 값과 같은지 여부")
+	public void isPlaceExsit1(int index) {
+		Round round = new Round(123);
+
+		assertTrue(round.isPlaceExsit(1, index));
+		assertFalse(round.isPlaceExsit(2, index));
+		assertFalse(round.isPlaceExsit(3, index));
+		assertFalse(round.isPlaceExsit(4, index));
+		assertFalse(round.isPlaceExsit(5, index));
+		assertFalse(round.isPlaceExsit(6, index));
+		assertFalse(round.isPlaceExsit(7, index));
+		assertFalse(round.isPlaceExsit(8, index));
+		assertFalse(round.isPlaceExsit(9, index));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1})
+	@DisplayName("두번째 자릿값이 특정한 값과 같은지 여부")
+	public void isPlaceExsit2(int index) {
+		Round round = new Round(123);
+
+		assertFalse(round.isPlaceExsit(1, index));
+		assertTrue(round.isPlaceExsit(2, index));
+		assertFalse(round.isPlaceExsit(3, index));
+		assertFalse(round.isPlaceExsit(4, index));
+		assertFalse(round.isPlaceExsit(5, index));
+		assertFalse(round.isPlaceExsit(6, index));
+		assertFalse(round.isPlaceExsit(7, index));
+		assertFalse(round.isPlaceExsit(8, index));
+		assertFalse(round.isPlaceExsit(9, index));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {2})
+	@DisplayName("세번째 자릿값이 특정한 값과 같은지 여부")
+	public void isPlaceExsit3(int index) {
+		Round round = new Round(123);
+
+		assertFalse(round.isPlaceExsit(1, index));
+		assertFalse(round.isPlaceExsit(2, index));
+		assertTrue(round.isPlaceExsit(3, index));
+		assertFalse(round.isPlaceExsit(4, index));
+		assertFalse(round.isPlaceExsit(5, index));
+		assertFalse(round.isPlaceExsit(6, index));
+		assertFalse(round.isPlaceExsit(7, index));
+		assertFalse(round.isPlaceExsit(8, index));
+		assertFalse(round.isPlaceExsit(9, index));
+	}
+
+	@Test
+	@DisplayName("의도하지 않은 생성자 파리미터 예외발생")
+	public void initRound() {
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			new Round(1);
+		}).withMessage("%s에 자릿수에 해당하는 숫자를 넘겨주시기 바랍니다.", 3);
 	}
 }
