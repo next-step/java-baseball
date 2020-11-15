7c7079039b743f34c265850a0a1481113c221f6b
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-15 13:20:34 +0900

    test: 정답과 사용자 입력의 게임 비교 결과 테스트

--- src/test/java/baseball/service/GameServiceTest.java
+++ src/test/java/baseball/service/GameServiceTest.java
@@ -1,11 +1,14 @@
 package baseball.service;
 
+import baseball.domain.MatchResult;
+import baseball.domain.MatchType;
 import baseball.domain.ThreeBall;
 import baseball.domain.strategy.BallGenerateStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -26,4 +29,20 @@ class GameServiceTest {
         //then
         assertTrue(goal.equals(compare));
     }
+
+    @DisplayName("정답과 사용자 입력을 비교 - 3스트라이크")
+    @Test
+    public void compareGoal_success1() throws Exception {
+        //given
+        ThreeBall goal = new ThreeBall(Arrays.asList(1, 2, 3));
+        List<Integer> userinput = Arrays.asList(1, 2, 3);
+
+        //when
+        GameService service = new GameService();
+        MatchResult matchResult = service.compareGoal(goal, userinput);
+
+        //then
+        assertTrue(matchResult.getMatch().get(MatchType.STRIKE) == 3);
+    }
+
 }
