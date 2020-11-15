bc6c3c17d863e7d7d266e5002fdcd04cd662daa0
Mingyu Lee <leemingyu05@gmail.com>
2020-11-15 16:42:28 +0900

    test: 숫자 판독-2: 다른자리 같은 수 > 볼

--- src/test/java/RefereeTest.java
+++ src/test/java/RefereeTest.java
@@ -24,9 +24,52 @@ public class RefereeTest {
 
 	}
 
-	private Integer getStrikeCount(Set<BaseballNumber> computerNumbers, Set<BaseballNumber> userNumbers) {
+	@DisplayName("볼 판단")
+	@Test
+	void checkBall() {
+		final Integer computerNumber = 425;
+		final Integer userNumber = 456;
+		Integer ballCount;
+
+		Set<BaseballNumber> computerNumbers = convertIntegerToBaseballNumberSet(computerNumber);
+		Set<BaseballNumber> userNumbers = convertIntegerToBaseballNumberSet(userNumber);
+
+		ballCount = this.getBallCount(computerNumbers, userNumbers);
+
+		assertThat(ballCount).isEqualTo(1);
+	}
+
+	private Integer getBallCount(Set<BaseballNumber> computerNumbers, Set<BaseballNumber> userNumbers) {
+		Set<BaseballNumber> retainSet = this.getRetainSetWith(computerNumbers, userNumbers);
+		Set<BaseballNumber> remainComputerNumbers = new HashSet<>(computerNumbers);
+		Set<BaseballNumber> remainUserNumbers = new HashSet<>(userNumbers);
+		remainComputerNumbers.removeAll(retainSet);
+		remainUserNumbers.removeAll(retainSet);
+
+		Set<Integer> computerIntegerNumbers = this.convertToIntegerSet(remainComputerNumbers);
+		Set<Integer> userIntegerNumbers = this.convertToIntegerSet(remainUserNumbers);
+
+		computerIntegerNumbers.retainAll(userIntegerNumbers);
+
+		return computerIntegerNumbers.size();
+	}
+
+	private Set<Integer> convertToIntegerSet(Set<BaseballNumber> remainBaseballNumberSet) {
+		Set<Integer> integerSet = new HashSet<>();
+		for (BaseballNumber baseballNumber : remainBaseballNumberSet) {
+			integerSet.add(baseballNumber.getValue());
+		}
+		return integerSet;
+	}
+
+	private Set<BaseballNumber> getRetainSetWith(Set<BaseballNumber> computerNumbers, Set<BaseballNumber> userNumbers) {
 		Set<BaseballNumber> compareSet = new HashSet<>(computerNumbers);
 		compareSet.retainAll(userNumbers);
+		return compareSet;
+	}
+
+	private Integer getStrikeCount(Set<BaseballNumber> computerNumbers, Set<BaseballNumber> userNumbers) {
+		Set<BaseballNumber> compareSet = this.getRetainSetWith(computerNumbers, userNumbers);
 		return compareSet.size();
 	}
 
