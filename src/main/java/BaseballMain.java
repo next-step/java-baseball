2378e0e9ae204064f8ee4b11709bcfddde66a876
sanghun <sanghun1984@gmail.com>
2020-11-15 23:15:10 +0900

    feat: 게임을 종료한다 -> 다시시작 or 종료.

--- src/main/java/BaseballMain.java
+++ src/main/java/BaseballMain.java
@@ -5,6 +5,8 @@ import service.BaseballService;
 public class BaseballMain {
 
 	private static final String NUMBER_INPUT_QUESTION = "숫자를 입력해주세요 : ";
+	private static final String END_OF_GAME_MESSAGE = BaseballService.SCORE_MAX_LENGTH
+		+ "개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	private static Scanner scanner;
 
@@ -15,8 +17,23 @@ public class BaseballMain {
 
 	private static void playBaseball() {
 		BaseballService baseballService = new BaseballService();
+		baseballService.generateGameMasterScore();
 		do {
 			System.out.print(NUMBER_INPUT_QUESTION);
+			baseballService.initScoreMap();
 		} while (!baseballService.playUntilEnd(scanner.next()));
+
+		endOfGameProcess();
+	}
+
+	private static void endOfGameProcess() {
+		System.out.println(END_OF_GAME_MESSAGE);
+		int answer = scanner.nextInt();
+		if (answer == 1) {
+			playBaseball();
+		}
+		if (answer == 2) {
+			System.exit(0);
+		}
 	}
 }
