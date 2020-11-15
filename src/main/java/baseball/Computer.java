db8a0caabbe2585d0e984a9ceb9743464660e76b
623nana <6230nana@gmail.com>
2020-11-15 20:27:38 +0900

    feat: 1부터 9까지 서로 다른 3자리 임의의 수 선택 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+  private static final int MAX_ANSWER_SIZE = 3;
+
+  public static List<Integer> generateAnswer() {
+    List<Integer> answer = new ArrayList<>();
+    while(answer.size() < MAX_ANSWER_SIZE) {
+      generateRandomNumber(answer);
+    }
+    return answer;
+  }
+
+  public static void generateRandomNumber(List<Integer> list) {
+    int random = (int) (Math.random() * 9) + 1;
+    if(!list.contains(random)) {
+      list.add(random);
+    }
+  }
+}
