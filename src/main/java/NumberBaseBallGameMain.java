85f69e7d5217f1025b79af188d568fdc03bf681a
songseongbin <ppsosqq@gmail.com>
2020-11-15 21:54:59 +0900

    <feat>: 입력한 수가 정답인 경우 게임을 계속 진행할지 게임 메인 순서 개발

--- src/main/java/NumberBaseBallGameMain.java
+++ src/main/java/NumberBaseBallGameMain.java
@@ -2,19 +2,43 @@ import java.util.Scanner;
 
 public class NumberBaseBallGameMain {
 
+	private NumberBaseBall numberBaseBall = new NumberBaseBall();
+
+	private static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		NumberBaseBallGameMain numberBaseBallGameMain = new NumberBaseBallGameMain();
-		numberBaseBallGameMain.play();
+		String restart = "1";
+
+		while (restart.equals("1")) {
+			numberBaseBallGameMain.play();
+			restart = sc.next();
+		}
 	}
 
 	public void play() {
-		String inputNumber = this.printMessageAndGetInput();
-		System.out.println("입력한 값: " + inputNumber);
+		String answerNumber = numberBaseBall.makeRandomNumberString();
+		String inputNumber = "";
+
+		while (isWrong(answerNumber, inputNumber)) {
+			inputNumber = this.printStartMessageAndGetInput();
+			System.out.println(numberBaseBall.calculate(answerNumber, inputNumber));
+		}
+
+		printAnswerMessage();
 	}
 
-	public String printMessageAndGetInput() {
+	private void printAnswerMessage() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+	}
+
+	private boolean isWrong(String answerNumber, String inputNumber) {
+		return !answerNumber.equals(inputNumber);
+	}
+
+	private String printStartMessageAndGetInput() {
 		System.out.print("숫자를 입력해주세요: ");
-		Scanner sc = new Scanner(System.in);
 		String inputNumber = sc.next();
 		return inputNumber;
 	}
