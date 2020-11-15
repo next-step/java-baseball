3ed4ab9500aea36938d35f3c3241f144c1242cc0
JUNGYI <>
2020-11-16 00:43:41 +0900

    feat : 컴퓨터는 입력한 숫자에 대한 결과(같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 포볼 또는 낫싱,  모든 수가 같은 자리에 있으면 성공)를 출력하고 성공할 때까지 게임을 반복

--- src/main/java/game/Baseball.java
+++ src/main/java/game/Baseball.java
@@ -6,17 +6,27 @@ import java.util.Scanner;
 
 public class Baseball {
 
-    private static final int MINIMUM_NUMBER = 1;
-    private static final int MAXIMUM_NUMBER = 9;
-    private static final int NUMBER_SELECTED_SIZE = 3;
-    private static final String REG_EXP =
+    public static final int MINIMUM_NUMBER = 1;
+    public static final int MAXIMUM_NUMBER = 9;
+    public static final int NUMBER_SELECTED_SIZE = 3;
+    public static final String REG_EXP =
             "^[" + MINIMUM_NUMBER + "-" + MAXIMUM_NUMBER + "]{" + NUMBER_SELECTED_SIZE + "}$";
+    private List<Integer> expectedBallList = new ArrayList<>();
 
     public Baseball() {}
 
     public void start() {
+        Scanner scan = new Scanner(System.in);
+        playBaseball(scan);
+    }
+
+    public void playBaseball(Scanner scan) {
         int[] computerNumbers = prepareComputerNumbers();
-        int[] userNumbers = prepareUserNumbers();
+        boolean loof = true;
+        while (loof) {
+            int[] userNumbers = prepareUserNumbers(scan);
+            loof = !checkAllStrike(computerNumbers, userNumbers);
+        }
     }
 
     public int[] prepareComputerNumbers() {
@@ -42,8 +52,7 @@ public class Baseball {
         return (int)(Math.random() * 10000) % (MAXIMUM_NUMBER - i);
     }
 
-    public int[] prepareUserNumbers() {
-        Scanner scan = new Scanner(System.in);
+    public int[] prepareUserNumbers(Scanner scan) {
         boolean loof = true;
         String inputNumber = null;
         while (loof) {
@@ -68,6 +77,97 @@ public class Baseball {
             userNumbers[i] = Integer.parseInt(String.valueOf(inputNumbers[i]));
         }
         return userNumbers;
+    }
+
+    public boolean checkAllStrike(int[] computerNumbers, int[] userNumbers) {
+        BaseballJudgment baseballJudgment = baseballJudgment(computerNumbers, userNumbers);
+        System.out.println(createMessage(baseballJudgment.getStrikeCount(), baseballJudgment.getBallCount()));
+        if (baseballJudgment.getStrikeCount() == NUMBER_SELECTED_SIZE) {
+            return true;
+        }
+        return false;
+    }
+
+    public BaseballJudgment baseballJudgment(int[] computerNumbers, int[] userNumbers) {
+        int strikeCount = 0;
+        for (int i = 0; i < NUMBER_SELECTED_SIZE; i++) {
+            strikeCount += isStrike(computerNumbers[i], userNumbers[i]);
+        }
+        int ballCount = getBallCount(userNumbers);
+        expectedBallList.clear();
+
+        BaseballJudgment baseballJudgment = new BaseballJudgment();
+        baseballJudgment.setStrikeCount(strikeCount);
+        baseballJudgment.setBallCount(ballCount);
+        return baseballJudgment;
+    }
+
+    public int isStrike(int computerNumber, int userNumber) {
+        if (computerNumber == userNumber) {
+            return 1;
+        }
+        expectedBallList.add(computerNumber);
+        return 0;
+    }
+
+    public int getBallCount(int[] userNumbers) {
+        int ballCount = 0;
+        for (int i = 0; i < userNumbers.length; i++) {
+            ballCount += isBall(userNumbers[i]);
+        }
+        return ballCount;
+    }
+
+    public int isBall(int number) {
+        if (expectedBallList.contains(number)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public String createMessage(int strikeCount, int ballCount) {
+        String message = createGameOverMessage(strikeCount);
+        message += createNothingMessage(strikeCount, ballCount);
+        message += createStrikeMessage(strikeCount, ballCount);
+        message += createBallMessage(strikeCount, ballCount);
+        message += createStrikeAndBallMessage(strikeCount, ballCount);
+        return message;
+    }
+
+    public String createGameOverMessage(int strikeCount) {
+        if (strikeCount == NUMBER_SELECTED_SIZE) {
+            return "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        }
+        return "";
+    }
+
+    public String createNothingMessage(int strikeCount, int ballCount) {
+        if (strikeCount == 0 && ballCount == 0) {
+            return "낫싱";
+        }
+        return "";
+    }
+
+    public String createStrikeMessage(int strikeCount, int ballCount) {
+        if (strikeCount > 0 && strikeCount < NUMBER_SELECTED_SIZE && ballCount == 0) {
+            return strikeCount  + " 스트라이크 ";
+        }
+        return "";
+    }
+
+    public String createBallMessage(int strikeCount, int ballCount) {
+        if (strikeCount == 0 && ballCount > 0) {
+            return ballCount  + " 볼";
+        }
+        return "";
+    }
+
+    public String createStrikeAndBallMessage(int strikeCount, int ballCount) {
+        if (strikeCount > 0 && strikeCount < NUMBER_SELECTED_SIZE
+                && ballCount > 0 && ballCount < NUMBER_SELECTED_SIZE) {
+            return strikeCount  + " 스트라이크 " + ballCount  + " 볼";
+        }
+        return "";
     }
 
 }
---
+++ src/main/java/game/BaseballJudgment.java
@@ -0,0 +1,25 @@
+package game;
+
+public class BaseballJudgment {
+
+    private int strikeCount;
+    private int ballCount;
+
+    public BaseballJudgment() {}
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public void setStrikeCount(int strikeCount) {
+        this.strikeCount = strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public void setBallCount(int ballCount) {
+        this.ballCount = ballCount;
+    }
+}
