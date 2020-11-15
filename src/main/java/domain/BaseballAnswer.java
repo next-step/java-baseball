20a5d7007fd433d2b9643c31bcb119be58a72553
hhlyn0328 <hhlyn0328@daum.net>
2020-11-15 23:19:37 +0900

    feat: 정답 숫자 비교 로직 및 테스트코드 작성
    - 정답 숫자와 사용자가 입력한 숫자를 비교한다
    - 스트라이크 갯수를 구한다
    - 스트라이크 갯수가 3개가 아니면 볼 갯수를 구한다
    - 스트라이크 / 볼 갯수가 없다면 낫싱 아니면 스트라이크 / 볼 갯수를 출력한다

--- src/main/java/domain/BaseballAnswer.java
+++ src/main/java/domain/BaseballAnswer.java
@@ -5,19 +5,87 @@ import java.util.Random;
 import java.util.Set;
 
 public class BaseballAnswer {
-    private Integer[] answer;
+    private Character[] answer;
+    private int strikeCnt;
+    private int ballCnt;
 
-    public Integer[] getAnswer() {
+    public Character[] getAnswer() {
         return answer;
     }
 
+    public void setAnswer(Character[] answer) {
+        this.answer = answer;
+    }
+
+    public int getStrikeCnt() {
+        return strikeCnt;
+    }
+
+    public int getBallCnt() {
+        return ballCnt;
+    }
+
     public void makeAnswer() {
-        Set<Integer> numbers = new HashSet<>();
+        Set<Character> numbers = new HashSet<>();
         Random random = new Random();
         while (numbers.size() != 3) {
-            numbers.add(random.nextInt(8) + 1);
+            numbers.add(Character.forDigit(random.nextInt(8) + 1, 10));
         }
-        answer = new Integer[numbers.size()];
+        answer = new Character[numbers.size()];
         numbers.toArray(answer);
+    }
+
+    public void checkAnswer(char[] inputCharArray) {
+        initCnt();
+        checkStrike(inputCharArray);
+        if (!isExatlyAnswer()) {
+            checkBall(inputCharArray);
+        }
+        printHint();
+    }
+
+    private void initCnt() {
+        strikeCnt = 0;
+        ballCnt = 0;
+    }
+
+    private void checkStrike(char[] inputCharArray) {
+        if (inputCharArray[0] == answer[0]) {
+            strikeCnt++;
+        }
+        if (inputCharArray[1] == answer[1]) {
+            strikeCnt++;
+        }
+        if (inputCharArray[2] == answer[2]) {
+            strikeCnt++;
+        }
+    }
+
+    private void checkBall(char[] inputCharArray) {
+        if (inputCharArray[0] != answer[0] && (inputCharArray[0] == answer[1] || inputCharArray[0] == answer[2])) {
+            ballCnt++;
+        }
+        if (inputCharArray[1] != answer[1] && (inputCharArray[1] == answer[0] || inputCharArray[1] == answer[2])) {
+            ballCnt++;
+        }
+        if (inputCharArray[2] != answer[2] && (inputCharArray[2] == answer[0] || inputCharArray[2] == answer[1])) {
+            ballCnt++;
+        }
+    }
+
+    private void printHint() {
+        if (isNothing()) {
+            System.out.println("낫싱");
+            return;
+        }
+        System.out.println(String.format("%d 스트라이크 %d 볼", strikeCnt, ballCnt));
+    }
+
+    public boolean isNothing() {
+        return strikeCnt == 0 && ballCnt == 0;
+    }
+
+    public boolean isExatlyAnswer() {
+        return strikeCnt == 3;
     }
 }
--- src/test/java/domain/BaseballAnswerTest.java
+++ src/test/java/domain/BaseballAnswerTest.java
@@ -29,7 +29,7 @@ public class BaseballAnswerTest {
         @Order(2)
         @DisplayName("1-9만 포함하는지 확인")
         void noZeroTest(int index) {
-            assertThat(answer.getAnswer()[index]).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(9);
+            assertThat(answer.getAnswer()[index]).isGreaterThanOrEqualTo('1').isLessThanOrEqualTo('9');
         }
 
         @Test
@@ -39,6 +39,52 @@ public class BaseballAnswerTest {
             assertThat(answer.getAnswer()[0]).isNotEqualTo(answer.getAnswer()[1]).isNotEqualTo(answer.getAnswer()[2]);
             assertThat(answer.getAnswer()[1]).isNotEqualTo(answer.getAnswer()[0]).isNotEqualTo(answer.getAnswer()[2]);
             assertThat(answer.getAnswer()[2]).isNotEqualTo(answer.getAnswer()[0]).isNotEqualTo(answer.getAnswer()[1]);
+        }
+    }
+
+    @Nested
+    @DisplayName("사용자 입력 숫자 비교 테스트 (정답: 3,2,8)")
+    class CheckAnswerTest {
+        @BeforeEach
+        void setUp() {
+            Character[] answerArray = {'3', '2', '8'};
+            answer.setAnswer(answerArray);
+        }
+
+        @Test
+        @Order(1)
+        @DisplayName("328 정답 확인")
+        void exatlyAnswerTest() {
+            String answerInput = "328";
+            answer.checkAnswer(answerInput.toCharArray());
+            assertThat(answer.getStrikeCnt()).isEqualTo(3);
+            assertThat(answer.getBallCnt()).isEqualTo(0);
+            assertThat(answer.isExatlyAnswer()).isTrue();
+            assertThat(answer.isNothing()).isFalse();
+        }
+
+        @Test
+        @Order(2)
+        @DisplayName("298 1 스트라이크 1볼 확인")
+        void strikeBallText() {
+            String answerInput = "298";
+            answer.checkAnswer(answerInput.toCharArray());
+            assertThat(answer.getStrikeCnt()).isEqualTo(1);
+            assertThat(answer.getBallCnt()).isEqualTo(1);
+            assertThat(answer.isExatlyAnswer()).isFalse();
+            assertThat(answer.isNothing()).isFalse();
+        }
+
+        @Test
+        @Order(3)
+        @DisplayName("769 낫싱 확인")
+        void nothingText() {
+            String answerInput = "769";
+            answer.checkAnswer(answerInput.toCharArray());
+            assertThat(answer.getStrikeCnt()).isEqualTo(0);
+            assertThat(answer.getBallCnt()).isEqualTo(0);
+            assertThat(answer.isExatlyAnswer()).isFalse();
+            assertThat(answer.isNothing()).isTrue();
         }
     }
 }
