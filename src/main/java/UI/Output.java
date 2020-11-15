f494153ba4c56f57185c85bb6ca2cc5a954f2bf9
taehan-yoon <workingyth@gmail.com>
2020-11-15 22:49:26 +0900

    feature($Output) : 입력받은 숫자에 대한 결과 출력
    입력받은 숫자에 대한 결과를 스트라이크, 볼의 갯수에 따라 다른 메시지
    출력
    - 스트라이크, 볼 모두 존재: 'N스트라이크 N볼'
    - 스트라이크, 볼 모두 없을때: '낫싱'
    - 스트라이크만 있을때: 'N스트라이크'
    - 볼만 있을때: '볼'
    - 3스트라이크 경우: '3개의 숫자를 모두 맞히셨습니다! 게임종료'

--- src/main/java/UI/Output.java
+++ src/main/java/UI/Output.java
@@ -1,2 +1,27 @@
-package UI;public class Output {
+package UI;
+
+import UI.msg.OutputMessage;
+import domain.baseballRule.BaseballResult;
+
+public class Output {
+
+	public static void printBaseballResult(BaseballResult baseballResult) {
+
+		if (baseballResult.isNothing()) {
+			System.out.print(OutputMessage.NOTHING);
+			return;
+		}
+
+		if (baseballResult.getStrikeCount() > 0) {
+			System.out.print(OutputMessage.STRIKE);
+		}
+
+		if (baseballResult.getBallCount() > 0) {
+			System.out.print(OutputMessage.BALL);
+		}
+
+		if (baseballResult.isThreeStrike()) {
+			System.out.print(OutputMessage.THREE_STRIKE);
+		}
+	}
 }
---
+++ src/main/java/UI/msg/OutputMessage.java
@@ -0,0 +1,8 @@
+package UI.msg;
+
+public class OutputMessage {
+	public static final String THREE_STRIKE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	public static final String NOTHING = "낫싱";
+	public static final String STRIKE = "스트라이크";
+	public static final String BALL = "볼";
+}
