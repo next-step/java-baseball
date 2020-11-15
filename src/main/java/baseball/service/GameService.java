acfbf7fca9cb4995bfd7dba1e899a9210a05faf8
hjdeepsleep <hjdeepsleep@gmail.com>
2020-11-15 13:21:35 +0900

    feat: 정답과 사용자 입력을 비교하는 서비스
    - 생성된 정답과 사용자가 입력한 숫자를 받아 비교하고 결과 반환

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,12 +1,20 @@
 package baseball.service;
 
+import baseball.domain.MatchResult;
 import baseball.domain.ThreeBall;
 import baseball.domain.strategy.BallGenerateStrategy;
+
+import java.util.List;
 
 public class GameService {
 
     public ThreeBall generateGoal(BallGenerateStrategy ballStrategy) {
         return new ThreeBall(ballStrategy);
+    }
+
+    public MatchResult compareGoal(final ThreeBall goal, List<Integer> userInput) {
+        ThreeBall userBall = new ThreeBall(userInput);
+        return goal.compareAll(userBall);
     }
 
 }
