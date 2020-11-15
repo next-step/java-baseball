9765b998fa7876929d88a02bffd3a513cca9ee3c
박상욱 <aristein.park@gmail.com>
2020-11-16 00:40:40 +0900

    chore: 미사용 코드 제거 및 미개발 구현 Task 설계 제거
    테스트용 모델 및 모델 테스트 제거
    테스트용 메인 임시 테스트 제거
    README.md에서 구현하지 않을 항목들 제거

--- src/test/java/baseballgame/MainTest.java
+++ src/test/java/baseballgame/MainTest.java
@@ -8,18 +8,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
-    @Test
-    @DisplayName("기본 테스트입니다")
-    void defaultTest() {
-        System.out.println("test");
-        assertEquals("test", "test");
-    }
-
-    @Test
-    void main() {
-    }
-
     @DisplayName("게임 재실행 여부 체크")
     @ParameterizedTest
     @CsvSource(value = {"1:true", "2:false", "a:false", "3:false"}, delimiter = ':')
