c2409a77fb05e0c126c7739343e1231da91d90c6
이다영 <lee.dy@kstc.co.kr>
2020-11-17 00:13:46 +0900

    style: 코딩 컨벤션 수정

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -4,28 +4,28 @@ import baseball.configuration.BaseballConfiguration;
 import baseball.exception.BizException;
 import baseball.exception.BizExceptionType;
 import baseball.util.BaseballUtil;
+
 import java.util.HashSet;
 
 public class BaseballNumbers {
 
     private String numbers;
 
-    public BaseballNumbers(String numbers){
+    public BaseballNumbers(String numbers) {
         this.numbers = numbers;
     }
 
-    public void checkValidation(){
-
-        if(this.numbers.length() != BaseballConfiguration.REQUIRED_COUNT){
+    public void checkValidation() {
+        if (this.numbers.length() != BaseballConfiguration.REQUIRED_COUNT) {
             throw new BizException(BizExceptionType.INVALID_LENGTH);
         }
-        if(!BaseballUtil.isAllNumber(this.numbers)){
+        if (!BaseballUtil.isAllNumber(this.numbers)) {
             throw new BizException(BizExceptionType.NUMBER_ONLY);
         }
-        if(!isRangeNumber(this.numbers)){
+        if (!isRangeNumber(this.numbers)) {
             throw new BizException(BizExceptionType.OUT_OF_BOUNDS_NUMBER);
         }
-        if(isIncludeDuplicateNumber(this.numbers)){
+        if (isIncludeDuplicateNumber(this.numbers)) {
             throw new BizException(BizExceptionType.DUPLICATE_NUMBER);
         }
     }
@@ -34,22 +34,23 @@ public class BaseballNumbers {
         return numbers;
     }
 
-    private boolean isRangeNumber(String inputText){
+    private boolean isRangeNumber(String inputText) {
         String[] textArr = inputText.split("");
-        for(String str : textArr){
+        for (String str : textArr) {
             int number = Integer.parseInt(str);
-            if(number < BaseballConfiguration.MIN_NUMBER || number > BaseballConfiguration.MAX_NUMBER){
+            if (number < BaseballConfiguration.MIN_NUMBER || number > BaseballConfiguration.MAX_NUMBER) {
                 return false;
             }
         }
         return true;
     }
-    private boolean isIncludeDuplicateNumber(String inputText){
+
+    private boolean isIncludeDuplicateNumber(String inputText) {
         HashSet<String> set = new HashSet<>();
-        for(String num : inputText.split("")){
+        for (String num : inputText.split("")) {
             set.add(num);
         }
-        if(set.size() != BaseballConfiguration.REQUIRED_COUNT){
+        if (set.size() != BaseballConfiguration.REQUIRED_COUNT) {
             return true;
         }
         return false;
