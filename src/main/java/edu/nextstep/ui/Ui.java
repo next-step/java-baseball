6bf15ccc10a53f6416532487834a358754e8f705
Donghwan Noh <namul.bap@gmail.com>
2020-11-16 19:53:46 +0900

    feat(ui): enters whether to start new game
    Ui 클래스에 다음 메소드 추가
    - inputForContinue : 새 게임을 시작할지 여부를 판단할 수 있도록 1 또는 2를 입력받아 리턴함.

--- src/main/java/edu/nextstep/ui/Ui.java
+++ src/main/java/edu/nextstep/ui/Ui.java
@@ -19,6 +19,15 @@ public class Ui {
 		printMessageLine(numberSize + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
 
+	public static String inputForContinue() {
+		printMessageLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String input = scanner.nextLine();
+		if (!Pattern.matches("^1|2$", input)) {
+			return inputForContinue();
+		}
+		return input;
+	}
+
 	public static void printMessage(String message) {
 		System.out.print(message);
 	}
