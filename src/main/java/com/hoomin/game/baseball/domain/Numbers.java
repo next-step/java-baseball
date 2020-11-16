f8cafb37e46ba403647755fafc8536c633f51bf7
lhm7877 <vkfdldh7>
2020-11-17 01:53:29 +0900

    feat: Numbers 생성시 validate 추가

--- src/main/java/com/hoomin/game/baseball/domain/Numbers.java
+++ src/main/java/com/hoomin/game/baseball/domain/Numbers.java
@@ -1,14 +1,37 @@
 package com.hoomin.game.baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
 
 import com.hoomin.game.baseball.enums.HintState;
 
 public class Numbers {
+	private static final int NUMBER_SIZE = 3;
 	private final List<Integer> numberList;
 
 	public Numbers(List<Integer> numberList) {
+		validateSize(numberList);
+		validateNumber(numberList);
+		validateDuplicate(numberList);
 		this.numberList = numberList;
+	}
+
+	private void validateDuplicate(List<Integer> numberList) {
+		if (numberList.size() != new HashSet<>(numberList).size()) {
+			throw new IllegalArgumentException("중복되지 않은 숫자를 입력해야 합니다.");
+		}
+	}
+
+	private void validateNumber(List<Integer> numberList) {
+		if (numberList.contains(0)) {
+			throw new IllegalArgumentException("1부터 9까지의 숫자를 입력해야 합니다.");
+		}
+	}
+
+	private void validateSize(List<Integer> numbers) {
+		if (numbers.size() != NUMBER_SIZE) {
+			throw new IllegalArgumentException("3자리의 숫자를 입력해야 합니다.");
+		}
 	}
 
 	Hints compareNumbers(Numbers inputNumbers) {
@@ -23,11 +46,9 @@ public class Numbers {
 		if (this.numberList.get(index).equals(inputNumbers.get(index))) {
 			return HintState.STRIKE;
 		}
-
 		if (this.numberList.contains(inputNumbers.get(index))) {
 			return HintState.BALL;
 		}
-
 		return HintState.NOTHING;
 	}
 }
