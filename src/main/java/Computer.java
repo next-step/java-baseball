4c2983d0ba20d5b9079242a4ee50def5da6cb055
JeJe <las9897@gmail.com>
2020-11-16 14:15:19 +0900

    feat: 컴퓨터는 게임 플레이어가 입력한 숫자에  힌트 출력

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -7,6 +7,8 @@ public class Computer {
 
     private List<String> numbers;
     private Random random;
+    private int strike;
+    private int boll;
 
     public Computer() {
         random = new Random(System.currentTimeMillis());
@@ -19,6 +21,39 @@ public class Computer {
             int randomIndex = random.nextInt(numberList.size());
             numbers.add(numberList.remove(randomIndex));
         }
+    }
+
+    public void checkResult(List<String> userNumbers) {
+        strike = 0;
+        boll = 0;
+        for (int i = 0; i < 3; i++) {
+            checkStrikeAndBoll(i, userNumbers.get(i));
+        }
+        resultMessage();
+    }
+
+    public void checkStrikeAndBoll(int index, String userNumber) {
+        if (numbers.contains(userNumber)) {
+            boll++;
+        }
+        if (userNumber.equals(numbers.get(index))) {
+            strike++;
+            boll--;
+        }
+    }
+
+    public void resultMessage() {
+        String message = "";
+        if (strike > 0) {
+            message += strike + " 스트라이크 ";
+        }
+        if (boll > 0) {
+            message += boll + "볼";
+        }
+        if (strike == 0 && boll == 0) {
+            message = "낫싱";
+        }
+        System.out.println(message);
     }
 
     public List<String> getNumbers() {
