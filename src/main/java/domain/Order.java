04a0776f65fea5e3fea85ee3a72d266216c518ef
hyeji <khji1210@naver.com>
2020-11-16 15:44:45 +0900

    feat : Order 구현

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -1,2 +1,22 @@
-package domain;public class Order {
+package domain;
+
+public class Order {
+    private static final int START_POSITION = 0;
+    private static final int LAST_POSITION = 2;
+    private final int value;
+
+    public Order(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+        if (START_POSITION > value || LAST_POSITION < value) {
+            throw new IllegalArgumentException("자릿수 범위를 초과하였습니다.");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class OrderTest {
-  
+
+    @DisplayName("자릿수 index 값이 0~2 범위를 넘어갈 경우, Exception 발생.")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5})
+    void validateRangePosition(int value) {
+        assertThatThrownBy(() -> new Order(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자릿수 범위를 초과하였습니다.");
+    }
+
 }
