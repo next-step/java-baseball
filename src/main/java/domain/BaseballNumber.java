e3b9ce06e5b7914da9583d29fa34aa2725ec3dcf
hyeji <khji1210@naver.com>
2020-11-16 15:43:54 +0900

    feat : BaseballNumber 구현

--- src/main/java/domain/BaseballNumber.java
+++ src/main/java/domain/BaseballNumber.java
@@ -1,2 +1,22 @@
-package domain;public class BaseballNumber {
+package domain;
+
+public class BaseballNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    private final Order order;
+    private final int baseballNumber;
+
+    public BaseballNumber(Order order, final int baseballNumber) {
+        validate(baseballNumber);
+
+        this.order = order;
+        this.baseballNumber = baseballNumber;
+    }
+
+    private void validate(final int baseballNumber) {
+        if (MIN_NUMBER > baseballNumber || MAX_NUMBER < baseballNumber) {
+            throw new IllegalArgumentException("숫자 범위를 초과하였습니다.");
+        }
+    }
 }
--- src/test/java/domain/BaseballNumberTest.java
+++ src/test/java/domain/BaseballNumberTest.java
@@ -1,4 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class BaseballNumberTest {
-  
+
+    @DisplayName("숫자가 1~9가 아닐 경우, Exception 발생")
+    @ParameterizedTest
+    @MethodSource("provideNumbers")
+    void validateRangeNumber(Order order, int baseballNumber) {
+        assertThatThrownBy(() -> new BaseballNumber(order, baseballNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자 범위를 초과하였습니다.");
+    }
+
+    private static Stream<Arguments> provideNumbers() {
+        return Stream.of(
+                Arguments.of(new Order(0), 0),
+                Arguments.of(new Order(1), 10)
+        );
+    }
 }
