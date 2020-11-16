bda987346ade7eb7aaf833d3f16742b510252e3f
parkeeseul <eeseul.p@gmail.com>
2020-11-17 03:41:19 +0900

    refactor: RESTART_GAME, END_GAME 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,9 +3,10 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final int RESTART = 1;
+    private static final int RESTART_GAME = 1;
+    private static final int END_GAME = 2;
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+    private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 " + RESTART_GAME + ", 종료하려면 " + END_GAME + "를 입력하세요";
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static String inputNumber() {
@@ -15,6 +16,6 @@ public class InputView {
 
     public static boolean restart() {
         System.out.println(INPUT_RESTART_MESSAGE);
-        return RESTART == SCANNER.nextInt();
+        return RESTART_GAME == SCANNER.nextInt();
     }
 }
