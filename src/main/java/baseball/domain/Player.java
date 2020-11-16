068f8a03a6bf84f2b03380945eeb0c17dedf2781
becky <becky@kmong.com>
2020-11-16 23:50:15 +0900

    feat: 1부터 9까지 3자리의 수 입력 받음

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,53 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+	private static final int BALL_SIZE = 3;
+
+	private static final String ERROR_BALL_SIZE = "3개의 숫자를 입력해주세요.";
+	private static final String ERROR_BALL_DUPLICATION = "중복된 숫자가 있습니다.";
+	private static final String ERROR_BALL_RANGE = "1~9 까지의 숫자만 허용하고 있습니다.";
+
+	private List<Integer> baseball;
+
+	public Player() {
+	}
+
+	public List<Integer> inputBaseBall(String playerInput) {
+		baseball = new ArrayList<>();
+		for(int i = 0; i < playerInput.length(); i++) {
+			int ball = Character.getNumericValue(playerInput.charAt(i));
+			validInput(ball);
+			baseball.add(ball);
+		}
+
+		checkBallSize();
+
+		return baseball;
+	}
+
+	private void validInput(int input) {
+		if(baseball.contains(input)) {
+			throw new IllegalArgumentException(ERROR_BALL_DUPLICATION);
+		}
+
+		if(input < 1 ) {
+			throw new IllegalArgumentException(ERROR_BALL_RANGE);
+		}
+
+	}
+
+	private void checkBallSize() {
+		if(baseball.size() != BALL_SIZE) {
+			throw new IllegalArgumentException(ERROR_BALL_SIZE);
+		}
+	}
+
+	public List<Integer> getBaseball() {
+		return baseball;
+	}
+
+
+}
