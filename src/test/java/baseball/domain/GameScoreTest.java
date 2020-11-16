85df815b6763fd6f012c0bc5edf02237b5c7fa76
이다영 <lee.dy@kstc.co.kr>
2020-11-16 23:47:56 +0900

    test: 게임 점수 계산 관련 테스트 케이스 추가

--- src/test/java/baseball/domain/GameScoreTest.java
+++ src/test/java/baseball/domain/GameScoreTest.java
@@ -2,17 +2,25 @@ package baseball.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("게임 점수 테스트")
 public class GameScoreTest {
 
+    private GameScore gameScore;
+    private String[] randomNumbers;
+
+    @BeforeEach
+    public void setUp(){
+        randomNumbers = new String[] {"3","1","4"};
+        gameScore = new GameScore();
+    }
+
     @DisplayName("게임 성적 계산 > 성공")
     @Test
-    public void checkGameScore(){
-        String[] randomNumbers = new String[] {"3","1","4"};
-        GameScore gameScore = new GameScore();
+    public void checkGameScoreSuccess(){
 
         gameScore.checkGameScore(randomNumbers, "314");
         assertEquals(3, gameScore.getStrike());
@@ -25,6 +33,27 @@ public class GameScoreTest {
         gameScore.checkGameScore(randomNumbers, "124");
         assertEquals(1, gameScore.getStrike());
         assertEquals(1, gameScore.getBall());
+    }
+
+    @DisplayName("게임 클리어 여부 체크 > 성공")
+    @Test
+    public void isGameClearSuccess(){
+        gameScore.checkGameScore(randomNumbers, "314");
+        assertEquals(true, gameScore.isGameClear());
+    }
+
+    @DisplayName("게임 스트라이크 카운트 존재여부 체크 > 성공")
+    @Test
+    public void isExistStrikeCountSuccess(){
+        gameScore.checkGameScore(randomNumbers, "314");
+        assertEquals(true, gameScore.isExistStrikeCount());
+    }
+
+    @DisplayName("게임 볼 카운트 존재여부 체크 > 성공")
+    @Test
+    public void isExistBallCountSuccess(){
+        gameScore.checkGameScore(randomNumbers, "134");
+        assertEquals(true, gameScore.isExistBallCount());
     }
 }
 
