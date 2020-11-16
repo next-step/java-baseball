11c7e6391cb3ceeef4fc0432a8f00b2e5a3b6a24
aiceru <aiceru@gmail.com>
2020-11-16 10:25:15 +0900

    feat(gameLogic): add Result class
    - add Result class for storing result of compare
    - add test cases for Result class

---
+++ src/main/java/baseballgame/Result.java
@@ -0,0 +1,34 @@
+package baseballgame;
+
+public class Result {
+  private static final String STRIKE_STR = "스트라이크";
+  private static final String BALL_STR = "볼";
+  private static final String NOTHING_STR = "낫싱";
+  private static final String SPACE_STR = " ";
+
+  public int strikes;
+  public int balls;
+
+  public Result() {
+    this(0, 0);
+  }
+
+  public Result(int s, int b) {
+    this.strikes = s;
+    this.balls = b;
+  }
+
+  public boolean hasWin() {
+    return strikes == BaseballGame.DIGITS;
+  }
+
+  public String getMessage() {
+    StringBuilder sb = new StringBuilder();
+    if (strikes == 0 && balls == 0) {
+      return new String(NOTHING_STR);
+    }
+    sb.append(strikes > 0 ? strikes + STRIKE_STR + SPACE_STR : "");
+    sb.append(balls > 0 ? balls + BALL_STR : "");
+    return sb.toString().trim();
+  }
+}
---
+++ src/test/java/baseballgame/ResultTest.java
@@ -0,0 +1,35 @@
+package baseballgame;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ResultTest {
+  @Test
+  @DisplayName("Message test")
+  void messageTest() {
+    Result result = new Result(1, 1);
+    assertThat(result.getMessage()).isEqualTo("1스트라이크 1볼");
+    assertThat(result.hasWin()).isEqualTo(false);
+
+    result.strikes = 0;
+    assertThat(result.getMessage()).isEqualTo("1볼");
+    assertThat(result.hasWin()).isEqualTo(false);
+
+    result.strikes = 2;
+    result.balls = 0;
+    assertThat(result.getMessage()).isEqualTo("2스트라이크");
+    assertThat(result.hasWin()).isEqualTo(false);
+
+    result.strikes = 0;
+    result.balls = 0;
+    assertThat(result.getMessage()).isEqualTo("낫싱");
+    assertThat(result.hasWin()).isEqualTo(false);
+
+    result.strikes = 3;
+    result.balls = 0;
+    assertThat(result.getMessage()).isEqualTo("3스트라이크");
+    assertThat(result.hasWin()).isEqualTo(true);
+  }
+}
