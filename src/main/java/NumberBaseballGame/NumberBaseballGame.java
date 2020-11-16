2668d32abd15aa13d09084db9d49f548570ab005
kimhaegyeong <schema9@gmail.com>
2020-11-16 20:59:29 +0900

    feat(main): 정답결과에 따라 진행사항
    - 3개의 숫자를 모두 맞힌 경우, 게임을 종료한다
    - 게임을 종료한 후 다시 시작할 것인지 완전종료할 것인지 선택한다

--- src/main/java/NumberBaseballGame/NumberBaseballGame.java
+++ src/main/java/NumberBaseballGame/NumberBaseballGame.java
@@ -18,11 +18,13 @@ public class NumberBaseballGame {
 		List<Integer> numList = new ArrayList<Integer>(Arrays.asList(numArray));
 		int randomIndex = 0;
 
-		for (int i = 0; i < answer.length; i++) {
+		for (int i = 0; i < 3; i++) {
 			randomIndex = (int)(Math.random() * (numList.size()));
 			answer[i] = numList.get(randomIndex);
 			numList.remove(randomIndex);
 		}
+
+		//System.out.println(answer[0] + ":" + answer[1] + ":" + answer[2]);
 	}
 
 	public int[] writeInputDate() {
@@ -41,14 +43,83 @@ public class NumberBaseballGame {
 		return inputData;
 	}
 
+	public BallCount judgeBallCount(int[] inputData) {
+		List<BallCount> ballCountList = new ArrayList<BallCount>();
+		BallCount ballCountSum = new BallCount();
+
+		for (int i = 0; i < 3; i++) {
+			ballCountList.add(checkBallCount(i, inputData[i]));
+		}
+
+		for (int i = 0; i < ballCountList.size(); i++) {
+			ballCountSum.addStrikeCount(ballCountList.get(i).getStrike());
+			ballCountSum.addBallCount(ballCountList.get(i).getBall());
+		}
+
+		return ballCountSum;
+	}
+
+	public BallCount checkBallCount(int index, int value) {
+		BallCount ballCount = new BallCount();
+
+		for (int j = 0; j < 3; j++) {
+			ballCount.checkStrike(index, value, j, answer[j]);
+			ballCount.checkBall(index, value, j, answer[j]);
+		}
+
+		return ballCount;
+	}
+
+	public void printBallCount(BallCount ballCount) {
+		if (ballCount.getStrike() != 0) {
+			System.out.print(ballCount.getStrike() + " 스트라이크    ");
+		}
+		if (ballCount.getBall() != 0) {
+			System.out.print(ballCount.getBall() + " 볼 ");
+		}
+		if (ballCount.getStrike() + ballCount.getBall() == 0) {
+			System.out.print("낫씽");
+		}
+		System.out.print("\n");
+	}
+
+	public void checkEndGame(BallCount ballCount) {
+		if (ballCount.getStrike() != 3) {
+			return;
+		}
+
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+		progressGame();
+	}
+
+	public void progressGame() {
+		String progressFlg = "";
+		Scanner scanner = new Scanner(System.in);
+
+		while (!progressFlg.equals("1") && !progressFlg.equals("2")) {
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			progressFlg = scanner.next();
+		}
+
+		if (progressFlg.equals("2")) {
+			System.exit(0);
+		}
+	}
+
 	public static void main(String[] args) {
 
-		int[] inputData = new int[3];
-		NumberBaseballGame game = new NumberBaseballGame();
+		while (true) {
+			int[] inputData = new int[3];
+			BallCount ballCount = new BallCount();
+			NumberBaseballGame game = new NumberBaseballGame();
 
-		game.setAnswer();
-		inputData = game.writeInputDate();
-
+			game.setAnswer();
+			inputData = game.writeInputDate();
+			ballCount = game.judgeBallCount(inputData);
+			game.printBallCount(ballCount);
+			game.checkEndGame(ballCount);
+		}
 	}
 
 }
