5570cd696477255dded617ad73cd84ee1f6ed0af
sybarits <sybarits@gmail.com>
2020-11-16 21:52:44 +0900

    feat: fix generate three number
    no duplicated number in generated three digit
    random number can be duplicated

--- src/main/java/com/woowa/baseball/GameHandler.java
+++ src/main/java/com/woowa/baseball/GameHandler.java
@@ -1,5 +1,6 @@
 package com.woowa.baseball;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameHandler {
@@ -38,19 +39,38 @@ public class GameHandler {
     }
 
     public String getThreeNumber() {
-        StringBuilder result = new StringBuilder();
-        while (result.length() < 3) {
-            result.append(getRandomNumber());
+        ArrayList<String> result = new ArrayList<>();
+        String number = getRandomNumber();
+        while (result.size() < 3) {
+            addNumber(result, number);
+            number = getRandomNumber();
         }
-        return result.toString();
+        return changeListToString(result);
+    }
+
+    public ArrayList<String> addNumber(ArrayList<String> array, String number) {
+        if (array.contains(number)) {//check duplicated number
+            return array;
+        }
+        array.add(number);
+        return array;
     }
 
     public String getRandomNumber() {
         Random rand = new Random();
         int upperbound = 9;//generate random values from 0-8
         int int_random = rand.nextInt(upperbound);
-        int_random++;
+        int_random++;//make random value 1-9
         return Integer.toString(int_random);
+    }
+
+    public String changeListToString(ArrayList<String> input) {
+        int i = 0;
+        StringBuilder result = new StringBuilder();
+        while (result.length() < input.size()) {
+            result.append(input.get(i++));
+        }
+        return result.toString();
     }
 
 }
--- src/test/java/com/woowa/baseball/GameHandlerTest.java
+++ src/test/java/com/woowa/baseball/GameHandlerTest.java
@@ -2,19 +2,22 @@ package com.woowa.baseball;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GameHandlerTest {
 
     private GameHandler gh;
+    ArrayList<String> array;
 
     @BeforeEach
     void setup() {
         gh = new GameHandler();
+        array = new ArrayList<>();
+        array.add("1");
+        array.add("2");
     }
 
     @Test
@@ -25,6 +28,29 @@ public class GameHandlerTest {
     @Test
     void getThreeNumberTest() {
         Assertions.assertThat(gh.getThreeNumber().matches("\\d{3}")).isTrue();
+    }
+
+    @Test
+    @DisplayName("add number 1 test no duplicate number")
+    void addNumberTest() {
+        Assertions.assertThat(gh.addNumber(array,"1")).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("add number 2 test no duplicate number")
+    void addNumberTest2() {
+        Assertions.assertThat(gh.addNumber(array,"2")).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("add number 3 test no duplicate number")
+    void addNumberTest3() {
+        Assertions.assertThat(gh.addNumber(array,"3")).containsExactly("1", "2", "3");
+    }
+
+    @Test
+    void changeListToStringTest() {
+        Assertions.assertThat(gh.changeListToString(array)).isEqualTo("12");
     }
 
 }
