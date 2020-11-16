e76ee30776225c63058e8ffbf2bc1ed89f80233f
Eugene Kim <eugene70kim@gmail.com>
2020-11-16 11:22:17 +0900

    test: 숫자생성 테스트 보완
    - 생성된 숫자 검증시 중복여부 확인 추가
    - refactor: 한글 테스트명 영문으로 변경

--- src/test/java/NumberGeneratorTest.java
+++ src/test/java/NumberGeneratorTest.java
@@ -1,19 +1,28 @@
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class NumberGeneratorTest {
 
     @RepeatedTest(100)
-    public void 생성된수는3개이고1부터9까지의수이다() {
+    public void testGeneratedNumbersByRepeat() {
         final int[] threeNumbers = NumberGenerator.generate();
-        assertThat(threeNumbers).hasSize(3)
-                                .containsAnyOf(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertThat(threeNumbers).hasSize(3) /*숫자는 3개*/
+                                .containsAnyOf(1, 2, 3, 4, 5, 6, 7, 8, 9) /*1부터 9까지의 숫자*/
+                                .has(new Condition<int[]>("생성된 숫자는 중복되지 않는다") {
+                                    @Override
+                                    public boolean matches(int[] value) {
+                                        return value[0] != value[1] &&
+                                               value[0] != value[2] &&
+                                               value[1] != value[2];
+                                    }
+                                });
     }
 
     @Test
-    public void 같은수가나올확율10퍼센트미만이여야함() {
+    public void testDuplicationProbabilityForGeneratedNumber() {
         // 확율상 9 * 8 * 7 = 504 의 경우의 수가 있으므로 중복이 발생할 확율은 0.2% 미만이지만 상식선에서 10%를 오차범위로 둔다.
         final int[] firstNumbers = NumberGenerator.generate();
         int collisionCount = 0;
