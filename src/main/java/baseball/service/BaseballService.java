4babed3e0d0c6423d2b0bfef4cce3a189105db0e
chudaeyoun <chudaeyoun@naver.com>
2020-11-17 01:30:36 +0900

    feat(feature): 임의의 중복되지 않는 3개의 난수 생성
    중복이 나올 경우 계속해서 재시도

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,21 +1,49 @@
 package baseball.service;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class BaseballService {
     private static final int BALL_COUNT = 3;
+    private static final int MAX_RANGE_NUM = 10;
+    private static int[] computerNums;
     private static int[] inputNums;
     private static Scanner sc;
 
     public BaseballService() {
+        computerNums = new int[BALL_COUNT];
         inputNums = new int[BALL_COUNT];
         sc = new Scanner(System.in);
     }
 
     public void start() {
+        setComputerNums();
         System.out.print("숫자를 입력해주세요 : ");
         divideInputNum(getInputNum());
+    }
+
+    public int[] setComputerNums() {
+        initComputerNums();
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < BALL_COUNT; i++) {
+            while (true) {
+                int num = makeNum();
+                if (!set.contains(num)) {
+                    set.add(num);
+                    computerNums[i] = num;
+                    break;
+                }
+            }
+        }
+        return computerNums;
+    }
+
+    private void initComputerNums() {
+        Arrays.fill(computerNums, -1);
+    }
+
+    private int makeNum() {
+        return new Random().nextInt(MAX_RANGE_NUM);
     }
 
     private String getInputNum() {
--- src/test/java/baseball/service/BaseballServiceTest.java
+++ src/test/java/baseball/service/BaseballServiceTest.java
@@ -5,14 +5,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BaseballServiceTest {
+    BaseballService baseballService = new BaseballService();
+
     @Test
     @DisplayName("숫자문자열을 나눠서 int 배열에 담기")
     void divideInputNum() {
         //given
-        BaseballService baseballService = new BaseballService();
         String strNum = "157";
 
         //when
@@ -24,5 +28,24 @@ public class BaseballServiceTest {
                 () -> assertEquals(inputNums[1], 5),
                 () -> assertEquals(inputNums[2], 7)
         );
+    }
+
+    @Test
+    @DisplayName("중복 없는 3개 숫자 생성")
+    void setComputerNums() {
+        //given
+        Set<Integer> set;
+
+        //when
+        //then
+        for (int i = 0; i < 100; i++) {
+            set = new HashSet<>();
+            int[] inputNums = baseballService.setComputerNums();
+
+            for (int num : inputNums) {
+                set.add(num);
+            }
+            assertEquals(set.size(), 3);
+        }
     }
 }
