164bbd1401c48ecc73d0e5e09abc55c169409ae6
parkeeseul <eeseul.p@gmail.com>
2020-11-17 03:00:20 +0900

    fix: Ball 상수 public 으로 수정

--- src/main/java/model/domain/Ball.java
+++ src/main/java/model/domain/Ball.java
@@ -8,8 +8,8 @@ import java.util.Objects;
 
 @Getter
 public class Ball {
-    private static final int MAX_NUMBER = 9;
-    private static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int MIN_NUMBER = 1;
 
     private int number;
 
--- src/main/java/model/domain/BallFactory.java
+++ src/main/java/model/domain/BallFactory.java
@@ -7,14 +7,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class BallFactory {
-    private static final int MAX_NUMBER = 9;
-    private static final int MIN_NUMBER = 1;
-
     @Getter
     private static final List<Ball> BALLS = new ArrayList<>();
 
     static {
-        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+        for (int i = Ball.MIN_NUMBER; i <= Ball.MAX_NUMBER; i++) {
             BALLS.add(Ball.of(i));
         }
     }
