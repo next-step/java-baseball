f669e614bc4074fbc5fe79207e99e4f5fb925cb0
limdingdong <goldbat9554@naver.com>
2020-11-16 11:53:54 +0900

    feat: Opponent 스트라이크 계산로직, 메시지출력 구현
    스트라이크 계산로직 구현
    스트라이크, 볼 수에 따른 메시지출력 구현
    Opponent 패배 판별로직 구현
    
    Authored by Dongsub.Lim

--- src/main/java/baseball/bo/Opponent.java
+++ src/main/java/baseball/bo/Opponent.java
@@ -7,10 +7,8 @@ import java.util.Set;
 public class Opponent {
 
     private String strikeNumber;
-
-    public String getStrikeNumber() {
-        return strikeNumber;
-    }
+    private int strike;
+    private int ball;
 
     public Opponent() {
         this.resetStrikeNumber();
@@ -42,5 +40,54 @@ public class Opponent {
                 return strikeNumber.toString();
             }
         }
+    }
+
+    public void calculateStrike(String inputNumber) {
+
+        strike = 0;
+        ball = 0;
+
+        String[] inputNumbers = inputNumber.split("");
+        String[] strikeNumbers = strikeNumber.split("");
+
+        for (int i = 0; i < inputNumbers.length; i++) {
+            if(inputNumbers[i].equals(strikeNumbers[i])) {
+                strike++;
+                continue;
+            }
+            if(strikeNumber.contains(inputNumbers[i])) {
+                ball++;
+            }
+        }
+    }
+
+    public String getMessage() {
+        if(strike==3) {
+            return "3개의 숫자를 모두 맞히셨습니다! 게임 종료.";
+        }
+        if(strike==0 && ball==0) {
+            return "낫싱";
+        }
+        String ballMessage = "";
+        if(ball>0) {
+            ballMessage = " " + ball + " 볼";
+        }
+        return strike + " 스트라이크" + ballMessage;
+    }
+
+    public boolean isLose() {
+        return strike == 3;
+    }
+
+    public String getStrikeNumber() {
+        return strikeNumber;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
     }
 }
--- src/test/java/baseball/bo/OpponentTest.java
+++ src/test/java/baseball/bo/OpponentTest.java
@@ -3,6 +3,7 @@ package baseball.bo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -60,5 +61,33 @@ class OpponentTest {
 
         // then
         assertThat(stringSet.contains("0")).isFalse();
+    }
+
+    @Test
+    @DisplayName("스트라이크 계산로직 테스트")
+    void calculateStrike() throws IllegalAccessException, NoSuchFieldException {
+        // given
+        Opponent opponent = new Opponent();
+
+        // when
+        Field field = opponent.getClass().getDeclaredField("strikeNumber");
+        field.setAccessible(true);
+        field.set(opponent, "425");
+
+        // then
+        opponent.calculateStrike("123");
+        assertThat(opponent.getStrike()).isEqualTo(1);
+        assertThat(opponent.getBall()).isEqualTo(0);
+        assertThat(opponent.getMessage()).isEqualTo("1 스트라이크");
+
+        opponent.calculateStrike("456");
+        assertThat(opponent.getStrike()).isEqualTo(1);
+        assertThat(opponent.getBall()).isEqualTo(1);
+        assertThat(opponent.getMessage()).isEqualTo("1 스트라이크 1 볼");
+
+        opponent.calculateStrike("789");
+        assertThat(opponent.getStrike()).isEqualTo(0);
+        assertThat(opponent.getBall()).isEqualTo(0);
+        assertThat(opponent.getMessage()).isEqualTo("낫싱");
     }
 }
