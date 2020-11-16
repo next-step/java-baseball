25fd56c12a6dbece521a75c469ba6159db08cf99
zettalyst <zettalyst@gmail.com>
2020-11-17 01:41:30 +0900

    feat(test): add evaluateRule test case
    Implemented test case here
    - evaluateRule should return "낫싱"
    - evaluateRule should return "1 볼"
    - evaluateRule should return "2 스트라이크"
    - evaluateRule should return "3 스트라이크"
    - evaluateRule should return "1 스트라이크 1 볼"

--- src/test/kotlin/baseballTest.kt
+++ src/test/kotlin/baseballTest.kt
@@ -24,4 +24,69 @@ class BaseballTest {
         // then
         assertEquals(3, generatedValue.length)
     }
+
+    @Test
+    fun shouldRuleEvalulatedNothing() {
+        // given
+        val userInput = "123"
+        val generatedValue = "456"
+
+        // when
+        val evalulatedResult = evaluateRule(userInput, generatedValue)
+
+        // then
+        assertEquals(EvalulatedRule.NOTHING.value, evalulatedResult)
+    }
+
+    @Test
+    fun shouldRuleEvalulated1ball() {
+        // given
+        val userInput = "123"
+        val generatedValue = "356"
+
+        // when
+        val evalulatedResult = evaluateRule(userInput, generatedValue)
+
+        // then
+        assertEquals("1 ${EvalulatedRule.BALL.value}", evalulatedResult)
+    }
+
+    @Test
+    fun shouldRuleEvalulated2strike() {
+        // given
+        val userInput = "123"
+        val generatedValue = "126"
+
+        // when
+        val evalulatedResult = evaluateRule(userInput, generatedValue)
+
+        // then
+        assertEquals("2 ${EvalulatedRule.STRIKE.value}", evalulatedResult)
+    }
+
+    @Test
+    fun shouldRuleEvalulated3strike() {
+        // given
+        val userInput = "123"
+        val generatedValue = "123"
+
+        // when
+        val evalulatedResult = evaluateRule(userInput, generatedValue)
+
+        // then
+        assertEquals("3 ${EvalulatedRule.STRIKE.value}", evalulatedResult)
+    }
+
+    @Test
+    fun shouldRuleEvalulated1strike1ball() {
+        // given
+        val userInput = "123"
+        val generatedValue = "138"
+
+        // when
+        val evalulatedResult = evaluateRule(userInput, generatedValue)
+
+        // then
+        assertEquals("1 ${EvalulatedRule.STRIKE.value} 1 ${EvalulatedRule.BALL.value}", evalulatedResult)
+    }
 }
