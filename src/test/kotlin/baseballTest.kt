f35fb214caa4c70fc0c91ad99913d93cbd7cf169
zettalyst <zettalyst@gmail.com>
2020-11-17 01:10:12 +0900

    feat(test): add generateRand3digit test case
    Implemented test case here
    - generateRand3digit should return "Int" string
    - generateRand3digit should return length 3 string

--- src/test/kotlin/baseballTest.kt
+++ src/test/kotlin/baseballTest.kt
@@ -1,9 +1,27 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 
 class BaseballTest {
     @Test
-    fun shouldHello() {
-        assertEquals(true, true)
+    fun shouldGeneratedValueIsNotNull() {
+        // given
+
+        // when
+        val generatedValue = generateRand3digit().toIntOrNull()
+
+        // then
+        assertNotEquals(null, generatedValue)
+    }
+
+    @Test
+    fun shouldGeneratedValueIs3digit() {
+        // given
+
+        // when
+        val generatedValue = generateRand3digit()
+
+        // then
+        assertEquals(3, generatedValue.length)
     }
 }
