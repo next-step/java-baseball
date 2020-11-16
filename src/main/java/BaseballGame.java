2ec5bfa124c734369cd38dc9b42ee88c834d8f89
minjikim <carrykim1126@gmail.com>
2020-11-16 12:09:56 +0900

    Add number for user and Refactor random number for computer

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,6 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class BaseballGame {
+    int strike = 0;
+    int ball = 0;
     public String num(){
         Random r = new Random();
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -15,8 +18,33 @@ public class BaseballGame {
         return num;
     }
 
-    public static void main(String[] args) {
+    public void play(){
         String com = num();
-        System.out.print("컴퓨터 수 : " + com);
+        System.out.println("computer number : " + com);
+        String user = "";
+        do{
+            Scanner sc = new Scanner(System.in);
+            user = sc.nextLine();
+            System.out.println("input user number : " + user);
+            for(int i=0; i<com.length(); i++) {
+                for(int j=0; j<user.length(); j++) {
+                    if (com.charAt(i) == user.charAt(j)) {
+                        if(i==j) {
+                            ++strike;
+                        }else {
+                            ++ball;
+                        }
+                    }
+                }
+            }
+            if (strike != 0) System.out.print(strike + " strike ");
+            if (ball != 0) System.out.print(ball + " ball ");
+            if (strike == 0 && ball == 0) System.out.println("Nothing!");
+        }while(!com.equals(user));
+    }
+
+    public static void main(String[] args) {
+        BaseballGame bg = new BaseballGame();
+        bg.play();
     }
 }
