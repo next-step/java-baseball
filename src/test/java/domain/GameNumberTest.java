b6f8fd532dbc2965514da11a3ae02e8b9af0a29c
ohjuntaek <201211734@pukyong.ac.kr>
2020-11-16 22:06:25 +0900

    test: 한 자리 게임 숫자(GameDigit)를 받아서 자신이 가진 digits 에 대해 매치 타입(MatchType) 을 반환한다.

--- src/test/java/domain/GameNumberTest.java
+++ src/test/java/domain/GameNumberTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,4 +35,21 @@ class GameNumberTest {
         assertThatThrownBy(() -> new GameNumber(gameDigits))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("한 자리 게임 숫자(GameDigit)를 받아서 자신이 가진 digits 에 대해 매치 타입(MatchType) 을 반환한다.")
+    void judgeMatchTypeTest() {
+        GameDigit compareGameDigit = new GameDigit(1);
+        GameDigit noMatchGameDigit = new GameDigit(4);
+        List<GameDigit> gameDigits = Arrays.asList(new GameDigit(1), new GameDigit(2), new GameDigit(3));
+        GameNumber gameNumber = new GameNumber(gameDigits);
+
+        assertThat(gameNumber.judgeMatchType(compareGameDigit, 0)).isEqualTo(MatchType.STRIKE);
+        assertThat(gameNumber.judgeMatchType(compareGameDigit, 1)).isEqualTo(MatchType.BALL);
+        assertThat(gameNumber.judgeMatchType(compareGameDigit, 2)).isEqualTo(MatchType.BALL);
+        assertThat(gameNumber.judgeMatchType(noMatchGameDigit, 0)).isEqualTo(MatchType.NO_MATCH);
+        assertThat(gameNumber.judgeMatchType(noMatchGameDigit, 1)).isEqualTo(MatchType.NO_MATCH);
+        assertThat(gameNumber.judgeMatchType(noMatchGameDigit, 2)).isEqualTo(MatchType.NO_MATCH);
+    }
+
 }
