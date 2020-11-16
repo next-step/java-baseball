aa6753d4d3305ef7d0d1c2249ca90252f5f67453
limdingdong <goldbat9554@naver.com>
2020-11-16 17:02:56 +0900

    refactor: isValid 메소드 내부로직 분리
    validation 조건별로 메소드 분리
    
    Authored by Dongsub.Lim

--- src/main/java/baseball/bo/Player.java
+++ src/main/java/baseball/bo/Player.java
@@ -30,24 +30,36 @@ public class Player {
      * @return 데이터 정합성 만족여부
      */
     private boolean isValid(String strikeNumber) {
+        return isLengthCorrect(strikeNumber) && isFormatCorrect(strikeNumber) && !isContainsZero(strikeNumber) && !isContainsDuplicateNumber(strikeNumber);
+    }
 
+    private boolean isLengthCorrect(String strikeNumber) {
         if(strikeNumber==null || strikeNumber.length()!=3) {
             System.out.println("3자리의 숫자를 입력해 주세요.");
             return false;
         }
+        return true;
+    }
 
+    private boolean isFormatCorrect(String strikeNumber) {
         try {
             Integer.parseInt(strikeNumber);
         } catch (NumberFormatException e) {
             System.out.println("숫자가 아닌 값을 입력하였습니다. 다시 입력해 주세요");
             return false;
         }
+        return true;
+    }
 
+    private boolean isContainsZero(String strikeNumber) {
         if(strikeNumber.contains("0")) {
             System.out.println("각 자리의 숫자는 1~9 까지의 값을 입력할 수 있습니다.");
-            return false;
+            return true;
         }
+        return false;
+    }
 
+    private boolean isContainsDuplicateNumber(String strikeNumber) {
         Set<String> stringSet = new HashSet<>();
         for (char c : strikeNumber.toCharArray()) {
             stringSet.add(String.valueOf(c));
@@ -56,7 +68,6 @@ public class Player {
             System.out.println("중복된 숫자를 입력하였습니다. 다시 입력해 주세요");
             return false;
         }
-
         return true;
     }
 }
