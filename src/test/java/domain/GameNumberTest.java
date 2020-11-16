8c28b9d6fbeb1bdab8adcc71ed124ca9c2ee6dae
ohjuntaek <201211734@pukyong.ac.kr>
2020-11-16 21:54:11 +0900

    test: 생성 시 중복된 한 자리 게임 숫자(GameDigit) 가 있으면 예외를 발생시킨다.

--- src/test/java/domain/GameNumberTest.java
+++ src/test/java/domain/GameNumberTest.java
@@ -14,12 +14,23 @@ class GameNumberTest {
 
     @Test
     @DisplayName("생성 시 3개의 한 자리 게임 숫자(GameDigit) 를 가지지 않으면 예외를 발생시킨다.")
-    void createValidationTest() {
+    void createValidationLengthTest() {
         List<GameDigit> gameDigits = new ArrayList<>();
         gameDigits.add(new GameDigit(0));
         gameDigits.add(new GameDigit(1));
         gameDigits.add(new GameDigit(2));
         gameDigits.add(new GameDigit(3));
+        assertThatThrownBy(() -> new GameNumber(gameDigits))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("생성 시 중복된 한 자리 게임 숫자(GameDigit) 가 있으면 예외를 발생시킨다.")
+    void createValidationDuplicationTest() {
+        List<GameDigit> gameDigits = new ArrayList<>();
+        gameDigits.add(new GameDigit(0));
+        gameDigits.add(new GameDigit(1));
+        gameDigits.add(new GameDigit(0));
         assertThatThrownBy(() -> new GameNumber(gameDigits))
                 .isInstanceOf(IllegalArgumentException.class);
     }
