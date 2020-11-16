848bf757b6b3379efa4ee11a3eb240ebb7637ba2
kim hyunlae <hyunlae@welgram.com>
2020-11-16 18:07:54 +0900

    Question: 문제제시객체. 문자열(랜덤한 3개 숫자) 생성 기능

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 문제제시
+ */
 public class Question {
+
+    private List<Integer> numberList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+    /**
+     * 3개숫자로 이루어진 문제열 생성
+     * @return
+     */
+    public String generate() {
+
+        String result = "";
+        int numberLength = 3;
+        for (int i = 0; i < numberLength; i++) {
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(numberList.size());
+            int randomElement = numberList.get(randomIndex);
+
+            result += randomElement;
+
+            numberList.remove(randomIndex);
+        }
+
+        return result;
+    }
 }
--- src/test/java/QuestionTest.java
+++ src/test/java/QuestionTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuestionTest {
 
+    /**
+     * 랜덤한 3개숫자로 이루어진 문자열 생성 테스트
+     */
+    @Test
+    public void testGetQuestionNumber() {
+        Question question = new Question();
+        String result = question.generate();
+        System.out.println("questionNumber: " + result);
+
+        assertTrue(result != null);
+    }
 }
