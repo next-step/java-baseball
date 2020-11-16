f36d60e47f5eba892550f27ed32da75567b00645
HanYonggu <hyyg111@naver.com>
2020-11-17 03:54:01 +0900

    feat : 상대방(컴퓨터)는 플레이어와 입력한 수와 비교하여 같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱이라는 힌트를 표시

---
+++ src/main/java/baseball/domain/Hint.java
@@ -0,0 +1,40 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Hint {
+
+    private int ballCount = 0;
+    private int strikeCount = 0;
+
+    public Hint(Number number1, Number number2) {
+        List<Integer> digits1 = number1.getDigits();
+        List<Integer> digits2 = number2.getDigits();
+
+        for (int i = 0; i < Game.BASEBALL_NUMBER_SIZE; i++) {
+            count(digits1, digits2, i);
+        }
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public boolean isAllStrike() {
+        return strikeCount == Game.BASEBALL_NUMBER_SIZE;
+    }
+
+    private void count(List<Integer> digits1, List<Integer> digits2, int index) {
+        Integer digit1 = digits1.get(index);
+        Integer digit2 = digits2.get(index);
+        if (digit1.equals(digit2)) {
+            strikeCount ++;
+        } else if (digits1.contains(digit2)) {
+            ballCount ++;
+        }
+    }
+}
