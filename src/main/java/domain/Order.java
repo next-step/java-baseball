9fccb82ccba2d9f59e241a7fc18c4e30a406ab09
hyeji <khji1210@naver.com>
2020-11-17 02:30:06 +0900

    feat[Order] : Add isEqual method

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -3,14 +3,19 @@ package domain;
 public class Order {
     private static final int START_POSITION = 0;
     private static final int LAST_POSITION = 2;
+
     private final int value;
 
-    public Order(int value) {
+    public Order(final int value) {
         validate(value);
         this.value = value;
     }
 
-    private void validate(int value) {
+    public boolean isEqual(final Order order) {
+        return value == order.getValue();
+    }
+
+    private void validate(final int value) {
         if (START_POSITION > value || LAST_POSITION < value) {
             throw new IllegalArgumentException("자릿수 범위를 초과하였습니다.");
         }
--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -1,12 +1,21 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OrderTest {
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        order = new Order(1);
+    }
 
     @DisplayName("자릿수 index 값이 0~2 범위를 넘어갈 경우, Exception 발생.")
     @ParameterizedTest
@@ -17,4 +26,19 @@ class OrderTest {
                 .hasMessageContaining("자릿수 범위를 초과하였습니다.");
     }
 
+    @DisplayName("입력받은 다른 자릿수 index 값이 일치할 경우, true 리턴.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true"}, delimiter = ':')
+    void isEqual_TRUE(int index, boolean expected) {
+        boolean result = order.isEqual(new Order(index));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("입력받은 다른 자릿수 index 값이 다를 경우, false 리턴.")
+    @ParameterizedTest
+    @CsvSource(value = {"0:false", "2:false"}, delimiter = ':')
+    void isEqual_FALSE(int index, boolean expected) {
+        boolean result = order.isEqual(new Order(index));
+        assertThat(result).isEqualTo(expected);
+    }
 }
