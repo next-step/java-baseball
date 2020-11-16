7e2f31f94e70f03f46ad04a8e6352d425673f856
Imchaehoon <youspend8@gmail.com>
2020-11-16 21:02:20 +0900

    test : 사용자가 입력한 숫자의 해당하는 볼 카운트 계산 기능 테스트 코드 작성

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -1,11 +1,14 @@
 package baseball;
 
+import baseball.domain.BaseballNumbers;
+import baseball.domain.BaseballResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("로직 테스트")
 public class BaseballGameTest {
@@ -29,5 +32,18 @@ public class BaseballGameTest {
     void validNumberFailTest(int number) {
         assertThat(baseballGame.confirmNumber(number).isEmpty())
                 .isTrue();
+    }
+
+    @Test
+    @DisplayName("볼 카운트 테스트")
+    void ballCount() {
+        assertThat(baseballGame.checkNumbers(new BaseballNumbers(123), new BaseballNumbers(132)))
+                .extracting(BaseballResult::getStrike, BaseballResult::getBall).containsExactly(1, 2);
+        assertThat(baseballGame.checkNumbers(new BaseballNumbers(576), new BaseballNumbers(576)))
+                .extracting(BaseballResult::getStrike, BaseballResult::getBall).containsExactly(3, 0);
+        assertThat(baseballGame.checkNumbers(new BaseballNumbers(194), new BaseballNumbers(419)))
+                .extracting(BaseballResult::getStrike, BaseballResult::getBall).containsExactly(0, 3);
+        assertThat(baseballGame.checkNumbers(new BaseballNumbers(456), new BaseballNumbers(789)))
+                .extracting(BaseballResult::getStrike, BaseballResult::getBall).containsExactly(0, 0);
     }
 }
