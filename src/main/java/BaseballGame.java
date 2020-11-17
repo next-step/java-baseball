6679d97cc5c8c32b726b339bd6c18eee04ab37ef
1004miriam96 <1004miriam96@gmail.com>
2020-11-17 20:17:58 +0900

    feat: 사용자에게 숫자 입력 받을 수 있는 기능

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,16 +1,17 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BaseballGame {
-
 	public static void main(String[] args) {
 		String com[] = new String[3];
-		//String user[] = new String[3];
+		String user[] = new String[3];
 
 		com = pickNumber(com);
+		user = getNumber(user);
 	}
 
 	public static String[] pickNumber(String[] com) {
-		String random = "";
+		String random;
 		for (int i = 0; i < com.length; i++) {
 			do {
 				random = String.valueOf((int)(Math.random() * 9) + 1);
@@ -22,5 +23,16 @@ public class BaseballGame {
 
 	public static boolean checkOverlap(String[] com, String target) {
 		return Arrays.asList(com).contains(target);
+	}
+
+	public static String[] getNumber(String[] user) {
+		String input = null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Press Number : ");
+		input = sc.next();
+		for (int i = 0; i < user.length; i++) {
+			user[i] = String.valueOf(input.charAt(i));
+		}
+		return user;
 	}
 }
