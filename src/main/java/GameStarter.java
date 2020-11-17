8d501237dc5e98e211194a6f21d80c69e9201796
tearight <tearightss@gmail.com>
2020-11-17 20:50:55 +0900

    feat(GameStarter): 사용자로부터 게임을 계속할 것인지 물어보고 계속 한다면 야구 게임을 반복 실행하기
    - askPlayNewGame() 추가
    - main() 에서 do while 구현
    - main() while 조건에서 gameStarter.askPlayerNewGame() 호출

--- src/main/java/GameStarter.java
+++ src/main/java/GameStarter.java
@@ -8,8 +8,22 @@ public class GameStarter {
     private final static String COMMAND_END = "2";
 
     public static void main(String[] args) {
-        BaseballGame baseballGame = new BaseballGame();
-        baseballGame.play();
+        GameStarter gameStarter = new GameStarter();
+        do {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.play();
+        } while (gameStarter.askPlayNewGame());
+    }
+
+    boolean askPlayNewGame() {
+        String guideMessage = String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.", COMMAND_START, COMMAND_END);
+        System.out.println(guideMessage);
+        String command = inputCommand();
+        if (command.equals(COMMAND_START)) {
+            return true;
+        }
+
+        return false;
     }
 
     String inputCommand() {
