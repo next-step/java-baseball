3d6bdacaf02a6beefe759335c789e466ec370123
slamdunk7575 <slamdunk7575@gmail.com>
2020-11-17 13:43:11 +0900

    (test) Player가 입력한 숫자 생성 TEST

---
+++ src/test/java/domain/PlayerBallNumberTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayerBallNumberTest {
+
+    @ParameterizedTest
+    @DisplayName("플레이어가 입력한 숫자의 야구볼 생성 테스트")
+    @CsvSource(value = {"751=5", "892=9", "321=2"}, delimiter = '=')
+    void player_ball_number_test(int input, int expected) {
+
+        // given
+        PlayerBallNumber playerBallNumber = new PlayerBallNumber(new PlayerNumberGenerator(input));
+
+        // when
+        BaseBallNumbers playerBallNumbers = playerBallNumber.getBallNumbers();
+
+        // then
+        assertThat(playerBallNumbers.getBaseBallNumber(1)).isEqualTo(new BaseBallNumber(expected));
+    }
+
+}
