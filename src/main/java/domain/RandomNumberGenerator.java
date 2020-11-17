968d423096142b6bb35859a0951c02f0909210f9
slamdunk7575 <slamdunk7575@gmail.com>
2020-11-17 12:39:44 +0900

    (refactor) Random 숫자 생성기 인터페이스 구현하도록 수정

--- src/main/java/domain/RandomNumberGenerator.java
+++ src/main/java/domain/RandomNumberGenerator.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements BaseBallNumberGenerator{
 
     public static final int BALL_MIN_SIZE = 0;
     private static final int BALL_MAX_SIZE = 3;
 
+    @Override
     public List<BaseBallNumber> getNumbers() {
         List<BaseBallNumber> sourceBallNumbers = SourceNumbers.getSourceBallNumbers();
         Collections.shuffle(sourceBallNumbers);
