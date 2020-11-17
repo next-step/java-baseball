bcaecba128f18ccc02b420b03668bf23483a4552
sang5c <skah321@gmail.com>
2020-11-17 21:18:37 +0900

    feat(Baseball): 3개의 숫자 문자열로 야구 객체를 생성한다

--- src/main/java/com/sang5c/baseball/Baseball.java
+++ src/main/java/com/sang5c/baseball/Baseball.java
@@ -1,4 +1,18 @@
 package com.sang5c.baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Baseball {
+
+    private final List<String> numbers;
+
+    public Baseball(String s) {
+        String[] split = s.split("");
+        List<String> numbers = new ArrayList<>(Arrays.asList(split));
+
+        this.numbers = numbers;
+    }
+
 }
