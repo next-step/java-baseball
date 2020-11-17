4445e66dedb537861c36bda076db55164b668d5b
lisam <lisam333@gmail.com>
2020-11-17 17:28:23 +0900

    feat(BaseballGameApplication): 플레이어에게 정답을 맞힐 입력을 제공한다.

---
+++ src/main/java/BaseballGameApplication.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BaseballGameApplication {
+	public static Scanner scanner = new Scanner(System.in);
+	private final BaseballGameService baseballGameService = new BaseballGameService();
+
+	public static void main(String[] args) {
+		BaseballGameApplication game = new BaseballGameApplication();
+		game.start();
+	}
+
+	private void start() {
+		int[] answer = baseballGameService.peek3RandomDistinctNumbers();
+		boolean hit = false;
+		while (!hit) {
+			String guess = guess();
+			hit = false;
+		}
+	}
+
+	private String guess() {
+		System.out.print("숫자를 입력해 주세요 : ");
+		return scanner.nextLine();
+	}
+	
+}
