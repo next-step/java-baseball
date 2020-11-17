9684e2519bf5db9c659b36caa94751ef430ab9d1
dktsudgg <dktsudgg@yonsei.ac.kr>
2020-11-17 20:23:06 +0900

    feat(add): 야구게임 완성

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,19 +1,46 @@
 import java.io.IOException;
-import java.util.List;
 
 public class BaseballGame {
 
+    static int answer;
+
     public static void main(String[] args) throws IOException {
         /**
-         * 유저의 입력을 받음
+         * 컴퓨터의 정답을 생성
          */
-        List<Integer> singleDigits = KeyboardInputManager.receiveUserInput();
+        answer = NumberGenerator.makeRandomThreeDigit();
 
-        /**
-         * 입력받은 값 출력
-         */
-        for (int digit : singleDigits) {
-            System.out.println(digit);
+        while (true) {
+            // 유저의 입력을 받음
+            int userInput = KeyboardInputManager.receiveUserInput();
+            // 컴퓨터의 정답과 비교하여 결과를 도출
+            BaseballCompareResult result = NumberComparator.compare(answer, userInput);
+
+            switch (result.getStatus()) {
+                case NOTHING:   // 맞는게 하나도 없는 경우
+                    System.out.println("낫싱");
+                    break;
+                case EXIST:     // 볼 또는 스트라이크가 존재하는 경우
+                    System.out.println(result.getStrikeCount() + "스트라이크, " + result.getBallCount() + "볼");
+                    break;
+                case MATCHED:   // 정답을 맞춘 경우
+                    System.out.println("정답입니다! 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    if(doAgainOrNot() == false) return;
+            }
+        }
+
+    }
+
+    private static boolean doAgainOrNot() throws IOException {
+        String userAction = KeyboardInputManager.receiveKeyboardInput();
+        if (userAction.equals("1")) {
+            System.out.println("게임을 새로 시작합니다.");
+            // 정답 초기화
+            answer = NumberGenerator.makeRandomThreeDigit();
+            return true;
+        } else {
+            System.out.println("종료되었습니다.");
+            return false;
         }
     }
 
--- src/main/java/KeyboardInputManager.java
+++ src/main/java/KeyboardInputManager.java
@@ -9,12 +9,12 @@ public class KeyboardInputManager {
      * 1~9 사이의 서로 다른 수로 이루어진 세자리 수를 입력받아서,
      * 각 자리수를 배열에 담아 리턴하는 메소드
      */
-    public static List<Integer> receiveUserInput() {
+    public static int receiveUserInput() {
         try {
             System.out.println("서로 다른 수로 이루어진 세자리 숫자를 입력해주세요. (1~9 사이의 수)");
             int input = Integer.parseInt(receiveKeyboardInput());
 
-            if (NumberGenerator.checkRightData(input)) return NumberGenerator.toSingleDigitArray(input);
+            if (NumberGenerator.checkRightData(input)) return input;
             else return receiveUserInput();
         } catch (Exception e) {
             return receiveUserInput();
@@ -24,7 +24,7 @@ public class KeyboardInputManager {
     /**
      * 키보드로부터 한줄 입력받는 메소드
      */
-    private static String receiveKeyboardInput() throws IOException {
+    public static String receiveKeyboardInput() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         return in.readLine();
     }
