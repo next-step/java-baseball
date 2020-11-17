f22cccd0a1fee6fc808589df75f4dc98c0fc967c
orgojy <giveawesome@gmail.com>
2020-11-17 22:50:55 +0900

    feat(Application.java): 숫자야구게임을 시작하기 위한 `NumberBaseball` 실행

---
+++ src/main/java/edu/example/Application.java
@@ -0,0 +1,10 @@
+package edu.example;
+
+import edu.example.numberbaseball.NumberBaseball;
+
+public class Application {
+    public static void main(String[] args) {
+        NumberBaseball numberBaseball = new NumberBaseball();
+        numberBaseball.run();
+    }
+}
