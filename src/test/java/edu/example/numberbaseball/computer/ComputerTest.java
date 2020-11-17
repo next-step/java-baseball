3ae3ac456de75c0c84aa5b4fe4906bd0ed9c7b9c
orgojy <giveawesome@gmail.com>
2020-11-17 21:41:10 +0900

    test(ComputerTest.java): 랜덤한 `Ball`을 생성하여 `Inning` 초기화하는 기능 테스트

---
+++ src/test/java/edu/example/numberbaseball/computer/ComputerTest.java
@@ -0,0 +1,43 @@
+package edu.example.numberbaseball.computer;
+
+import edu.example.numberbaseball.common.Inning;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("`Player`를 상대할 `Computer`에 대한 테스트")
+class ComputerTest {
+    private static final int VALID_BALL_SIZE_FOR_INNING = 3;
+
+    @DisplayName("`Computer`는 랜던하게 생성된 3개 `Ball`을 가진 `Inning`으로 생성된다.")
+    @Test
+    void createValidComputer() {
+        Computer computer = new Computer();
+        Inning computerInning = computer.getInning();
+        List<Integer> computerBallNumberList = computerInning.getBallNumberList();
+        int computerBallNumberListSize = computerBallNumberList.size();
+
+        assertEquals(VALID_BALL_SIZE_FOR_INNING, computerBallNumberListSize);
+    }
+
+    @DisplayName("`Computer`를 리셋하면 `Inning`의 정보는 새롭게 셋팅된다.")
+    @Test
+    void reset() {
+        Computer computer = new Computer();
+        Inning firstComputerInning = computer.getInning();
+        List<Integer> firstComputerBallNumberList = firstComputerInning.getBallNumberList();
+        int firstComputerBallNumberListSize = firstComputerBallNumberList.size();
+
+        computer.reset();
+
+        Inning secondComputerInning = computer.getInning();
+        List<Integer> secondComputerBallNumberList = secondComputerInning.getBallNumberList();
+        int secondComputerBallNumberListSize = secondComputerBallNumberList.size();
+
+        assertEquals(secondComputerBallNumberListSize, firstComputerBallNumberListSize);
+        assertFalse(firstComputerBallNumberList.containsAll(secondComputerBallNumberList));
+    }
+}
