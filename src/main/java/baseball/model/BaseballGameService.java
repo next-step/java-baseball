0acdf30c9d8e7b17d89afb1bcbd4d3adcf88f00e
Sang Rhee <leesy579@gmail.com>
2020-11-17 20:52:41 +0900

    feat(BaseballGameService): implement baseball game logic

--- src/main/java/baseball/model/BaseballGameService.java
+++ src/main/java/baseball/model/BaseballGameService.java
@@ -12,9 +12,15 @@ public class BaseballGameService {
 	private static int NUMBER_OF_GUESS_NUMBER = 3;
 
 	private List<Integer> computerNumber;
+	private boolean isUserGuessCorrect;
 
 	private BaseballGameService(List<Integer> computerNumber) {
 		this.computerNumber = computerNumber;
+		isUserGuessCorrect = false;
+	}
+
+	public static BaseballGameService ofGiven(List<Integer> computerNumber) {
+		return new BaseballGameService(computerNumber);
 	}
 
 	public static BaseballGameService ofRandom() {
@@ -43,11 +49,51 @@ public class BaseballGameService {
 	}
 
 	public GuessFeedbackResponse guessNumber(GuessNumberRequest request) {
-		return null;
+		List<Integer> userGuessNumber = request.getGuessNumbers();
+		int numStrike = countStrike(userGuessNumber);
+		int numBall = countBall(userGuessNumber);
+		setIsUserGuessCorrect(numStrike);
+		return new GuessFeedbackResponse(numStrike, numBall);
+	}
+
+	private void setIsUserGuessCorrect(int numStrike) {
+		if (numStrike == NUMBER_OF_GUESS_NUMBER) {
+			isUserGuessCorrect = true;
+		}
+	}
+
+	private int countStrike(List<Integer> userGuessNumber) {
+		int numStrike = 0;
+		for (int i = 0; i < NUMBER_OF_GUESS_NUMBER; i++) {
+			numStrike += addToStrikeCount(userGuessNumber.get(i), i);
+		}
+		return numStrike;
+	}
+
+	private int addToStrikeCount(int userGuessNumber, int position) {
+		if (computerNumber.get(position) == userGuessNumber) {
+			return 1;
+		}
+		return 0;
+	}
+
+	private int countBall(List<Integer> userGuessNumber) {
+		int numBall = 0;
+		for (int i = 0; i < NUMBER_OF_GUESS_NUMBER; i++) {
+			numBall += addToBallCount(userGuessNumber.get(i), i);
+		}
+		return numBall;
+	}
+
+	private int addToBallCount(int userGuessNumber, int position) {
+		if (computerNumber.get(position) != userGuessNumber && computerNumber.contains(userGuessNumber)) {
+			return 1;
+		}
+		return 0;
 	}
 
 	public boolean hasCorrectGuess() {
-		return true;
+		return isUserGuessCorrect;
 	}
 
 	public List<Integer> getComputerNumber() {
--- src/test/java/baseball/model/BaseballGameServiceTest.java
+++ src/test/java/baseball/model/BaseballGameServiceTest.java
@@ -1,7 +1,10 @@
 package baseball.model;
 
+import baseball.controller.dto.GuessFeedbackResponse;
+import baseball.controller.dto.GuessNumberRequest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,11 +28,72 @@ class BaseballGameServiceTest {
 	}
 
 	@Test
-	void guessNumber() {
+	void ofGiven() {
+		List<Integer> computerNumber = Arrays.asList(1, 2, 3);
+		BaseballGameService baseballGameService = BaseballGameService.ofGiven(computerNumber);
+
+		List<Integer> result = baseballGameService.getComputerNumber();
+
+		assertEquals(3, result.size());
+		assertEquals(computerNumber.get(0), result.get(0));
+		assertEquals(computerNumber.get(1), result.get(1));
+		assertEquals(computerNumber.get(2), result.get(2));
+	}
+
+	@Test
+	void guessNumberStrike() {
+		List<Integer> computerNumber = Arrays.asList(1, 2, 3);
+		BaseballGameService baseballGameService = BaseballGameService.ofGiven(computerNumber);
+
+		List<Integer> userGuessNumber = Arrays.asList(1, 2, 3);
+		GuessNumberRequest request = new GuessNumberRequest(userGuessNumber);
+
+		GuessFeedbackResponse response = baseballGameService.guessNumber(request);
+		assertEquals(3, response.getNumStrike());
+		assertEquals(0, response.getNumBall());
+	}
+
+	@Test
+	void guessNumberBall() {
+		List<Integer> computerNumber = Arrays.asList(1, 2, 3);
+		BaseballGameService baseballGameService = BaseballGameService.ofGiven(computerNumber);
+
+		List<Integer> userGuessNumber = Arrays.asList(3, 1, 2);
+		GuessNumberRequest request = new GuessNumberRequest(userGuessNumber);
+
+		GuessFeedbackResponse response = baseballGameService.guessNumber(request);
+		assertEquals(3, response.getNumBall());
+		assertEquals(0, response.getNumStrike());
+	}
+
+	@Test
+	void guessNumberNothing() {
+		List<Integer> computerNumber = Arrays.asList(1, 2, 3);
+		BaseballGameService baseballGameService = BaseballGameService.ofGiven(computerNumber);
+
+		List<Integer> userGuessNumber = Arrays.asList(4, 5, 6);
+		GuessNumberRequest request = new GuessNumberRequest(userGuessNumber);
+
+		GuessFeedbackResponse response = baseballGameService.guessNumber(request);
+		assertEquals(0, response.getNumBall());
+		assertEquals(0, response.getNumStrike());
 	}
 
 	@Test
 	void hasCorrectGuess() {
-	}
+		List<Integer> computerNumber = Arrays.asList(1, 2, 3);
+		BaseballGameService baseballGameService = BaseballGameService.ofGiven(computerNumber);
 
+		List<Integer> wrongGuessNumber = Arrays.asList(4, 5, 6);
+		GuessNumberRequest requestWrong = new GuessNumberRequest(wrongGuessNumber);
+
+		List<Integer> correctGuessNumber = Arrays.asList(1, 2, 3);
+		GuessNumberRequest requestCorrect = new GuessNumberRequest(correctGuessNumber);
+
+		baseballGameService.guessNumber(requestWrong);
+		assertFalse(baseballGameService.hasCorrectGuess());
+
+		baseballGameService.guessNumber(requestCorrect);
+		assertTrue(baseballGameService.hasCorrectGuess());
+	}
 }
