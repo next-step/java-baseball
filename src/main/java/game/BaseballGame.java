d34daf91e4dd506e0bc5d69e73a2778f028b52a0
Htree <bluehtree@gmail.com>
2020-11-17 12:52:43 +0900

    feat: 숫자입력 및 유효성 검증 추가

--- src/main/java/game/BaseballGame.java
+++ src/main/java/game/BaseballGame.java
@@ -1,6 +1,7 @@
 package game;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class BaseballGame {
 	
@@ -17,22 +18,28 @@ public class BaseballGame {
 	 *
 	 */
 	public static void main(String[] args) {
-		//playGame();
+		BaseballGame game = new BaseballGame();
+		
+		game.playGame();
+		
 	}
 	
-	private static void playGame() {
+	public void playGame() {
 		//정답 셋팅
 		int[] answer = makeAnswerNumber();
 		
 		//정답 맞출때까지 반복
 		//1. 사용자에게 입력받기
+		int[] user = userNumber();
+		
 		//2. 판단
+		
 		//3. 판단 결과 출력
 	}
 	
 	
 	// 3개의 랜덤숫자 정답 설정 (1~9)
-	public static int[] makeAnswerNumber() {
+	public int[] makeAnswerNumber() {
 		int[] answer = new int[3];
 		Random random = new Random();
 		
@@ -47,6 +54,80 @@ public class BaseballGame {
 		return answer;
 	}
 	
+	//사용자의 숫자 입력을 받는다
+	public int[] userNumber() {
+		Scanner sc = new Scanner(System.in);
+		int[] userNumber;
+		
+		do {
+			try {
+				System.out.print("숫자를 입력해주세요 : ");
+				userNumber = verifyInputNumber(sc.nextLine());
+				return userNumber;
+			} catch (RuntimeException e) {
+				System.out.println(e.getMessage());
+			}
+		} while(true);
+	}
 	
-
+	
+	//사용자 입력숫자 값 유효성 검증
+	public int[] verifyInputNumber(String data){
+		if (checkNumberType(data) == false){
+			throw new RuntimeException("입력된 값이 숫자가 아닙니다.");
+		}
+		
+		if (containZeroValue(data) == true) {
+			throw new RuntimeException("입력된 값에 0이 포함되어 있습니다. 잘못된 입력입니다.");
+		}
+		
+		if (checkInputLength(data) == false){
+			throw new RuntimeException("3자리 숫자를 입력해주세요.");
+		}
+		
+		int[] number = stringToIntArray(data);
+		if (isUniqueValue(number) == false) {
+			throw new RuntimeException("입력된 값에 중복된 숫자가 있습니다. 잘못된 입력입니다.");
+		}
+		
+		return number;
+	}
+	
+	
+	private boolean checkNumberType(String data) {
+		try {
+			Integer.parseInt(data);
+			return true;
+		} catch (NumberFormatException e){
+			return false;
+		}
+	
+	}
+	
+	private boolean checkInputLength(String data) {
+		if (data.isEmpty()) {
+			return false;
+		}
+		if (data.length() == 3) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean containZeroValue(String data) {
+		return data.contains("0");
+	}
+	
+	private boolean isUniqueValue(int[] num) {
+		return (num[0] != num[1]) && (num[0] != num[2]) && (num[1] != num[2]);
+	}
+	
+	public int[] stringToIntArray(String data){
+		int[] userNumber = new int[3];
+		for(int i = 0 ; i < data.length() ; i++){
+			userNumber[i] = Character.digit(data.charAt(i),10);
+		}
+		return userNumber;
+	}
+	
 }
--- src/test/java/game/BaseBallGameTest.java
+++ src/test/java/game/BaseBallGameTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -17,6 +20,49 @@ class BaseBallGameTest {
 	void makeAnswerNumber() {
 		int[] answer = game.makeAnswerNumber();
 		System.out.println("### 정답 : " + answer[0] + answer[1] + answer[2]);
+	}
+	
+	@DisplayName("숫자 - 숫자 아닌 값 확인")
+	@ParameterizedTest
+	@ValueSource(strings = {"afcf", "df123", " ", "-_-", "aber1a"})
+	void checkNumberType(String data) {
+		Throwable thrown = assertThrows(RuntimeException.class, () -> {
+			game.verifyInputNumber(data).toString();
+		});
+		assertTrue(thrown.getMessage().contains("숫자가 아닙니다"));
+	}
+	
+	@DisplayName("숫자 - 0 포함여부 확인")
+	@ParameterizedTest
+	@ValueSource(strings = {"011", "780", "600", "0", "007"})
+	void containZeroValue(String data) {
+		
+		Throwable thrown = assertThrows(RuntimeException.class, () -> {
+			game.verifyInputNumber(data).toString();
+		});
+		assertTrue(thrown.getMessage().contains("0이 포함"));
+	}
+	
+	@DisplayName("숫자 - 자리수 확인")
+	@ParameterizedTest
+	@ValueSource(strings = {"51", "8", "2316", "79114", "72"})
+	void checkInputLength(String data) {
+		
+		Throwable thrown = assertThrows(RuntimeException.class, () -> {
+			game.verifyInputNumber(data).toString();
+		});
+		assertTrue(thrown.getMessage().contains("3자리 숫자"));
+	}
+	
+	@DisplayName("숫자 - 중복숫자 확인")
+	@ParameterizedTest
+	@ValueSource(strings = {"155", "828", "336", "855", "414"})
+	void isUniqueValue(String data) {
+		
+		Throwable thrown = assertThrows(RuntimeException.class, () -> {
+			game.verifyInputNumber(data).toString();
+		});
+		assertTrue(thrown.getMessage().contains("중복된 숫자"));
 	}
 	
 }
