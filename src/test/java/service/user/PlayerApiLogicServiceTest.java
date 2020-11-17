8a67085dfd0c05be32059962fdab346e360f564a
chh <chh2644@gmail.com>
2020-11-17 10:15:39 +0900

    feat : 테스트 코드 추가
    - 숫자 입력 예외 처리 테스트 코드 추가
    - 메소드명 변경

--- src/test/java/service/user/PlayerApiLogicServiceTest.java
+++ src/test/java/service/user/PlayerApiLogicServiceTest.java
@@ -1,42 +1,41 @@
 package service.user;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.*;
-
 public class PlayerApiLogicServiceTest {
 
-    @DisplayName("4자리 이상 숫자 입력 확인")
+    @DisplayName("3자리 숫자 입력 확인")
     @ParameterizedTest
-    @ValueSource(strings = {"1", "1", "1111"})
-    void inputNumberLengthCheck(String input) {
-        if(input.length() > 3)
-            throw new IllegalArgumentException("3개의 숫자만 입력할 수 있습니다.");
+    @ValueSource(strings = {"1", "123", "1111"})
+    public void inputNumberLengthException(String input) {
+        if(input.length() != 3)
+            throw new IllegalArgumentException("3개의 숫자를 입력해야 합니다.");
     }
 
-    @Test
     @DisplayName("숫자 중복 입력 확인")
-    public void duplicateCheckNumber() {
-        String input = "233";
+    @ParameterizedTest
+    @ValueSource(strings = {"122", "123"})
+    public void duplicateNumberException(String input) {
+        ComputerApiLogicService computerApi = new ComputerApiLogicService();
+        List<Integer> numberList = computerApi.stringToList(input);
 
-        boolean flag = true;
-        char[] inputArr = new char[3];
-        for(int i=0; i<input.length(); i++) {
-            inputArr[i] = input.charAt(i);
+        if(numberList.get(0).intValue() == numberList.get(1).intValue() || numberList.get(0).intValue() == numberList.get(2).intValue()
+                || numberList.get(1).intValue() == numberList.get(2).intValue())
+            throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+    }
+
+    @DisplayName("숫자 아닌 입력 확인")
+    @ParameterizedTest
+    @ValueSource(strings = {"1ㄱ2", "123"})
+    public void numberFormatException(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("숫자만 입력이 가능합니다.");
         }
-
-        if(inputArr[0] == inputArr[1] || inputArr[0] == inputArr[2] || inputArr[1] == inputArr[2]) {
-            flag = false;
-        }
-
-        assertThat(flag).isFalse();
     }
 }
