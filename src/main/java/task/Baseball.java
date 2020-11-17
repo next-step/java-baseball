7deb01fb89fa9cc7260210fd28eb8444ddfaf513
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-11-17 19:17:07 +0900

    feat(compile) - 결과값에 따른 출력 메세지 메서드 작성

--- src/main/java/task/Baseball.java
+++ src/main/java/task/Baseball.java
@@ -50,4 +50,17 @@ public class Baseball {
         }
     }
 
+    void resultOutput() {
+        if(strike == 3)
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        if(strike == 0 && ball > 0)
+            System.out.println(ball+" 볼");
+        if(strike > 0 && ball == 0)
+            System.out.println(strike+" 스트라이크");
+        if(strike > 0 && ball > 0)
+            System.out.println(strike+" 스트라이크 " + ball+" 볼");
+        if(strike == 0 && ball == 0)
+            System.out.println("낫싱");
+    }
+
 }
