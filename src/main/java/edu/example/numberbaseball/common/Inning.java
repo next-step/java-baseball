905b4d8fd7c0e2d67a5e17d3bca7b8bc3cbf3d95
orgojy <giveawesome@gmail.com>
2020-11-17 17:20:33 +0900

    feat(Inning.java): 야구게임 1회를 구성하는 정보를 가진 `Inning` 생성

---
+++ src/main/java/edu/example/numberbaseball/common/Inning.java
@@ -0,0 +1,47 @@
+package edu.example.numberbaseball.common;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 야구게임 1회를 구성하는 정보를 가진 `Inning`
+ */
+public class Inning {
+    private static final int NUMBER_OF_BALLS = 3;
+
+    private final List<Ball> ballList;
+
+    public Inning(List<Ball> ballsList) {
+        if (ballsList.size() != NUMBER_OF_BALLS) {
+            throw new IllegalArgumentException("입력된 Ball의 갯수가 " + NUMBER_OF_BALLS + "개가 되지 않습니다.");
+        }
+        if (isDuplicated(ballsList)) {
+            throw new IllegalArgumentException("입력된 Ball 숫자 중에서 중복되는 수가 존재합니다.");
+        }
+        this.ballList = ballsList;
+    }
+
+    public List<Integer> getBallNumberList() {
+        return getBallNumberList(ballList);
+    }
+
+    private List<Integer> getBallNumberList(List<Ball> ballsList) {
+        List<Integer> numberList = new ArrayList<>(NUMBER_OF_BALLS);
+        for (Ball ball : ballsList) {
+            numberList.add(ball.getNumber());
+        }
+        return numberList;
+    }
+
+    private boolean isDuplicated(List<Ball> ballsList) {
+        Set<Integer> ballNumberSet = new HashSet<>(NUMBER_OF_BALLS);
+        ballNumberSet.addAll(getBallNumberList(ballsList));
+        return ballNumberSet.size() != NUMBER_OF_BALLS;
+    }
+
+    public int getNumberOfBalls() {
+        return NUMBER_OF_BALLS;
+    }
+}
