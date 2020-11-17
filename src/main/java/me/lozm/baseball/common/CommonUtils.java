f841b09e0c4d6113b1fa6eda435507aa1b06adf9
junlee <laonzenamoon@gmail.com>
2020-11-17 17:28:08 +0900

    feat($compile): 공통적으로 사용하기 위한 유틸성 CommonUtils 클래스를 생성

---
+++ src/main/java/me/lozm/baseball/common/CommonUtils.java
@@ -0,0 +1,13 @@
+package me.lozm.baseball.common;
+
+public class CommonUtils {
+
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 9;
+    public static final int ANSWER_SIZE = 3;
+    public static final boolean CANDIDATE_IS_USED = true;
+    public static final boolean CANDIDATE_IS_NOT_USED = false;
+
+    public static final String REG_EXP_ANSWER = "([1-9]{"+ANSWER_SIZE+"})";
+
+}
