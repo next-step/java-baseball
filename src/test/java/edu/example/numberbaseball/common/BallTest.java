4b6b322a2db4d966be84a7bb714f7fa5e3644ac4
orgojy <giveawesome@gmail.com>
2020-11-17 21:51:03 +0900

    test(BallTest.java): `Ball`을 생성하기 위한 초깃값의 자료형을 char로 지정하여 초기화

--- src/test/java/edu/example/numberbaseball/common/BallTest.java
+++ src/test/java/edu/example/numberbaseball/common/BallTest.java
@@ -13,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("야구게임에서 사용할 `Ball`에 대한 테스트")
 class BallTest {
+    private static final int RADIX = 10;
     private static final int START_NUMBER_FOR_BALL = 1;
     private static final int END_NUMBER_FOR_BALL = 9;
     private static final int TEST_BALL_SIZE = END_NUMBER_FOR_BALL - START_NUMBER_FOR_BALL + 1;
@@ -25,6 +26,24 @@ class BallTest {
         List<Ball> ballList = new ArrayList<>(TEST_BALL_SIZE);
         for (int i = START_NUMBER_FOR_BALL; i <= END_NUMBER_FOR_BALL; i++) {
             ballList.add(new Ball(i));
+        }
+        int actualBallSize = ballList.size();
+        Object[] expectedBallArr = expectedBallList.toArray();
+        Object[] actualBallArr = getBallNumberList(ballList).toArray();
+
+        assertEquals(TEST_BALL_SIZE, actualBallSize);
+        assertArrayEquals(expectedBallArr, actualBallArr);
+    }
+
+    @DisplayName("`Ball`의 숫자는 1~9까지 범위에서 결정된다. `Ball`의 초기화 자료형은 char로 한다.")
+    @Test
+    void generateValidBallWithCharacter() {
+        List<Integer> expectedBallList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        List<Ball> ballList = new ArrayList<>(TEST_BALL_SIZE);
+        for (int i = START_NUMBER_FOR_BALL; i <= END_NUMBER_FOR_BALL; i++) {
+            char iChar = Character.forDigit(i, RADIX);
+            ballList.add(new Ball(iChar));
         }
         int actualBallSize = ballList.size();
         Object[] expectedBallArr = expectedBallList.toArray();
