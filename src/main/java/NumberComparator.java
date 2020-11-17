09198a27ebdf8ca412034402a2c1759281290c32
dktsudgg <dktsudgg@yonsei.ac.kr>
2020-11-17 19:19:04 +0900

    feat(add): 볼, 스트라이크 갯수 계산하는 메소드 구현

--- src/main/java/NumberComparator.java
+++ src/main/java/NumberComparator.java
@@ -1,5 +1,36 @@
+import java.util.List;
+
 public class NumberComparator {
-    public static BaseballCompareResult compare(int testInput1, int testInput2) {
+    public static BaseballCompareResult compare(int inputFirst, int inputSecond) {
+        if (NumberGenerator.checkRightData(inputFirst) == false) throw new IllegalArgumentException("올바른 데이터가 아닙니다.");
+        if (NumberGenerator.checkRightData(inputSecond) == false) throw new IllegalArgumentException("올바른 데이터가 아닙니다.");
+
+        // 볼 카운트 계산
+
+        // 스트라이크 카운트 계산
+
         return new BaseballCompareResult();
+    }
+
+    public static int calculateBallCount(int inputFirst, int inputSecond) {
+        List<Integer> singleDigitsFirst = NumberGenerator.toSingleDigitArray(inputFirst);
+        List<Integer> singleDigitsSecond = NumberGenerator.toSingleDigitArray(inputSecond);
+        int ballCount = 0;
+        for (int i = 0; i < singleDigitsFirst.size(); i++)
+            for (int k = 0; k < singleDigitsSecond.size(); k++)
+                if(i != k && singleDigitsFirst.get(i).equals(singleDigitsSecond.get(k)))
+                    ballCount++;
+        return ballCount;
+    }
+
+    public static int calculateStrikeCount(int inputFirst, int inputSecond) {
+        List<Integer> singleDigitsFirst = NumberGenerator.toSingleDigitArray(inputFirst);
+        List<Integer> singleDigitsSecond = NumberGenerator.toSingleDigitArray(inputSecond);
+        int strikeCount = 0;
+        for (int i = 0; i < singleDigitsFirst.size(); i++)
+            for (int k = 0; k < singleDigitsSecond.size(); k++)
+                if(i == k && singleDigitsFirst.get(i).equals(singleDigitsSecond.get(k)))
+                    strikeCount++;
+        return strikeCount;
     }
 }
