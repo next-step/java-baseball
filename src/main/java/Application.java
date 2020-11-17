31e0ecf9ec13620ae5d213cae94322266466d267
shkwon <shkwon@rsupport.com>
2020-11-17 13:27:02 +0900

    feat: 게임 기본 틀 생성

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+import baseball.Game;
+
+public class Application {
+    public static void main(String[] args) {
+        new Game().run();
+    }
+}
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Game {
+    private Scanner scanner;
+
+    public Game() {
+        init();
+    }
+
+    private void init() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void run() { }
+}
