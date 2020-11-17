8a97cde72426be25240855b0767c34fd1ef48558
charllossDev <charlloss.dev@gmail.com>
2020-11-17 20:57:11 +0900

    refactor: (BaseballGameService) add calculate strike & ball
    - 야구게임 임의의 수와, 유저가 입력한 수 비교
    - strike 와 ball 갯수 체크

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -1,12 +1,61 @@
 package baseball.service;
 
-public class BaseballGameService {
+import java.util.ArrayList;
+import java.util.List;
 
-	public static final Integer GAME_RESTART = 1;
-	public static final Integer GAME_END = 2;
+import baseball.dao.BaseballConstant;
+
+public class BaseballGameService extends BaseballConstant {
+
+	public static List<Integer> baseGoalNumber;
+
+	public static boolean isThreeStrikeGame(List<Integer> baseballGoalNumber, List<Integer> userInputBaseballNumber) {
+
+		BaseballConstant.initBaseball();
+		baseGoalNumber = new ArrayList<>(baseballGoalNumber);
+
+		calculateBaseballList(baseballGoalNumber, userInputBaseballNumber);
+		printBaseballScore();
+
+		return isAllStrike();
+	}
+
+	private static void calculateBaseballList(List<Integer> baseballGoal, List<Integer> userInputBaseball) {
+
+		for (int i = 0; i < baseballGoal.size(); i++) {
+
+			calculateStrike(baseballGoal, userInputBaseball, i);
+		}
+
+		if (isAllStrike()) {
+			calculateBall(userInputBaseball);
+		}
+	}
+
+	private static void calculateStrike(List<Integer> baseballGoal, List<Integer> inputNumber, int index) {
+
+		if (baseballGoal.get(index).equals(inputNumber.get(index))) {
+
+			strike++;
+			baseGoalNumber.remove(baseballGoal.get(index));
+		}
+	}
+
+	private static void calculateBall(List<Integer> userInputBaseball) {
+
+		for (Integer goalNumber : baseGoalNumber) {
+			ball += userInputBaseball.contains(goalNumber) ? 1 : 0;
+		}
+	}
+
+	private static boolean isAllStrike() {
+
+		return !BaseballConstant.strike.equals(BASEBALL_GAME_COUNT);
+	}
 
 	public static boolean isStartGame(Integer gameStartState) {
 
-		return gameStartState.equals(GAME_RESTART);
+		return gameStartState.equals(BASEBALL_GAME_RESTART);
 	}
+
 }
