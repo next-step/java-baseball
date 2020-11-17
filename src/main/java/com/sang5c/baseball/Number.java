8f79e31ec13fea96a29cd9dc32b777bc39748402
sang5c <skah321@gmail.com>
2020-11-17 21:02:24 +0900

    feat(Number): 경계값 밖은 exception이 발생한다

--- src/main/java/com/sang5c/baseball/Number.java
+++ src/main/java/com/sang5c/baseball/Number.java
@@ -5,7 +5,14 @@ public class Number {
     private final int number;
 
     public Number(int number) {
+        validate(number);
         this.number = number;
+    }
+
+    private void validate(int number) {
+        if (number < 1 || number > 9) {
+            throw new IllegalArgumentException("out of range: " + number);
+        }
     }
 
 }
