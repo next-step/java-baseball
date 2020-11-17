f13c9c37eb9a01400700dbfec828c0fc5b71c7a3
rn00n <rn00n@naver.com>
2020-11-17 16:39:34 +0900

    ADD method getStrike, getBall, isNothing, ...testMethod

--- src/main/java/me/rn00n/baseball/pitcher/Pitcher.java
+++ src/main/java/me/rn00n/baseball/pitcher/Pitcher.java
@@ -19,6 +19,83 @@ public class Pitcher {
         this.number = BaseballRandomNumber.getNumber(NUMBER_LENGTH);
     }
 
+    /**
+     * 다음 자릿수가 있는지 확인
+     * @param number
+     * @return
+     */
+    private boolean hasNextNumber(int number) {
+        String s_number = String.valueOf(number);
+        return s_number.length() > 1; // 한자리수이면 false
+    }
+
+    /**
+     * 스트라이크 개수 확인
+     * @param predictNumber
+     * @return
+     */
+    public int getStrike(int predictNumber) {
+        int targetNumber = this.number;
+
+        return countStrike(targetNumber, predictNumber);
+    }
+    /** 개수 반환 **/
+    private int countStrike(int targetNumber, int predictNumber) {
+        int count = 0;
+        if (targetNumber % 10 == predictNumber % 10) {
+            count++;
+        }
+        if (hasNextNumber(targetNumber)) { // 다음 자릿수가 있다면 진행
+            count += countStrike(targetNumber / 10, predictNumber / 10);
+        }
+        return count;
+    }
+
+    /**
+     * 볼 개수 확인 (문자열로 변환하여 처리)
+     * @param predictNumber
+     * @return
+     */
+    public int getBall(int predictNumber) {
+        int targetNumber = this.number;
+        String s_targetNumber = String.valueOf(targetNumber);
+        String s_predictNumber = String.valueOf(predictNumber);
+        int count = 0;
+
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            count += countBall(s_targetNumber, s_predictNumber, i);
+        }
+        return count;
+    }
+    /** 개수 반환 **/
+    private int countBall(String s_targetNumber, String s_predictNumber, int i) {
+        char target = s_targetNumber.charAt(i);
+
+        if (target == s_predictNumber.charAt(i)) { // 스트라이크일 경우 카운트 x
+            return 0;
+        }
+        if (s_predictNumber.contains(String.valueOf(target))) {
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * 포볼(낫싱) 확인
+     * @param predictNumber
+     * @return
+     */
+    public boolean isNothing(int predictNumber) {
+        int count = 0;
+        count += this.getStrike(predictNumber);
+        count += this.getBall(predictNumber);
+
+        if (count == 0) { // 스트라이크 + 볼 개수가 0이면 낫싱
+            return true;
+        }
+        return false;
+    }
+
     /* Getter, Setter (테스트 코드에서 사용) */
     public int getNumber() {
         return number;
--- src/test/java/me/rn00n/baseball/pitcher/PitcherTest.java
+++ src/test/java/me/rn00n/baseball/pitcher/PitcherTest.java
@@ -38,4 +38,66 @@ class PitcherTest {
         assertEquals(pitcher.getNumber(), number);
     }
 
+    @Test
+    @DisplayName("예측 결과 확인 : 스트라이크")
+    void resultPredict_Strike() {
+
+        Pitcher pitcher = new Pitcher();
+        pitcher.setNumber(123);
+
+        int number1 = 145;
+        int number2 = 125;
+        int number3 = 123;
+        int number4 = 924;
+        int number5 = 923;
+        int number6 = 983;
+        int number7 = 456;
+
+        assertEquals(pitcher.getStrike(number1), 1);
+        assertEquals(pitcher.getStrike(number2), 2);
+        assertEquals(pitcher.getStrike(number3), 3);
+        assertEquals(pitcher.getStrike(number4), 1);
+        assertEquals(pitcher.getStrike(number5), 2);
+        assertEquals(pitcher.getStrike(number6), 1);
+        assertEquals(pitcher.getStrike(number7), 0);
+    }
+
+    @Test
+    @DisplayName("예측 결과 확인 : 볼")
+    void resultPredict_Ball() {
+
+        Pitcher pitcher = new Pitcher();
+        pitcher.setNumber(123);
+
+        int number1 = 918;
+        int number2 = 912;
+        int number3 = 312;
+        int number4 = 123;
+        int number5 = 456;
+        int number6 = 321;
+
+        assertEquals(pitcher.getBall(number1), 1);
+        assertEquals(pitcher.getBall(number2), 2);
+        assertEquals(pitcher.getBall(number3), 3);
+        assertEquals(pitcher.getBall(number4), 0);
+        assertEquals(pitcher.getBall(number5), 0);
+        assertEquals(pitcher.getBall(number6), 2);
+    }
+
+    @Test
+    @DisplayName("예측 결과 확인 : 낫싱")
+    void resultPredict_Nothing() {
+
+        Pitcher pitcher = new Pitcher();
+        pitcher.setNumber(123);
+
+        int number1 = 123;
+        int number2 = 456;
+        int number3 = 231;
+
+        assertEquals(pitcher.isNothing(number1), false);
+        assertEquals(pitcher.isNothing(number2), true);
+        assertEquals(pitcher.isNothing(number3), false);
+    }
+
 }
