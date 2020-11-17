2e9a410f39ad1bf457a2f3115754ff5ec0de6829
김동수 <kds@gimdongsuui-MacBookPro.local>
2020-11-17 22:34:24 +0900

    (add,test) Add Domain Model: BallCount
    Added BallCount
     - 볼카운트(스트라이크, 볼의 개수)를 관리한다.
     - 스트라이크를 증가시킨다.
     - 볼을 증가시킨다.
     - 현재 볼카운트를 반환한다.
     - 승리조건(스트라이크 3개)을 확인한다.
     - 볼카운트를 초기화시킨다.

---
+++ src/main/java/baseball/domain/BallCount.java
@@ -0,0 +1,45 @@
+package baseball.domain;
+
+public class BallCount {
+    private int strike;
+    private int ball;
+
+    public boolean isNoithing() {
+        return (strike == 0 && ball == 0);
+    }
+
+    public void addStrike() {
+        this.strike++;
+    }
+
+    public void addBall() {
+        this.ball++;
+    }
+
+    public boolean isAllStrike() {
+        return (strike == 3);
+    }
+
+    public String getCurrent() {
+        String result = "";
+
+        if (isNoithing()) { return "낫싱"; }
+
+        if (strike != 0) { result += strike  + " 스트라이크 "; }
+
+        if (ball != 0) { result += ball + " 볼"; }
+
+        return result;
+    }
+
+    public void clear() {
+        strike = 0;
+        ball = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "BallCount [strike=" + strike + ", ball=" + ball + "]";
+    }
+
+}
---
+++ src/test/java/baseball/domain/BallCountTest.java
@@ -0,0 +1,105 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BallCountTest {
+
+    @DisplayName("생성(낫싱) : 0 스트라이크 0 볼")
+    @Test
+    void 생성() {
+        // given/when
+        BallCount ballCount = new BallCount();
+
+        // then
+        assertThat(ballCount.toString())
+                .isEqualTo("BallCount [strike=0, ball=0]");
+
+        assertThat(ballCount.isNoithing()).isTrue();
+
+        assertThat(ballCount.getCurrent()).isEqualTo("낫싱");
+    }
+
+    @DisplayName("볼카운트 String 반환: 1 스트라이크")
+    @Test
+    void getCurrent_1_스트라이크() {
+        // given
+        BallCount ballCount = new BallCount();
+
+        // when
+        ballCount.addStrike();
+        assertThat(ballCount.toString())
+                .isEqualTo("BallCount [strike=1, ball=0]");
+
+        // then
+        assertThat(ballCount.getCurrent()).isEqualTo("1 스트라이크 ");
+    }
+
+    @DisplayName("볼카운트 String 반환: 1 볼")
+    @Test
+    void getCurrent_1_볼() {
+        // given
+        BallCount ballCount = new BallCount();
+
+        // when
+        ballCount.addBall();
+        assertThat(ballCount.toString())
+                .isEqualTo("BallCount [strike=0, ball=1]");
+
+        // then
+        assertThat(ballCount.getCurrent()).isEqualTo("1 볼");
+    }
+
+    @DisplayName("볼카운트 String 반환: 1 스트라이크 1 볼")
+    @Test
+    void getCurrent_1_스트라이크_1_볼() {
+        // given
+        BallCount ballCount = new BallCount();
+
+        // when
+        ballCount.addStrike();
+        ballCount.addBall();
+        assertThat(ballCount.toString())
+                .isEqualTo("BallCount [strike=1, ball=1]");
+
+        // then
+        assertThat(ballCount.getCurrent()).isEqualTo("1 스트라이크 1 볼");
+    }
+
+    @DisplayName("승리조건 : 3 스트라이크 (승리)")
+    @Test
+    void 승리조건() {
+        // given
+        BallCount ballCount = new BallCount();
+
+        // when
+        ballCount.addStrike();
+        ballCount.addStrike();
+        ballCount.addStrike();
+        assertThat(ballCount.toString())
+                .isEqualTo("BallCount [strike=3, ball=0]");
+
+        // then
+        assertThat(ballCount.isAllStrike()).isTrue();
+    }
+
+    @DisplayName("초기화(낫싱): 0 스트라이크 0 볼")
+    @Test
+    void 초기화() {
+        // given
+        BallCount ballCount = new BallCount();
+
+        // when
+        ballCount.addStrike();
+        ballCount.addBall();
+        assertThat(ballCount.toString())
+                .isEqualTo("BallCount [strike=1, ball=1]");
+        ballCount.clear();
+
+        // then
+        assertThat(ballCount.toString())
+                .isEqualTo("BallCount [strike=0, ball=0]");
+    }
+}
