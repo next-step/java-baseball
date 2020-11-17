27bd075a35e6df6153abfc9b8658c743f8dfb665
tearight <tearightss@gmail.com>
2020-11-17 20:32:05 +0900

    feat(GameStarter): 메인 추가
    - GameStarter 클래스 생성
    - BaseballGame 클래스 및 play() 메소드 생성
    - GameStarter 클래스에서 메인 메소드 추가
    - GameStarter 메인 메소드에서 BaseballGame을 생성하고 play() 호출

---
+++ src/main/java/GameStarter.java
@@ -0,0 +1,9 @@
+import baseball.BaseballGame;
+
+public class GameStarter {
+
+    public static void main(String[] args) {
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.play();
+    }
+}
