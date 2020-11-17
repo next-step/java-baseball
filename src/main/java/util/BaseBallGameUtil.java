d148bd02b084f941110aa603b560088b4ceb090b
heoheon <heoheon.dev@gmail.com>
2020-11-17 09:05:08 +0900

    fix : 각 기능별 메서드 분리

--- src/main/java/util/BaseBallGameUtil.java
+++ src/main/java/util/BaseBallGameUtil.java
@@ -1,8 +1,7 @@
 package util;
 
-import common.Constant.*;
-
-import static common.Constant.*;
+import static ui.Input.userInput;
+import static ui.Output.printResultAndReturn;
 
 
 public class BaseBallGameUtil {
@@ -11,78 +10,60 @@ public class BaseBallGameUtil {
     private static int ball = 0;
 
 
-    // 결과 리턴 메서드
+    // 메인 메서드
+    public static void playGame(int[] computerNumbers) {
+        boolean rtn_result = true;
 
-    public boolean baseBallGameUtil(int[] computerNumbers, int[] userNumbers) {
-
-        judgeStrikeBall(computerNumbers, userNumbers);
-
-        boolean rtn_result = printResultAndReturn();
-
-
-        return rtn_result;
+        while (rtn_result) {
+            int[] userNumbers = userInput();
+            rtn_result = judgeStrikeBall(computerNumbers, userNumbers);
+        }
     }
 
-
     // 스트라이크,볼 판단 메서드
-    public void judgeStrikeBall(int[] computerNumbers, int[] userNumbers) {
+    public static Boolean judgeStrikeBall(int[] computerNumbers, int[] userNumbers) {
+
+        Boolean result = true;
+
         for (int i = 0; i < 3; i++) {
-            judgeStrike(computerNumbers[i], userNumbers[i]);
-            judgeBall(computerNumbers, userNumbers[i], i);
+            strike = judgeStrike(computerNumbers[i], userNumbers[i]);
+            ball = judgeBall(computerNumbers, userNumbers[i], i);
         }
+
+        result = printResultAndReturn(strike, ball);
+        resetCount();
+
+        return result;
+
+
     }
 
     // 스트라이크 판단 메서드
-    private void judgeStrike(int computerNumber, int userNumber) {
+    private static int judgeStrike(int computerNumber, int userNumber) {
         if (computerNumber == userNumber) {
             strike++;
         }
+
+        return strike;
+
     }
 
     // 볼 판단 메서드
-    private void judgeBall(int[] ComputerNumbers, int userNumber, int idx) {
+    private static int judgeBall(int[] ComputerNumbers, int userNumber, int idx) {
         for (int i = 0; i < ComputerNumbers.length; i++) {
             if (i == idx) continue;
-
             if (ComputerNumbers[i] == userNumber) {
                 ball++;
             }
         }
+        return ball;
     }
 
 
-    // 결과값 출력 메서드
-    private boolean printResultAndReturn() {
-
-        StringBuilder sb = new StringBuilder();
-
-        boolean result = true;
-        if (strike != 3) {
-            if (strike > 0) {
-                sb.append(String.format(TEXT_STRIK, strike));
-            }
-            if (ball > 0 ){
-                sb.append(String.format(TEXT_BALL, ball));
-            }
-            if (strike == 0 && ball == 0) {
-                sb.append((String.format(TEXT_NOTHING)));
-            }
-        } else {
-            result = false;
-        }
-        System.out.println(sb.toString());
-
-        resetCount();
-
-        return result;
-    }
-
-    private void resetCount(){
-
+    // 스트라이크, 볼 초기화 메서드
+    private static void resetCount() {
         strike = 0;
         ball = 0;
-
-
     }
 
 
