97f4a9b96d4884ab377d528f305684f93de62dbd
sang5c <skah321@gmail.com>
2020-11-17 20:59:36 +0900

    refactor: number test to ParameterizedTest

--- src/test/java/com/sang5c/baseball/NumberTest.java
+++ src/test/java/com/sang5c/baseball/NumberTest.java
@@ -1,20 +1,20 @@
 package com.sang5c.baseball;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.junit.jupiter.api.Assertions.*;
 
 class NumberTest {
 
     @DisplayName("숫자는 1부터 9까지 생성 가능하다")
-    @Test
-    public void validRange() {
-        assertThatCode(() -> new Number(1)).doesNotThrowAnyException();
-        assertThatCode(() -> new Number(9)).doesNotThrowAnyException();
+    @ParameterizedTest
+    @ValueSource(ints = {
+            1, 9
+    })
+    public void validRange(int number) {
+        assertThatCode(() -> new Number(number)).doesNotThrowAnyException();
     }
 
 }
