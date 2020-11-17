2767a383bf287c5a21c5d0f53fc9826524b1d3fe
sang5c <skah321@gmail.com>
2020-11-17 22:04:43 +0900

    refactor(BaseballCount): 초기화 코드 분리

--- src/test/java/com/sang5c/baseball/domain/BaseballCountTest.java
+++ src/test/java/com/sang5c/baseball/domain/BaseballCountTest.java
@@ -1,5 +1,6 @@
 package com.sang5c.baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,12 @@ import static org.assertj.core.api.Assertions.*;
 
 class BaseballCountTest {
 
+    BaseballCount count;
+
+    @BeforeEach
+    public void setup() {
+        count = BaseballCount.of(0, 0);
+    }
     @DisplayName("카운트의 합은 0이상 3이하이다")
     @Test
     public void countSum() {
@@ -17,23 +24,20 @@ class BaseballCountTest {
     @DisplayName("카운트가 모두 0이면 낫싱")
     @Test
     public void nothing() {
-        BaseballCount baseballCount = BaseballCount.of(0, 0);
-        assertThat(baseballCount.isNothing()).isTrue();
+        assertThat(count.isNothing()).isTrue();
     }
 
     @DisplayName("ball count 증가")
     @Test
     public void increaseBallCount() {
-        BaseballCount baseballCount = BaseballCount.of(0, 0);
-        baseballCount = baseballCount.increaseBallCount();
+        BaseballCount baseballCount = count.increaseBallCount();
         assertThat(baseballCount.getBallCount()).isEqualTo(1);
     }
 
     @DisplayName("strike count 증가")
     @Test
     public void increaseStrikeCount() {
-        BaseballCount baseballCount = BaseballCount.of(0, 0);
-        baseballCount = baseballCount.increaseStrikeCount();
+        BaseballCount baseballCount = count.increaseStrikeCount();
         assertThat(baseballCount.getStrikeCount()).isEqualTo(1);
     }
 
