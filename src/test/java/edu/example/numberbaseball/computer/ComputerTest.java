65123213fbffbd6e7ddd7b139e1125952697ca70
orgojy <giveawesome@gmail.com>
2020-11-17 23:37:08 +0900

    refactor(ComputerTest.java): 재사용되는 `Computer` 객체를 클래스 인스턴스 변수로 수정

--- src/test/java/edu/example/numberbaseball/computer/ComputerTest.java
+++ src/test/java/edu/example/numberbaseball/computer/ComputerTest.java
@@ -1,21 +1,22 @@
 package edu.example.numberbaseball.computer;
 
-import edu.example.numberbaseball.common.Inning;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DisplayName("`Player`를 상대할 `Computer`에 대한 테스트")
 class ComputerTest {
     private static final int VALID_BALL_SIZE_FOR_INNING = 3;
 
+    private final Computer computer = new Computer();
+
     @DisplayName("`Computer`는 랜던하게 생성된 3개 `Ball`을 가진 `Inning`으로 생성된다.")
     @Test
     void createValidComputer() {
-        Computer computer = new Computer();
         List<Integer> computerBallNumberList = computer.getBallNumberList();
         int computerBallNumberListSize = computerBallNumberList.size();
 
@@ -25,7 +26,6 @@ class ComputerTest {
     @DisplayName("`Computer`를 리셋하면 `Inning`의 정보는 새롭게 셋팅된다.")
     @Test
     void reset() {
-        Computer computer = new Computer();
         List<Integer> firstComputerBallNumberList = computer.getBallNumberList();
         int firstComputerBallNumberListSize = firstComputerBallNumberList.size();
 
