cd51c45c56a7b842397074bef54167b516a298dc
junlee <laonzenamoon@gmail.com>
2020-11-17 18:56:57 +0900

    feat($compile): 야구 게임에서 유저가 번호를 입력하기 위한 CommonUtils 클래스의 input 메소드 생성
    3. 유저가 수를 입력한다.
    3.1. 생성된 수를 맞추는 3자리 수를 입력한다.
    3.2. 게임을 계속할지 1자리 수(1 또는 2)를 입력한다.드

--- src/main/java/me/lozm/baseball/common/CommonUtils.java
+++ src/main/java/me/lozm/baseball/common/CommonUtils.java
@@ -1,13 +1,37 @@
 package me.lozm.baseball.common;
 
+import java.util.Scanner;
+
 public class CommonUtils {
 
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 9;
     public static final int ANSWER_SIZE = 3;
+
     public static final boolean CANDIDATE_IS_USED = true;
     public static final boolean CANDIDATE_IS_NOT_USED = false;
 
+    public static final String RESTART_YES = "1";
+    public static final String RESTART_NO = "2";
+
     public static final String REG_EXP_ANSWER = "([1-9]{"+ANSWER_SIZE+"})";
+    public static final String REG_EXP_RESTART = "([1-2]{1})";
+
+    public static final String INPUT_MESSAGE_ANSWER = "" + ANSWER_SIZE + "자리 숫자를 입력해주세요: ";
+    public static final String INPUT_MESSAGE_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ";
+
+
+    public static String input(String printMessage, String inputRegExp) {
+        Scanner scanner = new Scanner(System.in);
+        String input = null;
+        boolean isNotComplete = true;
+        while (isNotComplete) {
+            System.out.println(printMessage);
+            input = scanner.next();
+            isNotComplete = input.matches(inputRegExp) ? false : true;
+        }
+        scanner.close();
+        return input;
+    }
 
 }
