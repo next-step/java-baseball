8d93ea95f64a3ad6c6e6a190bd2eb970101ab7cb
limdingdong <goldbat9554@naver.com>
2020-11-17 20:09:57 +0900

    docs: 메소드 주석 추가
    Authored by Dongsub.Lim

--- src/main/java/baseball/process/BaseballGame.java
+++ src/main/java/baseball/process/BaseballGame.java
@@ -16,6 +16,10 @@ public class BaseballGame {
         player = new Player();
     }
 
+    /**
+     * 게임 실행
+     *  - 게임이 종료될 때까지 User의 입력을 받아 게임을 진행
+     */
     public void executeGame() {
         initPlayer();
         do {
@@ -25,10 +29,20 @@ public class BaseballGame {
         } while (!isGameEnd());
     }
 
+    /**
+     * 게임 종료여부 판별
+     *  - Opponent가 게임에서 짐
+     *  - User가 게임종료 커맨드를 입력
+     * @return 상기 두 조건을 만족해야 true return
+     */
     private boolean isGameEnd() {
         return opponent.isLose() && exitGame();
     }
 
+    /**
+     * 게임 나가기
+     *  - User가 게임종료 커맨드를 입력하는 경우 true return
+     */
     private boolean exitGame() {
         System.out.println("게임을 새로 시작하려면 1 종료하려면 2 를 입력하세요.");
 
@@ -43,6 +57,9 @@ public class BaseballGame {
         return UserCommandEnum.COMMAND_END.equals(inputData);
     }
 
+    /**
+     * User Command 검증
+     */
     private boolean isCorrectCommand(String inputData) {
         if (!UserCommandEnum.contains(inputData)) {
             System.out.println("1 또는 2 를 입력해 주세요.");
@@ -51,6 +68,9 @@ public class BaseballGame {
         return true;
     }
 
+    /**
+     * User 커맨드 입력에 따른 로직 실행
+     */
     private void executeCommand(String inputData) {
         if(UserCommandEnum.COMMAND_RESTART.equals(inputData)) {
             opponent.resetStrikeNumber();
