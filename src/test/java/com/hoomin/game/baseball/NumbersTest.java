3db4d4968e4c44e8c557a6e6c295e7c1dc568a9e
이후민 <lhm7877>
2020-11-17 14:30:17 +0900

    test(NumberTest): 테스트 코드 메소드명 수정

--- src/test/java/com/hoomin/game/baseball/NumbersTest.java
+++ src/test/java/com/hoomin/game/baseball/NumbersTest.java
@@ -15,7 +15,7 @@ public class NumbersTest {
 	@CsvSource(value = {"1234:3자리의 숫자를 입력해야 합니다.",
 			"012:1부터 9까지의 숫자를 입력해야 합니다.",
 			"112:중복되지 않은 숫자를 입력해야 합니다."}, delimiter = ':')
-	public void newNumbers_DuplicateNumber_IllegalArgumentException(String numberString, String message) {
+	public void newNumbers_NotValidNumber_IllegalArgumentException(String numberString, String message) {
 		final String[] split = numberString.split("");
 		final List<Integer> numberCandidateList = new ArrayList<>();
 		for (String s : split) {
@@ -25,5 +25,6 @@ public class NumbersTest {
 				.isInstanceOf(IllegalArgumentException.class)
 				.hasMessage(message);
 	}
-
+	
+	
 }
