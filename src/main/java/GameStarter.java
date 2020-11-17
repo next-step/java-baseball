017952fb2c475d8bc1b0939b426c73cf53c57755
tearight <tearightss@gmail.com>
2020-11-17 20:42:59 +0900

    feat(GameStarter): 사용자로부터 1 또는 2를 입력 받을 때 까지 무한히 입력받는 로직 구현
    - inputCommand()
    - isCommand()

--- src/main/java/GameStarter.java
+++ src/main/java/GameStarter.java
@@ -1,9 +1,27 @@
 import baseball.BaseballGame;
 
+import java.util.Scanner;
+
 public class GameStarter {
+
+    private final static String COMMAND_START = "1";
+    private final static String COMMAND_END = "2";
 
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame();
         baseballGame.play();
+    }
+
+    String inputCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do {
+            input = scanner.next();
+        } while (!isCommand(input));
+        return input;
+    }
+
+    private boolean isCommand(String input) {
+        return input.equals(COMMAND_START) || input.equals(COMMAND_END);
     }
 }
