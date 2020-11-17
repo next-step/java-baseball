974553b7cf650337862ce914b2c7bdfa0d741823
sang5c <skah321@gmail.com>
2020-11-17 21:54:57 +0900

    feat(BaseballCount): 카운트의 합은 0이상 3이하이다

--- src/main/java/com/sang5c/baseball/domain/BaseballCount.java
+++ src/main/java/com/sang5c/baseball/domain/BaseballCount.java
@@ -1,4 +1,30 @@
 package com.sang5c.baseball.domain;
 
 public class BaseballCount {
+    private static final int MIN_COUNT = 0;
+    private static final int MAX_COUNT = 3;
+
+    private final int ballCount;
+    private final int strikeCount;
+
+    public BaseballCount(int ballCount, int strikeCount) {
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+
+    public static BaseballCount of(int ballCount, int strikeCount) {
+        if (invalidCountSum(ballCount, strikeCount)) {
+            throw new IllegalArgumentException("count error");
+        }
+        return new BaseballCount(ballCount, strikeCount);
+    }
+
+    private static boolean invalidCountSum(int ballCount, int strikeCount) {
+        return invalidRange(ballCount + strikeCount) || invalidRange(ballCount) || invalidRange(strikeCount);
+    }
+
+    private static boolean invalidRange(int count) {
+        return count < MIN_COUNT || count > MAX_COUNT;
+    }
+
 }
--- src/test/java/com/sang5c/baseball/domain/BaseballCountTest.java
+++ src/test/java/com/sang5c/baseball/domain/BaseballCountTest.java
@@ -3,15 +3,15 @@ package com.sang5c.baseball.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.*;
 
 class BaseballCountTest {
 
     @DisplayName("카운트의 합은 0이상 3이하이다")
     @Test
     public void countSum() {
-        assertThatCode(() -> new BaseballCount(0, 3)).doesNotThrowAnyException();
+        assertThatCode(() -> BaseballCount.of(0, 3)).doesNotThrowAnyException();
+        assertThatIllegalArgumentException().isThrownBy(() -> BaseballCount.of(1, 3));
     }
 
 }
