6a9a9af7feb6c3ffc193a0cff893335c0d61a6e2
orgojy <giveawesome@gmail.com>
2020-11-17 15:18:12 +0900

    feat(Ball.java): 야구게임에서 사용할 `Ball` 생성

---
+++ src/main/java/edu/example/numberbaseball/common/Ball.java
@@ -0,0 +1,30 @@
+package edu.example.numberbaseball.common;
+
+/**
+ * 야구게임에서 사용할 `Ball`
+ */
+public class Ball {
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 9;
+
+    private final int number;
+
+    public Ball(char value) {
+        this(Character.getNumericValue(value));
+    }
+
+    public Ball(int number) {
+        if (!isValid(number)) {
+            throw new IllegalArgumentException("공의 숫자가 " + number + "로 범위 [" + START_NUMBER + "," + END_NUMBER + "]를 넘었습니다.");
+        }
+        this.number = number;
+    }
+
+    private boolean isValid(int number) {
+        return number >= START_NUMBER && number <= END_NUMBER;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
