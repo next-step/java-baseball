bb0b47e4c48693444129532cce38fdd74ab57203
Pilhwan Kim <pilhwankim03@gmail.com>
2020-11-17 13:38:55 +0900

    feat(GameUI): 게임 결과 출력 및 실행 흐름 정리
    GameUI 에서 GameManager 에서 받은 결과를 출력하고 이후 흐름제어 추가함:
    - GameUI 내에서 입력 받기, 입력 변환, 판별 결과 출력 세부분으로 메서드 나눔
    - 적절하지 않은 입력일 경우 다시 받도록 함
    - 'q' 입력 혹은 3 스트라이크 일 경우 게임 종료

--- src/main/java/baseball/ui/GameUI.java
+++ src/main/java/baseball/ui/GameUI.java
@@ -1,7 +1,9 @@
 package baseball.ui;
 
-import baseball.GameManager;
+import baseball.rule.GameManager;
 import baseball.exceptions.QuitException;
+import baseball.rule.GameResult;
+import baseball.utils.InputParser;
 
 import java.util.Scanner;
 
@@ -14,19 +16,44 @@ public class GameUI {
     }
 
     public void start() {
+        Integer[] inputNums;
+
         while(true) {
-            System.out.println("숫자를 입력해주세요(종료는 'q' 입력) : ");
-            String inputStr = scanner.nextLine();
+            String inputStr = scanInput();
 
-            quitGame(inputStr);
+            try {
+                inputNums = reactInput(inputStr);
+            } catch (IllegalArgumentException ex){
+                continue;
+            }
 
-            // TODO: 핵심 로직 구현은 여기에 한다.
+            GameResult result = gm.judge(inputNums);
+            printResult(result);
         }
     }
 
-    private void quitGame(String inputStr) {
-        if(inputStr.equals("q")) {
-            String quitMessage = "야구 게임을 종료합니다.";
+    private String scanInput() {
+        System.out.println("숫자를 입력해주세요(종료는 'q' 입력) : ");
+        return scanner.nextLine();
+    }
+
+    private Integer[] reactInput(String inputStr) {
+        try {
+            return InputParser.parse(inputStr);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("3자리 숫자를 입력해 주세요.");
+            throw ex;
+        } catch (QuitException ex) {
+            System.out.println(ex.getMessage());
+            throw ex;
+        }
+    }
+
+    private void printResult(GameResult result) {
+        System.out.println("결과 : " + result);
+
+        if(result.isEndOfGame()) {
+            String quitMessage = "정답을 맞춰서 게임을 종료합니다.";
             System.out.println(quitMessage);
             throw new QuitException(quitMessage);
         }
--- src/main/java/baseball/utils/InputParser.java
+++ src/main/java/baseball/utils/InputParser.java
@@ -1,9 +1,16 @@
 package baseball.utils;
 
+import baseball.exceptions.QuitException;
+
 public class InputParser {
     private static final int ANSWER_LENGTH = 3;
 
     public static Integer[] parse(String inputStr) {
+
+        if(inputStr.equals("q")) {
+            String quitMessage = "야구 게임을 종료합니다.";
+            throw new QuitException(quitMessage);
+        }
 
         if(inputStr.length() != ANSWER_LENGTH)
             throw new IllegalArgumentException();
