68ec3b340ee40ad88d03cd1908b44469e97ba35f
hyenny <qlwb9191@naver.com>
2020-11-17 21:38:49 +0900

    test: add ComputerTest resetCount case

--- src/test/java/pre/baseball/ComputerTest.java
+++ src/test/java/pre/baseball/ComputerTest.java
@@ -2,6 +2,7 @@ package pre.baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ComputerTest {
@@ -25,7 +26,7 @@ class ComputerTest {
     }
 
     @Test
-    void 스트라이크_또는_볼_또는_낫싱() {
+    void 오답() {
 	// given
 	int[] numbers = { 1, 3, 5 };
 
@@ -34,6 +35,22 @@ class ComputerTest {
 
 	// then
 	assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("숫자 검사 후 스트라이크와 볼 카운트는 0으로 초기화된다")
+    void 오답_카운트리셋() {
+	// given
+	int[] numbers = { 1, 3, 5 };
+	computer.check(numbers);
+
+	// when
+	int strikeCount = computer.getStrike();
+	int ballCount = computer.getBall();
+
+	// then
+	assertThat(strikeCount).isEqualTo(0);
+	assertThat(ballCount).isEqualTo(0);
     }
 
 }
