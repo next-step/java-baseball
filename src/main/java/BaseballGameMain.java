cb0b8028538e34bd226f0bede247a70f64887ea4
jihyeonSong <withsongjihyeon@gmail.com>
2020-11-17 22:51:36 +0900

    feat: 숫자 야구 게임 시작 기능

--- src/main/java/BaseballGameMain.java
+++ src/main/java/BaseballGameMain.java
@@ -1,7 +1,7 @@
 import com.woowa.precourse.service.BaseballGame;
 import com.woowa.precourse.service.Game;
 
-public class BaseballGameMain { 
+public class BaseballGameMain {
 	/* Main */
 	public static void main(String[] args) {
 		Game game = new BaseballGame();
