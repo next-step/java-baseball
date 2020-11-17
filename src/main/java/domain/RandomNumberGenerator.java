bccc3d30d5061b380674e3d3c3c4f6413cadabf4
slamdunk7575 <slamdunk7575@gmail.com>
2020-11-17 12:34:53 +0900

    (refactor) Random 숫자 생성기에서 기본숫자 사용하도록 수정

--- src/main/java/domain/RandomNumberGenerator.java
+++ src/main/java/domain/RandomNumberGenerator.java
@@ -9,18 +9,8 @@ public class RandomNumberGenerator {
     public static final int BALL_MIN_SIZE = 0;
     private static final int BALL_MAX_SIZE = 3;
 
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 9;
-
-    private static List<BaseBallNumber> sourceBallNumbers = new ArrayList<>();
-
-    static {
-        for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
-            sourceBallNumbers.add(new BaseBallNumber(i));
-        }
-    }
-
     public List<BaseBallNumber> getNumbers() {
+        List<BaseBallNumber> sourceBallNumbers = SourceNumbers.getSourceBallNumbers();
         Collections.shuffle(sourceBallNumbers);
         return Collections.unmodifiableList(new ArrayList<>(sourceBallNumbers.subList(BALL_MIN_SIZE, BALL_MAX_SIZE)));
     }
