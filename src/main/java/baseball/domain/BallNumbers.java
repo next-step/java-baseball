206d95fb1d5c25f5dfd4f1992e7934a38dfc400d
ddaaac <skyblue300a@gmail.com>
2020-12-16 22:01:49 +0900

    feat: BallNumbers 생성할 때, 각 자리수가 1~9 사이의 숫자가 아닌 문자열일 경우 예외처리 구현

--- src/main/java/baseball/domain/BallNumbers.java
+++ src/main/java/baseball/domain/BallNumbers.java
@@ -12,11 +12,19 @@ public class BallNumbers {
 
     public static BallNumbers from(String ballNumbers) {
         List<Integer> numbers = new ArrayList<>();
-        for (char c : ballNumbers.toCharArray()) {
-            numbers.add(Character.getNumericValue(c));
+        for (char number : ballNumbers.toCharArray()) {
+            numbers.add(getNumberInRangeOneToNine(number));
         }
 
         return new BallNumbers(numbers);
+    }
+
+    private static int getNumberInRangeOneToNine(char number) {
+        int numberAsInt = Character.getNumericValue(number);
+        if (numberAsInt <= 0 || numberAsInt >= 10) {
+            throw new IllegalArgumentException("1~9 사이의 숫자만 허용됩니다.");
+        }
+        return numberAsInt;
     }
 
     public List<Integer> getNumbers() {
--- src/test/java/baseball/domain/BallNumbersTest.java
+++ src/test/java/baseball/domain/BallNumbersTest.java
@@ -2,10 +2,13 @@ package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class BallNumbersTest {
     @DisplayName("1~9 사이의 서로 다른 수로 이루어진 임의의 세자리 문자열로 BallNumbers를 생성한다")
@@ -13,5 +16,14 @@ class BallNumbersTest {
     void create() {
         assertThat(BallNumbers.from("123").getNumbers())
                 .isEqualTo(Arrays.asList(1, 2, 3));
+    }
+
+    @DisplayName("입력 문자열이 1~9 사이의 숫자가 아닌 문자열일 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"032", "a32"})
+    void validateNumber(String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> BallNumbers.from(input))
+                .withMessage("1~9 사이의 숫자만 허용됩니다.");
     }
 }
