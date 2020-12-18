6ecddfb4351f60002aff80571062f4c71213ef4b
brody <youoper@gmail.com>
2020-12-18 21:22:19 +0900

    feat(모델): 임의의 숫자 3개를 뽑아 정답을 만드는 기능 추가
    게임을 시작하게 되면, 프로그램은 1부터 9까지 중복되지 않은 임의의 숫자 3개를 뽑아서 정답을 만들어 저장하는 기능
    
    기능 구현 - 모델 1)

---
+++ src/main/java/BaseballModel.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class BaseballModel {
+
+    private String ballNumber;
+
+    public void initializeBallNumber() {
+        Set<Integer> integerSet = new HashSet<>();
+        Random random = new Random();
+
+        while(integerSet.size() < 3){
+            integerSet.add(random.nextInt(10));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(Integer integer: integerSet){
+            sb.append(integer);
+        }
+        ballNumber = sb.toString();
+    }
+}
