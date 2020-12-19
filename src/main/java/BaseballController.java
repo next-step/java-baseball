0606946c4a6fdfd031e50463d7832881c58500fe
kim <youoper@gmail.com>
2020-12-19 15:48:38 +0900

    feat(컨트롤러): 스트라이크 개수를 구하는 기능 추가
    사용자의 입력과 정답 사이의 스트라이크 개수를 구하는 기능 추가
    
    기능 추가 - 컨트롤러 2)

--- src/main/java/BaseballController.java
+++ src/main/java/BaseballController.java
@@ -4,4 +4,18 @@ public class BaseballController {
         return input.matches("[0-9]{3}");
     }
 
+    public int isStrikeAtIndex(String input, String answer, int index){
+        if(input.charAt(index) == answer.charAt(index)){
+            return 1;
+        }
+        return 0;
+    }
+
+    public int getStrikeCount(String input, String answer){
+        int strikeCount = 0;
+        for(int index = 0 ; index < input.length() ; ++index){
+            strikeCount += isStrikeAtIndex(input, answer, index);
+        }
+        return strikeCount;
+    }
 }
