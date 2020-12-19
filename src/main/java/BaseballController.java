2fee41401a3d5679d3e4ec043b745950a9b33160
kim <youoper@gmail.com>
2020-12-19 15:30:52 +0900

    feat(컨트롤러): 사용자가 입력한 숫자의 포맷을 확인하는 기능 추가
    사용자가 입력한 숫자가 정답의 길이와 같고 숫자로만 이루어져 있는지 확인하는 기능 추가
    
    기능 추가 - 컨트롤러 1)

--- src/main/java/BaseballController.java
+++ src/main/java/BaseballController.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class BaseballController {
+public class BaseballController {
+
+    public boolean isValidInput(String input){
+        return input.matches("[0-9]{3}");
+    }
+
 }
