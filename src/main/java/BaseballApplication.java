3a0d4bdcae8302b598ce881a329acfc5d8aeec8e
kim <youoper@gmail.com>
2020-12-20 19:42:38 +0900

    feat(애플리케이션): 전체 프로그램의 메인 함수 추가
    컨트롤러를 생성 및 실행하여 전체 프로그램을 동작하도록 하는 메인 함수 추가
    
    기능 추가 - 애플리케이션 1)

---
+++ src/main/java/BaseballApplication.java
@@ -0,0 +1,6 @@
+public class BaseballApplication {
+    public static void main(String[] args) {
+        BaseballController baseballController = new BaseballController();
+        baseballController.run();
+    }
+}
