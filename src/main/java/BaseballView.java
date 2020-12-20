b29f1c9fc0de9b505ef1734828e8e6ad73993d86
kim <youoper@gmail.com>
2020-12-20 18:36:52 +0900

    feat(뷰): 정답인 경우, 메시지 출력 및 사용자 입력 대기 기능 추가
    정답인 경우, 모두 정답이라는 메시지와 게임을 새로 시작할지 여부를 확인하는 메시지를 출력한 이후, 게임을 새로 시작하는지 여부에 대한 사용자 입력을 대기하는 기능
    
    기능 추가 - 뷰 3)

--- src/main/java/BaseballView.java
+++ src/main/java/BaseballView.java
@@ -19,4 +19,16 @@ public class BaseballView {
         }
         System.out.println(sb.toString());
     }
+
+    public String viewCorrectResult(int lengthOfBallNumber) {
+        System.out.println(String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", lengthOfBallNumber));
+        String input = "";
+
+        while(!input.equals("1") && !input.equals("2")){
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            input = scanner.nextLine();
+        }
+
+        return input;
+    }
 }
