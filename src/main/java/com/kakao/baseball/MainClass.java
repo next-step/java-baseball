2cecd63eb363979de60193bab7bb0349edaf7cd4
tlagyqls7 <tlagyqls7@naver.com>
2020-12-20 16:51:21 +0900

    feat: playground 클래스 추가
    playground는 야구 게임이 동작하는 그라운드로서 동작한다.
    게임의 반복과 종료가 해당된다.

--- src/main/java/com/kakao/baseball/MainClass.java
+++ src/main/java/com/kakao/baseball/MainClass.java
@@ -2,6 +2,7 @@ package com.kakao.baseball;
 
 public class MainClass {
     public static void main(String[] args) {
-        
+        Playground playground = new Playground();
+        playground.run();
     }
 }
--- src/main/java/com/kakao/baseball/Playground.java
+++ src/main/java/com/kakao/baseball/Playground.java
@@ -1,7 +1,40 @@
 package com.kakao.baseball;
 
-public class Playground {
-    public void run(){
+import com.kakao.baseball.game.BaseballGame;
 
+import java.util.Scanner;
+
+public class Playground {
+    private BaseballGame baseballGame;
+    private Scanner sc;
+    public Playground(){
+        baseballGame = new BaseballGame();
+        sc = new Scanner(System.in);
+    }
+
+    public void run(){
+        int input;
+        while(true){
+            System.out.print("숫자를 입력해 주세요 : ");
+            input = sc.nextInt();
+            if(baseballGame.validateInput(input)){
+                System.out.println("잘못된 입력입니다 다시 입력해주세요.");
+                continue;
+            }
+            baseballGame.checkUserInput(input);
+            System.out.println(baseballGame.getResult());
+            if(isEnd()){
+                break;
+            }
+        }
+    }
+    private boolean isEnd(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int input = sc.nextInt();
+        if(input==1){
+            baseballGame.initBaseballNumber();
+            return false;
+        }
+        return true;
     }
 }
