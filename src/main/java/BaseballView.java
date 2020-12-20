1f7c260a487c45acdb424567970f9156853d7507
kim <youoper@gmail.com>
2020-12-20 18:16:23 +0900

    feat(뷰): 스트라이크, 볼 개수를 알려주는 기능 추가
    스트라이트 개수와 볼 개수를 이용하여 사용자에게 결과 메시지를 출력하는 기능 추가
    
    기능 추가 - 뷰 2)

--- src/main/java/BaseballView.java
+++ src/main/java/BaseballView.java
@@ -9,4 +9,14 @@ public class BaseballView {
         return scanner.nextLine();
     }
 
+    public void viewStrikeBallCount(int strikeCount, int ballCount) {
+        StringBuilder sb = new StringBuilder();
+        if(strikeCount > 0){
+            sb.append(String.format("%d 스트라이크 ", strikeCount));
+        }
+        if(ballCount > 0){
+            sb.append(String.format("%d볼", ballCount));
+        }
+        System.out.println(sb.toString());
+    }
 }
