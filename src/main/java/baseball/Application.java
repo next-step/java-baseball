50eaef7c7f096c18f8a5aac4be8933981348f49c
ddaaac <skyblue300a@gmail.com>
2020-12-21 22:07:06 +0900

    refactor: 불필요한 필드 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,18 @@
 package baseball;
 
 import baseball.context.ApplicationContext;
+import baseball.controller.GameController;
 import baseball.domain.BaseBallGame;
-import baseball.ui.GameController;
 
 public class Application {
     public static void main(String[] args) {
         GameController controller = ApplicationContext.getGameController();
-
-        run(controller);
-    }
-
-    private static void run(GameController controller) {
         do {
-            doGame(controller);
+            doOneGame(controller);
         } while (controller.willRestartGame());
     }
 
-    private static void doGame(GameController controller) {
+    private static void doOneGame(GameController controller) {
         BaseBallGame game = controller.startGame();
         while (controller.isProceeding(game)) {
             controller.guess(game);
--- src/main/java/baseball/context/ApplicationContext.java
+++ src/main/java/baseball/context/ApplicationContext.java
@@ -1,9 +1,8 @@
 package baseball.context;
 
-import baseball.domain.NumberGenerateStrategy;
+import baseball.controller.GameController;
 import baseball.domain.Opponent;
 import baseball.domain.RandomBallNumbersGenerator;
-import baseball.ui.GameController;
 import baseball.ui.InputView;
 import baseball.ui.OutputView;
 
@@ -13,31 +12,20 @@ public class ApplicationContext {
     private static final ApplicationContext applicationContext;
 
     static {
-        RandomBallNumbersGenerator numberGenerateStrategy = new RandomBallNumbersGenerator();
-        Opponent opponent = new Opponent(numberGenerateStrategy);
+        Opponent opponent = new Opponent(new RandomBallNumbersGenerator());
+        InputView inputView = new InputView(new Scanner(System.in));
+        OutputView outputView = new OutputView();
 
-        applicationContext = new ApplicationContext(
-                opponent,
-                numberGenerateStrategy,
-                new GameController(new InputView(new Scanner(System.in)), new OutputView(), opponent)
-        );
+        applicationContext = new ApplicationContext(new GameController(inputView, outputView, opponent));
     }
 
     public static GameController getGameController() {
         return applicationContext.gameController;
     }
 
-    private final Opponent opponent;
-    private final NumberGenerateStrategy numberGenerateStrategy;
     private final GameController gameController;
 
-    private ApplicationContext(
-            Opponent opponent,
-            NumberGenerateStrategy numberGenerateStrategy,
-            GameController gameController
-    ) {
-        this.opponent = opponent;
-        this.numberGenerateStrategy = numberGenerateStrategy;
+    private ApplicationContext(GameController gameController) {
         this.gameController = gameController;
     }
 }
