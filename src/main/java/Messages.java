94aa135d38fff5b2fb91b7e278702725a03e2598
teo.kkim <teo.kkim@kakaocorp.com>
2020-12-28 02:59:42 +0900

    feat : 게임에 사용되는 메시지 정의

---
+++ src/main/java/Messages.java
@@ -0,0 +1,8 @@
+public class Messages {
+
+    public static final String numberInputMessage = "숫자를 입력해주세요. : ";
+    public static final String errorMessage = "[에러] 유효하지 않은 입력으로 인해 게임을 종료합니다.";
+    public static final String findAnswerMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String endOrContinueMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+}
