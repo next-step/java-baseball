9171f6794016e38a662fdadb9a4f39b2f3711b59
jeto <kcoms555@naver.com>
2020-12-28 01:51:02 +0900

    feat(Main.java): add feat6. starting a game and restarting or quitting the game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        for(String s : args)
-            System.out.println(s);
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        boolean restartFlag = false;
+        System.out.println("게임을 시작하겠습니다");
+        do {
+            Baseball.run();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 아무 메시지를 입력하세요.");
+            restartFlag = ("1" == scanner.next());
+        } while(restartFlag);
+        System.out.println("종료하였습니다");
     }
 }
