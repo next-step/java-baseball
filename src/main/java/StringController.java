ea90fbc2b01c4c42bc549f25049224496db31395
jeto <kcoms555@naver.com>
2020-12-27 23:46:47 +0900

    feat(StringController.java): add a method to check whether a string has duplated chars

--- src/main/java/StringController.java
+++ src/main/java/StringController.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringController {
     public static String chopAt(String in, int index) {
         in.charAt(index);      /* index range check */
@@ -8,5 +10,19 @@ public class StringController {
             return in.substring(0, in.length()-1);
         }
         return in.substring(0, index) + in.substring(index+1, in.length());
+    }
+
+    public static boolean hasDuplicatedChar(String in){
+        if(in.length() < 2) {
+            return false;
+        }
+
+        char[] chars = in.toCharArray();
+        Arrays.sort(chars);
+        boolean flag = false;
+        for(int i=0; i<chars.length-1; i++){
+            flag |= (chars[i] == chars[i+1]);
+        }
+        return flag;
     }
 }
