0ef09265240367d977c22fe51548f4c3f8c4ee85
teo.kkim <teo.kkim@kakaocorp.com>
2020-12-27 20:08:41 +0900

    feat : 볼과 스트라이크를 계산하는 메서드 구현 (기능 5)

--- src/main/java/BaseBall.java
+++ src/main/java/BaseBall.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class BaseBall {
 
     private final List<Integer> answer;
     private final int MAX_SIZE = 3;
+    private Set<Integer> answerNumberSet;
 
     public BaseBall() {
         answer = new ArrayList<>();
@@ -14,6 +13,35 @@ public class BaseBall {
             int randomNumber = generator.nextInt(10);
             answer.add(randomNumber);
         }
+    }
+
+    public Respond calculate(List<Integer> userInput) {
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < MAX_SIZE; i++) {
+            strikes = increaseWhenStrike(strikes, answer.get(i), userInput.get(i));
+        }
+
+        answerNumberSet = new HashSet<>(answer);
+        for (int i = 0; i < MAX_SIZE; i++) {
+            balls = increaseWhenBall(balls, userInput.get(i));
+        }
+
+        return new Respond(strikes, balls);
+    }
+
+    private int increaseWhenStrike(int strikes, int x, int y) {
+        if (x == y) return strikes + 1;
+        return strikes;
+    }
+
+    private int increaseWhenBall(int balls, int x) {
+        if (answerNumberSet.contains(x)) {
+            answerNumberSet.remove(x);
+            return balls + 1;
+        }
+        return balls;
     }
 
 }
