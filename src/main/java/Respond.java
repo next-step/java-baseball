f17da69560fe4ec959662c832289896cf6d46872
teo.kkim <teo.kkim@kakaocorp.com>
2020-12-27 19:48:05 +0900

    feat : 채점 결과 반환을 위한 Respond 클래스 구현

---
+++ src/main/java/Respond.java
@@ -0,0 +1,19 @@
+public class Respond {
+
+    private final int strikes;
+    private final int balls;
+
+    public Respond(int strikes, int balls) {
+        this.strikes = strikes;
+        this.balls = balls;
+    }
+
+    public int getStrikes() {
+        return strikes;
+    }
+
+    public int getBalls() {
+        return balls;
+    }
+
+}
