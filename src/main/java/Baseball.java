122ce478a0b4b507e585286354f3688f77f72581
jeto <kcoms555@naver.com>
2020-12-27 18:38:03 +0900

    docs(Baseball.java): add a description about license

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,5 +1,11 @@
+/**
+ * Baseball.java
+ * 2020년 12월 26일 생성
+ * 아무 제약 없이 사용할 수 있는 소스코드입니다
+ */
 public class Baseball {
     final private static int INPUT_LENGTH = 3;
+
     public static boolean isValidInput(String in) {
         if(!in.matches("^[0-9]+$")) {
             return false;
