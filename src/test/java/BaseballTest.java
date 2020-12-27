bb2cd9a4d9e640f4d57b763914f3ed10abfc4077
jeto <kcoms555@naver.com>
2020-12-27 23:06:30 +0900

    test(BaseballTest.java): add a test code for 'countBalls' and 'countStrikes'

--- src/test/java/BaseballTest.java
+++ src/test/java/BaseballTest.java
@@ -5,23 +5,37 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 
-
 public class BaseballTest {
-    Baseball b = new Baseball();
-
     @Test
-    void checkInputs(){
+    void isValidInput_LengthIsNot3_False(){
+        assertThat(Baseball.isValidInput("12345")).isEqualTo(false);
         assertThat(Baseball.isValidInput("1234")).isEqualTo(false);
+        assertThat(Baseball.isValidInput("12")).isEqualTo(false);
+        assertThat(Baseball.isValidInput("1")).isEqualTo(false);
+    }
+    @Test
+    void isValidInput_IncludesNonNumeric_False(){
         assertThat(Baseball.isValidInput("abc")).isEqualTo(false);
         assertThat(Baseball.isValidInput("1.2")).isEqualTo(false);
+    }
+    @Test
+    void isValidInput_LengthIs3Numeric_True() {
         assertThat(Baseball.isValidInput("123")).isEqualTo(true);
     }
 
     @Test
-    void testStrikesCounter(){
+    void countStrikes_0to3_0to3(){
         assertThat(Baseball.countStrikes("123", "234")).isEqualTo(0);
         assertThat(Baseball.countStrikes("123", "132")).isEqualTo(1);
         assertThat(Baseball.countStrikes("123", "124")).isEqualTo(2);
         assertThat(Baseball.countStrikes("123", "123")).isEqualTo(3);
+    }
+
+    @Test
+    void countBalls_0to3_0to3(){
+        assertThat(Baseball.countBalls("123", "456")).isEqualTo(0);
+        assertThat(Baseball.countBalls("123", "356")).isEqualTo(1);
+        assertThat(Baseball.countBalls("123", "631")).isEqualTo(2);
+        assertThat(Baseball.countBalls("123", "312")).isEqualTo(3);
     }
 }
