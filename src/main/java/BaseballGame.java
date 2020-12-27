48f1f9161523991f6ef40dd50a9368329701664c
Tony <lgock2023@gmail.com>
2020-12-27 16:18:54 +0900

    feat: baseball game random number
    BaseballGame object generates random number when initialized. This number will be used to check if the user's input is correct.

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+
 public class BaseballGame {
+    private final String randomNumber;
+
+    public BaseballGame(){
+        randomNumber = generateRandomNumber();
+    }
+
     public void start(){
         // TODO: 2020/12/27  
+    }
+
+    private String generateRandomNumber(){
+        Random rand = new Random();
+        return String.format(
+                "%d%d%d",
+                rand.nextInt(10),
+                rand.nextInt(10),
+                rand.nextInt(10)
+        );
     }
 }
