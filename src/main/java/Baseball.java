3ec1a5cd7ac60de0b5d054fdac1972cfa7116d08
jeto <kcoms555@naver.com>
2020-12-28 01:44:29 +0900

    feat(Baseball.java): add feat5. generatting a computer number and running a game with user inputs

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -3,14 +3,54 @@
  * 2020년 12월 26일 생성
  * 아무 제약 없이 사용할 수 있는 소스코드입니다
  */
+
+import java.util.Scanner;
+
 public class Baseball {
     final private static int INPUT_LENGTH = 3;
+    final private static Scanner scanner = new Scanner(System.in);
+
+    public static void run(){
+        int balls = 0;
+        int strikes = 0;
+        String opponentInput = generateBaseballNumbers();
+        do {
+            String userInput = getValidUserInput();
+            balls = countBalls(userInput, opponentInput);
+            strikes = countStrikes(userInput, opponentInput);
+            printBallsAndStrikes(balls, strikes);
+        } while(strikes != INPUT_LENGTH);
+        System.out.println(INPUT_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임종료");
+    }
+
+    private static void printBallsAndStrikes(int balls, int strikes) {
+        if((balls == 0) && (strikes == 0)){
+            System.out.println("낫싱");
+            return;
+        }
+        if(strikes > 0){
+            System.out.print(strikes + " 스트라이크 ");
+        }
+        if(balls > 0){
+            System.out.print(balls + " 볼");
+        }
+        System.out.println();
+    }
+
+    private static String getValidUserInput(){
+        String userInput = "";
+        do {
+            System.out.println(INPUT_LENGTH + "자리 숫자를 입력해주세요 : ");
+            userInput = scanner.next();
+        } while (!isValidInput(userInput));
+        return userInput;
+    }
 
     public static String generateBaseballNumbers() {
         String s = "";
         while(s.length() < INPUT_LENGTH){
             int random0to9 = (int) (Math.random() * 10);
-            s += (s.contains(Integer.toString(random0to9))) ? "" : random0to9;
+            s += (s.contains(Integer.toString(random0to9))) ? "" : random0to9;      /* 이미 중복된 문자열은 피하도록 한다 */
         }
         return s;
     }
