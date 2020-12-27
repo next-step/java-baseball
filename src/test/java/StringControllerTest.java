6426b8f8640c5007d6f38871fdbeacc19da42ed5
jeto <kcoms555@naver.com>
2020-12-28 01:46:56 +0900

    test(StringControllerTest.java): add a test code for hasDuplicatedChar

--- src/test/java/StringControllerTest.java
+++ src/test/java/StringControllerTest.java
@@ -9,4 +9,23 @@ public class StringControllerTest {
         assertThat(StringController.chopAt("abcd", 2)).isEqualTo("abd");
         assertThat(StringController.chopAt("abcd", 3)).isEqualTo("abc");
     }
+
+    @Test
+    void hasDuplicatedChar_LengthIslowerThen2_False() {
+        assertThat(StringController.hasDuplicatedChar("")).isEqualTo(false);
+        assertThat(StringController.hasDuplicatedChar("a")).isEqualTo(false);
+        assertThat(StringController.hasDuplicatedChar("\t")).isEqualTo(false);
+    }
+    @Test
+    void hasDuplicatedChar_IncludingDuplicatedChars_True() {
+        assertThat(StringController.hasDuplicatedChar("aa")).isEqualTo(true);
+        assertThat(StringController.hasDuplicatedChar("aba")).isEqualTo(true);
+        assertThat(StringController.hasDuplicatedChar("abcdefgc")).isEqualTo(true);
+    }
+    @Test
+    void hasDuplicatedChar_StringWithUniqueChars_False() {
+        assertThat(StringController.hasDuplicatedChar("ab")).isEqualTo(false);
+        assertThat(StringController.hasDuplicatedChar("abc")).isEqualTo(false);
+        assertThat(StringController.hasDuplicatedChar("abcdefg")).isEqualTo(false);
+    }
 }
