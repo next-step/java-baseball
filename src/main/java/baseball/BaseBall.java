9925fde1ad736d7450cb64058529261ce919dec6
이수연 <2017052988@hanyang.ac.kr>
2020-12-29 02:12:42 +0900

    feat: 사용자로부터 숫자 입력받아 힌트 문자열 출력
    - 사용자로부터 숫자 입력받음
    - 입력받은 숫자에 대한 힌트(스트라이크, 볼 수) 출력
    - 컴퓨터와 사용자의 숫자가 일치하면 true, 일치하지 않으면 false 반환

---
+++ src/main/java/baseball/BaseBall.java
@@ -0,0 +1,39 @@
+package baseball;
+
+import java.util.List;
+
+public class BaseBall {
+
+    private final NumberScanner numberScanner = new NumberScanner();
+    private final Checker checker = new Checker();
+
+    public boolean guess(List<Integer> computer) {
+        System.out.print("숫자를 입력해주세요: ");
+        List<Integer> user = numberScanner.scan();
+
+        return getResult(computer, user);
+    }
+
+    public boolean getResult(List<Integer> computer, List<Integer> user) {
+        int strike = checker.checkStrike(computer, user);
+        int ball = checker.checkBall(computer, user);
+        System.out.println(makeHindStr(strike, ball));
+
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+
+        return false;
+    }
+
+    public String makeHindStr(int strike, int ball) {
+        if (strike == 0) {
+            return String.format("%d볼", ball);
+        }
+        if (ball == 0) {
+            return String.format("%d스트라이크", strike);
+        }
+        return String.format("%d스트라이크 %d볼", strike, ball);
+    }
+}
---
+++ src/test/java/baseball/BaseBallTest.java
@@ -0,0 +1,44 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseBallTest {
+
+    private final BaseBall baseBall = new BaseBall();
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 1스트라이크 2볼",
+            "2, 0, 2스트라이크",
+            "0, 1, 1볼"})
+    @DisplayName("힌트 문자열을 반환한다.")
+    public void makeHintStrTest(int strike, int ball, String hintStr) {
+        String actual = baseBall.makeHindStr(strike, ball);
+        assertThat(actual).isEqualTo(hintStr);
+    }
+
+    @Test
+    @DisplayName("컴퓨터와 유저의 숫자가 같은 경우 true를 반환한다.")
+    public void getResultWhenCorrectAnswerTest() {
+        List<Integer> computer = List.of(8, 3, 5);
+        List<Integer> user = List.of(8, 3, 5);
+        boolean result = baseBall.getResult(computer, user);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("컴퓨터와 유저의 숫자가 다른 경우 true를 반환한다.")
+    public void getResultWhenNotCorrectAnswerTest() {
+        List<Integer> computer = List.of(8, 3, 5);
+        List<Integer> user = List.of(3, 7, 5);
+        boolean result = baseBall.getResult(computer, user);
+        assertThat(result).isFalse();
+    }
+}
