f38b0643965b0840a294777018f7d86cb138fa23
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2020-12-29 22:44:01 +0900

    feat(게임 실행 정보) : 게임 실행 정보 클래스 구현
    - 3개의 랜덤숫자 데이터를 가지고 있는 게임 실행 정보 클래스 구현

---
+++ src/main/java/game/GameProcessInfo.java
@@ -0,0 +1,16 @@
+package game;
+
+import java.util.ArrayList;
+
+public class GameProcessInfo {
+
+    private ArrayList<Integer> numberArray;
+
+    public ArrayList<Integer> getNumberArray() {
+        return numberArray;
+    }
+
+    public void setNumberArray(ArrayList<Integer> numberArray) {
+        this.numberArray = numberArray;
+    }
+}
