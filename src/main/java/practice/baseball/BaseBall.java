4f118480930db4094d5c34c6546fc346aa167110
Eli Nabro <eli.nabro@kakaocorp.com>
2020-12-30 02:14:23 +0900

    feat : Create getNumbers() method.
    Created getNumbers methode in BaseBall class. it makes three digits and return number to main function.

--- src/main/java/practice/baseball/BaseBall.java
+++ src/main/java/practice/baseball/BaseBall.java
@@ -4,12 +4,23 @@ import practice.javabasic.JavaBasic;
 
 public class BaseBall {
 
-
-
     public static void main(String[] args) {
+        BaseBall baseballObj = new BaseBall();
+
+        do {
+            String number = baseballObj.getNumbers();
 
 
+        } while(true);
 
+    }
+
+    private String getNumbers() {
+        String number = "";
+        number += Integer.toString( (int) ((Math.random()*10000) % 9) + 1 );
+        number += Integer.toString( (int) ((Math.random()*10000) % 9) + 1 );
+        number += Integer.toString( (int) ((Math.random()*10000) % 9) + 1 );
+        return number;
     }
 
 }
