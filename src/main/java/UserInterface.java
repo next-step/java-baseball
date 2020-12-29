33150182209a65b8e969fda8dc57ec831dca369a
sumniy <sumniy94@gmail.com>
2020-12-30 00:55:12 +0900

    feat($app): 사용자 입력 예외 처리 추가
    사용자의 잘못된 입력에 대응할 수 있도록
    예외처리 추가 구현.

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,3 +1,9 @@
+import exception.DuplicateNumberException;
+import exception.NumberLengthException;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -18,15 +24,30 @@ public class UserInterface {
 
     private boolean playGame() {
         int inputNumber = 0;
-        int answerNumber = quizGenerator.generate();
         Scanner sc = new Scanner(System.in);
+        int answerNumber = quizGenerator.generate();
         CountChecker countChecker = new CountChecker(answerNumber);
 
         System.out.println("테스트용 정답 출력 : " + answerNumber);
 
         while(inputNumber != answerNumber) {
             printQuizMessage();
-            inputNumber = sc.nextInt();
+
+            try {
+                inputNumber = sc.nextInt();
+                checkInputNumberFormat(inputNumber);
+            } catch (NumberLengthException error) {
+                System.out.println("잘못된 입력입니다. 입력 숫자의 길이를 맞춰주세요. 길이 : " + ANSWER_LENGTH);
+                continue;
+            } catch (DuplicateNumberException error) {
+                System.out.println("잘못된 입력입니다. 중복된 수를 제거해주세요.");
+                continue;
+            } catch (Exception error) {
+                System.out.println("잘못된 입력입니다. 숫자만 입력해주세요.");
+                sc.next();
+                continue;
+            }
+
             countChecker.check(inputNumber);
         }
 
@@ -36,6 +57,33 @@ public class UserInterface {
         inputNumber = sc.nextInt();
 
         return inputNumber != 1;
+    }
+
+    private void checkInputNumberFormat(int inputNumber) {
+        String input = String.valueOf(inputNumber);
+
+        if(input.length() != ANSWER_LENGTH) throw new NumberLengthException();
+        if(duplicateCheckInputNumber(input)) throw new DuplicateNumberException();
+        if(!isNumber(input)) throw new NumberFormatException();
+    }
+
+    private boolean duplicateCheckInputNumber(String inputNumber) {
+        HashSet<Character> set = new HashSet<>();
+
+        for(int i = 0; i < inputNumber.length(); i++) {
+            set.add(inputNumber.charAt(i));
+        }
+
+        return set.size() != ANSWER_LENGTH;
+    }
+
+    private boolean isNumber(String inputNumber) {
+        try {
+            Integer.parseInt(inputNumber);
+            return true;
+        } catch (Exception error) {
+            return false;
+        }
     }
 
     private void printQuizMessage() {
---
+++ src/main/java/exception/DuplicateNumberException.java
@@ -0,0 +1,4 @@
+package exception;
+
+public class DuplicateNumberException extends RuntimeException{
+}
