37351faf7125ffd2080cd75dbce95bb0e983b730
jamal.park <jamal.park@kakaocorp.com>
2020-12-29 20:30:57 +0900

    feat: add random 생성기능

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -10,8 +10,22 @@ public class Baseball {
         flag = 1;
 
         while (flag == 1) {
+            int[] computerNumber = setComputerNumber();
             setFlag();
         }
+    }
+
+    public int[] setComputerNumber() {
+        int[] computerNumber = new int[3];
+        List<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        while (integerList.size() > 6) {
+            int randomIndex = new Random().nextInt(integerList.size());
+            computerNumber[9 - integerList.size()] = integerList.get(randomIndex);
+            integerList.remove(randomIndex);
+        }
+
+        return computerNumber;
     }
 
     public void setFlag() {
