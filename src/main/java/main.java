491a108861378d2860064ca9d68024f94b8263a6
SimJung <tlawjd99gma@gmail.com>
2020-12-30 01:00:59 +0900

    feat(main class) : judge that it is strike or ball
    Added new function to main class :
     - isAnswer : show message how many strikes and balls
     - isStrikeOrBall() : set strike or ball value using answer and input data

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,13 +6,21 @@ public class main {
     public static Scanner sc = new Scanner(System.in);
     public static boolean used[] = new boolean[10];
     public static int answer[] = {0, 0, 0};
+    public static int strike = 0;
+    public static int ball = 0;
     public static void main(String[] args) {
+        makeAnswerRandom();
+        problem();
     }
 
     public static void problem(){
         while(true){
             System.out.print("숫자를 입력해주세요 : ");
             String num = sc.next();
+
+            isAnswer(num);
+            ball = 0;
+            strike = 0;
         }
 
     }
@@ -37,10 +45,43 @@ public class main {
 
     public static int getRandomNumber(){
         Random random = new Random();
-        int number = random.nextInt(9) + 1;
-        while(used[number])
-            number = random.nextInt(9) + 1;
-
+        int number = random.nextInt(9)+1;
+        while(used[number]){
+            number = random.nextInt(9)+1;
+        }
         return number;
+    }
+
+    public static boolean isAnswer(String num){
+        for(int i=0; i<3; i++){
+            isStrikeOrBall(i, num);
+        }
+
+        if(strike != 0)
+            System.out.print(strike+"스트라이크 ");
+
+        if(ball != 0)
+            System.out.print(ball+"볼");
+
+        if(strike == 0 && ball == 0)
+            System.out.print("일치하는 숫자가 없습니다.");
+
+        System.out.println();
+        if(strike == 3)
+            return true;
+
+        return false;
+    }
+
+    public static void isStrikeOrBall(int n, String num){
+        if(num.charAt(n) - '0' == answer[n]){
+            strike++;
+            return;
+        }
+
+        if(used[num.charAt(n) - '0']) {
+            ball++;
+            return ;
+        }
     }
 }
