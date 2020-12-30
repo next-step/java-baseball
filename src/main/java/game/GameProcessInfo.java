221dfae48b52a7e06383413d98be3c614799b983
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2020-12-30 22:55:53 +0900

    feat(게임 실행 정보) : 게임 실행 정보 클래스 구현
    - 게임 실행 정보 클래스 수정 (setter 제거 및 생성자 구현)
    - 게임 실행 정보 클래스 테스트 메소드 구현 (서로 다른 숫자 3개의 배열 생성 여부 테스트)

--- src/main/java/game/GameProcessInfo.java
+++ src/main/java/game/GameProcessInfo.java
@@ -1,16 +1,49 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
 
 public class GameProcessInfo {
 
-    private ArrayList<Integer> numberArray;
+    private ArrayList<String> randomNumberArray;
+    private Random random;
+    public final int RANDOM_NUMBER_COUNT = 3;
 
-    public ArrayList<Integer> getNumberArray() {
-        return numberArray;
+    public GameProcessInfo(){
+        makeGameProcessInfoNumberArray();
     }
 
-    public void setNumberArray(ArrayList<Integer> numberArray) {
-        this.numberArray = numberArray;
+    public ArrayList<String> getRandomNumberArray() {
+        return randomNumberArray;
+    }
+
+    public ArrayList<String> makeGameProcessInfoNumberArray() {
+
+        HashSet<Integer> numberSet = new HashSet<>();
+
+        while (numberSet.size() < RANDOM_NUMBER_COUNT) {
+            numberSet.add(getRandomNumber());
+        }
+
+        randomNumberArray = new ArrayList<>();
+
+        for (Integer number : numberSet) {
+            randomNumberArray.add(String.valueOf(number));
+        }
+
+        return randomNumberArray;
+    }
+
+    private int getRandomNumber() {
+        return getRandomInstance().nextInt(8) + 1;
+    }
+
+    public Random getRandomInstance() {
+        if (random == null) {
+            random = new Random();
+        }
+
+        return random;
     }
 }
---
+++ src/test/java/game/GameProcessInfoTest.java
@@ -0,0 +1,34 @@
+package game;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GameProcessInfoTest {
+
+    private GameProcessInfo gameProcessInfo;
+
+    @BeforeAll
+    void setgameProcessInfo() {
+        gameProcessInfo = new GameProcessInfo();
+    }
+
+    @Test
+    void makeGameProcessInfoNumberArray() {
+        ArrayList<String> numberArray = gameProcessInfo.makeGameProcessInfoNumberArray();
+        HashSet<Integer> numberSet = new HashSet<>();
+
+        for(String number : numberArray){
+            numberSet.add(Integer.parseInt(number));
+        }
+
+        assertTrue(numberSet.size() == gameProcessInfo.RANDOM_NUMBER_COUNT);
+    }
+
+}
