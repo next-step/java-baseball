d5c07c21b0dc97f544d99cef415956d0657d9d3a
teo.kkim <teo.kkim@kakaocorp.com>
2020-12-30 16:10:55 +0900

    feat : 0스트라이크 0볼인 경우 추가 구현

--- src/main/java/Respond.java
+++ src/main/java/Respond.java
@@ -21,6 +21,7 @@ public class Respond {
         String ret = "";
         if (strikes > 0) ret += (strikes + " 스트라이크 ");
         if (balls > 0) ret += (balls + " 볼 ");
+        if (ret.equals("")) return "낫싱";
         return ret;
     }
 }
