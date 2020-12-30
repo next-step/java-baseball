dd8d7ee3f5714b8dabddf354e9c153dc7b857da1
jh2880722 <jh2880722@naver.com>
2020-12-30 20:58:29 +0900

    feat: print result
    - print ball count and strike count
    - if 3 strike : print game end

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -112,5 +112,34 @@ public class BaseBall {
         return ball;
     }
 
+    private String getResult(int strike, int ball) {
+
+        String res = "";
+
+        if(strike > 0)
+            res += strike + "스트라이크 ";
+        if(ball > 0)
+            res += ball + "볼";
+        if(strike == 0 && ball == 0)
+            res += "낫싱";
+
+        return res;
+    }
+
+    private void printResult() {
+        if(error)
+            return;
+        int strike = getStrike();
+        int ball = getBall();
+
+        String res = getResult(strike,ball);
+
+        System.out.println(res);
+
+        if(strike == 3){
+            end = true;
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
 
 }
