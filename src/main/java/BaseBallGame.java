1792aa214f77fa00a3cc183386914cbf4fcc1ccf
dddfff22 <dddfff22@naver.com>
2020-12-31 04:45:48 +0900

    feat: 유저 입력 확인기능 추가

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -11,26 +11,73 @@ public class BaseBallGame {
         while (isStart()) {
             startGame();
         }
-
+        System.out.println("종료");
     }
 
     private void startGame(){
 
         String botAnswer=generateAnswer();
-        String userAnswer;
+        String userAnswer=getUserAnswer();
 
     }
-    private String getUserInput(){
+
+    private String getUserAnswer(){
 
         System.out.print("숫자를 입력해주세요:");
-        return sc.nextLine();
+        String userAnswer=sc.nextLine();
+        while (!validateUserAnswer(userAnswer)) {
+            System.out.print("숫자를 입력해주세요:");
+            userAnswer=sc.nextLine();
+        }
+        return userAnswer;
+    }
+
+    private boolean validateUserAnswer(String userAnswer){
+
+        if(validateMaxLength(userAnswer) && validateStringToInteger(userAnswer) && validateDuplicate(userAnswer)){
+            return true;
+        }
+        System.out.println("잘못된 입력입니다.");
+        return false;
+
+    }
+
+    private boolean validateMaxLength(String userAnswer){
+
+        if(userAnswer.length()==MAX_LENGTH){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean validateStringToInteger(String userAnswer){
+
+        try {
+            Integer.parseInt(userAnswer);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    private boolean validateDuplicate(String userAnswer){
+
+        HashSet<Character> set=new HashSet<>();
+        int index=0;
+        while (index<userAnswer.length()&&set.add(userAnswer.charAt(index))) {
+            index+=1;
+        }
+        if(index==userAnswer.length()){
+            return true;
+        }
+        return false;
     }
 
     private String generateAnswer(){
 
         HashSet<Integer> set=new HashSet<Integer>();
         Random random=new Random();
-        while(set.size()<MAX_LENGTH){
+        while (set.size()<MAX_LENGTH) {
             set.add(random.nextInt(9)+1);
         }
         List<Integer> list=new ArrayList<>(set);
@@ -41,7 +88,7 @@ public class BaseBallGame {
     private String listToString(List list){
 
         String answer="";
-        for(int i=0;i<list.size();i++){
+        for(int i=0; i<list.size(); i++){
             answer+=String.valueOf(list.get(i));
         }
         return answer;
