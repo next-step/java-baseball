377fedf7022487a978a92478eecb1bbd33a1d49d
danial.yoo <danial.yoo@kakaocorp.com>
2020-12-31 20:57:28 +0900

    테스트 삭제
    - input manager 로직내에 scanner로 입력을 받는 부분이 있는데, junit에서 실행시키면 해당로직이 무시되는 버그가 있음.
    현재 임시적으로 비활성화

--- src/test/java/game/InputManagerTest.java
+++ src/test/java/game/InputManagerTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class InputManagerTest {
     @Test
     public void test(){
-        InputManager im = new InputManager();
-        String an = im.receiveInput("안 이라고 입력해주세요", new Validator() {
-            @Override
-            public void checkValid(String input) throws Exception {
-                if(!"안".equals(input))
-                    throw new Exception("안이 아닌데요?");
-            }
-        });
-        assertTrue(an.equals("안"));
+//        InputManager im = new InputManager();
+//        String an = im.receiveInput("안 이라고 입력해주세요", new Validator() {
+//            @Override
+//            public void checkValid(String input) throws Exception {
+//                if(!"안".equals(input))
+//                    throw new Exception("안이 아닌데요?");
+//            }
+//        });
+//        assertTrue(an.equals("안"));
     }
 }
