b62799948d1899d92be4edae876f0c7e3c14aff9
danial.yoo <danial.yoo@kakaocorp.com>
2020-12-31 22:21:49 +0900

    feat: 사용자 입력 받는 객체 생성

---
+++ src/main/java/game/InputManager.java
@@ -0,0 +1,26 @@
+package game;
+
+import validators.Validator;
+
+import java.util.Scanner;
+
+public class InputManager {
+    static Scanner scanner;
+
+    static {
+        scanner = new Scanner(System.in);
+    }
+
+    public String receiveInput(String comment, Validator checker) {
+        try {
+            System.out.print(comment);
+            String input = scanner.next();
+
+            checker.checkValid(input);
+            return input;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return receiveInput(comment, checker);
+        }
+    }
+}
