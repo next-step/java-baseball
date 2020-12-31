ab2bf2b95d785e70f4e8c22d2e24f65c62fb242a
choihwan2 <choihwan2@naver.com>
2020-12-31 16:25:39 +0900

    feat(&app) : add BaseballGameApp.java
    - 프로그램 실행 소스코드 작성

---
+++ src/main/java/com/pavlo/baseball/BaseballGameApp.java
@@ -0,0 +1,15 @@
+/*
+ * BaseballGameApp.java
+ * version : 1.0
+ * Date : 2020 / 12 / 30
+ * Author : pavlo.v
+ */
+package com.pavlo.baseball;
+
+public class BaseballGameApp {
+    public static void main(String[] args) {
+        BaseballGame game = new BaseballGame();
+        game.start();
+    }
+
+}
