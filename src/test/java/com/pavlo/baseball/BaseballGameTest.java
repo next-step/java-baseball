5f2cea54a33d7b41d399aea715a0912d1cdf287a
choihwan2 <choihwan2@naver.com>
2020-12-31 22:31:33 +0900

    test($BaseballGameTest) : 단위 테스트 추가
    - 스트라이크 개수 확인하는 테스트 추가
    - 볼 개수 확인 하는 테스트 추가
    - 사용자의 입력값이 유효한지 검사하는 테스트 추가

--- src/test/java/com/pavlo/baseball/BaseballGameTest.java
+++ src/test/java/com/pavlo/baseball/BaseballGameTest.java
@@ -2,11 +2,11 @@ package com.pavlo.baseball;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BaseballGameTest {
 
@@ -21,5 +21,25 @@ class BaseballGameTest {
         }
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"3","32","456a","!312","z789"})
+    @DisplayName("사용자 입력값이 유효하지 않은 값인지 확인")
+    void isInValidTypePlayerInput(String input){
+        assertEquals(true, game.isInvalidInput(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:123:3","341:321:2","517:534:1","891:123:0"},delimiter = ':')
+    @DisplayName("스트라이크의 개수 값이 정확한지 확인")
+    void checkStrikeCount(String computer, String player, String cnt){
+        assertEquals(Integer.parseInt(cnt),game.getStrikeCount(computer,player));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"312:123:3","341:321:0","517:534:0","891:123:1"},delimiter = ':')
+    @DisplayName("볼의 개수 값이 정확한지 확인")
+    void checkBallCount(String computer, String player, String cnt){
+        assertEquals(Integer.parseInt(cnt),game.getBallCount(computer,player));
+    }
 
 }
