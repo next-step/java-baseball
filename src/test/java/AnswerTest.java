c59f8f73bdd197a5b774ff28b8edeb208f19d148
danial.yoo <danial.yoo@kakaocorp.com>
2020-12-31 20:12:05 +0900

    정답 클래스 유닛테스트

--- src/test/java/AnswerTest.java
+++ src/test/java/AnswerTest.java
@@ -1,12 +1,42 @@
+import game.Answer;
+import game.BaseballGame;
+import org.assertj.core.util.introspection.FieldUtils;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnswerTest {
+    Answer answer;
+    @BeforeEach
+    public void before(){
+        answer = new Answer();
+    }
+
     @Test
-    public void test(){
-        Answer at = new Answer();
-        String answer = at.answer;
-        assertTrue(at.isCorrect(answer));
+    public void testCorrect(){
+        String ans = answer.answer;
+        assertTrue(answer.isCorrect(ans));
+    }
+    @Test
+    public void testNonCorrect(){
+        String ans = answer.answer;
+        int[] nums = new int[10];
+        for (int i = 0; i < ans.length(); i++) {
+            nums[ans.charAt(i) - '0'] = 1;
+        }
+        String notAns = "";
+        int count = 0;
+        for (int i = 0; i < 10 && count < BaseballGame.NUMBER_LENGTH; i++) {
+            if(nums[i] == 0) {
+                notAns += i;
+                count++;
+            }
+        }
+        assertFalse(answer.isCorrect(notAns));
     }
 }
