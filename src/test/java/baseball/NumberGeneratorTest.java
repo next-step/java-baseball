c950804d19097da3e08bc1fc5ab4dc52973054bf
이수연 <2017052988@hanyang.ac.kr>
2020-12-31 17:12:04 +0900

    refactor: 테스트 1회 수행되게끔 수정

--- src/test/java/baseball/NumberGeneratorTest.java
+++ src/test/java/baseball/NumberGeneratorTest.java
@@ -11,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class NumberGeneratorTest {
 
     private final NumberGenerator numberGenerator = new NumberGenerator();
-    private final int NUMBER_OF_TESTS = 10;
 
     @Test
     @DisplayName("중복되지 않은 숫자를 반환한다.")
@@ -24,20 +23,16 @@ public class NumberGeneratorTest {
     @Test
     @DisplayName("사이즈가 3인 리스트를 반환한다.")
     public void generate3NumbersTest() {
-        for (int i = 0; i < NUMBER_OF_TESTS; i++) {
-            List<Integer> numbers = numberGenerator.generate();
-            assertThat(numbers.size()).isEqualTo(3);
-        }
+        List<Integer> numbers = numberGenerator.generate();
+        assertThat(numbers.size()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("1~9까지의 숫자가 생성된다.")
     public void generateRangeTest() {
-        for (int i = 0; i < NUMBER_OF_TESTS; i++) {
-            List<Integer> numbers = numberGenerator.generate();
-            for (int number : numbers) {
-                assertThat(number).isBetween(1, 9);
-            }
+        List<Integer> numbers = numberGenerator.generate();
+        for (int number : numbers) {
+            assertThat(number).isBetween(1, 9);
         }
     }
 }
