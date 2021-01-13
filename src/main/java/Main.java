804f41b2a5140c3cc3038178b0ec2301878c237d
d36choi <yuio1126@gmail.com>
2021-01-13 17:24:55 +0900

    feat: Create setRandomAnswer() and isAlreadySet() for setting answer number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
 
-    BaseballGame baseballGame = new BaseballGame();
+    public static void main(String [] args) {
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.start();
+    }
 
 }
