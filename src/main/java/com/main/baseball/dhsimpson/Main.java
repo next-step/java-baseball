a169f9f887be3b9f10c332966e616c4a3e11d04d
donghee youn <qeadzc9503@naver.com>
2021-01-13 17:31:51 +0900

    feat(Main.java) : Initialize properties.
    - variables
     - computerNum
     - playerNum
    - methods
     - constructor Main()
     - void playGame()
     - void setComputerNum()
     - void setPlayerNum()
     - void compareNums()
     - void gameMessage()
     - void main(String args)

---
+++ src/main/java/com/main/baseball/dhsimpson/Main.java
@@ -0,0 +1,35 @@
+package com.main.baseball.dhsimpson;
+
+public class Main {
+    int[] computerNum;
+    int[] playerNum;
+    // 기본 생성
+    Main(){
+        computerNum = new int[3];
+        playerNum = new int[3];
+    }
+    // 게임 실행
+    void playGame(){
+
+    }
+    // 컴퓨터가 숫자 지정하기. 한 번의 게임을 시작할 때 한 번만 실행한다.
+    void setComputerNum(){
+
+    }
+    // 플레이어가 숫자 지정하기. 맞출 때 까지 실행한다.
+    void setPlayerNum(){
+
+    }
+    // 컴퓨터와 플레이어의 숫자 비교하기
+    void compareNums(){
+
+    }
+    // 게임 진행 시 메시지 보여주기
+    void gameMessage(){
+
+    }
+    // Test
+    public static void main(String args){
+
+    }
+}
