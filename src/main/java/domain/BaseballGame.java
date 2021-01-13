792ba5d29f0fe79011c050f4e40211f2a2df2c26
munkyung <munkyung0324@gmail.com>
2021-01-13 18:09:29 +0900

    feat: add input number logic, add validate input logic
    Added logic to input 3 number from player:
    - get 3 number from player by char array
    - BaseballGame#getUserInput request ValidationUtil to validate player input

--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -1,6 +1,7 @@
 package domain;
 
 import util.PrintUtil;
+import util.ValidationUtil;
 
 import java.util.Scanner;
 
@@ -8,10 +9,20 @@ public class BaseballGame {
 
     public void play(Scanner scanner) {
         initPrint();
+        getUserInput(scanner);
     }
 
     private void initPrint() {
         PrintUtil.printInfo();
+    }
+
+    private void getUserInput(Scanner scanner) {
+        char[] userInput = scanner.next().toCharArray();
+        try {
+            ValidationUtil.validateUserInput(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
---
+++ src/main/java/util/ValidationUtil.java
@@ -0,0 +1,38 @@
+package util;
+
+public class ValidationUtil {
+    private static final int INPUT_SIZE_CONDITION = 3;
+
+    public static void validateUserInput(char[] userInput) {
+        validateInputSize(userInput.length);
+        validateInputType(userInput);
+        validateInputDuplicate(userInput);
+    }
+
+    private static void validateInputSize(int length) {
+        if(length != INPUT_SIZE_CONDITION) {
+            throw new IllegalArgumentException("잘못 입력하셨습니다. 3자리수를 입력해야합니다. 다시 입력해주세요.");
+        }
+    }
+
+    private static void validateInputType(char[] userInput) {
+        for (char c : userInput) {
+            int tmp = c - '0';
+            if (tmp > 9 || tmp < 1) {
+                throw new IllegalArgumentException("잘못 입력하셨습니다. 숫자가 아닙니다. 다시 입력해주세요.");
+            }
+        }
+    }
+
+    private static void validateInputDuplicate(char[] userInput) {
+        boolean[] checkDuplication = new boolean[10];
+        for (char c : userInput) {
+            int tmp = c - '0';
+            if (!checkDuplication[tmp]) {
+                checkDuplication[tmp] = true;
+            } else {
+                throw new IllegalArgumentException("잘못 입력하셨습니다. 각 자리수 숫자는 달라야 합니다. 다시 입력해주세요.");
+            }
+        }
+    }
+}
