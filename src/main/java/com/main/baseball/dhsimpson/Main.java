78043a682064153b4032440eec1542763d22b9de
donghee youn <qeadzc9503@naver.com>
2021-01-13 18:04:01 +0900

    feat(Main.java) : Implement playOnce Method, Add for Standard input
    - Standar input
     - Imported Scanner class
     - Add Scanner type variable 'scan'
     - Add scan initialize in constructor Main()

--- src/main/java/com/main/baseball/dhsimpson/Main.java
+++ src/main/java/com/main/baseball/dhsimpson/Main.java
@@ -1,12 +1,16 @@
 package com.main.baseball.dhsimpson;
 
+import java.util.Scanner;
+
 public class Main {
     int[] computerNum;
     int[] playerNum;
+    Scanner scan;
     // 기본 생성
     Main(){
         computerNum = new int[3];
         playerNum = new int[3];
+        scan = new Scanner(System.in);
     }
     // 게임 실행
     void playGame(){
@@ -31,7 +35,20 @@ public class Main {
     }
     // 한 루틴의 게임 : 숫자 맞출 때 까지 게임 진행하기
     boolean playOnce(){
-        return true;
+        //TODO : Scanner 관련 예외 발생 처(1,2 이외의 입력)
+        int isFinish;
+        while(true){
+            System.out.println("숫자를 입력해 주세요 : ");
+            setPlayerNum();
+            compareNums(); // 이 안에서 gameMessage를 출력하자.
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            isFinish = scan.nextInt();
+            if(isFinish == 1){
+                return true;
+            }else if(isFinish == 2){
+                return false;
+            }
+        }
     }
     // 게임 진행 시 메시지 보여주기
     void gameMessage(){
