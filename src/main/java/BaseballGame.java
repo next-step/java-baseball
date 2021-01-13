e1d774b145515fb6f274a4435cb8931110b985cb
d36choi <yuio1126@gmail.com>
2021-01-13 18:13:56 +0900

    feature: Create countStrikes and countBalls for counting answer
    그 외에 배열의 크기를 표현한 정수 3을 전역 변수화해 사용함

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -3,15 +3,16 @@ import java.util.*;
 public class BaseballGame {
 
     private int [] answer;
-    private int [] userAnswer = new int[3];
+    private int [] userAnswer;
+    private final int SIZE_INPUT = 3;
     public BaseballGame() {
-        this.answer = new int[3];
-
+        this.answer = new int[SIZE_INPUT];
+        this.userAnswer  = new int[SIZE_INPUT];
     }
     private void setRandomAnswer() {
         int number;
 
-        for (int i=0; i<3; i++) {
+        for (int i=0; i<SIZE_INPUT; i++) {
             number = (int) (Math.random()*9+1);
             if (!isAlreadySet(number)) {
                 answer[i] = number;
@@ -19,7 +20,7 @@ public class BaseballGame {
         }
     }
     private boolean isAlreadySet( int number) {
-        for(int i=0; i<3; i++) {
+        for(int i=0; i<SIZE_INPUT; i++) {
             if (answer[i] == number) {
                 return true;
             }
@@ -40,7 +41,7 @@ public class BaseballGame {
         int num;
         Scanner input = new Scanner(System.in);
 
-        for (int i = 0; i<3; i++) {
+        for (int i = 0; i<SIZE_INPUT; i++) {
             num = input.nextInt();
             userAnswer[i] = num;
         }
@@ -48,7 +49,7 @@ public class BaseballGame {
 
     }
     private boolean isWrongInputs() {
-        for (int i = 0; i<3; i++) {
+        for (int i = 0; i<SIZE_INPUT; i++) {
             if(!isPermittedNumber(userAnswer[i])) {
                 System.out.println("주의:: 1~9 의 숫자를 입력해주세요!");
                 return true;
@@ -69,7 +70,7 @@ public class BaseballGame {
         return 0 < num && num < 10;
     }
     private boolean isOverlap() {
-        for(int i=0; i<3; i++) {
+        for(int i=0; i<SIZE_INPUT; i++) {
             if(equalsTwoNumber(i)) {
                 return true;
             }
@@ -78,8 +79,44 @@ public class BaseballGame {
     }
 
     private boolean equalsTwoNumber(int i) {
-        for (int j=i+1; j<3; j++) {
+        for (int j=i+1; j<SIZE_INPUT; j++) {
             if(userAnswer[i] == userAnswer[j])  {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkAnswer() {
+        int strikes = countStrikes();
+        int balls = countBalls();
+        if (strikes == 0 && balls == 0) {
+            System.out.println("낫씽!");
+            return false;
+        }
+        return true;
+    }
+    private int countStrikes() {
+        int numOfStrike = 0;
+        for(int i=0; i<SIZE_INPUT; i++) {
+            if(answer[i] == userAnswer[i]) {
+                numOfStrike ++;
+            }
+        }
+        return numOfStrike;
+    }
+    private int countBalls() {
+        int numOfBall = 0;
+        for(int i=0; i<SIZE_INPUT; i++) {
+            if(isBall(i)) {
+                numOfBall++;
+            }
+        }
+        return numOfBall;
+    }
+    private boolean isBall(int i) {
+        for(int j=0; j<SIZE_INPUT; j++) {
+            if(answer[i] == answer[j] && i != j) {
                 return true;
             }
         }
