844371736c5b01a9e0318f871b6810df043527fe
Sunghwan-DS <jeonsung02@gmail.com>
2021-01-14 18:09:42 +0900

    fix: Reduce the depth of the makeAnswer Func

--- src/main/java/PlayBaseball.java
+++ src/main/java/PlayBaseball.java
@@ -3,25 +3,32 @@ import java.util.Random;
 public class PlayBaseball {
     public int makeAnswer() {
         Random rand = new Random();
-
         int[] ans_arr = {0, 0, 0};
-        for (int i = 0; i < 3; i++){
-            boolean isoverlap = true;
-            while (isoverlap) {
-                boolean fail = false;
-                int num = rand.nextInt(10);
-                for (int j = 0; j < i; j++){
-                    if (num == ans_arr[j] || num == 0){
-                        fail = true;
-                        break;
-                    }
-                }
-                if (!fail) {
-                    ans_arr[i] = num;
-                    isoverlap = false;
-                }
+
+        // 백의 자리에 해당하는 난수 생성
+        int num = rand.nextInt(10);
+        if (num != 0) {
+            ans_arr[0] = num;
+        }
+
+        // 십의 자리에 해당하는 난수 생성 및 백의 자리 숫자와 중복 체크
+        while (true) {
+            num = rand.nextInt(10);
+            if (num != 0 && num != ans_arr[0]) {
+                ans_arr[1] = num;
+                break;
             }
         }
+
+        // 일의 자리에 해당하는 난수 생성 및 중복 체크
+        while (true) {
+            num = rand.nextInt(10);
+            if (num != 0 && num != ans_arr[0] && num != ans_arr[1]) {
+                ans_arr[2] = num;
+                break;
+            }
+        }
+
         int ans = 100 * ans_arr[0] + 10 * ans_arr[1] + ans_arr[2];
         return ans;
     }
