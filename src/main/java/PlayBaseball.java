50376a1d45fd38159f1152b66c1199c9de834c99
Sunghwan-DS <jeonsung02@gmail.com>
2021-01-14 17:53:27 +0900

    feat: Add Feature - makeAnswer Func

--- src/main/java/PlayBaseball.java
+++ src/main/java/PlayBaseball.java
@@ -1,4 +1,31 @@
+import java.util.Random;
+
 public class PlayBaseball {
+    public int makeAnswer() {
+        Random rand = new Random();
+
+        int[] ans_arr = {0, 0, 0};
+        for (int i = 0; i < 3; i++){
+            boolean isoverlap = true;
+            while (isoverlap) {
+                boolean fail = false;
+                int num = rand.nextInt(10);
+                for (int j = 0; j < i; j++){
+                    if (num == ans_arr[j] || num == 0){
+                        fail = true;
+                        break;
+                    }
+                }
+                if (!fail) {
+                    ans_arr[i] = num;
+                    isoverlap = false;
+                }
+            }
+        }
+        int ans = 100 * ans_arr[0] + 10 * ans_arr[1] + ans_arr[2];
+        return ans;
+    }
+
     public static void main(String[] args) throws Exception{
 
     }
