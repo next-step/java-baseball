c3ab3060aa3df3ee00b5b161c81318a4ff2be32b
munkyung <munkyung0324@gmail.com>
2021-01-14 17:19:30 +0900

    refactor: create Player class
    - migrate player input feature to Player class

--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -1,38 +1,22 @@
 package domain;
 
 import util.PrintUtil;
+import util.RandomNumberUtil;
 import util.ValidationUtil;
 
 import java.util.Scanner;
 
 public class BaseballGame {
+    public static final int INPUT_NUMBER_SIZE = 3;
 
     public void play(Scanner scanner) {
         initPrint();
-        int[] userInput = getUserInput(scanner);
+        int[] userInput = Player.getPlayerInput(scanner);
+        int[] systemNum = RandomNumberUtil.getDistinctNumber();
     }
 
     private void initPrint() {
         PrintUtil.printInfo();
-    }
-
-    private int[] getUserInput(Scanner scanner) {
-        char[] userInput = scanner.next().toCharArray();
-        try {
-            ValidationUtil.validateUserInput(userInput);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return charToInt(userInput);
-    }
-
-    private int[] charToInt(char[] userInput) {
-        int[] userInputNum = new int[3];
-        for (int i = 0; i < 3; i++) {
-            int tmp = userInput[i] - '0';
-            userInputNum[i] = tmp;
-        }
-        return userInputNum;
     }
 
 }
---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,29 @@
+package domain;
+
+import util.ValidationUtil;
+
+import java.util.Scanner;
+
+import static domain.BaseballGame.INPUT_NUMBER_SIZE;
+
+public class Player {
+
+    public static int[] getPlayerInput(Scanner scanner) {
+        char[] userInput = scanner.next().toCharArray();
+        try {
+            ValidationUtil.validateUserInput(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return charToInt(userInput);
+    }
+
+    private static int[] charToInt(char[] userInput) {
+        int[] userInputNum = new int[3];
+        for (int i = 0; i < INPUT_NUMBER_SIZE; i++) {
+            int tmp = userInput[i] - '0';
+            userInputNum[i] = tmp;
+        }
+        return userInputNum;
+    }
+}
