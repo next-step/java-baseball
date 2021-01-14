20c7ce42a33ca6757e7203014a8ef6428aa297dc
hanseokhyeon <sah0322@naver.com>
2021-01-14 16:40:44 +0900

    feat: complete resetGame function
    Function of resetGame:
    - initialize the computer's number

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -1,35 +1,55 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class NumberBaseball {
   public static void main(String[] args) {
-      NumberBaseball nb = new NumberBaseball();
-      nb.startGame();
+    NumberBaseball nb = new NumberBaseball();
+    nb.startGame();
   }
 
   public void startGame() {
-      int gameStart = 1;
-      int number;
+    int gameStart = 1;
+    String userNumber, computerNumber;
 
-      Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-      while (gameStart == 1){
-          this.resetGame();
+    while (gameStart == 1){
+      computerNumber = this.resetGame();
 
-          System.out.println("숫자를 입력해 주세요 : ");
-          number = sc.nextInt();
+      System.out.println("숫자를 입력해 주세요 : ");
+      userNumber = sc.next();
 
-          this.playGame(number);
+      this.playGame(computerNumber, userNumber);
 
-          System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-          gameStart = sc.nextInt();
-      }
+      System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+      gameStart = sc.nextInt();
+    }
   }
 
-  private void resetGame() {
+  private String resetGame() {
+    int[] numbers = new int[3];
+    int result = 0;
+    Random rand = new Random();
 
+    numbers[0] = rand.nextInt(9)+1;
+    result += numbers[0]*100;
+
+    numbers[1] = rand.nextInt(9)+1;
+    while (numbers[0] == numbers[1]) {
+      numbers[1] = rand.nextInt(9)+1;
+    }
+    result += numbers[1]*10;
+
+    numbers[2] = rand.nextInt(9)+1;
+    while (numbers[0] == numbers[2] || numbers[1] == numbers[2]) {
+      numbers[2] = rand.nextInt(9)+1;
+    }
+    result += numbers[2];
+
+    return String.valueOf(result);
   }
 
-  private void playGame(int number) {
+  private void playGame(String target, String number) {
 
   }
 }
