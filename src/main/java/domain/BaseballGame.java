8bb4c43ad74389f42c8995753856c74396a2de30
munkyung <munkyung0324@gmail.com>
2021-01-14 16:40:49 +0900

    feat: change player input type
    - change char array to int array

--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -9,20 +9,30 @@ public class BaseballGame {
 
     public void play(Scanner scanner) {
         initPrint();
-        getUserInput(scanner);
+        int[] userInput = getUserInput(scanner);
     }
 
     private void initPrint() {
         PrintUtil.printInfo();
     }
 
-    private void getUserInput(Scanner scanner) {
+    private int[] getUserInput(Scanner scanner) {
         char[] userInput = scanner.next().toCharArray();
         try {
             ValidationUtil.validateUserInput(userInput);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+        return charToInt(userInput);
+    }
+
+    private int[] charToInt(char[] userInput) {
+        int[] userInputNum = new int[3];
+        for (int i = 0; i < 3; i++) {
+            int tmp = userInput[i] - '0';
+            userInputNum[i] = tmp;
+        }
+        return userInputNum;
     }
 
 }
