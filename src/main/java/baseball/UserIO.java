4c5a62b8264b669dcb2f65d28effee7937f332e4
quadam36 <quadam36@gmail.com>
2021-01-14 18:01:02 +0900

    refactor: Rename constants
    - Rename GET_NUMBER_MSG -> INPUT_NUMBER_MSG
    - Rename INPUT_ISRESTART_MSG -> INPUT_IS_RESTART_MSG

--- src/main/java/baseball/UserIO.java
+++ src/main/java/baseball/UserIO.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserIO {
-    private static final String GET_NUMBER_MSG = "숫자를 입력해주세요 : ";
-    private static final String INPUT_ISRESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INPUT_NUMBER_MSG = "숫자를 입력해주세요 : ";
+    private static final String INPUT_IS_RESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final Scanner scanner;
 
     public UserIO(Scanner scanner) {
@@ -16,7 +16,7 @@ public class UserIO {
     public ArrayList<Integer> inputNumber() {
         ArrayList<Integer> number = new ArrayList<>();
 
-        System.out.print(GET_NUMBER_MSG);
+        System.out.print(INPUT_NUMBER_MSG);
         String[] strings = this.scanner.next().split("");
         for (String s : strings) {
             int val = Integer.parseInt(s);
@@ -26,7 +26,7 @@ public class UserIO {
     }
 
     public Boolean inputIsRestart() {
-        System.out.println(INPUT_ISRESTART_MSG);
+        System.out.println(INPUT_IS_RESTART_MSG);
         Integer val = this.scanner.nextInt();
 
         return val.equals(1);
