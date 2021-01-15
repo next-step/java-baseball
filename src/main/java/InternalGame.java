8688193ef5657cb5dc9ed5aaebcadccf9e528d3d
jeongdaeun98 <uneap@sk.com>
2021-01-15 13:57:52 +0900

    feat: Add getStrikeCount that returns strike count

--- src/main/java/InternalGame.java
+++ src/main/java/InternalGame.java
@@ -1,2 +1,14 @@
 public class InternalGame {
+
+    private int getStrikeCount(GameInfo realGameInfo, GameInfo userGameInfo) {
+        String realBall = realGameInfo.getBall();
+        String userBall = userGameInfo.getBall();
+        int strikeCount = 0;
+        for (int i = 0; i < realBall.length(); i++) {
+            if (realBall.charAt(i) == userBall.charAt(i)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
 }
