213839233cdbb174a8dbfe9a4f4f704f1830f9e9
donghee youn <qeadzc9503@naver.com>
2021-01-15 17:30:43 +0900

    feat(Application.java) : Create and Implement main method
    main method is for test

--- src/main/java/com/main/baseball/dhsimpson/Application.java
+++ src/main/java/com/main/baseball/dhsimpson/Application.java
@@ -1,2 +1,7 @@
-package com.main.baseball.dhsimpson;public class Application {
+package com.main.baseball.dhsimpson;
+
+public class Application {
+    public static void main(String[] args){
+        new Baseball();
+    }
 }
