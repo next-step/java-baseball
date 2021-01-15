8ac394cf819816155d78fd81a3bda52272da241d
Changhwan Lee <chlee1252@gmail.com>
2021-01-15 22:54:56 +0900

    style: add comments
    Add comments on function that need an explanation.

--- src/main/java/features/NumberUtil.java
+++ src/main/java/features/NumberUtil.java
@@ -6,15 +6,28 @@ import java.util.Collections;
 import java.util.List;
 
 public class NumberUtil {
+    /**
+     * Return three unique random numbers.
+     * @return list {list of integers} with length of 3.
+     */
     public static List<Integer> generateAnswer() {
+        // Create list of integers that contains 1-9.
         List<Integer> answer = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        // Shuffle the list
         Collections.shuffle(answer);
         return answer.subList(0, 3);
     }
 
-    public static List<Integer> convertString(String userInput) {
+  /**
+   * Convert given string to list and returns it.
+   * @param string {contains only integers} Cannot be null.
+   * @return list {list of integers} with same order as param.
+   */
+  public static List<Integer> convertString(String userInput) {
+        // Create Empty list.
         List<Integer> converted = new ArrayList<>();
         for (int i = 0; i < userInput.length(); i++) {
+            // Convert each string of numbers to integer.
             converted.add(Character.getNumericValue(userInput.charAt(i)));
         }
         return converted;
--- src/main/java/features/Validator.java
+++ src/main/java/features/Validator.java
@@ -1,8 +1,14 @@
 package features;
 
 public class Validator {
+    // Regular Expression to detect unique 3 digit number.
     private static String regex = "(?!.*(.).*\\1)^[1-9]{3}";
 
+    /**
+     * Decide given string is validate.
+     * @param string any type of string.
+     * @return boolean.
+     */
     public static boolean isCorrectInput(String userInput) {
         return userInput.matches(regex);
     }
