a8d1d520a5e094cadce4fdf3790d071c5ed816eb
munkyung <munkyung0324@gmail.com>
2021-01-15 17:13:39 +0900

    feat: add ball check, print logic
    Added check ball function:
    - check ball
    - print ball hint in console

--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -6,7 +6,8 @@ import util.RandomNumberUtil;
 import java.util.Scanner;
 
 public class BaseballGame {
-    public static final int INPUT_NUMBER_SIZE = 3;
+    public static final int NUMBER_SIZE = 3;
+    public static final int GAME_OVER_CONDITION_STRIKE = 3;
 
     public void play(Scanner scanner) {
         PrintUtil.printStartInfo();
@@ -15,12 +16,13 @@ public class BaseballGame {
         int strikeCnt = checkStrike(userInput, systemNum);
         PrintUtil.printStrikeHint(strikeCnt);
         checkAnswer(strikeCnt);
-
+        int ballCnt = checkBall(userInput, systemNum);
+        PrintUtil.printBallHint(ballCnt);
     }
 
     private static int checkStrike(int[] userNum, int[] systemNum) {
         int strikeCnt = 0;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < NUMBER_SIZE; i++) {
             if(userNum[i] == systemNum[i]) {
                 strikeCnt++;
             }
@@ -29,13 +31,28 @@ public class BaseballGame {
     }
 
     private static boolean checkAnswer(int strikeCnt) {
-        if (strikeCnt == 3) {
+        if (strikeCnt == GAME_OVER_CONDITION_STRIKE) {
             PrintUtil.printGameOver();
             PrintUtil.printAskReplayGame();
             return true;
         } else {
             return false;
         }
+    }
+
+    private int checkBall(int[] userInput, int[] systemNum) {
+        int ballCnt = 0;
+        boolean[] checkSystemNum = new boolean[10];
+        for (int sysNum : systemNum) {
+            checkSystemNum[sysNum] = true;
+        }
+        for (int i = 0; i < 3; i++) {
+            int tmp = userInput[i];
+            if(tmp != systemNum[i] && checkSystemNum[tmp]) {
+                ballCnt++;
+            }
+        }
+        return ballCnt;
     }
 
 }
--- src/main/java/util/PrintUtil.java
+++ src/main/java/util/PrintUtil.java
@@ -18,4 +18,8 @@ public class PrintUtil {
     public static void printAskReplayGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public static void printBallHint(int ballCnt) {
+        System.out.println(ballCnt + "볼");
+    }
 }
