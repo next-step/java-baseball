12bce2fb9c61c2bebd9ff317ed1b9924fe01865a
Changhwan Lee <chlee1252@gmail.com>
2021-01-15 21:49:53 +0900

    feat: add feature to get user input
    Add feature to ask and get user numbers and game option.

--- src/main/java/io/PrintInput.java
+++ src/main/java/io/PrintInput.java
@@ -1,2 +1,21 @@
-package io;public class PrintInput {
+package io;
+
+import java.util.Scanner;
+
+public class PrintInput {
+    private final Scanner scanner;
+
+    public PrintInput(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String recieveUserNumber() {
+        System.out.print("숫자를 입력하세요: ");
+        return scanner.next();
+    }
+
+    public Integer askUserOption() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Integer.parseInt(scanner.next());
+    }
 }
