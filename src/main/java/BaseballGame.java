bcd8ce6d76fa317b76793d6c259d5ed91f2081fc
KimBB <tree9295@gmail.com>
2021-01-15 16:59:16 +0900

    feat: BaseballGame 구현
    - 게임 시작 메시지
    - 게임 종료 메시지
    - 게임 작동
    - Main 클래스 main 문에서 실행

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class BaseballGame implements Game {
+
+    @Override
+    public void gameRun() {
+        gameStartMsg();
+        gameEndMsg();
+    }
+
+    @Override
+    public void gameStartMsg() {
+        System.out.println("야구 게임 시작 !");
+    }
+
+    @Override
+    public void gameEndMsg() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        GameManager gm = new BaseballGameManager();
+        gm.gameProcess(new BaseballGame());
     }
 }
