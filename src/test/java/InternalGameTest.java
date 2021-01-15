22f13a37ca52cfc29ed0b521b62bc07059ea9123
jeongdaeun98 <uneap@sk.com>
2021-01-15 21:31:51 +0900

    test: Create getBallCount test code

--- src/test/java/InternalGameTest.java
+++ src/test/java/InternalGameTest.java
@@ -12,4 +12,14 @@ class InternalGameTest {
         assertEquals(internalGame.getStrikeCount(new GameInfo("123"), new GameInfo("453")), 1);
         assertEquals(internalGame.getStrikeCount(new GameInfo("123"), new GameInfo("456")), 0);
     }
+
+    @Test
+    void getBallCount() {
+        InternalGame internalGame = new InternalGame();
+        assertEquals(internalGame.getBallCount(new GameInfo("123"), new GameInfo("123")), 0);
+        assertEquals(internalGame.getBallCount(new GameInfo("123"), new GameInfo("321")), 2);
+        assertEquals(internalGame.getBallCount(new GameInfo("123"), new GameInfo("312")), 3);
+        assertEquals(internalGame.getBallCount(new GameInfo("123"), new GameInfo("142")), 1);
+        assertEquals(internalGame.getBallCount(new GameInfo("123"), new GameInfo("456")), 0);
+    }
 }
