23ff1b15516cb35af7e2f4b40b76597762e654b6
jeongdaeun98 <uneap@sk.com>
2021-01-15 21:31:29 +0900

    test: Create getStrikeCount test code

--- src/test/java/InternalGameTest.java
+++ src/test/java/InternalGameTest.java
@@ -1,4 +1,15 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class InternalGameTest {
-  
+
+    @Test
+    void getStrikeCount() {
+        InternalGame internalGame = new InternalGame();
+        assertEquals(internalGame.getStrikeCount(new GameInfo("123"), new GameInfo("123")), 3);
+        assertEquals(internalGame.getStrikeCount(new GameInfo("123"), new GameInfo("423")), 2);
+        assertEquals(internalGame.getStrikeCount(new GameInfo("123"), new GameInfo("453")), 1);
+        assertEquals(internalGame.getStrikeCount(new GameInfo("123"), new GameInfo("456")), 0);
+    }
 }
