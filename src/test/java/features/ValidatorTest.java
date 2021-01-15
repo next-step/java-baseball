70193eff462b8e1f4fc149cf00c92b9a57ba4a37
Changhwan Lee <chlee1252@gmail.com>
2021-01-15 21:19:14 +0900

    test: add test to validator
    Add false input test and correct input test.

--- src/test/java/features/ValidatorTest.java
+++ src/test/java/features/ValidatorTest.java
@@ -1,4 +1,25 @@
+package features;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
+
 public class ValidatorTest {
-  
+
+    @Test
+    @DisplayName("Test validator using false input")
+    void falseInputTest() {
+        String[] testSet = {"441", "414", "024", "2", "33", "asdf", "123asdf"};
+        for (String test: testSet) {
+            assertFalse(Validator.isCorrectInput(test));
+        }
+    }
+
+    @Test
+    @DisplayName("Test validator using correct input")
+    void trueInputTest() {
+        assertTrue(Validator.isCorrectInput("123"));
+    }
+
 }
