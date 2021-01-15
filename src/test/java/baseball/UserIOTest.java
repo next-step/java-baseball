bf6912153708eac7ab73d64cc95368fffcdc862d
quadam36 <quadam36@gmail.com>
2021-01-15 13:16:22 +0900

    style: fix wrong continuation indent
    - Fix wrong continuation indent from 8 -> 4

--- src/test/java/baseball/UserIOTest.java
+++ src/test/java/baseball/UserIOTest.java
@@ -42,14 +42,14 @@ class UserIOTest {
         ArrayList<Integer> number = io.inputNumber();
 
         assertEquals(
-                "숫자를 입력해주세요 : ",
-                testOut.toString()
+            "숫자를 입력해주세요 : ",
+            testOut.toString()
         );
         assertEquals(
-                number,
-                new ArrayList<>(
-                        Arrays.asList(1, 2, 3)
-                )
+            number,
+            new ArrayList<>(
+                Arrays.asList(1, 2, 3)
+            )
         );
     }
 
@@ -63,8 +63,8 @@ class UserIOTest {
         Boolean isRestart = io.inputIsRestart();
 
         assertEquals(
-                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-                testOut.toString()
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+            testOut.toString()
         );
         assertEquals(expected, isRestart);
     }
@@ -72,18 +72,18 @@ class UserIOTest {
     @ParameterizedTest
     @DisplayName("Test output is printed with argument successfully")
     @CsvSource({
-            "0, 0, '낫싱\n'",
-            "1, 0, '1볼\n'",
-            "0, 2, '2스트라이크\n'",
-            "2, 1, '2볼 1스트라이크\n'",
+        "0, 0, '낫싱\n'",
+        "1, 0, '1볼\n'",
+        "0, 2, '2스트라이크\n'",
+        "2, 1, '2볼 1스트라이크\n'",
     })
     public void outputInfo(Integer ball, Integer strike, String expected) {
         UserIO io = new UserIO(new Scanner(""));
         io.outputInfo(ball, strike);
 
         assertEquals(
-                expected,
-                testOut.toString()
+            expected,
+            testOut.toString()
         );
     }
 
@@ -94,8 +94,8 @@ class UserIOTest {
         io.outputIsFinish();
 
         assertEquals(
-                "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n",
-                testOut.toString()
+            "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n",
+            testOut.toString()
         );
     }
 }
