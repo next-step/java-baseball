79df7f5a545abc0f3d7b542b4f5f1cc6637daea9
jeongdaeun98 <uneap@sk.com>
2021-01-15 19:02:40 +0900

    feat: Add main function and call starting game

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class BaseballGame {
+
+    public static void main(String[] args) {
+        InternalGame internalGame = new InternalGame();
+        RequestUser requestUser = new RequestUser();
+        GameInfo realGameInfo = internalGame.getRealGameInfo();
+        while (true) {
+            GameInfo gameResultInfo = internalGame.startBaseBallGame(realGameInfo);
+            String userAnswer = requestUser.askUserForResumeGame(gameResultInfo);
+            if (userAnswer == null) {
+                continue;
+            }
+            if (!internalGame.isGameRenew(userAnswer)) {
+                break;
+            } else {
+                realGameInfo = internalGame.getRealGameInfo();
+            }
+        }
+    }
 }
