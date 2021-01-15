619a287626061c424264b771849a3e23545e2298
Changhwan Lee <chlee1252@gmail.com>
2021-01-15 21:52:07 +0900

    feat: add main function
    Add feature to start main function.

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Play {
+import baseball.BaseballGame;
+
+public class Play {
+    public static void main(String[] args) {
+        BaseballGame game = new BaseballGame();
+        game.play();
+    }
 }
