24ca6523fac0e395fb9a405b87e5f17cbf120c45
Changhwan Lee <chlee1252@gmail.com>
2021-01-15 22:21:05 +0900

    feat: add feature to convert String to list
    Add feature to convert user input string to list of integers.

--- src/main/java/features/NumberUtil.java
+++ src/main/java/features/NumberUtil.java
@@ -1,5 +1,6 @@
 package features;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -9,5 +10,13 @@ public class NumberUtil {
         List<Integer> answer = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         Collections.shuffle(answer);
         return answer.subList(0, 3);
+    }
+
+    public static List<Integer> convertString(String userInput) {
+        List<Integer> converted = new ArrayList<>();
+        for (int i = 0; i < userInput.length(); i++) {
+            converted.add(Character.getNumericValue(userInput.charAt(i)));
+        }
+        return converted;
     }
 }
