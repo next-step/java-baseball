ee66825e20c182fa02158ac67225367f4376f881
KimBB <tree9295@gmail.com>
2021-01-15 15:32:10 +0900

    feat: BaseballGameManager 프로세스 인풋 구현
    - BaseballGameManager 프로세스 과정에서 필요한 인풋 기능 구현

--- src/main/java/BaseballGameManager.java
+++ src/main/java/BaseballGameManager.java
@@ -1,13 +1,35 @@
 public class BaseballGameManager extends GameManager {
+    private int start;
+
+    public BaseballGameManager(){
+        start = 1;
+    }
 
     @Override
     public void gameProcess(Game game){
         this.game = game;
-        gameStart();
+        while(start == 1){
+            gameStart();
 
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            start = getOneOrTwoIntegerInput();
+        }
         gameEnd();
     }
+
+    public int getOneOrTwoIntegerInput() {
+        int input;
+        while (true){
+            input = GameManager.userInput.nextInt();
+            if (input == 1 || input == 2){
+                return input;
+            }
+            else {
+                System.out.println("1 또는 2를 입력해주세요.");
+            }
+        }
+    }
+
 
     @Override
     public void gameStart(){
