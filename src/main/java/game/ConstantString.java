b0549c0068948402fd06a080347c50f2c01f8319
jeongdaeun98 <uneap@sk.com>
2021-01-15 22:46:15 +0900

    refactor: remove constant string to modify enum type

--- src/main/java/game/ConstantString.java
+++ src/main/java/game/ConstantString.java
@@ -3,9 +3,6 @@ package game;
 public class ConstantString {
 
     static final String THREE_STRIKE = "3스트라이크\n 3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
-    static final String STRIKE = "스트라이크 ";
-    static final String BALL = "볼 ";
-    static final String NOTHING = "낫싱 ";
     static final String REQUEST_NUM_INPUT = "숫자를 입력해주세요 : ";
     static final String GAME_RESUMPTION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
 }
