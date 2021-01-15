bb8e4b597b7858255222d8f75046b98f86bc87d8
KimBB <tree9295@gmail.com>
2021-01-16 00:17:06 +0900

    feat: BaseballGame strike, ball 선언, 초기화 구현
    - strike, ball 속성 정의
    - strike, ball 초기화 함수 구현

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,14 +1,20 @@
 import java.util.HashMap;
 
 public class BaseballGame implements Game {
+    private int strike;
+    private int ball;
+
+    public BaseballGame() {
+        initJudgements();
+    }
 
     @Override
     public void gameRun() {
         gameStartMsg();
-        BaseballComputer opponent = new BaseballComputer();
+        BaseballComputer computer = new BaseballComputer();
         BaseballUser user = new BaseballUser();
+        initJudgements();
         user.userThreeIntegerInput();
-
         gameEndMsg();
     }
 
@@ -22,4 +28,8 @@ public class BaseballGame implements Game {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    private void initJudgements() {
+        strike = 0;
+        ball = 0;
+    }
 }
