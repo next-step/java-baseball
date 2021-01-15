11d7211cb7097076b88e3be0d783112b58165fac
jeongdaeun98 <uneap@sk.com>
2021-01-15 18:34:01 +0900

    feat: add askUserForSuitableNum in RequestUser

--- src/main/java/RequestUser.java
+++ src/main/java/RequestUser.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class RequestUser {
 
+    public GameInfo askUserForSuitableNum() {
+        Scanner sc = new Scanner(System.in);
+        InternalGame internalGame = new InternalGame();
+        String userNumber;
+        while (true) {
+            System.out.println(ConstantString.REQUEST_NUM_INPUT);
+            userNumber = sc.next();
+            if (internalGame.isSuitableNumber(userNumber)) {
+                break;
+            }
+        }
+        GameInfo userInfo = new GameInfo(userNumber);
+        return userInfo;
+    }
 }
