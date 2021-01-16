0e56a2fb017f739119282a2960bb2a1cd8325875
quadam36 <quadam36@gmail.com>
2021-01-17 04:10:18 +0900

    test: Add build by int methods
    - Check number is generated with input integer
    - Check exception is raised with wrong input integer

--- src/test/java/baseball/NumberTest.java
+++ src/test/java/baseball/NumberTest.java
@@ -2,6 +2,9 @@ package baseball;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +16,27 @@ class NumberTest {
     )
     void builderRandom() {
         assertDoesNotThrow(() -> new Number.Builder().Random());
+    }
+
+    @Test
+    @DisplayName("Test number is well generated with proper input")
+    void buildByInt() {
+        final int[] digits = {1, 2, 3};
+
+        Number number = assertDoesNotThrow(() -> {
+            return new Number.Builder().byInt(123);
+        });
+
+        assertArrayEquals(digits, number.getDigits());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test exception is well raised with wrong input")
+    @ValueSource(ints = {1234, 12, 112, 109})
+    void buildByIntWithException(int num) {
+        assertThrows(
+            Exception.class,
+            () -> new Number.Builder().byInt(num)
+        );
     }
 }
