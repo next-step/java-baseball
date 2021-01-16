faba5f10860fea5abc1c21ed4ad8931f46b311a6
gyumin-kim <starkying@gmail.com>
2021-01-16 11:11:03 +0900

    docs: `Input`, `KeyboardInput` javadoc 수정

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,6 +1,17 @@
 public interface Input {
 
+    /**
+     * 플레이어의 응답(입력값)을 받는다.
+     *
+     * @return 플레이어가 입력한 값
+     */
     String getInputValue();
 
+    /**
+     * 특정 메시지에 대한 플레이어의 응답(입력값)을 받는다.
+     *
+     * @param message 플레이어에게 출력할 요청 메시지
+     * @return 플레이어가 입력한 값
+     */
     String getInputValue(String message);
 }
--- src/main/java/KeyboardInput.java
+++ src/main/java/KeyboardInput.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+/**
+ * 플레이어가 키보드로 입력한 값을 받는다.
+ */
 public class KeyboardInput implements Input {
 
     private final Scanner scanner = new Scanner(System.in);
