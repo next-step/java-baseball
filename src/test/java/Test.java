24b423a1b05b91c20c138af92eab57d7dec9d996
donghee youn <qeadzc9503@naver.com>
2021-01-17 19:54:59 +0900

    feat(Test.java) : Create Test Module and test method for baseball game
     - Add void baseballGameTest() : It creates Baseball() instance.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Test {
+import com.main.baseball.dhsimpson.Baseball;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    void baseballGameTest() {
+        new Baseball();
+    }
 }
