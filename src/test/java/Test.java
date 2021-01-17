ac74e97b38233d601042e31dde652a3c60890c59
donghee youn <qeadzc9503@naver.com>
2021-01-17 20:15:47 +0900

    feat(Test.java) : Add methods that runs before, after of test
    Before : static void beforeAll()
    After : static void afterAll()

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,21 @@
 import com.main.baseball.dhsimpson.Baseball;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 public class Test {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("===테스트 시작===");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("===테스트 종료===");
+    }
+
     @org.junit.jupiter.api.Test
-    void baseballGameTest() {
+    public void baseballGameTest() {
         new Baseball();
     }
 }
