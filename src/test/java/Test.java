b3c8f6320049f65558c36a21dc16e5cf1b4b3a43
donghee youn <qeadzc9503@naver.com>
2021-01-17 21:03:22 +0900

    feat(Test.java) : Add Unit Test
    Add : testCheckBall (but also implemented)
    error : IDE gives me message 'no tests found for given includes'

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,6 +2,9 @@ import com.main.baseball.dhsimpson.Baseball;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 public class Test {
 
     @BeforeAll
@@ -11,11 +14,25 @@ public class Test {
 
     @AfterAll
     static void afterAll() {
-        System.out.println("===테스트 종료===");
+        System.out.println("===테스트 종===");
     }
 
+//    @org.junit.jupiter.api.Test
+//    public void baseballGameTest() {
+//        Baseball playGame = new Baseball();
+//        playGame.playGame();
+//    }
+
+    // Ball 갯수 check 하는 unit Test
     @org.junit.jupiter.api.Test
-    public void baseballGameTest() {
-        new Baseball();
+    public void testCheckBall() {
+        assertTrue(true);
+        Baseball playGame = new Baseball();
+        int[] pNum = {1,2,3};
+        int[] cNum = {3,2,1};
+        int idx = 1; // strike인 위치. 이 위치를 건너 뛰고 ball을 센다.
+        int answer = 2; //위의 경우는 2볼
+
+        assertSame(playGame.checkBall(pNum, cNum, idx), answer);
     }
 }
