d57f7529522fb66c12c5686c4bda7e0745a141b9
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 12:59:17 +0900

    feat: add generating random numbers

--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -1,2 +1,16 @@
-package domain;public class Computer {
+package domain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Computer {
+
+    public static final int MAX_NUMBER_INDEX = 3;
+
+    public List<Integer> generateRandomNumbers() {
+        List<Integer> answer = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Collections.shuffle(answer);
+        return answer.subList(0, MAX_NUMBER_INDEX);
+    }
 }
