602e51afdb7bf236b4fdcead200c7fa6f1b08d7e
Sunghwan-DS <jeonsung02@gmail.com>
2021-02-16 16:10:42 +0900

    feat: Add getLastInningTest
    - 마지막 기록 반환 테스트

--- src/test/java/BaseballGame/domain/BaseballRecordTest.java
+++ src/test/java/BaseballGame/domain/BaseballRecordTest.java
@@ -9,7 +9,7 @@ public class BaseballRecordTest {
 
     @DisplayName("이닝 기록 테스트")
     @Test
-    void recordInningResult() {
+    void recordInningResultTest() {
         // given
         BaseballRecord baseballRecord = new BaseballRecord();
         Inning inning1 = new Inning(123, 0, 1);
@@ -21,5 +21,21 @@ public class BaseballRecordTest {
 
         // then
         assertThat(baseballRecord.getChart()).hasSize(2);
+    }
+
+    @DisplayName("마지막 기록 반환 테스트")
+    @Test
+    void getLastInningTest() {
+        // given
+        BaseballRecord baseballRecord = new BaseballRecord();
+        Inning inning1 = new Inning(123, 0, 1);
+        Inning inning2 = new Inning(456, 0, 1);
+
+        // when
+        baseballRecord.recordInningResult(inning1);
+        baseballRecord.recordInningResult(inning2);
+
+        // then
+        assertThat(baseballRecord.getLastInning()).isEqualTo(inning2);
     }
 }
