0fea02e7c1a938e4d7ef02229ab71cb1b70f5228
hakzzang <mbh930312@naver.com>
2021-02-16 14:03:31 +0900

    feat : AnswerNumbersTest에 인덱스로 접근하는 테스트 추가

--- src/test/java/test/AnswerNumbersTest.java
+++ src/test/java/test/AnswerNumbersTest.java
@@ -7,16 +7,39 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AnswerNumbersTest {
     @Test
-    @DisplayName("AnswerNumbers를 만들었을 때, 값을 확인하는 테스")
+    @DisplayName("AnswerNumbers를 만들었을 때, 값을 확인하는 테스트")
     void getAnswerNumbersTest() {
         final LinearStrategy linearStrategy = new LinearStrategy();
         final AnswerNumbers answerNumbers = new AnswerNumbers(linearStrategy);
+        final List<Integer> answerNumberList = answerNumbers.getAnswerNumbers();
+        assertThat(answerNumberList).isEqualTo(new ArrayList<>(Arrays.asList(1, 2, 3)));
+    }
 
-        assertThat(answerNumbers.getAnswerNumbers()).isEqualTo(new ArrayList<>(Arrays.asList(1, 2, 3)));
+    @Test
+    @DisplayName("포지션으로 접근할 때, 값을 확인하는 테스트")
+    void getAnswerNumberTest() {
+        final LinearStrategy linearStrategy = new LinearStrategy();
+        final AnswerNumbers answerNumbers = new AnswerNumbers(linearStrategy);
+
+        assertThat(answerNumbers.getAnswerNumber(0)).isEqualTo(1);
+        assertThat(answerNumbers.getAnswerNumber(1)).isEqualTo(2);
+        assertThat(answerNumbers.getAnswerNumber(2)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("잘못된 포지션으로 접근시에 에러 확인 테스트")
+    void getAnswerNumberErrorTest() {
+        final LinearStrategy linearStrategy = new LinearStrategy();
+        final AnswerNumbers answerNumbers = new AnswerNumbers(linearStrategy);
+
+        assertThrows(IllegalArgumentException.class, () -> answerNumbers.getAnswerNumber(4));
+
     }
 }
