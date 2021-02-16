e6912183abd2b6597648643f21a268d2965b2b4d
munkyung <munkyung0324@gmail.com>
2021-02-16 15:01:25 +0900

    refactor: wrapping error strings

--- src/main/java/util/ValidationUtil.java
+++ src/main/java/util/ValidationUtil.java
@@ -4,6 +4,11 @@ import static controller.BaseballController.NUMBER_SIZE;
 
 public class ValidationUtil {
 
+    public static final String ERROR_WRONG_INPUT_SIZE = "잘못 입력하셨습니다. 3자리수를 입력해야합니다. 다시 입력해주세요.";
+    public static final String ERROR_WRONG_INPUT_NUMBER = "잘못 입력하셨습니다. 숫자가 아닙니다. 다시 입력해주세요.";
+    public static final String ERROR_WRONG_INPUT_DISTINCT = "잘못 입력하셨습니다. 각 자리수 숫자는 달라야 합니다. 다시 입력해주세요.";
+    public static final String ERROR_WRONG_INPUT_REPLAY = "잘못 입력하셨습니다. 1 또는 2 를 입력해주세요.";
+
     private ValidationUtil() {
     }
 
@@ -19,7 +24,7 @@ public class ValidationUtil {
 
     private static void validateInputSize(int length) {
         if (length != NUMBER_SIZE) {
-            throw new IllegalArgumentException("잘못 입력하셨습니다. 3자리수를 입력해야합니다. 다시 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_WRONG_INPUT_SIZE);
         }
     }
 
@@ -27,7 +32,7 @@ public class ValidationUtil {
         for (char c : userInput) {
             int tmp = c - '0';
             if (tmp > 9 || tmp < 1) {
-                throw new IllegalArgumentException("잘못 입력하셨습니다. 숫자가 아닙니다. 다시 입력해주세요.");
+                throw new IllegalArgumentException(ERROR_WRONG_INPUT_NUMBER);
             }
         }
     }
@@ -39,18 +44,15 @@ public class ValidationUtil {
             if (!checkDuplication[tmp]) {
                 checkDuplication[tmp] = true;
             } else {
-                throw new IllegalArgumentException("잘못 입력하셨습니다. 각 자리수 숫자는 달라야 합니다. 다시 입력해주세요.");
+                throw new IllegalArgumentException(ERROR_WRONG_INPUT_DISTINCT);
             }
         }
     }
 
     private static void validateReplayInputType(String userInput) {
-        if (userInput.length() != 1) {
-            throw new IllegalArgumentException("잘못 입력하셨습니다. 1 또는 2 를 입력해주세요.");
-        }
         int tmp = userInput.charAt(0) - '0';
         if (tmp != 1 && tmp != 2) {
-            throw new IllegalArgumentException("잘못 입력하셨습니다. 1 또는 2 를 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_WRONG_INPUT_REPLAY);
         }
     }
 
