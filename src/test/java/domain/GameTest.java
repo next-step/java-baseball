35bff46284d1c235289d19c82a9bbfda2e9b93d0
hanseokhyeon <sah0322@naver.com>
2021-02-16 14:55:12 +0900

    test: Game 클래스 test 코드 추가

---
+++ src/test/java/domain/GameTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    void setFinish() {
+        Game game = new Game();
+
+        game.setFinish(new MenuNumber("1"));
+        Assertions.assertThat(game.isRunning()).isFalse();
+
+        game.setFinish(new MenuNumber("2"));
+        Assertions.assertThat(game.isRunning()).isTrue();
+    }
+}
