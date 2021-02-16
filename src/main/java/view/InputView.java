de2f9abcbece6336ffc5782394f8cbf60804a673
d36choi <yuio1126@gmail.com>
2021-02-16 15:53:24 +0900

    feat: Add parseToInt for get RestartInput with Integer.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String GET_PLAYERS_NUMBERS_MESSAGE = "숫자를 입력해 주세요 : ";
-    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final Scanner scanner = new Scanner(System.in);
 
     public InputView() {
@@ -19,6 +19,14 @@ public class InputView {
 
     public int getRestartInput() {
         System.out.println(RESTART_MESSAGE);
-        return scanner.nextInt();
+        return parseToInt(scanner.nextLine());
+    }
+
+    public int parseToInt(String input) throws IllegalArgumentException{
+        int userInput = Integer.parseInt(input);
+        if(userInput != 1 && userInput != 2) {
+            throw new IllegalArgumentException("1 or 2를 입력해주세요.");
+        }
+        return userInput;
     }
 }
