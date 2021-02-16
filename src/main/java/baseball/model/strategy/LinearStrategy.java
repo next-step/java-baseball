ab82d4af11b20def866b751c16f321fd1b465283
hakzzang <mbh930312@naver.com>
2021-02-16 14:01:56 +0900

    refactoring : 생성자에서 사용하는 함수 메소드로 분리 및 LinearStrategy 구조 추가

--- src/main/java/baseball/model/strategy/LinearStrategy.java
+++ src/main/java/baseball/model/strategy/LinearStrategy.java
@@ -1,16 +1,44 @@
 package baseball.model.strategy;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LinearStrategy implements AnswerStrategy {
+    private static final String EMPTY_ERROR_MESSAGE = "AnswerNumber는 %d 개를 초과하여 호출할 수 없습니다.";
     private static final int ANSWER_SIZE = 3;
-    private int answer = 1;
+    private static final int START_NUMBER = 1;
+
+    private final Stack<Integer> answers = new Stack<>();
+
+    public LinearStrategy() {
+        addAnswers();
+    }
 
     @Override
     public int pop() {
-        return answer++;
+        if (answers.size() == 0) {
+            throw new IllegalStateException(String.format(EMPTY_ERROR_MESSAGE, ANSWER_SIZE));
+        }
+        return answers.pop();
     }
 
     @Override
     public int size() {
         return ANSWER_SIZE;
+    }
+
+    private void addAnswers() {
+        List<Integer> tempAnswers = IntStream
+                .range(START_NUMBER, START_NUMBER + ANSWER_SIZE)
+                .boxed()
+                .collect(Collectors.toList());
+
+        //1,2,3...으로 스택을 꺼내기 위해
+        Collections.reverse(tempAnswers);
+
+        answers.addAll(tempAnswers);
     }
 }
--- src/main/java/baseball/model/strategy/RandomStrategy.java
+++ src/main/java/baseball/model/strategy/RandomStrategy.java
@@ -10,25 +10,19 @@ public class RandomStrategy implements AnswerStrategy {
     private static final int START_NUMBER = 1;
     private static final int ANSWER_BOUNDARY = 9;
 
-    private static final String EMPTY_ERROR_MESSAGE = String
-            .format("AnswerNumber는 %d 개를 초과하여 호출할 수 없습니다.", ANSWER_SIZE);
+    private static final String EMPTY_ERROR_MESSAGE = "AnswerNumber는 %d 개를 초과하여 호출할 수 없습니다.";
 
     private final Random random = new Random();
     private final Stack<Integer> answers = new Stack<>();
 
     public RandomStrategy() {
-        final Set<Integer> baseballNumberSet = new HashSet<>();
-        while (baseballNumberSet.size() < ANSWER_SIZE) {
-            final int randomNumber = START_NUMBER + random.nextInt(ANSWER_BOUNDARY);
-            baseballNumberSet.add(randomNumber);
-        }
-        answers.addAll(baseballNumberSet);
+        addAnswers();
     }
 
     @Override
     public int pop() {
         if (answers.size() == 0) {
-            throw new IllegalStateException(EMPTY_ERROR_MESSAGE);
+            throw new IllegalStateException(String.format(EMPTY_ERROR_MESSAGE, ANSWER_SIZE));
         }
         return answers.pop();
     }
@@ -36,5 +30,14 @@ public class RandomStrategy implements AnswerStrategy {
     @Override
     public int size() {
         return ANSWER_SIZE;
+    }
+
+    private void addAnswers() {
+        final Set<Integer> baseballNumberSet = new HashSet<>();
+        while (baseballNumberSet.size() < ANSWER_SIZE) {
+            final int randomNumber = START_NUMBER + random.nextInt(ANSWER_BOUNDARY);
+            baseballNumberSet.add(randomNumber);
+        }
+        answers.addAll(baseballNumberSet);
     }
 }
