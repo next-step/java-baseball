399bc6fdabe2c6d0acf7cc3712e95c1656f0b20d
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 16:15:30 +0900

    feat: add main

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class BaseballGame {
+import controller.BaseballGameController;
+
+public class BaseballGame {
+    public static void main(String[] args) {
+        BaseballGameController baseballGameController = new BaseballGameController();
+        baseballGameController.play();
+    }
 }
