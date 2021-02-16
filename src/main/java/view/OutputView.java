00a4a216896ec8543db82b56f1bef470d2954024
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 16:21:21 +0900

    feat: add error message

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -11,4 +11,8 @@ public class OutputView {
     public void printComplete() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public void printError() {
+        System.out.println("입력값을 확인하세요!");
+    }
 }
