129feb3a3d1083716b36e2776282eb0c9d7fe610
hakzzang <mbh930312@naver.com>
2021-02-16 15:38:59 +0900

    refactoring : shot에 메시지를 보여지도록 로직 수정

--- src/main/java/baseball/model/shot/Shot.java
+++ src/main/java/baseball/model/shot/Shot.java
@@ -1,5 +1,16 @@
 package baseball.model.shot;
 
 public enum Shot {
-    OUT, BALL, STRIKE;
+    OUT(""), BALL("볼"), STRIKE("스트라이크"), NOTHING("낫싱");
+
+    private final String message;
+
+    Shot(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
 }
