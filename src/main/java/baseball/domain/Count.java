78779b2e8e602d8fe937c2361c5454ee7eddfd32
donghee youn <qeadzc9503@naver.com>
2021-02-16 15:08:00 +0900

    feat : Count 래퍼 클래스 increase 메서드에 예외 처리

--- src/main/java/baseball/domain/Count.java
+++ src/main/java/baseball/domain/Count.java
@@ -1,10 +1,11 @@
 package baseball.domain;
 
 public class Count {
+    public static final int ZERO = 0;
     private int count;
 
     public Count(){
-        count = 0;
+        count = ZERO;
     }
 
     public int getCount(){
@@ -12,6 +13,9 @@ public class Count {
     }
 
     public void increase(int increaseCount) {
+        if(increaseCount < ZERO){
+            throw new IllegalArgumentException("값 증가에 음수는 허용되지 않습니다.");
+        }
         count += increaseCount;
     }
 }
