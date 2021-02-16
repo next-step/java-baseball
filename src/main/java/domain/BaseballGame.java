98ff67fca8b385729e0fbb57173417fceedafc36
Sunghwan-DS <jeonsung02@gmail.com>
2021-02-16 14:30:31 +0900

    fix: Change data type
    - 원활한 비교를 위해 ArrayList<Integer>를 이용하여 로직을 구현

--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -1,18 +1,34 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class BaseballGame {
     private static final BaseballRecord baseballRecord = new BaseballRecord();
     private final int answer;
+    private final ArrayList<Integer> answerGroup;
 
     public BaseballGame(int answer) {
         this.answer = answer;
+        this.answerGroup = makeGroup(this.answer);
     }
 
     public void playInning(int tryNumber) {
-//        int strike = checkStrike(answer, tryNumber);
-//        int ball = checkBall(answer, tryNumber);
-        int strike = 1;
-        int ball = 1;
+        ArrayList<Integer> tryNumberGroup = makeGroup(tryNumber);
+
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tryNumberGroup.get(i) == this.answerGroup.get(j)) {
+                    if (i == j) {
+                        strike ++;
+                    } else {
+                        ball ++;
+                    }
+                }
+            }
+        }
 
         Inning inning = new Inning(tryNumber, strike, ball);
         baseballRecord.recordInningResult(inning);
@@ -20,5 +36,16 @@ public class BaseballGame {
 
     public BaseballRecord getBaseballRecord() {
         return this.baseballRecord;
+    }
+
+    public ArrayList<Integer> makeGroup(int value) {
+        ArrayList<Integer> valueGroup = new ArrayList<>();
+        valueGroup.add(value / 100);
+        value %= 100;
+        valueGroup.add(value / 10);
+        value %= 10;
+        valueGroup.add(value);
+
+        return valueGroup;
     }
 }
