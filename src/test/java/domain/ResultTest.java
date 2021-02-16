d2c78df2fc167485efde75a9b1c682dd7060e091
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 16:17:00 +0900

    test: add test for result string

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -1,4 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
 class ResultTest {
-  
+
+    @DisplayName("결과 값을 잘 가지고 오는지 확인한다.")
+    @ParameterizedTest
+    @MethodSource("providerGetResultTestParam")
+    void getResultTest(Result result, int count, String expected) {
+        assertThat(result.getResult(count)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> providerGetResultTestParam() {
+        return Stream.of(
+                Arguments.of(Result.STRIKE, 1, "1스트라이크"),
+                Arguments.of(Result.STRIKE, 0, "")
+        );
+    }
+
+
 }
