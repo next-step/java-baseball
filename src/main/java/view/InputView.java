d1c12212f3ca135f740055f3270a45d58eb175a5
d36choi <yuio1126@gmail.com>
2021-02-16 15:30:07 +0900

    feat: Add getRestartInput

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String GET_PLAYERS_NUMBERS_MESSAGE = "숫자를 입력해 주세요 : ";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
     private final Scanner scanner = new Scanner(System.in);
 
     public InputView() {
     }
 
     public String getPlayersNumbers() {
-        System.out.println(GET_PLAYERS_NUMBERS_MESSAGE);
+        System.out.print(GET_PLAYERS_NUMBERS_MESSAGE);
         return scanner.nextLine();
 
+    }
+
+    public int getRestartInput() {
+        System.out.println(RESTART_MESSAGE);
+        return scanner.nextInt();
     }
 }
