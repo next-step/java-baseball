3a74a1d780e914b3a4df7cfd03ae32c230df2f5f
hanseokhyeon <sah0322@naver.com>
2021-02-16 15:52:31 +0900

    test: UserNumberTest에 splitNumber 테스트 추가

--- src/test/java/domain/UserNumberTest.java
+++ src/test/java/domain/UserNumberTest.java
@@ -3,9 +3,23 @@ package domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class UserNumberTest {
     @Test
-    void validateNumberTest() {
+    void splitNumber() {
+        UserNumber userNumber = new UserNumber("123");
+        List<BaseballNumber> numbers = userNumber.getNumbers();
+
+        List<BaseballNumber> targets = Arrays.asList(
+                new BaseballNumber(1), new BaseballNumber(2), new BaseballNumber(3));
+
+        Assertions.assertThat(numbers).isEqualTo(targets);
+    }
+
+    @Test
+    void validateNumber() {
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
             new UserNumber("113");
         });
