d00884deed1714786c3fcab8442f2d8a778d5e35
KimBB <tree9295@gmail.com>
2021-02-16 16:23:56 +0900

    refactor: add strike, ball increase methods in BaseballStatus

--- src/main/java/domain/BaseballStatus.java
+++ src/main/java/domain/BaseballStatus.java
@@ -1,13 +1,17 @@
 package domain;
 
 public class BaseballStatus {
-    private final int strikeCount;
-    private final int ballCount;
+    private int strikeCount;
+    private int ballCount;
     private static final int JACKPOT_COUNT = 3;
     private static final String STRIKE_MESSAGE = "%d스트라이크";
     private static final String BALL_MESSAGE = "%d볼";
     private static final String NOTHING_MESSAGE = "낫싱";
 
+    public BaseballStatus() {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+    }
 
     public BaseballStatus(int strikeCount, int ballCount) {
         this.strikeCount = strikeCount;
@@ -20,6 +24,14 @@ public class BaseballStatus {
 
     public int getBallCount() {
         return ballCount;
+    }
+
+    public void increaseStrikeCount(){
+        strikeCount++;
+    }
+
+    public void increaseBallCount(){
+        ballCount++;
     }
 
     public boolean isJackpot() {
