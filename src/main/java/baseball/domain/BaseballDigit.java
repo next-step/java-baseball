e4c312c0e7df7a2b1828289bf743ec6c4d950716
koeonyack <covenant.ko@kakao.com>
2021-02-16 15:41:12 +0900

    feat: BaseballDigit 구현
    - List로 숫자 관리
    - 숫자 범위 관리
    - 문자열 에러 처리

---
+++ src/main/java/baseball/domain/BaseballDigit.java
@@ -0,0 +1,43 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballDigit {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final String OUT_OF_RANGE_EXCEPTION = "잘못된 숫자입니다.";
+
+    private static final List<Integer> DIGITS = new ArrayList<>();
+
+    public BaseballDigit(String numbers) {
+        addBallNumber(numbers);
+    }
+
+    public void addBallNumber(String numbers) {
+        String[] divideNumbers = numbers.split("");
+        for (String number : divideNumbers) {
+            int ballNumber = convertStringToInt(number);
+            validateNumberRange(ballNumber);
+            DIGITS.add(ballNumber);
+        }
+    }
+
+    public static List<Integer> getBaseballValues() {
+        return DIGITS;
+    }
+
+    private int convertStringToInt(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+        }
+        return Integer.parseInt(number);
+    }
+
+    private void validateNumberRange(int number) {
+        if (number > MAX_NUMBER || number < MIN_NUMBER) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_EXCEPTION);
+        }
+    }
+}
---
+++ src/test/java/baseball/domain/BaseballDigitTest.java
@@ -0,0 +1,21 @@
+package baseball.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BaseballDigitTest {
+    @Test
+    @DisplayName("숫자 생성 테스트")
+    public void baseballDigitTest() throws Exception {
+        BaseballDigit baseballDigit = new BaseballDigit("123");
+        List<Integer> DIGITS = new ArrayList<>();
+        DIGITS.add(1);
+        DIGITS.add(2);
+        DIGITS.add(3);
+        Assertions.assertThat(BaseballDigit.getBaseballValues().toString()).isEqualTo(DIGITS.toString());
+    }
+}
