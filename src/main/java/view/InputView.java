566bf7366e0ddae1c27034e6836f02dbd0962db0
munkyung <munkyung0324@gmail.com>
2021-02-16 14:32:23 +0900

    feat: immigrate PrintUtil method to InputVIew and OutputView

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,40 @@
 package view;
 
+import domain.Number;
+import domain.NumberSet;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import util.ValidationUtil;
+
 public class InputView {
+
+    public static final String INFO_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String INFO_INPUT_REPLAY_OPTION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static NumberSet getPlayerInput() {
+        System.out.print(INFO_INPUT_NUMBER);
+        char[] userInput = scanner.next().toCharArray();
+        try {
+            ValidationUtil.validateUserInput(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return new NumberSet(new String(userInput).chars()
+            .map(input -> input - '0')
+            .mapToObj(Number::new)
+            .collect(Collectors.toList()));
+    }
+
+    public static int getPlayerReplayInput() {
+        System.out.println(INFO_INPUT_REPLAY_OPTION);
+        String userInput = scanner.next();
+        try {
+            ValidationUtil.validateUserReplayInput(userInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return Integer.parseInt(userInput);
+    }
 
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,4 +2,40 @@ package view;
 
 public class OutputView {
 
+    public static final String INFO_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String HINT_NOTING = "낫싱";
+    public static final String HINT_BALL = "볼";
+    public static final String HINT_STRIKE = "스트라이크";
+
+    public static void checkGameStatus(int ballCnt, int strikeCnt) {
+        if (ballCnt == 0 && strikeCnt == 0) {
+            printNothing();
+        } else if (ballCnt == 0) {
+            printStrikeHint(strikeCnt);
+        } else if (strikeCnt == 0) {
+            printBallHint(ballCnt);
+        } else {
+            printBallAndStrike(ballCnt, strikeCnt);
+        }
+    }
+
+    public static void printGameOver() {
+        System.out.println(INFO_GAME_OVER);
+    }
+
+    public static void printNothing() {
+        System.out.println(HINT_NOTING);
+    }
+
+    public static void printBallHint(int ballCnt) {
+        System.out.println(ballCnt + HINT_BALL);
+    }
+
+    public static void printStrikeHint(int strikeCnt) {
+        System.out.println(strikeCnt + HINT_STRIKE);
+    }
+
+    public static void printBallAndStrike(int ballCnt, int strikeCnt) {
+        System.out.println(ballCnt + HINT_BALL + " " + strikeCnt + HINT_STRIKE);
+    }
 }
