0d714e681c080a9d291de863331d0363fe0c3d4c
Gyumin Kim <starkying@gmail.com>
2021-02-16 14:20:34 +0900

    feat: 플레이어에게 입력 받는 기능

---
+++ src/main/java/input/InputView.java
@@ -0,0 +1,13 @@
+package input;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getNumberInput() {
+        System.out.println("숫자를 입력해 주세요 : ");
+        return scanner.nextLine();
+    }
+}
