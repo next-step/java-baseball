106e941873a0dc20bdb6c080946cb361534609ea
Sunghwan-DS <jeonsung02@gmail.com>
2021-02-16 13:24:38 +0900

    feat: Create BaseballRecord, BaseballRecordTest
    - 한 이닝 기록에 해당하는 Inning을 담을 일급컬렉션 BaseballRecord 구현
    - BaseballRecord에 한 이닝을 기록하는 함수 및 테스트 구현

---
+++ src/main/java/domain/BaseballRecord.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballRecord {
+    private static final List<Inning> chart = new ArrayList<>();
+
+    public void recordInningResult(Inning inning) {
+        this.chart.add(inning);
+    }
+
+    public List<Inning> getChart() {
+        return this.chart;
+    }
+}
---
+++ src/test/java/domain/BaseballRecordTest.java
@@ -0,0 +1,25 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseballRecordTest {
+
+    @DisplayName("이닝 기록 테스트")
+    @Test
+    void recordInningResult() {
+        // given
+        BaseballRecord baseballRecord = new BaseballRecord();
+        Inning inning1 = new Inning(123, 0, 1);
+        Inning inning2 = new Inning(456, 0, 1);
+
+        // when
+        baseballRecord.recordInningResult(inning1);
+        baseballRecord.recordInningResult(inning2);
+
+        // then
+        assertThat(baseballRecord.getChart()).hasSize(2);
+    }
+}
