3061288e7e0b2b4d7d2dc10647e04f4d67a1dfb6
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 16:15:02 +0900

    feat: add views

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,2 +1,18 @@
-package view;public class InputView {
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getGuessNumbers() {
+        System.out.print("숫자를 입력해 주세요: ");
+        return scanner.nextLine();
+    }
+
+    public boolean askForRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return scanner.nextLine().equalsIgnoreCase("1");
+    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,2 +1,18 @@
-package view;public class OutputView {
+package view;
+
+import domain.BallCount;
+
+public class OutputView {
+
+    public void printResult(BallCount ballCount) {
+        System.out.println(ballCount.toString());
+    }
+
+    public void printComplete() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void printError() {
+        System.out.println("입력값을 확인하세요!");
+    }
 }
