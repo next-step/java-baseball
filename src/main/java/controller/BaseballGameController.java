34fd37445ba58304bc41f0a2fe05322da21b2afe
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 16:14:47 +0900

    feat: add basball game controller

--- src/main/java/controller/BaseballGameController.java
+++ src/main/java/controller/BaseballGameController.java
@@ -1,2 +1,31 @@
-package controller;public class BaseballGameController {
+package controller;
+
+import domain.BallCount;
+import domain.BallNumbers;
+import domain.Computer;
+import view.InputView;
+import view.OutputView;
+
+public class BaseballGameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private Boolean START = true;
+
+    public void play() {
+        while (START) {
+            Computer computer = new Computer();
+            BallNumbers answerNumbers = computer.generateRandomNumbers();
+            doGuess(new BallCount(), answerNumbers);
+            outputView.printComplete();
+            START = inputView.askForRestart();
+        }
+    }
+
+    public void doGuess(BallCount ballCount, BallNumbers answerNumbers) {
+        final int DONE = 3;
+        while (!ballCount.isDone(DONE)) {
+            BallNumbers guessNumbers = BallNumbers.of(inputView.getGuessNumbers());
+            outputView.printResult(answerNumbers.match(ballCount, guessNumbers));
+        }
+    }
 }
