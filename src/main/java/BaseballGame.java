7a3c93774e0005f1f834e57c963abaae402567cd
JuheonLee <zhd401@naver.com>
2021-04-18 22:09:23 +0900

    정답 비교 및 게임 셋
    입력된 3개의 수와 정답 중 하나씩 checkNum을 통해서 비교하고 자리도 같고
    수도 같으면 strike에 1을 더하고 수만 같다면 ball에 1을 더한다.
    checkAns에서 이를 3번 반복하여 총 ball과 strike 수를 구하여 출력하고
    만일 둘 다 0이라면 낫싱을 출력한다. 그리고 strike가 3이라면 정답이므로
    맞췄음을 알린다. 두번째 반복문에선 정답을 맞췄는지 판단하고 첫번째
    반복문은 게임의 재시작을 판단한다.

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -14,9 +14,21 @@ public class BaseballGame {
 			char[] ansArr = selectRandNum();
 			//System.out.println(ans[0] + " " + ans[1] + " " + ans[2]);
 			while(!correct) {
-				System.out.println("숫자를 입력해주세요.: ");
+				System.out.print("숫자를 입력해주세요.: ");
 				String input  = scanner.next();
 				char[] inputArr = input.toCharArray();
+			
+				correct = checkAns(inputArr, ansArr);
+			}
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+			int startQuit = scanner.nextInt();
+			
+			if(startQuit == 1) {
+				correct = false;
+			}
+			else{
+				scanner.close();
+				setGame = false;
 			}
 		}
 	}
@@ -48,4 +60,51 @@ public class BaseballGame {
 		return choice;
 	}
 
+	public static boolean checkAns(char[] inputArr, char[] ansArr) { //3개의 입력과 3개의 답 비교
+		int[][] hint = new int[3][2];
+		int ball = 0;
+		int strike = 0;
+		
+		for(int i = 0; i < ansArr.length; i++)  {
+			hint[i] = checkNum(inputArr, ansArr[i], i);
+			if(hint[i][0] == 1) {
+				strike+=1;
+			}
+			else if(hint[i][1] == 1) {
+				ball+=1;
+			}
+		}
+		
+		if(ball > 0) {
+			System.out.print(ball + "볼 ");
+		}
+		if(strike > 0) {
+			System.out.print(strike + "스트라이크");
+		}
+		if(ball == 0 & strike == 0) {
+			System.out.print("낫싱");
+		}
+		System.out.println();
+		
+		if(strike == 3) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static int[] checkNum(char[] inputArr, char ansChar, int ansIndex ) { //정답의 하나를 3개의 입력과 비교
+		int[] strikeBall = new int[2];
+		for(int i = 0; i < inputArr.length; i++) {
+			if(i == ansIndex && inputArr[i] == ansChar) {
+				strikeBall[0] = 1;
+			}
+			else if(i != ansIndex && inputArr[i] == ansChar) {
+				strikeBall[1] = 1;
+			}
+		}
+		
+		return strikeBall;
+	}
 }
