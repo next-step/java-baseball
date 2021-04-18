8e2f44e5c439d43fddce83bcd17f19889d4176a1
정진우 <jwjung@upchain.kr>
2021-04-18 14:40:21 +0900

    feat: 기능 추가
    "숫자를 입력해주세요 : (입력 값)" 문구를
    반복해서 출력하는 기능 추가

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,6 +1,10 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class BaseballGame {
 
     private static String answer;
+    private static int restart = 1;
 
     static void generateRandomNumber() {
         int randomNumber = (int)(Math.random()*1000);
@@ -8,7 +12,29 @@ public class BaseballGame {
         answer = stringNumber;
     }
 
+    static void start() {
+        Scanner scan = new Scanner(System.in);
+        int input;
+        String[] splittedAnswer = answer.split("");
+        String[] splittedInput;
+
+        while (restart == 1) {
+            System.out.print("숫자를 입력해주세요 : ");
+
+            try {
+                input = scan.nextInt();
+                splittedInput = String.valueOf(input).split("");
+
+                // compare answer with input
+            } catch (Exception e) {
+                System.out.println("예기치 않은 오류로 종료합니다.");
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         generateRandomNumber();
+        start();
     }
 }
