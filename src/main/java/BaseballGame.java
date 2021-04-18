e7b7477d34819de9a5017f9e383ce03466ea4ff1
정진우 <jwjung@upchain.kr>
2021-04-18 23:03:04 +0900

    feat: 기능 추가
    (입력 값)과 정답을 비교하여
    스트라이크, 볼, 낫싱 결과 출력하기

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,4 +1,3 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BaseballGame {
@@ -12,11 +11,66 @@ public class BaseballGame {
         answer = stringNumber;
     }
 
+    static String compareNumbers(String inputNumber, String answerNumber, int inputIndex, int answerIndex) {
+        String result = "";
+
+        if ((inputIndex == answerIndex) && (inputNumber.equals(answerNumber))) {
+            result = "s";
+        } else if (inputNumber.equals(answerNumber)) {
+            result = "b";
+        }
+
+        return result;
+    }
+
+    static boolean compare(String[] splittedAnswer, String[] splittedInput) {
+        String result = "";
+        int ballCount = 0;
+        int strikeCount = 0;
+        String[] splittedResult;
+
+        for (int i = 0; i < splittedInput.length; i++) {
+            for (int j = 0; j < splittedAnswer.length; j++) {
+                result += compareNumbers(splittedInput[i], splittedAnswer[j], i, j);
+            }
+        }
+
+        splittedResult = result.split("");
+
+        for (String status:
+             splittedResult) {
+            if (status.equals("b")) {
+                ballCount += 1;
+            } else if (status.equals("s")) {
+                strikeCount += 1;
+            }
+        }
+
+        if (ballCount > 0 && strikeCount > 0) {
+            result = ballCount+"볼 "+strikeCount+"스트라이크";
+        } else if (ballCount > 0) {
+            result = ballCount+"볼";
+        } else if (strikeCount > 0){
+            result = strikeCount+"스트라이크";
+        } else {
+            result = "낫싱";
+        }
+
+        System.out.println(result);
+
+        if (strikeCount == 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     static void start() {
         Scanner scan = new Scanner(System.in);
         int input;
         String[] splittedAnswer = answer.split("");
         String[] splittedInput;
+        boolean isEnd = false;
 
         while (restart == 1) {
             System.out.print("숫자를 입력해주세요 : ");
@@ -26,6 +80,7 @@ public class BaseballGame {
                 splittedInput = String.valueOf(input).split("");
 
                 // compare answer with input
+                isEnd = compare(splittedAnswer, splittedInput);
             } catch (Exception e) {
                 System.out.println("예기치 않은 오류로 종료합니다.");
                 break;
