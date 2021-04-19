353be2fb5ed5f948fe63401d4dfdcc976d6cc1fb
yhback <yhback@upchain.kr>
2021-04-19 16:25:32 +0900

    feat : 스트라이크 볼 체크 숫자 입력시 스트라이크 볼 표시 다 맞을 경우 종료멘트후 1,2 선택후 게임종료 or시작

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -3,45 +3,40 @@ package baseball;
 import java.util.Scanner;
 
 public class Baseball {
+        int strike = 0;
+        int ball = 0;
+        int count=0;
+    boolean resulet=true;
     public String numberInput(){
         System.out.println("숫자를 꼭 3개만 입력해주세요! ex)123  592  230");
         Scanner sc = new Scanner(System.in);
         String num = sc.nextLine();
-        System.out.println(num);
         String checkNum = numberCheck(num);
 
         return checkNum;
     }
     public String numberCheck(String num){
+        boolean isNumeric = false;
         num = num.replaceAll(" " , "");
-        num.charAt(0);
         if(num.length() !=3){
             System.out.println("길이는 무적건 3글자만 가능합니다.");
             numberInput();
         }
-        for(int i=1; i<=9; i++){
-            if( 0> num.charAt(0) && num.charAt(0) >10){
-                System.out.println("숫자만 입력해주세요!!!");
-                numberInput();
+        for (int i = 0; i < num.length(); i++) {
+            if (!Character.isDigit(num.charAt(i))) {
+                isNumeric = true;
             }
         }
-        for(int i=1; i<=9; i++){
-            if( 0> num.charAt(1) && num.charAt(1) >10){
-                System.out.println("숫자만 입력해주세요!!!");
-                numberInput();
-            }
+        if(isNumeric){
+            System.out.println("숫자만 입력해주세요");
+            return numberInput();
+        }else{
+            return num;
         }
-        for(int i=1; i<=9; i++){
-            if( 0> num.charAt(2) && num.charAt(2) >10){
-                System.out.println("숫자만 입력해주세요!!!");
-                numberInput();
-            }
-        }
-        return num;
     }
     public String startBaseball( ){
         System.out.println("야구게임 시작!");
-        System.out.println("중복된 수가 없는 4자리 정수를 생성중입니다.(1000 ~ 9999)");
+        System.out.println("중복된 수가 없는 3자리 정수를 생성중입니다.(100 ~ 999)");
         char chc1, chc2, chc3;
         String randomNum="";
         while(true){
@@ -58,18 +53,60 @@ public class Baseball {
         return randomNum;
     }
     public void endBaseball(){
-        System.out.println("야구게임 끝!");
+        System.out.println("게임을 새로 시작할려면 1, 종료하실려면 2를 입력해주세요");
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        System.out.println(num);
+        System.out.println("++++++++++++++++");
+        if(num.equals("1")){
+            resulet=true;
+            playGoing();
+        }else{
+            System.out.println("야구게임 끝!");
+        }
     }
-    public void baseballCheck(String checkNum ,String robotNum3){
+    public void baseballCheck(String checkNum ,String robotNum){
+        int playNum1 =checkNum.charAt(0);
+        int playNum2 =checkNum.charAt(1);
+        int playNum3 =checkNum.charAt(2);
+        int[] playNum = {playNum1 , playNum2,playNum3};
+
+        int robotNum1 =robotNum.charAt(0);
+        int robotNum2 =robotNum.charAt(1);
+        int robotNum3 =robotNum.charAt(2);
+        int[] com = {robotNum1 , robotNum2,robotNum3};
+        for (int i=0; i<com.length; i++){
+            for (int j=0; j<playNum.length; j++) {
+                strikeBallUp(i , j ,playNum[i] ,com[j]);
+            }
+        }
+        count++;
+        if (strike == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다. 게임종료");
+            resulet = false;
+            count=0;
+        }else if(strike ==0 && ball ==0){
+            System.out.println( "낫싱");
+        }else {
+            System.out.println(strike + "스트라이크  " + ball + "볼" + "   횟수"+ count+ "번");
+        }
+        strike=0;
+        ball=0;
 
     }
-    public int playGoing(String robotNum3){
-
-        String checkNum=numberInput();
-        baseballCheck(checkNum , robotNum3);
-
-
-
-        return 12;
+    public void strikeBallUp(int i , int j ,int playnum , int robotNum){
+        if (playnum == robotNum && i == j){
+            strike++;
+        } else if (playnum == robotNum && i != j){
+            ball++;
+        }
+    }
+    public void playGoing(){
+        String robotNum3 = startBaseball();
+        while(resulet){
+            String checkNum=numberInput();
+            baseballCheck(checkNum , robotNum3);
+        }
+        endBaseball();
     }
 }
--- src/main/java/baseball/StartMain.java
+++ src/main/java/baseball/StartMain.java
@@ -2,19 +2,8 @@ package baseball;
 
 
 public class StartMain {
-
-//        baseball.startBaseball();
-//
-//        baseball.playGoing();
-//
-//        baseball.endBaseball();
     public static void main(String[] args) {
-
-        System.out.println("메인시작");
-        String robotNum3 = new Baseball().startBaseball();
-        new Baseball().playGoing(robotNum3);
-        new Baseball().endBaseball();
-
+        new Baseball().playGoing();
     }
 
 }
