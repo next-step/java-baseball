8fa63ba26b981bada516f7762b0b3c78c812dc2b
taemin <taminging@kakao.com>
2021-04-21 23:16:00 +0900

    feat(BallNumber) : create BallNumber custom exception class

--- src/main/java/baseball/exception/InvalidateBallNumberException.java
+++ src/main/java/baseball/exception/InvalidateBallNumberException.java
@@ -5,4 +5,16 @@ public class InvalidateBallNumberException extends RuntimeException {
     public InvalidateBallNumberException(String message) {
         super(message);
     }
+
+    public static class InvalidateBallNumberSizeError extends InvalidateBallNumberException {
+        public InvalidateBallNumberSizeError(final String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidateBallNumberValueError extends InvalidateBallNumberException {
+        public InvalidateBallNumberValueError(final String message) {
+            super(message);
+        }
+    }
 }
