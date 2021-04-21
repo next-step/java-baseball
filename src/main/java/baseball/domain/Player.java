a9c1cf21ccca3bff69be538fee065fd64a2f5066
taemin <taminging@kakao.com>
2021-04-21 23:38:32 +0900

    feat(Player) create Player

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+public class Player {
+
+    private BallNumber ballNumber;
+
+    public BallNumber getBallNumber() {
+        return this.ballNumber;
+    }
+
+    public void setBallNumber(BallNumber ballNumber) {
+        this.ballNumber = ballNumber;
+    }
+}
