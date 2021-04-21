7e23528b2948a6d0acbe51fddbe5fc46c33e95f6
taemin <taminging@kakao.com>
2021-04-21 23:58:45 +0900

    test(ComputerTest) : create ComputerTest and initComputerTest method

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -1,2 +1,20 @@
-package baseball.domain;public class ComputerTest {
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Computer Test")
+public class ComputerTest {
+
+    @DisplayName("init Computer Test")
+    @Test
+    public void initComputerTest(){
+        Computer computer = new Computer();
+        assertNotNull(computer);
+        assertNotNull(computer.getBallNumber());
+    }
+
 }
