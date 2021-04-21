da0c7412b0a39dc9bd471bbe1c013b3592deabf9
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-22 07:59:02 +0900

    test : StringTest 요구사항 1 - split 구현

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    @DisplayName("split 처리 정상")
+    void split() {
+        // given
+        String target = "1,2";
+
+        // when
+        String[] splitTarget = target.split(",");
+
+        // then
+        assertThat(2).isEqualTo(splitTarget.length);
+        assertThat(splitTarget).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("구분자가 아닌 문자로 split 했을 때 정상처리")
+    void split_withoutDelimiter() {
+        // given
+        String target = "1";
+
+        // when
+        String[] splitTarget = target.split(",");
+
+        // then
+        assertThat(1).isEqualTo(splitTarget.length);
+        assertThat(splitTarget).containsExactly("1");
+    }
+}
