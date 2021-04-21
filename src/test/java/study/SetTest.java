ebb6de4151bd66a51908c53600408c9d4f6a05db
ianjang <ianjang0718@gmail.com>
2021-04-22 00:16:17 +0900

    test: set collection requirement #3 - contains with csvSourse

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -31,5 +32,12 @@ public class SetTest {
 	@ValueSource(ints = {1, 2, 3})
 	void contains(int number) {
 		assertThat(numbers.contains(number)).isTrue();
+
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void contains2(int number, boolean expected) {
+		assertThat(numbers.contains(number)).isEqualTo(expected);
 	}
 }
