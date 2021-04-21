313e34880c0a4cd035b7e3023a553c4f1705cf0f
catsbi <killfine198@gmail.com>
2021-04-21 16:32:47 +0900

    feat: 학습 테스트 요구사항 테스트 케이스 작성

---
+++ src/test/java/study/StringTest.java
@@ -0,0 +1,33 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("String 클래스에 대한 기능 테스트")
+public class StringTest {
+
+    @DisplayName("문자열 분리메서드 split 테스트")
+    @Test
+    void splitTest() {
+
+    }
+
+    @DisplayName("문자열 substring 테스트")
+    @Test
+    void substringTest() {
+
+    }
+
+    @DisplayName("charAt 테스트")
+    @Test
+    void charAtTest() {
+
+    }
+
+    @DisplayName("charAt 실패 테스트")
+    @Test
+    void charAtExceptionTest() {
+
+    }
+
+}
