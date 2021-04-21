bac881fa5f015adc8ce3a1b5922444819b51ae82
dhmin5693 <dhmin5693@naver.com>
2021-04-21 23:03:52 +0900

    refactor: 필드에 final 추가

--- src/main/java/RandomGenerator.java
+++ src/main/java/RandomGenerator.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class RandomGenerator {
 
-    private Random random = new Random();
+    private final Random random = new Random();
 
     public int nextInt() {
         return random.nextInt(8) + 1;
