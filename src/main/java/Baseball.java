b97f01bee6bda1d6b00d8d66e66353f48fcb258b
mark <mark.jeongh@gmc-labs.com>
2021-04-21 15:43:25 +0900

    feat(makeSecretNumber): 상대방 수 생성 기능
    Baseball 클래스 생성
    makeSecretNumber 생성

---
+++ src/main/java/Baseball.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Baseball {
+
+    int SecretNumber = 0;
+
+    public void makeSecretNumber() {
+        Random rnd = new Random();
+
+        int hundred = (rnd.nextInt(9) + 1) * 100;
+        int ten = (rnd.nextInt(9) + 1) * 10;
+        int one = rnd.nextInt(9) + 1;
+
+        SecretNumber = hundred + ten + one;
+    }
+
+}
