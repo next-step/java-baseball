4470f1c970ee660b0e1fd0a4de4f435ca983968d
dhmin5693 <dhmin5693@naver.com>
2021-04-22 03:04:49 +0900

    feat: 사용자가 입력한 3자리 숫자를 담는 기능 추가

---
+++ src/main/java/domain/Numbers.java
@@ -0,0 +1,44 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Numbers {
+
+    private static final int MINIMUM_NUMBERS_VALUE = 100;
+    private static final int MAXIMUM_NUMBERS_VALUE = 999;
+    private static final int COUNT_OF_NUMBERS = 3;
+
+    private final List<Integer> list;
+
+    public Numbers(int numbers) {
+        validateNumbers(numbers);
+        this.list = new ArrayList<>(3);
+        String str = String.valueOf(numbers);
+
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i) - '0');
+        }
+    }
+
+    public Numbers(List<Integer> list) {
+        validateList(list);
+        this.list = list;
+    }
+
+    private void validateNumbers(int numbers) {
+        if (numbers < MINIMUM_NUMBERS_VALUE || numbers > MAXIMUM_NUMBERS_VALUE) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private void validateList(List<Integer> list) {
+        if (list.size() != COUNT_OF_NUMBERS) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해야 합니다.");
+        }
+    }
+
+    public List<Integer> get() {
+        return list;
+    }
+}
