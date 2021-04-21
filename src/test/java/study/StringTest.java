efb5cec9cd37bb2f256256b47262282d7e161a7d
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-22 08:11:20 +0900

    test : StringTest 요구사항 3 - charAt 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -47,4 +48,30 @@ public class StringTest {
         // then
         assertThat("1,2").isEqualTo(substring);
     }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기")
+    void charAt() {
+        // given
+        String target = "abc";
+
+        // when then
+        assertThat('a').isEqualTo(target.charAt(0));
+        assertThat('b').isEqualTo(target.charAt(1));
+        assertThat('c').isEqualTo(target.charAt(2));
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기 - 위치 값이 문자열 크기를 초과했을 때")
+    void charAt_outOfBound() {
+        // given
+        String target = "abc";
+
+        // when then
+        assertThatThrownBy(
+                () -> target.charAt(3)
+        ).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+
 }
