d1c937c1c2576d4fe17c6dd4aeed920a98d9d620
taemin <taminging@kakao.com>
2021-04-21 23:40:27 +0900

    feat(Computer) : create Computer

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+public class Computer {
+
+    private BallNumber ballNumber;
+
+    public Computer(){
+        this.ballNumber = new BallNumber();
+    }
+
+    public BallNumber getBallNumber(){
+        return this.ballNumber;
+    }
+}
