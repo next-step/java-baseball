c2d89bec6c8a184654c40d3f1a97434e665af093
catsbi <killfine1988@gmail.com>
2021-04-22 00:18:16 +0900

    feat: 야구숫자 래퍼클래스, 일급 콜렉션 기능 구현

--- src/main/java/baseball/domain/BallNumber.java
+++ src/main/java/baseball/domain/BallNumber.java
@@ -1,4 +1,33 @@
 package baseball.domain;
 
+import baseball.exceptions.InvalidBallNumberException;
+
+import java.util.Objects;
+
 public class BallNumber {
+    private int number;
+
+    public BallNumber(int number) {
+        validation(number);
+        this.number = number;
+    }
+
+    private void validation(int number) {
+        if (number < 1 || number > 9) {
+            throw new InvalidBallNumberException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BallNumber)) return false;
+        BallNumber that = (BallNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
--- src/main/java/baseball/domain/BallNumbers.java
+++ src/main/java/baseball/domain/BallNumbers.java
@@ -1,4 +1,70 @@
 package baseball.domain;
 
+import baseball.exceptions.DuplicateBallNumberException;
+import baseball.exceptions.InvalidBallNumbersSizeException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static baseball.domain.PitchingResultStatus.*;
+
 public class BallNumbers {
+
+    private final List<BallNumber> ballNumbers = new ArrayList<>();
+
+    public BallNumbers(List<BallNumber> ballNumbers) {
+        checkSize(ballNumbers);
+        for (BallNumber ballNumber : ballNumbers) {
+            checkDuplicate(ballNumber);
+            this.ballNumbers.add(ballNumber);
+        }
+    }
+
+    private void checkSize(List<BallNumber> ballNumbers) {
+        if (ballNumbers.size() != 3) {
+            throw new InvalidBallNumbersSizeException();
+        }
+    }
+
+    private void checkDuplicate(BallNumber ballNumber) {
+        if (this.ballNumbers.contains(ballNumber)) {
+            throw new DuplicateBallNumberException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BallNumbers)) return false;
+        BallNumbers that = (BallNumbers) o;
+        return Objects.equals(ballNumbers, that.ballNumbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ballNumbers);
+    }
+
+    public boolean containsAll(List<BallNumber> ballNumbers) {
+        return this.ballNumbers.containsAll(ballNumbers);
+    }
+
+    public BallNumber get(int index) {
+        return ballNumbers.get(index);
+    }
+
+    public int size() {
+        return ballNumbers.size();
+    }
+
+    public PitchingResultStatus getPitchingResultStatus(BallNumber ballNumber, int index) {
+        if (ballNumbers.get(index).equals(ballNumber)) {
+            return STRIKE;
+        }
+        if (ballNumbers.contains(ballNumber)) {
+            return BALL;
+        }
+        return NOTHING;
+    }
 }
