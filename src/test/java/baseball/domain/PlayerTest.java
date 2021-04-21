0963c7539b363ab3ccd2db037759756956fe99cd
taemin <taminging@kakao.com>
2021-04-21 23:51:54 +0900

    test(PlayerTest) : create PlayerTest, initPlayerTest

---
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -0,0 +1,19 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Player Test")
+public class PlayerTest {
+
+    @DisplayName("init Player Test")
+    @Test
+    public void initPlayerTest() {
+        Player player = new Player();
+        BallNumber ballNumber = new BallNumber(123);
+        player.setBallNumber(ballNumber);
+        assertTrue(Integer.parseInt(player.getBallNumber().toString()) == 123);
+    }
+}
