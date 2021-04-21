b1bfc29e7c5dd5d65d78a415a17a4cba6a36069a
taemin <taminging@kakao.com>
2021-04-21 23:58:05 +0900

    test(PlayerTest) add initPlayerTest method assertNotNull test

--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -3,6 +3,7 @@ package baseball.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Player Test")
@@ -14,6 +15,8 @@ public class PlayerTest {
         Player player = new Player();
         BallNumber ballNumber = new BallNumber(123);
         player.setBallNumber(ballNumber);
+        assertNotNull(player);
+        assertNotNull(player.getBallNumber());
         assertTrue(Integer.parseInt(player.getBallNumber().toString()) == 123);
     }
 }
