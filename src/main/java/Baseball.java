77a647f5f1fdee7919234d1c97f81d9a4333ddc8
mark <mark.jeongh@gmc-labs.com>
2021-04-21 16:37:46 +0900

    feat(inputUserNumber): 사용자 수 입력 기능
    Baseball 클래스 수정
    inputUserNumber 생성

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,8 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Baseball {
 
     int SecretNumber = 0;
+    int InputUserNumber = 0;
 
     public void makeSecretNumber() {
         Random rnd = new Random();
@@ -12,5 +16,18 @@ public class Baseball {
         int one = rnd.nextInt(9) + 1;
 
         SecretNumber = hundred + ten + one;
+    }
+
+    public void inputUserNumber() {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("숫자를 입력해 주세요 : ");
+        String stringBuffer = null;
+        try {
+            stringBuffer = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        InputUserNumber = Integer.parseInt(stringBuffer);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
         baseball.makeSecretNumber();
+        baseball.inputUserNumber();
     }
 }
