ec6f6f21913d037b3bd8454985be36fd957cb541
elther@naver.com <cost#!8800>
2021-04-22 01:43:53 +0900

    create(file) : created file
    - BaseballGameTest.java

---
+++ src/test/java/BaseballGameTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BaseballGameTest {
+    @Test
+    @DisplayName("켄트백 형님의 말씀대로 nothing 부터..")
+    public void nothing(){
+
+    }
+}
