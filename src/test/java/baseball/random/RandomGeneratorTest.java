c009f397c61a0e7ffd4c3cf85af4dde0ff258fa8
Jonny-Cho <andante2183@gmail.com>
2021-04-22 19:10:32 +0900

    test: random generator

---
+++ src/test/java/baseball/random/RandomGeneratorTest.java
@@ -0,0 +1,19 @@
+package baseball.random;
+
+import baseball.domain.Numbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomGeneratorTest {
+
+    @DisplayName("RandomGenerator getRandomNumber는 shuffle한 Numbers를 반환한다")
+    @Test
+    void getRandomNumberTest(){
+        final Random testRandomGenerator = new RandomGenerator();
+        final Numbers randomNumber = testRandomGenerator.getRandomNumber(3);
+        assertThat(randomNumber.getNumbers()).doesNotContainSequence("1", "2", "3");
+    }
+
+}
---
+++ src/test/java/baseball/random/TestRandomGeneratorTest.java
@@ -0,0 +1,20 @@
+package baseball.random;
+
+import baseball.domain.Numbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestRandomGeneratorTest {
+
+    @DisplayName("TestRandomGenerator getRandomNumber는 shuffle하지 않는 Numbers를 반환한다")
+    @Test
+    void getRandomNumberTest(){
+        final Random testRandomGenerator = new TestRandomGenerator();
+        final Numbers randomNumber = testRandomGenerator.getRandomNumber(3);
+        assertThat(randomNumber.getNumbers()).containsExactly("1", "2", "3");
+    }
+
+}
