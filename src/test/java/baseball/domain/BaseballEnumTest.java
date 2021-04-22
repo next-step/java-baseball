1e5f20e3b76f04ee185c39d3fb5996de28b67e9e
Jonny-Cho <andante2183@gmail.com>
2021-04-22 20:46:18 +0900

    test: BaseballEnum

---
+++ src/test/java/baseball/domain/BaseballEnumTest.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class BaseballEnumTest {
+
+    @Test
+    void enumMessageTest(){
+        assertThat(BaseballEnum.STRIKE.getMessage()).isEqualTo("스트라이크");
+        assertThat(BaseballEnum.BALL.getMessage()).isEqualTo("볼");
+        assertThat(BaseballEnum.OUT.getMessage()).isEqualTo("아웃");
+    }
+}
