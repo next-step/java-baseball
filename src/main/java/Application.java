d146c59ebcf9ec39ed181be031c61c1f0a9f0a29
dhmin5693 <dhmin5693@naver.com>
2021-04-23 02:12:11 +0900

    feat: 게임 실행 기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,17 @@
 import domain.BaseballGame;
+import domain.RandomNumberGenerator;
+import domain.RandomNumbersGenerator;
+import ui.IOManager;
+import ui.StandardInput;
+import ui.StandardOutput;
 
 public class Application {
     public static void main(String[] args) {
-        BaseballGame.getInstance()
-                    .run();
+        IOManager ioManager = new IOManager(new StandardInput(), new StandardOutput());
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator(randomNumberGenerator);
+
+        BaseballGame game = new BaseballGame(ioManager, randomNumbersGenerator);
+        game.run();
     }
 }
--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -1,35 +1,81 @@
 package domain;
 
+import ui.IOManager;
+import ui.Input;
+import ui.Output;
+
 public class BaseballGame {
 
-    private static BaseballGame instance;
+    private final Input input;
+    private final Output output;
+    private final NumbersGenerator numbersGenerator;
 
-    private BaseballGame() { }
-
-    public static BaseballGame getInstance() {
-        if (instance == null) {
-            instance = new BaseballGame();
-        }
-
-        return instance;
+    public BaseballGame(IOManager ioManager, NumbersGenerator numbersGenerator) {
+        this.input = ioManager.input();
+        this.output = ioManager.output();
+        this.numbersGenerator = numbersGenerator;
     }
 
     public void run() {
         do {
-            // do something...
-        } while(isNotEnd());
-
-        exitGame();
+            progress();
+        } while(continueGame());
     }
 
-    private boolean isNotEnd() {
+    private void progress() {
 
-        // do something...
+        Numbers answer = new Numbers(numbersGenerator.makeNumbers());
+        CorrectAnswerChecker checker = new CorrectAnswerChecker(answer);
+        boolean isNotEnd;
 
-        return false;
+        do {
+            output.print("숫자를 입력해주세요: ");
+            Numbers numbers = inputNumbers();
+            isNotEnd = checkNumbers(checker, numbers);
+        } while(isNotEnd);
     }
 
-    private void exitGame() {
-        // do something...
+    private Numbers inputNumbers() {
+        try {
+            int number = input.nextInt();
+            return new Numbers(number);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            output.printLine(e.getMessage());
+        }
+
+        return null;
+    }
+
+    private boolean checkNumbers(CorrectAnswerChecker checker, Numbers numbers) {
+
+        if (numbers == null) {
+            return true;
+        }
+
+        Hint hint = checker.makeResultHint(numbers);
+
+        if (hint == null) {
+            return true;
+        }
+
+        output.printLine(hint.toString());
+        return hint.isNotVictory();
+    }
+
+    private boolean continueGame() {
+        output.printLine("정답입니다. 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        input.nextLine();
+        String command = input.nextLine();
+
+        while (isInvalidCommand(command)) {
+            output.printLine("1이나 2만 입력해주세요.");
+            command = input.nextLine();
+        }
+
+        return command.equals("1");
+    }
+
+    private boolean isInvalidCommand(String command) {
+        return !"1".equals(command) && !"2".equals(command);
     }
 }
