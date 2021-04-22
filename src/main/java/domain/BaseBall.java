d589380996240683c13235ad6e3665444132184d
jessi68 <lynn312.unist.ac.kr>
2021-04-22 19:13:12 +0900

    feat: 자릿수 하나에 대하여 ball 점수를 매기는 기능 구현

--- src/main/java/domain/BaseBall.java
+++ src/main/java/domain/BaseBall.java
@@ -2,25 +2,31 @@ package domain;
 
 import java.util.HashMap;
 
+
 public class BaseBall {
     public static final Integer INCLUSIVE_MIN = 1;
     public static final Integer INCLUSIVE_MAX = 9;
     public static final Integer TRUE = 1;
     public static final Integer FALSE = 0;
 
-    private static final HashMap<Integer, BaseBall> cachedBaseBall = new HashMap<>();
+    private static HashMap<Info, BaseBall> cachedBaseBalls = new HashMap<Info, BaseBall>();
 
     private final int number;
+    private final int location;
 
-    private BaseBall(int number) {
+    private BaseBall(int number, int location) {
         this.number = number;
+        this.location = location;
     }
 
-    public static BaseBall fromNumber(int number) {
-        if(!cachedBaseBall.containsKey(number)) {
-            cachedBaseBall.put(number, new BaseBall(number));
+
+    public static BaseBall fromNumAndLocation(int number, int location) {
+        Info info = new Info(number, location);
+
+        if(!cachedBaseBalls.containsKey(info)) {
+            cachedBaseBalls.put(info, new BaseBall(number, location));
         }
-        return cachedBaseBall.get(number);
+        return cachedBaseBalls.get(info);
     }
 
     public static boolean isValidate(int number) {
@@ -32,6 +38,17 @@ public class BaseBall {
             return TRUE;
         }
         return FALSE;
+    }
+
+    public boolean isSameLocation(BaseBall opponent) {
+        return location == opponent.location;
+    }
+
+    public int ballScore(BaseBall answer) {
+        if(!isSameLocation(answer)) {
+            return isEqualNumber(answer);
+        }
+        return 0;
     }
 
 }
