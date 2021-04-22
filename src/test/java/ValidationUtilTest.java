70a26ab47310ef6a712113fbb441f4e6e21713e3
mintheon <mintheon@mintheon.com>
2021-04-22 14:49:18 +0900

    test: 사용자 숫자 입력 로직 검증 Test코드 추가
    - 3자리 숫자가 아닌 문자 입력 테스트
    - 숫자가 아닌 문자 입력 테스트
    - 0이 포함되어 있을 경우 테스트
    - 정상적인 3자리 숫자 입력시 테스트

---
+++ src/test/java/ValidationUtilTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.ValidationUtil;
+
+public class ValidationUtilTest {
+    private static final ValidationUtil validationUtil = new ValidationUtil();
+
+    @Test
+    @DisplayName("3자리 숫자가 아닌 문자 입력 테스트")
+    void is3DigitTest() {
+        String number = "12";
+        Assertions.assertFalse(validationUtil.isCorrectNumber(number));
+
+        number = "12345";
+        Assertions.assertFalse(validationUtil.isCorrectNumber(number));
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 문자 입력 테스트")
+    void isNumberTest() {
+        String number = "!@#";
+        Assertions.assertFalse(validationUtil.isCorrectNumber(number));
+
+        number = "1@3";
+        Assertions.assertFalse(validationUtil.isCorrectNumber(number));
+    }
+
+    @Test
+    @DisplayName("사용자가 입력한 숫자에 0이 포함되어 있을 경우 테스트")
+    void isContainsZeroTest() {
+        String number = "012";
+        Assertions.assertFalse(validationUtil.isCorrectNumber(number));
+
+        number = "890";
+        Assertions.assertFalse(validationUtil.isCorrectNumber(number));
+    }
+
+    @Test
+    @DisplayName("3자리 숫자 입력시의 테스트")
+    void numberTest() {
+        String number = "123";
+        Assertions.assertTrue(validationUtil.isCorrectNumber(number));
+
+        number = "789";
+        Assertions.assertTrue(validationUtil.isCorrectNumber(number));
+    }
+}
