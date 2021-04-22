4c5a0e9449817f027d0e33ecdf07bff345f78707
khg <gusrn160304@gmail.com>
2021-04-23 00:44:15 +0900

    임의의 숫자 생성 기능 테스트코드 수정

--- src/test/java/study/BaseballTest.java
+++ src/test/java/study/BaseballTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import baseball.BaseballGame;
@@ -11,13 +13,18 @@ import baseball.BaseballGame;
 public class BaseballTest {
 
 	private BaseballGame baseballGame = new BaseballGame();
+	private List<Integer> inputListValue;
+	private List<Integer> randomListValue;
+	
+	@BeforeEach
+	public void init() {
+		inputListValue = BaseballGame.generateRandomIntegerList(3);
+		randomListValue = BaseballGame.generateRandomIntegerList(3);
+	}
 	
 	@Test
+	@DisplayName("랜덤 생성 비교")
 	public void generate_random_list_test() {
-		List<Integer> list = BaseballGame.generateRandomIntegerList(3);
-		List<Integer> list2 = BaseballGame.generateRandomIntegerList(3);
-		
-		assertThat(list)
-			.isEqualTo(list2);
+		assertThat(inputListValue).isNotEqualTo(randomListValue);
 	}
 }
