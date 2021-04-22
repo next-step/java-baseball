4b7032d36863c031125fe0782a7b208c4e52fa7d
Jonny-Cho <andante2183@gmail.com>
2021-04-22 23:07:07 +0900

    feat: count

---
+++ src/main/java/baseball/domain/count/Ball.java
@@ -0,0 +1,21 @@
+package baseball.domain.count;
+
+import baseball.domain.Numbers;
+
+public class Ball implements Count {
+    private int count = 0;
+
+    @Override
+    public int count(Numbers randomNumbers, Numbers userNumbers) {
+        for (int i = 0; i < randomNumbers.size(); i++) {
+            check(randomNumbers, userNumbers, i);
+        }
+        return count;
+    }
+
+    private void check(Numbers randomNumbers, Numbers userNumbers, int i) {
+        if (!(randomNumbers.get(i).equals(userNumbers.get(i))) && randomNumbers.contains(userNumbers.get(i))) {
+            count++;
+        }
+    }
+}
---
+++ src/main/java/baseball/domain/count/Count.java
@@ -0,0 +1,7 @@
+package baseball.domain.count;
+
+import baseball.domain.Numbers;
+
+public interface Count {
+    int count(Numbers randomNumbers, Numbers userNumbers);
+}
