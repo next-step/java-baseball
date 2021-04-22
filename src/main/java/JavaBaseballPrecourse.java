1d4bf528906545dc6942be84fb23542ddcaa7d92
kimchild <allcreation@nate.com>
2021-04-22 21:52:55 +0900

    style: formatting에 맞게 수정헀을때 github에 변경 표시를 확인한다.
    코드컨벤션을 맞추었을때 github에 체크되는지를 확인한다.

--- src/main/java/JavaBaseballPrecourse.java
+++ src/main/java/JavaBaseballPrecourse.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class JavaBaseballPrecourse {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("hello");
-        try(Scanner scan = new Scanner(System.in)) {
-            // while(true) {
-                System.out.println("입력하신 내용은 : " + scan.nextLine());
-                System.out.println("입력하신 내용2는 : " + scan.nextLine());
-                scan.close();
-            //     break;
-            // }
-        }
+		System.out.println("hello");
+		try (Scanner scan = new Scanner(System.in)) {
+			// while(true) {
+			System.out.println("입력하신 내용은 : " + scan.nextLine());
+			System.out.println("입력하신 내용2는 : " + scan.nextLine());
+			scan.close();
+			//     break;
+			// }
+		}
 
-    }
+	}
 }
