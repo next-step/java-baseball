b346de44a87c0e98ab5cb60ba291e8b25a053c2d
dhmin5693 <dhmin5693@naver.com>
2021-04-23 03:24:09 +0900

    fix: 숫자가 무조건 오름차순으로 생성되는 버그 수정

--- src/main/java/domain/RandomNumbersGenerator.java
+++ src/main/java/domain/RandomNumbersGenerator.java
@@ -3,6 +3,7 @@ package domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -19,7 +20,7 @@ public class RandomNumbersGenerator implements NumbersGenerator {
     @Override
     public List<Integer> makeNumbers() {
 
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new LinkedHashSet<>();
 
         while(set.size() < MAX_SIZE_OF_NUMBERS) {
             set.add(randomNumberGenerator.nextInt());
