ace4d1ef2f0d03064a6dea78a16fbaabee385ea9
HoonHee Jeong <jhh992000@gmail.com>
2021-04-23 01:18:08 +0900

    feat: 모두 맞췄을때 종료여부를 묻는 메서드 추가

--- src/main/java/nextstep/study/baseball/service/BaseballService.java
+++ src/main/java/nextstep/study/baseball/service/BaseballService.java
@@ -1,9 +1,12 @@
 package nextstep.study.baseball.service;
 
+import java.util.Scanner;
+import nextstep.study.baseball.common.BaseConstants;
 import nextstep.study.baseball.domain.Batter;
 import nextstep.study.baseball.domain.Pitcher;
 import nextstep.study.baseball.domain.Referee;
 import nextstep.study.baseball.domain.RefreeDecision;
+import nextstep.study.baseball.util.BaseballUtil;
 
 public class BaseballService {
 
@@ -11,18 +14,31 @@ public class BaseballService {
 		Batter batter = new Batter();
 		batter.generateRandomNumbers();
 
+		//TODO - 테스트 후 제거
+		System.out.println(batter.getNumbers());
+
 		int strikeCount = 0;
 
 		while (strikeCount < 3) {
 			Pitcher pitcher = new Pitcher();
 			pitcher.makeMixBallNumber(); // 투수 볼배합 만들기 (입력받기)
 
-			RefreeDecision decision = Referee.makeRefreeDecision(batter.getNumbers(), pitcher.getNumbers());
+			Referee referee = new Referee();
+			RefreeDecision decision = referee.makeRefreeDecision(batter.getNumbers(), pitcher.getNumbers());
 			strikeCount = decision.getStrikeCount();
 
-			System.out.println("타자 : " + batter.getNumbers());
-			System.out.println("투수 : " + pitcher.getNumbers());
-			System.out.println("strikeCount : " + strikeCount);
+			//TODO - 판정 결과 출력 포맷팅
+			System.out.println(decision);
+		}
+		askFinish();
+	}
+
+	public void askFinish() {
+		BaseballUtil.showMessage(BaseConstants.MESSAGE_FINISH);
+		Scanner scanner = new Scanner(System.in);
+		String inputStr = scanner.nextLine();
+		if (inputStr.equals("1")) {
+			this.playBall();
 		}
 	}
 
