90af4b03127d55edb76eb61d5e6f027c26dd6d3e
Jonny-Cho <andante2183@gmail.com>
2021-04-22 23:40:25 +0900

    feat: output

--- src/main/java/baseball/view/output/ConsoleOutput.java
+++ src/main/java/baseball/view/output/ConsoleOutput.java
@@ -1,8 +1,9 @@
 package baseball.view.output;
 
+import baseball.domain.BaseballResult;
+
 import static baseball.view.input.InputMessages.PLEASE_INPUT_VALID_NUMBERS;
-import static baseball.view.output.OutputMessages.END_GAME;
-import static baseball.view.output.OutputMessages.RE_START;
+import static baseball.view.output.OutputMessages.*;
 
 public class ConsoleOutput implements Output {
     @Override
@@ -19,6 +20,15 @@ public class ConsoleOutput implements Output {
     public void printEndMessage() {
         System.out.println(END_GAME);
         System.out.println(RE_START);
+    }
+
+    @Override
+    public void printResult(BaseballResult result) {
+        if(result.isNothing()){
+            System.out.println(NOTHING);
+            return;
+        }
+        System.out.println(result.getResult());
     }
 
 }
--- src/main/java/baseball/view/output/Output.java
+++ src/main/java/baseball/view/output/Output.java
@@ -1,9 +1,13 @@
 package baseball.view.output;
 
+import baseball.domain.BaseballResult;
+
 public interface Output {
     void print(final String message);
 
     void printValiedateNumber(int numberSize);
 
     void printEndMessage();
+
+    void printResult(BaseballResult result);
 }
