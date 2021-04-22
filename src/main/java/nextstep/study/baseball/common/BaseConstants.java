1795dfc65b469ee399a244c1a08d851cfab63f30
HoonHee Jeong <jhh992000@gmail.com>
2021-04-22 23:16:47 +0900

    refactor: 랜덤 수 생성 메서드 리팩토링, 숫자가 순차적으로 만들어지는 문제 수정

---
+++ src/main/java/nextstep/study/baseball/common/BaseConstants.java
@@ -0,0 +1,10 @@
+package nextstep.study.baseball.common;
+
+public class BaseConstants {
+
+	/**
+	 * 입력 숫자 개수
+	 */
+	public static final int INPUT_NUMBER_LENGTH = 3;
+
+}
--- src/main/java/nextstep/study/baseball/domain/Batter.java
+++ src/main/java/nextstep/study/baseball/domain/Batter.java
@@ -1,25 +1,38 @@
 package nextstep.study.baseball.domain;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
+import nextstep.study.baseball.common.BaseConstants;
 
 public class Batter {
+
+	private Set<Integer> numbers = new LinkedHashSet<Integer>();
+
+	public Batter() {
+	}
+
+	public List<Integer> generateRandomNumbers() {
+		return this.generateRandomNumbers(BaseConstants.INPUT_NUMBER_LENGTH);
+	}
 
 	public List<Integer> generateRandomNumbers(int length) {
 		if (length == 0) {
 			return new ArrayList();
 		}
-
-		Set<Integer> set = new HashSet<>();
-		while (set.size() < length) {
-			Double value = (Math.random() * 9) + 1;
-			set.add(value.intValue());
+		if (length > 9) {
+			length = 9;
 		}
-		List<Integer> list = new ArrayList<>(set);
-
-		return list;
+		Random randNum = new Random();
+		while (numbers.size() < length) {
+			numbers.add(randNum.nextInt(9) + 1);
+		}
+		return new ArrayList<>(numbers);
 	}
 
+	public ArrayList<Integer> getNumbers() {
+		return new ArrayList<>(numbers);
+	}
 }
