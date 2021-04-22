886a9392e311672785eaf0188f9afced59215aca
mac2019 <pooheyup@naver.com>
2021-04-23 04:23:38 +0900

    BaseballGameResultCalculator-getSBallsCnt - getBallsCnt 테스트 2. (214,inputStr) -> 2 (1ball)                                           - isStrikeAtIndex : boolean 추가                                           - isBallAtIndex : boolean 추가

--- src/main/java/BaseballGameResultCalculator.java
+++ src/main/java/BaseballGameResultCalculator.java
@@ -12,13 +12,27 @@ public class BaseballGameResultCalculator {
         int count = 0;
 
         for (int i=0; i<maxLen; i++)
-            if (createdStr.charAt(i) == inputStr.charAt(i))
+            if (isStrikeAtIndex(createdStr,inputStr,i))
                 count += 1;
 
         return count;
     }
 
+    private boolean isStrikeAtIndex(String createdStr, String inputStr, int index) {
+        return createdStr.charAt(index) == inputStr.charAt(index);
+    }
+
     public int getBallsCnt(String createdStr, String inputStr) {
-        return 0;
+        int cnt = 0;
+
+        for (int i=0; i<maxLen; i++)
+            if (isBallAtIndex(createdStr, inputStr, i))
+                cnt += 1;
+
+        return cnt;
+    }
+
+    private boolean isBallAtIndex(String createdStr, String inputStr, int index) {
+        return !(isStrikeAtIndex(createdStr, inputStr, index)) && createdStr.contains(inputStr.charAt(index)+"");
     }
 }
--- src/test/java/BaseballGameResultCalculatorTest.java
+++ src/test/java/BaseballGameResultCalculatorTest.java
@@ -18,7 +18,7 @@ public class BaseballGameResultCalculatorTest {
 
     /* 스트라이크 1개 반환하기 (1 고정, 1 변합)
      * 1개의 스트라이크는 3개의 숫자라고 가정할 때 3곳에서 일어난다. 그것을 표현한 테스트 셋이다.
-     * 214, 201 -> 1
+     * 214, 203 -> 1
      * 214, 816 -> 1
      * 214, 984 -> 1
      */
@@ -26,14 +26,14 @@ public class BaseballGameResultCalculatorTest {
     public void isCountedOneStrikeWhenFirstIsFixed(){
         String createdStr = "214";
 
-        assertEquals(calculator.getStrikesCnt(createdStr, "201"), 1);
+        assertEquals(calculator.getStrikesCnt(createdStr, "203"), 1);
         assertEquals(calculator.getStrikesCnt(createdStr, "816"), 1);
         assertEquals(calculator.getStrikesCnt(createdStr, "984"), 1);
     }
 
     /* 스트라이크 2개 반환하기 (1 고정, 1 변합)
      * 2개의 스트라이크는 3개의 숫자라고 가정할 때 3곳에서 일어난다. 그것을 표현한 테스트 셋이다.
-     * 214, 211 -> 2
+     * 214, 213 -> 2
      * 214, 814 -> 2
      * 214, 284 -> 2
      */
@@ -41,39 +41,53 @@ public class BaseballGameResultCalculatorTest {
     public void isCountedTwoStrikeWhenFirstIsFixed(){
         String createdStr = "214";
 
-        assertEquals(calculator.getStrikesCnt(createdStr, "211"), 2);
+        assertEquals(calculator.getStrikesCnt(createdStr, "213"), 2);
         assertEquals(calculator.getStrikesCnt(createdStr, "814"), 2);
         assertEquals(calculator.getStrikesCnt(createdStr, "284"), 2);
     }
 
     /* 스트라이크 최종 테스트. 많은 상황에서 잘 동작하는지 확인
      * 214, 928 -> 0
-     * 214, 201 -> 1
+     * 214, 203 -> 1
      * 214, 816 -> 1
      * 214, 984 -> 1
-     * 214, 211 -> 2
+     * 214, 213 -> 2
      * 214, 814 -> 2
      * 214, 284 -> 2
-     * 213, 213 -> 3
+     * 214, 214 -> 3
      */
     @Test
-    public void isCountedStrikesManyWhenFixed(){
+    public void isCountedStrikesManyWhenFirstIsFixed(){
         String createdStr = "214";
 
         assertEquals(calculator.getStrikesCnt(createdStr, "928"), 0);
-        assertEquals(calculator.getStrikesCnt(createdStr, "201"), 1);
+        assertEquals(calculator.getStrikesCnt(createdStr, "203"), 1);
         assertEquals(calculator.getStrikesCnt(createdStr, "816"), 1);
         assertEquals(calculator.getStrikesCnt(createdStr, "984"), 1);
-        assertEquals(calculator.getStrikesCnt(createdStr, "211"), 2);
+        assertEquals(calculator.getStrikesCnt(createdStr, "213"), 2);
         assertEquals(calculator.getStrikesCnt(createdStr, "814"), 2);
         assertEquals(calculator.getStrikesCnt(createdStr, "284"), 2);
-        assertEquals(calculator.getStrikesCnt(createdStr, "213"), 3);
+        assertEquals(calculator.getStrikesCnt(createdStr, "214"), 3);
     }
 
     @Test
     public void isCountedBallsWhen123And456(){
         String testStr = "123";
         assertEquals(calculator.getBallsCnt(testStr, "456"), 0);
+    }
+
+    /* 볼 1개 반환하기 (1개 고정, 1개 변함)
+     * 214, 456 -> 1
+     * 214, 561 -> 1
+     * 214, 732 -> 1
+     */
+    @Test
+    public void isCountedOneBallWhenFirstIsFixed(){
+        String createdStr = "214";
+
+        assertEquals(calculator.getBallsCnt(createdStr, "456"), 1);
+        assertEquals(calculator.getBallsCnt(createdStr, "561"), 1);
+        assertEquals(calculator.getBallsCnt(createdStr, "732"), 1);
     }
 
 }
