4d30c8927d5469e161d06ad6abbeb8516fac8725
mintheon <mintheon@mintheon.com>
2021-04-23 00:01:28 +0900

    feat: 사용자가 동일한 숫자를 입력했을경우 검증 로직 추가
    - 중복 숫자입력을 막기 위한 isDuplicateNumber 메소드 추가
    - Error 메시지 추가

--- src/main/java/domain/enums/Error.java
+++ src/main/java/domain/enums/Error.java
@@ -1,7 +1,7 @@
 package domain.enums;
 
 public enum Error {
-    INPUT_ERROR("잘못된 숫자를 입력하셨습니다. 3자리의 숫자를 입력해주세요.");
+    INPUT_ERROR("잘못된 숫자를 입력하셨습니다. 각 자리 숫자가 중복되지 않는 3자리 숫자를 입력해주세요.");
 
     String message;
 
--- src/main/java/util/ValidationUtil.java
+++ src/main/java/util/ValidationUtil.java
@@ -2,11 +2,25 @@ package util;
 
 import domain.enums.Error;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ValidationUtil {
+    public boolean isCorrectNumber(String number) {
+        if (!validateNumber(number)) {
+            System.out.println(Error.INPUT_ERROR.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
     private boolean validateNumber(String number) {
         if (!isNumber(number))
             return false;
         if (!is3Digits(number))
+            return false;
+        if (isDuplicateNumber(number))
             return false;
 
         return true;
@@ -20,12 +34,14 @@ public class ValidationUtil {
         return number.length() == 3;
     }
 
-    public boolean isCorrectNumber(String number) {
-        if (!validateNumber(number)) {
-            System.out.println(Error.INPUT_ERROR.getMessage());
-            return false;
+    private boolean isDuplicateNumber(String number) {
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            set.add(number.charAt(i));
         }
 
-        return true;
+        return number.length() != set.size();
     }
+
 }
