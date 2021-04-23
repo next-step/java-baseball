c26248c4fb47a7ab7a57a7186da6eccb853502e4
HoonHee.Jeong <jhh992000@gmail.com>
2021-04-23 09:34:16 +0900

    test: 판정결과 출력 테스트 값 추가
    [git-author 변경] 잘못된 작성자 커밋 수정

--- src/test/java/nextstep/study/baseball/service/BaseballServiceTest.java
+++ src/test/java/nextstep/study/baseball/service/BaseballServiceTest.java
@@ -9,7 +9,7 @@ public class BaseballServiceTest {
 
 	@DisplayName("판정 결과 출력 테스트")
 	@ParameterizedTest
-	@CsvSource(value = {"2,1,0", "1,2,0", "3,0,0", "0,0,3"}, delimiter = ',')
+	@CsvSource(value = {"2,1,0", "1,1,1", "1,2,0", "3,0,0", "0,0,3"}, delimiter = ',')
 	void testShowDecisionMessage(Integer strikeCount, Integer ballCount, Integer nothingCount) {
 		RefreeDecision decision = new RefreeDecision(strikeCount, ballCount, nothingCount);
 		BaseballService baseballService = new BaseballService();
