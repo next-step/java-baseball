0f545012d083ea7ba1464d4b0c7398fdfdf2e863
mac2019 <pooheyup@naver.com>
2021-04-24 01:52:05 +0900

    RandomNumGenerator- 유효성 검사 완료 - 기능 완료

--- src/main/java/RandomNumGenerator.java
+++ src/main/java/RandomNumGenerator.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class RandomNumGenerator {
-    private int maxLen;
+    private static int maxLen;
+    private static int BASEBALL_MAX_NUM = 9;
+
     public RandomNumGenerator(){
         this(BaseballGame.DEFAULT_LEN);
     }
@@ -10,10 +15,28 @@ public class RandomNumGenerator {
     }
 
     public String generateRandomNum(){
+        List<Integer> baseballRandomNums = createRandomNum();
+        Collections.shuffle(baseballRandomNums);
+        return getStringWhichIsConvertedList(pickNumbers(baseballRandomNums));
+    }
+
+    private static List<Integer> createRandomNum() {
+        List<Integer> baseballNums = new ArrayList<>();
+        for(int i = 1; i <= BASEBALL_MAX_NUM; i++){
+            baseballNums.add(i);
+        }
+        return baseballNums;
+    }
+
+    private static List<Integer> pickNumbers(List<Integer> baseballNums) {
+        return baseballNums.subList(0, maxLen);
+    }
+
+    private static String getStringWhichIsConvertedList(List<Integer> baseballNums) {
         StringBuilder builder = new StringBuilder();
-        Random random = new Random();
-        for (int i=0; i<maxLen; i++)
-            builder.append(random.nextInt(9) + 1);
+        for (Integer i : baseballNums) {
+            builder.append(i);
+        }
         return builder.toString();
     }
 }
