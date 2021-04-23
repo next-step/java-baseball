0a79f12c6f656876d565a4d575144c9b71f1afcc
HyungjuJang <dacapolife87@gmail.com>
2021-04-24 00:36:26 +0900

    feat : 게임의 결과를 관리하는 클래스 - 볼, 스트라이크의 갯수와 게임종료상태를 관리 - 결과를 출력해주는 기능 추가

---
+++ src/main/java/domain/BaseballResult.java
@@ -0,0 +1,60 @@
+package domain;
+
+public class BaseballResult {
+
+    private static final String MESSAGE_STRIKE = " 스트라이크";
+    private static final String MESSAGE_BALL = " 볼";
+    private static final String MESSAGE_NOTHING = "낫싱!";
+
+    private int strikeCount;
+    private int ballCount;
+    private boolean isWin = false;
+
+    public void ball() {
+        this.ballCount++;
+    }
+
+    public void strike() {
+        this.strikeCount++;
+
+        if(strikeCount > 2) {
+            isWin = true;
+        }
+    }
+
+    public boolean isEndGame() {
+        return isWin;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public boolean isFoul() {
+        return strikeCount == 0 && ballCount == 0;
+    }
+
+    public String resultScreen() {
+        if(isFoul()) {
+            return MESSAGE_NOTHING;
+        }
+        return makeResultMessage();
+    }
+
+    private String makeResultMessage() {
+        StringBuilder sb = new StringBuilder();
+        if(strikeCount > 0) {
+            sb.append(strikeCount).append(MESSAGE_STRIKE);
+        }
+
+        if(ballCount > 0) {
+            sb.append(ballCount).append(MESSAGE_BALL);
+        }
+
+        return sb.toString();
+    }
+}
