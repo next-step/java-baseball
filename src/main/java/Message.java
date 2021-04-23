8faef43b93de1838e53498ae397c8b35b36fc570
Karll <hankikab@gmail.com>
2021-04-23 18:21:18 +0900

    - 메세지 메서드 주석 추가

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -4,22 +4,39 @@ public class Message {
     private final String NOT_THREE_NUMBER = "숫자 3자리를 입력해주세요.";
     private final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다. 게임종료";
 
+    /**
+     * 입력 받기 메세지
+     */
     public void getInput() {
         System.out.println(this.GET_INPUT);
     }
 
+    /**
+     * 숫자가 아닌 입력값일때 메세지
+     */
     public void notNumber() {
         System.out.println(this.NOT_NUMBER);
     }
 
+    /**
+     * 3자리 숫자가 아닌 입력값일때 메세지
+     */
     public void notThreeNumber() {
         System.out.println(this.NOT_THREE_NUMBER);
     }
 
+    /**
+     * 게임 종료 메세지
+     */
     public void gameOver() {
         System.out.println(this.GAME_OVER);
     }
 
+    /**
+     * 입력 결과 메세지
+     * @param strike 스크라이크 수
+     * @param ball 볼 수
+     */
     public void getResult(int strike, int ball) {
         String message = "";
         if (strike > 0) {
