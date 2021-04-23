dd7289bd392267fb99e15e4176592a0747519759
iia <kha2464@gmail.com>
2021-04-23 21:01:09 +0900

    refactor(conventions): static import에만 와일드 카드 허용

--- src/main/java/BaseBall.java
+++ src/main/java/BaseBall.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class BaseBall {
     private static final List<Integer> DEFAULT_RANGE = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
