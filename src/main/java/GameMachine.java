320e5c4347b166c29234577fd4dd8975d1deaa83
dohchoi91 <dohchoi91@gmail.com>
2021-04-23 14:37:28 +0900

    feat: 유효성 검사 추가
    게임머신의 사용자가 전달한 값의 유효성 검사
    - 전달 받은 값의 수 확인
    - 전달 받은 값의 0~9의 숫자인지 확인
    - 유효성 검사에 대한 테스트 작성

--- src/main/java/GameMachine.java
+++ src/main/java/GameMachine.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -29,13 +30,34 @@ public class GameMachine {
             isRandomValue(value);
             randomSet.add(value);
         }
-        if (randomSet.size() < RANDOM_VALUE_MAX_LENGTH) {
-            throw new IllegalStateException("중복된 값이 존재합니다.");
-        }
+        if (randomSet.size() < RANDOM_VALUE_MAX_LENGTH)
+            throw new IllegalStateException("중복된 값이 존재합니다");
     }
 
     private void isRandomValue(int value) {
         if (value < 1 || value > 9)
-            throw new IllegalArgumentException("랜덤 값은 1 ~ 9까지 수여야 합니다.");
+            throw new IllegalArgumentException("랜덤 값은 1 ~ 9까지 수여야 합니다");
+    }
+    private void isRandomValue(String value) {
+        String regExp="^[0-9]";
+        if (!value.matches(regExp))
+            throw new IllegalArgumentException("랜덤 값은 1 ~ 9까지 수여야 합니다");
+    }
+
+    public boolean isValid(String[] inputs) {
+        Set<Integer> inputSet = new HashSet<>();
+
+        if (inputs.length != RANDOM_VALUE_MAX_LENGTH)
+            throw new IllegalArgumentException("입력된 값의 길이는 반드시 3이어야 합니다");
+
+        for (String value : inputs) {
+            isRandomValue(value);
+            inputSet.add(Integer.parseInt(value));
+        }
+
+        if (inputSet.size() < RANDOM_VALUE_MAX_LENGTH)
+            throw new IllegalStateException("중복된 값이 존재합니다");
+
+        return true;
     }
 }
--- src/test/java/GameMachineTest.java
+++ src/test/java/GameMachineTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@DisplayName("게임머신 테스트")
 class GameMachineTest {
 
     @Test
@@ -31,8 +31,34 @@ class GameMachineTest {
         assertThat(gameMachine.getRandomSetSize()).isEqualTo(3);
 
         assertThatIllegalStateException().isThrownBy(() -> GameMachine.of().init(new int[]{1, 1, 3}))
-                .withMessage("중복된 값이 존재합니다.");
+                .withMessage("중복된 값이 존재합니다");
         assertThatIllegalArgumentException().isThrownBy(() -> GameMachine.of().init(new int[]{1, 2, 11}))
-                .withMessage("랜덤 값은 1 ~ 9까지 수여야 합니다.");
+                .withMessage("랜덤 값은 1 ~ 9까지 수여야 합니다");
+
+    }
+
+    @Test
+    @DisplayName("입력값 유효성 검사")
+    void isValidTest() {
+        assertThat(GameMachine.of().isValid(new String[]{"1", "2", "3"})).isTrue();
+        assertThat(GameMachine.of().isValid(new String[]{"4", "5", "6"})).isTrue();
+        assertThat(GameMachine.of().isValid(new String[]{"7", "8", "9"})).isTrue();
+
+        assertThatIllegalArgumentException().isThrownBy(() -> GameMachine.of().isValid(new String[]{"a", "b", "c"}))
+                .withMessage("랜덤 값은 1 ~ 9까지 수여야 합니다");
+        assertThatIllegalArgumentException().isThrownBy(() -> GameMachine.of().isValid(new String[]{"11", "2", "3"}))
+                .withMessage("랜덤 값은 1 ~ 9까지 수여야 합니다");
+        assertThatIllegalStateException().isThrownBy(() -> GameMachine.of().isValid(new String[]{"1", "2", "2"}))
+                .withMessage("중복된 값이 존재합니다");
+    }
+
+    @Test
+    @DisplayName("입력값 길이 유효성 검사")
+    void isValidLengthTest() {
+        assertThatIllegalArgumentException().isThrownBy(() -> GameMachine.of().isValid(new String[]{"1", "2"}))
+                .withMessage("입력된 값의 길이는 반드시 3이어야 합니다");
+
+        assertThatIllegalArgumentException().isThrownBy(() -> GameMachine.of().isValid(new String[]{"1", "2", "3", "4"}))
+                .withMessage("입력된 값의 길이는 반드시 3이어야 합니다");
     }
 }
