d3059b94af6e940684c5aa4a9bb2ff97aa52195d
Karll <hankikab@gmail.com>
2021-04-23 18:00:02 +0900

    - 게임중 여부 체크 판단값 추가

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -1,3 +1,15 @@
 public class BaseBallGame {
+    private boolean isRunning;
 
+    public BaseBallGame() {
+        this.isRunning = true;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
 }
