83fd730346268153dd58ffd6f42d268ba9c52590
HyungjuJang <dacapolife87@gmail.com>
2021-04-24 00:34:45 +0900

    test : pitching 클래스의 테스트케이스 - 순번화 숫자를 입력받아 객체생성하는 테스트

---
+++ src/test/java/domain/PitchingTest.java
@@ -0,0 +1,19 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PitchingTest {
+
+    @DisplayName("Pitching 객체 생성 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:2","2:7","3:3"}, delimiter = ':')
+    void createPitchingObject(int sequence, int pitchingNumber) {
+        Pitching pitching = new Pitching(sequence, pitchingNumber);
+        assertThat(sequence).isEqualTo(pitching.pitchingSequence());
+        assertThat(pitchingNumber).isEqualTo(pitching.pitching());
+    }
+}
