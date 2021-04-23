23f1e46bef9981816bb728f65bfbc3daf866e603
shpark <bingbingpa@gmail.com>
2021-04-23 16:44:37 +0900

    feat: 정답과 입력받은 숫자의 값과 자리가 일치할 경우 스트라이크 힌트 출력하기. 3자리 모두 맞추면 게임 종료.

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -2,32 +2,47 @@ package baseball;
 
 import util.RandomGenerator;
 
-import java.util.Set;
+import java.util.List;
 
 public class BaseBallGame {
-    private final Set<Integer> numbers;
-    private int count = 0;
+    private final int MAX_STRIKE_COUNT = 3;
+    private final List<Integer> numbers;
+    private int strikeCount = 0;
+    private int ballCount = 0;
 
     public BaseBallGame(RandomGenerator randomGenerator) {
-        numbers = randomGenerator.makeNumbersLessThanTen();
+        this.numbers = randomGenerator.makeNumbersLessThanTen();
+    }
+
+    public boolean isCompleted() {
+        return strikeCount == MAX_STRIKE_COUNT;
     }
 
     public String guess(String str) {
-        int index = 0;
-        for (Integer number : numbers) {
-            Integer input = str.charAt(index) - '0';
-            matchCheck(number, input);
-            index++;
+        int length = numbers.size();
+        for (int index = 0; index < length; index++) {
+            matchStrike(str.charAt(index) - '0', index);
         }
-        if (count == 0) {
-            return BaseBall.NOTHING.getValue();
-        }
-        return "알수없음";
+
+        return printMessage();
     }
 
-    private void matchCheck(Integer number, Integer input) {
-        if (number.equals(input)) {
-            count++;
+    private void matchStrike(int input, int index) {
+        if (numbers.get(index).equals(input)) {
+            strikeCount++;
         }
+    }
+
+    private String printMessage() {
+        String message = BaseBall.NOTHING.getValue();
+        if (strikeCount > 0) {
+            message = strikeCount + " " + BaseBall.STRIKE.getValue();
+        }
+
+        if (ballCount > 0) {
+            message = BaseBall.BALL.getValue();
+        }
+
+        return message;
     }
 }
--- src/test/java/baseball/BaseBallGameTest.java
+++ src/test/java/baseball/BaseBallGameTest.java
@@ -12,10 +12,39 @@ class BaseBallGameTest {
 
     @DisplayName("정답과 같은 수가 전혀 없는 경우 테스트")
     @ParameterizedTest
-    @CsvSource(value = {"248:낫싱", "378:낫싱", "246:낫싱", "134:알수없음", "589:알수없음"}, delimiter = ':')
+    @CsvSource(value = {"248:낫싱", "378:낫싱", "246:낫싱", "134:1 스트라이크", "589:1 스트라이크"}, delimiter = ':')
     void all_numbers_mismatch(String input, String expected) {
-        BaseBallGame baseBallGame = new BaseBallGame(new RandomNumberGeneratorStub(new int[]{1, 5, 9}));
+        BaseBallGame baseBallGame = new BaseBallGame(new RandomNumberGeneratorStub("159"));
         String message = baseBallGame.guess(input);
         assertThat(message).isEqualTo(expected);
+    }
+
+    @DisplayName("같은 수가 같은 자리에 있으면 스트라이크를 출력하는지 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"367:2 스트라이크", "123:낫싱", "346:1 스트라이크"}, delimiter = ':')
+    void match_index_and_number(String input, String expected) {
+        BaseBallGame baseBallGame = new BaseBallGame(new RandomNumberGeneratorStub("369"));
+        String message = baseBallGame.guess(input);
+        assertThat(message).isEqualTo(expected);
+    }
+
+    @DisplayName("스트라이크 카운트를 출력하는 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"127:2 스트라이크", "124:1 스트라이크"}, delimiter = ':')
+    void match_index_and_number_return_message(String input, String expected) {
+        BaseBallGame baseBallGame = new BaseBallGame(new RandomNumberGeneratorStub("137"));
+        String message = baseBallGame.guess(input);
+        assertThat(message).isEqualTo(expected);
+    }
+
+    @DisplayName("3 스트라이크일 경우 게임이 종료되는지 테스트")
+    @Test
+    void game_end_when_three_strike() {
+        BaseBallGame baseBallGame = new BaseBallGame(new RandomNumberGeneratorStub("369"));
+        baseBallGame.guess("369");
+        assertThat(baseBallGame.isCompleted()).isTrue();
+        BaseBallGame baseBallGame2 = new BaseBallGame(new RandomNumberGeneratorStub("369"));
+        baseBallGame.guess("362");
+        assertThat(baseBallGame.isCompleted()).isFalse();
     }
 }
