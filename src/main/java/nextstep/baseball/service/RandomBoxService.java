a653a6ecc4c7a130d43c3bc5d279b7dc3e2626dc
ekcho <ekcho@ncsoft.com>
2021-04-23 16:22:24 +0900

    add(classfile) : 프로덕트 코드 생성
     - RandomBoxService
     - RandomBoxServiceImpl
     - Main

--- src/main/java/nextstep/baseball/service/RandomBoxService.java
+++ src/main/java/nextstep/baseball/service/RandomBoxService.java
@@ -3,6 +3,7 @@ package nextstep.baseball.service;
 import java.util.List;
 
 public interface RandomBoxService {
+    List<Integer> inputUserNumber(String str);
     void setRandomBox();
     List<Integer> createRandomNumber();
 }
