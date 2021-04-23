0eaea727b94e1f808d15fa34c045a1b2620452e8
ohgillwhan <soora_genius@naver.com>
2021-04-23 21:53:18 +0900

    feat : 공들안에는 같은 숫자가 존재해서는 안된다.

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -6,13 +6,18 @@ import java.util.List;
 import java.util.Set;
 
 public class Balls {
-    private final Set<Ball> balls;
+    private final List<Ball> balls;
 
     public Balls(Ball ...balls) {
         this(Arrays.asList(balls));
     }
 
     public Balls(List<Ball> balls) {
-        this.balls = new HashSet<>(balls);
+        Set<Ball> ballSet = new HashSet<>(balls);
+        if(ballSet.size() != balls.size()) {
+            throw new IllegalArgumentException("같은 공의 숫자를 가질 수 없습니다.");
+        }
+
+        this.balls = balls;
     }
 }
--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -3,6 +3,7 @@ package baseball.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class BallsTest {
@@ -10,5 +11,11 @@ public class BallsTest {
     @DisplayName("공들을 생성할 때 공을 여러개 받을 수 있다")
     public void 공들을_생성할_때_공을_여러개_받을_수_있다() {
         assertThatNoException().isThrownBy(() -> new Balls(Ball.of(1), Ball.of(2), Ball.of(3)));
+    }
+
+    @Test
+    @DisplayName("공들안에는 같은 숫자가 존재해서는 안된다")
+    public void 공들안에는_같은_숫자가_존재해서는_안된다() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Balls(Ball.of(1), Ball.of(1)));
     }
 }
