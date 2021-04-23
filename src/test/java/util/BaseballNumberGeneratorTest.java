0dd205ce0314e1f72633b4e125cc1e48ea95679b
HyungjuJang <dacapolife87@gmail.com>
2021-04-23 22:45:23 +0900

    test : 임의숫자 3개 메이커 테스트 - 1~9까지의 숫자를 섞은뒤 3개를 반환하여 크기가 3인지 확인 - Set에 추가하여 중복이없는지 확인

---
+++ src/test/java/util/BaseballNumberGeneratorTest.java
@@ -0,0 +1,25 @@
+package util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BaseballNumberGeneratorTest {
+
+    @Test
+    @DisplayName("베이스볼 생성 번호의 갯수가 3개인지 확인 테스트")
+    void generateBaseballNumberTest() {
+        List<Integer> generatedNumbers = BaseballNumberGenerator.generator();
+        Set<Integer> numberSet = new HashSet<>(generatedNumbers);
+
+        generatedNumbers.stream().forEach(System.out::print);
+        assertThat(numberSet).hasSize(3);
+        assertThat(BaseballNumberGenerator.generator()).hasSize(3);
+    }
+
+}
