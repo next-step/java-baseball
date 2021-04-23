7c3d7b7f84981e789e01902edd4a4b6733fdc4ff
khg <gusrn160304@gmail.com>
2021-04-24 00:31:45 +0900

    볼카운트 세는 기능 테스트 코드 추가

--- src/test/java/study/BaseballTest.java
+++ src/test/java/study/BaseballTest.java
@@ -2,6 +2,8 @@ package study;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import baseball.BaseballGame;
+import baseball.BaseballGame.BallCount;
 
 public class BaseballTest {
 
@@ -26,5 +29,21 @@ public class BaseballTest {
 	@DisplayName("임의의 숫자 생성 기능 테스트")
 	public void generate_random_list_test() {
 		assertThat(inputListValue).isNotEqualTo(randomListValue);
+	}
+	
+	@Test
+	@DisplayName("볼카운트 모두 스트라이크 성공 테스트")
+	public void ballcount_all_strike_pass_test() {
+		BallCount ballcnt = BaseballGame.ballCount(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
+		assertThat(ballcnt.isAllStrike())
+			.isEqualTo(true);
+	}
+	
+	@Test
+	@DisplayName("볼카운트 모두 스트라이크 실패 테스트")
+	public void ballcount_all_strike_fail_test() {
+		BallCount ballcnt = BaseballGame.ballCount(Arrays.asList(1,2,4), Arrays.asList(1,2,3));
+		assertThat(ballcnt.isAllStrike())
+			.isEqualTo(false);
 	}
 }
