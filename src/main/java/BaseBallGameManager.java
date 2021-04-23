0d7b5037a2f46ce8e22841727d2ed42b1ca89ace
MaximSungmo <ksm5318@naver.com>
2021-04-24 00:12:49 +0900

    feat(BaseBallGameManager): 컴퓨터 난수 및 사용자 입력값 비교 로직 (게임 구현)
    - 컴퓨터 난수와 사용자의 입력값을 비교

--- src/main/java/BaseBallGameManager.java
+++ src/main/java/BaseBallGameManager.java
@@ -1,8 +1,32 @@
 import java.util.*;
 
 public class BaseBallGameManager {
+    private boolean playGame = true;
+    private String pitcherNumber;
+    private final BaseBallReferee referee = new BaseBallReferee();
+    private final BaseBallBatter batter = new BaseBallBatter();
+    private final BaseBallPitcher pitcher = new BaseBallPitcher();
 
     public void start() {
+        pitcherNumber = getPitcherNumber();
+        System.out.println(pitcherNumber);
+        while (playGame) {
+            String batterNumber = batter.getUserInput();
+            // 심판 입장
+            int strikeCount = referee.judgeGame(pitcherNumber, batterNumber);
+        }
+    }
 
+    private String implode(Set<Integer> set) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Integer integer : set) {
+            stringBuilder.append(integer);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getPitcherNumber() {
+        Set<Integer> pitcherChoice = pitcher.generateRandomNumber();
+        return implode(pitcherChoice);
     }
 }
--- src/main/java/PlayGame.java
+++ src/main/java/PlayGame.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class PlayGame {
 
     public static final int RANDOM_NUMBER_LENGTH = 3;
-    
+    public static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         new BaseBallGameManager().start();
     }
