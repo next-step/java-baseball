49013291a89c753dec979846dc6e6fb2fd8346f7
dhmin5693 <dhmin5693@naver.com>
2021-04-23 11:34:33 +0900

    style: unused import 제거

--- src/main/java/domain/RandomNumbersGenerator.java
+++ src/main/java/domain/RandomNumbersGenerator.java
@@ -2,7 +2,6 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
