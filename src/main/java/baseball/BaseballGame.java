c1d75d2e3e94b2321acee3de581f673f10864856
khg <gusrn160304@gmail.com>
2021-04-24 00:29:54 +0900

    볼카운트 세는 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,6 +2,7 @@ package baseball;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Collections;
 
 public class BaseballGame {
@@ -21,4 +22,64 @@ public class BaseballGame {
 		}
 		return Collections.unmodifiableList(randList);
  	}
+	
+	public static BallCount ballCount(final List<Integer> inputIntegerList, final List<Integer> randomIntegerList) {
+		int len = randomIntegerList.size();		
+		BallCount ballCount = new BallCount(len);
+		for(int i = 0; i < len; ++i) {
+			countStrikeOrBallElseNothing(inputIntegerList.get(i), randomIntegerList.get(i), randomIntegerList, ballCount);
+		}
+		return ballCount;
+	}
+	
+	public static void countStrikeOrBallElseNothing(final int inputValue, final int randomValue, final List<Integer> randomIntegerList, final BallCount ballCount) {
+		if(inputValue == randomValue) {
+			ballCount.addStrikeCount(1);
+			return;
+		}
+		if(randomIntegerList.contains(inputValue)) {
+			ballCount.addBallCount(1);
+			return;
+		}
+		ballCount.addNothingCount(1);
+	}
+	
+	public static class BallCount {
+		private int strikeCount = 0;
+		private int ballCount = 0;
+		private int nothingCount = 0;
+		private int totalBallCount = 0;
+		
+		public BallCount(int totalBallCount)  {
+			this.totalBallCount = totalBallCount;
+		}
+		
+		public void addStrikeCount(int cnt) {
+			this.strikeCount += cnt;
+		}
+		
+		public void addBallCount(int cnt) {
+			this.ballCount += cnt;
+		}
+		
+		public void addNothingCount(int cnt) {
+			this.nothingCount += cnt;
+		}
+		
+		public void printBallCount() {			
+			String[] ballCountStrArr = {"", ""};
+			if(strikeCount > 0) ballCountStrArr[0] = strikeCount + " 스트라이크";
+			if(ballCount > 0) ballCountStrArr[1] = ballCount + " 볼";
+			if(nothingCount == totalBallCount) ballCountStrArr[0] = "낫싱";
+			System.out.println(String.join(" ", ballCountStrArr));
+		}
+		
+		public boolean isAllStrike() {
+			if(strikeCount == totalBallCount) {
+				return true;
+			}
+			
+			return false;
+		}
+	}
 }
