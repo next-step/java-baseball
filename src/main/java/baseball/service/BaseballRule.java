7121e1aa13d5b72a5a5f8b5b80605cb81603c624
tmkwak <tmkwak@hmm21.com>
2021-04-23 17:11:22 +0900

    feat(BaseballRule) : create BaseballRull

--- src/main/java/baseball/service/BaseballRule.java
+++ src/main/java/baseball/service/BaseballRule.java
@@ -1,4 +1,45 @@
 package baseball.service;
 
+import baseball.domain.BallNumber;
+import baseball.domain.BaseballResult;
+
 public class BaseballRule {
+
+	private BallNumber playerBallNumber;
+	private BallNumber computerBallNumber;
+	private BaseballResult baseballResult;
+	private int ballSize;
+
+	public BaseballRule(BallNumber playerBallNumber, BallNumber computerBallNumber) {
+		this.playerBallNumber = playerBallNumber;
+		this.computerBallNumber = computerBallNumber;
+		this.ballSize = BallNumber.SIZE;
+		compareBallNumbers();
+	}
+
+	public BaseballResult getResult() {
+		return this.baseballResult;
+	}
+
+	//todo: indent < 2 줄여야 합니다. refactoring 필요
+	private void compareBallNumbers() {
+		BaseballResult baseballResult = new BaseballResult();
+		int index = 0;
+		while (index < this.ballSize){
+			checkStrike(baseballResult, index);
+			checkBall(baseballResult, index);
+			index++;
+		}
+		this.baseballResult = baseballResult;
+	}
+
+	private BaseballResult checkStrike(BaseballResult baseballResult, int index) {
+		if(this.playerBallNumber.getNumberByIndex(index) == this.computerBallNumber.getNumberByIndex(index))
+			baseballResult.addStrikeCount();
+		return baseballResult;
+	}
+
+	private BaseballResult checkBall(BaseballResult baseballResult, int index) {
+		return baseballResult;
+	}
 }
