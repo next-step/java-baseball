f97fe49cf1b0a1972c76f944a9e8f9438429b7c3
prodo-developer <wjdals300@gmail.com>
2021-04-24 03:25:47 +0900

    feat(UtilMeassage) : 야구게임 메시지 구현

---
+++ src/main/java/utils/UtilMeassage.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class UtilMeassage {
+
+    public static final String STRIKE = " 스트라이크 ";
+    public static final String BALL = " 볼 ";
+    public static final String NOTHING = "낫싱";
+    public static final String REQUIRE_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String REQUIRE_RESTART_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String CORRECT_ANSWER_AND_EXIT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료.";
+
+    public static final String BALL_SIZE_EXCEPTION_MESSAGE = "허용 범위 안에 포함되지 않습니다 (0~3)";
+    public static final String BASEBALL_SIZE_EXCEPTION_MESSAGE = "자리수가 올바르지 않습니다. 세자리로 입력해주세요.";
+    public static final String BASEBALL_DUPLICATE_EXCEPTION_MESSAGE = "중복된 숫자가 존재합니다.";
+    public static final String INPUT_NUMBER_FORMAT_EXCEPTION_MESSAGE = "각 자리의 수는 1부터 9까지의 숫자만 가능합니다.";
+    public static final String NULL_EXCEPTION_MESSAGE = "입력값이 NULL이 될 수 없습니다.";
+
+    public static final String START = "야구게임 시작";
+    public static final String END = "야구게임 완전히 종료";
+}
