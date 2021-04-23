3743282d4aa0bfb0ee0d584376ea85b4ea194dac
dohchoi91 <dohchoi91@gmail.com>
2021-04-23 17:33:46 +0900

    feat: 체크 결과 문자열 출력 기능 추가
    게임머신의 검사 결과를 문자열로 출력

--- src/main/java/GameMachine.java
+++ src/main/java/GameMachine.java
@@ -60,6 +60,11 @@ public class GameMachine {
         return true;
     }
 
+    /**
+     * 사용자 입력에 대한 결과 확인
+     * @param inputs 사용자가 전달한 String 배열
+     * @return 체크 결과에 대한 int 배열
+     */
     public int[] check(String[] inputs) {
         int[] result = new int[RANDOM_VALUE_MAX_LENGTH];
         List<Integer> randomList = new ArrayList<>(randomSet);
@@ -75,5 +80,23 @@ public class GameMachine {
 
     private int getIndexResult(int random, int target) {
         return (random == target) ? ResultStatus.STRIKE.getIndex() : ResultStatus.BALL.getIndex();
+    }
+
+    /**
+     * 체크 결과 문자열 출력
+     * @param result 체크결과로 받은 int 배열
+     * @return 결과에 대한 문자열
+     */
+    public String print(int[] result) {
+        if (result[ResultStatus.NOTING.getIndex()] == 3)
+            return ResultStatus.NOTING.getName();
+
+        StringBuilder stb = new StringBuilder();
+        if (result[ResultStatus.STRIKE.getIndex()] > 0)
+            stb.append(result[ResultStatus.STRIKE.getIndex()]).append(ResultStatus.STRIKE.getName()).append(" ");
+        if (result[ResultStatus.BALL.getIndex()] > 0)
+            stb.append(result[ResultStatus.BALL.getIndex()]).append(ResultStatus.BALL.getName());
+
+        return stb.toString().trim();
     }
 }
--- src/test/java/GameMachineTest.java
+++ src/test/java/GameMachineTest.java
@@ -68,16 +68,27 @@ class GameMachineTest {
         GameMachine gameMachine = GameMachine.of();
         gameMachine.init(new int[]{1, 2, 3});
 
-        String[] input1 = new String[]{"1", "2", "4"}; // 2 스트라이크
-        String[] input2 = new String[]{"1", "4", "2"}; // 1 스트라이크 1볼
+        String[] input1 = new String[]{"1", "2", "4"}; // 2스트라이크
+        String[] input2 = new String[]{"1", "4", "2"}; // 1스트라이크 1볼
         String[] input3 = new String[]{"7", "8", "9"}; // 낫싱
-        String[] input4 = new String[]{"3", "8", "9"}; // 1 볼
-        String[] input5 = new String[]{"1", "2", "3"}; // 3 스트라이크
+        String[] input4 = new String[]{"3", "8", "9"}; // 1볼
+        String[] input5 = new String[]{"1", "2", "3"}; // 3스트라이크
 
         assertThat(gameMachine.check(input1)).isEqualTo(new int[]{2, 0, 1});
         assertThat(gameMachine.check(input2)).isEqualTo(new int[]{1, 1, 1});
         assertThat(gameMachine.check(input3)).isEqualTo(new int[]{0, 0, 3});
         assertThat(gameMachine.check(input4)).isEqualTo(new int[]{0, 1, 2});
         assertThat(gameMachine.check(input5)).isEqualTo(new int[]{3, 0, 0});
+    }
+
+    @Test
+    @DisplayName("검사 결과 출력")
+    void printTest() {
+        GameMachine gameMachine = GameMachine.of();
+        assertThat(gameMachine.print(new int[]{2, 0, 1})).isEqualTo("2스트라이크");
+        assertThat(gameMachine.print(new int[]{1, 1, 1})).isEqualTo("1스트라이크 1볼");
+        assertThat(gameMachine.print(new int[]{0, 0, 3})).isEqualTo("낫싱");
+        assertThat(gameMachine.print(new int[]{0, 1, 2})).isEqualTo("1볼");
+        assertThat(gameMachine.print(new int[]{3, 0, 0})).isEqualTo("3스트라이크");
     }
 }
