977e8cc8bfc3993643a53bde1e5e06f55179f523
iia <kha2464@gmail.com>
2021-04-23 20:37:03 +0900

    test(BaseBall): 야구게임 정답 불일치 케이스별 테스트케이스 생성

--- src/test/java/BaseBallTest.java
+++ src/test/java/BaseBallTest.java
@@ -11,7 +11,10 @@ public class BaseBallTest {
     @DisplayName("야구게임 정답 난수 생성")
     @Test
     public void testMakeRandomAnswer() {
+        //when
         List<Integer> answer = BaseBall.makeAnswer();
+
+        //then
         assertThat(answer.size()).isEqualTo(3);
         assertThat(answer.size()).isEqualTo(new HashSet<>(answer).size());
     }
@@ -30,16 +33,14 @@ public class BaseBallTest {
         assertThat(game.getUserResult(answer).getStrike()).isEqualTo(3);
     }
 
-    @DisplayName("야구게임 정답 불일치")
+    @DisplayName("야구게임 정답 불일치1(0스트라이크 0볼)")
     @Test
-    public void testAnswerIsWrong() {
+    public void testAnswerIsWrong1() {
         //given
         List<Integer> answer = Arrays.asList(1, 2, 3);
 
         //when
         List<Integer> notMatch = Arrays.asList(4, 5, 6);
-        List<Integer> ball3 = Arrays.asList(3, 1, 2);
-        List<Integer> ball2strike1 = Arrays.asList(1, 3, 2);
 
         BaseBall game = new BaseBall(answer);
 
@@ -47,11 +48,37 @@ public class BaseBallTest {
         assertThat(game.getUserResult(notMatch).getIsAnswer()).isEqualTo(false);
         assertThat(game.getUserResult(notMatch).getStrike()).isEqualTo(0);
         assertThat(game.getUserResult(notMatch).getBall()).isEqualTo(0);
+    }
 
+    @DisplayName("야구게임 정답 불일치(0스트라이크 3볼)")
+    @Test
+    public void testAnswerIsWrong2() {
+        //given
+        List<Integer> answer = Arrays.asList(1, 2, 3);
+
+        //when
+        List<Integer> ball3 = Arrays.asList(3, 1, 2);
+
+        BaseBall game = new BaseBall(answer);
+
+        //then
         assertThat(game.getUserResult(ball3).getIsAnswer()).isEqualTo(false);
         assertThat(game.getUserResult(ball3).getStrike()).isEqualTo(0);
         assertThat(game.getUserResult(ball3).getBall()).isEqualTo(3);
+    }
 
+    @DisplayName("야구게임 정답 불일치3(1스트라이크 2볼)")
+    @Test
+    public void testAnswerIsWrong3() {
+        //given
+        List<Integer> answer = Arrays.asList(1, 2, 3);
+
+        //when
+        List<Integer> ball2strike1 = Arrays.asList(1, 3, 2);
+
+        BaseBall game = new BaseBall(answer);
+
+        //then
         assertThat(game.getUserResult(ball2strike1).getIsAnswer()).isEqualTo(false);
         assertThat(game.getUserResult(ball2strike1).getStrike()).isEqualTo(1);
         assertThat(game.getUserResult(ball2strike1).getBall()).isEqualTo(2);
