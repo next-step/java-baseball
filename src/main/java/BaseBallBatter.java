0c2e9b93c4bdc2fa65f9f7ad26bec538afc6623e
MaximSungmo <ksm5318@naver.com>
2021-04-24 00:14:14 +0900

    refactor(BaseBallBatter): 스캐너 위치 변경, while 조건문 수정
    - 스캐너 위치 변경 및 while 조건문 에러 수정

--- src/main/java/BaseBallBatter.java
+++ src/main/java/BaseBallBatter.java
@@ -6,16 +6,14 @@ import java.util.Scanner;
  * receive command line input, valid number input only
  */
 public class BaseBallBatter {
-    final private Scanner scanner = new Scanner(System.in);
-
     public String getUserInput() {
         boolean pass;
         String input;
         do {
             System.out.println("숫자를 입력해주세요 : ");
-            input = scanner.nextLine();
+            input = PlayGame.scanner.nextLine();
             pass = ValidationInputUtils.validInputNumber(input);
-        } while (pass);
+        } while (!pass);
 
         return input;
     }
