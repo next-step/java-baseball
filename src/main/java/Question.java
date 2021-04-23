31347892032feb23a3a972ca839953a03fde4139
DongHyeokJeong <ddkkd1@naver.com>
2021-04-24 00:01:57 +0900

    feature(init): add init method in Question class
    Added new method:
    - init method initialize question
    - init method test added
    - setAndGetUserQuestionTest are divided into two tests
    
    BREAKING CHANGE:  init method which initialize user question was added in Question class and modified QuestionTest

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -1,13 +1,19 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Question {
 	private List<Integer> question;
 
 	public void setUserQuestion(List<Integer> userQuestion) {
+		//검증 필요
 		this.question = userQuestion;
 	}
 
 	public List<Integer> getUserQuestion() {
 		return this.question;
+	}
+
+	public void init() {
+		this.question = new ArrayList<Integer>();
 	}
 }
--- src/test/java/QuestionTest.java
+++ src/test/java/QuestionTest.java
@@ -3,23 +3,47 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class QuestionTest {
-	private Question question = new Question();
+	private Question question;
 
-	@Test
-	void setAndGetUserQuestionTest() {
+	@BeforeEach
+	void setUp() {
+		question = new Question();
 		List<Integer> userQuestion = new ArrayList<>();
 		userQuestion.add(1);
 		userQuestion.add(2);
 		userQuestion.add(3);
-
 		question.setUserQuestion(userQuestion);
+	}
+
+	@Test
+	void setUserQuestionTest() {
+		List<Integer> userQuestion = new ArrayList<>();
+		userQuestion.add(3);
+		userQuestion.add(2);
+		userQuestion.add(1);
+		question.setUserQuestion(userQuestion);
+
+		assertThat(question.getUserQuestion().size()).isEqualTo(3);
+		assertThat(question.getUserQuestion().get(0)).isEqualTo(3);
+		assertThat(question.getUserQuestion().get(1)).isEqualTo(2);
+		assertThat(question.getUserQuestion().get(2)).isEqualTo(1);
+	}
+
+	@Test
+	void getUserQuestionTest() {
 		assertThat(question.getUserQuestion().size()).isEqualTo(3);
 		assertThat(question.getUserQuestion().get(0)).isEqualTo(1);
 		assertThat(question.getUserQuestion().get(1)).isEqualTo(2);
 		assertThat(question.getUserQuestion().get(2)).isEqualTo(3);
 	}
 
+	@Test
+	void initUserQuestionTest() {
+		question.init();
+		assertThat(question.getUserQuestion().size()).isEqualTo(0);
+	}
 }
