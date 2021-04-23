8b34990aceb2d16e4d2a230b12c4c7f86357a516
HyungjuJang <dacapolife87@gmail.com>
2021-04-23 23:20:52 +0900

    feat : 입력받은 문자열을 숫자리스트로 변환하는 유틸 - 입력받은 문자열을 정수 List로 변환하는 기능

---
+++ src/main/java/util/TransStringNumberToNumberListUtil.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransStringNumberToNumberListUtil {
+
+    public static List<Integer> toNumberList(String numberString) {
+        List<Integer> numbers = new ArrayList<>();
+        char[] numberChar = numberString.toCharArray();
+        for (char number : numberChar) {
+            numbers.add(Integer.parseInt(String.valueOf(number)));
+        }
+
+        return numbers;
+    }
+}
