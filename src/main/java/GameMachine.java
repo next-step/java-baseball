e78e1a9bd18a2acfc64d5a1c4f040c02632654e4
dohchoi91 <dohchoi91@gmail.com>
2021-04-23 12:24:32 +0900

    feat: 게임 랜덤값 설정 기능 추가
    게임머신의 랜덤값 설정 추가
    - 파라미터가 없을 경우 랜덤으로 지정
    - 파라미터가 있을 경우 전달 받은 파라미터로 설정
    - 랜덤값 설정에 대한 테스트 작성

--- src/main/java/GameMachine.java
+++ src/main/java/GameMachine.java
@@ -14,4 +14,28 @@ public class GameMachine {
     public int getRandomSetSize() {
         return randomSet.size();
     }
+
+    public void init() {
+        randomSet.clear();
+        while (randomSet.size() < RANDOM_VALUE_MAX_LENGTH) {
+            int randomValue = (int) (Math.random() * 9) + 1;
+            randomSet.add(randomValue);
+        }
+    }
+
+    public void init(int[] inputs) {
+        randomSet.clear();
+        for (int value : inputs) {
+            isRandomValue(value);
+            randomSet.add(value);
+        }
+        if (randomSet.size() < RANDOM_VALUE_MAX_LENGTH) {
+            throw new IllegalStateException("중복된 값이 존재합니다.");
+        }
+    }
+
+    private void isRandomValue(int value) {
+        if (value < 1 || value > 9)
+            throw new IllegalArgumentException("랜덤 값은 1 ~ 9까지 수여야 합니다.");
+    }
 }
--- src/test/java/GameMachineTest.java
+++ src/test/java/GameMachineTest.java
@@ -1,17 +1,38 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class GameMachineTest {
 
     @Test
-    @DisplayName("GameMachine 생성 테스트")
+    @DisplayName("GameMachine 생성")
     void createGameMachineTest() {
         GameMachine gameMachine = GameMachine.of();
-        assertNotEquals(null, gameMachine);
-        assertEquals(0, gameMachine.getRandomSetSize());
+        assertThat(gameMachine).isNotNull();
+        assertThat(gameMachine.getRandomSetSize()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("랜덤값 설정")
+    void setRandomValueTest() {
+        GameMachine gameMachine = GameMachine.of();
+        gameMachine.init();
+        assertThat(gameMachine.getRandomSetSize()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("고정 랜덤값 설정")
+    void setFixedRandomValueTest() {
+        GameMachine gameMachine = GameMachine.of();
+        gameMachine.init(new int[]{1, 2, 3});
+        assertThat(gameMachine.getRandomSetSize()).isEqualTo(3);
+
+        assertThatIllegalStateException().isThrownBy(() -> GameMachine.of().init(new int[]{1, 1, 3}))
+                .withMessage("중복된 값이 존재합니다.");
+        assertThatIllegalArgumentException().isThrownBy(() -> GameMachine.of().init(new int[]{1, 2, 11}))
+                .withMessage("랜덤 값은 1 ~ 9까지 수여야 합니다.");
     }
 }
