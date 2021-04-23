350fc62218670c9935d0af0efef19b5a56aa5433
Karll <hankikab@gmail.com>
2021-04-23 17:45:32 +0900

    - 게임에 필요한 정적 메세지 출력 메서드 추가

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,3 +1,22 @@
 public class Message {
+    private final String GET_INPUT = "숫자를 입력해 주세요 : ";
+    private final String NOT_NUMBER = "숫자만 입력가능합니다.";
+    private final String NOT_THREE_NUMBER = "숫자 3자리를 입력해주세요.";
+    private final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다. 게임종료";
 
+    public void getInput() {
+        System.out.println(this.GET_INPUT);
+    }
+
+    public void notNumber() {
+        System.out.println(this.NOT_NUMBER);
+    }
+
+    public void notThreeNumber() {
+        System.out.println(this.NOT_THREE_NUMBER);
+    }
+
+    public void gameOver() {
+        System.out.println(this.GAME_OVER);
+    }
 }
