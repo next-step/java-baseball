924e90dcfab3a8b27ab0bba79c88cbdebef05adb
sanggu <tkdrn8578@naver.com>
2021-04-24 23:52:27 +0900

    refactor: Judgeable 인터페이스 추가에 따른 수정

--- src/main/java/BaseBall.java
+++ src/main/java/BaseBall.java
@@ -7,41 +7,21 @@ public class BaseBall {
     }
 
     public JudgementCountPair play(Number input) {
-        JudgementCountPair hint = new JudgementCountPair();
+        return addCountByAllJudge(input, new JudgementCountPair());
+    }
 
-        hint = addStrikeCounts(input, hint);
-        hint = addBallCounts(input, hint);
+    private JudgementCountPair addCountByAllJudge(Number input, JudgementCountPair hint) {
+        hint = addCountByJudge(input, hint, new StrikeJudgement());
+        hint = addCountByJudge(input, hint, new BallJudgement());
 
         return hint;
     }
 
-    private JudgementCountPair addBallCounts(Number input, JudgementCountPair hint) {
+    private JudgementCountPair addCountByJudge(Number input, JudgementCountPair hint, Judgeable judgeable) {
         int sizeOfInput = getSizeOfInput(input);
 
         for (int index = 0; index < sizeOfInput; index++) {
-            hint = addBallCountWhenEqualsOnlyValue(input, hint, index);
-        }
-
-        return hint;
-    }
-
-    private JudgementCountPair addBallCountWhenEqualsOnlyValue(Number input, JudgementCountPair hint, int index) {
-        if (isBall(input, index)) {
-            return hint.createAddedMatchCount(Judgements.BALL);
-        }
-
-        return hint;
-    }
-
-    private boolean isBall(Number input, int index) {
-        return answer.haveValue(input, index) && !isStrike(input, index);
-    }
-
-    private JudgementCountPair addStrikeCounts(Number input, JudgementCountPair hint) {
-        int sizeOfInput = getSizeOfInput(input);
-
-        for (int index = 0; index < sizeOfInput; index++) {
-            hint = addStrikeCountWhenEqualsValueAndPosition(input, hint, index);
+            hint = judgeable.addMatchCount(answer, input, hint, index);
         }
 
         return hint;
@@ -49,17 +29,5 @@ public class BaseBall {
 
     private int getSizeOfInput(Number input) {
         return input.length();
-    }
-
-    private JudgementCountPair addStrikeCountWhenEqualsValueAndPosition(Number input, JudgementCountPair hint, int index) {
-        if (isStrike(input, index)) {
-            return hint.createAddedMatchCount(Judgements.STRIKE);
-        }
-
-        return hint;
-    }
-
-    private boolean isStrike(Number input, int index) {
-        return answer.isStrike(input, index);
     }
 }
