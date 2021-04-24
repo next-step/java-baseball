ac8f2c8fa247e34256973053d5ae1f12a587fe80
Jeong Eunwoo <zenic88@naver.com>
2021-04-24 21:43:01 +0900

    refactor: formatter 변경

--- src/main/java/com/zenic88/baseball/Computer.java
+++ src/main/java/com/zenic88/baseball/Computer.java
@@ -6,20 +6,20 @@ import java.util.List;
 
 public class Computer {
 
-  public List<Integer> generate() {
-    List<Integer> numberList = getRandomNumberList();
-    while (numberList.size() > 3) {
-      numberList.remove(0);
-    }
-    return numberList;
-  }
+	public List<Integer> generate() {
+		List<Integer> numberList = getRandomNumberList();
+		while (numberList.size() > 3) {
+			numberList.remove(0);
+		}
+		return numberList;
+	}
 
-  private List<Integer> getRandomNumberList() {
-    List<Integer> randomNumberList = new ArrayList<>();
-    for (int i = 1; i < 10; i++) {
-      randomNumberList.add(i);
-    }
-    Collections.shuffle(randomNumberList);
-    return randomNumberList;
-  }
+	private List<Integer> getRandomNumberList() {
+		List<Integer> randomNumberList = new ArrayList<>();
+		for (int i = 1; i < 10; i++) {
+			randomNumberList.add(i);
+		}
+		Collections.shuffle(randomNumberList);
+		return randomNumberList;
+	}
 }
