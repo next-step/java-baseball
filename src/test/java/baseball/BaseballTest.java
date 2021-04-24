437d048ae812fd4153c4320d9c8c893d0c56cc21
ianjang <ianjang0718@gmail.com>
2021-04-24 18:08:33 +0900

    test: add test code for makeAnswerNumbers method

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.HashSet;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -14,5 +15,12 @@ public class BaseballTest {
 		baseball.makeAnswerNumbers();
 		List<Integer> answerNumbers = baseball.answerNumbers;
 		assertThat(answerNumbers).hasSize(3);
+
+		HashSet<Integer> uniqueSet = new HashSet<>(answerNumbers);
+		assertThat(uniqueSet).hasSize(3);
+
+		for(Integer number: answerNumbers) {
+			assertThat(number).isIn(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		}
 	}
 }
