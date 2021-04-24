a1a8f65e4e1474fbed5fc05ddd718294444f4608
Donghui-Lee <ehdgml3206@naver.com>
2021-04-24 16:58:12 +0900

    refactor: 테스트 성공을 위한 코드 수정

--- src/main/java/baseball/domain/PitchResult.java
+++ src/main/java/baseball/domain/PitchResult.java
@@ -1,12 +1,16 @@
 package baseball.domain;
 
+import java.util.Objects;
+
 public class PitchResult {
     private static final int STRIKE_COUNT = 3;
+    private static final int NO_COUNT = 0;
 
     private final int strikeCount;
     private final int ballCount;
 
     private PitchResult(int strikeCount, int ballCount) {
+        validate(strikeCount, ballCount);
         this.strikeCount = strikeCount;
         this.ballCount = ballCount;
     }
@@ -15,7 +19,28 @@ public class PitchResult {
         return new PitchResult(strikeCount, ballCount);
     }
 
+    private void validate(int strikeCount, int ballCount) {
+        if (strikeCount + ballCount > STRIKE_COUNT) {
+            throw new IllegalArgumentException("유효하지 않은 결과입니다");
+        }
+    }
+
     public boolean isFinished() {
         return this.strikeCount == STRIKE_COUNT;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        PitchResult that = (PitchResult)o;
+        return strikeCount == that.strikeCount && ballCount == that.ballCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(strikeCount, ballCount);
     }
 }
