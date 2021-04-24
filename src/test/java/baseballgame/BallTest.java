3d7e40c12f45adc41e9521fe8fefc01398a1b52e
KyungSik9870 <rudtlr9870@gmail.com>
2021-04-24 22:30:57 +0900

    test(Ball): 정답이 될 3자리의 임의의 수 테스트
    3개의 숫자 테스트 진행
    - 3개가 선택되었는지 확인.
    - 1부터 9까지의 숫자인지 확인.

---
+++ src/test/java/baseballgame/BallTest.java
@@ -0,0 +1,36 @@
+package baseballgame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallTest {
+
+    private Ball ball = new Ball();
+    private Set answer = new LinkedHashSet();
+
+    @BeforeEach
+    void setUpRandomAnswerBall() {
+        answer = ball.getAnswer();
+    }
+
+    @Test
+    @DisplayName("임의의 숫자가 3개 선택되었는지 테스트.")
+    void isThreeDigitsGeneratedRandomBalls() {
+        assertThat(answer.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10})
+    @DisplayName("생성된 정답의 각 자리 숫자가 1부터 9까지를 만족하는지 테스트.")
+    void isContainsZeroOrTen(int number) {
+        assertThat(answer.contains(number)).isFalse();
+    }
+}
