39e2906e6faf253030a16461123cf1362f9b89ff
daeyong-sim <48154dd3ccc2eff16343e00714bdf55ae2caa334>
2021-04-25 08:39:38 +0900

    feat(BaseballGame): add BaseballGame Class

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,21 @@
+public class BaseballGame {
+	public static void main(String[] args){
+
+		// TODO: play Baseball game
+		BaseballGame baseballGame = new BaseballGame();
+		baseballGame.playBaseballGame();
+
+		// TODO: select to replay game
+
+	}
+
+
+	public void playBaseballGame(){
+		// TODO: init numbers
+
+		// TODO: get input numbers
+		// TODO: show score
+		// TODO: check Game status
+	}
+
+}
