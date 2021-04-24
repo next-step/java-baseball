600b4b2948b704c527028e2893d9cd691ad5e993
KIM GI TAE <c0433c@gmail.com>
2021-04-24 17:29:51 +0900

    feat(number): add baseball numbers first class collection
    - validate baseball number's size is 3
    - validate duplicate
    - validate between 1 and 9

---
+++ src/main/java/number/BaseballNumbers.java
@@ -0,0 +1,76 @@
+package number;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BaseballNumbers {
+	private static final int SUITABLE_SIZE = 3;
+
+	private final List<Integer> baseballNumbers;
+
+	public BaseballNumbers(List<Integer> baseballNumbers) {
+		validateSuitableSize(baseballNumbers);
+		validateBetweenOneAndNine(baseballNumbers);
+		validateDuplication(baseballNumbers);
+		this.baseballNumbers = baseballNumbers;
+	}
+
+	private void validateSuitableSize(List<Integer> baseballNumbers) {
+		if (baseballNumbers.size() != SUITABLE_SIZE) {
+			throw new IllegalArgumentException("세자리의 수가 아닙니다.");
+		}
+	}
+
+	private void validateDuplication(List<Integer> baseballNumbers) {
+		Set<Integer> nonDuplicate = new HashSet<>(baseballNumbers);
+
+		if (baseballNumbers.size() != nonDuplicate.size()) {
+			throw new IllegalArgumentException("숫자에 중복이 존재합니다.");
+		}
+	}
+
+	private void validateBetweenOneAndNine(List<Integer> baseballNumbers) {
+		if (baseballNumbers.contains(Integer.valueOf(0))) {
+			throw new IllegalArgumentException("1~9의 숫자로 구성되어 있지 않습니다.");
+		}
+	}
+
+	public List<Integer> getBaseballNumbers() {
+		return baseballNumbers;
+	}
+
+	public int size() {
+		return baseballNumbers.size();
+	}
+
+	public Integer get(int index) {
+		return baseballNumbers.get(index);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (baseballNumbers == obj) {
+			return true;
+		}
+
+		if (obj == null || baseballNumbers.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		final List<Integer> compareBaseballNumbers = (List<Integer>) obj;
+
+		return baseballNumbers.equals(compareBaseballNumbers);
+	}
+
+	@Override
+	public int hashCode() {
+		return baseballNumbers.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return baseballNumbers.toString();
+	}
+
+}
