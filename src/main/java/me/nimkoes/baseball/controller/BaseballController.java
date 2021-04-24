65e7f96e8e924d74b9e9953cdccda024e01ae065
nimkoes <xxxelppa@gmail.com>
2021-04-24 20:55:27 +0900

    feat(CF05) : 게임 종료 기능 추가
    플레이어가 생성된 난수를 맞췄을 때, 게임을 종료할 수 있는 기능 추가

--- src/main/java/me/nimkoes/baseball/controller/BaseballController.java
+++ src/main/java/me/nimkoes/baseball/controller/BaseballController.java
@@ -14,18 +14,49 @@ public class BaseballController {
     private final PlayerInterface playerInterface;
     private final RandomNumberRepository randomNumberRepository;
 
+    private int strikeCount;
+    private int ballCount;
+
+    private String userInput;
+
     public BaseballController(PlayerInterface playerInterface) {
         this.playerInterface = playerInterface;
         this.randomNumberRepository = RandomNumberRepository.getInstance();
     }
 
+    /*
+     * 숫자 야구 게임 도입부
+     */
+    public void run() {
+        do {
+            setUp();     // 초기값 세팅
+            playBall();  // 숫자 야구 게임 시작
+        } while (playerInterface.checkContinue());
+    }
+
+    /*
+     * 새 게임을 시작할 때 초기값 세팅
+     */
+    private void setUp() {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+        this.userInput = "";
+
+        // 새 게임 시작시 새로운 난수를 생성해서 저장
+        randomNumberRepository.setTargetNumber(generateRandomNumber());
+    }
+
     private void playBall() {
+        do {
+            userInput = playerInterface.inputNumber();  // 사용자 입력
+            strikeCount = getStrikeCount(userInput);    // strike 계산
+            ballCount = getBallCount(userInput);        // ball 계산
 
-        String userInput = playerInterface.inputNumber();  // 사용자 입력
-        int strikeCount = getStrikeCount(userInput);       // strike 계산
-        int ballCount = getBallCount(userInput);           // ball 계산
+            playerInterface.printCountMessage(strikeCount, ballCount);  // 비교 결과 출력
 
-        playerInterface.printCountMessage(strikeCount, ballCount);  // 비교 결과 출력
+        } while (strikeCount != MainLauncher.LENGTH_OF_NUMBER);
+
+        playerInterface.printEndingMessage(MainLauncher.LENGTH_OF_NUMBER);
     }
 
     /*
