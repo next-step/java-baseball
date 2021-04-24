23bfd2d1540fd942a3a72d3e9e6e752aac16063b
HyungjuJang <dacapolife87@gmail.com>
2021-04-24 20:04:19 +0900

    feat : 베이스볼넘버 클래스 - 3개의 베이스볼 숫자의 리스트를 관리하는 일급컬렉션 - n번째 숫자의 반환 / 포함여부 / 일치하는지 판단하는 기능

---
+++ src/main/java/domain/BaseballNumbers.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.List;
+
+public class BaseballNumbers {
+
+    private static final String INVALID_BASEBALL_NUMBER_SIZE = "잘못된 베이스볼 숫자 갯수입니다.";
+    private static final int BASEBALL_NUMBER_SIZE = 3;
+
+    private List<Integer> numbers;
+
+    public BaseballNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+
+        if(numbers.size() != BASEBALL_NUMBER_SIZE) {
+            throw new IllegalArgumentException(INVALID_BASEBALL_NUMBER_SIZE);
+        }
+    }
+
+    public boolean contains(int playerNumber) {
+        return numbers.contains(playerNumber);
+    }
+
+    public int numberOfRound(int round) {
+        return numbers.get(round - 1);
+    }
+
+    public boolean isSameIndex(Pitching pitching) {
+        return (numbers.indexOf(pitching.pitching()) + 1) == pitching.pitchingSequence();
+    }
+}
