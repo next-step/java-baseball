f7855fee216c90b88aa53b3e92d35b592c445782
HyungjuJang <dacapolife87@gmail.com>
2021-04-24 20:08:40 +0900

    feat : 베이스볼넘버의 비즈지스로직이 담긴 클래스 - 볼 / 스트라이크 판단여부 기능

---
+++ src/main/java/domain/BaseballNumberScore.java
@@ -0,0 +1,38 @@
+package domain;
+
+
+public class BaseballNumberScore {
+
+    private BaseballNumbers numbers;
+
+    public BaseballNumberScore(BaseballNumbers numbers) {
+        this.numbers = numbers;
+    }
+
+    public BaseballResult judge(BaseballNumbers playerScore) {
+        BaseballResult baseballResult = new BaseballResult();
+
+        for(int i = 1; i < 4; i++) {
+            Pitching pitching = new Pitching(i, playerScore.numberOfRound(i));
+            comparePitching(baseballResult, pitching);
+        }
+
+        return baseballResult;
+    }
+
+    private void comparePitching(BaseballResult baseballResult, Pitching pitching) {
+
+        if(numbers.contains(pitching.pitching())) {
+            pitchingJudge(baseballResult, pitching);
+        }
+    }
+
+    private void pitchingJudge(BaseballResult baseballResult, Pitching pitching) {
+        if(numbers.isSameIndex(pitching)) {
+            baseballResult.strike();
+            return;
+        }
+        baseballResult.ball();
+    }
+
+}
