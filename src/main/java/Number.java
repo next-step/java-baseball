cfd94ddc7330ae6e85d6dec3deb8dad210e357be
sanggu <tkdrn8578@naver.com>
2021-04-24 23:30:22 +0900

    feat: 입력한 숫자에 중복된 수가 있을 때 예외처리하는 것에 대한 테스트 및 로직 추가

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -1,22 +1,40 @@
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class Number {
 
     public static final int INPUT_NUMBER_SIZE = 3;
     public static final int NOT_CONTAIN_SIGNATURE = -1;
+    public static final String SPLIT_NUMBER_DELIMITER = "";
 
     private final String number;
 
     public Number(String number) {
         validateInputNumberSize(number);
+        validateInputNumberDuplicate(number);
 
         this.number = number;
     }
 
-    private void validateInputNumberSize(String number) {
-        if (number.length() != INPUT_NUMBER_SIZE) {
-            throw new IllegalArgumentException();
+    private void validateInputNumberDuplicate(String number) {
+        List<String> splitNumber = List.of(number.split(SPLIT_NUMBER_DELIMITER));
+
+        Set<String> removedDuplicateNumber = new HashSet<>(splitNumber);
+        if (isSizeOfValidNumber(removedDuplicateNumber.size())) {
+            throw new IllegalArgumentException("중복된 숫자는 입력할 수 없습니다. : " + number);
         }
+    }
+
+    private void validateInputNumberSize(String number) {
+        if (isSizeOfValidNumber(number.length())) {
+            throw new IllegalArgumentException("입력한 숫자의 길이는 3자리이어야 합니다. : " + number);
+        }
+    }
+
+    private boolean isSizeOfValidNumber(int inputSize) {
+        return inputSize != INPUT_NUMBER_SIZE;
     }
 
     public int length() {
--- src/test/java/NumberTest.java
+++ src/test/java/NumberTest.java
@@ -10,6 +10,15 @@ public class NumberTest {
     @ValueSource(strings = {"12", "1234", "123456"})
     @DisplayName("입력한 숫자의 개수가 3개가 아닌 경우 예외처리한다.")
     void validateInputNumberSize(String input) {
-        assertThatIllegalArgumentException().isThrownBy(() -> new Number(input));
+        assertThatIllegalArgumentException().isThrownBy(() -> new Number(input))
+        .withMessageContaining("입력한 숫자의 길이는 3자리이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"122", "999"})
+    @DisplayName("입력한 숫자가 서로 다른 임의의 수가 아닌 경우 예외처리한다.")
+    void validateDuplicate(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Number(input))
+        .withMessageContaining("중복된 숫자는 입력할 수 없습니다.");
     }
 }
