b5cec6102f46cd540c18fbccfbb727f98c7995e1
sanggu <tkdrn8578@naver.com>
2021-04-24 18:38:43 +0900

    refactor: Judgement 추가에 따른 테스트코드 수정

--- src/test/java/BaseBallTest.java
+++ src/test/java/BaseBallTest.java
@@ -12,7 +12,7 @@ public class BaseBallTest {
     @DisplayName("같은 수가 전혀 없으면 포볼 또는 낫싱이다.")
     void fourBall() {
         BaseBall baseball = new BaseBall("123");
-        Map<Character, Integer> play = baseball.play("659");
+        Map<Judgements, Integer> play = baseball.play("659");
 
         assertThat(play).isEmpty();
     }
@@ -21,8 +21,17 @@ public class BaseBallTest {
     @DisplayName("같은 수가 같은 자리에 1개 있으면 1스트라이크이다.")
     void oneStrike() {
         BaseBall baseball = new BaseBall("123");
-        Map<Character, Integer> hint = baseball.play("189");
+        Map<Judgements, Integer> hint = baseball.play("189");
 
-        assertThat(hint.get(BaseBall.STRIKE_KEY)).isEqualTo(1);
+        assertThat(hint.get(Judgements.STRIKE)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("같은 수가 같은 자리에 2개 있으면 2스트라이크이다.")
+    void twoStrike() {
+        BaseBall baseball = new BaseBall("123");
+        Map<Judgements, Integer> hint = baseball.play("129");
+
+        assertThat(hint.get(Judgements.STRIKE)).isEqualTo(2);
     }
 }
