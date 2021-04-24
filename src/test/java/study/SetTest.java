fd99e72b98b43ca7ee84a9edf7d60588e9275952
KyungSik9870 <rudtlr9870@gmail.com>
2021-04-24 18:55:36 +0900

    test : JUnit 과 AssertJ 사용법 학습
    String 클래스에 대한 학습 테스트
    - split, substring, chartAt 테스트
    Set 클래스에 대한 학습테스트
    - size, contains 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,50 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set 의 size() 메소드로 Set 의 크기를 테스트.")
+    void checkSetSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Set 의 contains() 메소드로 1,2,3의 값이 존재하는지 테스트.")
+    void contains(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    @DisplayName("Set 의 contains() 메소드로 1,2,3값은 true, 4,5값은 false 를 반환하는지 테스트.")
+    void containsCase(int number, boolean expected) {
+        assertEquals(numbers.contains(number), expected);
+    }
+
+}
