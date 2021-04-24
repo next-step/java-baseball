a2fbdc8551d13a8cb874532a5dd2e14308f04b85
siwonSeo <siwonst@naver.com>
2021-04-25 00:10:52 +0900

    상태Enum 테스트 구현

---
+++ src/test/java/StatusTest.java
@@ -0,0 +1,48 @@
+
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class StatusTest {
+	@BeforeEach
+	void setUp() {
+	}
+	
+	public enum Status{
+		S("스트라이크"),
+		B("볼");
+		
+		private String status; 
+		
+		Status(String status){
+			this.status = status;
+		}
+		
+		public static String findStatus(String arg) {
+			for(Status st : Status.values()) {
+				if(st.name().equals(arg)) {
+					return st.status;
+				}
+			}
+			return "";
+		}
+		
+	}
+	
+	@Test
+	@ParameterizedTest
+	@ValueSource(strings = {"S","B","N"})
+	@DisplayName("상태,상태명 검증")
+	void enumTest(String input) {
+		assertTrue(Status.findStatus(input).isEmpty());
+	}
+	
+}
