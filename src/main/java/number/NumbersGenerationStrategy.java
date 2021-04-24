96bf308537c46b34e2f06a5f271a4c34cfec559a
KIM GI TAE <c0433c@gmail.com>
2021-04-24 21:55:42 +0900

    feat(player) add user and computer player
    player user and computer

--- src/main/java/number/NumbersGenerationStrategy.java
+++ src/main/java/number/NumbersGenerationStrategy.java
@@ -1,8 +1,8 @@
 package number;
 
 public interface NumbersGenerationStrategy {
-	
+
 	BaseballNumbersFactory baseballNumbersFactory = BaseballNumbersFactory.getInstance();
-	
+
 	BaseballNumbers generate();
 }
--- src/main/java/ui/PrintOutputManager.java
+++ src/main/java/ui/PrintOutputManager.java
@@ -1,14 +1,16 @@
 package ui;
 
 public class PrintOutputManager implements OutputManager {
-	
+
 	private static OutputManager outputManager = new PrintOutputManager();
-	private PrintOutputManager() {}
-	
+
+	private PrintOutputManager() {
+	}
+
 	public static OutputManager getInstance() {
 		return outputManager;
 	}
-	
+
 	@Override
 	public void print(String contents) {
 		System.out.print(contents);
