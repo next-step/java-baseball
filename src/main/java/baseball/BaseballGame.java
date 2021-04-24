21d3ea3211840f4a0c84936f78398b4d7c34ab31
Donghui-Lee <ehdgml3206@naver.com>
2021-04-24 17:19:10 +0900

    refactor: ProgressType 적용을 위한 코드 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import baseball.domain.Player;
+import baseball.domain.ProgressType;
 import baseball.ui.InputView;
 import baseball.ui.ResultView;
 import baseball.utils.RandomNumbersGenerator;
@@ -9,12 +10,20 @@ public class BaseballGame {
     private static final int MAX_PITCH_SIZE = 3;
 
     private Player player;
+    private ProgressType progressType;
 
     public BaseballGame() {
-        player = Player.of(RandomNumbersGenerator.generateStringNumbers(MAX_PITCH_SIZE));
     }
 
     public void run() {
+        do {
+            player = Player.of(RandomNumbersGenerator.generateStringNumbers(MAX_PITCH_SIZE));
+            play();
+            progressType = ProgressType.of(InputView.inputReplay());
+        } while(progressType.progress());
+    }
+
+    public void play() {
         do {
             player.play(InputView.inputPitchNumbers());
             ResultView.printPlayResult(player.result());
