65be29a0e24d0f9f91f78b7a1fc5faa57cf149a0
kimchild <allcreation@nate.com>
2021-04-24 14:19:11 +0900

    feat(JavaBaseballPrecourse.java): 메인함수에서기본 기능
    main함수에서 입력 종료 시작 등을 처리하는 기능을 추가해서
    정상적으로 사용자가 게임을 진행할 수 있는 기능 추가
    
    * 5. main함수에서 입력 종료 시작 등을 처리하는 기능

--- src/main/java/JavaBaseballPrecourse.java
+++ src/main/java/JavaBaseballPrecourse.java
@@ -4,10 +4,9 @@ public class JavaBaseballPrecourse {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello");
 		try (Scanner scan = new Scanner(System.in)) {
-			System.out.println("입력하신 내용은 : " + scan.nextLine());
-			System.out.println("입력하신 내용2는 : " + scan.nextLine());
+			new Game().start(scan);
+			System.exit(0);
 		}
 
 	}
