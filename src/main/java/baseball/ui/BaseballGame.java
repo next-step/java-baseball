a39934d2fc1773776754b5b53aaf8115028e8134
taemin <taminging@kakao.com>
2021-04-24 21:52:24 +0900

    refactor(BaseballGame) : change class BallNumber to Player and Computer

--- src/main/java/baseball/ui/BaseballGame.java
+++ src/main/java/baseball/ui/BaseballGame.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 import baseball.common.PrintMessage;
 import baseball.domain.BallNumber;
 import baseball.domain.BaseballResult;
+import baseball.domain.Computer;
+import baseball.domain.Player;
 import baseball.enums.GameState;
 import baseball.service.BaseballRule;
 
@@ -13,31 +15,32 @@ public class BaseballGame {
 	private final static int RESTART = 1;
 	private final static int EXIT = 2;
 	private static GameState gameState;
-	private static BallNumber computerNumber;
-	private static BallNumber playerNumber;
+	private static Computer computer;
+	private static Player player;
 	private static BaseballRule baseballRule;
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		gameInit();
+		initGame();
 		while(gameState != GameState.GAME_END) {
-			System.out.println(computerNumber.toString());
+			//System.out.println(computer.getBallNumber().toString());
 			inputPlayerNumber();
 			printHint();
 			checkWin();
 		}
 	}
 
-	private static void gameInit() {
+	private static void initGame() {
 		gameState = GameState.GAME_START;
 		System.out.println(PrintMessage.GAME_START_MESSAGE);
-		computerNumber = new BallNumber();
+		computer = new Computer();
+		player = new Player();
 	}
 
 	private static void inputPlayerNumber() {
 		System.out.println(PrintMessage.INPUT_NUMBER_MESSAGE);
 		int playerBallNumber = scanner.nextInt();
-		playerNumber = new BallNumber(playerBallNumber);
+		player.setBallNumber(new BallNumber(playerBallNumber));
 	}
 
 	private static void checkWin() {
@@ -45,12 +48,12 @@ public class BaseballGame {
 		if (baseballResult.getStrikeCount() == BallNumber.MAX_SIZE) {
 			System.out.println(PrintMessage.WIN_MESSAGE);
 			gameState = GameState.GAME_END;
-			gameEnd();
+			endGame();
 		}
 	}
 
 	private static void printHint() {
-		baseballRule = new BaseballRule(playerNumber, computerNumber);
+		baseballRule = new BaseballRule(player.getBallNumber(), computer.getBallNumber());
 		BaseballResult baseballResult = baseballRule.getResult();
 		if (baseballResult.getStrikeCount() > BallNumber.MIN_SIZE)
 			System.out.println(baseballResult.getStrikeCount() + " " + PrintMessage.HINT_STRIKE_MESSAGE);
@@ -60,11 +63,11 @@ public class BaseballGame {
 			System.out.println(PrintMessage.HINT_NOTHING);
 	}
 
-	private static void gameEnd(){
+	private static void endGame(){
 		System.out.println(PrintMessage.GAME_END_MESSAGE);
 		int end = scanner.nextInt();
 		if(end == RESTART)
-			gameInit();
+			initGame();
 		if(end == EXIT)
 			gameState = GameState.GAME_END;
 	}
