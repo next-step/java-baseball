88434bb4ad297ce434e35a9b8a19e1196577a47f
junseok Oh <ixtears23@icloud.com>
2021-04-25 00:09:18 +0900

    feat(domain) : 컴퓨터 야구 게임 숫자 선택(서로 다른 3개의 수)
    게임시작 시 컴퓨터가 선택한 숫자 3자리는
    서로 다른 임의의 수로 생성 된다.

--- src/main/java/com/woowahan/precourse/baseball/domain/BaseBallGame.java
+++ src/main/java/com/woowahan/precourse/baseball/domain/BaseBallGame.java
@@ -2,7 +2,31 @@ package com.woowahan.precourse.baseball.domain;
 
 public class BaseBallGame {
 
+    private String[] gameNumber;  // 컴퓨터가 선택한 세자리 수 - Game 생성 시 같이 생성되어야 함.
+
+    public BaseBallGame() {
+        this.gameNumber = createBallGameNumber();
+    }
+
     private String generateRandomNumber() {
         return String.valueOf((int) (Math.random() * 9 + 1));
     }
+
+    private String[] createBallGameNumber() {
+
+        String firstNumber = generateRandomNumber();
+        String secondNumber = "";
+        String thirdNumber = "";
+
+        do {
+            secondNumber = generateRandomNumber();
+        } while (firstNumber.equals(secondNumber));
+
+        do {
+            thirdNumber = generateRandomNumber();
+        } while (secondNumber.equals(thirdNumber) || firstNumber.equals(thirdNumber));
+
+        return new String[] { firstNumber, secondNumber, thirdNumber };
+    }
+
 }
