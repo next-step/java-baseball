a060e474963c6629ac47441ed742f5f38d4657bb
siwonSeo <siwonst@naver.com>
2021-04-24 22:35:56 +0900

    입력값 검증 로직 구현

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -21,7 +21,7 @@ public class BaseballGame {
 
 	public void startBaseballGame() {
 		String rNumStr = createRandomNum();
-		System.out.println(rNumStr);
+		userGameStart(rNumStr);
 	}
 	
 	public String createRandomNum() {
@@ -38,5 +38,43 @@ public class BaseballGame {
 			returnStrBd.append(num.toString());
 		}
 		return returnStrBd.toString();
+	}
+	
+	public void userGameStart(String rNumStr) {
+		String userInputStr = inputGameNumber();
+		System.out.println(userInputStr);
+	}
+	
+	public String inputGameNumber() {
+		System.out.println("숫자를 입력해주세요 : ");
+		String inputStr = new Scanner(System.in).next();
+		if (!validateGameNum(inputStr)) {
+			return inputGameNumber();
+		}
+		return inputStr;
+	}
+	
+	public boolean validateGameNum(String inputStr) {
+		if (checkPatternNum(inputStr)) {
+			Set<Character> distinctCharSet = convertNumSet(inputStr);
+			if (inputStr.length() == distinctCharSet.size()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean checkPatternNum(String inputStr) {
+		Pattern numPattern = Pattern.compile("([1-9]{3})");
+		Matcher matcher = numPattern.matcher(inputStr);
+		return matcher.matches();
+	}
+	
+	public Set<Character> convertNumSet(String inputStr){
+		Set<Character> distinctCharSet = new LinkedHashSet<>();
+		for (char a : inputStr.toCharArray()) {
+			distinctCharSet.add(a);
+		}
+		return distinctCharSet;
 	}
 }
