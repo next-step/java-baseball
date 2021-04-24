a1e9b98c86c9de873096f030bf04bd7ba75d0784
highlighter9 <ggw6344@gmail.com>
2021-04-25 03:22:40 +0900

    feat: 플레이어 input

--- src/main/java/baseball/GameMaker.java
+++ src/main/java/baseball/GameMaker.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 public class GameMaker {
@@ -12,5 +14,17 @@ public class GameMaker {
 			numberSet.add(random.nextInt(9) + 1);
 		} while (numberSet.size() < 3);
 		return numberSet;
+	}
+
+	public int scanNumber(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력해주세요 :");
+		try {//try-catch문 안의 if문도 depth가 추가되는건가?
+			int inputNumber = sc.nextInt();
+			return inputNumber;
+		} catch (InputMismatchException e){
+			System.out.println("3자리 정수만 허용됩니다.");
+			return scanNumber();
+		}
 	}
 }
