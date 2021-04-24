c757439b1afb5236d042a9ea5745086166c2802e
jigmini <jdsw7789@naver.com>
2021-04-24 15:40:29 +0900

    initial commit
    add start method
    
    no feature

---
+++ src/main/java/BaseBallGame.java
@@ -0,0 +1,11 @@
+public class BaseBallGame {
+
+    private void start() {
+        System.out.println("야구 게임 시작");
+    }
+
+    public static void main(String[] args) {
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.start();
+    }
+}
---
+++ src/test/java/BaseBallGameTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BaseBallGameTest {
+
+    @Test
+    @DisplayName("야구 게임 시작")
+    public void start() {
+
+    }
+
+}
