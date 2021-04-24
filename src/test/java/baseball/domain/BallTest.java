636e163b603dad972a46c005465f745b88d854ad
juniq <juniqlim@gmail.com>
2021-04-24 11:47:25 +0900

    test: 숫자 생성 - 숫자는 0~9 범위만 허용

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -1,12 +1,22 @@
 package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BallTest {
+class BallTest {
     @Test
     void create() {
         assertThatCode(() -> Ball.of(0)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("숫자는 0~9 사이어야 한다")
+    void validNumber() {
+        assertThatThrownBy(() -> Ball.of(10))
+            .hasMessageMatching("숫자는 0~9 사이어야 합니다.")
+            .isInstanceOf(NoAllowNumber.class);
     }
 }
