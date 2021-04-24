dd26cb17c8ab82fc1e844c97b677e4c1300dbe52
ianjang <ianjang0718@gmail.com>
2021-04-24 23:35:14 +0900

    test: add BallCount class tests

---
+++ src/test/java/baseball/BallCountTest.java
@@ -0,0 +1,44 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class BallCountTest {
+
+	@Test
+	void ballCount() {
+		assertThat(new BallCount()).extracting("strike", "ball").containsExactly(0, 0);
+		assertThat(new BallCount(1, 2)).extracting("strike", "ball").containsExactly(1, 2);
+	}
+
+	@Test
+	void setStrike() {
+		BallCount ballCount = new BallCount();
+		ballCount.setStrike(1);
+		assertThat(ballCount).extracting("strike", "ball").containsExactly(1, 0);
+		ballCount.setStrike(2);
+		assertThat(ballCount).extracting("strike", "ball").containsExactly(2, 0);
+	}
+
+	@Test
+	void getStrike() {
+		BallCount ballCount = new BallCount(1, 2);
+		assertThat(ballCount.getStrike()).isEqualTo(1);
+	}
+
+	@Test
+	void setBall() {
+		BallCount ballCount = new BallCount();
+		ballCount.setBall(1);
+		assertThat(ballCount).extracting("strike", "ball").containsExactly(0, 1);
+		ballCount.setBall(2);
+		assertThat(ballCount).extracting("strike", "ball").containsExactly(0, 2);
+	}
+
+	@Test
+	void getBall() {
+		BallCount ballCount = new BallCount(1, 2);
+		assertThat(ballCount.getBall()).isEqualTo(2);
+	}
+}
