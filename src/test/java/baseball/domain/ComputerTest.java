2e6846e276e876733ffb78b309b6be41f49d639f
jwhlll <jwhlll@hanmail.net>
2021-04-24 09:44:08 +0900

    test: 서로 다른 3자리 수 랜덤 값 생성 기능 테스트 생성

---
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -0,0 +1,20 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ComputerTest {
+    private Set<Integer> numbers;
+
+    @Test
+    @DisplayName("Set Size 3 Check")
+    void size_check() {
+        numbers = Computer.createNumbers();
+
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
