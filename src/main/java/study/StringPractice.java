aba5667182fb101e902c5c7a38f38a225bc02335
etff <wish2rich@gmail.com>
2021-04-24 15:44:45 +0900

    feature(study): 괄호가 포함된 문자를 입력받았을 때 숫자 목록을 리턴한다
    refactor(study): 하드코딩 문자를 삭제하고 이름을 수정한다.

--- src/main/java/study/StringPractice.java
+++ src/main/java/study/StringPractice.java
@@ -2,10 +2,14 @@ package study;
 
 import utils.StringUtils;
 
-public class StringSplit {
+public class StringPractice {
+    public static final String OPEN_BRACKET = "(";
+    public static final String CLOSE_BRACKET = ")";
+    public static final String NOT_NUMBER = "숫자가 아닙니다.";
+
     private final String input;
 
-    public StringSplit(String input) {
+    public StringPractice(String input) {
         if (StringUtils.isBlank(input)) {
             throw new IllegalArgumentException();
         }
@@ -16,14 +20,22 @@ public class StringSplit {
         if (StringUtils.isBlank(delimiter)) {
             throw new IllegalArgumentException();
         }
-        final String trimText = input.trim();
+        final String trimText = trimText(input);
         final String[] textNumbers = trimText.split(delimiter);
         return changeToNumbers(textNumbers);
     }
 
+    private String trimText(String text) {
+        if (text.contains(OPEN_BRACKET) && text.contains(CLOSE_BRACKET)) {
+            int startBracketIndex = text.indexOf(OPEN_BRACKET);
+            int closeBracketIndex = text.indexOf(CLOSE_BRACKET);
+            return text.substring(startBracketIndex + 1,  closeBracketIndex).trim();
+        }
+        return text.trim();
+    }
+
     private int[] changeToNumbers(String[] textNumbers) {
         final int[] numbers = new int[textNumbers.length];
-
         for (int i = 0; i < textNumbers.length; i++) {
             numbers[i] = changeToNumber(textNumbers[i]);
         }
@@ -34,7 +46,7 @@ public class StringSplit {
         try {
             return (Integer.parseInt(stringNumber));
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닙니다.");
+            throw new IllegalArgumentException(NOT_NUMBER);
         }
     }
 }
--- src/test/java/study/StringPracticeTest.java
+++ src/test/java/study/StringPracticeTest.java
@@ -5,28 +5,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-class StringSplitTest {
+class StringPracticeTest {
     @DisplayName("두개 문자를 입력 받았을때 숫자를 두개 리턴한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,2"})
+    @ValueSource(strings = {"1,2", "(1,2)"})
     void splitWithTwoNumber(String input) {
-        StringSplit stringSplit = new StringSplit(input);
+        StringPractice stringPractice = new StringPractice(input);
 
         String delimiter = ",";
 
-        assertThat(stringSplit.splitWith(delimiter)).containsExactly(1,2);
+        assertThat(stringPractice.splitWith(delimiter)).containsExactly(1,2);
     }
 
     @DisplayName("한개 문자를 입력 받았을때 숫자를 한개 리턴한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1"})
+    @ValueSource(strings = {"1", "(1)"})
     void splitWithOneNumber(String input) {
-        StringSplit stringSplit = new StringSplit(input);
+        StringPractice stringPractice = new StringPractice(input);
 
         String delimiter = ",";
 
-        assertThat(stringSplit.splitWith(delimiter)).containsExactly(1);
+        assertThat(stringPractice.splitWith(delimiter)).containsExactly(1);
     }
 }
