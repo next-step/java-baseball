d7c86d3be4cd4828df23c77586bcc582f5bceab5
HyungjuJang <dacapolife87@gmail.com>
2021-04-24 20:13:23 +0900

    feat : 게임의 전체 흐름을 관리하기 위한 메인 클래스 - 야구게임의 시작부터 반복입력 및 게임의 종료를 판단하는 기능

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,53 @@
+import domain.BaseballNumberScore;
+import domain.BaseballNumbers;
+import domain.BaseballResult;
+import util.BaseballNumberGenerator;
+import util.TransStringNumberToNumberListUtil;
+import view.InputView;
+import view.ResultView;
+
+import java.util.List;
+
+public class BaseballGame {
+
+    private final InputView inputView = new InputView();
+    private final ResultView resultView = new ResultView();
+
+    public static void main(String[] args) {
+        new BaseballGame().gameStart();
+    }
+
+    private void gameStart() {
+        boolean playingFlag = true;
+        while (playingFlag) {
+            BaseballNumberScore baseballNumberScore = new BaseballNumberScore(
+                    new BaseballNumbers(BaseballNumberGenerator.generator())
+            );
+            playRound(baseballNumberScore);
+            
+            playingFlag = isContinuePlay();
+        }
+    }
+
+    private boolean isContinuePlay() {
+        int continueAnwser = inputView.isContinue();
+        if(continueAnwser == 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void playRound(BaseballNumberScore baseballNumberScore) {
+        boolean playBall = true;
+
+        while(playBall) {
+            List<Integer> inputNumbers = TransStringNumberToNumberListUtil.toNumberList(inputView.inputNumber());
+            BaseballResult baseballResult = baseballNumberScore.judge(new BaseballNumbers(inputNumbers));
+            resultView.showResult(baseballResult);
+
+            playBall = !baseballResult.isEndGame();
+        }
+        resultView.finishScreen();
+    }
+}
