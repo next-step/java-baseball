d448ed9117d6b117b4913af30c866bc9c85593f1
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-04-24 14:46:39 +0900

    test(numGenerator): 숫자 생성기 기능 테스트 추가
    숫자 생성기 기능 테스트 추가
     - 기존 숫자_생성 테스트에서는 자리 수가 맞는지와 숫자로 이루어졌는지를 확인
     - 테스트를 나누어 각각의 기능을 테스트하도록 함, n자리_숫자 테스트에서는 자리수 확인, 서로_다른_숫자 테스트에서는 서로 다른 숫자인지 확인
     - 0이 포함되어 있는지는 숫자_한개_생성 테스트에서 커버 가능(이 테스트를 통과하면 0이 포함되지 않은 숫자로 이루어짐을 보증)

--- src/test/java/NumberGeneratorTest.java
+++ src/test/java/NumberGeneratorTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,30 +11,48 @@ public class NumberGeneratorTest {
 
     private static final int TEST_TRIAL = 1000;
 
-    @DisplayName("1~9로 이루어진 n자리 숫자를 잘 만들어내는지 확인")
-    @Test
-    void 숫자_생성() {
-        for (int i = 0; i < TEST_TRIAL; i++) {
-            //when
-            String number = NumberGenerator.generateNumber(GameOptions.HOW_MANY_DIGIT);
-            //then
-            assertThat(number.length()).isEqualTo(GameOptions.HOW_MANY_DIGIT);
-            assertThat(number).containsPattern(Pattern.compile("[1-9]{" + GameOptions.HOW_MANY_DIGIT + "}"));
-        }
-
-    }
 
     @DisplayName("1~9로 이루어진 1자리 숫자를 잘 만들어내는지 확인")
     @Test
     void 숫자_한개_생성() {
-
         for (int i = 0; i < TEST_TRIAL; i++) {
-            //when
-            int digit = NumberGenerator.generateDigit();
+            //given, when
+            String strDigit = NumberGenerator.generateDigit();
+            int digit = Integer.parseInt(strDigit);
             //then
             assertThat(digit).isGreaterThan(0).isLessThan(10);
         }
-
     }
 
+    @DisplayName("n자리 숫자로 만들어졌는지 확인")
+    @Test
+    void n자리_숫자() {
+        for (int i = 0; i < TEST_TRIAL; i++) {
+            //given, when
+            String number = NumberGenerator.generateNumber();
+            //then
+            assertThat(number).hasSize(GameOptions.HOW_MANY_DIGIT);
+        }
+    }
+
+    @DisplayName("서로 다른 숫자로 만들어졌는지 확인")
+    @Test
+    void 서로_다른_숫자() {
+        for(int i = 0; i< TEST_TRIAL; i++){
+            //given
+            String number = NumberGenerator.generateNumber();
+            //when
+            Set<Character> words = getCharacters(number);
+            //then
+            assertThat(words).hasSize(GameOptions.HOW_MANY_DIGIT);
+        }
+    }
+
+    Set<Character> getCharacters(String number){
+        Set<Character> words = new HashSet<>();
+        for (char c : number.toCharArray()) {
+            words.add(c);
+        }
+        return words;
+    }
 }
