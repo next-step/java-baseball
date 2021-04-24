d4c307a2e4f991291b1d5a0df0c2e43b0a1fef3d
taemin <taminging@kakao.com>
2021-04-24 17:04:39 +0900

    feat(BaseballRule) : compreBallNumber function 수정

--- src/main/java/baseball/service/BaseballRule.java
+++ src/main/java/baseball/service/BaseballRule.java
@@ -24,22 +24,28 @@ public class BaseballRule {
 	//todo: indent < 2 줄여야 합니다. refactoring 필요
 	private void compareBallNumbers() {
 		BaseballResult baseballResult = new BaseballResult();
-		int index = 0;
-		while (index < this.ballSize) {
-			checkStrike(baseballResult, index);
-			checkBall(baseballResult, index);
-			index++;
+		int playerIndex = 0;
+		while (playerIndex < this.ballSize) {
+			checkStrike(baseballResult, playerIndex);
+			checkBall(baseballResult, playerIndex);
+			playerIndex++;
 		}
 		this.baseballResult = baseballResult;
 	}
 
-	private BaseballResult checkStrike(BaseballResult baseballResult, int index) {
-		if (this.playerBallNumber.getNumberByIndex(index) == this.computerBallNumber.getNumberByIndex(index))
+	private BaseballResult checkStrike(BaseballResult baseballResult, int playerIndex) {
+		if (this.playerBallNumber.getNumberByIndex(playerIndex) == this.computerBallNumber.getNumberByIndex(playerIndex))
 			baseballResult.addStrikeCount();
 		return baseballResult;
 	}
 
-	private BaseballResult checkBall(BaseballResult baseballResult, int index) {
+	private BaseballResult checkBall(BaseballResult baseballResult, int playerIndex) {
+		int computerIndex = playerIndex;
+		computerIndex++;
+		if(computerIndex == this.ballSize)
+			computerIndex = 0;
+		if(this.playerBallNumber.getNumberByIndex(playerIndex) == this.computerBallNumber.getNumberByIndex(computerIndex) )
+			baseballResult.addBallCount();
 		return baseballResult;
 	}
 }
