34b655f7ab128c2aec1d6a0ad8a98ebd90b6f052
juniq <juniqlim@gmail.com>
2021-04-24 11:58:37 +0900

    repactor: 매직넘버 추출

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
 public class Ball {
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
     private final int number;
 
     public Ball(int number) {
@@ -8,7 +10,7 @@ public class Ball {
     }
 
     public static Ball of(int number) {
-        if (number < 0 || number > 9) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new NoAllowNumber("숫자는 0~9 사이어야 합니다.");
         }
         return new Ball(number);
