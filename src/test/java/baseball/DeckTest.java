8bc311713dabddd2f8ef7d3baac71efea7a9fe37
Dohyun Kim <10915235+rurumimic@users.noreply.github.com>
2021-04-24 21:32:59 +0900

    test: add number range test
    Deck number range: 1 ~ 9

--- src/test/java/baseball/DeckTest.java
+++ src/test/java/baseball/DeckTest.java
@@ -11,6 +11,9 @@ public class DeckTest {
 		Deck deck = new Deck();
 		int[] numbers = deck.draw();
 		assertThat(numbers).hasSize(3);
+		assertThat(numbers[0]).isBetween(1, 9);
+		assertThat(numbers[1]).isBetween(1, 9);
+		assertThat(numbers[2]).isBetween(1, 9);
 	}
 
 }
