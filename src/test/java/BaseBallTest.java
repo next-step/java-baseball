707d573f8446746b938a7b49d41358caf9bb4ee9
sanggu <tkdrn8578@naver.com>
2021-04-24 18:55:12 +0900

    refactor: strike, ball 둘 다 가지는 경우에 대한 테스트코드를 Parameterized 테스트로 중복 제거

--- src/test/java/BaseBallTest.java
+++ src/test/java/BaseBallTest.java
@@ -22,12 +22,15 @@ public class BaseBallTest {
 
     @ParameterizedTest
     @CsvSource({
-                "123, 189, STRIKE, 1",
-                "123, 129, STRIKE, 2",
-                "123, 123, STRIKE, 3",
+            "123, 189, STRIKE, 1",
+            "123, 129, STRIKE, 2",
+            "123, 123, STRIKE, 3",
+            "123, 419, BALL, 1",
+            "123, 219, BALL, 2",
+            "123, 312, BALL, 3",
     })
-    @DisplayName("같은 자리에 같은 수가 있는 개수만큼 STRIKE를 가진다.")
-    void strike(String answer, String input, Judgements judgement, int expectedCount) {
+    @DisplayName("같은 자리에 같은 수가 있는 개수만큼 해당 판정을 가진다.")
+    void strikeOrBall(String answer, String input, Judgements judgement, int expectedCount) {
         BaseBall baseball = new BaseBall(answer);
         Map<Judgements, Integer> hint = baseball.play(input);
 
@@ -36,39 +39,17 @@ public class BaseBallTest {
 
     @ParameterizedTest
     @CsvSource({
-            "123, 419, BALL, 1",
-            "123, 219, BALL, 2",
-            "123, 312, BALL, 3",
+            "123, 137, 1, 1",
+            "123, 132, 2, 1"
     })
-    @DisplayName("다른 자리에 같은 숫자를 가진 개수만큼 BALL을 가진다.")
-    void ball(String answer, String input, Judgements judgement, int expectedCount) {
+    @DisplayName("같은 수가 같은 자리에 1개 있고 다른 자리에 1개 있으면 1스트라이크 1볼이다.")
+    void StrikeAndBall(String answer, String input, int expectedBallCount, int expectedStrikeCount) {
         BaseBall baseball = new BaseBall(answer);
         Map<Judgements, Integer> hint = baseball.play(input);
 
-        assertThat(hint.get(judgement)).isEqualTo(expectedCount);
-    }
-
-    @Test
-    @DisplayName("같은 수가 같은 자리에 1개 있고 다른 자리에 1개 있으면 1스트라이크 1볼이다.")
-    void OneStrikeAndOneBall() {
-        BaseBall baseball = new BaseBall("123");
-        Map<Judgements, Integer> hint = baseball.play("137");
-
         assertAll(
-                () -> assertThat(hint.get(Judgements.BALL)).isEqualTo(1),
-                () -> assertThat(hint.get(Judgements.STRIKE)).isEqualTo(1)
-        );
-    }
-
-    @Test
-    @DisplayName("같은 수가 같은 자리에 2개 있고 다른 자리에 1개 있으면 1스트라이크 2볼이다.")
-    void OneStrikeAndTwoBall() {
-        BaseBall baseball = new BaseBall("123");
-        Map<Judgements, Integer> hint = baseball.play("132");
-
-        assertAll(
-                () -> assertThat(hint.get(Judgements.BALL)).isEqualTo(2),
-                () -> assertThat(hint.get(Judgements.STRIKE)).isEqualTo(1)
+                () -> assertThat(hint.get(Judgements.BALL)).isEqualTo(expectedBallCount),
+                () -> assertThat(hint.get(Judgements.STRIKE)).isEqualTo(expectedStrikeCount)
         );
     }
 }
