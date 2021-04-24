b3748f5023e25d983a112c60da999100943c71a0
HeeJaeKK <agmlwo@naver.com>
2021-04-24 20:05:01 +0900

    RandomGenerator : 1~9로만 이루어져 있는지 테스트 성공

--- src/test/java/RandomGeneratorTest.java
+++ src/test/java/RandomGeneratorTest.java
@@ -2,17 +2,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Pattern;
+
 public class RandomGeneratorTest {
     private RandomGenerator randomGenerator;
+    private String randomBalls;
 
     @BeforeEach
     public void beforeSet() {
         randomGenerator = new RandomGenerator(BaseballGame.DEFAULT_LEN);
+        randomBalls = randomGenerator.generateBalls();
     }
 
     @Test
     public void isValidatedLength() {
-        String randomBalls = randomGenerator.generateBalls();
         Assertions.assertEquals(randomBalls.length(), BaseballGame.DEFAULT_LEN);
+    }
+
+    @Test
+    public void isValidatedDigit() {
+        for (int i=0; i<randomBalls.length(); i++) {
+            int num = randomBalls.charAt(i) - '0';
+            Assertions.assertEquals(Pattern.matches("^[1-9]", randomBalls.charAt(i) + ""), true );
+        }
     }
 }
