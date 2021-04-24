7dd5760c0e4d97d423bdb3368db3e7a6c03dba34
juniq <juniqlim@gmail.com>
2021-04-24 11:40:09 +0900

    feat: 볼 생성

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,6 +1,13 @@
 package baseball.domain;
 
 public class Ball {
-    public static void of(int number) {
+    private final int number;
+
+    public Ball(int number) {
+        this.number = number;
+    }
+
+    public static Ball of(int number) {
+        return new Ball(number);
     }
 }
