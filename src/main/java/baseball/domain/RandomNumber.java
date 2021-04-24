68a40ac7f81ea41cc3ffbde3a9f4f84d547dbe5b
juniq <juniqlim@gmail.com>
2021-04-24 14:42:58 +0900

    repactor: 매직넘버 추출

--- src/main/java/baseball/domain/RandomNumber.java
+++ src/main/java/baseball/domain/RandomNumber.java
@@ -3,8 +3,10 @@ package baseball.domain;
 import java.util.Random;
 
 public class RandomNumber implements OneDigitNumber {
+    public static final int BOUND = 10;
+
     @Override
     public int value() {
-        return new Random().nextInt(10);
+        return new Random().nextInt(BOUND);
     }
 }
