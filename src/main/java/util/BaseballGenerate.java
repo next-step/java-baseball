472af63904d37617fec16f51d4fab153fafcd2d4
grchoi <grchoi@emro.co.kr>
2021-04-24 17:07:24 +0900

    feat : baseball 랜덤 값 생성을 위한 util 개발

---
+++ src/main/java/util/BaseballGenerate.java
@@ -0,0 +1,33 @@
+package util;
+
+import domain.baseball.Baseball;
+import domain.baseball.BaseballGroup;
+
+import java.util.*;
+
+public class BaseballGenerate {
+
+    public static BaseballGroup createBaseballGroup(){
+        List<Baseball> baseballGroup = new ArrayList<>();
+        Set<Long> baseballRandomValues = new HashSet<>();
+
+        int minBaseballNum = Baseball.getMinValue();
+        int maxBaseballNum = Baseball.getMaxValue();
+
+        while (baseballRandomValues.size() < BaseballGroup.getRandomLength()){
+            Number randomBaseballNum = new Random().nextInt((maxBaseballNum - minBaseballNum) + 1) + minBaseballNum;
+            baseballRandomValues.add(randomBaseballNum.longValue());
+        }
+
+        for(Long randomBaseballNum : baseballRandomValues){
+            baseballGroup.add(createBaseballValue(randomBaseballNum));
+        }
+
+        return new BaseballGroup(baseballGroup);
+    }
+
+
+    private static Baseball createBaseballValue(Long numberValue){
+        return new Baseball(numberValue);
+    }
+}
