9148b3811c21a6ccba87052ba2539424212902f6
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2021-04-24 21:24:47 +0900

    Feat(Dealer): Create Dealer class
    Create Dealer class which generate list of 3 random numbers
    - numbers should be between 1 and 9

---
+++ src/main/java/Dealer.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Dealer {
+    private int[] numList;
+
+    private void pickNumbers() {
+        numList = new int[3];
+        Random random = new Random();
+        numList[0] = random.nextInt(9) + 1;
+        numList[1] = random.nextInt(9) + 1;
+        numList[2] = random.nextInt(9) + 1;
+    }
+
+    public int[] getList() {
+        return numList;
+    }
+
+    public Dealer() {
+        pickNumbers();
+    }
+}
---
+++ src/test/java/DealerTest.java
@@ -0,0 +1,17 @@
+import jdk.nashorn.internal.AssertsEnabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealerTest {
+    @Test
+    public void create_Dealer() {
+        Dealer dealer = new Dealer();
+
+        assertEquals(3, dealer.getList().length);
+        assertThat(dealer.getList()[0]).isBetween(1, 9);
+        assertThat(dealer.getList()[1]).isBetween(1, 9);
+        assertThat(dealer.getList()[2]).isBetween(1, 9);
+    }
+}
