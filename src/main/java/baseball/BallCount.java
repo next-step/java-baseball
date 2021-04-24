14df47f83fa80414be8fe80b68f3ab8979f48dbb
ianjang <ianjang0718@gmail.com>
2021-04-24 23:12:10 +0900

    feat: implement default constructor, setter methods inside BallCount class

--- src/main/java/baseball/BallCount.java
+++ src/main/java/baseball/BallCount.java
@@ -4,13 +4,26 @@ public class BallCount {
 	private int strike;
 	private int ball;
 
+	public BallCount() {
+		this.strike = 0;
+		this.ball = 0;
+	}
+
 	public BallCount(int strike, int ball) {
 		this.strike = strike;
 		this.ball = ball;
 	}
 
+	public void setStrike(int strike) {
+		this.strike = strike;
+	}
+
 	public int getStrike() {
 		return this.strike;
+	}
+
+	public void setBall(int ball) {
+		this.ball = ball;
 	}
 
 	public int getBall() {
