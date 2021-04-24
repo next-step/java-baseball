622e075ad041fd9ab6dfbf453c58bd18608d46e3
kimchild <allcreation@nate.com>
2021-04-24 11:02:48 +0900

    feat(Computer.java): 컴퓨터의 볼값 3개 지정하는 기능 추가
    컴퓨터의 중복없는 임의의수 3개값을 지정하는 기능 추가
    
    * 2. 컴퓨터의 1~9 임의의수 3개를 선택하는 기능

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,6 +1,30 @@
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Computer {
 
-	public int numberRandom(int min, int max) {
+	private Set<Integer> ballSet = new TreeSet<>();
+
+	private int numberRandom(int min, int max) {
 		return (int)(Math.random() * max + min);
+	}
+
+	public int getMaxBallSize() {
+		return 3;
+	}
+
+	public Set<Integer> getBallSet() {
+		return ballSet;
+	}
+
+	public void setBallSet() {
+		Set<Integer> ballSet = new TreeSet<>();
+		int randomMin = 1;
+		int randomMax = 10;
+
+		while (ballSet.size() != this.getMaxBallSize()) {
+			ballSet.add(this.numberRandom(randomMin, randomMax));
+		}
+		this.ballSet = ballSet;
 	}
 }
--- src/test/java/ComputerTest.java
+++ src/test/java/ComputerTest.java
@@ -5,31 +5,10 @@ import org.junit.jupiter.api.Test;
 public class ComputerTest {
 
 	Computer computer = new Computer();
-	final int min = 1;
-	final int max = 10;
 
 	@Test
-	void numberRandomResult() {
-		int ran = computer.numberRandom(1, 100);
-		System.out.println(ran);
-		assertTrue(ran != 0);
-	}
-
-	@Test
-	void numberMinResultTest() {
-		for (int i = 0; i < 100; i++) {
-			int ran = computer.numberRandom(min, max);
-			System.out.println(ran);
-			assertTrue(ran >= min);
-		}
-	}
-
-	@Test
-	void numberMaxResultTest() {
-		for (int i = 0; i < 100; i++) {
-			int ran = computer.numberRandom(min, max);
-			System.out.println(ran);
-			assertTrue(ran <= max);
-		}
+	void initBallTest() {
+		computer.setBallSet();
+		assertEquals(computer.getBallSet().size(), computer.getMaxBallSize());
 	}
 }
