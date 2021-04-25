43003b955dceb61239de73a66c73f9f4f28726f8
dmswo106 <dmswo106@naver.com>
2021-04-25 15:35:13 +0900

    게임이 중지되고 사용자로부터 1,2를 입력받는다. 1~2사이의 숫자만 입력받는 ValidationCheck를한다.

--- src/main/java/baseball/domain/BaseBallGame.java
+++ src/main/java/baseball/domain/BaseBallGame.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import java.util.Scanner;
+
 public class BaseBallGame {
     private String computerBaseNum = "";
     private String userBaseNum = "";
@@ -19,5 +21,30 @@ public class BaseBallGame {
             userBaseNum = baseBallUser.getUserCreateNum();
             check = baseBallComputer.game(computerBaseNum, userBaseNum);
         }
+        gameRestart();
+    }
+
+    private boolean gameRestart(){
+        boolean check = true;
+        while(check){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("게임을 새로 시작하려면 1, 종료 하려면 2를 입력하세요.");
+            String number = sc.nextLine();
+            check = gameRestartCheck(number);
+        }
+        return check;
+    }
+
+    private boolean gameRestartCheck(String num){
+        if("2".equals(num)){
+            return false;
+        }else if("1".equals(num)){
+            init();
+            gameStart();
+        }else if(!num.matches("^[1-2]+$")){
+            System.out.println("1~2까지의 수를 입력해주시기 바랍니다.");
+            return true;
+        }
+        return false;
     }
 }
