ebf95dd379c75a60c493388116437679ee8aba55
leemingyu09 <hikakaka13@naver.com>
2021-04-25 16:00:36 +0900

    feat: 입력값 볼카운트를 판단

---
+++ src/main/java/DTO.java
@@ -0,0 +1,11 @@
+public class DTO {
+    private int forLoopIndex;
+
+    public int getForLoopIndex() {
+        return forLoopIndex;
+    }
+
+    public void setForLoopIndex(int forLoopIndex) {
+        this.forLoopIndex = forLoopIndex;
+    }
+}
