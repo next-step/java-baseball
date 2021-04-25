2feae57e23a8ad844210ebc81952fae10ba2f902
DESKTOP-OP3B5IB\kmyms <kmyms2@naver.com>
2021-04-26 00:52:07 +0900

    Fix number generator
    - 같은 숫자로 이루어진 3자리 수 생성 됨

--- src/main/java/generator/NumGenerator.java
+++ src/main/java/generator/NumGenerator.java
@@ -1,14 +1,39 @@
 package generator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NumGenerator {
-    public static String numGenerator(int figure, int min, int max){
+    private final static int FIGURE = 3;
+    private final static int MIN = 1;
+    private final static int MAX = 9;
+    private static int[] num = new int[FIGURE];
+    private static Set<Integer> chkNum = new HashSet<Integer>();
+
+    /*
+        서로 다른 숫자로 이루어진 3자리 숫자 반환
+     */
+    public static String numGenerator(){
         String numStr = "";
-        int[] num = new int[figure];
-        for(int i=0 ; i<figure ; i++){
-            num[i] = (int)(Math.random()*(max - min + 1)) + 1;
-            //numStr += Integer.toString(num[i]);
+        num[0] = (int)(Math.random()*(MAX - MIN + 1)) + 1;
+        numStr = numStr.concat(Integer.toString(num[0]));
+        chkNum.add(num[0]);
+        for(int i=1 ; i<FIGURE ; i++){
+            num[i] = newNum();
             numStr = numStr.concat(Integer.toString(num[i]));
         }
         return numStr;
+    }
+
+    /*
+        중복 체크 후 임의의 한자리 숫자 반환
+     */
+    public static int newNum(){
+        int now = 0;
+        do{
+            now = (int)(Math.random()*(MAX - MIN + 1)) + 1;
+        }while(chkNum.contains(now));
+        chkNum.add(now);
+        return now;
     }
 }
