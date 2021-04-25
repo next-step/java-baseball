1ea68423391db178dfa8a8b73a5e35476493bed5
accidentlywoo <accidentlywoo@gmail.com>
2021-04-25 14:48:26 +0900

    feat(2.대결 준비) : 대결용 임의의수생성 메소드
    랜덤 3자리 수.
    겹치지 않아야됨.

--- src/main/java/NumberBaseBallGame/GenerateGameNumber.java
+++ src/main/java/NumberBaseBallGame/GenerateGameNumber.java
@@ -1,12 +1,28 @@
 package NumberBaseBallGame;
 
+import java.util.Random;
+
 public class GenerateGameNumber {
 	/**
 	 * 대결용 임의의 수 1 ~ 9 사이 서로다른 Random 수 3개
 	 *
 	 * @return
 	 */
-	public char[] generateGameNumber(){
-		return null;
+	public int[] generateGameNumber(){
+		int[] gameNumbers = new int[3];
+		Random random = new Random();
+		out:
+		for (int i = 0; i < gameNumbers.length; i++){
+			int aRandomNumber = random.nextInt(10);
+
+			for(int item : gameNumbers){
+				if(aRandomNumber == item){
+					i--;
+					continue out;
+				}
+			}
+			gameNumbers[i] = aRandomNumber;
+		}
+		return gameNumbers;
 	}
 }
