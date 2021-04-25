5d308cfe3b7daca5bb7d2b4baae46810714059dd
highlighter9 <ggw6344@gmail.com>
2021-04-25 23:35:01 +0900

    feat: 플레이어 input validation

--- src/main/java/baseball/GameMaker.java
+++ src/main/java/baseball/GameMaker.java
@@ -1,7 +1,6 @@
 package baseball;
 
 import java.util.HashSet;
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
@@ -16,15 +15,49 @@ public class GameMaker {
 		return numberSet;
 	}
 
-	public int scanNumber(){
+	public Set<Integer> inputNumberUntilSuccess() {
+		try {
+			Set<Integer> inputNumberSet = inputNumber();
+			return inputNumberSet;
+		} catch (RuntimeException e) {
+			System.out.println("중복되지않은 3자리 정수만 허용됩니다.");
+			return inputNumberUntilSuccess();
+		}
+	}
+
+	public Set<Integer> inputNumber(){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 입력해주세요 :");
-		try {//try-catch문 안의 if문도 depth가 추가되는건가?
-			int inputNumber = sc.nextInt();
-			return inputNumber;
-		} catch (InputMismatchException e){
-			System.out.println("3자리 정수만 허용됩니다.");
-			return scanNumber();
+		int inputNumber = sc.nextInt();
+
+		validateInputNumber(inputNumber);
+		Set<Integer> numberSet = toSetNumber(inputNumber);
+		validateInputSetNumber(numberSet);
+
+		return numberSet;
+	}
+
+	public Set<Integer> toSetNumber(int number) {
+		Set<Integer> numberSet = new HashSet<>();
+		while (number > 0) {
+			numberSet.add(number % 10);
+			number = number / 10;
 		}
+		return numberSet;
+	}
+
+	public boolean validateInputNumber(int number) {
+		int pos = (int)(Math.log10(number)+1);
+		if(pos != 3){
+			throw new RuntimeException();
+		}
+		return true;
+	}
+
+	public boolean validateInputSetNumber(Set numberSet) {
+		if(numberSet.size() != 3){
+			throw new RuntimeException();
+		}
+		return true;
 	}
 }
