13f2576082fb01937efc3a3bc3dbc4de96a31485
TJ NAM <dynamicnam@gmail.com>
2021-04-25 23:36:33 +0900

    feat(game class) 프로그램 시작 메서드 구현
    프로그램 시작 및 게임을 실행하기 위한 메서드 구현.
    
    재시작 기능과 관련한 조건체크를 위한 임시 매서드 함께 작성

--- src/main/java/com/tjnam/baseballgame/Game.java
+++ src/main/java/com/tjnam/baseballgame/Game.java
@@ -4,8 +4,22 @@ public class Game {
     public Game(){
 
     }
+
     public void start(){
+        do{
+            this.run();
+        }while(this.checkRestart());
+    }
+
+    public void run(){
 
     }
+
+    private Boolean checkRestart(){
+    //TODO : do implement
+        return true;
+    }
+
+
 
 }
