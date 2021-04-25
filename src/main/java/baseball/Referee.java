a978dd0962a9286dc508619a44323bf0895f5d67
cptbong <cptbbong@gmail.com>
2021-04-25 20:59:58 +0900

    test(Referee) - 레프리테스트추가.

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,4 +1,13 @@
 package baseball;
 
+import java.util.Random;
+
 public class Referee {
+    private final Numbers numbers;
+
+    public Referee() {
+        Random random = new Random();
+        int firstNumber = random.nextInt();
+        this.numbers = new Numbers();
+    }
 }
---
+++ src/test/java/baseball/RefereeTest.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RefereeTest {
+    @Test
+    @DisplayName("기본테스트")
+    void defaultTest() {
+        Referee referee = new Referee();
+    }
+}
