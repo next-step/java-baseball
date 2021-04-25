87033b10704233a75b289b3b01090a064422f3d5
jordy-torvalds <jordy-torvalds@kakao.com>
2021-04-25 15:50:44 +0900

    feat(View, Service) : 플레이어 입력 기반 야구 번호 생성 기능,  프로세스 상 적용

--- src/main/java/baseball/application/BaseballService.java
+++ src/main/java/baseball/application/BaseballService.java
@@ -1,6 +1,8 @@
 package baseball.application;
 
 import baseball.domain.BaseballNumbers;
+import baseball.domain.Score;
+import baseball.util.BaseballException;
 import baseball.util.BaseballNumberFactory;
 
 public class BaseballService {
@@ -12,5 +14,15 @@ public class BaseballService {
 
     public BaseballNumbers generateAnswer() {
         return this.baseballNumberFactory.generateNumbers();
+    }
+
+    public Score scorePlayerInput(String playerInput) {
+        try {
+            BaseballNumbers playerBaseballNumber = new BaseballNumbers(playerInput);
+        } catch (BaseballException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null; // TODO 기능 미구현으로 NULL 반환
     }
 }
--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -2,12 +2,14 @@ package baseball.view;
 
 import baseball.application.BaseballService;
 import baseball.domain.BaseballNumbers;
+import baseball.domain.Score;
 
 import java.util.Scanner;
 
 public class BaseballView {
     private final BaseballService baseballService;
     private final Scanner sc;
+    private final String MESSAGE_ENTER_NUMERIC = "숫자를 입력해주세요 : ";
 
     public BaseballView() {
         baseballService = new BaseballService();
@@ -18,9 +20,22 @@ public class BaseballView {
         boolean isContinued = true;
         while (isContinued){
             BaseballNumbers answer = this.baseballService.generateAnswer();
+            deduceNumber(answer);
             isContinued = false;
         }
     }
 
+    private void deduceNumber(BaseballNumbers answer) {
+        boolean isContinued = true;
+        while (isContinued){
+            String playerInput = enterPlayerInput();
+            Score score = baseballService.scorePlayerInput(playerInput);
+            isContinued = false;
+        }
+    }
 
+    private String enterPlayerInput() {
+        System.out.print(MESSAGE_ENTER_NUMERIC);
+        return sc.next();
+    }
 }
