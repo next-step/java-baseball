6c149e8451ec81a34db72be57cf97d9d8ac8b2cb
dongkyu <dongkyu.parkk@gmail.com>
2021-04-25 23:44:16 +0900

    feat(BaseballUI.java, referee.java): UI창
    숫자 야구게임을 진행하는 UI창
    게임 플레이어에게 숫자를 입력받아 컴퓨터가 선택한 값과 비교하여 결과를 출력한다

---
+++ src/main/java/baseball/BaseballUI.java
@@ -0,0 +1,75 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class BaseballUI {
+
+    private Number generateNumbers = new Number();
+    private BaseballService baseballService = new BaseballService();
+    private Referee referee;
+    private Scanner sc = new Scanner(System.in);
+
+    public boolean startGame() {
+
+        referee = new Referee();
+
+        System.out.print("숫자를 입력해주세요.");
+        int inputValue = sc.nextInt();
+
+        if(!baseballService.validateInputValue(inputValue)) {
+            return true;
+        }
+
+        baseballService.compareNumber(referee, generateNumbers, inputValue);
+        System.out.println(printResult(referee));
+
+        if (!printResult(referee).equals("3스트라이크 ")) {
+            return true;
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        return checkInputContinueButton();
+
+    }
+
+    public boolean checkInputContinueButton() {
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int continueButton = sc.nextInt();
+
+        if (continueButton == 1) {
+            generateNumbers = new Number();
+            return true;
+        }
+
+        if (continueButton == 2) {
+            sc.close();
+            return false;
+        }
+
+        System.out.println("잘못 된 값이 입력되었습니다.");
+        return checkInputContinueButton();
+
+    }
+
+    public String printResult(Referee referee) {
+
+        String result = "";
+
+        if(referee.getStrike() != 0) {
+            result = result + referee.getStrike() + "스트라이크 ";
+        }
+
+        if(referee.getBall() != 0) {
+            result = result + referee.getBall() + "볼 ";
+        }
+
+        if(result.equals("")) {
+            result = "낫싱";
+        }
+
+        return result;
+
+    }
+
+}
