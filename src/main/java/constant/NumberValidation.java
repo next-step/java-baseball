3010d796c5c550bfe7b7c5db84b400a22abe26f3
JJuniJJuni <qkrgud305@naver.com>
2021-04-26 00:39:38 +0900

    refactor: 난수 생성 로직 및 검증 기능 테스트 케이스 구현
    난수 생성 기능 구현
    - 입력된 수에 대해 오류가 있을 시 해당 메시지 출력
    - 에러 메시지 좀 더 명확하게 수정

--- src/main/java/constant/NumberValidation.java
+++ src/main/java/constant/NumberValidation.java
@@ -8,8 +8,8 @@ import util.GameUtil;
 
 public enum NumberValidation {
 	IsNotEmptyString("입력 값이 비어있습니다", number -> number != null && !number.isEmpty()),
-	IsRightNumberLength("자리수가 옳바르지 않습니다", number -> number.length() == GameUtil.LIMIT_DIGIT),
-	IsRightNumberFormat("숫자로만 이루어져 있지 않습니다", number -> number.matches("^[1-9]+$")),
+	IsRightNumberLength("수의 길이가 옳지 않습니다", number -> number.length() == GameUtil.LIMIT_DIGIT),
+	IsRightNumberFormat("1~9의 숫자로만 이루어져 있지 않습니다", number -> number.matches("^[1-9]+$")),
 	IsDiffertNumber("중복되는 숫자가 존재합니다", NumberValidation::validateDifferentNumber);
 
 	NumberValidation(String errorMsg, Predicate<String> validate) {
--- src/main/java/util/GameUtil.java
+++ src/main/java/util/GameUtil.java
@@ -22,12 +22,20 @@ public class GameUtil {
 		return number;
 	}
 
-	private static boolean validateNumberFormat(String number) {
+	public static boolean validateNumberFormat(String number) {
 		boolean isRightFlag = true;
 		for (NumberValidation validation : NumberValidation.values()) {
-			isRightFlag = isRightFlag && validation.getValidate().test(number);
+			isRightFlag = isRightFlag && validateByCurrentFormat(validation, number);
 		}
 		return isRightFlag;
+	}
+
+	private static boolean validateByCurrentFormat(NumberValidation validation, String number) {
+		if (!validation.getValidate().test(number)) {
+			System.out.printf("입력 값 오류: %s\n\n", validation.getErrorMsg());
+			return false;
+		}
+		return true;
 	}
 
 	private static String makeRandomNumber() {
