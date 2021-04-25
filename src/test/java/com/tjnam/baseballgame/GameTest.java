507ed9e91ecbf0ea6114a54f8e6cd2706e4ce57f
TJ NAM <dynamicnam@gmail.com>
2021-04-26 07:18:18 +0900

    test(game class) 유저 입력에 대한 테스트 추가
    유저 입력에 대한 validation을 위한 테스트 케이스 추가
     - 주어진 요구사항에 따른 다양한 가능한 입력에 대한 validation 및 평가

--- src/test/java/com/tjnam/baseballgame/GameTest.java
+++ src/test/java/com/tjnam/baseballgame/GameTest.java
@@ -1,4 +1,110 @@
 package com.tjnam.baseballgame;
 
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GameTest {
+
+
+    @Test
+    public void gamerInputValidationTest(){
+        Boolean isNotValidInput;
+
+        Game game = new Game();
+
+        try {
+            Method testMethod = game.getClass().getDeclaredMethod("isValidInput", String.class);
+            testMethod.setAccessible(true);
+
+            String sameNumberUserInput = "112";
+            isNotValidInput = (Boolean) testMethod.invoke(game, sameNumberUserInput);
+            assertThat(isNotValidInput).isTrue();
+
+            String overLengthUserInput = "11335566666";
+            isNotValidInput = (Boolean) testMethod.invoke(game, overLengthUserInput);
+            assertThat(isNotValidInput).isTrue();
+
+            String shortLengthUserInput = "13";
+            isNotValidInput = (Boolean) testMethod.invoke(game, shortLengthUserInput);
+            assertThat(isNotValidInput).isTrue();
+
+            String notNumberUserInput = "1a3";
+            isNotValidInput = (Boolean) testMethod.invoke(game, notNumberUserInput);
+            assertThat(isNotValidInput).isTrue();
+
+            String rightUserInput = "135";
+            isNotValidInput = (Boolean) testMethod.invoke(game, rightUserInput);
+            assertThat(isNotValidInput).isFalse();
+
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void duplicatedInputValidationFuncTest(){
+        Game game = new Game();
+        Boolean isDuplicate;
+        String userInputStringSample;
+
+        try {
+            Method checkDuplicateInputMethod = game.getClass().getDeclaredMethod("checkDuplicateInput", String.class);
+            checkDuplicateInputMethod.setAccessible(true);
+
+            userInputStringSample = "131";
+            isDuplicate = (Boolean) checkDuplicateInputMethod.invoke(game, userInputStringSample);
+            assertThat(isDuplicate).isTrue();
+
+            userInputStringSample = "111";
+            isDuplicate = (Boolean) checkDuplicateInputMethod.invoke(game, userInputStringSample);
+            assertThat(isDuplicate).isTrue();
+
+            userInputStringSample = "115";
+            isDuplicate = (Boolean) checkDuplicateInputMethod.invoke(game, userInputStringSample);
+            assertThat(isDuplicate).isTrue();
+
+            userInputStringSample = "511";
+            isDuplicate = (Boolean) checkDuplicateInputMethod.invoke(game, userInputStringSample);
+            assertThat(isDuplicate).isTrue();
+
+            userInputStringSample = "123";
+            isDuplicate = (Boolean) checkDuplicateInputMethod.invoke(game, userInputStringSample);
+            assertThat(isDuplicate).isFalse();
+
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void inputIsOnNumberCharValidationTest(){
+        Game game = new Game();
+        String userInputSample;
+        Boolean isNumberString;
+
+        try{
+            Method isInputIsNumberStringMethod = game.getClass().getDeclaredMethod("isInputIsNumberString", String.class);
+            isInputIsNumberStringMethod.setAccessible(true);
+
+            userInputSample = "13a";
+            isNumberString = (Boolean) isInputIsNumberStringMethod.invoke(game, userInputSample);
+            assertThat(isNumberString).isTrue();
+
+            userInputSample = "abc";
+            isNumberString = (Boolean) isInputIsNumberStringMethod.invoke(game, userInputSample);
+            assertThat(isNumberString).isTrue();
+
+            userInputSample = "123";
+            isNumberString = (Boolean) isInputIsNumberStringMethod.invoke(game, userInputSample);
+            assertThat(isNumberString).isFalse();
+
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
