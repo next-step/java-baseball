a87c1f07861d1aba40017e7e0cc90330d6ded418
hyunsung_kim <hyunsung.kim@superb-ai.com>
2021-04-26 04:55:35 +0900

    feat(player): add player at baseball game
    Added player class
    - ball input for calculating score
    - random digit string generator

--- src/main/java/com/baseball/precourse/baseball/Player.java
+++ src/main/java/com/baseball/precourse/baseball/Player.java
@@ -1,16 +1,31 @@
-package com.baseball.precourse;
+package com.baseball.precourse.baseball;
+
+import com.baseball.precourse.util.DigitRandom;
+import com.baseball.precourse.util.DigitString;
 
 public class Player {
-
+    private DigitString numbers;
 
     public Player() {
+        this(DigitRandom.generate(Ball.BALL_COUNT));
     }
 
-    public Score hit(Ball ball) {
-        return new Score();
+    public Player(String numbers) {
+        this.numbers = new DigitString(numbers);
     }
 
-    public boolean isOut(){
-        return false;
+    public Score hit(Ball ball){
+        return new Score(
+                countStrike(ball),
+                countBall(ball)
+        );
+    }
+
+    private int countStrike(final Ball ball){
+        return numbers.matchPosCount(ball.getValue());
+    }
+
+    private int countBall(final Ball ball){
+        return numbers.matchCount(ball.getValue()) - numbers.matchPosCount(ball.getValue());
     }
 }
--- src/test/java/com/baseball/precourse/baseball/PlayerTest.java
+++ src/test/java/com/baseball/precourse/baseball/PlayerTest.java
@@ -1,2 +1,25 @@
-package com.baseball.precourse.baseball;public class PlayerTest {
+package com.baseball.precourse.baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerTest {
+
+    @DisplayName("Play")
+    @ParameterizedTest
+    @CsvSource(value = {"123:123:3 스트라이크", "111:123:1 스트라이크", "321:123:1 스트라이크 2 볼", "123:312:3 볼"}, delimiter = ':')
+    void hitTest(String ballInput, String playerInput, String expectedText){
+        // given
+        Ball ball = new Ball(ballInput);
+        Player player = new Player(playerInput);
+
+        // when
+        Score score = player.hit(ball);
+
+        // then
+        assertThat(score.toString()).isEqualTo(expectedText);
+    }
 }
