dde5961a0f5c09cd253cb3ce684cfff347fa525b
freeefly <freefly3557@gmail.com>
2021-04-25 18:02:44 +0900

    feat : 값을 입력 받는 기능 개발 완료
    값을 입력받는 InputProcessor.java 를 생성하고
    system.in 에서 입력받도록 설정

---
+++ src/main/java/InputProcessor.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * 입력을 처리
+ */
+public class InputProcessor {
+    public static String input(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BaseballBoard baseballBoard = new BaseballBoard();
+        while(baseballBoard.isContinuing()){
+            InputProcessor.input();
+
+        }
+    }
+}
