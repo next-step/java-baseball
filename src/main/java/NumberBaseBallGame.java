bde21e988725c5c4e61bc8bca881188437cddfaa
Dongbin <bnitech88@gmail.com>
2021-04-26 04:28:40 +0900

    feat(NumberBaseBallGame): set game stage
    - 게임 스테이지가 반복적으로 수행되도록 세팅
    - 유효하지 않은 숫자가 입력되면 재입력하도록 유도
    - 예측되지 않은 에러 발생 시, 게임 종료

--- src/main/java/NumberBaseBallGame.java
+++ src/main/java/NumberBaseBallGame.java
@@ -1,5 +1,8 @@
+import java.util.InputMismatchException;
 import java.util.Map;
+import java.util.Scanner;
 
+import exception.InvalidValueException;
 import service.IInitializer;
 import service.impl.Initializer;
 
@@ -8,6 +11,32 @@ public class NumberBaseBallGame {
 	private final static IInitializer iInitializer = new Initializer();
 
 	public static void main(String[] args) {
-		Map<Integer, Integer> computerNumbers = iInitializer.initComputerNumbers();
+
+		final Map<Integer, Integer> computerNumber = iInitializer.initComputerNumber();
+
+		boolean gameOver = false;
+		while (!gameOver) {
+			gameOver = runningGameStage(computerNumber);
+		}
+	}
+
+	/**
+	 * 게임 스테이지를 진행
+	 */
+	private static boolean runningGameStage(final Map<Integer, Integer> computerNumber) {
+		final Scanner scanner = new Scanner(System.in);
+
+		System.out.print("숫자를 입력해주세요 : ");
+		try {
+			final int[] ints = iInitializer.initPlayerNumber(scanner.nextInt());
+		} catch (InvalidValueException | InputMismatchException e) {
+			System.out.println("유효하지 않은 숫자입니다!");
+			return false;
+		} catch (Exception e) {
+			System.out.println("게임이 비정상적으로 종료되었습니다.");
+			return true;
+		}
+
+		return false;
 	}
 }
