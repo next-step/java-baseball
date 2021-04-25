196dd242e4af35cbe5a1f345010e32dffe7b3ebd
JH-PC\경재하 <wogk652@gmail.com>
2021-04-25 13:29:05 +0900

    feat(BaseballGame.java): 게임 시작 로직 구현

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class BaseballGame {
+import java.util.Random;
+
+public class BaseballGame {
+
+    private final BaseballGameView gameView = new BaseballGameView();
+
+    //게임 시작 로직
+    public boolean baseballGameStart() {
+        int[] computerGameNumber = makeComputerGameNumber();
+        boolean finishGame = false;
+        
+        do {
+            finishGame = progressGame();
+        } while (!finishGame);
+
+        return restartGame();
+    }
+
+    //게임 종료 후 재시작 유무 질의
+    private boolean restartGame() {
+
+    }
+
+    //게임 진행 로직
+    private boolean progressGame() {
+
+    }
+
+    //컴퓨터 3자리 난수 출력
+    private int[] makeComputerGameNumber() {
+
+    }
+
+
+
 }
