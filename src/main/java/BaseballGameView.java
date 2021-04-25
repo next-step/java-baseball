e8e1f0331920c0e139f8e56af442e49586dcec90
JH-PC\경재하 <wogk652@gmail.com>
2021-04-25 13:10:07 +0900

    feat(BaseballGameView.java): 숫자 입력 기능 추가

--- src/main/java/BaseballGameView.java
+++ src/main/java/BaseballGameView.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class BaseballGameView {
+import java.util.Scanner;
+
+public class BaseballGameView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    //숫자 입력
+    public String getUserNumber(){
+        System.out.println("숫자를 입력해주세요 : ");
+
+        return scanner.next();
+    }
+
 }
