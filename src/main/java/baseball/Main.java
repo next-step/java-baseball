f2814c59958933822cd689f0149b76b0a20c9cac
dongkyu <dongkyu.parkk@gmail.com>
2021-04-26 01:18:27 +0900

    feat(Main.java): 메인메서드 추가
    어플리케이션 실행용 메인메서드 추가

---
+++ src/main/java/baseball/Main.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BaseballUI baseballUI = new BaseballUI();
+        while (baseballUI.startGame()) {
+
+        }
+    }
+
+}
