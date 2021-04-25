51f94929de291a8ab7a7f82d3bfd472f7699859a
shinyoungpark <shinnyoungpark@gmail.com>
2021-04-25 20:41:45 +0900

    feat(main): main feature
    main commit

---
+++ src/main/java/com/wootechcamp/precource/baseball/BaseballGameApplication.java
@@ -0,0 +1,11 @@
+package com.wootechcamp.precource.baseball;
+
+
+public class BaseballGameApplication {
+
+    public static void main (String[] args){
+
+        System.out.println("hello!");
+
+    }
+}
