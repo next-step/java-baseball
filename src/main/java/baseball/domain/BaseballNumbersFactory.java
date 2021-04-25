d133feb523c27c1551c4c4c5e185d529c83459a7
britnin <baekseongsa@gmail.com>
2021-04-25 19:32:20 +0900

    feat: 문자열 값으로 야구 숫자 리스트 생성 구현

--- src/main/java/baseball/domain/BaseballNumbersFactory.java
+++ src/main/java/baseball/domain/BaseballNumbersFactory.java
@@ -2,26 +2,41 @@ package baseball.domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BaseballNumbersFactory {
 
-	private static List<BaseballNumber> cacheBaseballNumbers = new ArrayList<>();
+	private static final List<Integer> cacheNumbers = new ArrayList<>();
+
+	private static final Map<Integer, BaseballNumber> cacheBaseballNumbers = new HashMap<>();
 
 	static {
 		for (int i = 1; i < 10; i++) {
-			cacheBaseballNumbers.add(new BaseballNumber(i));
+			cacheNumbers.add(i);
+			cacheBaseballNumbers.put(i, new BaseballNumber(i));
 		}
 	}
 
 	private BaseballNumbersFactory() { }
 
 	public static List<BaseballNumber> create(int size) {
-		Collections.shuffle(cacheBaseballNumbers);
+		Collections.shuffle(cacheNumbers);
 
-		List<BaseballNumber> baseballNumbers  = new ArrayList<>();
+		List<BaseballNumber> baseballNumbers = new ArrayList<>();
 		for (int i = 0; i < size; i++) {
-			baseballNumbers.add(cacheBaseballNumbers.get(i));
+			baseballNumbers.add(cacheBaseballNumbers.get(cacheNumbers.get(i)));
+		}
+
+		return baseballNumbers;
+	}
+
+	public static List<BaseballNumber> create(String value) {
+		List<BaseballNumber> baseballNumbers = new ArrayList<>();
+		for (int i = 0; i < value.length(); i++) {
+			int number = Character.getNumericValue(value.charAt(i));
+			baseballNumbers.add(cacheBaseballNumbers.get(number));
 		}
 
 		return baseballNumbers;
