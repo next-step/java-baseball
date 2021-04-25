ccdab8128c26af09d6dfb755b097dc0e844d53e2
KyungSik9870 <rudtlr9870@gmail.com>
2021-04-25 17:29:41 +0900

    refactor(BallTest): 정답이 될 3자리의 임의의 수 테스트 수정
    생성된 정답의 타입을 Set -> List 로 변경함.

--- src/test/java/baseballgame/BallTest.java
+++ src/test/java/baseballgame/BallTest.java
@@ -6,31 +6,30 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallTest {
 
-    private Ball ball = new Ball();
-    private Set answer = new LinkedHashSet();
+	private Ball ball = new Ball();
+	private List<Integer> answerList;
 
-    @BeforeEach
-    void setUpRandomAnswerBall() {
-        answer = ball.getAnswer();
-    }
+	@BeforeEach
+	void setUpRandomAnswerBall() {
+		answerList = ball.getAnswerArray();
+	}
 
-    @Test
-    @DisplayName("임의의 숫자가 3개 선택되었는지 테스트.")
-    void isThreeDigitsGeneratedRandomBalls() {
-        assertThat(answer.size()).isEqualTo(3);
-    }
+	@Test
+	@DisplayName("임의의 숫자가 3개 선택되었는지 테스트.")
+	void isThreeDigitsGeneratedRandomBalls() {
+		assertThat(answerList.size()).isEqualTo(3);
+	}
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, 10})
-    @DisplayName("생성된 정답의 각 자리 숫자가 1부터 9까지를 만족하는지 테스트.")
-    void isContainsZeroOrTen(int number) {
-        assertThat(answer.contains(number)).isFalse();
-    }
+	@ParameterizedTest
+	@ValueSource(ints = {0, 10})
+	@DisplayName("생성된 정답의 각 자리 숫자가 1부터 9까지를 만족하는지 테스트.")
+	void isContainsZeroOrTen(int paramNumber) {
+		assertThat(answerList.contains(paramNumber)).isFalse();
+	}
 }
