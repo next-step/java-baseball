a3202450349ab73bcf6fb11146cd54383b5b36b1
최홍희 <vvshinevv@nhntoast.com>
2021-04-25 17:42:04 +0900

    test : String 클래스에 대한 학습 테스트 : 요구사항2
    "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 () 을 제거하고 "1,2"를 반환하도록 구현

--- src/test/java/study/BasicTest.java
+++ src/test/java/study/BasicTest.java
@@ -1,9 +1,11 @@
 package study;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -27,5 +29,18 @@ public class BasicTest {
                 Arguments.of("1,2", new String[]{"1", "2"}),
                 Arguments.of("1", new String[]{"1"})
         );
+    }
+
+    @Test
+    public void substringTest() {
+
+        // given
+        String given = "(1,2)";
+
+        // when
+        String substring = given.substring(1, 4);
+
+        // then
+        Assertions.assertThat(substring).isEqualTo("1,2");
     }
 }
