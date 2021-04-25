dfec1eb8a169092b5656b662c7a8c7d76184c8ef
cooingpop <cooingpop@gmail.com>
2021-04-25 17:42:14 +0900

    feat: 실행 메인 추가

---
+++ src/main/java/baseball/game/play/Application.java
@@ -0,0 +1,17 @@
+/*
+ * @(#) Application.java 2021. 04. 25.
+ *
+ * Created by cooingpop
+ */
+
+package baseball.game.play;
+
+/**
+ * @author 박준영
+ **/
+public class Application {
+	public static void main(String[] args) {
+
+
+	}
+}
