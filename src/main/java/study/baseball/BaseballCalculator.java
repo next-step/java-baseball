7bff4d5d04db86c6c96d4a2b99e7b31c0b9aeaf0
kwonby <kwonby@mz.co.kr>
2021-04-26 03:53:10 +0900

    test: 야구게임관련 계산 기능, 점수 출력 기능 테스트 추가
    * 두 숫자에 대한 strike, ball 점수 계산이 맞는지 확인 테스트
    * 점수에 대한 반환값 확인 테스트 구현

---
+++ src/main/java/study/baseball/BaseballCalculator.java
@@ -0,0 +1,31 @@
+package study.baseball;
+
+public class BaseballCalculator {
+
+
+    public static boolean compareNumber(int computerNumber, int playerNumber) {
+        //TODO
+        return true;
+    }
+
+
+    public static void allocatedNum(int computerNumber, int playerNumber) {
+        //TODO
+    }
+
+    public static int calculateStrike() {
+        //TODO
+        return 1;
+    }
+
+    public static int calculateBall() {
+        //TODO
+        return 1;
+    }
+
+    public static boolean compareResult(int strike, int ball) {
+        //TODO
+        return true;
+    }
+}
+
