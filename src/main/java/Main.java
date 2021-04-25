8a5e20cf733e31f802a31bbfb8f12943dd950c47
dohchoi91 <dohchoi91@gmail.com>
2021-04-25 12:10:43 +0900

    feat: 게임 실행, 종료 기능 추가
    게임 실행, 종료
    - Main 시작 시 게임 생성 및 시작
    - 사용자의 입력 값에 대한 결과가 3 스타라이크 일 경우 게임 종료

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,45 @@
+import game.GameMachine;
+import game.ResultStatus;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public final static int ANSWER = 3;
+
+    public static void main(String[] args) {
+        GameMachine gameMachine = GameMachine.of();
+        gameMachine.init(new int[]{1, 2, 3});
+
+        boolean isExit = false;
+        while (!isExit)
+            isExit = runningGame(gameMachine);
+    }
+
+    private static boolean runningGame(GameMachine gameMachine) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("숫자를 입력해주세요 : ");
+            String[] input = sc.nextLine().split(" ");
+            gameMachine.isValid(input);
+
+            int[] result = gameMachine.check(input);
+            System.out.println(gameMachine.print(result));
+
+            return checkAnswer(gameMachine, result);
+
+        } catch (IllegalStateException | IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return false;
+    }
+
+    private static boolean checkAnswer(GameMachine gameMachine, int[] result) {
+        if (result[ResultStatus.STRIKE.getIndex()] == ANSWER) {
+            System.out.println("모두 맞히졌습니다.");
+            return true;
+        }
+        return false;
     }
 }
