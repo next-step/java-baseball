32fef2dbc0b7c14859929d15259661d71866b937
cptbong <cptbbong@gmail.com>
2021-04-25 21:07:44 +0900

    test(Referee) - 생성자를통한 사이즈확인.

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,13 +1,26 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Referee {
-    private final Numbers numbers;
+    protected final Numbers numbers;
 
     public Referee() {
+        List<Integer> randomList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            generateRandomWithoutDuplicate(randomList);
+        }
+        this.numbers = new Numbers(randomList);
+    }
+
+    private void generateRandomWithoutDuplicate(List<Integer> randomList) {
         Random random = new Random();
-        int firstNumber = random.nextInt();
-        this.numbers = new Numbers();
+        int randomNumber = random.nextInt();
+        if (randomList.contains(randomNumber)) {
+            generateRandomWithoutDuplicate(randomList);
+        }
+        randomList.add(randomNumber);
     }
 }
--- src/test/java/baseball/RefereeTest.java
+++ src/test/java/baseball/RefereeTest.java
@@ -3,10 +3,19 @@ package baseball;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RefereeTest {
     @Test
     @DisplayName("기본테스트")
     void defaultTest() {
+        Referee referee = new Referee();
+        assertThat(referee.numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("스트라이크테스트")
+    void strike() {
         Referee referee = new Referee();
     }
 }
