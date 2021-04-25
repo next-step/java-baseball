ed880553d4e03c9591580332f916cb3e55d4fd6a
cptbong <cptbbong@gmail.com>
2021-04-25 19:21:29 +0900

    test - 요구사항1테스트.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,20 @@
 package study;
 
-public class StringTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    @DisplayName("\"1,2\"을 , 로 split 했을 때 1과 2로 잘 분리되는가?")
+    void require1to1() {
+        assertThat("1,2".split(",")).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("\"1\"을 , 로 split 했을 때 1만을 포함하는 배열이 반환되는가?")
+    void require1to2() {
+        assertThat("1".split(",")).contains("1");
+    }
 }
