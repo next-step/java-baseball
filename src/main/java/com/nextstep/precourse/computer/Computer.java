7a93cf222e04e2c6711cc8f30e7f6fabb26dfa32
YS <yongsang@kakao.com>
2021-04-25 11:55:37 +0900

    Feat(Computer): 정답 채점 기능 개발
    Computer 정답 채점 기능 개발
     - 스트라이크, 볼 갯수 결과 출력
     - 스트라이크가 3일 경우 정답으로 판단

--- src/main/java/com/nextstep/precourse/computer/Computer.java
+++ src/main/java/com/nextstep/precourse/computer/Computer.java
@@ -9,16 +9,25 @@ import java.util.Random;
 public class Computer {
 	private static final int ANSWER_LENGTH = 3;
 
+	private static final int RESULT_ARRAY_IDX_STRIKE = 0;
+	private static final int RESULT_ARRAY_IDX_BALL = 1;
+
+	private Random random;
 	private String answer;
 
-	public String generateNewAnswer() {
-		Random random = null;
+	public Computer() {
 		try {
 			random = SecureRandom.getInstance("SHA1PRNG");
 		} catch (NoSuchAlgorithmException e) {
 			random = new Random();
 		}
+	}
 
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public String generateNewAnswer() {
 		String newAnswer = "";
 		List<Integer> selectedNumber = new ArrayList<>();
 		while (newAnswer.length() != ANSWER_LENGTH) {
@@ -39,5 +48,55 @@ public class Computer {
 
 	public void ready() {
 		generateNewAnswer();
+	}
+
+	public boolean checkAnswer(String userInput) {
+		int[] checkResult = checkStrikeBall(userInput, answer);
+		return checkGameEnd(checkResult);
+	}
+
+	public int[] checkStrikeBall(String userInput, String answer) {
+		int[] checkResult = new int[2];
+		for (int index = 0; index < ANSWER_LENGTH; index++) {
+			checkResult[RESULT_ARRAY_IDX_STRIKE] += (userInput.charAt(index) == answer.charAt(index)) ? 1 : 0;
+			checkResult[RESULT_ARRAY_IDX_BALL] += isBall(answer, userInput.charAt(index), index) ? 1 : 0;
+		}
+		return checkResult;
+	}
+
+	private boolean isBall(String answer, char currentInput, int currentIndex) {
+		int prevIndex = ((currentIndex + 2) % 3);
+		int nextIndex = ((currentIndex + 1) % 3);
+
+		if (currentInput == answer.charAt(prevIndex)) {
+			return true;
+		}
+		if (currentInput == answer.charAt(nextIndex)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean checkGameEnd(int[] examResult) {
+		printScore(examResult);
+
+		if (examResult[RESULT_ARRAY_IDX_STRIKE] == ANSWER_LENGTH) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			return true;
+		}
+		return false;
+	}
+
+	private void printScore(int[] examResult) {
+		if (examResult[RESULT_ARRAY_IDX_STRIKE] > 0) {
+			System.out.print(String.format("%d 스트라이크 ", examResult[RESULT_ARRAY_IDX_STRIKE]));
+		}
+		if (examResult[RESULT_ARRAY_IDX_BALL] > 0) {
+			System.out.print(String.format("%d 볼", examResult[RESULT_ARRAY_IDX_BALL]));
+		}
+		if (examResult[RESULT_ARRAY_IDX_STRIKE] == 0 && examResult[RESULT_ARRAY_IDX_BALL] == 0) {
+			System.out.print("낫싱");
+		}
+		System.out.println();
 	}
 }
--- src/test/java/com/nextstep/precourse/computer/ComputerTest.java
+++ src/test/java/com/nextstep/precourse/computer/ComputerTest.java
@@ -5,14 +5,21 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 
 import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ComputerTest {
+	private Computer computer;
+
+	@BeforeEach
+	public void init() {
+		computer = new Computer();
+	}
+
 	@Test
 	@DisplayName("야구 게임 새 정답값 생성 테스트")
 	public void generateNewAnswerTest() {
-		Computer computer = new Computer();
 		String newAnswer = computer.generateNewAnswer();
 		char[] answerChars = newAnswer.toCharArray();
 		List<Object> charList = Arrays.asList(answerChars);
@@ -21,5 +28,27 @@ public class ComputerTest {
 			.hasSize(3);
 		assertThat(
 			answerChars[0] == answerChars[1] || answerChars[0] == answerChars[2] || answerChars[1] == answerChars[2]);
+	}
+
+	@Test
+	@DisplayName("입력된 값으로 스트라이크, 볼 갯수 카운팅")
+	public void checkStrikeBallTest() {
+		String answer = "123";
+		String[] inputs = {"123", "231", "456", "156", "256"};
+		int[][] results = {{3, 0}, {0, 3}, {0, 0}, {1, 0}, {0, 1}};
+
+		assertThat(Arrays.asList(inputs)).map(input -> computer.checkStrikeBall((String)input, answer))
+			.containsExactly(results);
+	}
+
+	@Test
+	@DisplayName("입력된 값 정답 여부 확인")
+	public void checkAnswerTest() {
+		String answer = "123";
+		String[] inputs = {"123", "231", "456", "156", "256"};
+
+		computer.setAnswer(answer);
+		assertThat(Arrays.asList(inputs)).map(input -> computer.checkAnswer((String)input))
+			.containsExactly(true, false, false, false, false);
 	}
 }
