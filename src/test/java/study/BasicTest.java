7f00f104ec74b38b2621ae870feda71abb04a481
최홍희 <vvshinevv@nhntoast.com>
2021-04-25 17:52:24 +0900

    test : String 클래스에 대한 학습 테스트 : 요구사항3
    - "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현
    - String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면
    StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현
    - JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러냄

--- src/test/java/study/BasicTest.java
+++ src/test/java/study/BasicTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -42,5 +41,41 @@ public class BasicTest {
 
         // then
         Assertions.assertThat(substring).isEqualTo("1,2");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringForCharAtTest")
+    public void charAtTest(String given, int index, char expected) {
+
+        // when
+        char charAt = given.charAt(index);
+
+        // then
+        Assertions.assertThat(charAt).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideStringForCharAtTest() {
+        return Stream.of(
+                Arguments.of("abc", 0, 'a'),
+                Arguments.of("abc", 1, 'b'),
+                Arguments.of("abc", 2, 'c')
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringForCharAtStringIndexOutOfBoundExceptionTest")
+    public void charAtStringIndexOutOfBoundExceptionTest(String given, int index, String expected) {
+
+        Assertions.assertThatThrownBy(() -> {
+            char ch = given.charAt(index);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining(expected);
+    }
+
+    private static Stream<Arguments> provideStringForCharAtStringIndexOutOfBoundExceptionTest() {
+        return Stream.of(
+                Arguments.of("abc", 3, "String index out of range: 3"),
+                Arguments.of("abc", 4, "String index out of range: 4")
+        );
     }
 }
