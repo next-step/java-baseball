3d84b549b2ddebe7f597d5113dfab8c09700809c
HyungjuJang <dacapolife87@gmail.com>
2021-04-25 23:30:30 +0900

    refactor : 입력받은 숫자의 값이 정수가 아닐경우 예외처리

--- src/main/java/util/TransStringNumberToNumberListUtil.java
+++ src/main/java/util/TransStringNumberToNumberListUtil.java
@@ -5,16 +5,28 @@ import java.util.List;
 
 public class TransStringNumberToNumberListUtil {
 
+    private static final String EXCEPTION_MESSAGE_INVALID_NUMBER_CHAR = "입력값은 정수여야만 합니다.";
+
     private TransStringNumberToNumberListUtil() {
     }
 
     public static List<Integer> toNumberList(String numberString) {
         List<Integer> numbers = new ArrayList<>();
-        char[] numberChar = numberString.toCharArray();
-        for (char number : numberChar) {
-            numbers.add(Integer.parseInt(String.valueOf(number)));
+        char[] numberChars = numberString.toCharArray();
+        for (char numberChar : numberChars) {
+            numbers.add(parseNumber(numberChar));
         }
 
         return numbers;
+    }
+
+    private static int parseNumber(char numberChar) {
+        int number;
+        try {
+            number = Integer.parseInt(String.valueOf(numberChar));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_INVALID_NUMBER_CHAR);
+        }
+        return number;
     }
 }
