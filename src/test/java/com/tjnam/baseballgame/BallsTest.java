2b10a460bf156799be0ff937d86859018ca38dfe
TJ NAM <dynamicnam@gmail.com>
2021-04-25 23:48:42 +0900

    test(balls test) 랜덤 숫자 뽑기의 테스트 구현
    랜덤 숫자 뽑기에 대한 테스트 구현

--- src/test/java/com/tjnam/baseballgame/BallsTest.java
+++ src/test/java/com/tjnam/baseballgame/BallsTest.java
@@ -1,4 +1,62 @@
 package com.tjnam.baseballgame;
 
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BallsTest {
+
+    private static String ballString = "123";
+    private static int[] ballValues = new int[]{1, 2, 3};
+
+    @Test
+    public void getRandomNumbersTest(){
+        Balls ball = new Balls();
+
+        try{
+            Method getRandomNumbersMethod = ball.getClass().getDeclaredMethod("getRandomNumbers");
+            getRandomNumbersMethod.setAccessible(true);
+
+            int[] randomNumbers = (int[]) getRandomNumbersMethod.invoke(ball);
+
+            assertThat(randomNumbers.length).isEqualTo(3);
+
+            assertThat(randomNumbers[0]).isPositive()
+                    .isGreaterThan(0)
+                    .isLessThan(10);
+
+            assertThat(randomNumbers[1]).isPositive()
+                    .isGreaterThan(0)
+                    .isLessThan(10);
+
+            assertThat(randomNumbers[2]).isPositive()
+                    .isGreaterThan(0)
+                    .isLessThan(10);
+
+            assertThat(randomNumbers[0]).isNotEqualTo(randomNumbers[1]);
+            assertThat(randomNumbers[1]).isNotEqualTo(randomNumbers[2]);
+            assertThat(randomNumbers[0]).isNotEqualTo(randomNumbers[2]);
+
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void setBallByIntArrayTest(){
+        Balls ball = new Balls(ballValues);
+
+        int[] setBalls = ball.getNumbers();
+        assertThat(setBalls).isEqualTo(ballValues);
+
+        ball = new Balls();
+        ball.setBallValues(ballValues);
+        setBalls = ball.getNumbers();
+        assertThat(setBalls).isEqualTo(ballValues);
+
+    }
+
 }
