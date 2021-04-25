f92f9cd186bebd5d69ccf14162132b8622bcbfb6
breezeunn <je7689@gmail.com>
2021-04-25 23:48:39 +0900

    feat(ScannerUtil): 사용자 숫자 입력 추가
    ScannerUtil 신규 생성:
    * 사용자로부터 3자리의 수를 입력받는 기능

--- src/main/java/baseball/GameMain.java
+++ src/main/java/baseball/GameMain.java
@@ -1,9 +1,11 @@
 package baseball;
 
 import util.RandomUtil;
+import util.ScannerUtil;
 
 public class GameMain {
     public static void main(String[] args) {
         int[] com = RandomUtil.getRandom3DigitWithoutDuplicates();
+        int[] user = ScannerUtil.getUserInputAsArray();
     }
 }
