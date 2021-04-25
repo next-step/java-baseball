18ed297a4dc5325d801e2a9f396c17a17b773753
mac2019 <pooheyup@naver.com>
2021-04-25 20:13:03 +0900

    UserSelectOption - 1.restart 기능구현

--- src/main/java/UserSelectOption.java
+++ src/main/java/UserSelectOption.java
@@ -1,2 +1,8 @@
 public class UserSelectOption {
+    public boolean selectOption(String input) {
+        if(input == "1"){
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/UserSelectOptionTest.java
+++ src/test/java/UserSelectOptionTest.java
@@ -1,11 +1,18 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class UserSelectOptionTest {
     private UserSelectOption option;
 
     @Before
     public void setUp(){
         option = new UserSelectOption();
+    }
+
+    @Test
+    public void selectOptionRestart(){
+        assertEquals(option.selectOption("1"), true);
     }
 }
