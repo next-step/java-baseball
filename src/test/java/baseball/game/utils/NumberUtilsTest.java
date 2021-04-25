afa094d5e9a3b5c1bcf1614ec1c669b58be94743
cooingpop <cooingpop@gmail.com>
2021-04-25 22:07:57 +0900

    refactor: import 정리

--- src/test/java/baseball/game/utils/NumberUtilsTest.java
+++ src/test/java/baseball/game/utils/NumberUtilsTest.java
@@ -1,7 +1,7 @@
 package baseball.game.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
