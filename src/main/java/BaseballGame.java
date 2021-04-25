17e725b10a100dd173a2d3b1e378e26af3bbddce
조예찬 <choyuichan@gmail.com>
2021-04-25 21:23:35 +0900

    기능: 세자리 숫자 만들기
    세 자리 숫자 만들기 (generateNumers 메서드 추가)
    - 세자리 숫자를 만드는지 테스트 추가

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,3 +1,7 @@
 public class BaseballGame {
 
+	public Integer generateNumbers(){
+		return 100;
+	}
+
 }
--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -1,3 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class BaseballGameTest{
 
-}
+	@Test
+	public void couldMakeThreeDigits(){
+		BaseballGame game = new BaseballGame();
+		Integer numbers = game.generateNumbers();
+		assertTrue(numbers >= 100 && numbers <= 999);
+	}
+
+	}
