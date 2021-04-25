4a47cb1820d49ad118256831412aba3655e9d664
daeyong-sim <48154dd3ccc2eff16343e00714bdf55ae2caa334>
2021-04-25 09:56:19 +0900

    feat(BaseballGame): add Random Numbers function

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class BaseballGame {
 
+	final int NUMBER_SIZE = 3;
+
 	public static void main(String[] args){
 		do{
 			// TODO: play Baseball game
@@ -16,15 +18,30 @@ public class BaseballGame {
 
 	public void playBaseballGame(){
 		// TODO: init numbers
+		String randomNumbers = getRandomNumbers();
 
 		// get input numbers
 		String input = getInput();
 		// TODO: show score
 		// TODO: check Game status
+
+		System.out.println("Random Numbers: " + randomNumbers);
+	}
+
+	public String getRandomNumbers(){
+		StringBuilder randomNumber = new StringBuilder();
+		for(int i = 0; i < NUMBER_SIZE; ){
+			String r = (int)((Math.random() * 9) + 1) + "";
+			if(randomNumber.toString().contains(r))
+				continue;
+			randomNumber.append(r);
+			i++;
+		}
+		return randomNumber.toString();
 	}
 
 	public String getInput(){
-		String input = "";
+		String input;
 		Scanner scanner = new Scanner(System.in);
 		input = scanner.nextLine();
 		return input;
