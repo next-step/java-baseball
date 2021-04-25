f9c85379b30177d22a53f67cd3a2feaff8f435d8
britnin <baekseongsa@gmail.com>
2021-04-25 15:51:59 +0900

    feat: 숫자 야구 게임 실행 구현

---
+++ src/main/java/baseball/Main.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import baseball.domain.BaseballGame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseballGame.play();
+	}
+
+}
---
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+public class BaseballGame {
+
+	public static void play() {
+
+	}
+
+}
