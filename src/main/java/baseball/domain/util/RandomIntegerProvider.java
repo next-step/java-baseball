eaa46e8e1a2f3f4e5956bb3b4d062c722c4fc7fa
eunsolJo <eunsol704@gmail.com>
2021-04-25 14:44:08 +0900

    test: [1.1] 랜덤 정수 생성 기능 테스트

---
+++ src/main/java/baseball/domain/util/RandomIntegerProvider.java
@@ -0,0 +1,9 @@
+package baseball.domain.util;
+
+public class RandomIntegerProvider {
+
+    public static int getRandomNumber(int minNumber, int maxNumber) {
+        // TODO
+        return 0;
+    }
+}
