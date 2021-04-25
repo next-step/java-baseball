3f2711468f3032f38f29fe2216f1b59522fcf19a
Jun <p_illusion@naver.com>
2021-04-26 01:42:47 +0900

    [feat] Game Main 메인을 구현하고 구현하다 보니 필요한 input, output view 작성 또한 입력 때 필요한 string --> number 공통 유틸 생성

--- src/main/java/GameMain.java
+++ src/main/java/GameMain.java
@@ -1,2 +1,25 @@
+import model.Numbers;
+import model.WinningNumbers;
+import view.ConsoleInputView;
+import view.ConsoleOutputView;
+
 public class GameMain {
+	public static void main(final String... args){
+		final WinningNumbers inputWinningNumbers = ConsoleInputView.readWinningNumbers();
+
+		int strikeCount = 0;
+		int ballCount = 0;
+
+		do {
+			while(strikeCount < 3) {
+				Numbers gameNumbers = ConsoleInputView.readNumbers();
+				strikeCount = inputWinningNumbers.getStrike(gameNumbers);
+				ballCount = inputWinningNumbers.getBall(gameNumbers);
+				ConsoleOutputView.printGameResult(strikeCount, ballCount);
+			}
+
+			ConsoleOutputView.printEndGame();
+			ConsoleOutputView.newline();
+		} while (ConsoleInputView.readRestart() == 1);
+	}
 }
