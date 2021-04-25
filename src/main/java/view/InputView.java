538bdea890c874d60f4da247687ddda7f6c85b46
HyungjuJang <dacapolife87@gmail.com>
2021-04-25 19:05:03 +0900

    refactor : 출력문구 상수로 추출

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String MESSAGE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String MESSAGE_RENEW_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     private Scanner scanner;
 
     public InputView() {
@@ -11,12 +14,12 @@ public class InputView {
     }
 
     public String inputNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(MESSAGE_INPUT_NUMBER);
         return scanner.nextLine();
     }
 
     public int isContinue() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(MESSAGE_RENEW_OR_END);
         return Integer.parseInt(scanner.nextLine());
     }
 }
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -4,11 +4,13 @@ import domain.BaseballResult;
 
 public class ResultView {
 
+    private static final String FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+
     public void showResult(BaseballResult result) {
         System.out.println(result.resultScreen());
     }
 
     public void finishScreen() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        System.out.println(FINISH_MESSAGE);
     }
 }
