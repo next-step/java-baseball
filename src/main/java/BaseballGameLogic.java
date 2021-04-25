98173cca0bfae9d254035b31c637e4328a46ffb1
younghoon <bblnotbb@yahho.com>
2021-04-26 03:41:30 +0900

    feat : get un-duplicate number

--- src/main/java/BaseballGameLogic.java
+++ src/main/java/BaseballGameLogic.java
@@ -3,9 +3,32 @@ import java.util.Set;
 
 public class BaseballGameLogic {
 	private StringBuilder number;
+	private StringBuilder previousNumber;
+	private final int retryLimit = 5;
+	private int retryCount = 0;
 
 	public void makeNumber() {
+		if (!isFirstTry()) {
+			setPreviousNumber();
+			setUnduplicateNumber();
+
+			return;
+		}
+
 		setNumber();
+	}
+
+	private void setUnduplicateNumber() {
+		while (previousNumber.equals(number) && retryCount < retryLimit) {
+			setNumber();
+			retryCount++;
+		}
+
+		retryCount = 0;
+	}
+
+	private boolean isFirstTry() {
+		return previousNumber == null ? true : false;
 	}
 
 	private void setNumber() {
@@ -19,6 +42,10 @@ public class BaseballGameLogic {
 			number.append(singleNumber);
 			usedNumbers.add(singleNumber);
 		}
+	}
+
+	private void setPreviousNumber() {
+		this.previousNumber = this.number;
 	}
 
 	private int getUnduplicateSingleDigit(Set<Integer> usedNumbers) {
