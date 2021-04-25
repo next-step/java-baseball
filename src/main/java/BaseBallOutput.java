180928935460d7bfac0993744d49a26218a83cef
leemingyu09 <hikakaka13@naver.com>
2021-04-25 16:36:55 +0900

    feat: 입력값 볼카운트를 판단(출력기능 추가)

---
+++ src/main/java/BaseBallOutput.java
@@ -0,0 +1,40 @@
+public class BaseBallOutput {
+
+    protected int resultOfAtack(BaseBallDTO dto){
+        int result = new BaseBallOutput()
+                .strikeOutput(dto)
+                .ballOutput(dto)
+                .nothingOutput(dto)
+                .threeStrikeOutput(dto);
+        return result;
+    }
+
+    public BaseBallOutput strikeOutput(BaseBallDTO dto){
+        if(dto.getCountOfStrike()>0){
+            System.out.print(dto.getCountOfStrike()+" 스트라이크 ");
+        }
+        return this;
+    }
+    public BaseBallOutput ballOutput(BaseBallDTO dto){
+        if(dto.getCountOfBall()>0){
+            System.out.print(dto.getCountOfBall()+" 볼");
+        }
+        return this;
+    }
+
+    public BaseBallOutput nothingOutput(BaseBallDTO dto){
+        if((dto.getCountOfStrike()+dto.getCountOfBall())==0){
+            System.out.print("낫싱");
+        }
+        return this;
+    }
+
+    public int threeStrikeOutput(BaseBallDTO dto){
+        if(dto.getCountOfStrike()==3){
+            System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        }
+        System.out.println();
+        return dto.getCountOfStrike();
+    }
+
+}
