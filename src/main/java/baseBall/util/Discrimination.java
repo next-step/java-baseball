1f1e67648fd02e740c0a44f19de79399c45e6277
김수윤 <darecaga1@gamil.com>
2021-04-25 20:37:37 +0900

    feat: 사용자 숫자, 시스템 숫자 비교하여 스트라이크, 볼 판정 기능

--- src/main/java/baseBall/util/Discrimination.java
+++ src/main/java/baseBall/util/Discrimination.java
@@ -8,27 +8,29 @@ public class Discrimination {
     private StringBuilder systemNumbers;
     static CreateNumber createNumber;
 
+    public void match (String systemNumber, String userNumber) {
+        for(int i=0; i < userNumber.length(); i++) {
+            getStrikeCount(systemNumber, userNumber.charAt(i), i);
+            getBallCount(systemNumber, userNumber.charAt(i), i);
+        }
+
+    }
 
 
-    public int getStrikeCount(String systemNumber, String userNumber, int index) {
+
+    public void getStrikeCount(String systemNumber, char userNumber, int index) {
 
         if(systemNumber.indexOf(userNumber) == index) {
             strikeCount++;
         }
 
-        return strikeCount;
     }
 
-    public int getBallCount(String SystemNumber, String userNumber, int index) {
+    public void getBallCount(String SystemNumber, char userNumber, int index) {
         if(SystemNumber.indexOf(userNumber) > -1 && SystemNumber.indexOf(userNumber) != index) {
             ballCount++;
         }
 
-        return ballCount;
-    }
-
-    public void getSystemNumber() {
-        systemNumbers = createNumber.generateNumbers();
     }
 
 
--- src/test/java/DiscriminationTest.java
+++ src/test/java/DiscriminationTest.java
@@ -14,14 +14,30 @@ public class DiscriminationTest {
     @DisplayName("위치와 숫자가 일치하는 경우 스트라이크 판정 테스트")
     @ParameterizedTest
     @CsvSource(value = {"124 : 2"}, delimiter = ':')
-    public void discriminationStrikeTest(String systemNumber, String userNumber) {
+    public void discriminationStrikeTest(String systemNumber, char userNumber) {
         assertThat(discrimination.getStrikeCount(systemNumber, userNumber, 1)).isEqualTo(1);
     }
 
     @DisplayName("위치는 다르지만 숫자가 일치하는 경우 스트라이크 판정 테스트")
     @ParameterizedTest
     @CsvSource(value = {"124 : 1"}, delimiter = ':')
-    public void discriminationBallTest(String systemNumber, String userNumber) {
+    public void discriminationBallTest(String systemNumber, char userNumber) {
         assertThat(discrimination.getBallCount(systemNumber, userNumber, 1)).isEqualTo(1);
+    }
+
+    @DisplayName("스트라이크 볼 판정 테스트 2스트라이크")
+    @ParameterizedTest
+    @CsvSource(value = "124 : 123", delimiter = ':')
+    public void matchSrikeTest(String systemNumber, String userNumber) {
+        discrimination.match(systemNumber, userNumber);
+
+    }
+
+    @DisplayName("스트라이크 볼 판정 테스트 1볼 1스트라이크")
+    @ParameterizedTest
+    @CsvSource(value = "124 : 143", delimiter = ':')
+    public void matchBallTest(String systemNumber, String userNumber) {
+        discrimination.match(systemNumber, userNumber);
+
     }
 }
