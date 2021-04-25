155ce53fc3ecd4874c6eb67c72a1857f4dcf4cd2
mac2019 <pooheyup@naver.com>
2021-04-25 19:12:06 +0900

    BaseballGameResultPrinter -  convertBaseballGameResult 1. 낫싱, 2. 단일 스트라이크 처리

--- src/main/java/BaseballGameResultPrinter.java
+++ src/main/java/BaseballGameResultPrinter.java
@@ -1,7 +1,10 @@
 public class BaseballGameResultPrinter {
 
     public String convertBaseballGameResult(int[] res) {
-        return "낫싱";
+        return isNothing(res) ? "낫싱" : res[0] + " 스트라이크";
+    }
+    private boolean isNothing(int[] res) {
+        return res[0] == 0 && res[1] == 0;
     }
 
 }
--- src/test/java/BaseballGameResultPrinterTest.java
+++ src/test/java/BaseballGameResultPrinterTest.java
@@ -17,4 +17,11 @@ public class BaseballGameResultPrinterTest {
         assertEquals(printer.convertBaseballGameResult(new int[]{0, 0}), "낫싱");
     }
 
+    @Test
+    public void printerConvertGameResultStrikes(){
+        assertEquals(printer.convertBaseballGameResult(new int[]{1, 0}), "1 스트라이크");
+        assertEquals(printer.convertBaseballGameResult(new int[]{2, 0}), "2 스트라이크");
+        assertEquals(printer.convertBaseballGameResult(new int[]{3, 0}), "3 스트라이크");
+    }
+
 }
