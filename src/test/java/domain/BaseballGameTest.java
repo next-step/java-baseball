ebf563a8fde0861b80081a6ebe7f7228d9c3723a
yjs2952 <yjs2952@naver.com>
2021-04-25 16:15:02 +0900

    refactor: Numbers 생성 유효성 검사 테스트 코드 NumbersTest 로 이관

--- src/test/java/domain/BaseballGameTest.java
+++ src/test/java/domain/BaseballGameTest.java
@@ -9,33 +9,5 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class BaseballGameTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"2345", "12", "1"})
-    @DisplayName("입력한 숫자 길이가 3보다 크거나 작을 시 예외 발생")
-    void whenInputNotValidNumberLengthThenExceptionTest(String numbers) {
 
-        // given
-        NumberGenerator numberGenerator = () -> Numbers.of(NumberUtils.toList(numbers));
-
-        // then
-        assertNumbersThrowIllegalArgumentException(numberGenerator, "숫자는 3자리 수여야 합니다.");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"111", "122", "331"})
-    @DisplayName("입력한 숫자에 중복이 있을 시 예외 발생")
-    void whenInputDuplicatedNumberThenExceptionTest(String numbers) {
-
-        // given
-        NumberGenerator numberGenerator = () -> Numbers.of(NumberUtils.toList(numbers));
-
-        // then
-        assertNumbersThrowIllegalArgumentException(numberGenerator, "숫자는 서로 다른 수여야 합니다.");
-    }
-
-    private void assertNumbersThrowIllegalArgumentException(NumberGenerator numberGenerator, String message) {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> new BaseballGame(numberGenerator))
-                .withMessage(message);
-    }
 }
---
+++ src/test/java/domain/NumbersTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import utils.NumberUtils;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class NumbersTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"2345", "12", "1"})
+    @DisplayName("입력한 숫자 길이가 3보다 크거나 작을 시 예외 발생")
+    void whenInputNotValidNumberLengthThenExceptionTest(String numbers) {
+
+        // then
+        assertNumbersThrowIllegalArgumentException(numbers, "숫자는 3자리 수여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"111", "122", "331"})
+    @DisplayName("입력한 숫자에 중복이 있을 시 예외 발생")
+    void whenInputDuplicatedNumberThenExceptionTest(String numbers) {
+
+        // then
+        assertNumbersThrowIllegalArgumentException(numbers, "숫자는 서로 다른 수여야 합니다.");
+    }
+
+    private void assertNumbersThrowIllegalArgumentException(String numbers, String message) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> Numbers.of(NumberUtils.toList(numbers)))
+                .withMessage(message);
+    }
+}
