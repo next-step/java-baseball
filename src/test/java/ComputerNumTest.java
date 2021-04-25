98a82034e544c0105d93aa7038e840a9fc417315
이유진 <ujin@deali.net>
2021-04-26 01:26:58 +0900

    test: 1부터 9까지의 서로 다른 수로 이루어진 리스트 생성 TEST 변수 이동
    해당 테스트 내부에서만 사용될 변수를 메서드 내부로 이동

--- src/test/java/ComputerNumTest.java
+++ src/test/java/ComputerNumTest.java
@@ -4,14 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 class ComputerNumTest {
-    Integer startNum = 1;
-    Integer endNum = 10;
     @Test
     @DisplayName("컴퓨터 수를 설정할 1-9의 숫자 리스트를 확인한다")
     void setDefaultNumCheckTest(){
+        int START = 1;
+        int END = 10;
         ComputerNum computerNum = new ComputerNum();
         List<Integer> defaultNum = computerNum.setDefaultNum();
-        for(Integer i = startNum; i < endNum; i++){
+        for(int i = START; i < END; i++){
             assertThat(defaultNum.contains(i)).isTrue();
         }
     }
