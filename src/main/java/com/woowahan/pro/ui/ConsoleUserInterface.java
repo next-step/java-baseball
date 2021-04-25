eed7cc6553ff60946592df51d62ef5de340f4c8e
YoonBing9 <akass191@gmail.com>
2021-04-25 23:32:56 +0900

    refactor(입출력기): 메시지 리소스와 입출력 로직 분리

--- src/main/java/com/woowahan/pro/ui/ConsoleUserInterface.java
+++ src/main/java/com/woowahan/pro/ui/ConsoleUserInterface.java
@@ -4,12 +4,6 @@ import java.util.Scanner;
 
 public class ConsoleUserInterface implements UserInterface {
     private final Scanner scanner = new Scanner(System.in);
-    private static final String REQUIRE_NUMBERS = "숫자를 입력해주세요: ";
-    private static final String GAME_DONE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
-    private static final String STRIKE = "스트라이크";
-    private static final String BALL = "볼";
-    private static final String NOTHING = "낫싱";
 
     public String input() {
         return scanner.nextLine();
@@ -21,14 +15,14 @@ public class ConsoleUserInterface implements UserInterface {
 
     @Override
     public String[] requireNumbers() {
-        output(REQUIRE_NUMBERS);
+        output(Message.REQUIRE_NUMBERS);
         String[] playerNumbers = input().split("");
         return playerNumbers;
     }
 
     @Override
     public void outputOnFinishGame() {
-        output(GAME_DONE);
+        output(Message.GAME_DONE);
     }
 
     @Override
@@ -40,13 +34,13 @@ public class ConsoleUserInterface implements UserInterface {
     public void outputScore(int strike, int ball) {
         StringBuilder result = new StringBuilder();
         if (strike > 0) {
-            result.append(strike + " " + STRIKE + " ");
+            result.append(strike + " " + Message.STRIKE + " ");
         }
         if (ball > 0) {
-            result.append(ball + " " + BALL);
+            result.append(ball + " " + Message.BALL);
         }
         if (result.length() == 0) {
-            result.append(NOTHING);
+            result.append(Message.NOTHING);
         }
         output(result + "\n");
     }
