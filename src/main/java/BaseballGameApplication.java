7ee167efbf0375c562cdce5db062962954728b90
bwowby <pring120@gmail.com>
2021-04-25 18:27:17 +0900

    feat: 게임 실행 application class 생성

---
+++ src/main/java/BaseballGameApplication.java
@@ -0,0 +1,8 @@
+import baseball.common.BaseballGame;
+
+public class BaseballGameApplication {
+    public static void main(String[] args) {
+        BaseballGame game = new BaseballGame();
+        game.startGame();
+    }
+}
