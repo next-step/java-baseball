50cfece8461ad4721de2f1eb11656f3b71443c8b
junseok Oh <ixtears23@icloud.com>
2021-04-25 22:27:10 +0900

    feat(rule) : 낫싱, 포볼 규칙
    스트라이크와 볼이 하나도 없는 경우(0스트라이크, 0볼)
    낫싱, 포볼이라 판단해서 true 반환

--- src/main/java/com/woowahan/precourse/baseball/rule/BaseBallRule.java
+++ src/main/java/com/woowahan/precourse/baseball/rule/BaseBallRule.java
@@ -15,4 +15,8 @@ public class BaseBallRule {
     public static boolean isVictory(int strikeCount) {
         return strikeCount == 3;
     }
+
+    public static boolean isNothingAndFourBall(int strikeCount, int ballCount) {
+        return strikeCount == 0 && ballCount == 0;
+    }
 }
