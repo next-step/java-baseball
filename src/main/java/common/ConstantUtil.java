4b3a7e4c11bcf2220bdb348e647c02d654a905a3
Kyungbin Ryu <ryukbin94@gmail.com>
2021-04-25 23:32:38 +0900

    feat: Hitter(타자) 객체 생성 시 서로 다른 무작위 양의 정수 3개(볼)를 생성하는 기능 추가
    숫자 야구 게임에서 필요한 3개의 서로 다른 무작위 양의 정수 3개를 생성해 ballList로 사용하는 기능을 Hitter(타자)
    클래스에 추가했습니다.

---
+++ src/main/java/common/ConstantUtil.java
@@ -0,0 +1,8 @@
+package common;
+
+public class ConstantUtil {
+    public static final int RANDOM_NUMBER_SIZE = 3;
+    public static final int RANDOM_NUMBER_MAX = 9;
+    public static final int RANDOM_NUMBER_MIN = 1;
+    public static final String INPUT_NUMBER_PATTERN = "^[0-9]{3}$";
+}
--- src/main/java/game/Hitter.java
+++ src/main/java/game/Hitter.java
@@ -1,4 +1,29 @@
 package game;
 
+import common.ConstantUtil;
+
+import java.util.*;
+
 public class Hitter {
+
+    private final List<Integer> ballList;
+
+    public List<Integer> getBallList() {
+        return ballList;
+    }
+
+    public Hitter() {
+        this.ballList = makeRandomBalls();
+    }
+
+    public List<Integer> makeRandomBalls() {
+        Set<Integer> randomNumberSet = new LinkedHashSet<>();
+        Random random = new Random();
+
+        while (randomNumberSet.size() < ConstantUtil.RANDOM_NUMBER_SIZE) {
+            randomNumberSet.add(random.nextInt(ConstantUtil.RANDOM_NUMBER_MAX + ConstantUtil.RANDOM_NUMBER_MIN) + ConstantUtil.RANDOM_NUMBER_MIN);
+        }
+
+        return new ArrayList<>(randomNumberSet);
+    }
 }
