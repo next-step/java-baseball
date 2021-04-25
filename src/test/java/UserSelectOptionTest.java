edfa91aa7c8602ebb15550dd1c019e74da9d5395
mac2019 <pooheyup@naver.com>
2021-04-25 19:55:38 +0900

    UserSelectOption - 생성 테스트 및 리팩토링 완료

---
+++ src/test/java/UserSelectOptionTest.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserSelectOptionTest {
+    private UserSelectOption option;
+
+    @Before
+    public void setUp(){
+        option = new UserSelectOption();
+    }
+}
