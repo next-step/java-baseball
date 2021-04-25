3ede10fb38f81b5001c34c22f113b22f3abb32f7
jordy-torvalds <jordy-torvalds@kakao.com>
2021-04-26 01:28:56 +0900

    feat(Score) : 점수 채점 및 결과 메시지 출력과 관련된 기능 및 테스트 구현

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -1,6 +1,69 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.domain.BaseballNumbers.RANDOM_NUMBER_COUNT;
+import static java.lang.String.format;
+
 public class Score {
     private int strike;
     private int ball;
+
+    public static Score measureScore (BaseballNumbers answer, BaseballNumbers playerInput) {
+        Score result = new Score();
+        for (int i = 0 ; i < RANDOM_NUMBER_COUNT ; i++) {
+            result.judgeBaseballNumber(answer, playerInput.getSpecificIndexValue(i), i);
+        }
+
+        return result;
+    }
+
+    private void judgeBaseballNumber(BaseballNumbers answer, String partialPlayerInput, int playerInputIndex) {
+        int judgementResult = answer.indexOf(partialPlayerInput);
+        if (judgementResult == playerInputIndex) {
+            plusStrikeCount();
+        } else if (judgementResult > -1) {
+            plusBallCount();
+        }
+    }
+
+    private void plusStrikeCount() {
+        this.strike++;
+    }
+
+    private void plusBallCount() {
+        this.ball++;
+    }
+
+    public String getJudgeMessage() {
+        String result = "";
+        if (strike == 0 && ball == 0)
+            result = "낫싱";
+        else if (strike == RANDOM_NUMBER_COUNT)
+            result = format("%d개의 숫자를 모두 맞히셨습니다! 게임종료", RANDOM_NUMBER_COUNT);
+        else if (strike >= 1 || ball >= 1)
+            result = getStrikeBallMessage();
+
+        return result;
+    }
+
+    private String getStrikeBallMessage() {
+        List<String> result = new ArrayList<>();
+        if (strike >= 1 && strike < RANDOM_NUMBER_COUNT)
+            result.add(format("%d 스트라이크", strike));
+
+        if (ball >= 1)
+            result.add(format("%d 볼", ball));
+
+        return String.join(" ", result);
+    }
+
+    public boolean isPassed() {
+        boolean result = false;
+        if (strike == RANDOM_NUMBER_COUNT) {
+            result = true;
+        }
+        return result;
+    }
 }
---
+++ src/test/java/baseball/domain/ScoreTest.java
@@ -0,0 +1,122 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static baseball.domain.BaseballNumbers.RANDOM_NUMBER_COUNT;
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("점수_측정_테스트")
+class ScoreTest {
+
+    @ParameterizedTest
+    @DisplayName("정답인_경우_테스트")
+    @ValueSource(strings = {"123", "423", "836"})
+    void testPassingCase(String arg) {
+        // Given
+        BaseballNumbers sameBaseballNumbers = new BaseballNumbers(arg);
+
+        // When
+        Score score = Score.measureScore(sameBaseballNumbers, sameBaseballNumbers);
+
+        // Then
+        assertTrue(score.isPassed());
+    }
+
+    @ParameterizedTest
+    @DisplayName("정답인_경우_메시지_확인")
+    @ValueSource(strings = {"123", "423", "836"})
+    void testPassingMessage(String arg) {
+        // Given
+        BaseballNumbers sameBaseballNumbers = new BaseballNumbers(arg);
+
+        // When
+        Score score = Score.measureScore(sameBaseballNumbers, sameBaseballNumbers);
+
+        // Then
+        assertEquals(format("%d개의 숫자를 모두 맞히셨습니다! 게임종료", RANDOM_NUMBER_COUNT), score.getJudgeMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("1_스트라이크인_경우_메시지_확인")
+    @ValueSource(strings = {"123,145", "421,325", "137,247"})
+    void testOneStrikeMessage(String arg) {
+        // Given
+        String[] argArray = arg.split(",");
+        BaseballNumbers answer = new BaseballNumbers(argArray[0]);
+        BaseballNumbers playerInput = new BaseballNumbers(argArray[01]);
+
+        // When
+        Score score = Score.measureScore(answer, playerInput);
+
+        // Then
+        assertEquals("1 스트라이크", score.getJudgeMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("2_스트라이크인_경우_메시지_확인")
+    @ValueSource(strings = {"123,125", "421,521", "137,147"})
+    void testTwoStrikeMessage(String arg) {
+        // Given
+        String[] argArray = arg.split(",");
+        BaseballNumbers answer = new BaseballNumbers(argArray[0]);
+        BaseballNumbers playerInput = new BaseballNumbers(argArray[01]);
+
+        // When
+        Score score = Score.measureScore(answer, playerInput);
+
+        // Then
+        assertEquals("2 스트라이크", score.getJudgeMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("1_볼인_경우_메시지_확인")
+    @ValueSource(strings = {"123,415", "421,352", "137,274"})
+    void testOneBallMessage(String arg) {
+        // Given
+        String[] argArray = arg.split(",");
+        BaseballNumbers answer = new BaseballNumbers(argArray[0]);
+        BaseballNumbers playerInput = new BaseballNumbers(argArray[01]);
+
+        // When
+        Score score = Score.measureScore(answer, playerInput);
+
+        // Then
+        assertEquals("1 볼", score.getJudgeMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("1_스트라이크_1_볼인_경우_메시지_확인")
+    @ValueSource(strings = {"123,142", "421,251", "137,417"})
+    void testOneStrikeOneBallMessage(String arg) {
+        // Given
+        String[] argArray = arg.split(",");
+        BaseballNumbers answer = new BaseballNumbers(argArray[0]);
+        BaseballNumbers playerInput = new BaseballNumbers(argArray[01]);
+
+        // When
+        Score score = Score.measureScore(answer, playerInput);
+
+        // Then
+        assertEquals("1 스트라이크 1 볼", score.getJudgeMessage());
+    }
+
+    @ParameterizedTest
+    @DisplayName("낫싱인_경우_메시지_확인")
+    @ValueSource(strings = {"312,645", "421,378", "375,164"})
+    void testNothingMessage(String arg) {
+        // Given
+        String[] argArray = arg.split(",");
+        BaseballNumbers answer = new BaseballNumbers(argArray[0]);
+        BaseballNumbers playerInput = new BaseballNumbers(argArray[01]);
+
+        // When
+        Score score = Score.measureScore(answer, playerInput);
+
+        // Then
+        assertEquals("낫싱", score.getJudgeMessage());
+    }
+}
