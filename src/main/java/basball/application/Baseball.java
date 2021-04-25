46e3e1ea1d7d927712a3297b59cab84c7b4bc8f8
kwkim <kkw5657@gmail.com>
2021-04-25 19:45:01 +0900

    feat : 컴퓨터의 숫자와 플레이어의 숫자를 비교한다
    같은 자리에 같은 숫자가 있으면 스트라이크
    다른 자리에 같은 숫자가 있으면 볼
    같은 숫자가 없으면 낫싱
    모두 맞출때까지 입력을 반복한다.

--- src/main/java/basball/application/Baseball.java
+++ src/main/java/basball/application/Baseball.java
@@ -17,8 +17,67 @@ public class Baseball {
     }
 
     private void play() {
+        boolean isCorrect = false;
+        while (!isCorrect) {
+            List<Integer> playerSet = factory.createUserNumber(validator.input());
+            //System.out.println("입력 숫자" + playerSet.toString());
+            isCorrect = matching(playerSet);
+        }
+    }
+    private boolean matching(List<Integer> numbers) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0 ; i < 3 ; i ++) {
+            strike += findStrike(numbers.get(i), i);
+            ball += findBall(numbers.get(i),i);
+        }
+        System.out.println(result(strike, ball));
+        return strike == 3;
+    }
+    private int findStrike(int value, int position) {
+        int ret = 0;
+        for (int i = 0 ; i < 3 ; i ++) {
+            ret += CountingStrike(questionSet.get(i), value, i, position);
+        }
+        return ret;
+    }
 
-        List<Integer> playerSet = factory.createUserNumber(validator.input());
-        System.out.println("입력 숫자" + playerSet.toString());
+    private int CountingStrike(int correctValue, int value, int correctPosition, int position) {
+        if (correctValue == value && correctPosition == position) return 1;
+        return 0;
+    }
+
+    private int findBall(int value, int position) {
+        int ret = 0;
+        for (int i = 0 ; i < 3 ; i ++) {
+            ret += CountingBall(questionSet.get(i), value, i, position);
+        }
+        return ret;
+    }
+
+    private int CountingBall(int correctValue, int value, int correctPosition, int position) {
+        if (correctValue == value && correctPosition != position) return 1;
+        return 0;
+    }
+
+    private String result(int strike, int ball) {
+        if (strike == 3) {
+            return "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+        }
+
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+
+        if (ball != 0 && strike != 0) {
+            return strike + " 스트라이크 " + ball + "볼";
+        }
+
+        if (strike != 0) {
+            return strike + " 스트라이크";
+        }
+
+        return ball + "볼";
+
     }
 }
