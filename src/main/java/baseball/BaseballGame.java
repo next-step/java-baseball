61fcf3e79359476f7f3cc580559027014409daa7
chpark0904 <chpark0904@gmail.com>
2021-04-25 14:33:50 +0900

    feat: 재시작 및 종료 선택(UI)
    게임의 상태(대기/재시작/종료)를 설정하기위해 GameStatus 내부 클래스를 구현했다.
    매 게임이 종료되면 Scanner를 통해 게임의 재시작/종료를 선택받을 수 있게 하였다.
    1을 선택하면 재시작되고 그 외에는 종료처리하였다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -6,28 +6,54 @@ import baseball.player.User;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static baseball.BaseballGame.GameStatus.READY;
+import static baseball.BaseballGame.GameStatus.RESTART;
+import static baseball.BaseballGame.GameStatus.SHUTDOWN;
+
 /**
  * Created by Choen-hee Park
  * User : chpark
  * Date : 2021/04/25
  * Time : 1:03 AM
  */
+
 public class BaseballGame {
 	private static final int BALL_COUNT = 3;
 
 	public static void main(String[] args) {
 		BaseballGame baseballGame = new BaseballGame();
+
+		GameStatus gameStatus = READY;
+		while (gameStatus != SHUTDOWN) {
+			baseballGame.start();
+			gameStatus = selectRestartOrExit();
+		}
+	}
+
+	private void start() {
 		Computer computer = new Computer();
 		int[] computerBalls = computer.selectComputerBalls(BALL_COUNT);
 		System.out.println("Computer: " + Arrays.toString(computerBalls));
 		User user = new User();
-		int[] userBalls = user.selectUserBalls(BALL_COUNT, baseballGame.inputUserBalls());
+		int[] userBalls = user.selectUserBalls(BALL_COUNT, inputUserBalls());
 		System.out.println("User: " + Arrays.toString(userBalls));
+	}
+
+	private static GameStatus selectRestartOrExit() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextInt() == 1 ? RESTART : SHUTDOWN;
 	}
 
 	private String inputUserBalls() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("숫자를 입력해주세요: ");
 		return scanner.next();
+	}
+
+	enum GameStatus {
+		READY,
+		RESTART,
+		SHUTDOWN
 	}
 }
