f4e88317b8e87c596d856632520a4587c4d08a45
cooingpop <cooingpop@gmail.com>
2021-04-25 17:06:31 +0900

    feat:임의의 수 생성

---
+++ src/test/java/baseball/game/utils/NumberUtilsTest.java
@@ -0,0 +1,15 @@
+package baseball.game.utils;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class NumberUtilsTest {
+
+	// @Test
+	// @ParameterizedTest
+	// @CsvSource(value = {"test:test","tEst:test","Java:java"}, delimiter = ':')
+	void getGeneratedRandomNumbers() {
+	}
+}
