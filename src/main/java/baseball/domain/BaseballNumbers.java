ef300b64674f7c985320fe17399447db36ebbc2d
britnin <baekseongsa@gmail.com>
2021-04-25 19:33:03 +0900

    feat: 야구 숫자 리스트 검증 및 객체 생성 구현

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -1,10 +1,12 @@
 package baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaseballNumbers {
 
-	private final static int MAX_BASEBALL_NUMBER_COUNT = 3;
+	private static final int MAX_BASEBALL_NUMBER_COUNT = 3;
 
 	private final List<BaseballNumber> baseballNumbers;
 
@@ -12,8 +14,25 @@ public class BaseballNumbers {
 		return new BaseballNumbers(BaseballNumbersFactory.create(MAX_BASEBALL_NUMBER_COUNT));
 	}
 
+	public static BaseballNumbers newBaseballNumbers(List<BaseballNumber> baseballNumbers) {
+		return new BaseballNumbers(baseballNumbers);
+	}
+
 	private BaseballNumbers(List<BaseballNumber> baseballNumbers) {
+		validate(baseballNumbers);
+
 		this.baseballNumbers = baseballNumbers;
+	}
+
+	private void validate(List<BaseballNumber> baseballNumbers) {
+		if (baseballNumbers.size() != MAX_BASEBALL_NUMBER_COUNT) {
+			throw new IllegalArgumentException(MAX_BASEBALL_NUMBER_COUNT + "자리 숫자여야 합니다.");
+		}
+
+		Set<BaseballNumber> set = new HashSet<>(baseballNumbers);
+		if (set.size() < MAX_BASEBALL_NUMBER_COUNT) {
+			throw new IllegalArgumentException("중복된 숫자가 존재합니다.");
+		}
 	}
 
 }
