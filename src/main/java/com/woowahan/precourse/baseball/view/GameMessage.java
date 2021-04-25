d834bf1e11cb6a2efd48c8c591239e8ac84b1bb3
junseok Oh <ixtears23@icloud.com>
2021-04-25 14:42:44 +0900

    feat(view) : output 문구(힌트)
    사용자 입력이 끝난 뒤 게임 종료가 안된 경우
    사용자에게 현재 상황에 대한 힌트를 보여주기 위한 텍스트

--- src/main/java/com/woowahan/precourse/baseball/view/GameMessage.java
+++ src/main/java/com/woowahan/precourse/baseball/view/GameMessage.java
@@ -4,6 +4,10 @@ public class GameMessage {
 
     private GameMessage() {}
 
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+    public static final String FOUR_BALL = "포볼";
     public static final String ENTER_NUMBER = "숫자를 입력해주세요 : ";
     public static final String VICTORY = "3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
 }
