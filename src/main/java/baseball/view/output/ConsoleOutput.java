18d9e54c2a3d97295a378d224c696fe8a2b764ee
Jonny-Cho <andante2183@gmail.com>
2021-04-25 18:24:20 +0900

    feat: missing requirements done

--- src/main/java/baseball/view/output/ConsoleOutput.java
+++ src/main/java/baseball/view/output/ConsoleOutput.java
@@ -3,19 +3,23 @@ package baseball.view.output;
 import baseball.domain.BaseballEnum;
 import baseball.domain.BaseballResult;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static baseball.view.input.InputMessages.PLEASE_INPUT_VALID_NUMBERS;
 import static baseball.view.output.OutputMessages.*;
 
 public class ConsoleOutput implements Output {
+    private final String STRING_DELIMITER = " ";
+
     @Override
     public void print(String message) {
         System.out.print(message);
     }
 
     @Override
-    public void printValiedateNumber(int numberSize) {
+    public void printValiedateNumber() {
         System.out.println(PLEASE_INPUT_VALID_NUMBERS);
     }
 
@@ -35,13 +39,19 @@ public class ConsoleOutput implements Output {
     }
 
     private String formatResult(Map<BaseballEnum, Integer> result) {
-        return result.get(BaseballEnum.STRIKE) +
-                " " +
-                BaseballEnum.STRIKE.getMessage() +
-                " " +
-                result.get(BaseballEnum.BALL) +
-                " " +
-                BaseballEnum.BALL.getMessage();
+        List<String> strings = new ArrayList<>();
+        for (BaseballEnum baseballEnum : result.keySet()) {
+            addToList(strings, result, baseballEnum);
+        }
+        return String.join(STRING_DELIMITER, strings);
+    }
+
+    private void addToList(List<String> strings, Map<BaseballEnum, Integer> result, BaseballEnum baseballEnum) {
+        final int count = result.get(baseballEnum);
+        if (count > 0) {
+            strings.add(String.valueOf(count));
+            strings.add(baseballEnum.getMessage());
+        }
     }
 
 }
