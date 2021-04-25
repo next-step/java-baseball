78bdb8081f580a3713d3bcba0ae67cf4ef16bbdd
jwhlll <jwhlll@hanmail.net>
2021-04-25 09:25:27 +0900

    test: 서로 다른 3자리 수 랜덤 값 생성 기능 테스트 set -> arrayList 수정

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -3,18 +3,17 @@ package baseball.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Set;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ComputerTest {
-    private Set<Integer> numbers;
+    private ArrayList<Integer> numbers;
 
     @Test
     @DisplayName("Set Size 3 Check")
     void size_check() {
         numbers = Computer.createNumbers();
-
         assertThat(numbers.size()).isEqualTo(3);
     }
 }
