9f1b5b946e3163abd6f47099b49cee367e8d9be8
daeyong-sim <48154dd3ccc2eff16343e00714bdf55ae2caa334>
2021-04-25 09:22:52 +0900

    feat(BaseballGame): create input function

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,21 +1,33 @@
+import java.util.Scanner;
+
 public class BaseballGame {
+
 	public static void main(String[] args){
+		do{
+			// TODO: play Baseball game
+			BaseballGame baseballGame = new BaseballGame();
+			baseballGame.playBaseballGame();
 
-		// TODO: play Baseball game
-		BaseballGame baseballGame = new BaseballGame();
-		baseballGame.playBaseballGame();
-
-		// TODO: select to replay game
-
+			// TODO: select to replay game
+			break;
+		}while(true);
 	}
 
 
 	public void playBaseballGame(){
 		// TODO: init numbers
 
-		// TODO: get input numbers
+		// get input numbers
+		String input = getInput();
 		// TODO: show score
 		// TODO: check Game status
+	}
+
+	public String getInput(){
+		String input = "";
+		Scanner scanner = new Scanner(System.in);
+		input = scanner.nextLine();
+		return input;
 	}
 
 }
