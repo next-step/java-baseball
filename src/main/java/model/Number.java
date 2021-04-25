2baf0f8a4876ba05e3aa22e491a26ff1b1a3f1dd
Jun <p_illusion@naver.com>
2021-04-26 00:55:47 +0900

    [feat] Number 구현 1.생성자 구현 2. comparable로 Number를 정렬 가능한 클래스로 구현 3. 정렬 가능한 클래스로 구현하기 위해 compareTo, equals, hashCode 구현

---
+++ src/main/java/model/Number.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.Objects;
+
+public class Number implements Comparable<Number> {
+
+    private final String number;
+
+    public Number(final String number) { this.number = number; }
+
+    @Override
+    public int compareTo(Number o) {
+        return number.compareTo(o.number);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof Number)) {
+            return false;
+        }
+
+        final Number that = (Number) o;
+        if(number.toString().compareTo(that.number.toString()) == 0) return true;
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
