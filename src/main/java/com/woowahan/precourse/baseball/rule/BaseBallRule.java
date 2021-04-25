e26fffc34969f27bfc2ee2e9c4ebfd42472f8c29
junseok Oh <ixtears23@icloud.com>
2021-04-25 14:15:04 +0900

    feat(rule) : 스트라이크 규칙
    게임숫자와 사용자가 입력한 숫자의 같은 자리 하나를 비교하여
    동일하면 스트라이크

---
+++ src/main/java/com/woowahan/precourse/baseball/rule/BaseBallRule.java
@@ -0,0 +1,8 @@
+package com.woowahan.precourse.baseball.rule;
+
+public class BaseBallRule {
+
+    public static boolean isStrike(String gameNumber, String inputNumber) {
+        return gameNumber.equals(inputNumber);
+    }
+}
