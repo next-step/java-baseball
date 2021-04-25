91c5db09102b186b7de45441df7d00b72bfbf367
yjs2952 <yjs2952@naver.com>
2021-04-25 21:20:18 +0900

    refactor: 게임 클리어 시 입력 코드 메소드 추출

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -19,10 +19,13 @@ public class Controller {
             Results results = game.start(Numbers.of(inputView.getNumbers()));
 
             printView.printResult(results.createResultMessage());
+            continueOrInputNextAction(game, inputView);
+        }
+    }
 
-            if (game.isClear()) {
-                inputView.inputContinueNumber();
-            }
+    private static void continueOrInputNextAction(BaseballGame game, InputView inputView) {
+        if (game.isClear()) {
+            inputView.inputContinueNumber();
         }
     }
 }
