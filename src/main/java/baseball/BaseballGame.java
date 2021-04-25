ba06ae8825fe10e560297f08b33deb222360c9ef
chpark0904 <chpark0904@gmail.com>
2021-04-25 14:28:25 +0900

    feat: 유저의 볼 선택 (UI)
    Scanner를 통해 유저의 볼을 String으로 받아온다.
    User객체는 String을 int[] 형태로 변환한다.
    입력이 어떤 값이든간에 ballCount의 길이로 반환한다

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,8 +1,10 @@
 package baseball;
 
 import baseball.player.Computer;
+import baseball.player.User;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Created by Choen-hee Park
@@ -10,13 +12,22 @@ import java.util.Arrays;
  * Date : 2021/04/25
  * Time : 1:03 AM
  */
-
 public class BaseballGame {
 	private static final int BALL_COUNT = 3;
 
 	public static void main(String[] args) {
+		BaseballGame baseballGame = new BaseballGame();
 		Computer computer = new Computer();
 		int[] computerBalls = computer.selectComputerBalls(BALL_COUNT);
 		System.out.println("Computer: " + Arrays.toString(computerBalls));
+		User user = new User();
+		int[] userBalls = user.selectUserBalls(BALL_COUNT, baseballGame.inputUserBalls());
+		System.out.println("User: " + Arrays.toString(userBalls));
+	}
+
+	private String inputUserBalls() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("숫자를 입력해주세요: ");
+		return scanner.next();
 	}
 }
