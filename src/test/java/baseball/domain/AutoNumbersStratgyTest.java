04803de6efd3c510948385342c9a795f8c144a65
bbbnam <bbbnam@naver.com>
2021-04-26 01:37:45 +0900

    test : 랜덤 숫자 생성시 유효성 검사 테스트

--- src/test/java/baseball/domain/AutoNumbersStratgyTest.java
+++ src/test/java/baseball/domain/AutoNumbersStratgyTest.java
@@ -1,7 +1,10 @@
 package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +25,26 @@ class AutoNumbersStratgyTest {
         BallNumbers actualGenerated = auto.generate();
 
         assertThat(expected).isEqualTo(actualGenerated);
+    }
+
+    @DisplayName("랜덤 숫자 생성시 3자리 숫자로 생성되는지 테스트")
+    @Test
+    void checkNumbersSize() {
+        AutoNumbersStratgy auto = new AutoNumbersStratgy();
+        BallNumbers generated = auto.generate();
+
+        assertThat(generated.getNumbers()).hasSize(3);
+    }
+
+    @DisplayName("랜덤 숫자 생성시 1~9 사이 값으로 생성되는지 테스트")
+    @RepeatedTest(100)
+    void checkNumbersRange() {
+        AutoNumbersStratgy auto = new AutoNumbersStratgy();
+        BallNumbers generated = auto.generate();
+        List<BallNumber> numbers = generated.getNumbers();
+
+        for (BallNumber number : numbers) {
+            assertThat(number.getNumber()).isBetween(BallNumber.MIN_NUMBER, BallNumber.MAX_NUMBER);
+        }
     }
 }
