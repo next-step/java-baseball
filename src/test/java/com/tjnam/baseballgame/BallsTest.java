65351005006684f843bf443a676f13ca8c875998
TJ NAM <dynamicnam@gmail.com>
2021-04-26 07:20:26 +0900

    test(Balls 클래스) ball의 비교로직에 대한 테스트 추가
    Ball간 비교에 대한 테스트 추가
     - 비교를 위해 필요한 setBall에 대한 테스트 추가
     - 비교에 사용하는 숫자 exsiting 배열 설정에 대한 테스트 추가

--- src/test/java/com/tjnam/baseballgame/BallsTest.java
+++ src/test/java/com/tjnam/baseballgame/BallsTest.java
@@ -2,6 +2,7 @@ package com.tjnam.baseballgame;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -57,6 +58,87 @@ public class BallsTest {
         setBalls = ball.getNumbers();
         assertThat(setBalls).isEqualTo(ballValues);
 
+    }
+
+    @Test
+    public void setBallByStringTest(){
+        Balls ball = new Balls(ballString);
+
+        int[] setBalls = ball.getNumbers();
+        assertThat(setBalls).isEqualTo(ballValues);
+
+        ball = new Balls();
+        ball.setBallValues(ballString);
+        setBalls = ball.getNumbers();
+        assertThat(setBalls).isEqualTo(ballValues);
+    }
+
+
+    @Test
+    public void compareBallsTest(){
+        Balls dealerBall = new Balls("123");
+        Balls userBall = new Balls();
+        userBall.setBallValues("132");
+
+        GameResult comparedResult = dealerBall.compareBalls(userBall);
+
+        assertThat(comparedResult.strike).isEqualTo(1);
+        assertThat(comparedResult.ball).isEqualTo(2);
+
+        //0s 0b
+        userBall.setBallValues("444");
+        comparedResult = dealerBall.compareBalls(userBall);
+        assertThat(comparedResult.strike).isEqualTo(0);
+        assertThat(comparedResult.ball).isEqualTo(0);
+
+        userBall.setBallValues("312");
+        comparedResult = dealerBall.compareBalls(userBall);
+        assertThat(comparedResult.strike).isEqualTo(0);
+        assertThat(comparedResult.ball).isEqualTo(3);
+
+        userBall.setBallValues("144");
+        comparedResult = dealerBall.compareBalls(userBall);
+        assertThat(comparedResult.strike).isEqualTo(1);
+        assertThat(comparedResult.ball).isEqualTo(0);
+
+        userBall.setBallValues("309");
+        comparedResult = dealerBall.compareBalls(userBall);
+        assertThat(comparedResult.strike).isEqualTo(0);
+        assertThat(comparedResult.ball).isEqualTo(1);
+
+        dealerBall.setBallValues("381");
+        userBall.setBallValues("189");
+        comparedResult = dealerBall.compareBalls(userBall);
+        assertThat(comparedResult.strike).isEqualTo(0);
+        assertThat(comparedResult.ball).isEqualTo(1);
+    }
+
+
+    @Test
+    public void markBallValuesTest(){
+        Balls ball = new Balls("123");
+        Boolean[] valueMarking;
+
+        try {
+            Field ballExistMarkVariable = ball.getClass().getDeclaredField("ballExists");
+            ballExistMarkVariable.setAccessible(true);
+
+            valueMarking = (Boolean[])ballExistMarkVariable.get(ball);
+            assertThat(valueMarking[0]).isFalse();
+            assertThat(valueMarking[4]).isFalse();
+            assertThat(valueMarking[5]).isFalse();
+            assertThat(valueMarking[6]).isFalse();
+            assertThat(valueMarking[7]).isFalse();
+            assertThat(valueMarking[8]).isFalse();
+            assertThat(valueMarking[9]).isFalse();
+
+            assertThat(valueMarking[1]).isTrue();
+            assertThat(valueMarking[2]).isTrue();
+            assertThat(valueMarking[3]).isTrue();
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 }
