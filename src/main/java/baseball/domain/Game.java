ce9ed4ed550df9f8888edf99b16a650ceec374f4
ohgillwhan <soora_genius@naver.com>
2021-04-25 14:03:04 +0900

    feat : 게임에서 공을 받아 게임결과를 볼 수 있다

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -15,11 +15,13 @@ public class Game {
         this.computerBalls = generationStrategy.generate(BALL_SIZE);
     }
 
-    public void match(Balls balls) {
+    public GameRules match(Balls balls) {
         if(balls.size() != BALL_SIZE) {
             String errorMessage = String.format("매칭할 공의 개수는 %d개 여야 합니다.", BALL_SIZE);
 
             throw new IllegalArgumentException(errorMessage);
         }
+
+        return computerBalls.match(balls);
     }
 }
--- src/test/java/baseball/domain/GameTest.java
+++ src/test/java/baseball/domain/GameTest.java
@@ -2,10 +2,14 @@ package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -42,4 +46,88 @@ public class GameTest {
 
         assertThatIllegalArgumentException().isThrownBy(() -> game.match(balls));
     }
+
+    @ParameterizedTest
+    @MethodSource("createMatchingPackageSource")
+    public void 공을_받아_게임결과를_볼_수_있다(MatchingPackage matchingPackage) {
+        Game game = new Game((size) -> new Balls(Ball.of(7), Ball.of(1), Ball.of(3)));
+
+        GameRules match = game.match(matchingPackage.getDest());
+
+        assertThat(match.countOfStrike()).isEqualTo(matchingPackage.getCountOfStrike());
+        assertThat(match.countOfBall()).isEqualTo(matchingPackage.getCountOfBall());
+        assertThat(match.countOfMissing()).isEqualTo(matchingPackage.getCountOfMissing());
+    }
+
+    private static List<MatchingPackage> createMatchingPackageSource() {
+        List<MatchingPackage> matchingPackages = new ArrayList<>();
+
+        matchingPackages.add(new MatchingPackage(
+                Arrays.asList(1, 2, 3),
+                1,
+                1,
+                1
+        ));
+
+        matchingPackages.add(new MatchingPackage(
+                Arrays.asList(1, 4, 5),
+                0,
+                1,
+                2
+        ));
+
+        matchingPackages.add(new MatchingPackage(
+                Arrays.asList(6, 7, 1),
+                0,
+                2,
+                1
+        ));
+
+        matchingPackages.add(new MatchingPackage(
+                Arrays.asList(2, 1, 6),
+                1,
+                0,
+                2
+        ));
+
+        matchingPackages.add(new MatchingPackage(
+                Arrays.asList(7, 1, 3),
+                3,
+                0,
+                0
+        ));
+
+        return matchingPackages;
+    }
+
+    private static class MatchingPackage {
+        private Balls dest;
+        private int countOfStrike;
+        private int countOfBall;
+        private int countOfMissing;
+
+        public MatchingPackage(List<Integer> dest, int countOfStrike, int countOfBall, int countOfMissing) {
+            this.dest = new Balls(dest.stream().map(Ball::of).collect(Collectors.toList()));
+            this.countOfStrike = countOfStrike;
+            this.countOfBall = countOfBall;
+            this.countOfMissing = countOfMissing;
+        }
+
+        public Balls getDest() {
+            return dest;
+        }
+
+        public int getCountOfStrike() {
+            return countOfStrike;
+        }
+
+        public int getCountOfBall() {
+            return countOfBall;
+        }
+
+        public int getCountOfMissing() {
+            return countOfMissing;
+        }
+    }
+    
 }
