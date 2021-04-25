82ac919af6864fe52946e0c711f3c6235aa61034
cooingpop <cooingpop@gmail.com>
2021-04-25 18:53:23 +0900

    feat: 게임 메인 실행 추가

--- src/main/java/baseball/game/play/Application.java
+++ src/main/java/baseball/game/play/Application.java
@@ -6,12 +6,21 @@
 
 package baseball.game.play;
 
+import baseball.game.constants.GameStatusType;
+
 /**
  * @author 박준영
  **/
 public class Application {
 	public static void main(String[] args) {
+		System.out.println("야구 게임시작");
+		Baseball baseball = new Baseball();
+		baseball.initialize();
 
+		while (GameStatusType.START.equals(baseball.type)) {
+			baseball.inputNumbers();
+			System.out.println(baseball.getResultMessage());
+		}
 
 	}
 }
