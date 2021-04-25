052a8c12f9d9a8644ec2c908817e55fa8f9a66e3
momentjin <momentjin@gmail.com>
2021-04-26 00:11:49 +0900

    feature: 연습용 Junit Test 추가 - 문자열 분리

---
+++ src/test/java/practice/StringTest.java
@@ -0,0 +1,35 @@
+package practice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+
+    @Test
+    public void splitTest() {
+
+        // given
+        String input = "1,2";
+
+        // when
+        String[] splited = input.split(",");
+
+        // then
+        assertThat(splited).containsExactly("1", "2");
+    }
+
+
+    @Test
+    public void splitTest2() {
+
+        // given
+        String input = "1";
+
+        // when
+        String[] splited = input.split(",");
+
+        // then
+        assertThat(splited).containsExactly("1");
+    }
+}
