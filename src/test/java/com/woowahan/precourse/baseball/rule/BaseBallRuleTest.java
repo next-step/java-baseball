0518ac24b195169edf6c699077a8aee4706c89ff
junseok Oh <ixtears23@icloud.com>
2021-04-25 14:34:23 +0900

    test(rule) : 스트라이크 규칙 테스트 작성
    비교 값이 같을때 스트라이크 규칙 테스트
    비교 값이 다를때 스트라이크 규칙 테스트
    비교 값이 같아야 스트라이크

---
+++ src/test/java/com/woowahan/precourse/baseball/rule/BaseBallRuleTest.java
@@ -0,0 +1,30 @@
+package com.woowahan.precourse.baseball.rule;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BaseBallRuleTest {
+
+    private String gameNumber1;
+    private String inputNumber1;
+    private String gameNumber2;
+    private String inputNumber2;
+
+    @BeforeEach
+    void init() {
+        this.gameNumber1 = "3";
+        this.inputNumber1 = "3";
+        this.gameNumber2 = "3";
+        this.inputNumber2 = "4";
+    }
+
+    @Test
+    @DisplayName("gameNumber와 inputNumber가 같으면 isStrike는 true 다르면 isStrike는 false")
+    void isStrikeTest() {
+        assertThat(BaseBallRule.isStrike(gameNumber1, inputNumber1)).isTrue();
+        assertThat(BaseBallRule.isStrike(gameNumber2, inputNumber2)).isFalse();
+    }
+}
