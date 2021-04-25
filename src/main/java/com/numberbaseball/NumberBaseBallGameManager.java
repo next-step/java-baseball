dfaae42aafc6bea9206b14b607d27a776f99c884
songseongbin <ppsosqq@gmail.com>
2021-04-25 17:16:23 +0900

    refactor(NumberBaseBallGameManager): 정답일 때 게임 재시작 또는 종료 구현
    start 함수 길이 및 depth 조절을 위해 함수화

--- src/main/java/com/numberbaseball/NumberBaseBallGameManager.java
+++ src/main/java/com/numberbaseball/NumberBaseBallGameManager.java
@@ -13,20 +13,36 @@ public class NumberBaseBallGameManager {
 
 	public void start() {
 		try (Scanner sc = new Scanner(System.in)) {
-			do {
-				int answer = numberCreator.createEachNumberIsUniqueThreeDigitNumber();
-				NumberBaseBallResult result;
-				do {
-					System.out.print("숫자를 입력해주세요 : ");
-					result = numberBaseBallGame.toResult(answer, sc.nextInt());
-					System.out.println(result.toStr());
-				} while (!result.isThreeStrike());
-
-				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
-					+ "게임을 새로 시작하려면1, 종료하려면 2를 입력하세요.");
-
-			} while (isContinue(sc.nextInt()));
+			startGames(sc);
 		}
+	}
+
+	private void startGames(Scanner sc) {
+		do {
+			startGame(sc);
+
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면1, 종료하려면 2를 입력하세요.");
+		} while (isContinue(sc.nextInt()));
+	}
+
+	private void startGame(Scanner sc) {
+		int answer = numberCreator.createEachNumberIsUniqueThreeDigitNumber();
+
+		NumberBaseBallResult result;
+		do {
+			System.out.print("숫자를 입력해주세요 : ");
+
+			result = compareInputWithAnswer(sc.nextInt(), answer);
+		} while (!result.isThreeStrike());
+	}
+
+	private NumberBaseBallResult compareInputWithAnswer(int input, int answer) {
+		NumberBaseBallResult result;
+
+		result = numberBaseBallGame.toResult(answer, input);
+
+		System.out.println(result.toStr());
+		return result;
 	}
 
 	private boolean isContinue(int input) {
