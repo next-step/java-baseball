f561bc6b23d234be5cda017f326d010171243c5d
cptbong <cptbbong@gmail.com>
2021-04-25 19:26:31 +0900

    test - String테스트완료.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,9 +1,11 @@
 package study;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -23,5 +25,22 @@ public class StringTest {
     void require2() {
         String requiredString = "(1,2)";
         assertThat(requiredString.substring(1, requiredString.length() - 1)).isEqualTo("1,2");
+    }
+
+    @Nested
+    class Require3 {
+        @Test
+        @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는가?")
+        void require1() {
+            assertThat("abc".charAt(0)).isEqualTo('a');
+        }
+
+        @Test
+        @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는가?")
+        void require2() {
+            assertThatThrownBy(() -> {
+                "abc".charAt(10);
+            }).isInstanceOf(StringIndexOutOfBoundsException.class);
+        }
     }
 }
