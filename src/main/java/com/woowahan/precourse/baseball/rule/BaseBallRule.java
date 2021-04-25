803856e9989a0323861e85678310443c6255bfe7
junseok Oh <ixtears23@icloud.com>
2021-04-25 21:37:19 +0900

    feat(rule) : 승리 규칙
    3스트라이크가 나온경우 게임을 승리
    3스트라이크의 경우 true 반환

--- src/main/java/com/woowahan/precourse/baseball/rule/BaseBallRule.java
+++ src/main/java/com/woowahan/precourse/baseball/rule/BaseBallRule.java
@@ -2,11 +2,17 @@ package com.woowahan.precourse.baseball.rule;
 
 public class BaseBallRule {
 
+    private BaseBallRule() {}
+
     public static boolean isStrike(String gameNumber, String inputNumber) {
         return gameNumber.equals(inputNumber);
     }
 
     public static boolean isBall(String gameNumber, String inputNumber) {
         return !gameNumber.equals(inputNumber);
+    }
+
+    public static boolean isVictory(int strikeCount) {
+        return strikeCount == 3;
     }
 }
