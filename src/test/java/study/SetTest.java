f260985280cc2df9ed66dabb8e3faef11898db5d
최홍희 <vvshinevv@nhntoast.com>
2021-04-25 18:14:43 +0900

    test : Set Collection에 대한 학습 테스트 - 요구사항3
    - 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과
    값이 다른 경우에 대한 테스트도 가능하도록 구현
    - 예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스
    트를 하나의 Test Case로 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -45,5 +46,17 @@ public class SetTest {
 
         // then
         Assertions.assertThat(result).isTrue();
+    }
+
+    @DisplayName("JUnit의 CsvSource 어노테이션 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiterString = ":")
+    public void csvSourceTest(String given, String expected) {
+
+        // when
+        boolean result = numbers.contains(Integer.parseInt(given));
+
+        // then
+        Assertions.assertThat(result).isEqualTo(Boolean.parseBoolean(expected));
     }
 }
