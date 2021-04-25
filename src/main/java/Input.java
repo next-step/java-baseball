e9105147b349827962a54e342472ba3b2e50ab0c
ChangSubKwak <paradise1732@gmail.com>
2021-04-26 01:26:35 +0900

    feat: 입력 기능 추가 - 플레이어 숫자, 재시작 여부

---
+++ src/main/java/Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Input {
+	static final Scanner sc = new Scanner(System.in);
+
+	public int inputByHuman() {
+		return sc.nextInt();
+	}
+
+	public int inputWantContinuity() {
+		return sc.nextInt();
+	}
+}
