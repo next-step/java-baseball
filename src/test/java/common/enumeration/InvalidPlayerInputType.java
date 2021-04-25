0c66660b25845b828483947c6e054a5c28071397
Dongbin <bnitech88@gmail.com>
2021-04-26 04:32:41 +0900

    test(Initializer): add test
    플레이어가 제시한 숫자 세팅 테스트
    - 비정상적인 숫자 입력 테스트
    - 정상 처리 테스트

---
+++ src/test/java/common/enumeration/InvalidPlayerInputType.java
@@ -0,0 +1,5 @@
+package common.enumeration;
+
+public enum InvalidPlayerInputType {
+	ZERO, NEGATIVE, LESS_THAN_THREE_DIGITS, MORE_THAN_THREE_DIGITS, INCLUDE_ZERO
+}
--- src/test/java/service/impl/InitializerTest.java
+++ src/test/java/service/impl/InitializerTest.java
@@ -6,7 +6,11 @@ import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
+import common.argumentprovider.InvalidPlayerInputArgumentsProvider;
+import exception.InvalidValueException;
 import service.IInitializer;
 
 class InitializerTest {
@@ -14,20 +18,63 @@ class InitializerTest {
 	private final IInitializer initializer = new Initializer();
 
 	@Test
-	@DisplayName("컴퓨터 초기 숫자 테스트")
-	void initComputerNumbers() {
+	@DisplayName("컴퓨터 초기 숫자 세팅 테스트")
+	void testInitComputerNumber() {
 
 		/// region given
 		final int expectedSize = 3;
 		/// endregion
 
 		/// region when
-		final Map<Integer, Integer> actualComputerNumbers = initializer.initComputerNumbers();
+		final Map<Integer, Integer> actualComputerNumbers = initializer.initComputerNumber();
 		/// endregion
 
 		/// region then
 		assertNotNull(actualComputerNumbers);
 		assertEquals(expectedSize, actualComputerNumbers.size());
+		/// endregion
+	}
+
+	@DisplayName("플레이어가 제시한 유효하지 않은 숫자 테스트")
+	@ArgumentsSource(InvalidPlayerInputArgumentsProvider.class)
+	@ParameterizedTest(name = "입력 받은 숫자: {0}")
+	void testInitInvalidPlayerNumber(final String title, final int givenInput) {
+
+		/// region given
+		final Throwable expectedException = new InvalidValueException();
+		/// endregion
+
+		/// region when
+		final Throwable actualException = assertThrows(
+			expectedException.getClass(),
+			() -> initializer.initPlayerNumber(givenInput)
+		);
+		/// endregion
+
+		/// region then
+		assertEquals(expectedException.getClass(), actualException.getClass());
+		/// endregion
+	}
+
+	@DisplayName("플레이어가 제시한 숫자 세팅 테스트")
+	@Test
+	void testInitPlayerNumber() {
+
+		/// region given
+		final int givenInput = 123;
+		final int[] expectedPlayerNumber = {1, 2, 3};
+		/// endregion
+
+		/// region when
+		final int[] actualPlayerNumber = initializer.initPlayerNumber(givenInput);
+		/// endregion
+
+		/// region then
+		assertNotNull(actualPlayerNumber);
+		assertEquals(expectedPlayerNumber.length, actualPlayerNumber.length);
+		for (int i = 0; i < 3; i++) {
+			assertEquals(expectedPlayerNumber[i], actualPlayerNumber[i]);
+		}
 		/// endregion
 	}
 }
