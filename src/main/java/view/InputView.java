13f2f2a1a447c7072a7f775c12b1bbf7209218bd
HyungjuJang <dacapolife87@gmail.com>
2021-04-25 22:37:20 +0900

    refactor : 새로운 게임여부 예외처리추가 - 숫자 이외의 다른값 입력시 예외

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
 
     private static final String MESSAGE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
     private static final String MESSAGE_RENEW_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
+    private static final String EXCEPTION_MESSAGE_WRONG_CONTINUE_ANSWER = "입력값은 1 또는 2 정수여야합니다. (게임이 종료됩니다.)";
     private Scanner scanner;
 
     public InputView() {
@@ -20,6 +20,13 @@ public class InputView {
 
     public int isContinue() {
         System.out.println(MESSAGE_RENEW_OR_END);
-        return Integer.parseInt(scanner.nextLine());
+        int answer;
+        try {
+            answer = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_WRONG_CONTINUE_ANSWER);
+        }
+
+        return answer;
     }
 }
