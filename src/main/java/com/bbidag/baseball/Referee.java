4d02f0831dfe9d7772c1d06daccecb268322789c
bbidag <skawlgh112@gmail.com>
2021-04-25 18:01:40 +0900

    feat: 사용자가 입력한 숫자와 컴퓨터가 가진 숫자를 비교해서 스트라이크, 볼 카운트를 하는 기능

--- src/main/java/com/bbidag/baseball/Referee.java
+++ src/main/java/com/bbidag/baseball/Referee.java
@@ -9,6 +9,37 @@ public class Referee {
         this.computer = computer;
     }
 
+    public void generateRandomNumberOfComputer(){
+        this.computer.setNumbers(this.computer.generateRandomNumbers());
+    }
+
+    public void calculateStrikeBall(int[] inputNumbers){
+        for(int computerNumIdx=0; computerNumIdx<3; computerNumIdx++){
+            setStrikeBallEachNumber(computerNumIdx, inputNumbers);
+        }
+    }
+
+    public void setStrikeBallEachNumber(int computerNumIdx, int[] inputNumbers){
+        int computerNumber = computer.getNumbers()[computerNumIdx];
+        for(int inputNumberIdx = 0; inputNumberIdx < 3; inputNumberIdx++){
+            int inputNumber = inputNumbers[inputNumberIdx];
+            setStrikeIfEqualBothIndexAndNumber(inputNumberIdx, inputNumber, computerNumIdx, computerNumber);
+            setBallIfEqualNumberAndNotEqualIndex(inputNumberIdx, inputNumber, computerNumIdx, computerNumber);
+        }
+    }
+
+    public void setStrikeIfEqualBothIndexAndNumber(int inputIdx, int inputNumber, int computerNumberIndex, int computerNumber){
+        if(inputIdx == computerNumberIndex && inputNumber == computerNumber){
+            setStrikeCount(getStrikeCount() + 1);
+        }
+    }
+
+    public void setBallIfEqualNumberAndNotEqualIndex(int inputIdx, int inputNumber, int computerNumberIndex, int computerNumber){
+        if(inputIdx != computerNumberIndex && inputNumber == computerNumber){
+            setBallCount(getBallCount() + 1);
+        }
+    }
+
     public boolean applyWinOrNot() {
         return strikeCount == 3;
     }
--- src/test/java/com/bbidag/baseball/RefereeTest.java
+++ src/test/java/com/bbidag/baseball/RefereeTest.java
@@ -3,8 +3,12 @@ package com.bbidag.baseball;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Random;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,13 +23,80 @@ public class RefereeTest {
         referee = new Referee(computer);
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("provideStrikeForWinOrNot")
     @DisplayName("3 스트라이크 유무에 따라 승리 여부를 판단 테스트")
-    public void isWinOrNot(){
+    public void isWinOrNot(int strikeCount, boolean isWin){
+        referee.setStrikeCount(strikeCount);
+        assertThat(referee.applyWinOrNot()).isEqualTo(isWin);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIndexAndNumberForStrikeCount")
+    @DisplayName("스트라이크 수 세기 테스트")
+    public void strikeCount(int inputIdx, int inputNumber, int computerNumberIndex, int computerNumber, int expected){
         referee.setStrikeCount(0);
-        assertThat(referee.applyWinOrNot()).isFalse();
-        referee.setStrikeCount(3);
-        assertThat(referee.applyWinOrNot()).isTrue();
+        referee.setStrikeIfEqualBothIndexAndNumber(inputIdx,inputNumber,computerNumberIndex,computerNumber);
+        assertThat(referee.getStrikeCount()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIndexAndNumberForBallCount")
+    @DisplayName("볼 수 세기 테스트")
+    public void ballCount(int inputIdx, int inputNumber, int computerNumberIndex, int computerNumber, int expected){
+        referee.setBallCount(0);
+        referee.setBallIfEqualNumberAndNotEqualIndex(inputIdx,inputNumber,computerNumberIndex,computerNumber);
+        assertThat(referee.getBallCount()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("심판이 초기화 선언한 3자리 수의 랜덤 배열 컴퓨터 생성 테스트")
+    public void generateRandomNumberOfComputer(){
+        referee.generateRandomNumberOfComputer();
+        assertThat(computer.getNumbers().length).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("사용자 입력에 대해 컴퓨터의 2번째 자리수에 대한 스트라이크, 카운트 체크 테스트")
+    public void setStrikeBallByComputerIndex(){
+        computer.setNumbers(new int[]{1,2,3});
+        referee.setBallCount(0);
+        referee.setStrikeCount(0);
+        referee.setStrikeBallEachNumber(1, new int[]{3,2,9});
+        assertThat(referee.getStrikeCount()).isEqualTo(1);
+        assertThat(referee.getBallCount()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("사용자 입력에 대해 모든 스트라이크, 볼 카운트 테스트")
+    public void calculateStrikeBall(){
+        computer.setNumbers(new int[]{1,2,3});
+        referee.setBallCount(0);
+        referee.setStrikeCount(0);
+        referee.calculateStrikeBall(new int[]{3,2,1});
+        assertThat(referee.getStrikeCount()).isEqualTo(1);
+        assertThat(referee.getBallCount()).isEqualTo(2);
+    }
+
+    private static Stream<Arguments> provideStrikeForWinOrNot(){
+        return Stream.of(
+            Arguments.of(3,true), Arguments.of(2,false),
+            Arguments.of(1,false), Arguments.of(0,false)
+        );
+    }
+
+    private static Stream<Arguments> provideIndexAndNumberForStrikeCount(){
+        return Stream.of(
+                Arguments.of(0,2,1,2,0),
+                Arguments.of(1,5,1,5,1)
+        );
+    }
+
+    private static Stream<Arguments> provideIndexAndNumberForBallCount(){
+        return Stream.of(
+                Arguments.of(0,2,1,2,1),
+                Arguments.of(2,3,2,3,0)
+        );
     }
 
 }
