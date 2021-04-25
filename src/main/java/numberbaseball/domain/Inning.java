3f869e4e54b05868c2941cf052d555c1464e123b
ted.kang <ted.kang@kakaocorp.com>
2021-04-26 01:29:57 +0900

    refactor: Inning 도메인 클래스 작성
    내부 컬렉션을 List -> Map으로 변경
    
    숫자를 key, 위치를 value로 잡아서
    value가
    null이면
    nothing
    value가 다르면 ball
    value가 같으면 strike로 하기 위함

--- src/main/java/numberbaseball/domain/Inning.java
+++ src/main/java/numberbaseball/domain/Inning.java
@@ -1,40 +1,45 @@
 package numberbaseball.domain;
 
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Inning {
 
   private static final int PITCHING_COUNT_PER_INNING = 3;
+  private static final String PLEASE_INPUT_N_DIGITS_INTEGER = "%d 자리 수를 입력해주세요.";
+  private static final String PLEASE_INPUT_DIFFERENT_PITCHING_EACH_OTHER = "각 자리수가 서로 다르게 입력해주세요.";
 
-  private final List<Pitching> pitchingCollection;
+  private final Map<Pitching, Integer> pitchingAndOrder;
 
   public Inning(List<Pitching> pitchingCollection) {
-    validatePitchingCollection(pitchingCollection);
-    this.pitchingCollection = pitchingCollection;
+    throwExceptionIfNotValidPitchingCount(pitchingCollection);
+    this.pitchingAndOrder = transformToPitchingAndOrderMap(pitchingCollection);
   }
 
-  private void validatePitchingCollection(List<Pitching> pitchingCollection) {
-    throwExceptionIfNotValidPitchingCount(pitchingCollection);
+  public static Inning generateRandomInning() {
+    return new Inning(PitchingFactory.generateGivenCountPitching(PITCHING_COUNT_PER_INNING));
+  }
 
-    Set<Pitching> sameNumberFilter = new HashSet<>();
-    for(Pitching pitching : pitchingCollection) {
-      throwExceptionIfDuplicated(sameNumberFilter, pitching);
+  private Map<Pitching, Integer> transformToPitchingAndOrderMap(List<Pitching> pitchingCollection) {
+    Map<Pitching, Integer> pitchingAndOrderMap = new HashMap<>();
+    int pitchingCollectionSize = pitchingCollection.size();
+    for(int index = 0; index < pitchingCollectionSize; index++) {
+      Pitching pitching = pitchingCollection.get(index);
+      throwExceptionIfDuplicatePitching(pitchingAndOrderMap, pitching);
+      pitchingAndOrderMap.put(pitching, index);
+    }
+    return pitchingAndOrderMap;
+  }
+
+  private void throwExceptionIfDuplicatePitching(Map<Pitching, Integer> pitchingAndOrder, Pitching pitching) {
+    if(pitchingAndOrder.containsKey(pitching)) {
+      throw new IllegalArgumentException(PLEASE_INPUT_DIFFERENT_PITCHING_EACH_OTHER);
     }
   }
 
   private void throwExceptionIfNotValidPitchingCount(List<Pitching> pitchingCollection) {
     if(pitchingCollection.size() != PITCHING_COUNT_PER_INNING) {
-      throw new IllegalArgumentException("");
-    }
-  }
-
-  private void throwExceptionIfDuplicated(Set<Pitching> sameNumberFilter, Pitching number) {
-    if(!sameNumberFilter.add(number)) {
-      throw new IllegalArgumentException("");
+      throw new IllegalArgumentException(String.format(PLEASE_INPUT_N_DIGITS_INTEGER, PITCHING_COUNT_PER_INNING));
     }
   }
 
@@ -43,11 +48,11 @@ public class Inning {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Inning inning = (Inning) o;
-    return pitchingCollection.equals(inning.pitchingCollection);
+    return pitchingAndOrder.equals(inning.pitchingAndOrder);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(pitchingCollection);
+    return Objects.hash(pitchingAndOrder);
   }
 }
