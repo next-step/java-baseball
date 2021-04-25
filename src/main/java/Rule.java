aa29b064bf6088ececf3fde0f1ff5f0620a65d89
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2021-04-25 23:29:28 +0900

    Style(Rule): Divide method of judge
    Divide judge method into judgeStrike and judgeBall

--- src/main/java/Rule.java
+++ src/main/java/Rule.java
@@ -11,19 +11,33 @@ public class Rule {
         return (judge.strike == 3);
     }
 
+    private static int judgeStrike(int[] dealerList, int[] playerList) {
+        int strike = 0;
+
+        if (dealerList[0] == playerList[0]) strike++;
+        if (dealerList[1] == playerList[1]) strike++;
+        if (dealerList[2] == playerList[2]) strike++;
+
+        return strike;
+    }
+
+    private static int judgeBall(int[] dealerList, int[] playerList) {
+        int ball = 0;
+
+        if (dealerList[0] == playerList[1]) ball++;
+        if (dealerList[0] == playerList[2]) ball++;
+        if (dealerList[1] == playerList[0]) ball++;
+        if (dealerList[1] == playerList[2]) ball++;
+        if (dealerList[2] == playerList[0]) ball++;
+        if (dealerList[2] == playerList[1]) ball++;
+
+        return ball;
+    }
+
     public static Judge judge(int[] dealerList, int[] playerList) {
         Judge judge = new Judge();
-
-        if (dealerList[0] == playerList[0]) judge.strike++;
-        if (dealerList[1] == playerList[1]) judge.strike++;
-        if (dealerList[2] == playerList[2]) judge.strike++;
-
-        if (dealerList[0] == playerList[1]) judge.ball++;
-        if (dealerList[0] == playerList[2]) judge.ball++;
-        if (dealerList[1] == playerList[0]) judge.ball++;
-        if (dealerList[1] == playerList[2]) judge.ball++;
-        if (dealerList[2] == playerList[0]) judge.ball++;
-        if (dealerList[2] == playerList[1]) judge.ball++;
+        judge.strike = judgeStrike(dealerList, playerList);
+        judge.ball = judgeBall(dealerList, playerList);
 
         return judge;
     }
