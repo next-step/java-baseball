c278f6b6b8169f4b86972c667f14274d221c55b4
rladowl92 <rladowl92@naver.com>
2021-04-25 16:46:12 +0900

    feat(ui): initialize the game

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -1,9 +1,23 @@
 package service;
 
+import support.ConvertUtils;
+
 import java.util.Random;
+import java.util.Scanner;
 
 public class GameService {
     private RuleService ruleService = new RuleService();
+
+    public void initGame(int len) {
+        int[] botNumber = generateBotNumber(len);
+        System.out.println("[debug] bot number : "+ConvertUtils.convertArrayToInt(botNumber)); //TODO: Remove this line
+
+        System.out.print("숫자를 입력해주세요 : ");
+        Scanner scan = new Scanner(System.in);
+        int[] userNumber = ConvertUtils.convertIntToArray(Integer.parseInt(scan.next()));
+        System.out.println("[debug] user number : "+ConvertUtils.convertArrayToInt(userNumber)); //TODO: Remove this line
+        //TODO: Check the result
+    }
 
     int[] generateBotNumber(int len) {
         Random rand = new Random();
