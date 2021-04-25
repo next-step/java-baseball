38705386c558075cd8360bc6c0fcc36dbc86235b
TJ NAM <dynamicnam@gmail.com>
2021-04-25 23:48:06 +0900

    feat(balls class) dealer의 랜덤 공뽑기 기능 구현
    dealer의 랜덤 공뽑기 기능 구현
    random함수를 사용한 3개의 1-9까지의 숫자를 뽑도록 함.
    
    이 로직을 위한 setBallValues 함수도 구현

--- src/main/java/com/tjnam/baseballgame/Balls.java
+++ src/main/java/com/tjnam/baseballgame/Balls.java
@@ -3,14 +3,45 @@ package com.tjnam.baseballgame;
 import java.util.Arrays;
 
     public class Balls {
+        final int BALL_LENGTH = 3;
+        final int BALL_RANGE = 10;
+        private int[] ballValues;
+
+
         public Balls(){
-
-        }
-}
-        public void SetRandomBalls(){
-
+            this.ballValues = new int[BALL_LENGTH];
         }
 
-        private int[] getRandomNumbers(){
-        
+        public Balls(int[] selection) {
+            this.ballValues = new int[BALL_LENGTH];
         }
+
+        public void SetRandomBalls() {
+            int[] randomBalls = this.getRandomNumbers();
+            this.setBallValues(randomBalls);
+        }
+
+        public void setBallValues(int[] selection) {
+            this.ballValues = selection;
+        }
+
+        public int[] getNumbers(){
+            return this.ballValues;
+        }
+
+        private int[] getRandomNumbers() {
+            int numbers[] = new int[BALL_LENGTH];
+            boolean numberCheck[] = new boolean[BALL_RANGE];
+
+            do {
+                int pick = (((int) (Math.random() * 10)) % 9) + 1;
+
+                if (numberCheck[pick] == false) {
+                    numbers[numbers.length] = pick;
+                    numberCheck[pick] = true;
+                }
+            } while (numbers.length < BALL_LENGTH);
+
+            return numbers;
+        }
+    }
