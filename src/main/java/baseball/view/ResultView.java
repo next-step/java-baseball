386cb6d7a890ec9be9c9a137e4caf07a48ffd68f
juniq <juniqlim@gmail.com>
2021-04-25 23:09:57 +0900

    feat: 결과 출력

---
+++ src/main/java/baseball/view/ResultView.java
@@ -0,0 +1,15 @@
+package baseball.view;
+
+public class ResultView {
+    public static void printScore(int strikeCount, int ballCount) {
+        if (strikeCount != 0) {
+            System.out.print(strikeCount + " 스트라이크 ");
+        }
+        if (ballCount != 0) {
+            System.out.print(ballCount + " 볼");
+        }
+        if (strikeCount == 3) {
+            System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
+}
