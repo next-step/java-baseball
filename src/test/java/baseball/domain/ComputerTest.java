779e6edc0eda1dbb7b6ee1871401aa371ed44465
bbbnam <bbbnam@naver.com>
2021-04-26 02:33:18 +0900

    test : 컴퓨터와 유저간 숫자 매칭 테스트 구분자 누락 추가

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -19,7 +19,7 @@ public class ComputerTest {
 
     @DisplayName("사용자와 컴퓨터가 선택한 숫자 매칭 - 스트라이크의 경우")
     @ParameterizedTest
-    @CsvSource(value = {"123:123:3", "428:325:1", "369:189:1", "274:273:2"})
+    @CsvSource(value = {"123:123:3", "428:325:1", "369:189:1", "274:273:2"}, delimiter = ':')
     void match_strike(String comNumbers, String userNumbers, int strikeCount) {
         Computer computer = new Computer(new BallNumbers(comNumbers));
         Result result = computer.match(new BallNumbers(userNumbers));
@@ -29,7 +29,7 @@ public class ComputerTest {
 
     @DisplayName("사용자와 컴퓨터가 선택한 숫자 매칭 - 볼의 경우")
     @ParameterizedTest
-    @CsvSource(value = {"312:123:3", "428:352:1", "369:193:2", "427:273:2"})
+    @CsvSource(value = {"312:123:3", "428:352:1", "369:193:2", "427:273:2"}, delimiter = ':' )
     void match_ball(String comNumbers, String userNumbers, int ballCount) {
         Computer computer = new Computer(new BallNumbers(comNumbers));
         Result result = computer.match(new BallNumbers(userNumbers));
@@ -39,7 +39,7 @@ public class ComputerTest {
 
     @DisplayName("사용자와 컴퓨터가 선택한 숫자 매칭 - 낫싱의 경우")
     @ParameterizedTest
-    @CsvSource(value = {"123:456", "428:315", "379:182", "274:861"})
+    @CsvSource(value = {"123:456", "428:315", "379:182", "274:861"}, delimiter = ':')
     void match_nothing(String comNumbers, String userNumbers) {
         Computer computer = new Computer(new BallNumbers(comNumbers));
         Result result = computer.match(new BallNumbers(userNumbers));
