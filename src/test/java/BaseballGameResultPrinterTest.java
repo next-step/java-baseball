14b4cf2719a7f4c725b6e0c3259f4fa16c849108
mac2019 <pooheyup@naver.com>
2021-04-25 19:00:44 +0900

    BaseballGameResultPrinter - 생성 테스트 완료

---
+++ src/test/java/BaseballGameResultPrinterTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class BaseballGameResultPrinterTest {
+
+    @Test
+    public void createBaseballGameResultPrinterTest(){
+        BaseballGameResultPrinter printer = new BaseballGameResultPrinter();
+    }
+
+}
