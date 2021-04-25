02ad47c22d9835f25f3c81a163e76bcb3be94939
hstamp <injoo25@naver.com>
2021-04-25 18:13:37 +0900

    refactor: QuestionTest 추가 검증
    - 입력된 값들이 각각 중복되는지 검증

--- src/test/java/techcamp/baseball/domain/QuestionTest.java
+++ src/test/java/techcamp/baseball/domain/QuestionTest.java
@@ -13,15 +13,17 @@ class QuestionTest {
     @Test
     public void validate_generated_numbers() {
         //Given
-        Question game = new Question();
+        Question question = new Question();
+        Validation validation = new Validation();
 
         //When
-        List<Integer> numbers = game.getNumbers();
+        List<Integer> numbers = question.getNumbers();
 
         //Then
         assertEquals(Config.NUMBERS_LENGTH, numbers.size(), "생성된 숫자 리스트의 사이즈는 설정된 값과 같아야 한다.");
         for (int number : numbers) {
             assertTrue(number >= Config.NUMBERS_MIN && number <= Config.NUMBERS_MAX, "생성된 숫자들은 설정된 값 범위 내이다.");
         }
+        assertTrue(validation.isDuplicatedList(question.getNumbers()), "입력된 값이 서로 다른 수여야 한다.");
     }
 }
