9baa6343e45ba830f8fec75d33dffab39cd28bfd
dmswo106 <dmswo106@naver.com>
2021-04-25 15:28:27 +0900

    사용자로부터 입력받은 숫자를 ValidationCheck한다.  - 3자리 길이제한 ValidationCheck를 한다.  - 1~9까지의 수를 입력받는 ValidationCheck를 한다.  - 중복된 숫자 ValidationCheck를 한다.

--- src/main/java/baseball/domain/BaseBallUser.java
+++ src/main/java/baseball/domain/BaseBallUser.java
@@ -14,7 +14,7 @@ public class BaseBallUser {
         String number = "";
         while(check){
             number = userCreateNum();
-            //check = userCreateNumCheck(number);
+            check = userCreateNumCheck(number);
         }
         userNum = number;
     }
@@ -24,5 +24,44 @@ public class BaseBallUser {
         System.out.print("숫자를 입력해주세요 : ");
         String number = sc.nextLine();
         return number;
+    }
+
+    private boolean userCreateNumCheck(String num){
+        boolean check1 = lenCheck(num);
+        boolean check2 = inputCheck(num);
+        boolean check3 = dupCheck(num);
+        if(check1 == true || check2 == true || check3 == true){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean lenCheck(String num){
+        if(num.length() != 3){
+            System.out.println("3자리로 입력해주시기 바랍니다.");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean inputCheck(String num){
+        if(!num.matches("^[1-9]+$")){
+            System.out.println("1~9까지의 수를 입력해주시기 바랍니다.");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean dupCheck(String num){
+        String[] arr = num.split("");
+        String number = "";
+        for(int i=0; i<arr.length; i++){
+            if(number.contains(arr[i])){
+                System.out.println("중복되지 않은 숫자를 입력하시기 바랍니다.");
+                return true;
+            }
+            number += arr[i];
+        }
+        return false;
     }
 }
