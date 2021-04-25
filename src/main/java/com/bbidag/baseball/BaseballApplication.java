147881c725fe864e0ca7eb991c3cf4d1af7dc89f
bbidag <skawlgh112@gmail.com>
2021-04-25 14:34:10 +0900

    feat: 기능 목록 정의, UI 로직 사용자 숫자 3자리 입력 기능

---
+++ src/main/java/com/bbidag/baseball/BaseballApplication.java
@@ -0,0 +1,16 @@
+package com.bbidag.baseball;
+
+import java.util.Scanner;
+
+public class BaseballApplication {
+
+    public static void main(String[] args){
+
+    }
+
+    public String inputThreeDigits(){
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+
+}
