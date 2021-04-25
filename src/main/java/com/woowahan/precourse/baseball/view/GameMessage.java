0ffcc33d60d87ded7d3c7d440203c94cccdf87ea
junseok Oh <ixtears23@icloud.com>
2021-04-25 14:41:19 +0900

    feat(view) : output 문구(게임 종료)
    게임 종료 시 사용자에게 표시되는 텍스트
    게임을 종료할 것인지 새로 시잘할 것인지 묻는 텍스트

--- src/main/java/com/woowahan/precourse/baseball/view/GameMessage.java
+++ src/main/java/com/woowahan/precourse/baseball/view/GameMessage.java
@@ -5,4 +5,5 @@ public class GameMessage {
     private GameMessage() {}
 
     public static final String ENTER_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String VICTORY = "3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
 }
