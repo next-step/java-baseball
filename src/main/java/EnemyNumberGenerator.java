e396a1191f1f2279f6fc0b90e99dbaa11b27e3f7
leemingyu09 <hikakaka13@naver.com>
2021-04-25 19:46:20 +0900

    feat: 유효성 체크

--- src/main/java/EnemyNumberGenerator.java
+++ src/main/java/EnemyNumberGenerator.java
@@ -5,7 +5,7 @@ public class EnemyNumberGenerator {
 
     private void initNumber(BaseBallDTO dto){
         List<String> numbers = dto.getEnemyNumbers();
-        String randomNumber = String.valueOf((int)(Math.random()*GameSetting.COUNT.getValue())+1);
+        String randomNumber = String.valueOf((int)(Math.random()*GameSetting.COUNT.value)+1);
         if(!numbers.contains(randomNumber)){
             numbers.add(randomNumber);
         }
@@ -15,7 +15,7 @@ public class EnemyNumberGenerator {
     protected void generateNumber(BaseBallDTO dto){
         List<String> numberStorage = new ArrayList<>();
         dto.setEnemyNumbers(numberStorage);
-        int maxLength = GameSetting.LENGTH.getValue();
+        int maxLength = GameSetting.LENGTH.value;
         while(dto.getEnemyNumbers().size()<maxLength){
             initNumber(dto);
         }
