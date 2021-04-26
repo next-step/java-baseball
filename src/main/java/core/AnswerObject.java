996c74f3784333f42eaa86fadb9eba2f33b87dba
Jun Young Choi <neocjy@naver.com>
2021-04-27 03:31:00 +0900

    feat (core logic) : 퀴즈 진행 관련 class 개발
    - 퀴즈 진행 관련 class 개발
    - 기존 generateNumber class refactoring 진행

--- src/main/java/core/AnswerObject.java
+++ src/main/java/core/AnswerObject.java
@@ -1,11 +1,28 @@
 package core;
 
+import java.util.Objects;
+
 /**
  * Description : 게임 내 정답 관련 class
  *
  * Created by jychoi on 2021/04/27.
  */
 public class AnswerObject {
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		AnswerObject that = (AnswerObject)o;
+		return strike == that.strike && ball == that.ball && isFourBall == that.isFourBall;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(strike, ball, isFourBall);
+	}
 
 	public AnswerObject(int strike, int ball) {
 		this.strike = strike;
