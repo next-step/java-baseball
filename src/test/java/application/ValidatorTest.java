87dd97e1b625636fbd72b980401f3b45d763d2cd
kwkim <kkw5657@gmail.com>
2021-04-26 21:14:51 +0900

    test: Validator 클래스 테스트 추가
    중복숫자 입력 테스트
    1~9입력테스트

--- src/test/java/application/ValidatorTest.java
+++ src/test/java/application/ValidatorTest.java
@@ -37,4 +37,38 @@ public class ValidatorTest {
         }
     }
 
+    @Test
+    @DisplayName("중복수 입력 방지 테스트")
+    public void checkSameTest() {
+        try {
+            Method method = validator.getClass().getDeclaredMethod("checkSame", String.class);
+            method.setAccessible(true);
+            boolean ret = (boolean) method.invoke(validator, "321");
+            assertThat(ret).isTrue();
+            ret = (boolean) method.invoke(validator, "549");
+            assertThat(ret).isTrue();
+            ret = (boolean) method.invoke(validator, "112");
+            assertThat(ret).isFalse();
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("1~9 입력 테스트")
+    public void checkRangeTest() {
+        try {
+            Method method = validator.getClass().getDeclaredMethod("checkRange", String.class);
+            method.setAccessible(true);
+            boolean ret = (boolean) method.invoke(validator, "123");
+            assertThat(ret).isTrue();
+            ret = (boolean) method.invoke(validator, "987");
+            assertThat(ret).isTrue();
+            ret = (boolean) method.invoke(validator, "012");
+            assertThat(ret).isFalse();
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
