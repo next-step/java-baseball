773db488c9d1f1647720459785ce5806060ac787
NIck0526 <km.lee0526@gmail.com>
2021-04-27 02:07:22 +0900

    feat: 임의의수 생성기능 구현

---
+++ src/test/java/baseball/player/ComputerTest.java
@@ -0,0 +1,38 @@
+package baseball.player;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputerTest {
+
+    private Computer computer;
+    private int ballCount;
+
+    @BeforeEach
+    void init() {
+        computer = new Computer();
+        ballCount = 3;
+    }
+
+    @Test
+    void 임의의_수_생성기능_구현() {
+
+        //given
+
+        //when
+        List<Integer> balls = computer.ready(ballCount);
+
+        //then
+        assertThat(balls).hasSize(ballCount);
+        assertThat(balls).doesNotHaveDuplicates();
+
+    }
+
+}
