89bfd2f24c07892e63fce19c34506fc5c8e4a831
accidentlywoo <accidentlywoo@gmail.com>
2021-04-26 23:04:10 +0900

    feat : 이넘분리, UI 패키지로 이동

--- src/main/java/NumberBaseBallGame/domain/BaseBallConst.java
+++ src/main/java/NumberBaseBallGame/domain/BaseBallConst.java
@@ -5,23 +5,5 @@ public class BaseBallConst {
 	public static final int INPUT_RANGE_MIN = 1;
 	public static final int INPUT_RANGE_MAX = 9;
 
-	public enum GAME_STATUS{
-		STRIKE("STRIKE", "스트라이크"),
-		BALL("BALL", "볼"),
-		NOTHING("NOTHING", "낫싱");
-		private String code;
-		private String name;
 
-		GAME_STATUS(String code, String name) {
-			this.code = code;
-			this.name = name;
-		}
-		public String getCode() {
-			return code;
-		}
-
-		public String getName() {
-			return name;
-		}
-	}
 }
---
+++ src/main/java/NumberBaseBallGame/view/BaseBallStatus.java
@@ -0,0 +1,21 @@
+package NumberBaseBallGame.view;
+
+public enum BaseBallStatus {
+	STRIKE("STRIKE", "스트라이크"),
+	BALL("BALL", "볼"),
+	NOTHING("NOTHING", "낫싱");
+	private String code;
+	private String message;
+
+	BaseBallStatus(String code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+	public String getCode() {
+		return code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
