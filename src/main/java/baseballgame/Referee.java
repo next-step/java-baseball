406ea35323055edfcf82e0f0b430770fb15e2678
KyungSik9870 <rudtlr9870@gmail.com>
2021-04-26 22:11:55 +0900

    refactor(Referee): 입력값에 대한 모든 판단은 Referee 클래스가 하도록 수정
    비교하는 두 대상의 자료형을 동일하게 맞춤.
    validate를 수행 후, 결과를 BaseBallResult에 담도록 수정

--- src/main/java/baseballgame/Referee.java
+++ src/main/java/baseballgame/Referee.java
@@ -1,23 +1,36 @@
 package baseballgame;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Referee {
 	private int containsCount = 0;
 	private int strikeCount = 0;
+	private InputValidator validator = new InputValidator();
 
-	public BaseBallResult judgeBall(List<Integer> answerList, String userInput) {
-		int[] userInputArray = getUserInputArray(userInput);
-		for (int index = 0; index < answerList.size(); index++) {
-			plusContainsCount(answerList, userInputArray[index]);
-			plusStrikeCount(answerList.get(index), userInputArray[index]);
+	public BaseBallResult getResult(List<Integer> answerList, String userInput) {
+		String errorMessage = null;
+		try {
+			validator.validate(userInput);
+			List<Integer> userInputList = changeUserInputStringToList(userInput);
+			judgeUserInput(answerList, userInputList);
+		} catch (IllegalArgumentException e) {
+			errorMessage = e.getMessage();
 		}
-		return new BaseBallResult(containsCount, strikeCount);
+
+		return new BaseBallResult(containsCount, strikeCount, errorMessage);
 	}
 
 	public void initCount() {
 		containsCount = 0;
 		strikeCount = 0;
+	}
+
+	private void judgeUserInput(List<Integer> answerList, List<Integer> userInputList) {
+		for (int index = 0; index < answerList.size(); index++) {
+			plusContainsCount(answerList, userInputList.get(index));
+			plusStrikeCount(answerList.get(index), userInputList.get(index));
+		}
 	}
 
 	private void plusContainsCount(List<Integer> answerList, int userInput) {
@@ -32,12 +45,12 @@ public class Referee {
 		}
 	}
 
-	private int[] getUserInputArray(String userInput) {
+	private List<Integer> changeUserInputStringToList(String userInput) {
 		String[] userStringInputArray = userInput.split("");
-		int[] userInputArray = new int[userStringInputArray.length];
-		for (int i = 0; i < userStringInputArray.length; i++) {
-			userInputArray[i] = Integer.parseInt(userStringInputArray[i]);
+		List<Integer> userInputList = new ArrayList<>();
+		for (String s : userStringInputArray) {
+			userInputList.add(Integer.parseInt(s));
 		}
-		return userInputArray;
+		return userInputList;
 	}
 }
--- src/test/java/baseballgame/RefereeTest.java
+++ src/test/java/baseballgame/RefereeTest.java
@@ -26,7 +26,7 @@ public class RefereeTest {
 	@DisplayName("User 가 입력한 숫자에 맞게 스트라이크 개수가 세어지는지 테스트.")
 	void countStrike() {
 		String userInput = "134";
-		BaseBallResult baseBallResult = referee.judgeBall(answerList, userInput);
+		BaseBallResult baseBallResult = referee.getResult(answerList, userInput);
 		assertEquals(baseBallResult.getStrikeCount(), 1);
 	}
 
@@ -34,7 +34,7 @@ public class RefereeTest {
 	@DisplayName("User 가 입력한 숫자에 맞게 볼 개수가 세어지는지 테스트.")
 	void countBall() {
 		String userInput = "345";
-		BaseBallResult baseBallResult = referee.judgeBall(answerList, userInput);
+		BaseBallResult baseBallResult = referee.getResult(answerList, userInput);
 		assertEquals(baseBallResult.getBallCount(), 1);
 	}
 
@@ -42,7 +42,7 @@ public class RefereeTest {
 	@DisplayName("User 가 입력한 숫자에 스트라이크와, 볼이 없으면 낫싱인지 테스트.")
 	void isZeroStrikeAndBallThenNothing() {
 		String userInput = "456";
-		BaseBallResult baseBallResult = referee.judgeBall(answerList, userInput);
+		BaseBallResult baseBallResult = referee.getResult(answerList, userInput);
 		assertTrue(baseBallResult.isNothing());
 	}
 }
