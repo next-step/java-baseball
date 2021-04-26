8138a900540805d64a948d279ad100f072bf65cf
seungkwon <yevgnenll@gmail.com>
2021-04-26 15:04:58 +0900

    feat: 유저에게 채점 결과를 알려주는 기능 구현
    k-v 형식으로 오는 유저의 채점 결과를 받아 UserInterface 는
    사람이 읽을 수 있는 메세지 형태로 변경하여 console 에 출력한다.
    주어진 채점 결과에 맞춰 필요한 메세지를 작성하고 이 메세지는
    테스트 코드를 통해 검증한다.

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,47 @@
+import java.util.Map;
+import java.util.Scanner;
+
+import validator.NumberFormatValidator;
+
+public class UserInterface {
+  private final Scanner scanner = new Scanner(System.in);
+
+  private final String[] messages = {
+      "숫자를 입력해 주세요",
+      "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+  };
+
+  public String interactUser() {
+    System.out.println(messages[0]);
+    String input = scanner.next();
+    NumberFormatValidator.validate(input);
+    return input;
+  }
+
+  protected String convertScoreToMessage(Map<Result, Integer> score) {
+    StringBuilder answer = new StringBuilder();
+    writeMessageBy(score, Result.STRIKE, answer);
+    if (!isInitMessage(answer) && hasNext(score))
+      answer.append(" ");
+    writeMessageBy(score, Result.BALL, answer);
+    return answer.toString();
+  }
+
+  private void writeMessageBy(Map<Result, Integer> score, Result resultType, StringBuilder message) {
+    if (!score.containsKey(resultType))
+      return;
+    message.append(score.get(resultType))
+        .append(" ")
+        .append(resultType.getMessage());
+  }
+
+  private boolean isInitMessage(StringBuilder message) {
+    return message.length() == 0;
+  }
+
+  private boolean hasNext(Map<Result, Integer> score) {
+    return score.containsKey(Result.BALL);
+  }
+
+}
---
+++ src/test/java/UserInterfaceTest.java
@@ -0,0 +1,58 @@
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserInterfaceTest {
+
+  @Test
+  @DisplayName(value = "k-v STRIKE:1, BALL:2 은 1 스트라이크 2 볼 이라는 메세지를 생산한다")
+  void oneStrikeTwoBall() {
+    Referee referee = new Referee(135, "531");
+    Map<Result, Integer> judge = referee.judge();
+
+    UserInterface userInterface = new UserInterface();
+    String message = userInterface.convertScoreToMessage(judge);
+
+    assertThat(message).isEqualTo("1 스트라이크 2 볼");
+  }
+
+  @Test
+  @DisplayName(value = "k-v STRIKE:3 이면 3 스트라이크 라는 메세지를 생성한다")
+  void threeStrike() {
+    Referee referee = new Referee(135, "135");
+    Map<Result, Integer> judge = referee.judge();
+
+    UserInterface userInterface = new UserInterface();
+    String message = userInterface.convertScoreToMessage(judge);
+
+    assertThat(message).isEqualTo("3 스트라이크");
+  }
+
+  @Test
+  @DisplayName(value = "k-v BALL:3 이면 3 볼 이라는 메세지를 생성한다.")
+  void threeBall() {
+    Referee referee = new Referee(135, "513");
+    Map<Result, Integer> judge = referee.judge();
+
+    UserInterface userInterface = new UserInterface();
+    String message = userInterface.convertScoreToMessage(judge);
+
+    assertThat(message).isEqualTo("3 볼");
+  }
+
+  @Test
+  @DisplayName(value = "일치하는 것이 없을 때 빈 문자열을 반환한다")
+  void notMatched() {
+    Referee referee = new Referee(135, "897");
+    Map<Result, Integer> judge = referee.judge();
+
+    UserInterface userInterface = new UserInterface();
+    String message = userInterface.convertScoreToMessage(judge);
+
+    assertThat(message).isEqualTo("");
+  }
+
+}
