4b09493c7575a4f22f8141180fe38d5555428aaf
HueWoon Kim <kho37@naver.com>
2021-04-27 00:08:30 +0900

    feat(UI): 사용자 입력값 받기 추가.

--- src/main/java/com/nextstep/baseball/BaseballUI.java
+++ src/main/java/com/nextstep/baseball/BaseballUI.java
@@ -1,10 +1,19 @@
 package com.nextstep.baseball;
 
+import java.util.Scanner;
+
 public class BaseballUI {
+	private Scanner sc;
 	public BaseballUI() {
+		sc = new Scanner(System.in);
 	}
 	
 	public void printToInputNumber() {
 		System.out.println("숫자를입력해주세요:");
+	}
+	
+	public int getUserInputNumber() {
+		int count = sc.nextInt();
+		return count;
 	}
 }
