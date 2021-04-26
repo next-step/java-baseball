271d87cc0c71ccb640968f11c070687a916641ee
kimmin-ko <kimmin2301@gmail.com>
2021-04-26 19:55:58 +0900

    test: 정답 비교 로직에 대한 반환 값 테스트 구현

--- src/test/java/baseball/opponent/OpponentTest.java
+++ src/test/java/baseball/opponent/OpponentTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.Map;
+
+import static baseball.opponent.Opponent.ResultType;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -68,6 +72,93 @@ class OpponentTest {
         assertThatThrownBy(() -> opponent.compare(answer))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복된 값을 입력할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("수와 자리 모두 2개가 같을 경우 2 스트라이크를 반환한다.")
+    void compare_two_strike_test() {
+        // given
+        int[] answers = opponent.getAnswers();
+
+        int answer1 = answers[0] * 100;
+        int answer2 = answers[1] * 10;
+        int wrongAnswer = getNonDuplicatedValue(answers);
+
+        int answer = answer1 + answer2 + wrongAnswer;
+
+        // when
+        Map<ResultType, Integer> compare = opponent.compare(answer);
+
+        // then
+        assertThat(compare.get(ResultType.STRIKE)).isEqualTo(2);
+        assertThat(compare).doesNotContainKey(ResultType.BALL);
+        assertThat(compare).doesNotContainKey(ResultType.NOTHING);
+    }
+
+    @Test
+    @DisplayName("수와 자리가 1개, 수는 2개 같지만 자리가 다를 경우 1 스트라이크 2 볼을 반환한다.")
+    void compare_one_strike_two_ball_test() {
+        // given
+        int[] answers = opponent.getAnswers();
+
+        int changed3answer1 = answers[2] * 100;
+        int answer2 = answers[1] * 10;
+        int changed1answer3 = answers[0];
+
+        int answer = changed3answer1 + answer2 + changed1answer3;
+
+        // when
+        Map<ResultType, Integer> compare = opponent.compare(answer);
+
+        // then
+        assertThat(compare.get(ResultType.STRIKE)).isEqualTo(1);
+        assertThat(compare.get(ResultType.BALL)).isEqualTo(2);
+        assertThat(compare).doesNotContainKey(ResultType.NOTHING);
+    }
+
+    @Test
+    @DisplayName("같은 값이 하나도 없으면 낫싱을 반환한다.")
+    void compare_nothing_test() {
+        // given
+        int[] answers = opponent.getAnswers();
+
+        int answer1 = getNonDuplicatedValue(answers) * 100;
+        int checkAnswer1 = answer1 / 100;
+
+        int answer2 = getNonDuplicatedValue(answers, checkAnswer1) * 10;
+        int checkAnswer2 = answer2 / 10;
+
+        int answer3 = getNonDuplicatedValue(answers, checkAnswer1, checkAnswer2);
+
+        int wrongAnswer = answer1 + answer2 + answer3;
+
+        // when
+        Map<ResultType, Integer> compare = opponent.compare(wrongAnswer);
+
+        // then
+        assertThat(compare).containsKey(ResultType.NOTHING);
+        assertThat(compare).doesNotContainKey(ResultType.STRIKE);
+        assertThat(compare).doesNotContainKey(ResultType.BALL);
+    }
+
+    private int getNonDuplicatedValue(int[] answers, int... addedAnswer) {
+        int random = (int) (Math.random() * 9) + 1;
+
+        // 배열을 정렬 위한 얕은 복사
+        int[] tempAnswers = Arrays.copyOf(answers, 10);
+
+        // 추가 된 값도 중복 검사에 포함시키기 위해 tempAnswers에 추가
+        System.arraycopy(addedAnswer, 0, tempAnswers, 3, addedAnswer.length);
+
+        // binarySearch 하기 위한 정렬
+        Arrays.sort(tempAnswers);
+
+        // random 값이 배열에 있으면 재귀 호출로 random 값 재생성
+        if (Arrays.binarySearch(tempAnswers, random) >= 0)
+            return getNonDuplicatedValue(answers, addedAnswer);
+
+        // 중복되지 않은 random 값 반환
+        return random;
     }
 
 }
