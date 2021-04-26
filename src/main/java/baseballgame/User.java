c07c93459232fa2afa4ecc48a21c622e9f0e1a5b
KyungSik9870 <rudtlr9870@gmail.com>
2021-04-26 12:46:34 +0900

    refactor(User): User 클래스가 사용자의 입력에 관한 것만 처리하도록 리팩토링

--- src/main/java/baseballgame/User.java
+++ src/main/java/baseballgame/User.java
@@ -4,32 +4,28 @@ import java.util.Scanner;
 
 public class User {
 
-	private String[] userStringInputArray;
 	private String userInput;
 
-	public void setUserAnswerNumberArray() {
+	public String setUserInputNumberArray() {
 		try {
+			System.out.println(PrintMessageEnum.ENTER_A_NUMBER.getMessage());
 			Scanner scanner = new Scanner(System.in);
 			userInput = scanner.nextLine();
-			validateInput(userInput);
+			validateNumberInput(userInput);
+			validateCharacterInput(userInput);
 		} catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 		}
+		return userInput;
 	}
 
-	public int[] getUserInputArray() {
-		userStringInputArray = userInput.split("");
-		int[] userInputArray = new int[userStringInputArray.length];
-		for (int i = 0; i < userStringInputArray.length; i++) {
-			userInputArray[i] = Integer.parseInt(userStringInputArray[i]);
-		}
-		return userInputArray;
-	}
-
-	public void validateInput(String userInput) {
+	public void validateNumberInput(String userInput) {
 		if (userInput.length() != 3) {
 			throw new IllegalArgumentException("입력값은 3자리여야 합니다.");
 		}
+	}
+
+	public void validateCharacterInput(String userInput) {
 		if (!userInput.matches("^[0-9]*$")) {
 			throw new IllegalArgumentException("입력값은 숫자여야 합니다.");
 		}
--- src/test/java/baseballgame/UserTest.java
+++ src/test/java/baseballgame/UserTest.java
@@ -20,13 +20,13 @@ public class UserTest {
 	@ValueSource(strings = {"1234", "12", "12345", "", "1"})
 	@DisplayName("User 가 입력한 input 값이 3자리가 아니면 IllegalArgumentException 이 발생하는지 테스트.")
 	void validateInputLength(String userInput) {
-		assertThrows(IllegalArgumentException.class, () -> user.validateInput(userInput));
+		assertThrows(IllegalArgumentException.class, () -> user.validateNumberInput(userInput));
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"12d", "sss", " ff", " ", "123 "})
 	@DisplayName("User 가 입력한 input 값에 숫자가 아닌값이 포함되어있으면 IllegalArgumentException 이 발생되는지 테스트.")
 	void validateInputPattern(String userInput) {
-		assertThrows(IllegalArgumentException.class, () -> user.validateInput(userInput));
+		assertThrows(IllegalArgumentException.class, () -> user.validateNumberInput(userInput));
 	}
 }
