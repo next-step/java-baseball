259d3af6edb40d6b561afdcfeedb1f34696fc172
seongmin <qowlxj08@naver.com>
2021-04-26 23:38:12 +0900

    게임 시작 및 진행관리

--- src/main/java/PlayBall.java
+++ src/main/java/PlayBall.java
@@ -6,23 +6,26 @@ public class PlayBall {
 
     private UserInputNumber userInput = new UserInputNumber();
     private CreateGameBall createBall = new CreateGameBall();
+    private CompareBall compareBall = new CompareBall();
 
-    public PlayBall() {
-
-    }
+    private Scanner scan = new Scanner(System.in);
 
     public void gameReady() {
-        Scanner scan = new Scanner(System.in);
+        List<Integer> computerBall;
         int status = 1;
         while (status == 1) {
-            int[] userNumber = userInput.InputNumber();
-            gameStart(userNumber);
-            System.out.print("게임을 계속할려면 1 종료할려면 2를 눌러주세요.");
+            compareBall.setStatus(true);
+            computerBall = createBall.getBall();
+            gameStart(computerBall);
+            System.out.print("게임을 계속할려면 1, 그만할려면 2를 입력하세요 :");
             status = scan.nextInt();
         }
     }
 
-    private void gameStart(int[] userNumber) {
-
+    private void gameStart(List<Integer> computerBall) {
+        while (compareBall.getStatus()) {
+            List<Integer> userNumber = userInput.InputNumber();
+            compareBall.checkStart(userNumber, computerBall);
+        }
     }
 }
