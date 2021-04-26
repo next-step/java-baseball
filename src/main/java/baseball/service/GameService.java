82fd8027c1664a4effdac0f9b7875e10e4c8cefc
kslee <meant0415@gmail.com>
2021-04-26 19:10:16 +0900

    feat: 난수 생성 기능 추가 (src/main/java/baseball/service)

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,4 +1,18 @@
 package baseball.service;
 
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class GameService {
+
+	public Set<Integer> generateNumber() {
+		Random random = new Random();
+		Set<Integer> numbers = new LinkedHashSet<>();
+		do {
+			numbers.add(random.nextInt(9) + 1);
+		} while (numbers.size() < 3);
+		return numbers;
+	}
+
 }
