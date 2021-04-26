8d8230640cbb108866a3b128605f8e09ee7f599a
Shinyoung Kim <rolroralra@gmail.com>
2021-04-26 17:59:32 +0900

    build: add TODO list
    remove printing answer in Controller, before deployment

--- src/main/java/baseball/controller/impl/BaseballController.java
+++ src/main/java/baseball/controller/impl/BaseballController.java
@@ -19,6 +19,8 @@ public class BaseballController extends BaseballControllerTemplate {
                 .size(size)
                 .radix(radix)
                 .build();
+        
+        // TODO: remove print answer before deployment.
         System.out.println(this.answer);
     }
 
