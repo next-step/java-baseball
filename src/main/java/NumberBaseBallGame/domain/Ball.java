bca163f281847ad51bb9c217af892916c45cbf93
accidentlywoo <accidentlywoo@gmail.com>
2021-04-26 22:17:29 +0900

    feat : override equals compair valueNumber
    필드의 valueNumber가 일치하면 같은 공으로 판단

--- src/main/java/NumberBaseBallGame/domain/Ball.java
+++ src/main/java/NumberBaseBallGame/domain/Ball.java
@@ -1,9 +1,11 @@
 package NumberBaseBallGame.domain;
 
+import java.util.Objects;
+
 public class Ball {
 	public static final int BALL_NUMBER_MIN = BaseBallConst.INPUT_RANGE_MIN;
 	public static final int BALL_NUMBER_MAX = BaseBallConst.INPUT_RANGE_MAX;
-	private int valueNumber;
+	private final int valueNumber;
 
 	public Ball(int valueNumber) {
 		if (valueNumber<BALL_NUMBER_MIN){
@@ -17,5 +19,18 @@ public class Ball {
 
 	public int getValueNumber() {
 		return valueNumber;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Ball ball = (Ball) o;
+		return valueNumber == ball.valueNumber;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(valueNumber);
 	}
 }
