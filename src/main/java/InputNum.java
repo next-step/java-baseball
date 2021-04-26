2b6c339880964b095fae707ddecb445b54f924a6
이유진 <ujin@deali.net>
2021-04-27 03:07:15 +0900

    feat: 입력받은 숫자 갯수가 3개인지 검사
    입력받은 숫자 갯수가 3개가 아니라면 false, 맞다면 true를 반환한가

--- src/main/java/InputNum.java
+++ src/main/java/InputNum.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 
 public class InputNum {
+    private static final Integer NUM_SIZE = 3;
 
     public String scanNum(){
         Scanner scan = new Scanner(System.in);
         System.out.print("숫자를 입력해주세요 : ");
         return scan.next();
     }
+
+    public static Boolean checkNumSize(String num){
+        if(num.length() != NUM_SIZE){
+            return false;
+        }
+        return true;
+    }
+
 }
