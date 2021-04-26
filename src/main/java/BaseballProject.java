5bdfdfece13fb35964d09f67211b20420728e4d7
hyoungki <lhkgogo@gmail.com>
2021-04-27 06:56:12 +0900

    feat: 게임 시작 로직 작성.
    - 기존 BaseballRules의 createRandomNumbers 함수 BaseballService로 이동.

--- src/main/java/BaseballProject.java
+++ src/main/java/BaseballProject.java
@@ -1,8 +1,7 @@
 public class BaseballProject {
 
     public static void main(String[] args) {
-        BaseballRules rules = new BaseballRules();
-        String [] baseNumbers = rules.createRandomNumbers(Constants.BASEBALL_DEFAULT_NUMBER_SIZE);
+        BaseballService service = new BaseballService();
+        service.startGame(Constants.BASEBALL_DEFAULT_NUMBER_SIZE);
     }
-
 }
