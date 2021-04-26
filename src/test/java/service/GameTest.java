0b8b6cc749fbb714bad54a6235d57fd9561978b8
rnthd2 <rnthd2@naver.com>
2021-04-27 06:06:39 +0900

    test(GameTest): 게임을 진행하는 로직의 테스트 코드를 작성합니다
    사용자 입력값을 System.setIn으로 임의로 설정
    1. 임의로 설정한 사용자 입력값과 임의로 생성한 난수생성값으로 게임 테스트 진행 확인
     2. 게임이 정상적으로 종료되는지, 정상적으로 재시작되는지 확인

---
+++ src/test/java/service/GameTest.java
@@ -0,0 +1,48 @@
+package service;
+
+import domain.Bettings;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+@DisplayName(value = "게임 로직 테스트")
+class GameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123"})
+    void game(String input) {
+        setSystemInputStream(input);
+        Bettings bettings = new Bettings(new ArrayList(Arrays.asList(1,2,3)));
+        Game.init();
+        Game.setBettings(bettings);
+        Game.playGame();
+    }
+
+    private void setSystemInputStream(String input) {
+        InputStream in = generateUserInput(input);
+        System.setIn(in);
+    }
+
+    private static InputStream generateUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2", "1"})
+    void isGameOver(String input) {
+        setSystemInputStream(input);
+        assumingThat(input == "2", () ->
+                assertFalse(Game.isGameOver()));
+        assumingThat(input == "1", () ->
+                assertTrue(Game.isGameOver()));
+    }
+}
