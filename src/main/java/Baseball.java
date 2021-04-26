d62c54f04d61fc264a7f598905fd460c93685ad5
박지원 <mz_jwp@woowafriends.com>
2021-04-26 15:19:02 +0900

    feat(main): receive input number

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -8,12 +8,29 @@ public class Baseball {
     private List<Integer> opponentNumber;
     private List<Integer> userNumber;
 
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
     public List<Integer> getOpponentNumber() {
         return opponentNumber;
     }
 
     public void setOpponentNumber(List<Integer> opponentNumber) {
         this.opponentNumber = opponentNumber;
+    }
+
+    public void setUserNumber(List<Integer> userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public void init() {
+        this.strike = 0;
+        this.ball = 0;
     }
 
 }
--- src/main/java/BaseballService.java
+++ src/main/java/BaseballService.java
@@ -1,10 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class BaseballService {
 
+    private Scanner scanner;
+
     public void start() {
+        boolean isPlay = true;
         Baseball baseball;
         baseball = new Baseball();
         baseball.setOpponentNumber(BaseUtil.getRandomNumber());
+        while (isPlay) {
+            baseball.init();
+            baseball.setUserNumber(getUserNumber());
+        }
+    }
+
+    private void initScanner() {
+        scanner = new Scanner(System.in);
+    }
+
+    private List<Integer> getUserNumber() {
+        List<Integer> userNumber = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요: ");
+        initScanner();
+        int inputNumber = scanner.nextInt();
+        while (inputNumber > 0) {
+            userNumber.add(inputNumber % 10);
+            inputNumber /= 10;
+        }
+        Collections.reverse(userNumber);
+        return userNumber;
     }
 
 }
