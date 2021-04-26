07cb1815304c1203004ca24da31e161531f833e8
JSA <sa.cho@newploy.net>
2021-04-27 03:17:43 +0900

    feat($pre-game): create computer play value
    컴퓨터가 가지는 수를 생성했다.

--- src/main/java/com/woowahan/BaseballGame.java
+++ src/main/java/com/woowahan/BaseballGame.java
@@ -12,16 +12,31 @@ import static com.woowahan.enums.PlayType.*;
 public class BaseballGame {
 
     static Scanner scanner;
+    static Set<Integer> computerSet;
 
     public static void main(String[] args) {
         System.out.println(GREETING_MESSAGE);
         scanner = new Scanner(System.in);
         boolean isContinue = true;
+        computerSet = getComputerSet();
         while (isContinue) {
-            isContinue = false;
+            isContinue = playGame();
         }
         System.out.println(END_MESSAGE);
         scanner.close();
+    }
+
+    private static boolean playGame() {
+        return false;
+    }
+
+    private static LinkedHashSet<Integer> getComputerSet() {
+        LinkedHashSet<Integer> computerSet = new LinkedHashSet<>();
+        Random random = new Random();
+        while (computerSet.size() < 3) {
+            computerSet.add(random.nextInt(10));
+        }
+        return computerSet;
     }
 
 }
