18624f46d82490caaabd2172cfd18f3c6ad0e9e8
expyh <dublineryh@gmail.com>
2021-04-26 09:50:26 +0900

    feat(MenuUI.java): add main menu UI and main menu UI tests
    1-1. 게임 메인 메뉴 in README.md

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+import ui.MenuUI;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuUI menu = new MenuUI();
+        Scanner scan = new Scanner(System.in);
+        boolean isRunning = true;
+        while(isRunning) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            isRunning = menu.getMenuInput(scan.nextLine());
+        }
+    }
+
+}
+
+
---
+++ src/main/java/ui/MenuUI.java
@@ -0,0 +1,27 @@
+package ui;
+
+import gamelogic.Game;
+
+public class MenuUI {
+    Game game = new Game();
+
+    public boolean getMenuInput(String input){
+        if (!isValidMenuInput(input)){
+            System.out.println("유효하지 않은 입력입니다.");
+            return true;
+        }
+        if (input.equals("2")){
+            System.out.println("게임을 종료합니다.");
+            return false;
+        }
+        game.runNewGame();
+        return true;
+    }
+
+    public boolean isValidMenuInput(String input){
+        if (input.equals("1") || input.equals("2")){
+            return true;
+        }
+        return false;
+    }
+}
