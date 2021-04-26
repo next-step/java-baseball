42b2d2cbac97ab30b774dcdd5127b158df1b8ff7
이유진 <ujin@deali.net>
2021-04-26 22:13:05 +0900

    feat: 3자리의 서로다른 수로 이루어진 Computer숫자를 반환
    - 클래스 메서드들을 활용하여 3자리의 서로다른 수로 이루어진 Computer숫자를 반환한다
    - Computer숫자의 자릿수를
    설정하는 상수 추가 (PICK)

--- src/main/java/ComputerNum.java
+++ src/main/java/ComputerNum.java
@@ -3,7 +3,19 @@ import java.util.*;
 public class ComputerNum {
     private static final Integer START = 1;
     private static final Integer END = 10;
+    private static final Integer PICK = 3;
 
+    public String getComputerNum(){
+        StringBuilder computerNum = new StringBuilder();
+        List<Integer> defaultNum = setDefaultNum();
+
+        for(int p = 0; p < PICK; p++) {
+            Integer randomNum = pickRandomNum(defaultNum);
+            computerNum.append(defaultNum.get(randomNum).toString());
+            defaultNum = outNumInList(defaultNum, randomNum);
+        }
+        return computerNum.toString();
+    }
 
     public List<Integer> setDefaultNum(){
         List<Integer> defaultNum = new ArrayList<>();
