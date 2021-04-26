c097f5557df87ac0868e923ad034f38444549d68
accidentlywoo <accidentlywoo@gmail.com>
2021-04-26 16:05:33 +0900

    test : create BallNumberMatcher unit tests

---
+++ src/test/java/NumberBaseBallGame/generater/BallNumberMatcherTest.java
@@ -0,0 +1,73 @@
+package NumberBaseBallGame.generater;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallNumberMatcherTest {
+
+	@Test
+	@DisplayName("4. 스트라이크 [😊] 원 스트라이크 메치 테스트")
+	public void match_one_strike_green(){
+		// given
+		// when
+		// than
+	}
+
+	@Test
+	@DisplayName("4. 스트라이크 [😊] 투 스트라이크 메치 테스트")
+	public void match_two_strike_green(){
+		// given
+		// when
+		// than
+	}
+
+	@Test
+	@DisplayName("4. 스트라이크 [😊] 쓰리 스트라이크 메치 테스트 :: 올 매칭")
+	public void match_three_strike_green(){
+		// given
+		// when
+		// than
+	}
+
+	@Test
+	@DisplayName("5. 볼 [😊] 원 볼 메치 테스트")
+	public void match_one_ball_green(){
+		// given
+		// when
+		// than
+	}
+
+	@Test
+	@DisplayName("5. 볼 [😊] 투 볼 메치 테스트")
+	public void match_two_ball_green(){
+		// given
+		// when
+		// than
+	}
+
+	@Test
+	@DisplayName("5. 볼 [😊] 쓰리 볼 메치 테스트")
+	public void match_three_ball_green(){
+		// given
+		// when
+		// than
+	}
+
+	@Test
+	@DisplayName("6. 포볼 / 낫싱 [😊] 나싱 메치 테스트")
+	public void match_nothing_green(){
+	    // given
+	    // when
+	    // than
+	}
+
+	@Test
+	@DisplayName("4. 스트라이크 / 5. 볼 [😊] 스트라이크 볼 믹스 메치 테스트")
+	public void match_strike_ball_green(){
+		// given
+		// when
+		// than
+	}
+}
