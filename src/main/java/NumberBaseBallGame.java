47b79465e9eed212b30bdfd15913c3f2d644d103
Dongbin <bnitech88@gmail.com>
2021-04-27 00:44:49 +0900

    feat(NumberBaseBallGame): 볼 카운트 출력

--- src/main/java/NumberBaseBallGame.java
+++ src/main/java/NumberBaseBallGame.java
@@ -2,18 +2,21 @@ import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
+import dto.BallCount;
 import exception.InvalidValueException;
 import service.IInitializer;
+import service.IMatchingSystem;
 import service.impl.Initializer;
+import service.impl.MatchingSystem;
 
 public class NumberBaseBallGame {
 
-	private final static IInitializer iInitializer = new Initializer();
+	private final static int MAX_DIGIT_NUMBER = 3;
+	private final static IInitializer initializer = new Initializer(MAX_DIGIT_NUMBER);
+	private final static IMatchingSystem matchingSystem = new MatchingSystem(MAX_DIGIT_NUMBER);
 
 	public static void main(String[] args) {
-
-		final Map<Integer, Integer> computerNumber = iInitializer.initComputerNumber();
-
+		final Map<Integer, Integer> computerNumber = initializer.initComputerNumber();
 		boolean gameOver = false;
 		while (!gameOver) {
 			gameOver = runningGameStage(computerNumber);
@@ -24,19 +27,31 @@ public class NumberBaseBallGame {
 	 * 게임 스테이지를 진행
 	 */
 	private static boolean runningGameStage(final Map<Integer, Integer> computerNumber) {
-		final Scanner scanner = new Scanner(System.in);
-
 		System.out.print("숫자를 입력해주세요 : ");
 		try {
-			final int[] ints = iInitializer.initPlayerNumber(scanner.nextInt());
+			final BallCount ballCount = matchingSystem.match(
+				computerNumber, initializer.initPlayerNumber(new Scanner(System.in).nextInt()));
+			printBallCount(ballCount);
+			return ballCount.getStrike() == 3;
 		} catch (InvalidValueException | InputMismatchException e) {
 			System.out.println("유효하지 않은 숫자입니다!");
 			return false;
-		} catch (Exception e) {
-			System.out.println("게임이 비정상적으로 종료되었습니다.");
-			return true;
 		}
+	}
 
-		return false;
+	/**
+	 * 볼 카운트 출력
+	 */
+	private static void printBallCount(final BallCount ballCount) {
+		if (ballCount.getStrike() == 0 && ballCount.getBall() == 0) {
+			System.out.println("낫싱");
+			return;
+		}
+		StringBuilder stringBuilder = new StringBuilder();
+		if (ballCount.getStrike() != 0)
+			stringBuilder.append(ballCount.getStrike()).append(" 스트라이크 ");
+		if (ballCount.getBall() != 0)
+			stringBuilder.append(ballCount.getStrike()).append(" 볼");
+		System.out.println(stringBuilder.toString().trim());
 	}
 }
