3f5881d9d52db73078506f81f5791039b89499d2
ohgillwhan <soora_genius@naver.com>
2021-04-26 22:41:38 +0900

    refact : Game에 있는 재활용이 없는 errorMessage 제거

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -17,9 +17,7 @@ public class Game {
 
     public GameRules match(Balls balls) {
         if(balls.size() != BALL_SIZE) {
-            String errorMessage = String.format("매칭할 공의 개수는 %d개 여야 합니다.", BALL_SIZE);
-
-            throw new IllegalArgumentException(errorMessage);
+            throw new IllegalArgumentException(String.format("매칭할 공의 개수는 %d개 여야 합니다.", BALL_SIZE));
         }
 
         return computerBalls.match(balls);
