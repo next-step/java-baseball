f7925f8dba2a5c415cc7b84793d3d68e1550fa61
Jeong Eunwoo <zenic88@naver.com>
2021-04-26 14:15:01 +0900

    refactor: 기능 분리 및 중복 제거
    1. 메서드 기능에 맞게 수정
    2. 입력 받는 메서드를 질문을 동적으로 처리할 수 있도록 수정

--- src/main/java/com/zenic88/baseball/Game.java
+++ src/main/java/com/zenic88/baseball/Game.java
@@ -13,33 +13,30 @@ public class Game {
 
 		List<Integer> computerNumber = computer.generate();
 
-		Score score = new Score();
-		while (!referee.isEnd(score)) {
-			score = new Score();
-			System.out.print("숫자를 입력해주세요 : ");
-
-			String userNumber = inputValue();
-
-			throwBall(score, referee, computerNumber, userNumber);
-		}
+		Score score;
+		do {
+			score = throwBall(referee, computerNumber, inputValue("숫자를 입력해주세요 : "));
+			System.out.println(score);
+		} while (!referee.isEnd(score));
 	}
 
-	public void throwBall(Score score, Referee referee, List<Integer> computerNumber, String userNumber) {
+	public Score throwBall(Referee referee, List<Integer> computerNumber, String userNumber) {
+		Score score = new Score();
 		String[] strArr = userNumber.split("");
 		for (int i = 0; i < strArr.length; i++) {
 			referee.check(score, computerNumber, Integer.parseInt(strArr[i]), i);
 		}
-		System.out.println(score);
+		return score;
 	}
 
-	public String inputValue() throws IOException {
+	public String inputValue(String question) throws IOException {
+		System.out.print(question);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		return br.readLine();
 	}
 
 	public boolean gameRepeat() throws IOException {
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-		return inputValue().equals("1");
+		return inputValue("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n").equals("1");
 	}
 }
