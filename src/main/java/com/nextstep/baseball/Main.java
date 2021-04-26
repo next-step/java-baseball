c78d8a19cf92ebfd00c86aa6c0ba66a2da07894b
nahosung <skghtjd10@naver.com>
2021-04-26 19:05:48 +0900

    feat(Main) : 상대방(컴퓨터) 3자리 수 생성
    - 1에서 9까지 수
    - 중복되지 않는 수
    - 3자리 수

---
+++ src/main/java/com/nextstep/baseball/Main.java
@@ -0,0 +1,37 @@
+package com.nextstep.baseball;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static String autoNum = ""; //컴퓨터가 선택한 3개의 숫자
+	public static String ballNum = ""; //사용자 입력한 3개의 숫자
+	public static int strikeCnt = 0; //스트라이크 카운트
+	public static int ballCnt = 0; //볼 카운트
+	
+	
+	public static void main(String[] args) {
+		//컴퓨터가 선택한 3개의 숫자 생성 메서드 & 최초 실행
+		autoNumCreate();
+	}
+	
+	//1~9 사이의 중복되지 않는 랜덤한 3개의 숫자 생성 메서드
+	public static void autoNumCreate() {
+		autoNum = "";
+		List<Integer> numList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+		
+		Collections.shuffle(numList);
+	    StringBuilder sb = new StringBuilder(3);
+	    for (Integer num : numList.subList(0, 3)) {
+	        sb.append(num);
+	    }
+
+	    autoNum = sb.toString();
+	    
+	}
+
+}
