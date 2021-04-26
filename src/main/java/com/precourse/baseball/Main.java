293073782c4e9233944a62dbb54ac3e60f8bcde8
nakyoung <nacoon53@gmail.com>
2021-04-27 00:46:07 +0900

    수를 입력 받아 배열에 넣는 메소드 추가

--- src/main/java/com/precourse/baseball/Main.java
+++ src/main/java/com/precourse/baseball/Main.java
@@ -1,10 +1,13 @@
 package com.precourse.baseball;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 	
 	public final static int DIGIT_COUNT = 3; //3자리 수 
+	
+	Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 	}
@@ -30,5 +33,22 @@ public class Main {
 		
 		return digitNum;
 	}
+	
+	// 2) 수를 입력 받아 배열에 넣는다.
+		public int[] getInputDigitNum(Scanner scan) {
+			int digitNum[] = new int[DIGIT_COUNT]; //사용자가 입력한 숫자를 저장할 배열
+			
+			System.out.print("숫자를 입력해주세요 : ");
+			int inputNum = scan.nextInt(); //사용자로부터 입력받은 숫자		
+			
+			int j = 0;
+			for(int i=DIGIT_COUNT-1; i>=0; i--) {
+				digitNum[j] = (int) (inputNum / Math.pow(10, i)); ///j번째 자리 수 구하기
+				inputNum = (int) (inputNum - (digitNum[j] * Math.pow(10, i))); //다음 자리수를 구하기 위해 구한 자리 값의 크기만큼 빼주기
+				j++;
+			}
+			
+			return digitNum;		
+		}
 
 }
--- src/test/java/com/precourse/baseball/MainTest.java
+++ src/test/java/com/precourse/baseball/MainTest.java
@@ -1,5 +1,7 @@
 package com.precourse.baseball;
 
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class MainTest {
@@ -11,6 +13,20 @@ public class MainTest {
 		int[] result = mainObj.getRandomDigitNum();
 		
 		System.out.print("랜덤한 서로 다른 수 출력: ");
+		for(int i=0; i<result.length; i++) {
+			System.out.print(result[i]);
+		}
+		
+		System.out.println("");
+	}
+	
+	@Test
+	public void testInputDigitNum(){
+		//입력받은 수를 배열로 출력
+		Main mainObj = new Main();
+		Scanner scan = new Scanner(System.in);
+		int[] result = mainObj.getInputDigitNum(scan);
+		
 		for(int i=0; i<result.length; i++) {
 			System.out.print(result[i]);
 		}
