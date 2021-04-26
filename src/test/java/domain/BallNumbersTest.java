0cde8ffe5a821e221e70dd572708ef2c1ea95575
right42 <chjm452@naver.com>
2021-04-26 20:57:36 +0900

    test: BallNumbers 테스트 코드 추가
    비교 테스트 추가
    신규 생성자 메소드 테스트 코드 추가

--- src/test/java/domain/BallNumbersTest.java
+++ src/test/java/domain/BallNumbersTest.java
@@ -2,9 +2,13 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import me.right42.domain.BallMatchResult;
 import me.right42.domain.BallNumbers;
 import me.right42.exception.BallNumberDuplicationException;
 
@@ -14,19 +18,56 @@ class BallNumbersTest {
 	@DisplayName("잘못된 볼사이즈 입력 테스트")
 	void ballSizeValidateTest(){
 		String value = "12";
+		List<Integer> numbers = Arrays.asList(1, 2);
+		String message = "볼 사이즈는 3개 입니다.";
+
 
 		assertThatThrownBy(() -> BallNumbers.create(value))
 			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("볼 사이즈는 3개입니다.");
+			.hasMessage(message);
+
+
+		assertThatThrownBy(() -> BallNumbers.create(numbers))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage(message);
 	}
 
 	@Test
 	@DisplayName("중복된 값 입력 테스트")
-	void validateThreeBallTest(){
+	void validateBallNumbersTest(){
 		String value = "122";
+		List<Integer> numbers = Arrays.asList(1, 2, 2);
+		String message = "볼 값은 중복불가 입니다.";
 
 		assertThatThrownBy(() -> BallNumbers.create(value))
 			.isInstanceOf(BallNumberDuplicationException.class)
-			.hasMessage("볼 값은 중복불가 입니다.");
+			.hasMessage(message);
+
+		assertThatThrownBy(() -> BallNumbers.create(numbers))
+			.isInstanceOf(BallNumberDuplicationException.class)
+			.hasMessage(message);
+	}
+
+	@Test
+	@DisplayName("같은 BallNumbers compareTo 테스트")
+	void compareToTest(){
+		List<Integer> numbers = Arrays.asList(1, 2, 3);
+		BallNumbers ballNumbers1 = BallNumbers.create(numbers);
+		BallNumbers ballNumbers2 = BallNumbers.create(numbers);
+
+		BallMatchResult ballMatchResult = ballNumbers1.compareTo(ballNumbers2);
+
+		assertThat(ballMatchResult.isGameComplete()).isTrue();
+	}
+
+	@Test
+	@DisplayName("다른 BallNumbers compareTo 테스트")
+	void anotherCompareToTest(){
+		BallNumbers ballNumbers1 = BallNumbers.create(Arrays.asList(1, 2, 3));
+		BallNumbers ballNumbers2 = BallNumbers.create(Arrays.asList(1, 2, 4));
+
+		BallMatchResult ballMatchResult = ballNumbers1.compareTo(ballNumbers2);
+
+		assertThat(ballMatchResult.isGameComplete()).isFalse();
 	}
 }
