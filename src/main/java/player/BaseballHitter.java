f938080da15a5af1e61d1299ff50e5efa97dbc5e
ever0de <jiseok.dev@gmail.com>
2021-04-26 23:04:37 +0900

    fix(Hitter): initial numbers의 길이는 수정하지 않는다.

--- src/main/java/player/BaseballHitter.java
+++ src/main/java/player/BaseballHitter.java
@@ -10,18 +10,19 @@ public class BaseballHitter {
     private static final int MAXIMUM_NUMBER = 9;
     private static final int SIZE = 3;
 
+    private static final List<Integer> initNumbers = new ArrayList<>();
     private List<Integer> numbers = new ArrayList<>();
 
     public BaseballHitter() {
         for (int i = MINIMUM_NUMBER; i <= MAXIMUM_NUMBER; i++) {
-            numbers.add(i);
+            initNumbers.add(i);
         }
         hittingBall();
     }
 
-    public void hittingBall() {
-        Collections.shuffle(numbers);
-        this.numbers = numbers.subList(0, SIZE);
+    private void hittingBall() {
+        Collections.shuffle(initNumbers);
+         numbers = initNumbers.subList(0, SIZE);
     }
 
     public List<Integer> getNumbers() {
