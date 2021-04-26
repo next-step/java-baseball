987e997893d8f63f2df3a3305c2a98111bbc3847
이유진 <ujin@deali.net>
2021-04-26 21:55:41 +0900

    feat: 1부터 9까지의 리스트 내부 인덱스중 랜덤한 숫자 한개를 반환

--- src/main/java/ComputerNum.java
+++ src/main/java/ComputerNum.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ComputerNum {
     private static final Integer START = 1;
     private static final Integer END = 10;
+
 
     public List<Integer> setDefaultNum(){
         List<Integer> defaultNum = new ArrayList<>();
@@ -12,5 +12,10 @@ public class ComputerNum {
             defaultNum.add(i);
         }
         return defaultNum;
+    }
+
+    public static Integer pickRandomNum(List<Integer> defaultNum){
+        double random = Math.random();
+        return (int)(random * defaultNum.size());
     }
 }
