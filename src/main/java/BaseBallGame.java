121ad703d8efd9fbfc2c68a3cf5e69c114d4e846
Darron Park <darron1217@gmail.com>
2021-04-27 00:59:13 +0900

    feat: 컴퓨터 답안(랜덤숫자) 생성 구현

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class BaseBallGame {
     char[] answer = new char[3];
+
+    /**
+     * 컴퓨터 답안 생성
+     * (중복되지 않는 1~9 사이의 숫자 3자리)
+     */
+    protected void generateAnswer() {
+        List<Character> numbers = Arrays.asList('1','2','3','4','5','6','7','8','9');
+        Collections.shuffle(numbers);
+        answer[0] = numbers.get(0);
+        answer[1] = numbers.get(1);
+        answer[2] = numbers.get(2);
+    }
 
     /**
      * 게임 스타트
