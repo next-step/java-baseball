29210ad694b267de980649b2b22e382c9504c317
jongjoo <jjongju8790@gmail.com>
2021-04-26 22:20:16 +0900

    feat: 포볼 기능 추가.

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -56,6 +56,59 @@ public class BaseballTest {
         assertThat(ball).isEqualTo(1);
     }
 
+    @DisplayName("볼 스트라이크 기능 테스트. ")
+    @Test
+    public void 볼_스트라이크_기능_테스트(){
+
+        String custNums = "245";
+        StringBuffer answer = new StringBuffer();
+
+        // 스트라이크 카운트
+        for(int i = 0 ; i < computerNumber.length; i++){
+            String tmp = String.valueOf(custNums.charAt(i));
+            strike += getEqual(computerNumber[i], Integer.parseInt(tmp));
+        }
+        // 볼 카운트
+        for(int i = 0 ; i < computerNumber.length; i++){
+            getBall(i, computerNumber[i], custNums);
+        }
+
+        // 출력
+        if(strike > 0 ){
+            answer.append(strike + "스트라이크 ");
+        }
+        if(ball > 0 ){
+            answer.append(ball + "볼");
+        }
+
+        assertThat(answer).contains("1스트라이크 2볼");
+    }
+
+    @DisplayName("포볼 또는 낫싱 테스트. ")
+    @Test
+    public void 포볼_기능_테스트(){
+
+        String custNums = "987";
+        StringBuffer answer = new StringBuffer();
+
+        // 스트라이크 카운트
+        for(int i = 0 ; i < computerNumber.length; i++){
+            String tmp = String.valueOf(custNums.charAt(i));
+            strike += getEqual(computerNumber[i], Integer.parseInt(tmp));
+        }
+        // 볼 카운트
+        for(int i = 0 ; i < computerNumber.length; i++){
+            getBall(i, computerNumber[i], custNums);
+        }
+
+        // 출력
+        if(strike == 0 && ball == 0){
+            answer.append("포볼");
+        }
+
+        assertThat(answer).contains("포볼");
+    }
+
     /*
         같으면 return 1 , 아니면 return 0
      */
