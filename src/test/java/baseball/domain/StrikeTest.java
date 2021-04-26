266ad9604d19f16928f9bb19bca6676712967366
shkwon <oper912@naver.com>
2021-04-26 15:19:56 +0900

    test: Strike 객체 전부 스트라이크, 스트라이크없음 판단 테스트 추가

--- src/test/java/baseball/domain/StrikeTest.java
+++ src/test/java/baseball/domain/StrikeTest.java
@@ -44,4 +44,42 @@ class StrikeTest {
         // then
         assertThat(strike.getCount()).isEqualTo(0);
     }
+
+    @DisplayName("3스트라이크 일때 모두 스트라이크인지 물어보면 true 반환, 그렇지 않을 경우 false를 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,true", "1,2,4,false", "1,4,5,false", "4,5,6,false"})
+    void isAllStrike(int firstNum, int secondNum, int thirdNum, boolean expected) {
+        // given
+        Answer answer = Answer.from(new TestAnswerGenerator());
+        List<BaseballNumber> baseballNumbers = new ArrayList<>();
+        baseballNumbers.add(BaseballNumber.from(firstNum));
+        baseballNumbers.add(BaseballNumber.from(secondNum));
+        baseballNumbers.add(BaseballNumber.from(thirdNum));
+
+        // when
+        Strike strike = Strike.newInstance();
+        strike.countStrike(answer, baseballNumbers);
+
+        // then
+        assertThat(strike.isAllStrike()).isEqualTo(expected);
+    }
+
+    @DisplayName("0스트라이크 일때 nothing 인지 물어보면 true를 반환, 그렇지 않을 경우 false를 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,false", "1,2,4,false", "1,4,5,false", "4,5,6,true"})
+    void isNothing(int firstNum, int secondNum, int thirdNum, boolean expected) {
+        // given
+        Answer answer = Answer.from(new TestAnswerGenerator());
+        List<BaseballNumber> baseballNumbers = new ArrayList<>();
+        baseballNumbers.add(BaseballNumber.from(firstNum));
+        baseballNumbers.add(BaseballNumber.from(secondNum));
+        baseballNumbers.add(BaseballNumber.from(thirdNum));
+
+        // when
+        Strike strike = Strike.newInstance();
+        strike.countStrike(answer, baseballNumbers);
+
+        // then
+        assertThat(strike.isNothing()).isEqualTo(expected);
+    }
 }
