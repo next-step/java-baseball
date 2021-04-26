13666c4f443a9e61769049c44ae58d5600a90955
락원 <geekseong@gmail.com>
2021-04-27 01:05:25 +0900

    refactor(Computer): 스트라이크, 볼 판정구문 람다 매서드로 분리

--- src/main/java/basballgame/game/Computer.java
+++ src/main/java/basballgame/game/Computer.java
@@ -1,5 +1,7 @@
 package basballgame.game;
 
+import java.util.function.Supplier;
+
 public class Computer {
 
 	private final BaseballSet answer;
@@ -14,34 +16,26 @@ public class Computer {
 		return new BaseballResult(strike, ball);
 	}
 
-	private int getStrikeCountFrom(BaseballSet userInputBaseballSet) {
+	private int getStrikeCountFrom(BaseballSet userInputSet) {
 		int count = 0;
-
-		if (this.answer.getFirst() == userInputBaseballSet.getFirst())
-			count++;
-
-		if (this.answer.getSecond() == userInputBaseballSet.getSecond())
-			count++;
-
-		if (this.answer.getThird() == userInputBaseballSet.getThird())
-			count++;
-
+		count = isStrike(answer::getFirst, userInputSet::getFirst) 	 ? count + 1 : count;
+		count = isStrike(answer::getSecond, userInputSet::getSecond) ? count + 1 : count;
+		count = isStrike(answer::getThird, userInputSet::getThird)   ? count + 1 : count;
 		return count;
 	}
 
-	private  int getBallCountFrom(BaseballSet userInputBaseballSet) {
+	private  int getBallCountFrom(BaseballSet userInputSet) {
 		int count = 0;
-		if (this.answer.getSecond() == userInputBaseballSet.getFirst() || this.answer.getThird() == userInputBaseballSet.getFirst())
-			count++;
-
-		if (this.answer.getFirst() == userInputBaseballSet.getSecond() || this.answer.getThird() == userInputBaseballSet.getSecond())
-			count++;
-
-		if (this.answer.getFirst() == userInputBaseballSet.getThird() || this.answer.getSecond() == userInputBaseballSet.getThird())
-			count++;
-
+		count = isBall(userInputSet::getFirst, answer::getSecond, answer::getThird) ? count + 1 : count;
+		count = isBall(userInputSet::getSecond, answer::getFirst, answer::getThird) ? count + 1 : count;
+		count = isBall(userInputSet::getThird, answer::getFirst, answer::getSecond) ? count + 1 : count;
 		return count;
 	}
+	private boolean isStrike(Supplier<Integer> answerSet, Supplier<Integer> userSet) {
+		return answerSet.get().equals(userSet.get());
+	}
 
+	private boolean isBall(Supplier<Integer> userSet, Supplier<Integer> answerSet1, Supplier<Integer> answerSet2) {
+		return userSet.get().equals(answerSet1.get()) || userSet.get().equals(answerSet2.get());
+	}
 }
-
