053146385dd4bf79caf3aab82ff86dd7a2d98540
pok3mon025 <pok3mon025@gmail.com>
2021-04-27 00:15:29 +0900

    feat : 힌트 반환 기능 구현 - 기능 목록 003 구현 - 힌트 enum 생성

--- src/main/java/baseball/BaseballApplication.java
+++ src/main/java/baseball/BaseballApplication.java
@@ -1,12 +1,15 @@
 package baseball;
 
+import baseball.utils.HintUtils;
 import baseball.utils.InputUtils;
 import baseball.utils.RandomUtils;
 
 public class BaseballApplication {
 	public static void main(String[] args) {
-		RandomUtils randomUtils = new RandomUtils();
-		int[] result = randomUtils.createRandomNumber(1, 9);
-		int[] userArray = InputUtils.getNumber(1, 9);
+		boolean success = true;
+		int[] computerNumber = RandomUtils.createRandomNumber(1, 9);
+		int[] userNumber = InputUtils.getNumber(1, 9);
+
+		success = HintUtils.isThreeStrike(userNumber, computerNumber);
 	}
 }
