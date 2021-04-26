777e747f4004350a4cef8c615eff330257850df9
kwonby <kwonby@mz.co.kr>
2021-04-26 20:43:02 +0900

    feat: 게임 숫자 관련 계산 기능  추가
    * strike, ball 계산
    * 계산에 맞는 결과 출력 메서드 추가

--- src/main/java/study/baseball/BaseballCalculator.java
+++ src/main/java/study/baseball/BaseballCalculator.java
@@ -1,31 +1,123 @@
 package study.baseball;
 
+import study.common.utils.NumberUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static study.common.Constants.BASEBALL_NUMBER_LEN;
+import static study.common.GameMessages.*;
+
+
+/**
+ * <p> 야구 게임 관련 계산 클래스  </p>
+ *
+ * @author bo-yoon
+ * created on 2021.04.25
+ */
 public class BaseballCalculator {
 
 
+    private static int strike;
+    private static int ball;
+    private static List<Integer> arrComputerNumbers = new ArrayList<>();
+    private static List<Integer> arrPlayerNumbers = new ArrayList<>();
+    private static NumberUtils numberUtils = new NumberUtils();
+
+
+    /**
+     * <p> 숫자 비교 메서드 </p>
+     * 설명 : 두 수를 비교해 야구게임 결과를 반환
+     *
+     * @param computerNumber
+     * @param playerNumber
+     * @return boolean
+     */
     public static boolean compareNumber(int computerNumber, int playerNumber) {
-        //TODO
-        return true;
+
+        allocatedNum(computerNumber, playerNumber);
+
+        strike = calculateStrike();
+        ball = calculateBall();
+
+        return compareResult(strike, ball);
     }
 
-
+    /**
+     * <p> 숫자 설정 메서드 </p>
+     * 숫자를 배열로 치환 후 클래스 내에서 사용할 수 있게 할당
+     *
+     * @param computerNumber
+     * @param playerNumber
+     * @return void
+     */
     public static void allocatedNum(int computerNumber, int playerNumber) {
-        //TODO
+        arrComputerNumbers = numberUtils.convertNumberToList(computerNumber);
+        arrPlayerNumbers = numberUtils.convertNumberToList(playerNumber);
     }
 
+    /**
+     * <p> strike 계산 메서드 </p>
+     * 설명 : strike 계산
+     *
+     * @return int
+     */
     public static int calculateStrike() {
-        //TODO
-        return 1;
+        int ans = 0;
+        for (int i = 0; i < arrComputerNumbers.size(); i++) {
+            if (arrComputerNumbers.get(i) == arrPlayerNumbers.get(i)) {
+                ans++;
+            }
+        }
+        return ans;
     }
 
+    /**
+     * <p> ball 계산 메서드 </p>
+     * 설명 : ball 계산
+     *
+     * @return int
+     */
     public static int calculateBall() {
-        //TODO
-        return 1;
+        int ans = 0;
+        for (int i = 0; i < arrComputerNumbers.size(); i++) {
+            if (arrComputerNumbers.get(i) != arrPlayerNumbers.get(i)) {
+                if (!arrComputerNumbers.contains(arrPlayerNumbers.get(i))) {
+                    continue;
+                }
+                ans++;
+            }
+        }
+        return ans;
     }
 
+    /**
+     * <p> 야구 게임 결과 반환 메서드 </p>
+     * 설명 : 입력 받은 strike, ball 수로 결과 반환
+     *
+     * @param strike
+     * @param ball
+     * @return int
+     */
     public static boolean compareResult(int strike, int ball) {
-        //TODO
-        return true;
+
+        if (strike == BASEBALL_NUMBER_LEN) {
+            return true;
+        }
+
+        if (strike != 0) {
+            System.out.print(strike + STRIKE_MESSAGE);
+        }
+
+        if (ball != 0) {
+            System.out.print(ball + BALL_MESSAGE);
+        }
+
+        if (ball == 0 && strike == 0) {
+            System.out.print(NOT_THING);
+        }
+        System.out.println();
+        return false;
     }
 }
 
---
+++ src/main/java/study/common/GameMessages.java
@@ -0,0 +1,34 @@
+package study.common;
+
+/**
+ * <p> 게임 메시지 모음 </p>
+ *
+ * @author bo-yoon
+ * created on 2021.04.24
+ */
+
+public class GameMessages {
+
+    /** 숫자 입력시 메시지  */
+    public static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+    /** 유효하지 않은 숫자 메시지 */
+    public static final String INVALID_NUMBER_MESSAGE = "유효하지 않은 숫자 형식입니다. 다시 입력해주세요.";
+
+    /** 유효하지 않은 형식 메시지 */
+    public static final String INVALID_FORMAT_MESSAGE = "유효하지 않은 형식 입니다. 숫자 형식으로 입력해주세요.";
+
+    /** 종료 메시지  */
+    public static final String NUMBER_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다!"
+            + " 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n" ;
+
+    public static final String STRIKE_MESSAGE = " 스트라이크 ";
+
+    public static final String BALL_MESSAGE = "볼";
+
+    public static final String NOT_THING = "낫싱";
+
+    public static final String GAME_END = "게임종료!";
+
+
+}
