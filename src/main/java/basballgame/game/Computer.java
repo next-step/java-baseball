f285f846e9eaa0e22d16b5a54d336d0a911d53be
락원 <geekseong@gmail.com>
2021-04-27 01:08:00 +0900

    refactor(Computer): 스트라이크, 볼 판정구문 람다 매서드로 분리

--- src/main/java/basballgame/game/Computer.java
+++ src/main/java/basballgame/game/Computer.java
@@ -16,21 +16,22 @@ public class Computer {
 		return new BaseballResult(strike, ball);
 	}
 
-	private int getStrikeCountFrom(BaseballSet userInputSet) {
+	private int getStrikeCountFrom(BaseballSet userSet) {
 		int count = 0;
-		count = isStrike(answer::getFirst, userInputSet::getFirst) 	 ? count + 1 : count;
-		count = isStrike(answer::getSecond, userInputSet::getSecond) ? count + 1 : count;
-		count = isStrike(answer::getThird, userInputSet::getThird)   ? count + 1 : count;
+		count = isStrike(answer::getFirst, userSet::getFirst) 	 ? count + 1 : count;
+		count = isStrike(answer::getSecond, userSet::getSecond) ? count + 1 : count;
+		count = isStrike(answer::getThird, userSet::getThird)   ? count + 1 : count;
 		return count;
 	}
 
-	private  int getBallCountFrom(BaseballSet userInputSet) {
+	private  int getBallCountFrom(BaseballSet userSet) {
 		int count = 0;
-		count = isBall(userInputSet::getFirst, answer::getSecond, answer::getThird) ? count + 1 : count;
-		count = isBall(userInputSet::getSecond, answer::getFirst, answer::getThird) ? count + 1 : count;
-		count = isBall(userInputSet::getThird, answer::getFirst, answer::getSecond) ? count + 1 : count;
+		count = isBall(userSet::getFirst, answer::getSecond, answer::getThird) ? count + 1 : count;
+		count = isBall(userSet::getSecond, answer::getFirst, answer::getThird) ? count + 1 : count;
+		count = isBall(userSet::getThird, answer::getFirst, answer::getSecond) ? count + 1 : count;
 		return count;
 	}
+
 	private boolean isStrike(Supplier<Integer> answerSet, Supplier<Integer> userSet) {
 		return answerSet.get().equals(userSet.get());
 	}
