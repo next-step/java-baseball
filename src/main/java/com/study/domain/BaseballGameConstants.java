97fae262a84f548a41996f085a52b5c6d87ba6fb
Jaedoo <dlwoen9@godo.co.kr>
2021-04-26 23:38:11 +0900

    test: 3자리 난수 중복 글자 테스트

---
+++ src/main/java/com/study/domain/BaseballGameConstants.java
@@ -0,0 +1,13 @@
+package com.study.domain;
+
+/**
+ * @author Jaedoo Lee
+ */
+public class BaseballGameConstants {
+    public final static int NUMBER_LENGTH = 3;
+    public final static String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public final static String GAME_CONTINUE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public final static String GAME_STRIKE = "스트라이크";
+    public final static String GAME_BALL = "볼";
+    public final static String COUNT_NOTHING = "낫싱";
+}
