7261a411c56de417eb86d6be47da884ad3047317
jangpanda <javapanda@hanmail.net>
2021-04-27 05:37:58 +0900

    feat : view 메시지 출력 클래스 추가

---
+++ src/main/java/com/tistory/javapanda/baseball/view/BaseBallView.java
@@ -0,0 +1,14 @@
+package com.tistory.javapanda.baseball.view;
+
+import com.tistory.javapanda.baseball.enums.BaseballMessage;
+
+public class BaseBallView {
+
+    public void inputNumbersMessage() {
+        System.out.println(BaseballMessage.INPUT_MESSAGE);
+    }
+
+    public void finishMessage() {
+        System.out.println(BaseballMessage.FINISH_MESSAGE);
+    }
+}
