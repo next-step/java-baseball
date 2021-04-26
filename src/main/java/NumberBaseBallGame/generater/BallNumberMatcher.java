428c2236dc070829f1c12367a29cbd04ce1dec61
accidentlywoo <accidentlywoo@gmail.com>
2021-04-26 16:03:05 +0900

    feat(3.게임) : 행위 분리. 게임생성 / 숫자 매칭 기능 개발
    게임생성.
    숫자 매칭 기능 호출부

--- src/main/java/NumberBaseBallGame/generater/BallNumberMatcher.java
+++ src/main/java/NumberBaseBallGame/generater/BallNumberMatcher.java
@@ -1,22 +1,42 @@
 package NumberBaseBallGame.generater;
 
+import NumberBaseBallGame.domain.Ball;
 import NumberBaseBallGame.domain.BallNumberMatchResult;
-import NumberBaseBallGame.domain.BaseBallConst;
+
+import java.util.List;
 
 public class BallNumberMatcher {
-	private final int GAMENUMBERS_LENGTH= BaseBallConst.GAMENUMBERS_LENGTH;
 
-	public BallNumberMatchResult matchNumber(){
+	public BallNumberMatchResult matchNumber(List<Ball> clientBalls, List<Ball> targetBalls){
 		return new BallNumberMatchResult(
-				matchStrike()
-				,matchBall()
+				matchStrike(clientBalls, targetBalls)
+				,matchBall(clientBalls, targetBalls)
 		);
 	}
-	private int matchStrike(){
-		return 0;
+	private int matchStrike(List<Ball> clientBalls, List<Ball> targetBalls){
+		int gameLength = targetBalls.size();
+		int strikeCount = 0;
+		for(int i = 0; i < gameLength; i++){
+			strikeCount = targetBalls.get(i).equals(clientBalls.get(i)) ? ++strikeCount :strikeCount;
+		}
+		return strikeCount;
 	}
 
-	private int matchBall(){
-		return 0;
+	private int matchBall(List<Ball> clientBalls, List<Ball> targetBalls){
+		int gameLength = targetBalls.size();
+		int ballCount = 0;
+		for(int i = 0; i < gameLength; i++){
+			ballCount = containButNotEqual(i, clientBalls.get(i), targetBalls) ? ++ballCount :ballCount;
+		}
+		return ballCount;
 	}
+	private boolean containButNotEqual(int index,Ball ball, List<Ball> targetBalls){
+		boolean contains = targetBalls.contains(ball);
+		if (contains){
+			int indexOf = targetBalls.indexOf(ball);
+			return indexOf!= index;
+		}
+		return false;
+	}
+
 }
---
+++ src/main/java/NumberBaseBallGame/generater/GenerateGame.java
@@ -0,0 +1,65 @@
+package NumberBaseBallGame.generater;
+
+import NumberBaseBallGame.domain.BallNumberMatchResult;
+import NumberBaseBallGame.domain.BaseBallConst;
+import NumberBaseBallGame.domain.Ball;
+import NumberBaseBallGame.domain.ValidationCheckUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateGame {
+	private final List<Ball> randomBalls;
+	private final BallNumberMatcher ballNumberMatcher;
+	private final int GAMENUMBERS_LENGTH= BaseBallConst.GAMENUMBERS_LENGTH;
+
+	public GenerateGame() {
+		randomBalls = new GenerateGameNumber(GAMENUMBERS_LENGTH).getRandomBalls();
+		ballNumberMatcher = new BallNumberMatcher();
+	}
+
+	/**
+	 *
+	 *
+	 * @return
+	 */
+	public List<Ball> getGameNumbers() {
+		return randomBalls;
+	}
+
+	/**
+	 * 게임 시작!
+	 *  사용자 입력과 게임 값을 비교
+	 *
+	 * @param getClientNumber
+	 * @return
+	 */
+	public BallNumberMatchResult playGame(String getClientNumber){
+		ValidationCheckUtils.checkInputNumber(getClientNumber);
+
+		List<Ball> clientBalls = generateClientBalls(getClientNumber);
+
+		return checkMatch(clientBalls);
+	}
+
+	private BallNumberMatchResult checkMatch(List<Ball> clientBalls) {
+		return ballNumberMatcher.matchNumber(clientBalls, randomBalls);
+	}
+
+	/**
+	 * 고객 Ball 생성
+	 *
+	 * @param getClientNumber
+	 * @return
+	 */
+	private List<Ball> generateClientBalls(String getClientNumber) {
+		List<Ball> balls = new ArrayList<>();
+		for (char item : getClientNumber.toCharArray()){
+			Ball aBall = new Ball();
+			aBall.setValueNumber(item);
+			balls.add(aBall);
+		}
+		return balls;
+	}
+
+}
