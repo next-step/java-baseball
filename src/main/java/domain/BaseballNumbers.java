75607ce8027aecfb4401b790837252a8823e1148
HyungjuJang <dacapolife87@gmail.com>
2021-04-27 00:05:48 +0900

    refactor : BaseballNumbers 객체 생성시 Validation추가 - 숫자의 범위체크도 판단

--- src/main/java/domain/BaseballNumbers.java
+++ src/main/java/domain/BaseballNumbers.java
@@ -5,19 +5,37 @@ import java.util.List;
 public class BaseballNumbers {
 
     private static final String INVALID_BASEBALL_NUMBER_SIZE = "잘못된 베이스볼 숫자 갯수입니다.";
+    private static final String INVALID_BASEBALL_NUMBER_RANGE = "베이스볼 숫자는 1~9 사이의 정수여야합니다.";
     private static final int BASEBALL_NUMBER_SIZE = 3;
 
     private List<Integer> numbers;
 
     private BaseballNumbers() {
-
     }
 
     private BaseballNumbers(List<Integer> numbers) {
         this.numbers = numbers;
 
+        validation(numbers);
+    }
+
+    private void validation(List<Integer> numbers) {
+        checkNumbersSize(numbers);
+
+        for (int number : numbers) {
+            checkRange(number);
+        }
+    }
+
+    private void checkNumbersSize(List<Integer> numbers) {
         if (numbers.size() != BASEBALL_NUMBER_SIZE) {
             throw new IllegalArgumentException(INVALID_BASEBALL_NUMBER_SIZE);
+        }
+    }
+
+    private void checkRange(int number) {
+        if(number < 1 || number  > 9) {
+            throw new IllegalArgumentException(INVALID_BASEBALL_NUMBER_RANGE);
         }
     }
 
