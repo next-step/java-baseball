d82e0f2652f0eef72299551249883e01e7773003
Karll <hankikab@gmail.com>
2021-04-26 16:45:31 +0900

    - 정답 만들기 추가

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BaseBallGame {
@@ -5,15 +6,18 @@ public class BaseBallGame {
     Message message;
     int strike;
     int ball;
+    int[] answer;
 
     public BaseBallGame(Message message) {
         this.message = message;
         this.resetGame();
+        this.createAnswer();
     }
 
     public void resetGame() {
         this.strike = 0;
         this.ball = 0;
+        this.answer = this.createAnswer();
 
         this.setRunning(true);
     }
@@ -34,6 +38,39 @@ public class BaseBallGame {
         String input = sc.next();
 
         return input;
+    }
+
+    private int[] createAnswer() {
+        ArrayList<Integer> temp_array = new ArrayList<Integer>(){
+            {
+                add(1);
+                add(2);
+                add(3);
+                add(4);
+                add(5);
+                add(6);
+                add(7);
+                add(8);
+                add(9);
+            }
+        };
+        int max_idx = temp_array.size();
+
+        int[] answer = new int[3];
+        for (int i = 0; i < 3; i++) {
+            int remove_idx = this.getRandomNum(max_idx);
+            answer[i] = temp_array.get(remove_idx);
+            temp_array.remove(remove_idx);
+            max_idx--;
+        }
+        return answer;
+    }
+
+    private int getRandomNum(int max_idx) {
+        double random = Math.random();
+        int random_idx = (int)(random * max_idx);
+
+        return random_idx;
     }
 
     public void checkAnswer(String input) {
