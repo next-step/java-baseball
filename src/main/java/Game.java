ba4707fdbb9ed333935c789043d05b94a001cd84
cheol <mettlekc@gmail.com>
2021-04-27 00:03:37 +0900

    refactor: play method 리펙터링
    strike/ball method 추출
    strike method 중복 제거
    - getStrike / checkStrike를 분리하여 loop문에서 indent 1depth 처리
    - getStrike strike의 count를 반환
    - checkStrike strike인지 판단 후 count 증가

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -18,33 +18,37 @@ public class Game {
     }
 
     public Score play(int numbers) {
-
-        int strike = 0;
-        int ball = 0;
-
-        if (match(getNumber(numbers, 1), 1))
-            strike++;
-
-        if (match(getNumber(numbers, 2), 2))
-            strike++;
-
-        if (match(getNumber(numbers, 3), 3))
-            strike++;
-
-        if (match(getNumber(numbers, 1), 2) || match(getNumber(numbers, 1), 3))
-            ball++;
-
-        if (match(getNumber(numbers, 2), 1) || match(getNumber(numbers, 2), 3))
-            ball++;
-
-        if (match(getNumber(numbers, 3), 1) || match(getNumber(numbers, 3), 2))
-            ball++;
+        int strike = getStrike(numbers);
+        int ball = getBall(numbers);
 
         if (strike == 0 && ball == 0)
             return new Score(strike, 4);
 
         return new Score(strike, ball);
+    }
 
+    private int getBall(int numbers) {
+        int ball = 0;
+        if (match(getNumber(numbers, 1), 2) || match(getNumber(numbers, 1), 3))
+            ball++;
+        if (match(getNumber(numbers, 2), 1) || match(getNumber(numbers, 2), 3))
+            ball++;
+        if (match(getNumber(numbers, 3), 1) || match(getNumber(numbers, 3), 2))
+            ball++;
+        return ball;
+    }
+
+    private int getStrike(int numbers) {
+        int strike = 0;
+        for (int i = 1; i <= 3; i++)
+            strike = checkStrike(numbers, strike, i);
+        return strike;
+    }
+
+    private int checkStrike(int numbers, int count, int pos) {
+        if (getNumber(numbers, pos) == getNumber(opponent, pos))
+            count++;
+        return count;
     }
 
     private int getNumber(int numbers, int pos) {
@@ -58,13 +62,7 @@ public class Game {
     }
 
     private boolean match(int number, int pos) {
-        if (pos == 3)
-            return number == getNumber(opponent, 3);
-        else if (pos == 2)
-            return number == getNumber(opponent ,2);
-        else if (pos == 1)
-            return number == getNumber(opponent, 1);
-        throw new IllegalPositionException();
+        return number == getNumber(opponent, pos);
     }
 
 }
