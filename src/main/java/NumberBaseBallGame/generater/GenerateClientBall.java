7821f1bb257be4bb5f7f6b6e4228a026cae260a5
accidentlywoo <accidentlywoo@gmail.com>
2021-04-26 23:00:59 +0900

    feat : create GenerateClientBall
    클라이언트가 선택한 공 뽑기 메소드 정의

---
+++ src/main/java/NumberBaseBallGame/generater/GenerateClientBall.java
@@ -0,0 +1,23 @@
+package NumberBaseBallGame.generater;
+
+import NumberBaseBallGame.domain.Ball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateClientBall {
+	private final int gameBallLength;
+
+	public GenerateClientBall(int gameBallLength) {
+		this.gameBallLength = gameBallLength;
+	}
+
+	public List<Ball> generateBall(String getClientNumber){
+		List<Ball> balls = new ArrayList<>();
+		for (char item : getClientNumber.toCharArray()){
+			Ball aBall = new Ball(Integer.parseInt(String.valueOf(item)));
+			balls.add(aBall);
+		}
+		return balls;
+	}
+}
