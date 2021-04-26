cfc53767174a9a21054857c5c2be37426cfb113d
pok3mon025 <pok3mon025@gmail.com>
2021-04-27 00:27:20 +0900

    refactor : 기호 상수로 치환 - min, max 값 기호 상수로 치환

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -6,19 +6,21 @@ import baseball.utils.InputUtils;
 import baseball.utils.RandomUtils;
 
 public class BaseballService {
+	private static final int MIN_VALUE = 1;
+	private static final int MAX_VALUE = 9;
 
 	public BaseballService() {
 	}
 
 	public int[] createRandomNumber() {
-		return RandomUtils.createRandomNumber(1, 9);
+		return RandomUtils.createRandomNumber(MIN_VALUE, MAX_VALUE);
 	}
 
 	public GameStatus playGame(int[] computerNumber) {
 		boolean isContinue = true;
 
 		while (isContinue) {
-			int[] userNumber = InputUtils.getNumber(1, 9);
+			int[] userNumber = InputUtils.getNumber(MIN_VALUE, MAX_VALUE);
 			isContinue = !HintUtils.isThreeStrike(userNumber, computerNumber);
 		}
 
