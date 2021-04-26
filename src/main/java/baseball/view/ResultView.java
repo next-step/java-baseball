b37c21f7c042ee615ceefc7be81b5d3737ab44c7
juniq <juniqlim@gmail.com>
2021-04-26 17:45:22 +0900

    feat: 출력 - 스트라이크, 볼이 없으면 '낫싱' 출력

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -8,6 +8,9 @@ public class ResultView {
         if (ballCount != 0) {
             System.out.print(ballCount + " 볼");
         }
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.print("낫싱");
+        }
         if (strikeCount == 3) {
             System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
