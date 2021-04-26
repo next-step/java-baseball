2bc2ed20a40a37fde8912d99903411fa10a9554b
apdkeb <apdkeb@naver.com>
2021-04-26 12:19:31 +0900

    feat(NumberBall) : 범위 예외 추가
    2021. 04. 26.
    1. NumberBall
      - 범위 예외 추가
    
    Resolves : loop-study
    See also : .

--- src/main/java/domain/NumberBall.java
+++ src/main/java/domain/NumberBall.java
@@ -1,13 +1,25 @@
 package domain;
 
 public class NumberBall {
-    private final int ball;
+    private static final String RANGE_ERROR = "숫자범위는 1 ~ 9 입니다.";
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
 
-    public NumberBall(int ball) {
-        this.ball = ball;
+    private final int numberBall;
+
+    public NumberBall(int number) {
+        validRange(number);
+        this.numberBall = number;
+    }
+
+    private void validRange(int number) {
+        if (number < MIN_NUMBER
+                || MAX_NUMBER < number) {
+            throw new IllegalArgumentException(RANGE_ERROR);
+        }
     }
 
     public int toInt() {
-        return ball;
+        return numberBall;
     }
 }
