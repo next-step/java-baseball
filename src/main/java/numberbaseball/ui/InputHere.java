f513e5c42e4608b31b41693ad43166e1a05eb968
ted.kang <ted.kang@kakaocorp.com>
2021-04-26 23:26:51 +0900

    refactor: InputHere 클래스 리팩토링
    매직넘버제거

--- src/main/java/numberbaseball/ui/InputHere.java
+++ src/main/java/numberbaseball/ui/InputHere.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class InputHere {
 
+  private static final int MEANING_CONTINUE = 1;
   private static final String PLEASE_INPUT_NUMBERS = "숫자를 입력주세요 : ";
   private static final String INPUT_SHOULD_INTEGER = "자연수로 입력 해 주세요.";
   private static final String PLEASE_INPUT_CONTINUE_FLAG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
@@ -31,6 +32,6 @@ public class InputHere {
 
   public static boolean getContinueFlag() {
     System.out.println(PLEASE_INPUT_CONTINUE_FLAG);
-    return getIntValue() == 1;
+    return getIntValue() == MEANING_CONTINUE;
   }
 }
