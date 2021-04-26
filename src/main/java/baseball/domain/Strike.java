a219763862292b816b7fbfdf93fa9759e166760d
shkwon <oper912@naver.com>
2021-04-26 13:05:20 +0900

    refactor: 디미터의 법칙 어긋나는 부분 수정

--- src/main/java/baseball/domain/Strike.java
+++ src/main/java/baseball/domain/Strike.java
@@ -19,7 +19,7 @@ public class Strike {
 
     public void countStrike(final Answer answer, final List<BaseballNumber> baseballNumbers) {
         for (int i = 0; i < Constants.BASEBALL_NUMBER_SIZE; i++) {
-            plusCount(answer.getAnswer().get(i), baseballNumbers.get(i));
+            plusCount(answer.getAnswer(i), baseballNumbers.get(i));
         }
     }
 
