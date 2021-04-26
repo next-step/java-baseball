84aa8c7192454e86598b49d554a1a4b4bf2bc8d3
ted.kang <ted.kang@kakaocorp.com>
2021-04-26 23:21:14 +0900

    feat: InputHere UI 클래스 작성
    한 게임이 종료되었을때 다음 게임을 시작할 지 프로그램을 종료할지 입력받는 메서드 구현

--- src/main/java/numberbaseball/ui/InputHere.java
+++ src/main/java/numberbaseball/ui/InputHere.java
@@ -9,6 +9,7 @@ public class InputHere {
 
   private static final String PLEASE_INPUT_NUMBERS = "숫자를 입력주세요 : ";
   private static final String INPUT_SHOULD_INTEGER = "자연수로 입력 해 주세요.";
+  private static final String PLEASE_INPUT_CONTINUE_FLAG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
   private static final Scanner SCANNER = new Scanner(System.in);
 
   private InputHere() {}
@@ -16,7 +17,6 @@ public class InputHere {
   public static ExpectNumbers getExpectNumbers() {
     System.out.println(PLEASE_INPUT_NUMBERS);
     return new ExpectNumbers(getIntValue());
-
   }
 
   private static int getIntValue() {
@@ -29,5 +29,8 @@ public class InputHere {
     }
   }
 
-
+  public static boolean getContinueFlag() {
+    System.out.println(PLEASE_INPUT_CONTINUE_FLAG);
+    return getIntValue() == 1;
+  }
 }
