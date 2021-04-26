dfc9949bf93956277af81aad6a7949b230cb5513
이유진 <ujin@deali.net>
2021-04-27 03:10:57 +0900

    test: 입력받을 수의 숫자갯수 체크 TEST
    입력받을 임의의 수가 3자리인 경우와 아닌경우 각각 true, false가 반환되는지 TEST한다

---
+++ src/test/java/InputNumTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InputNumTest {
+    @Test
+    @DisplayName("입력받은 수가 3자리가 아닐경우 false값을 반환하는지 확인한다")
+    void checkNumSizeErrorTest() {
+        String testNum = "1433";
+        Boolean checkResult = InputNum.checkNumSize(testNum);
+        assertThat(checkResult).isFalse();
+    }
+
+    @Test
+    @DisplayName("입력받은 수가 3자리가 맞을경우 true값을 반환하는지 확인한다")
+    void checkNumSizeTest() {
+        String testNum = "143";
+        Boolean checkResult = InputNum.checkNumSize(testNum);
+        assertThat(checkResult).isTrue();
+    }
+
+}
