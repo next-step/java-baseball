d3c2528a2152fe4da2542bcbe19ece24c6a3eec6
mkhwang <orange2652@gmail.com>
2021-04-26 13:32:06 +0900

    feat(domain) : 기능정의 2-1 숫자생성

---
+++ src/main/java/me/mkhwang/baseball/domain/Computer.java
@@ -0,0 +1,50 @@
+package me.mkhwang.baseball.domain;
+
+import me.mkhwang.baseball.util.BaseballValid;
+
+import java.util.Random;
+
+/**
+ * Created by mkhwang on 2021/04/25
+ * Email : orange2652@gmail.com
+ * Github : https://github.com/myeongkwonhwang
+ */
+public class Computer {
+
+    private static final int BALL_LENGTH = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    private String ballNumber;
+    private BaseballValid baseballValid = new BaseballValid();
+
+    public Computer() {
+        makeBallCount();
+    }
+
+    private void makeBallCount() {
+        String ball = "";
+        while (ball.length() < BALL_LENGTH){
+            ball = makeBall(ball);
+        }
+        this.ballNumber = ball;
+    }
+
+    private String makeBall(String makeBall) {
+        String number = extractNumber();
+        if(baseballValid.isBaseballDuplicateValid(makeBall + number)){
+            makeBall += number;
+        }
+        return makeBall;
+    }
+
+    private String extractNumber() {
+        int i = new Random().nextInt(MAX_NUMBER) + MIN_NUMBER;
+        String number = String.valueOf(i);
+        return number;
+    }
+
+    public String getBallNumber() {
+        return ballNumber;
+    }
+}
