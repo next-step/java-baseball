cf0ce9459ca8fb8cc3226a87835ef7f48b60ca1d
seungkwon <yevgnenll@gmail.com>
2021-04-27 01:49:54 +0900

    feat: 게임을 실행하기 위한 기능 추가
    - 게임을 실행하기 위해 필요한 객체 관계 명시
    - main 함수 구현
    - 게임을 실행, 재실행

--- src/main/java/BaseballGameManager.java
+++ src/main/java/BaseballGameManager.java
@@ -10,6 +10,22 @@ public class BaseballGameManager {
 
 	public static void main(String[] args) {
 		BaseballGameManager baseballGame = new BaseballGameManager(new Questioner(), new UserInterface());
+		while (baseballGame.execute() == 1) {
+		}
+	}
+
+	public int execute() {
+		final int question = questioner.makeProblem();
+		System.out.println(question);
+		String userInput;
+		boolean retry = true;
+		while (retry) {
+			userInput = userInterface.interactUser();
+			Referee referee = new Referee(question, userInput);
+			retry = !referee.isFinish();
+			userInterface.answerTo(referee.judge());
+		}
+		return userInterface.replayOnNot();
 	}
 
 }
