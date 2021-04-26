3704fea29e2c3cc075b58c396b23bdf2b24bfa84
Shinyoung Kim <rolroralra@gmail.com>
2021-04-26 18:04:30 +0900

    style: change unit test name

--- src/test/java/baseball/model/BaseballResultTest.java
+++ src/test/java/baseball/model/BaseballResultTest.java
@@ -12,7 +12,7 @@ public class BaseballResultTest {
             "baseball.model.TestParametersProvider#provideNonPositiveSizeOrNonPositiveSizeRadix",
             "baseball.model.TestParametersProvider#provideTooMuchSizeOrTooMuchRadix"
     })
-    public void test1_baseballNumber_builder_generate_number_which_does_not_contain_0(int size, int radix) {
+    public void test1_BaseballResult_calcResult(int size, int radix) {
         BaseballNumber left = BaseballNumber.builder().size(size).radix(radix).build();
         BaseballNumber right = BaseballNumber.builder().size(size).radix(radix).build();
         BaseballResult baseballResult = BaseballResult.calcResult(left, right);
@@ -26,7 +26,6 @@ public class BaseballResultTest {
         assertThat(baseballResult.getBall()).isBetween(0, left.getSize());
         assertThat(baseballResult.getRadix()).isBetween(BaseballNumber.MIN_RADIX, BaseballNumber.MAX_RADIX);
         assertThat(baseballResult.getSize()).isBetween(BaseballNumber.MIN_RADIX - 1, BaseballNumber.MAX_RADIX - 1);
-
     }
 
     @ParameterizedTest(name = "BaseballNumber.builder().("+ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER+").build()")
@@ -35,7 +34,7 @@ public class BaseballResultTest {
             "baseball.model.TestParametersProvider#provideNonPositiveSizeOrNonPositiveSizeRadix",
             "baseball.model.TestParametersProvider#provideTooMuchSizeOrTooMuchRadix"
     })
-    public void test2_baseballNumber_builder_generate_number_which_does_not_contain_0(int size, int radix) {
+    public void test2_BaseballResult_create_failed_result(int size, int radix) {
         BaseballResult baseballResult = BaseballResult.createFailedResult();
 
         assertThat(baseballResult).isNotNull();
