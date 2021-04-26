cc6266a4ab6e0fe2392778d936af1ae695c4cd9c
Jason Park <jasonpark02@gmail.com>
2021-04-26 13:39:24 +0900

    refactor: 세 자리 난수 생성하는 기능 테스트 수정
    * assertj를 이용하도록 수정

--- src/test/java/domain/ComputerTest.java
+++ src/test/java/domain/ComputerTest.java
@@ -1,6 +1,6 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,15 +8,14 @@ class ComputerTest {
 	@Test
 	public void generateNumberTest() {
 		int genNumber = Integer.parseInt(Computer.generateNumber());
-		assertTrue(genNumber > 111 && genNumber < 999);
+		assertThat(genNumber).isBetween(111, 999);
 
-		/*
-		 * 100의 자리수는 항상 참이므로 검증할 필요가 없으며 10의 자리수와 1의 자리수는 항상 한 자리 수이므로
-		 * 0 이상인지에 대해서만 검증하면 된다
-		 */
+		int hundDigit = genNumber / 100;
 		int tensDigit = genNumber % 100 / 10;
 		int unitDigit = genNumber % 100 % 10;
-		assertTrue(tensDigit > 0);
-		assertTrue(unitDigit > 0);
+
+		assertThat(hundDigit).isGreaterThan(0).isLessThan(10);
+		assertThat(tensDigit).isGreaterThan(0).isLessThan(10);
+		assertThat(unitDigit).isGreaterThan(0).isLessThan(10);
 	}
 }
