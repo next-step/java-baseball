6ba959c73e9f66fd1872e3df3a8c4f59f17c7520
cheol <mettlekc@gmail.com>
2021-04-27 00:09:11 +0900

    test: ball 추가 단위 테스트
    ball 기능에 대한 추가 테스트
    ball/strike 통합 테스트

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -36,6 +36,22 @@ class GameTest {
         assertPlay(game.play(456), 0, 4);
     }
 
+    @Test
+    void one_ball() {
+        assertPlay(game.play(451), 0, 1);
+    }
+
+    @Test
+    void many_ball() {
+        assertPlay(game.play(431), 0, 2);
+        assertPlay(game.play(231), 0, 3);
+    }
+
+    @Test
+    void one_strike_two_ball() {
+        assertPlay(game.play(132), 1, 2);
+    }
+
     private void assertPlay(Score score, int strike, int ball) {
         assertThat(score.getStrike()).isEqualTo(strike);
         assertThat(score.getBall()).isEqualTo(ball);
