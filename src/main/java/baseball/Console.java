0583aedb1a545b66271a7a3c426128a782ee1955
cheol <mettlekc@gmail.com>
2021-04-27 02:25:18 +0900

    feat: console intput 기능
    scanner input 숫자 입력 기능

--- src/main/java/baseball/Console.java
+++ src/main/java/baseball/Console.java
@@ -1,4 +1,18 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Console {
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(scanner.nextInt());
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
--- src/test/java/ConsoleTest.java
+++ src/test/java/ConsoleTest.java
@@ -1,11 +1,31 @@
 import baseball.Console;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ConsoleTest {
 
     @Test
     void create_console() {
         Console console = new Console();
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        String input = "123";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(byteArrayInputStream);
+        console.start();
+
+        assertThat(out.toString()).hasToString("숫자를 입력해주세요 : " + 123);
     }
+
+
+
 
 }
