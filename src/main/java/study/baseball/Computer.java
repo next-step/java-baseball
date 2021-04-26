ebb66c7df09c8787f72ba7abfcb345d72424a1b6
kwonby <kwonby@mz.co.kr>
2021-04-27 01:38:44 +0900

    feat: 컴퓨터 숫자 생성 로직 추가

--- src/main/java/study/baseball/Computer.java
+++ src/main/java/study/baseball/Computer.java
@@ -1,8 +1,22 @@
 package study.baseball;
 
+
+import study.common.utils.NumberUtils;
+
+/**
+ * <p> 야구 게임 숫자 관련 클래스  </p>
+ */
 public class Computer {
+    private static NumberUtils numberUtils = new NumberUtils();
+
+    /**
+     * <p> 범위 안에서 랜덤 함수 반환 메서드 </p>
+     *
+     * @return int
+     */
     public static int generateBaseballNumber() {
-        //TODO
-        return 123;
+        int number = numberUtils.nextInt();
+
+        return number;
     }
 }
