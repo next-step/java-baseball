e1d7313e11d34bfd8968662e1a19451c1ad89824
frostnight <frostnightsong@gmail.com>
2021-04-27 01:22:34 +0900

    test: Set 클래스 Test 및 AssertJ 활용법 공부 + 커밋 메시지 수정 커밋 메시지 컨벤션 2차 적용

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -44,7 +44,6 @@ public class SetTest {
 		// assertThat(numbers.contains(1)).isTrue();
 		// assertThat(numbers.contains(2)).isTrue();
 		// assertThat(numbers.contains(3)).isTrue();
-		
 		assertTrue(numbers.contains(input));
 	}
 
