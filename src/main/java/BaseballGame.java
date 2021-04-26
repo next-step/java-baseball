87f5a4cb28efb5a955838c700a4137974a9f8011
조예찬 <choyuichan@gmail.com>
2021-04-26 11:37:14 +0900

    문서(요구사항): README.md 요구사항 내용 변경
    요구사항 상세 정의 및 체크
    파일 정렬

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -2,24 +2,24 @@ import java.util.Random;
 
 public class BaseballGame {
 
-    final static Random r = new Random();
+	final static Random r = new Random();
 
 	public String generateNumbers(){
 		return generateNumbers(null,3);
 	}
 
-    private String generateNumbers(String all,int length){
-        if(all==null) all="";
-        if(all.length() == length){
-            return all;
-        }
-        String number = generateNumberNotZero();
-        return  generateNumbers(all+(all.indexOf(number) < 0?number:""),length);
-    }
+	private String generateNumbers(String all,int length){
+		if(all==null) all="";
+		if(all.length() == length){
+			return all;
+		}
+		String number = generateNumberNotZero();
+		return  generateNumbers(all+(all.indexOf(number) < 0?number:""),length);
+	}
 
-    private String generateNumberNotZero(){
-        int number = r.nextInt(10);
-        if(number==0) return generateNumberNotZero();
-        return number+"";
-    }
+	private String generateNumberNotZero(){
+		int number = r.nextInt(10);
+		if(number==0) return generateNumberNotZero();
+		return number+"";
+	}
 }
--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -10,7 +10,7 @@ public class BaseballGameTest{
 	public void couldMakeThreeDigits(){
 		BaseballGame game = new BaseballGame();
 		String numbers = game.generateNumbers();
-        Integer intNumbers = Integer.parseInt(numbers);
+		Integer intNumbers = Integer.parseInt(numbers);
 		assertTrue(intNumbers  >= 100 && intNumbers <= 999);
 	}
 
@@ -18,34 +18,34 @@ public class BaseballGameTest{
 	public void couldMakeDigitsContainsTo1From9(){
 		BaseballGame game = new BaseballGame();
 		String numbers = game.generateNumbers();
-        assertTrue(numbers.indexOf("0") < 0);
+		assertTrue(numbers.indexOf("0") < 0);
 	}
 
-    @Test
+	@Test
 	public void couldMakeRandomDigits(){
 		BaseballGame game = new BaseballGame();
 		String first_numbers = game.generateNumbers();
 		String second_numbers = game.generateNumbers();
 		String third_numbers = game.generateNumbers();
-        assertNotEquals(first_numbers, second_numbers);
-        assertNotEquals(second_numbers, third_numbers);
-        assertNotEquals(first_numbers, third_numbers);
+		assertNotEquals(first_numbers, second_numbers);
+		assertNotEquals(second_numbers, third_numbers);
+		assertNotEquals(first_numbers, third_numbers);
 	}
 
-    @Test
+	@Test
 	public void couldMakeRandomDigitsNotDuplicated(){
 		BaseballGame game = new BaseballGame();
-        String numbers = game.generateNumbers();
-        String[] splitedNumbers = numbers.split("");
-        assertNotEquals(splitedNumbers[0], splitedNumbers[1]);
-        assertNotEquals(splitedNumbers[1], splitedNumbers[2]);
-        assertNotEquals(splitedNumbers[0], splitedNumbers[2]);
+		String numbers = game.generateNumbers();
+		String[] splitedNumbers = numbers.split("");
+		assertNotEquals(splitedNumbers[0], splitedNumbers[1]);
+		assertNotEquals(splitedNumbers[1], splitedNumbers[2]);
+		assertNotEquals(splitedNumbers[0], splitedNumbers[2]);
 
-        String numbers1 = game.generateNumbers();
-        String[] splitedNumbers1 = numbers1.split("");
-        assertNotEquals(splitedNumbers1[0], splitedNumbers1[1]);
-        assertNotEquals(splitedNumbers1[1], splitedNumbers1[2]);
-        assertNotEquals(splitedNumbers1[0], splitedNumbers1[2]);
+		String numbers1 = game.generateNumbers();
+		String[] splitedNumbers1 = numbers1.split("");
+		assertNotEquals(splitedNumbers1[0], splitedNumbers1[1]);
+		assertNotEquals(splitedNumbers1[1], splitedNumbers1[2]);
+		assertNotEquals(splitedNumbers1[0], splitedNumbers1[2]);
 	}
 
 }
