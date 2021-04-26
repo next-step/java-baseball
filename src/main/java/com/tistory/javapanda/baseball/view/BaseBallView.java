b3d8076d976f95ddaac0d5f0d3249329ea5e9ea5
jangpanda <javapanda@hanmail.net>
2021-04-27 08:02:12 +0900

    refactor : view 클래스 결과 출력 추가

--- src/main/java/com/tistory/javapanda/baseball/view/BaseBallView.java
+++ src/main/java/com/tistory/javapanda/baseball/view/BaseBallView.java
@@ -1,11 +1,36 @@
 package com.tistory.javapanda.baseball.view;
 
 import com.tistory.javapanda.baseball.enums.BaseballMessage;
+import com.tistory.javapanda.baseball.vo.BaseBallVo;
 
 public class BaseBallView {
 
     public void inputNumbersMessage() {
         System.out.println(BaseballMessage.INPUT_MESSAGE.getMessage());
+    }
+
+    public void resultMessage(BaseBallVo baseBallVo) {
+        String message = strikeMessage(baseBallVo) + ballMessage(baseBallVo);
+        if(message.length() == 0) {
+            message = "포볼";
+        }
+        System.out.println(message);
+    }
+
+    public String strikeMessage(BaseBallVo baseBallVo) {
+        String message = "";
+        if(baseBallVo.getStrike() > 0) {
+            message = baseBallVo.getStrike() + "스트라이크 ";
+        }
+        return message;
+    }
+
+    public String ballMessage(BaseBallVo baseBallVo) {
+        String message = "";
+        if(baseBallVo.getBall() > 0) {
+            message = baseBallVo.getBall() + "볼";
+        }
+        return message;
     }
 
     public void finishMessage() {
