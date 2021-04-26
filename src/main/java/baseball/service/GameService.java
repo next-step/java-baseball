67eafca3d6c13179aa189581e049f75e259464d6
kslee <meant0415@gmail.com>
2021-04-27 02:22:34 +0900

    feat: 난수와 사용자 입력 값에 대한 스트라이크, 볼의 수를 반환 (src/main/java/baseball/service, src/main/java/baseball/domain)

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,11 +1,10 @@
 package baseball.service;
 
+import baseball.domain.BallCount;
 import baseball.error.DuplicateCharactersException;
 import baseball.error.NonNumericCharactersException;
 
 import java.util.*;
-
-;
 
 public class GameService {
 
@@ -13,19 +12,19 @@ public class GameService {
 	private final int MIN_NUMBER = 1;
 	private final int MAX_NUMBER = 9;
 
-	public Set<Integer> generateNumber() {
+	public List<Integer> generateNumbers() {
 		Random random = new Random();
 		Set<Integer> numbers = new LinkedHashSet<>();
 		do {
 			numbers.add(random.nextInt(MAX_NUMBER) + MIN_NUMBER);
 		} while (numbers.size() < NUMBER_LENGTH);
-		return numbers;
+		return new ArrayList<>(numbers);
 	}
 
-	public Set<Integer> parseNumbers(String input) {
+	public List<Integer> parseNumbers(String input) {
 		validateNumericCharacters(input);
 		validateNonDuplicateCharacters(input);
-		return convertNumericStringToIntegerSet(input);
+		return convertNumericStringToIntegerList(input);
 	}
 
 	private void validateNumericCharacters(String input) {
@@ -40,12 +39,39 @@ public class GameService {
 		}
 	}
 
-	private Set<Integer> convertNumericStringToIntegerSet(String input) {
+	private List<Integer> convertNumericStringToIntegerList(String input) {
 		Set<Integer> numbers = new LinkedHashSet<>();
 		for (String s : input.split("")) {
 			numbers.add(Integer.parseInt(s));
 		}
-		return numbers;
+		return new ArrayList<>(numbers);
+	}
+
+	public BallCount compareNumbers(List<Integer> generateNumbers, List<Integer> parseNumbers) {
+		int strike = getStrike(generateNumbers, parseNumbers);
+		int ball = getBall(generateNumbers, parseNumbers);
+		return new BallCount(strike, ball);
+	}
+
+	private int getStrike(List<Integer> generateNumbers, List<Integer> parseNumbers) {
+		int strike = 0;
+		for (int i = 0, max = generateNumbers.size(); i < max; i++) {
+			int generateNumber = generateNumbers.get(i);
+			int parseNumber = parseNumbers.get(i);
+			strike += generateNumber == parseNumber ? 1 : 0;
+		}
+		return strike;
+	}
+
+	private int getBall(List<Integer> generateNumbers, List<Integer> parseNumbers) {
+		int ball = 0;
+		for (int i = 0, max = generateNumbers.size(); i < max; i++) {
+			int generateNumber = generateNumbers.get(i);
+			int parseNumber = parseNumbers.get(i);
+			boolean contains = parseNumbers.contains(generateNumber);
+			ball += contains && generateNumber != parseNumber ? 1 : 0;
+		}
+		return ball;
 	}
 
 }
