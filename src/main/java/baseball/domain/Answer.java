1f0bd3cd16be63dd75e4b0489f999cb84203f7c2
shkwon <oper912@naver.com>
2021-04-26 12:59:00 +0900

    feat: add Answer

---
+++ src/main/java/baseball/domain/Answer.java
@@ -0,0 +1,26 @@
+package baseball.domain;
+
+import baseball.generator.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Answer {
+    private final List<BaseballNumber> answer;
+
+    private Answer(final List<BaseballNumber> answer) {
+        this.answer = answer;
+    }
+
+    public static Answer from(final NumberGenerator numberGenerator) {
+        List<BaseballNumber> answer = new ArrayList<>();
+        for (Integer number : numberGenerator.generator()) {
+            answer.add(BaseballNumber.from(number));
+        }
+        return new Answer(answer);
+    }
+
+    public BaseballNumber getAnswer(int index) {
+        return answer.get(index);
+    }
+}
