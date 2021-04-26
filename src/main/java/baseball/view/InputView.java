8bbd180ad6ca9784203ed68fedbf135d19119649
juniq <juniqlim@gmail.com>
2021-04-26 10:49:37 +0900

    feat: 게임 재시작 입력

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,4 +9,12 @@ public class InputView {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
+
+    public static int restartNumber() {
+        System.out.println();
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
 }
