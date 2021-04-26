0916564dbb9f9c89ccd24c35294ef2a2a7ef7a3f
pok3mon025 <pok3mon025@gmail.com>
2021-04-27 00:12:16 +0900

    feat : 3개의 숫자 입력 기능 구현 - 기능 목록 002 구현 - 입력값 유효성 검사

--- src/main/java/baseball/BaseballApplication.java
+++ src/main/java/baseball/BaseballApplication.java
@@ -1,10 +1,12 @@
 package baseball;
 
+import baseball.utils.InputUtils;
 import baseball.utils.RandomUtils;
 
 public class BaseballApplication {
 	public static void main(String[] args) {
 		RandomUtils randomUtils = new RandomUtils();
 		int[] result = randomUtils.createRandomNumber(1, 9);
+		int[] userArray = InputUtils.getNumber(1, 9);
 	}
 }
---
+++ src/main/java/baseball/utils/InputUtils.java
@@ -0,0 +1,73 @@
+package baseball.utils;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class InputUtils {
+	private static final int NUMBER_LENGTH = 3;
+	private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public InputUtils() {
+	}
+
+	public static int[] getNumber(final int min, final int max) {
+		System.out.print(INPUT_MESSAGE);
+		int answer = scanner.nextInt();
+
+		int[] result = getResultArray(answer);
+
+		checkValidLenth(result.length);
+		checkValidRange(result, min, max);
+		checkDuplicate(result);
+
+		return result;
+	}
+
+	private static void checkValidAnswer(int answer) {
+		if (answer != 1 && answer != 2) {
+			throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+		}
+	}
+
+	public static void checkDuplicate(int[] result) {
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < NUMBER_LENGTH; i++) {
+			set.add(result[i]);
+		}
+
+		checkValidLenth(set.size());
+	}
+
+	private static void checkValidLenth(int length) {
+		if (length != NUMBER_LENGTH) {
+			throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+		}
+	}
+
+	private static void checkValidRange(int[] number, int min, int max) {
+		for (int i = 0; i < NUMBER_LENGTH; i++) {
+			if (number[i] < min) {
+				throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+			}
+
+			if (number[i] > max) {
+				throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+			}
+		}
+	}
+
+	private static int[] getResultArray(int answer) {
+		String str = Integer.toString(answer);
+
+		int[] result = new int[str.length()];
+
+		for (int i = 0; i < result.length; i++) {
+			result[i] = str.charAt(i) - '0';
+		}
+
+		return result;
+	}
+}
