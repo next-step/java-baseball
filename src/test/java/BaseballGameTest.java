436ebc739a4deece01346f1fd8a6bf8ea1a648ef
jaewon.lim <jewon945@gmail.com>
2021-04-27 00:29:03 +0900

    feat: convertStringToNumbers가 입력받은 숫자를 Key(값)과 Value(인덱스)로 변환하는지 테스트

--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -1,8 +1,12 @@
 import org.hamcrest.junit.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -42,4 +46,25 @@ class BaseballGameTest {
     public void initTargetNumbers() {
         assertEquals(baseballGame.initTargetNumbers().size(), 3);
     }
+
+    /**
+     * convertStringToNumbers
+     * 입력받은 숫자를 Key(값)과 Value(인덱스)로 변환하는지 테스트
+     * @param str 플레이어로부터 입력받은 숫자
+     * @param expected 예측되는 결과값
+     */
+    @ParameterizedTest
+    @MethodSource("provideCSTNParameters")
+    public void convertStringToNumbers(String str, int expected) {
+        assertEquals(baseballGame.convertStringToNumbers(str).size(), expected);
+    }
+
+    private static Stream<Arguments> provideCSTNParameters(){
+        return Stream.of(
+                Arguments.of("239",3),
+                Arguments.of("78",2),
+                Arguments.of("3492",4)
+        );
+    }
+
 }
