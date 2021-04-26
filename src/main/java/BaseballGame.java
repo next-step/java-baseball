c627b1ac720ecaffb5f870e94d674607167d9e04
조예찬 <choyuichan@gmail.com>
2021-04-26 12:14:38 +0900

    테스트: 숫자 맞추기 테스트(3 스트라이크)
    숫자 맞추기 테스트 변경 (3 스트라이크일 경우)

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -30,7 +30,7 @@ public class BaseballGame {
 			ball+=1;
 		}
 
-		return (strike==0 && ball==0?"낫싱":(strike>0?strike + "스트라이크":"")+(ball>0?ball + "볼":""));
+		return strike==3?"3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.":(strike==0 && ball==0?"낫싱":(strike>0?strike + "스트라이크":"")+(ball>0?ball + "볼":""));
 	}
 
 	private String generateNumbers(String all,int length){
--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -109,7 +109,7 @@ public class BaseballGameTest{
 		BaseballGame game = new BaseballGame();
 		String output = game.guess(answer,threestrike_guess);
 
-		assertEquals("3스트라이크", output);
+		assertEquals("3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", output);
 	}
 
 
@@ -122,6 +122,9 @@ public class BaseballGameTest{
 
 		assertEquals("낫싱", output);
 	}
+
+
+
 
 
 }
