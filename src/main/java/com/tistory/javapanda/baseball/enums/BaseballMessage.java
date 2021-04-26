2577357cc41268e5726344bd72537d15558188ae
jangpanda <javapanda@hanmail.net>
2021-04-27 05:36:04 +0900

    feat : view에서 메시지 별도 enum으로 분리

---
+++ src/main/java/com/tistory/javapanda/baseball/enums/BaseballMessage.java
@@ -0,0 +1,14 @@
+package com.tistory.javapanda.baseball.enums;
+
+public enum BaseballMessage {
+
+    INPUT_MESSAGE("숫자를 입력해주세요 : "),
+    FINISH_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+    private String message;
+
+    BaseballMessage(String message) {
+        this.message = message;
+    }
+
+}
