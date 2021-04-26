29b9ef7bf24d426bbe6014748bf35cf47bc1fc91
britnin <baekseongsa@gmail.com>
2021-04-26 23:21:33 +0900

    feat: 숫자 야구 게임 플레이 기능 구현 및 Main 클라이언트 구현

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,11 +1,14 @@
 package baseball;
 
 import baseball.domain.BaseballGame;
+import baseball.view.InputView;
 
 public class Main {
 
 	public static void main(String[] args) {
-		BaseballGame.play();
+		do {
+			BaseballGame.play();
+		} while (InputView.inputReplayBaseBallGame());
 	}
 
 }
--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,9 +1,21 @@
 package baseball.domain;
 
+import baseball.view.InputView;
+import baseball.view.ResultView;
+
 public class BaseballGame {
 
 	public static void play() {
 		BaseballNumbers autoBaseballNumbers = BaseballNumbers.newAutoBaseballNumbers();
+
+		while (true) {
+			BaseballNumbers inputBaseballNumbers = InputView.inputBaseballNumbers();
+			BaseballGameMatchResult matchResult = autoBaseballNumbers.getBaseballGameMatchResult(inputBaseballNumbers);
+			ResultView.printMatchResult(matchResult);
+			if (matchResult.isAllMatch(BaseballNumberMatch.STRIKE)) {
+				break;
+			}
+		}
 	}
 
 }
