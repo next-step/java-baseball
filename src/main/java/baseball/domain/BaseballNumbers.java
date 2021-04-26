8c0007b5af2d8d861188ba4e4f2f7c2584a98a31
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-04-26 23:30:25 +0900

    feat : Iterable 인터페이스 구현, equalsCount, containsCount 메서드 신규구현

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -1,10 +1,9 @@
 package baseball.domain;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
 
-public class BaseballNumbers {
+public class BaseballNumbers implements Iterable<BaseballNumber> {
 
     public static final int SIZE = 3;
     public static final int MIN_INDEX = 0;
@@ -58,5 +57,51 @@ public class BaseballNumbers {
 
     private static int calculateDistinctSize(List<?> baseballNumbers) {
         return new HashSet<>(baseballNumbers).size();
+    }
+
+    public int equalsCount(BaseballNumbers attackNumbers) {
+        int equalsCount = 0;
+        int index = 0;
+        for (BaseballNumber attackNumber : attackNumbers) {
+            equalsCount += getEqualsCount(attackNumber, index++);
+        }
+        return equalsCount;
+    }
+
+    private int getEqualsCount(BaseballNumber attackNumber, int index) {
+        if (attackNumber.equals(this.baseballNumbers.get(index))) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public int containsCount(BaseballNumbers attackNumbers) {
+        int containsCount = 0;
+        for (BaseballNumber attackNumber : attackNumbers) {
+            containsCount = getContainsCount(attackNumber);
+        }
+        return containsCount;
+    }
+
+    private int getContainsCount(BaseballNumber attackNumber) {
+        if (this.baseballNumbers.contains(attackNumber)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public Iterator<BaseballNumber> iterator() {
+        return baseballNumbers.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super BaseballNumber> action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<BaseballNumber> spliterator() {
+        return Iterable.super.spliterator();
     }
 }
