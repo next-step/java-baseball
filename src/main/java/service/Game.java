e29b0c42cf7bea0a7618c8ed92395eaa8066b2c7
rnthd2 <rnthd2@naver.com>
2021-04-27 06:02:41 +0900

    feat(playGame): 게임을 진행하는 코드를 작성합니다
    스트라이크 개수가 3이 아니면, 사용자에게 입력을 요구하는 출력부터 재시작
    스트라이크 개수가 3이면 게임종료

--- src/main/java/service/Game.java
+++ src/main/java/service/Game.java
@@ -2,11 +2,30 @@ package service;
 
 import domain.Bettings;
 import domain.Notice;
+import domain.Strike;
 
 public class Game {
 
     private static UI ui;
+    private static Referee referee;
     private static Bettings bettings;
+
+    public static void init(){
+        ui = new UI();
+        referee = new Referee();
+    }
+
+    public static void setBettings(Bettings bettings) {
+        Game.bettings = bettings;
+    }
+
+    public static void playGame() {
+        Strike strike;
+        do{
+            setUserInputs();
+            strike = referee.compareInputsWithValues(bettings);
+        } while(!strike.isWin());
+    }
 
     private static void setUserInputs(){
         char[] userInputs = ui
@@ -14,7 +33,7 @@ public class Game {
                 .toCharArray();
         bettings.setUserInputs(userInputs);
     }
-    
+
     public static boolean isGameOver() {
         Notice.NOTICE_WIN.print();
         String option = ui.getAnswerByNotice(Notice.ASK_CONTINUE_GAME);
