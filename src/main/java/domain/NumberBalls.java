6781ed99dd7593689df6bdcd35215d9305e83b1a
apdkeb <apdkeb@naver.com>
2021-04-26 13:51:59 +0900

    feat(NumberBall) : 사이즈 유효성 추가
    2021. 04. 26.
    1. 숫자볼 개수 3개 유효성 추가
    2. 생성자 추가
    3. 이전 로직 리팩토링
    
    Resolves : loop-study
    See also : .

--- src/main/java/domain/NumberBalls.java
+++ src/main/java/domain/NumberBalls.java
@@ -6,23 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NumberBalls {
+    private static final String SIZE_ERROR = "숫자볼 개수는 3 입니다";
     private static final int SIZE = 3;
 
     private final List<NumberBall> numberBalls;
 
     public NumberBalls() {
-        numberBalls = createNumberBalls();
+        this(createNumberBalls());
+    }
+
+    public NumberBalls(List<NumberBall> numberBalls) {
+        validSize(numberBalls);
+        this.numberBalls = numberBalls;
     }
 
     public boolean isSize(int size) {
         return numberBalls.size() == size;
     }
 
-    private List<NumberBall> createNumberBalls() {
+    private static List<NumberBall> createNumberBalls() {
         List<NumberBall> result = new ArrayList<>();
         for (int i = 0; i < SIZE; i++) {
             result.add(new NumberBall(RandomGenerator.value()));
         }
         return result;
+    }
+
+    private void validSize(List<NumberBall> numberBalls) {
+        if (numberBalls.size() != SIZE) {
+            throw new IllegalArgumentException(SIZE_ERROR);
+        }
     }
 }
