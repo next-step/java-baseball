6c61ecd0eb173d7978da901f2e192bc229c7f075
seondongpyo <psd1390@gmail.com>
2021-04-27 04:33:46 +0900

    refactor(BaseballTest): 테스트 케이스 추가
    - BaseballGenerator 객체로 테스트할 Baseballs 객체 생성
    - 스트라이크, 볼 개수를 각각 1 ~ 3까지 테스트되도록 테스트 케이스 추가

--- src/test/java/precourse/baseball/BaseballsTest.java
+++ src/test/java/precourse/baseball/BaseballsTest.java
@@ -2,54 +2,36 @@ package precourse.baseball;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BaseballsTest {
 
     @DisplayName("두 야구공 세트를 비교하여 스트라이크 개수를 센다.")
-    @Test
-    void countStrike() {
-        List<precourse.baseball.Baseball> basket1 = new ArrayList<>();
-        basket1.add(new precourse.baseball.Baseball(1));
-        basket1.add(new precourse.baseball.Baseball(2));
-        basket1.add(new precourse.baseball.Baseball(3));
-
-        List<precourse.baseball.Baseball> basket2 = new ArrayList<>();
-        basket2.add(new precourse.baseball.Baseball(1));
-        basket2.add(new precourse.baseball.Baseball(2));
-        basket2.add(new precourse.baseball.Baseball(4));
-
-        precourse.baseball.Baseballs baseballs1 = new precourse.baseball.Baseballs(basket1);
-        precourse.baseball.Baseballs baseballs2 = new precourse.baseball.Baseballs(basket2);
+    @ParameterizedTest(name = "{0} vs {1}일 떄, 스트라이크 수는 {2}이다.")
+    @CsvSource(value = {"123,123,3", "123,124,2", "123,145,1"})
+    void countStrike(String answer, String numberAsString, int expectedStrike) {
+        BaseballGenerator generator = new BaseballGenerator();
+        Baseballs baseballs1 = generator.createBaseballs(answer);
+        Baseballs baseballs2 = generator.createBaseballs(numberAsString);
 
         int strike = baseballs1.countStrike(baseballs2);
 
-        assertThat(strike).isEqualTo(2);
+        assertThat(strike).isEqualTo(expectedStrike);
     }
 
     @DisplayName("두 야구공 세트를 비교하여 볼 개수를 센다.")
-    @Test
-    void countBall() {
-        List<precourse.baseball.Baseball> basket1 = new ArrayList<>();
-        basket1.add(new precourse.baseball.Baseball(1));
-        basket1.add(new precourse.baseball.Baseball(2));
-        basket1.add(new precourse.baseball.Baseball(3));
-
-        List<precourse.baseball.Baseball> basket2 = new ArrayList<>();
-        basket2.add(new precourse.baseball.Baseball(3));
-        basket2.add(new precourse.baseball.Baseball(1));
-        basket2.add(new precourse.baseball.Baseball(2));
-
-        precourse.baseball.Baseballs baseballs1 = new precourse.baseball.Baseballs(basket1);
-        precourse.baseball.Baseballs baseballs2 = new precourse.baseball.Baseballs(basket2);
+    @ParameterizedTest(name = "{0} vs {1}일 떄, 볼 개수는 {2}이다.")
+    @CsvSource(value = {"123,123,0", "123,142,1", "123,132,2"})
+    void countBall(String answer, String numberAsString, int expectedBall) {
+        BaseballGenerator generator = new BaseballGenerator();
+        Baseballs baseballs1 = generator.createBaseballs(answer);
+        Baseballs baseballs2 = generator.createBaseballs(numberAsString);
 
         int ball = baseballs1.countBall(baseballs2);
 
-        assertThat(ball).isEqualTo(3);
+        assertThat(ball).isEqualTo(expectedBall);
     }
 
 }
