6ed8d6abf24aaa4a9b2ba6255018bd53bddb6aea
catsbi <killfine198@gmail.com>
2021-04-27 08:54:18 +0900

    feat: 야구숫자게임 결과 응답객체 테스트케이스 생성 - of 정적 메서드 팩토리 생성자

--- src/test/java/baseball/domain/dto/BaseballResultResponseTest.java
+++ src/test/java/baseball/domain/dto/BaseballResultResponseTest.java
@@ -1,13 +1,78 @@
 package baseball.domain.dto;
 
+import baseball.domain.PitchingResultStatus;
+import baseball.exceptions.InvalidBallNumbersSizeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.EnumMap;
+import java.util.Map;
+
+import static baseball.domain.PitchingResultStatus.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("BaseballResultResponse 클래스")
 class BaseballResultResponseTest {
+
+    @DisplayName("of 메서드는")
+    @Nested
+    class Describe_of {
+        @DisplayName("Map의 모든 Entry의 value 합이 3이하면")
+        @Nested
+        class Context_With_Three_Under_Sum{
+            @DisplayName("BallNumbersSizeException 예외가 발생한다.")
+            @Test
+            void it_is_returns_exception() {
+                //given
+                Map paramMap = createResultParamMap(1, 0, 0);
+
+                //when, then
+                assertThatThrownBy(()->{
+                    BaseballResultResponse response = BaseballResultResponse.of(paramMap);
+                }).isInstanceOf(InvalidBallNumbersSizeException.class);
+            }
+        }
+
+        @DisplayName("Map의 모든 Entry의 value 합이 3이상이면")
+        @Nested
+        class Context_With_Three_Over_Sum{
+            @DisplayName("BallNumbersSizeException 예외가 발생한다.")
+            @Test
+            void it_is_returns_exception() {
+                //given
+                Map paramMap = createResultParamMap(4, 0, 0);
+
+                //when, then
+                assertThatThrownBy(()->{
+                    BaseballResultResponse response = BaseballResultResponse.of(paramMap);
+                }).isInstanceOf(InvalidBallNumbersSizeException.class);
+            }
+
+        }
+
+        @DisplayName("Map의 모든 Entry의 value 합이 3이면")
+        @Nested
+        class Context_With_Equals_Three_Sum{
+            @DisplayName("정상적으로 응답댁체가 생성된다.")
+            @Test
+            void it_is_returns_BaseballResultResponse() {
+                //given
+                Map paramMap = createResultParamMap(1, 1, 1);
+
+                //when
+                BaseballResultResponse response = BaseballResultResponse.of(paramMap);
+
+                //then
+                assertThat(response.isAllStrike()).isFalse();
+                assertThat(response.isNothing()).isFalse();
+                assertThat(response.getPitchingResult()).isEqualTo(paramMap);
+            }
+        }
+    }
+
 
     @DisplayName("getAllStrike 메소드는")
     @Nested
@@ -16,10 +81,17 @@ class BaseballResultResponseTest {
         @DisplayName("결과가 3스트라이크인 값으로 객체를 생성하면")
         @Nested
         class Context_with_3_Strike {
-            @DisplayName("true를 반환한다. ")
+            @DisplayName("true를 반환한다.")
             @Test
             void it_is_returns_true() {
+                //given
+                Map paramMap = createResultParamMap(3, 0, 0);
 
+                //when
+                BaseballResultResponse response = BaseballResultResponse.of(paramMap);
+
+                //then
+                assertThat(response.isAllStrike()).isTrue();
             }
         }
         @DisplayName("결과가 3스트라이크가 아닌 값으로 객체를 생성하면")
@@ -28,7 +100,17 @@ class BaseballResultResponseTest {
             @DisplayName("false를 반환한다.")
             @Test
             void it_is_returns_false() {
+                //given
+                Map paramMap1 = createResultParamMap(0, 0, 3);
+                Map paramMap2 = createResultParamMap(0, 3, 0);
 
+                //when
+                BaseballResultResponse response1 = BaseballResultResponse.of(paramMap1);
+                BaseballResultResponse response2 = BaseballResultResponse.of(paramMap2);
+
+                //then
+                assertThat(response1.isAllStrike()).isFalse();
+                assertThat(response2.isAllStrike()).isFalse();
             }
         }
     }
@@ -43,7 +125,14 @@ class BaseballResultResponseTest {
             @DisplayName("true를 반화한다.")
             @Test
             void it_is_returns_true() {
+                //given
+                Map paramMap = createResultParamMap(0, 0, 3);
 
+                //when
+                BaseballResultResponse response = BaseballResultResponse.of(paramMap);
+
+                //then
+                assertThat(response.isNothing()).isTrue();
             }
         }
         @DisplayName("결과가 3낫싱이 아닌 값으로 객체를 생성하면")
@@ -52,13 +141,29 @@ class BaseballResultResponseTest {
             @DisplayName("false를 반환한다.")
             @Test
             void it_is_returns_false() {
+                //given
+                Map paramMap1 = createResultParamMap(1, 0, 2);
+                Map paramMap2 = createResultParamMap(0, 3, 0);
 
+                //when
+                BaseballResultResponse response1 = BaseballResultResponse.of(paramMap1);
+                BaseballResultResponse response2 = BaseballResultResponse.of(paramMap2);
+
+                //then
+                assertThat(response1.isNothing()).isFalse();
+                assertThat(response2.isNothing()).isFalse();
             }
         }
 
     }
 
-
+    private static Map createResultParamMap(int strikeCount, int ballCount, int nothingCount) {
+        return new EnumMap<PitchingResultStatus, Integer>(PitchingResultStatus.class){{
+            put(STRIKE, strikeCount);
+            put(BALL, ballCount);
+            put(NOTHING, nothingCount);
+        }};
+    }
 
 
 }
