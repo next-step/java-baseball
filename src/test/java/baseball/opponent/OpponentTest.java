2de097e6a7acd44b21660251604478e0eac3a888
kimmin-ko <kimmin2301@gmail.com>
2021-04-26 16:38:31 +0900

    test: 사용자가 입력한 값에 대한 유효성 검사 예외 발생 테스트 구현

--- src/test/java/baseball/opponent/OpponentTest.java
+++ src/test/java/baseball/opponent/OpponentTest.java
@@ -3,8 +3,11 @@ package baseball.opponent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OpponentTest {
 
@@ -32,4 +35,39 @@ class OpponentTest {
         assertThat(answers[0]).isNotEqualTo(answers[2]).isNotEqualTo(0);
         assertThat(answers[1]).isNotEqualTo(answers[2]).isNotEqualTo(0);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {99, 1000})
+    @DisplayName("두 자릿수 또는 네 자릿수를 입력하면 예외가 발생한다.")
+    void compare_input_answer_length_test(int answer) {
+        // when then
+        assertThatThrownBy(() -> opponent.compare(answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("세 자릿수를 입력해 주세요.");
+    }
+
+    @Test
+    @DisplayName("0이 포함된 값을 입력하면 예외가 발생한다.")
+    void compare_input_answer_zero_test() {
+        // given
+        int answer = 880;
+
+        // when then
+        assertThatThrownBy(() -> opponent.compare(answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("정답에 0이 포함될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("중복된 값을 입력하면 예외가 발생한다.")
+    void compare_input_answer_duplicate_test() {
+        // given
+        int answer = 777;
+
+        // when then
+        assertThatThrownBy(() -> opponent.compare(answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 값을 입력할 수 없습니다.");
+    }
+
 }
