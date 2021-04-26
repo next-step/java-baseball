ae6b1bf8370d40e5b050d36b7fc11ba8bb0202ba
stua92 <stau92@gmail.com>
2021-04-27 05:51:27 +0900

    feat: 3자리 숫자 정보를 가진 Baseball 클래스 작성

---
+++ src/src/baseball/Util/GameStatus.java
@@ -0,0 +1,8 @@
+package baseball.Util;
+
+public class GameStatus {
+	public static final int BALL_SIZE = 3;
+	public static final int BALL_RANGE = 9;
+	public static final int RESTART_CODE = 1;
+	public static final int END_CODE = 2;
+}
