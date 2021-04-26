c64297f048702025983162350c461575018514ba
apdkeb <apdkeb@naver.com>
2021-04-26 12:10:42 +0900

    feat(NumberBall) : NumberBall 생성
    2021. 04. 26.
    1. NumberBall 생성
    
    Resolves : loop-study
    See also : .

---
+++ src/main/java/domain/NumberBall.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class NumberBall {
+    private final int ball;
+
+    public NumberBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int toInt() {
+        return ball;
+    }
+}
--- src/test/java/domain/NumberBallTest.java
+++ src/test/java/domain/NumberBallTest.java
@@ -1,8 +1,6 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,12 +13,9 @@ public class NumberBallTest {
     @ValueSource(ints = {1,2,3,8,9})
     @DisplayName("범위는 1~9")
     public void numberBallRangeOneToNine(int number) throws Exception {
-        //given
         NumberBall numberBall = new NumberBall(number);
-
-        //when
-
-        //then
         assertThat(numberBall.toInt()).isEqualTo(number);
     }
+
+
 }
