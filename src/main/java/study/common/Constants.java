10158386801275d0c593b67405aeccbbc4cfc353
kwonby <kwonby@mz.co.kr>
2021-04-26 23:25:45 +0900

    feat: 사용자 입력 관련 기능 생성
    * 사용자의 값을 입력 받는 메서드 작성
    * 사용자의 입력 값이 유효한 형식인지 체크하는 메서드 작성
    * 게임을 계속 진행할지 사용자에게 묻는 메서드 작성

--- src/main/java/study/common/Constants.java
+++ src/main/java/study/common/Constants.java
@@ -12,4 +12,17 @@ public class Constants {
 
     /** 야구게임 최대 숫자 */
     public static final int MAX_BASEBALL_NUMBER = 9;
+
+
+    /** 야구게임 최소 숫자 범위 */
+    public static final int MIN_BASEBALL_NUMBER_RANGE = 123;
+
+    /** 야구게임 최대 숫자 범위 */
+    public static final int MAX_BASEBALL_NUMBER_RANGE = 987;
+
+    /** 야구게임 계속 여부 답변 최대값 */
+    public static final int MAX_BASEBALL_IS_AGAIN_ANSWER = 2;
+
+    /** 야구게임 계속 여부 답변 최소 */
+    public static final int MIN_BASEBALL_IS_AGAIN_ANSWER = 1;
 }
