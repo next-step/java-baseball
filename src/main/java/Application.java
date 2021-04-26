c610556f0e57a6199889aa501a0a6ed1086c0ea3
ParkJiwoon <bcp0109@gmail.com>
2021-04-27 02:41:45 +0900

    feat: Application 클래스 추가
    - 프로그램을 실행하는 main 함수가 존재합니다.
    - BaseBallGame 객체를 생성해서 게임을 진행시킵니다.

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+    private static final int DEFAULT_NUMBER_SIZE = 3;
+
+    public static void main(String[] args) {
+        BaseBallGame game = new BaseBallGame();
+
+        while (game.canPlay()) {
+            game.init(DEFAULT_NUMBER_SIZE);
+            game.inputNumberAndGuess();
+            game.afterSuccess();
+        }
+    }
+}
