7de30dd2dab22e9ccd401dda7f72a7dbe6e9b2fa
coplayer <ally9489@gmail.com>
2021-04-26 23:06:43 +0900

    feat: 게임 플레이어에게 입력 받은 숫자의 중복 여부와 길이를 확인하여 오류를 처리하는 기능
    입력 받은 배열의 길이와 중복을 제거하여 길이를 확인한다.

--- src/main/java/NumberBaseBallGame.java
+++ src/main/java/NumberBaseBallGame.java
@@ -1,17 +1,34 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class NumberBaseBallGame {
 
 	public static int[] numberBaseballGameGenerate() {
-		Integer[] baseNumbers = {1,2,3,4,5,6,7,8,9};
+		Integer[] baseNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 		Collections.shuffle(Arrays.asList(baseNumbers));
-		Integer[] pickNumbers = Arrays.copyOfRange(baseNumbers, 0,2);
+		Integer[] pickNumbers = Arrays.copyOfRange(baseNumbers, 0, 2);
 
 		int[] gameNumbers = new int[3];
-		for(int i = 0; i < gameNumbers.length - 1; i ++) {
+		for (int i = 0; i < gameNumbers.length - 1; i++) {
 			gameNumbers[i] = pickNumbers[i];
 		}
 		return gameNumbers;
+	}
+
+	public static boolean playerNumberValidate(int[] playerInputNumbers) {
+		if (playerInputNumbers.length < 3)
+			return false;
+
+		Set<Integer> duplicateRemovedGameNumbers = new HashSet<>();
+		for (int playerInputNumber : playerInputNumbers) {
+			duplicateRemovedGameNumbers.add(playerInputNumber);
+		}
+
+		if (duplicateRemovedGameNumbers.size() < 3)
+			return false;
+
+		return true;
 	}
 }
--- src/test/java/NumberBaseBallGameTest.java
+++ src/test/java/NumberBaseBallGameTest.java
@@ -10,17 +10,25 @@ class NumberBaseBallGameTest {
 
 	@Test
 	@DisplayName("숫자 야구 게임을 위한 숫자 배열생성 확인")
-	public void baseballGameGenerate() {
+	public void baseballGameGenerateTest() {
 		int[] gameNumbers = NumberBaseBallGame.numberBaseballGameGenerate();
 
 		/* 생성된 배열의 길이가 3인지 확인 */
 		assertEquals(3, gameNumbers.length);
 
 		Set<Integer> duplicateRemovedGameNumbers = new HashSet<>();
-		for(int gameNumber : gameNumbers) {
+		for (int gameNumber : gameNumbers) {
 			duplicateRemovedGameNumbers.add(gameNumber);
 		}
 		/* 숫자 배열의 중복여부 확인 */
 		assertEquals(gameNumbers.length, duplicateRemovedGameNumbers.size());
+	}
+
+	@Test
+	@DisplayName("플레이어가 입력한 숫자의 길이와 중복을 확인한다")
+	public void playerNumberValidateTest() {
+		int[] playerInputNumbers = {1, 2, 3};
+
+		assertEquals(true, NumberBaseBallGame.playerNumberValidate(playerInputNumbers));
 	}
 }
