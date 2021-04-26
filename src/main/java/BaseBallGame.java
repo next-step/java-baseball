86e3bb3b0afc9b36bbc69df7e5356d4b6998cc37
ParkJiwoon <bcp0109@gmail.com>
2021-04-27 02:42:29 +0900

    feat: 숫자야구 게임을 이끌어가는 BaseBallGame 클래스 추가
    - 문제를 제출하는 `Computer` 클래스를 갖고 있습니다.
    - 사용자에게 보여주기 위한 UI 기능 (Input, Output) 을 담당합니다.
    - `Status` 라는 값으로 게임의 진행, 성공, 종료 상태를 나타냅니다.

---
+++ src/main/java/BaseBallGame.java
@@ -0,0 +1,174 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaseBallGame {
+    enum Status {
+        PROGRESS, SUCCESS, EXIT
+    }
+
+    enum Select {
+        RESTART("1"),
+        EXIT("2")
+        ;
+
+        private final String response;
+
+        Select(String response) {
+            this.response = response;
+        }
+
+        public boolean isSelected(String choice) {
+            return response.equals(choice);
+        }
+    }
+
+    private static final String EMPTY_STRING = "";
+
+    private final BufferedReader br;
+    private Computer computer;
+    private Status status;
+
+    public BaseBallGame() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        status = Status.PROGRESS;
+    }
+
+    private void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public boolean canPlay() {
+        return status == Status.PROGRESS;
+    }
+
+    /**
+     * 게임 시작하기 전 세팅
+     *
+     * @param numberSize    랜덤 숫자 길이
+     */
+    public void init(int numberSize) {
+        computer = new Computer(numberSize);
+    }
+
+    /**
+     * 답을 맞출 때까지 입력 받고 비교하기 반복
+     */
+    public void inputNumberAndGuess() {
+        while (!computer.isCorrectAnswer()) {
+            String input = inputAnswer();
+            Result result = computer.compareNumberWith(input);
+            printResult(result);
+        }
+    }
+
+    /**
+     * 플레이어의 값을 입력받고 검증
+     * 올바른 값을 입력할 때까지 반복
+     *
+     * @return 플레이어의 입력값
+     */
+    private String inputAnswer() {
+        String input;
+
+        do {
+            Log.REQUEST_ANSWER.print();
+            input = input();
+        } while (invalidAnswerSize(input));
+
+        return input;
+    }
+
+    /**
+     * 플레이어가 입력한 값이 올바른지 검증
+     *
+     * @param input 플레이어의 입력값
+     * @return      invalid 한 값이면 true, 아니면 false
+     */
+    private boolean invalidAnswerSize(String input) {
+        if (input.length() != computer.getNumberSize()) {
+            Log.INVALID_ANSWER_SIZE.printFormat(computer.getNumberSize());
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 결과값을 기반으로 성공 | 낫싱 | 스트라이크 | 볼 출력
+     *
+     * @param result    플레이어 값과 정답을 비교한 결과
+     */
+    private void printResult(Result result) {
+        if (computer.isCorrectAnswer()) {
+            setStatus(Status.SUCCESS);
+            Log.STRIKE.printCount(result.getStrikeCount());
+            Log.SUCCESS.printFormat(computer.getNumberSize());
+            return;
+        }
+
+        printWrongAnswerCount(result);
+    }
+
+    private void printWrongAnswerCount(Result result) {
+        if (result.isNothing()) {
+            Log.NOTHING.println();
+            return;
+        }
+
+        Log.STRIKE.printCount(result.getStrikeCount());
+        Log.BALL.printCount(result.getBallCount());
+        System.out.println();
+    }
+
+    /**
+     * 플레이어가 정답을 맞추고 난 후의 선택지
+     * 재시작("1") | 종료("2") 중 하나를 선택할 수 있음
+     * 선택하지 않으면 반복문을 벗어나지 못함
+     */
+    public void afterSuccess() {
+        while (status == Status.SUCCESS) {
+            Log.ASK_RESTART_OR_EXIT.println();
+            String choice = input();
+            restartOrExit(choice);
+        }
+    }
+
+    private void restartOrExit(String choice) {
+        if (Select.RESTART.isSelected(choice)) {
+            setStatus(Status.PROGRESS);
+            return;
+        }
+
+        if (Select.EXIT.isSelected(choice)) {
+            exit();
+        }
+    }
+
+    /**
+     * 게임 종료
+     */
+    private void exit() {
+        setStatus(Status.EXIT);
+
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 공통 input
+     *
+     * @return 에러가 나면 "" 리턴
+     */
+    private String input() {
+        try {
+            return br.readLine();
+        } catch (Exception e) {
+            Log.INPUT_ERROR.println();
+            return EMPTY_STRING;
+        }
+    }
+}
