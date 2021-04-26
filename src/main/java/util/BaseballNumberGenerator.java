d840e915712262a4e68daee1fe2ff50880da1a0c
HyungjuJang <dacapolife87@gmail.com>
2021-04-26 21:43:42 +0900

    refactor : 불필요하게 new ArrayList사용

--- src/main/java/util/BaseballNumberGenerator.java
+++ src/main/java/util/BaseballNumberGenerator.java
@@ -1,13 +1,12 @@
 package util;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class BaseballNumberGenerator {
 
-    private static final List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    private static final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
     private static final int BASEBALL_NUMBER_MAX_RANGE = 3;
 
     private BaseballNumberGenerator() {
