cadfabdfcca544be3b82b940b5f5d7e9c4f09ee9
HyeongJinK <wkzkfmxk23@gmail.com>
2021-04-26 22:20:38 +0900

    feat: 중복되지 않는 숫자 문자열 만들기 1~9 까지의 숫자로 문자열 만들기 같은 숫자가 두번 중복되면 안된다. 입력된 length 만큼의 문자열을 만든 다.

--- src/main/java/khj/baseball/util/generator/RandomNumberStringGenerator.java
+++ src/main/java/khj/baseball/util/generator/RandomNumberStringGenerator.java
@@ -1,8 +1,48 @@
 package khj.baseball.util.generator;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.HashSet;
+import java.util.Set;
+
 public class RandomNumberStringGenerator implements RandomStringGenerator {
+    private char[] availableCharacters  = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
     @Override
     public String generatorString(int length) {
-        return null;
+        Set<Character> choiceCharSet = createRandomCharacterSet(length);
+
+        StringBuilder builder = mergeCharacter(choiceCharSet);
+
+        return builder.toString();
+    }
+
+    private Set<Character> createRandomCharacterSet(int length) {
+        Set<Character> choiceCharSet = new HashSet<>();
+
+        SecureRandom random = getSecureRandomCatchError();
+        while (choiceCharSet.size() < length) {
+            choiceCharSet.add(availableCharacters[random.nextInt(availableCharacters.length)]);
+        }
+
+        return choiceCharSet;
+    }
+
+    private SecureRandom getSecureRandomCatchError() {
+        SecureRandom random = null;
+        try {
+            random = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return random;
+    }
+
+    private StringBuilder mergeCharacter(Set<Character> choiceCharSet) {
+        StringBuilder builder = new StringBuilder();
+        for (Character choiceChar: choiceCharSet) {
+            builder.append(choiceChar);
+        }
+        return builder;
     }
 }
