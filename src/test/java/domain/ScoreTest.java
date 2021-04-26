8a5c5c75fd381a4d88dc4d8305a259d50ed6cc0e
apdkeb <apdkeb@naver.com>
2021-04-26 14:32:13 +0900

    test(Score) : Score 테스트 생성
    2021. 04. 26.
    - 랜덤볼과 입력볼 스트라이크, 볼 테스트 추가
    
    Resolves : loop-study
    See also : .

---
+++ src/test/java/domain/ScoreTest.java
@@ -0,0 +1,79 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("점수")
+public class ScoreTest {
+    private NumberBalls randomBalls;
+
+    @BeforeEach
+    public void setUp() {
+        List<NumberBall> balls = new ArrayList<>();
+        balls.add(new NumberBall(1));
+        balls.add(new NumberBall(2));
+        balls.add(new NumberBall(3));
+
+        randomBalls = new NumberBalls(balls);
+    }
+
+    public NumberBalls createInputBalls(String inputText) {
+        List<NumberBall> result = new ArrayList<>();
+        String[] split = inputText.split(",");
+        for (int i = 0; i < split.length; i++) {
+            result.add(new NumberBall(Integer.parseInt(split[i])));
+        }
+        return new NumberBalls(result);
+    }
+
+    @Test
+    @DisplayName("원 스트라이크 확인")
+    public void oneStrike() throws Exception {
+        NumberBalls inputBalls = createInputBalls("1,3,4");
+
+        Score resultScore = Referee.matchToScore(randomBalls, inputBalls);
+        Score oneStrikeScore = new Score(1, 0);
+
+        assertThat(resultScore).isEqualTo(oneStrikeScore);
+    }
+
+    @Test
+    @DisplayName("투 스트라이크 확인")
+    public void oneStrike() throws Exception {
+        NumberBalls inputBalls = createInputBalls("1,2,4");
+
+        Score resultScore = Referee.matchToScore(randomBalls, inputBalls);
+        Score oneStrikeScore = new Score(2, 0);
+
+        assertThat(resultScore).isEqualTo(oneStrikeScore);
+    }
+
+    @Test
+    @DisplayName("쓰리 스트라이크 확인")
+    public void oneStrike() throws Exception {
+        NumberBalls inputBalls = createInputBalls("1,2,3");
+
+        Score resultScore = Referee.matchToScore(randomBalls, inputBalls);
+        Score oneStrikeScore = new Score(3, 0);
+
+        assertThat(resultScore).isEqualTo(oneStrikeScore);
+    }
+
+    @Test
+    @DisplayName("원 볼 확인")
+    public void oneBall() throws Exception {
+        NumberBalls inputBalls = createInputBalls("9,8,1");
+        Score resultScore = Referee.matchToScore(randomBalls, inputBalls);
+        Score oneBallScore = new Score(0, 1);
+
+        assertThat(resultScore).isEqualTo(oneBallScore);
+    }
+
+}
