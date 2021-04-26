76684fc4042746d5ffc39352bd20e78d266600a6
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-04-26 19:16:25 +0900

    feat(num validator): 사용자 입력값 검증 클래스 추가
    사용자 입력값 검증 클래스 추가
     - 사용자가 값을 입력했을 때 유효한 값인지 확인
     - 3자리 숫자인지, 1-9로 이루어졌는지, 서로 다른 숫자인지 확인
     - 관련 테스트 코드 작성

---
+++ src/main/java/InputNumberValidator.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+public class InputNumberValidator {
+
+    public static boolean validateNumber(String number) {
+        return isNumberMatchRegex(number) && isAllNumberDifferent(number);
+    }
+
+    private static boolean isNumberMatchRegex(String number) {
+        String regex = "[1-9]{" + GameOptions.HOW_MANY_DIGIT + "}";
+
+        return number.matches(regex);
+    }
+
+    private static boolean isAllNumberDifferent(String number) {
+        char[] numberArr = number.toCharArray();
+        Set<Character> digitCollection = new HashSet<>();
+        for (char c : numberArr) {
+            digitCollection.add(c);
+        }
+
+        return number.length() == digitCollection.size();
+    }
+}
