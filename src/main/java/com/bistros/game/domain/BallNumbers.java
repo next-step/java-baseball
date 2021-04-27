f774e64014a5513cef95c979e29a677bdea42e7f
tea.k <tea.k@kakaopaycorp.com>
2021-04-27 10:04:49 +0900

    feat: 정답을 담을 클래스를 작성

---
+++ src/main/java/com/bistros/game/domain/BallNumbers.java
@@ -0,0 +1,38 @@
+package com.bistros.game.domain;
+
+import com.bistros.game.domain.exception.BallNumberRuleAgainstException;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class BallNumbers {
+    public static final int LENGTH = 3;
+
+    private final List<Integer> nums;
+
+    public BallNumbers(List<Integer> nums) {
+        validate(nums);
+        this.nums = nums;
+    }
+
+    private void validate(List<Integer> data) {
+        if (data == null || data.size() != LENGTH) {
+            throw new BallNumberRuleAgainstException("게임숫자를 생성하기위한 숫자가 부족합니다");
+        }
+
+        Set<Integer> set = new HashSet<>(data);
+        if (set.size() < data.size()) {
+            throw new BallNumberRuleAgainstException("중복 숫자가 존재해서 게임숫자를 생성할 수 없습니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nums.stream()
+            .map(c -> Integer.toString(c))
+            .collect(Collectors.joining());
+    }
+
+}
---
+++ src/main/java/com/bistros/game/domain/exception/BallNumberRuleAgainstException.java
@@ -0,0 +1,7 @@
+package com.bistros.game.domain.exception;
+
+public class BallNumberRuleAgainstException extends BaseBallException {
+    public BallNumberRuleAgainstException(String message) {
+        super(message);
+    }
+}
