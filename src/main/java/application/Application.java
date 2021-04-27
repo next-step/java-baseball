aa33eefd920a69368bbb6644f74121034ebb92a6
Arasia <rlacndtjs261@gmail.com>
2021-04-27 18:56:47 +0900

    feat: add Baseball & update Application
    Baseball 게임 메인 구현
    - game/baseball/Baseball
    게임 실행부에 Baseball 게임 추가
    - application/Application

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,11 +1,13 @@
 package application;
 
 import game.GameRunner;
+import game.baseball.Baseball;
 
 public class Application {
     public static void main(String[] args) {
         try {
             do {
+                GameRunner.startGame(new Baseball());
             } while (GameRunner.checkRestart());
         } catch (Exception e) {
             System.out.println("게임이 실행 중 문제가 발생하였습니다. : " + e.getMessage());
