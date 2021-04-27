044e6f0d2dc25f3a396dbe7222dee096761ff64a
coplayer <ally9489@gmail.com>
2021-04-27 22:30:58 +0900

    feat: 2 ~ 6의 기능을 활용하여 숫자 야구게임의 입력, 출력, 반복을 제어하는 기능
    - main 메소드 실행 기능
    - 게임 시작 기능

--- src/test/java/NumberBaseBallGameTest.java
+++ src/test/java/NumberBaseBallGameTest.java
@@ -1,8 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +10,8 @@ class NumberBaseBallGameTest {
 
 	@Test
 	@DisplayName("숫자 야구 게임을 위한 숫자 배열생성 확인")
-	public void baseballGameGenerateTest() {
-		int[] gameNumbers = NumberBaseBallGame.numberBaseballGameGenerate();
+	public void generateNumberBaseballGame() {
+		int[] gameNumbers = NumberBaseBallGame.generateNumberBaseballGame();
 
 		/* 생성된 배열의 길이가 3인지 확인 */
 		assertEquals(3, gameNumbers.length);
@@ -27,10 +25,17 @@ class NumberBaseBallGameTest {
 	}
 
 	@Test
-	@DisplayName("플레이어가 입력한 숫자 배열의 길이와 중복을 확인한다")
-	public void playerNumberValidateTest() {
+	@DisplayName("플레이어가 입력한 숫자 배열의 길이를 확인한다")
+	public void validatePlayerNumbersLengthTest() {
 		int[] playerInputNumbers = {1, 2, 3};
-		assertEquals(true, NumberBaseBallGame.playerNumberValidate(playerInputNumbers));
+		assertEquals(true, NumberBaseBallGame.validatePlayerNumbersLength(playerInputNumbers));
+	}
+
+	@Test
+	@DisplayName("플레이어가 입력한 숫자 배열의 중복을 확인한다")
+	public void validatePlayerNumbersDuplicate() {
+		int[] playerInputNumbers = {1, 2, 3};
+		assertEquals(true, NumberBaseBallGame.validatePlayerNumbersLength(playerInputNumbers));
 	}
 
 	@Test
@@ -44,16 +49,16 @@ class NumberBaseBallGameTest {
 	@Test
 	@DisplayName("컴퓨터가 생성한 배열과 게임 플레이어가 입력한 배열을 비교하여 스트라이크 리턴")
 	public void checkStrikeTest() {
-		int[] playerInputNumbers = {1,2,3};
-		int[] gameNumbers = {1,4,3};
+		int[] playerInputNumbers = {1, 2, 3};
+		int[] gameNumbers = {1, 4, 3};
 		assertEquals(2, NumberBaseBallGame.checkStrike(playerInputNumbers, gameNumbers));
 	}
 
 	@Test
 	@DisplayName("컴퓨터가 생성한 배열과 게임 플레이어가 입력한 배열을 비교하여 볼을 리턴")
 	public void checkBallTest() {
-		int[] playerInputNumbers = {1,2,3};
-		int[] gameNumbers = {2,4,3};
+		int[] playerInputNumbers = {1, 2, 3};
+		int[] gameNumbers = {2, 4, 3};
 		assertEquals(1, NumberBaseBallGame.checkBall(playerInputNumbers, gameNumbers));
 	}
 }
