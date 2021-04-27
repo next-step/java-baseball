732b8f12f13a2a6c27516fcaecb917de960fe458
bambam <green5940@gmail.com>
2021-04-27 22:54:04 +0900

    feat: 야구 게임 심판 구현, 테스트 추가

--- src/test/java/baseball/domain/MockGameResult.java
+++ src/test/java/baseball/domain/MockGameResult.java
@@ -5,13 +5,14 @@ import java.util.Objects;
 
 import baseball.Const;
 
-public class BaseballResult {
+public class MockGameResult implements GameResult {
 	private final Map<BallKind, Integer> resultMap;
 
-	public BaseballResult(Map<BallKind, Integer> resultMap) {
+	MockGameResult(Map<BallKind, Integer> resultMap) {
 		this.resultMap = resultMap;
 	}
 
+	@Override
 	public Map<BallKind, Integer> getResultMap() {
 		return resultMap;
 	}
@@ -20,9 +21,9 @@ public class BaseballResult {
 	public boolean equals(Object o) {
 		if (this == o)
 			return true;
-		if (!(o instanceof BaseballResult))
+		if (!(o instanceof MockGameResult))
 			return false;
-		BaseballResult that = (BaseballResult)o;
+		MockGameResult that = (MockGameResult)o;
 		return Objects.equals(resultMap.get(BallKind.STRIKE), that.getResultMap().get(BallKind.STRIKE))
 			&& Objects.equals(resultMap.get(BallKind.BALL), that.getResultMap().get(BallKind.BALL))
 			&& Objects.equals(resultMap.get(BallKind.NOTHING), that.getResultMap().get(BallKind.NOTHING));
@@ -36,14 +37,7 @@ public class BaseballResult {
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-
-		sb.append(getMessage(BallKind.STRIKE));
-		sb.append(getMessage(BallKind.BALL));
-		sb.append(getMessage(BallKind.NOTHING));
-		sb.append(Const.NEW_LINE);
-
-		return sb.toString();
+		return getMessage();
 	}
 
 	private String getMessage(BallKind kind) {
@@ -56,6 +50,18 @@ public class BaseballResult {
 			sb.append(kind.getMessage());
 			sb.append(' ');
 		}
+		return sb.toString();
+	}
+
+	@Override
+	public String getMessage() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(getMessage(BallKind.STRIKE));
+		sb.append(getMessage(BallKind.BALL));
+		sb.append(getMessage(BallKind.NOTHING));
+		sb.append(Const.NEW_LINE);
+
 		return sb.toString();
 	}
 }
