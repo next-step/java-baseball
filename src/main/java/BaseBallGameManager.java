65640dba5ae2285de43da2cb2b399d4994efe292
frostnight <frostnightsong@gmail.com>
2021-04-27 23:24:39 +0900

    fix: 스트라이크 카운팅 구현 및 볼 카운팅 테스트 작성

--- src/main/java/BaseBallGameManager.java
+++ src/main/java/BaseBallGameManager.java
@@ -1,7 +1,17 @@
+import java.util.List;
+import java.util.Set;
+
 public class BaseBallGameManager {
 
-	public BaseBallGameManager() {
+	public final int NUMBER_LIMIT = 3;
+	private BaseBallNumberGenerator baseBallNumberGenerator;
+	private UserInputReader userInputReader;
 
+	public BaseBallGameManager() {
+		// 사용자 입력  UI
+		userInputReader = new UserInputReader();
+		// 야구게임 숫자 생성기
+		baseBallNumberGenerator = new BaseBallNumberGenerator(NUMBER_LIMIT);
 	}
 
 	/**
@@ -10,4 +20,27 @@ public class BaseBallGameManager {
 	public void run() throws Exception {
 	}
 
+	public int countStrike(List<String> targetList, List<String> compareList, int index, int count) {
+		int result = count;
+		if (index == NUMBER_LIMIT) {
+			return result;
+		}
+		if (targetList.get(index).equals(compareList.get(index))) {
+			result++;
+		}
+		return countStrike(targetList, compareList, ++index, result);
+	}
+
+	public int countBalls(List<String> targetList, List<String> compareList, Set<String> compareSet, int index,
+		int count) {
+
+		int result = count;
+		if (index == NUMBER_LIMIT) {
+			return result;
+		}
+		if (compareSet.contains(targetList.get(index)) && !targetList.get(index).equals(compareList.get(index))) {
+			result++;
+		}
+		return countBalls(targetList, compareList, compareSet, ++index, result);
+	}
 }
--- src/test/java/BaseBallGameManagerTest.java
+++ src/test/java/BaseBallGameManagerTest.java
@@ -1,7 +1,9 @@
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +20,15 @@ class BaseBallGameManagerTest {
 		int countStrike = baseBallGameManager.countStrike(targetList, compareList, 0, 0);
 		assertThat(countStrike).isEqualTo(1);
 	}
-	
+
+	@DisplayName("볼 횟수 테스트")
+	@Test
+	public void countBallsTest() {
+		List<String> targetList = Arrays.asList(new String[] {"4", "7", "2"});
+		List<String> compareList = Arrays.asList(new String[] {"9", "5", "6"});
+		Set<String> compareSet = new HashSet<>(compareList);
+		int countStrike = baseBallGameManager.countBalls(targetList, compareList, compareSet, 0, 0);
+		assertThat(countStrike).isEqualTo(0);
+	}
+
 }
