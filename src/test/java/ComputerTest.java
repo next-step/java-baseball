fb7c9487af53694124c067143bfa77f7d859643e
jonghaha <bgh9197@naver.com>
2021-04-27 20:30:00 +0900

    refactor: 스트라이크 매치 결과 테스트 위치 변경
    - UserTest.java -> ComputerTest.java

--- src/test/java/ComputerTest.java
+++ src/test/java/ComputerTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,5 +22,62 @@ public class ComputerTest {
 				.hasSize(3)
 				.doesNotContainNull()
 				.containsOnly(numbers.get(0), numbers.get(1), numbers.get(2));
+	}
+
+	@Test
+	@DisplayName("유저가 입력한 숫자 매칭 테스트 - 1 스트라이크")
+	public void 원_스트라이크_매칭_테스트() {
+		List<Integer> computerNumber = new ArrayList<>();
+		computerNumber.add(4);
+		computerNumber.add(8);
+		computerNumber.add(6);
+		computer.setComputerNumbers(computerNumber);
+
+		List<Integer> userNumber = new ArrayList<>();
+		userNumber.add(4);
+		userNumber.add(6);
+		userNumber.add(8);
+
+		computer.matchResult(userNumber);
+
+		assertThat(computer.getStrikeCount()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("유저가 입력한 숫자 매칭 테스트 - 2 스트라이크")
+	public void 투_스트라이크_매칭_테스트() {
+		List<Integer> computerNumber = new ArrayList<>();
+		computerNumber.add(4);
+		computerNumber.add(8);
+		computerNumber.add(6);
+		computer.setComputerNumbers(computerNumber);
+
+		List<Integer> userNumber = new ArrayList<>();
+		userNumber.add(4);
+		userNumber.add(8);
+		userNumber.add(7);
+
+		computer.matchResult(userNumber);
+
+		assertThat(computer.getStrikeCount()).isEqualTo(2);
+	}
+
+	@Test
+	@DisplayName("유저가 입력한 숫자 매칭 테스트 - 3 스트라이크")
+	public void 쓰리_스트라이크_매칭_테스트() {
+		List<Integer> computerNumber = new ArrayList<>();
+		computerNumber.add(4);
+		computerNumber.add(8);
+		computerNumber.add(6);
+		computer.setComputerNumbers(computerNumber);
+
+		List<Integer> userNumber = new ArrayList<>();
+		userNumber.add(4);
+		userNumber.add(8);
+		userNumber.add(6);
+
+		computer.matchResult(userNumber);
+
+		assertThat(computer.getStrikeCount()).isEqualTo(3);
 	}
 }
