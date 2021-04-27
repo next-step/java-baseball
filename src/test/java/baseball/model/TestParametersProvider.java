08ceb809ff1dd0026fa5a370a6c8c9c1b9b99253
Shinyoung Kim <rolroralra@gmail.com>
2021-04-27 20:45:10 +0900

    refactor: test parameter provider

--- src/test/java/baseball/model/TestParametersProvider.java
+++ src/test/java/baseball/model/TestParametersProvider.java
@@ -1,5 +1,6 @@
 package baseball.model;
 
+import baseball.constants.BaseballConstant;
 import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.ArrayList;
@@ -8,14 +9,14 @@ import java.util.Random;
 import java.util.function.Function;
 
 public class TestParametersProvider {
-    private static final int MAX_RADIX = BaseballNumber.MAX_RADIX;
-    private static final int MIN_RADIX = BaseballNumber.MIN_RADIX;
-    private static final int MAX_SIZE = BaseballNumber.MAX_SIZE;
-    private static final int MIN_SIZE = BaseballNumber.MIN_SIZE;
+    private static final int MAX_RADIX = BaseballConstant.MAX_RADIX;
+    private static final int MIN_RADIX = BaseballConstant.MIN_RADIX;
+    private static final int MAX_SIZE = BaseballConstant.MAX_SIZE;
+    private static final int MIN_SIZE = BaseballConstant.MIN_SIZE;
     private static final String[] CHARACTERS = "`1234567890-=~!@#$%^&*()_+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ[]{};'\":,./<>?".split("");
     private static final int DEFAULT_TEST_CASE_COUNT = 100;
 
-    private static List<Arguments> provideSize() {
+    public static List<Arguments> provideSize() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = MIN_RADIX; radix <= MAX_RADIX; radix++) {
             list.add(Arguments.of(radix));
@@ -23,7 +24,7 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideRadix() {
+    public static List<Arguments> provideRadix() {
         List<Arguments> list = new ArrayList<>();
         for (int size = MIN_SIZE; size < MAX_SIZE; size++) {
             list.add(Arguments.of(size));
@@ -31,7 +32,7 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideSizeAndRadix() {
+    public static List<Arguments> provideSizeAndRadix() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = MIN_RADIX; radix <= MAX_RADIX; radix++) {
             for (int size = 1; size < radix; size++) {
@@ -41,7 +42,7 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideNonPositiveSizeOrNonPositiveSizeRadix() {
+    public static List<Arguments> provideNonPositiveSizeOrNonPositiveSizeRadix() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = -MAX_RADIX; radix <= 1; radix++) {
             for (int size = -MAX_RADIX; size <= 0; size++) {
@@ -51,7 +52,7 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideTooMuchSizeOrTooMuchRadix() {
+    public static List<Arguments> provideTooMuchSizeOrTooMuchRadix() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = MAX_RADIX * 2; radix > MAX_RADIX; radix--) {
             for (int size = 2 * radix; size >= radix; size--) {
@@ -61,13 +62,13 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideRandomBaseballNumberOfAllSizeAndAllRadix() {
+    public static List<Arguments> provideRandomBaseballNumberOfAllSizeAndAllRadix() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = MIN_RADIX; radix <=  MAX_RADIX; radix++) {
             for (int size = MIN_SIZE; size < radix; size++) {
                 list.add(
                         Arguments.of(
-                                provideRandomBaseballNumberString(size, radix),
+                                generateRandomBaseballNumberString(size, radix),
                                 size,
                                 radix
                         )
@@ -77,25 +78,25 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideRandomBaseballNumberOfDefaultSizeAndDefaultRadix() {
+    public static List<Arguments> provideRandomBaseballNumberOfDefaultSizeAndDefaultRadix() {
         List<Arguments> list = new ArrayList<>();
         for (int i = 0; i < DEFAULT_TEST_CASE_COUNT; i++) {
             list.add(
                     Arguments.of(
-                            provideRandomBaseballNumberString()
+                            generateRandomBaseballNumberString()
                     )
             );
         }
         return list;
     }
 
-    private static List<Arguments> provideRandomBaseballRadixOutBoundExceptionCase() {
+    public static List<Arguments> provideRandomBaseballRadixOutBoundExceptionCase() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = MIN_RADIX; radix <=  MAX_RADIX; radix++) {
             for (int size = MIN_SIZE; size < radix; size++) {
                 list.add(
                         Arguments.of(
-                                provideRandomBaseballNumberStringRadixOutBoundException(size, radix),
+                                generateRandomBaseballNumberStringRadixOutBoundException(size, radix),
                                 size,
                                 radix
                         )
@@ -106,13 +107,13 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideRandomBaseballContainsZeroExceptionCase() {
+    public static List<Arguments> provideRandomBaseballContainsZeroExceptionCase() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = MIN_RADIX; radix <=  MAX_RADIX; radix++) {
             for (int size = MIN_SIZE; size < radix; size++) {
                 list.add(
                         Arguments.of(
-                                provideRandomBaseballNumberStringContainsZeroException(size, radix),
+                                generateRandomBaseballNumberStringContainsZeroException(size, radix),
                                 size,
                                 radix
                         )
@@ -123,13 +124,13 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideRandomBaseballDuplicatedExceptionCase() {
+    public static List<Arguments> provideRandomBaseballDuplicatedExceptionCase() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = MIN_RADIX + 1; radix <=  MAX_RADIX; radix++) {
             for (int size = 2; size < radix; size++) {
                 list.add(
                         Arguments.of(
-                                provideRandomBaseballNumberStringDuplicatedException(size, radix),
+                                generateRandomBaseballNumberStringDuplicatedException(size, radix),
                                 size,
                                 radix
                         )
@@ -140,13 +141,31 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static List<Arguments> provideRandomBaseballNotValidCharacterExceptionCase() {
+    public static List<Arguments> provideRandomBaseballHavingWrongLengthExceptionCase() {
+        List<Arguments> list = new ArrayList<>();
+        for (int radix = MIN_RADIX + 1; radix <=  MAX_RADIX; radix++) {
+            for (int size = 2; size < radix; size++) {
+                int differentSize = generateDifferentValue(size, MIN_SIZE, MAX_SIZE);
+                list.add(
+                        Arguments.of(
+                                generateRandomNumberString(differentSize, radix),
+                                size,
+                                radix
+                        )
+                );
+            }
+        }
+
+        return list;
+    }
+
+    public static List<Arguments> provideRandomBaseballNotValidCharacterExceptionCase() {
         List<Arguments> list = new ArrayList<>();
         for (int radix = MIN_RADIX; radix <=  MAX_RADIX; radix++) {
             for (int size = MIN_SIZE; size < radix; size++) {
                 list.add(
                         Arguments.of(
-                                provideRandomBaseballNumberStringNotValidCharacterException(size, radix),
+                                generateRandomBaseballNumberStringNotValidCharacterException(size, radix),
                                 size,
                                 radix
                         )
@@ -157,10 +176,8 @@ public class TestParametersProvider {
         return list;
     }
 
-    private static Character provideRandomCharacter(Function<Character, Boolean> filter) {
+    private static Character generateRandomCharacter(Function<Character, Boolean> filter) {
         Random random = new Random();
-
-        StringBuilder sb = new StringBuilder();
 
         String result = "";
         do {
@@ -170,15 +187,33 @@ public class TestParametersProvider {
         return result.charAt(0);
     }
 
-    public static String provideRandomString(int length, Function<Character, Boolean> filter) {
-        Random random = new Random();
+    public static Character generateRandomCharacter() {
+        return generateRandomCharacter(Character::isDefined);
+    }
 
+    private static Character generateRandomNumberCharacter() {
+        return generateRandomCharacter(c -> Character.isDigit(c) || Character.isAlphabetic(c));
+    }
+
+    private static Character generateRandomNumberCharacter(int radix) {
+        return generateRandomCharacter(c -> {
+            if (radix >= 2 && radix <= 10) {
+                return c >= '0' && c < '0' + radix;
+            } else if (radix <= 'z' - 'a' + 10) {
+                char upperC = Character.toUpperCase(c);
+                return Character.isDigit(c) || (Character.isAlphabetic(c) && upperC >= 'A' && upperC < 'A' + radix - 10);
+            }
+            return false;
+        });
+    }
+
+    private static String generateRandomString(int length, Function<Character, Boolean> filter, Function<Void, Character> characterGenerator) {
         StringBuilder sb = new StringBuilder();
 
-        String result = "";
         for (int i = 0; i < length; i++) {
+            String result;
             do {
-                result = String.valueOf(provideRandomCharacter());
+                result = String.valueOf(characterGenerator.apply(null));
             } while(!filter.apply(result.charAt(0)));
 
             sb.append(result);
@@ -187,9 +222,20 @@ public class TestParametersProvider {
         return sb.toString();
     }
 
-    public static String provideRandomNumberString(int length, int radix) {
-        boolean[] isChecked = new boolean[radix];
-        return provideRandomString(length, c -> {
+    private static String generateRandomString(int length, Function<Character, Boolean> filter) {
+        return generateRandomString(length, filter, (v) -> generateRandomCharacter());
+    }
+
+    private static String generateRandomNumberString(int length, Function<Character, Boolean> filter) {
+        return generateRandomString(length, filter, (v) -> generateRandomNumberCharacter());
+    }
+
+    private static String generateRandomNumberString(int length, int radix, Function<Character, Boolean> filter) {
+        return generateRandomString(length, filter, (v) -> generateRandomNumberCharacter(radix));
+    }
+
+    private static String generateRandomNumberString(int length, int radix) {
+        return generateRandomNumberString(length, c -> {
             if (radix >= 2 && radix <= 10) {
                 return c >= '0' && c < '0' + radix;
             } else if (radix <= 'z' - 'a' + 10) {
@@ -201,14 +247,13 @@ public class TestParametersProvider {
         }).toUpperCase();
     }
 
-    public static String provideRandomBaseballNumberString() {
-        return provideRandomBaseballNumberString(BaseballNumber.DEFAULT_NUMBERS_SIZE, BaseballNumber.DEFAULT_NUMBER_RADIX);
+    private static String generateRandomBaseballNumberString() {
+        return generateRandomBaseballNumberString(BaseballConstant.DEFAULT_NUMBERS_SIZE, BaseballConstant.DEFAULT_NUMBER_RADIX);
     }
 
-    public static String provideRandomBaseballNumberString(int length, int radix) {
+    private static String generateRandomBaseballNumberString(int length, int radix) {
         boolean[] isChecked = new boolean[radix];
-
-        return provideRandomString(length, c -> {
+        return generateRandomNumberString(length, radix, c -> {
             if (radix >= 2 && radix <= 10) {
                 return Character.isDigit(c) && c > '0' && c < '0' + radix && !isChecked[c - '0'] && (isChecked[c - '0'] = true);
             } else if (radix <= 'z' - 'a' + 10) {
@@ -221,8 +266,8 @@ public class TestParametersProvider {
         }).toUpperCase();
     }
 
-    public static String provideRandomBaseballNumberStringRadixOutBoundException(int size, int radix) {
-        char[] original = provideRandomBaseballNumberString(size, radix).toCharArray();
+    private static String generateRandomBaseballNumberStringRadixOutBoundException(int size, int radix) {
+        char[] original = generateRandomBaseballNumberString(size, radix).toCharArray();
 
         Random random = new Random();
         int count = random.nextInt(size) + 1;
@@ -235,15 +280,15 @@ public class TestParametersProvider {
                 int gap = random.nextInt('z' - 'a' - radix  + 11);
                 original[index] = (char)('a' + radix - 10 + gap);
             } else if (radix == MAX_RADIX) {
-                original[index] = provideRandomCharacter(c -> !Character.isDigit(c) && !Character.isAlphabetic(c));
+                original[index] = generateRandomCharacter(c -> !Character.isDigit(c) && !Character.isAlphabetic(c));
             }
         }
 
         return new String(original);
     }
 
-    public static String provideRandomBaseballNumberStringContainsZeroException(int size, int radix) {
-        char[] original = provideRandomBaseballNumberString(size, radix).toCharArray();
+    private static String generateRandomBaseballNumberStringContainsZeroException(int size, int radix) {
+        char[] original = generateRandomBaseballNumberString(size, radix).toCharArray();
 
         Random random = new Random();
         int count = random.nextInt(size) + 1;
@@ -255,8 +300,8 @@ public class TestParametersProvider {
         return new String(original);
     }
 
-    public static String provideRandomBaseballNumberStringDuplicatedException(int size, int radix) {
-        char[] original = provideRandomBaseballNumberString(size, radix).toCharArray();
+    private static String generateRandomBaseballNumberStringDuplicatedException(int size, int radix) {
+        char[] original = generateRandomBaseballNumberString(size, radix).toCharArray();
 
         if (size < 2) {
             return new String(original);
@@ -278,29 +323,25 @@ public class TestParametersProvider {
         return new String(original);
     }
 
-    public static String provideRandomBaseballNumberStringNotValidCharacterException(int size, int radix) {
-        char[] original = provideRandomBaseballNumberString(size, radix).toCharArray();
+    private static String generateRandomBaseballNumberStringNotValidCharacterException(int size, int radix) {
+        char[] original = generateRandomBaseballNumberString(size, radix).toCharArray();
 
         Random random = new Random();
         int count = random.nextInt(size) + 1;
         for (int i = 0 ; i < count; i++) {
             int index = random.nextInt(size);
-            original[index] = provideRandomCharacter(c -> !Character.isDigit(c) && !Character.isAlphabetic(c));
+            original[index] = generateRandomCharacter(c -> !Character.isDigit(c) && !Character.isAlphabetic(c));
         }
 
         return new String(original);
     }
 
+    private static int generateDifferentValue(int pivot, int min, int max) {
+        int result;
+        do {
+            result = new Random().nextInt(max - min + 1) + min;
+        } while(result == pivot);
 
-    public static Character provideRandomCharacter() {
-        return provideRandomCharacter(Character::isDefined);
-    }
-
-    public static Character provideRandomNumericCharacter() {
-        return provideRandomCharacter(Character::isDigit);
-    }
-
-    public static Character provideRandomAlphabeticCharacter() {
-        return provideRandomCharacter(Character::isAlphabetic);
+        return result;
     }
 }
