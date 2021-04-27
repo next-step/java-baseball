1d729067937cbc96236e85c9436c393281bd2178
shin <shin@LAPTOP-MM4U4FF8>
2021-04-27 18:52:32 +0900

    feat: 난수 발생 및 비교 수행

--- src/main/java/play/GamePlayController.java
+++ src/main/java/play/GamePlayController.java
@@ -7,13 +7,35 @@ import java.util.Set;
 
 public class GamePlayController {
 
+
 	public static void gameProcess() {
 		Map<Integer, Integer> inNumMap = new HashMap<Integer, Integer>();
 		Set<Integer> inNumSet = new HashSet<>();
+		Map<Integer, Integer> ranNumMap = new HashMap<Integer, Integer>();
+
+		RandomNumProcess.setRandomNum(ranNumMap);
+
+		boolean whileYn = false;
+
+		repeatProcess(inNumMap, inNumSet, ranNumMap, whileYn);
+		
+	}
+
+	public static void repeatProcess(Map<Integer, Integer> inNumMap, Set<Integer> inNumSet,
+		Map<Integer, Integer> ranNumMap, boolean whileYn) {
+
+		if (whileYn) {
+			return;
+		}
 
 		InputNumberProcess.inputWaitingProcess(inNumMap, inNumSet);
+		whileYn = CompareProcess.gameProcess(ranNumMap, inNumMap);
 
+		inNumSet.clear();
+
+		repeatProcess(inNumMap, inNumSet, ranNumMap, whileYn);
 	}
+
 
 	public static void main(String[] args) {
 		gameProcess();
