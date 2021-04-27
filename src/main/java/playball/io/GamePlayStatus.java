4b8b5c55364329d07798d6db6c9a4d3d0b9d84a3
최홍희 <vvshinevv@nhntoast.com>
2021-04-27 22:45:49 +0900

    feat : GamePlayStatus 클래스 추가
     - strike를 맞춘 이후 게임 재실행 혹은 종료를 알 수 있게 Enum클래스로
    따로 구현하였음

---
+++ src/main/java/playball/io/GamePlayStatus.java
@@ -0,0 +1,43 @@
+package playball.io;
+
+import playball.exception.PlayBallIllegalArgumentException;
+
+import static playball.exception.ExceptionMessage.INVALID_PLAY_BALL_GAME_STATUS_MESSAGE;
+
+public enum GamePlayStatus {
+
+    START(0),
+    RE_START(1),
+    END(2);
+
+    private final Integer status;
+
+    GamePlayStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public static GamePlayStatus findGamePlayStatus(int input) {
+        GamePlayStatus result = null;
+        for (GamePlayStatus gamePlayStatus : GamePlayStatus.values()) {
+            result = findGamePlayStatus(gamePlayStatus, input);
+        }
+
+        if (result == null) {
+            throw new PlayBallIllegalArgumentException(INVALID_PLAY_BALL_GAME_STATUS_MESSAGE);
+        }
+
+        return result;
+    }
+
+    private static GamePlayStatus findGamePlayStatus(GamePlayStatus gamePlayStatus, int input) {
+        if (gamePlayStatus.getStatus() == input) {
+            return gamePlayStatus;
+        }
+
+        return null;
+    }
+}
