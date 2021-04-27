19122f56046eb52f440aaa72430de3195e519939
yjs2952 <yjs2952@naver.com>
2021-04-27 14:29:04 +0900

    refactor: 난수 생성 범위 클래스 상수화

--- src/main/java/domain/GameNumberGenerator.java
+++ src/main/java/domain/GameNumberGenerator.java
@@ -7,13 +7,14 @@ import java.util.*;
 public class GameNumberGenerator implements NumberGenerator {
 
     private static final Random RANDOM = new Random();
+    private static final int BOUND = 8;
 
     @Override
     public GameNumbers generate() {
         Set<Integer> set = new HashSet<>();
 
         while (set.size() < 3) {
-            set.add(RANDOM.nextInt(8) + 1); // 1 ~ 9
+            set.add(RANDOM.nextInt(BOUND) + 1); // 1 ~ 9
         }
 
         return GameNumbers.of(NumberUtils.toList(set));
