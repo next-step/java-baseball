f28fbb6fcafeaa96337de745172b0b392773cf8d
MaKyuSeok <mks502@naver.com>
2021-04-27 21:57:36 +0900

    test(ball) : 공 관련 테스트 추가
    랜덤/선택 을 이용해
    정해진 개수의 공을 생성하는 테스트 추가
    중복 선택에 대한 테스트 추가

--- src/test/java/baseballgame/ball/BallTest.java
+++ src/test/java/baseballgame/ball/BallTest.java
@@ -1,8 +1,12 @@
 package baseballgame.ball;
 
+import baseballgame.constant.BallCount;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.LinkedHashSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -23,6 +27,33 @@ class BallTest {
     void createBall_outOfRange_fail(int number) {
         assertThatThrownBy(() -> {
             Ball ball = Ball.createBall(number);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("숫자야구 규칙에 정해진 개수의 공만큼 랜덤 생성한다.")
+    void createBalls_random_success(){
+        BallsFactory ballsFactory = new RandomBallsFactory();
+        LinkedHashSet<Ball> balls = ballsFactory.createBalls();
+        assertThat(balls.size()).isEqualTo(BallCount.COUNT);
+    }
+
+    @Test
+    @DisplayName("숫자야구 규칙에 정해진 개수의 공을 선택하여 생성한다.")
+    void createBalls_select_success(){
+        Integer[] numbers = {1,2,3};
+        BallsFactory ballsFactory = new SelectBallsFactory(numbers);
+        LinkedHashSet<Ball> balls = ballsFactory.createBalls();
+        assertThat(balls.size()).isEqualTo(BallCount.COUNT);
+    }
+
+    @Test
+    @DisplayName("숫자야구 규칙에 정해진 개수의 공을 중복되게 선택하여 생성한다.")
+    void createBalls_selectDuplicate_fail(){
+        Integer[] numbers = {1,2,2};
+        BallsFactory ballsFactory = new SelectBallsFactory(numbers);
+        assertThatThrownBy(() -> {
+            LinkedHashSet<Ball> balls = ballsFactory.createBalls();
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
