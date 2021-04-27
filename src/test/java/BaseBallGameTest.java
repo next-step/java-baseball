417868b3958faa5e2874831e968d24d283bb6d13
DDuckchul <dduckchul@naver.com>
2021-04-27 22:38:59 +0900

    feat(BaseBallGameTest.java): callBall, compareBall 단위 테스트 구현
    callBallTest, compareBallTest 단위 테스트 구현
    
    BREAKING CHANGE: BaseBallGameTest.java
    callBallTest, compareBallTest 메서드 구현

--- src/test/java/BaseBallGameTest.java
+++ src/test/java/BaseBallGameTest.java
@@ -37,4 +37,17 @@ public class BaseBallGameTest {
 		assertThat(baseBallGame.compareStrike('1','2') == 0).isTrue();
 		assertThat(baseBallGame.compareStrike('1','1') == 1).isTrue();
 	}
+
+	@Test
+	public void callBallTest() {
+		assertThat(baseBallGame.callBall("123", "231") == 3).isTrue();
+		assertThat(baseBallGame.callBall("124", "421") == 2).isTrue();
+		assertThat(baseBallGame.callBall("125", "125") == 0).isTrue();
+	}
+
+	@Test
+	public void compareBallTest(){
+		assertThat(baseBallGame.compareBall('1', "123", 0) == 0).isTrue();
+		assertThat(baseBallGame.compareBall('2', "123", 0) == 1).isTrue();
+	}
 }
