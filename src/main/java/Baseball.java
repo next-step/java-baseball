f2e5bc07747fea01384484abfbbb2f75bb36d9df
chirsjune <chrisjune@gamil.com>
2021-04-27 23:20:28 +0900

    test: add Baseball testcase
    Add testcase Baseball
    - 스트라이크 계산
    - 볼 계산
    - 3스트라이크 계산
    - 게임종료 계산

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -12,7 +12,8 @@ public class Baseball {
         String source = RandomString.generate(3);
         boolean finished = false;
         while (!finished) {
-            finished = gameFinished(source);
+            String target = getTarget(source);
+            finished = gameFinished(source, target);
         }
     }
 
@@ -28,10 +29,9 @@ public class Baseball {
         return target;
     }
 
-    public static boolean gameFinished(String source) {
+    public static boolean gameFinished(String source, String target) {
         int strike = 0;
         int ball = 0;
-        String target = getTarget(source);
         for (int i = 0; i < 3; i++) {
             strike += calculateStrike(target.charAt(i), source.charAt(i));
             // strike 인 경우에도 ball을 무조건 계산해야하는 비효율 존재
@@ -39,6 +39,7 @@ public class Baseball {
         }
         return finished(strike, ball);
     }
+
     public static boolean finished(int strike, int ball){
         if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
@@ -56,11 +57,11 @@ public class Baseball {
         return 0;
     }
 
-    public static int calculateBall(String source, String target, int index) {
-        if (source.substring(0, index).contains(target.substring(index, index + 1))) {
+    public static int calculateBall(String source, String target, int targetIndex) {
+        if (source.substring(0, targetIndex).contains(target.substring(targetIndex, targetIndex + 1))) {
             return 1;
         }
-        if (source.substring(index + 1).contains(target.substring(index, index + 1))) {
+        if (source.substring(targetIndex + 1).contains(target.substring(targetIndex, targetIndex + 1))) {
             return 1;
         }
         return 0;
--- src/test/java/BaseballTest.java
+++ src/test/java/BaseballTest.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class BaseballTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+public class BaseballTest {
+    @Test
+    @DisplayName("스트라이크를 계산한다")
+    void testStrike(){
+        assertThat(Baseball.calculateStrike('1','1')).isEqualTo(1);
+        assertThat(Baseball.calculateStrike('1','2')).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("볼을 계산한다")
+    void testBall(){
+        String source = "123";
+        String target = "234";
+        assertThat(Baseball.calculateBall(source,target,0)).isEqualTo(1);
+        assertThat(Baseball.calculateBall(source,target,1)).isEqualTo(1);
+        assertThat(Baseball.calculateBall(source,target,2)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("3스트라이크면 게임을 종료한다")
+    void testFinished(){
+        assertThat(Baseball.finished(3, 0)).isTrue();
+    }
+
+    @Test
+    @DisplayName("게임 종료여부를 테스트")
+    void testGameFinished(){
+        assertThat(Baseball.gameFinished("123","456")).isFalse();
+        assertThat(Baseball.gameFinished("123","156")).isFalse();
+        assertThat(Baseball.gameFinished("123","425")).isFalse();
+        assertThat(Baseball.gameFinished("123","453")).isFalse();
+        assertThat(Baseball.gameFinished("123","123")).isTrue();
+    }
 }
