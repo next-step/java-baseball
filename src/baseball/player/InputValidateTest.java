5094c97bb568264e2d50b3d90ad9c9babf3f4295
stua92 <stau92@gmail.com>
2021-04-27 21:37:52 +0900

    test: 사용자의 잘못된 입력 오류 예외처리 테스트

---
+++ src/baseball/player/InputValidateTest.java
@@ -0,0 +1,21 @@
+package baseball.player;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputValidateTest {
+
+	@ParameterizedTest
+	@DisplayName("잘못된 입력 오류 테스트")
+	@ValueSource(strings = {"123", "112", "rsd", "124567", "012"})
+	void testValidationChecker(String stringValue) {
+		assertThatThrownBy(() -> InputValidate.validationChecker(stringValue))
+		.isInstanceOf(Exception.class);
+	}
+
+}
