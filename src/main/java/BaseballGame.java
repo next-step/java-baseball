68334031c70a030ede17f02d63c4d1d220afeaf3
gokoy <gokoy50@gmail.com>
2021-04-27 20:37:04 +0900

    feature: add logic to receive answers
    BaseballGame.java
    - received three different numbers between 0 and 9 and returned to arrayList
    
    BaseballGameTest.java
    - check the input is three different numbers between 0 and 9
    - check to convert a given string to an arrayList of Integer type

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 public class BaseballGame implements Game {
 	private static final int ANSWER_LENGTH = 3;
@@ -32,5 +36,44 @@ public class BaseballGame implements Game {
 
 	private int getRandomNumber() {
 		return (int)(Math.random() * 10);
+	}
+
+	//---------------------------------------------------------
+
+	private List<Integer> receiveAnswer() {
+		System.out.print("숫자를 입력해주세요. : ");
+		String input = scanner.nextLine();
+
+		if (areThreeNumbersFromZeroToNine(input)) {
+			return stringToIntegers(input);
+		}
+
+		System.out.println("0부터 9까지의 숫자 중에 서로 다른 3개의 숫자를 입력해주세요.");
+		return receiveAnswer();
+	}
+
+	private boolean areThreeNumbersFromZeroToNine(String input) {
+		if (!Pattern.matches("[0-9]{3}", input)) {
+			return false;
+		}
+
+		return duplicateElementRemovedSize(input) == ANSWER_LENGTH;
+	}
+
+	private int duplicateElementRemovedSize(String input) {
+		Set<String> set = new HashSet<>(Arrays.asList(input.split("")));
+
+		return set.size();
+	}
+
+	private List<Integer> stringToIntegers(String input) {
+		List<String> strings = Arrays.asList(input.split(""));
+		List<Integer> integers = new ArrayList<>();
+
+		for (int i = 0; i < ANSWER_LENGTH; i++) {
+			integers.add(Integer.parseInt(strings.get(i)));
+		}
+
+		return integers;
 	}
 }
--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -3,6 +3,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -49,5 +50,82 @@ class BaseballGameTest {
 		//then
 		assertThat(randomNumber).isGreaterThanOrEqualTo(0)
 			.isLessThanOrEqualTo(9);
+	}
+
+	@Test
+	@DisplayName("0-9 사이의 서로 다른 3개의 수인지 확인하는 areThreeNumbersFromZeroToNine 메소드에 "
+		+ "'123'을 입력했을 때 true 반환")
+	public void areThreeNumbersFromZeroToNine_input123_returnTrue_Test() throws
+		NoSuchMethodException,
+		InvocationTargetException,
+		IllegalAccessException {
+
+		//given
+		Method method = baseballGame.getClass().getDeclaredMethod("areThreeNumbersFromZeroToNine", String.class);
+		method.setAccessible(true);
+
+		//when
+		boolean flag = (boolean)method.invoke(baseballGame, "123");
+
+		//then
+		assertThat(flag).isTrue();
+	}
+
+	@Test
+	@DisplayName("0-9 사이의 서로 다른 3개의 수인지 확인하는 areThreeNumbersFromZeroToNine 메소드에 "
+		+ "'113'을 입력했을 때 false 반환")
+	public void areThreeNumbersFromZeroToNine_input113_returnFalse_Test() throws
+		NoSuchMethodException,
+		InvocationTargetException,
+		IllegalAccessException {
+
+		//given
+		Method method = baseballGame.getClass().getDeclaredMethod("areThreeNumbersFromZeroToNine", String.class);
+		method.setAccessible(true);
+
+		//when
+		boolean flag = (boolean)method.invoke(baseballGame, "113");
+
+		//then
+		assertThat(flag).isFalse();
+	}
+
+	@Test
+	@DisplayName("0-9 사이의 서로 다른 3개의 수인지 확인하는 areThreeNumbersFromZeroToNine 메소드에 "
+		+ "'string'을 입력했을 때 false 반환")
+	public void areThreeNumbersFromZeroToNine_inputString_returnFalse_Test() throws
+		NoSuchMethodException,
+		InvocationTargetException,
+		IllegalAccessException {
+
+		//given
+		Method method = baseballGame.getClass().getDeclaredMethod("areThreeNumbersFromZeroToNine", String.class);
+		method.setAccessible(true);
+
+		//when
+		boolean flag = (boolean)method.invoke(baseballGame, "string");
+
+		//then
+		assertThat(flag).isFalse();
+	}
+
+	@Test
+	@DisplayName("stringToIntegers 메소드에 '123' 입력시 arrayList 반환")
+	public void stringToIntegers_input123_returnArrayList_Test()throws
+		NoSuchMethodException,
+		InvocationTargetException,
+		IllegalAccessException {
+
+		final List<Integer> EXPECTED_LIST = Arrays.asList(1, 2, 3);
+
+		//given
+		Method method = baseballGame.getClass().getDeclaredMethod("stringToIntegers", String.class);
+		method.setAccessible(true);
+
+		//when
+		List<Integer> integers = (List<Integer>)method.invoke(baseballGame, "123");
+
+		//then
+		assertThat(integers).isEqualTo(EXPECTED_LIST);
 	}
 }
