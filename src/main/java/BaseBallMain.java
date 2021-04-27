9a09a2833ffce25c2e6fb8af2bad2fa04e227490
heo.h <heo.h@legaltech.co.kr>
2021-04-27 22:41:51 +0900

    feat : 게임 종료, 재시작 메서드 추가

--- src/main/java/BaseBallMain.java
+++ src/main/java/BaseBallMain.java
@@ -1,22 +1,34 @@
 import util.BaseBallUtil;
 
 import java.util.Arrays;
+import java.util.Scanner;
+
+import static common.Constant.TEXT_GAME_END;
+import static common.Constant.TEXT_GAME_RESTART;
 
 public class BaseBallMain
 {
-
-    // 메인메소드
     public static void main(String[] args){
         // 게임 시작,종료 판단
-        boolean isContinue = true;
         // 컴퓨터 랜덤값
         BaseBallUtil baseBallUtil = new BaseBallUtil();
         int[] computerNumbers = baseBallUtil.generatorNumbers();
         System.out.println(Arrays.toString(computerNumbers));
-//        baseBallUtil.playGame(computerNumbers);
-//        isContinue = gameEndCheck();
+        baseBallUtil.playGame(computerNumbers);
+        gameEndCheck();
 
     }
-
+    // 게임 계속, 종료 메서드
+    private static void gameEndCheck() {
+        Scanner scanner = new Scanner(System.in);
+        int gameFlag = 1;
+        boolean gameStatus = false;
+        System.out.println(TEXT_GAME_END);
+        System.out.println(TEXT_GAME_RESTART);
+        int input = scanner.nextInt();
+        if (gameFlag == input) {
+            gameStatus = true;
+        }
+    }
 
 }
