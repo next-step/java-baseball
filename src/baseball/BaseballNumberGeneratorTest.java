b34fe04f96442d5b771edea70bac7443bdca5ff8
stua92 <stau92@gmail.com>
2021-04-27 19:04:06 +0900

    feat: 1~9범위의 서로 다른 3개의 숫자를 랜덤 생성하는 테스트

---
+++ src/baseball/BaseballNumberGeneratorTest.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BaseballNumberGeneratorTest {
+
+	@Test
+	void testCreateNumber() {
+		fail("Not yet implemented");
+	}
+
+}
