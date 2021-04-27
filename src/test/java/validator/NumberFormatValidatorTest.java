6005f933dc84b7db52e68904237d2f3574a7f050
seungkwon <yevgnenll@gmail.com>
2021-04-27 20:12:50 +0900

    refactor: test 코드에 동일한 코드가 많아 공통 모듈로 분리한다.

--- src/test/java/validator/NumberFormatValidatorTest.java
+++ src/test/java/validator/NumberFormatValidatorTest.java
@@ -3,6 +3,7 @@ package validator;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,29 +13,33 @@ class NumberFormatValidatorTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"1l1", "숫자아님", "/12", "3:5"})
 	void inputIsNumber(String input) {
-		assertThrows(IllegalArgumentException.class, () -> NumberFormatValidator.validate(input));
-		assertThrows(IllegalArgumentException.class, () -> NumberFormatValidator.checkParseToInteger(input));
+		assertIllegalArgumentException(() -> NumberFormatValidator.validate(input));
+		assertIllegalArgumentException(() -> NumberFormatValidator.checkParseToInteger(input));
 	}
 
 	@DisplayName(value = "숫자에 같은 수가 있을 경우 IllegalArgumentException 을 일으킨다")
 	@ParameterizedTest
 	@ValueSource(strings = {"171", "777"})
 	void validateNumber(String input) {
-		assertThrows(IllegalArgumentException.class, () -> NumberFormatValidator.validate(input));
+		assertIllegalArgumentException(() -> NumberFormatValidator.validate(input));
 	}
 
 	@DisplayName(value = "3자리가 아니면 IllegalArgumentException 을 일으킨다")
 	@ParameterizedTest
 	@ValueSource(strings = {"9867", "12"})
 	void numberLength(String input) {
-		assertThrows(IllegalArgumentException.class, () -> NumberFormatValidator.validate(input));
+		assertIllegalArgumentException(() -> NumberFormatValidator.validate(input));
 	}
 
 	@DisplayName(value = "입력에 0이 있으면 IllegalArgumentException 을 일으킨다")
 	@ParameterizedTest
 	@ValueSource(strings = {"308", "204", "910"})
 	void noZero(String input) {
-		assertThrows(IllegalArgumentException.class, () -> NumberFormatValidator.validate(input));
+		assertIllegalArgumentException(() -> NumberFormatValidator.validate(input));
+	}
+
+	private void assertIllegalArgumentException(Executable executable) {
+		assertThrows(IllegalArgumentException.class, executable);
 	}
 
 }
