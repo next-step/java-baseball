7a919256aef6decb7e2e6e103ce2297ef390fb4b
DESKTOP-OP3B5IB\kmyms <kmyms2@naver.com>
2021-04-27 13:25:27 +0900

    Fix variable property
    - static으로 선언한 변수 지역 변수로 변경

--- src/main/java/generator/NumGenerator.java
+++ src/main/java/generator/NumGenerator.java
@@ -7,19 +7,19 @@ public class NumGenerator {
     private final static int FIGURE = 3;
     private final static int MIN = 1;
     private final static int MAX = 9;
-    private static int[] num = new int[FIGURE];
-    private static Set<Integer> chkNum = new HashSet<Integer>();
 
     /*
         서로 다른 숫자로 이루어진 3자리 숫자 반환
      */
     public static String numGenerator(){
         String numStr = "";
+        int[] num = new int[FIGURE];
+        Set<Integer> chkNum = new HashSet<Integer>();
         num[0] = (int)(Math.random()*(MAX - MIN + 1)) + 1;
         numStr = numStr.concat(Integer.toString(num[0]));
         chkNum.add(num[0]);
         for(int i=1 ; i<FIGURE ; i++){
-            num[i] = newNum();
+            num[i] = newNum(num, chkNum);
             numStr = numStr.concat(Integer.toString(num[i]));
         }
         return numStr;
@@ -28,7 +28,7 @@ public class NumGenerator {
     /*
         중복 체크 후 임의의 한자리 숫자 반환
      */
-    public static int newNum(){
+    public static int newNum(int[] num, Set<Integer> chkNum){
         int now = 0;
         do{
             now = (int)(Math.random()*(MAX - MIN + 1)) + 1;
