41386a96a7e578779221c0c2efa6224a80d31c21
JisuLim <sanola3@gmail.com>
2021-04-27 22:30:48 +0900

    feat: add Evaluation class
    evaluate method added
    - it evaluates player's input number
    - check strikes and balls

---
+++ src/main/java/services/Evaluation.java
@@ -0,0 +1,42 @@
+package services;
+
+import java.util.List;
+
+public class Evaluation {
+	private int strikeCount;
+	private int ballCount;
+
+	public Evaluation() {
+		this.strikeCount = 0;
+		this.ballCount = 0;
+	}
+
+	public int getStrikeCount() {
+		return strikeCount;
+	}
+
+	public int getBallCount() {
+		return ballCount;
+	}
+
+	public void evaluate(String answerNumber, List<Integer> problemNumber) {
+		int strikeCount = 0;
+		int ballCount = 0;
+
+		for (int i = 0; i < answerNumber.length(); i++) {
+			strikeCount += getEvaluatedStrikeCount(answerNumber.charAt(i), problemNumber.get(i));
+			ballCount += getEvaluatedBallCount(answerNumber.charAt(i), problemNumber);
+		}
+
+		this.strikeCount = strikeCount;
+		this.ballCount = ballCount - strikeCount;
+	}
+
+	private int getEvaluatedBallCount(char answerNumber, List<Integer> problemNumber) {
+		return problemNumber.contains(Character.getNumericValue(answerNumber)) ? 1 : 0;
+	}
+
+	private int getEvaluatedStrikeCount(char answerNumber, int problemNumber) {
+		return problemNumber == Character.getNumericValue(answerNumber) ? 1 : 0;
+	}
+}
