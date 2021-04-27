636e99bef2f371e2557e8167537d046381cef74f
shin <shin@LAPTOP-MM4U4FF8>
2021-04-27 17:50:34 +0900

    feat: 입력 받은 수 범위 체크 및 입력 받은 수 숫자 중복 체크

--- src/main/java/play/InputNumberProcess.java
+++ src/main/java/play/InputNumberProcess.java
@@ -7,6 +7,9 @@ import java.util.Set;
 import common.MessagesCommon;
 
 public class InputNumberProcess {
+
+	public static final int MIN_VALUE = 123;
+	public static final int MAX_VALUE = 987;
 	
 	public static void inputWaitingProcess(Map<Integer, Integer> inNumMap, Set<Integer> inNumSet) {
 		System.out.print(MessagesCommon.INPUT_MSG);
@@ -21,5 +24,43 @@ public class InputNumberProcess {
 		
 		int inputNum = scan.nextInt();
 
+		valueCheckProcess(inputNum, inNumMap, inNumSet);
+	}
+
+	public static void valueCheckProcess(int inputNum, Map<Integer, Integer> inNumMap, Set<Integer> inNumSet) {
+		numMinCheck(inputNum, inNumMap, inNumSet);
+		numMaxCheck(inputNum, inNumMap, inNumSet);
+		numOverlapCheck(inputNum, inNumMap, inNumSet);
+	}
+	
+	public static void numMinCheck(int inputNum, Map<Integer, Integer> inNumMap, Set<Integer> inNumSet) {
+		if (numMinCheck(inputNum)) {
+			System.out.print(MessagesCommon.ERROR_MIN_MSG);
+			inputWaitingProcess(inNumMap, inNumSet);
+		}
+	}
+
+	public static boolean numMinCheck(int inputNum) {
+		return inputNum < MIN_VALUE;
+	}
+
+	public static void numMaxCheck(int inputNum, Map<Integer, Integer> inNumMap, Set<Integer> inNumSet) {
+		if (numMaxCheck(inputNum)) {
+			System.out.print(MessagesCommon.ERROR_MAX_MSG);
+			inputWaitingProcess(inNumMap, inNumSet);
+		}
+	}
+	
+	public static boolean numMaxCheck(int inputNum) {
+		return inputNum > MAX_VALUE;
+	}
+
+	public static void numOverlapCheck(int inputNum, Map<Integer, Integer> inNumMap, Set<Integer> inNumSet) {
+		inNumSet.clear();
+
+		if (inNumSet.size() != 3) {
+			System.out.print(MessagesCommon.ERROR_OVERLAP_MSG);
+			inputWaitingProcess(inNumMap, inNumSet);
+		}
 	}
 }
