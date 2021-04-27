63445aab86b46aef989f6742d312251a26a6fe11
John Doe <eozzz23@gmail.com>
2021-04-27 09:07:33 +0900

    feat(InputHelper): 숫자야구 게임 관련 표준 입력을 처리하는 클래스
    - 게임 로직과 상관없는 기능을 하는 클래스를 따로 분리
    - 사용자의 입력을 처리하는 클래스 작성

---
+++ src/main/java/com/precourse/baseball/helper/input/InputHelper.java
@@ -0,0 +1,45 @@
+package com.precourse.baseball.helper.input;
+
+import java.util.Scanner;
+
+import com.precourse.baseball.helper.display.DisplayHelper;
+import com.precourse.baseball.helper.validator.Validator;
+
+/**
+ * 숫자야구 게임 관련 표준 입력을 처리하는 클래스
+ * 사용방법 : InputHelper.getBallInput();
+ * @author eoz
+ * @version 1.0
+ */
+public class InputHelper {
+	private static final String GAME_CONTINUE = "1";
+	private static final String GAME_EXIT = "2";
+	private static Scanner sc = new Scanner(System.in);
+	
+	
+	public static String getBallInput() {
+		String result = "";
+		while(true) {
+			String input = sc.nextLine();
+			if( Validator.validInput(input) ) {
+				result = input;
+				break;
+			}
+			
+			DisplayHelper.wrongInputMessage();
+			DisplayHelper.inputMessage();
+		}
+		
+		return result;
+	}
+	
+	public static boolean continueGame() {
+		DisplayHelper.strikeOutMessage();
+		DisplayHelper.countinueMessage();	
+		
+		String input = sc.nextLine();
+		
+		return GAME_CONTINUE.equals(input);
+	}
+
+}
