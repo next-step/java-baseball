6e2ad3f36f1f1fd6002a25c399a05e7830ad95ba
최홍희 <vvshinevv@nhntoast.com>
2021-04-27 17:47:28 +0900

    feat : PlayBall 비지니스 로직 구현
     - PlayBall 은 1부터 9까지 서로 다른 수로 이루어진 3자리여야 한다.

---
+++ src/main/java/playball/domain/Balls.java
@@ -0,0 +1,52 @@
+package playball.domain;
+
+import playball.exception.PlayBallIllegalArgumentException;
+import playball.util.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Balls {
+
+    private static final Integer BALLS_SIZE = 3;
+    private static final Integer UPPER_BOUND_OF_RANDOM_BALL_NUMBER = 10;
+
+    private final List<Ball> balls;
+
+    private Balls(List<Ball> balls) {
+        validate(balls);
+        this.balls = balls;
+    }
+
+    private void validate(List<Ball> balls) {
+        if (!isValidBallsSize(balls)) {
+            throw new PlayBallIllegalArgumentException();
+        }
+    }
+
+    private Boolean isValidBallsSize(List<Ball> balls) {
+        return balls.size() == BALLS_SIZE;
+    }
+
+    public List<Ball> getBalls() {
+        return Collections.unmodifiableList(balls);
+    }
+
+    public static Balls of(List<Ball> balls) {
+        return new Balls(balls);
+    }
+
+    public static Balls createRandomBalls() {
+        Set<Ball> balls = new HashSet<>();
+        Integer index = 1;
+        while (balls.size() != BALLS_SIZE) {
+            Integer generated = NumberGenerator.generateNumbers(UPPER_BOUND_OF_RANDOM_BALL_NUMBER);
+            Ball ball = Ball.of(generated);
+            balls.add(ball);
+        }
+        return Balls.of(new ArrayList<>(balls));
+    }
+}
