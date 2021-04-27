800b298bdeffb0a75a43d1d1981609253949ad49
조예찬 <choyuichan@gmail.com>
2021-04-27 12:40:49 +0900

    리펙터: 10줄 안넘어가도록 메시드 추출
    10줄 안넘어가도록 메서드 추출 및 변경

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -5,6 +5,9 @@ public class BaseballGame {
 
 	final static Random r = new Random();
 
+	final String CONTINUE = "1";
+	final String EXIT = "2";
+
 	boolean running = true;
 	boolean exit = false;
 
@@ -18,10 +21,10 @@ public class BaseballGame {
 	}
 
 	public void continueOrExit(String continuePlay){
-		if("1".equals(continuePlay)){
+		if(CONTINUE.equals(continuePlay)){
 			running = true;
 		}
-		if("2".equals(continuePlay)){
+		if(EXIT.equals(continuePlay)){
 			exit = true;
 		}
 	}
@@ -41,18 +44,20 @@ public class BaseballGame {
 		return generateNumbers(null,3);
 	}
 
-	public String guess(String answerNumbers, String guessNumbers){
-
+	public String guess(String guessNumbers,String answerNumbers){
 		int[] count = {0,0};
+		return output(guess(guessNumbers,answerNumbers,count));
+	}
+
+	private int[] guess(String guessNumbers,String answerNumbers,int[] count){
+		int index = 0;
 
 		String[] splitedGuessNumbers = guessNumbers.split("");
 
-		int i = -1;
-		for(String guessNumber : splitedGuessNumbers){
-			count = checkNumbers(guessNumber, answerNumbers, ++i, count);
+		for(String guessNumber : guessNumbers.split("")){
+			count = checkNumbers(guessNumber, answerNumbers, index++, count);
 		}
-
-		return output(count);
+		return count;
 	}
 
 	private String generateNumbers(String all,int length){
@@ -76,34 +81,55 @@ public class BaseballGame {
 		int strike = count[0];
 		int ball = count[1];
 
-		if(strike==0 && ball==0){
-			return "낫싱";
-		}
+		return output(strike, ball);
+	}
+
+	private String output(int strike, int ball){
+		return isNothing(strike,ball)?"낫싱":(isStrike(strike,ball)?outputContinueOrExit():outputStrikeOrBall(strike,ball));
+	}
+
+	private boolean isStrike(int strike,int ball){
+		return strike==3;
+	}
+
+	private boolean isNothing(int strike,int ball){
+		return strike==0 && ball==0;
+	}
+
+	private String outputContinueOrExit(){
+		running = false;
+		return "3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	}
 
 
-		if(strike==3){
-			running = false;
-			return "3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-		}
-
+	private String outputStrikeOrBall(int strike, int ball){
 		String strikeOutput = strike>0? strike + "스트라이크":"";
 		String ballOutput = ball>0?ball + "볼":"";
-
 		return strikeOutput + (strikeOutput.length()>0 && ballOutput.length()>0 ? " ":"") + ballOutput;
 	}
 
+
 	private int[] checkNumbers(String guessNumber,String answerNumbers, int index, int[] count) {
-		String[] splitedAnswerNumbers = answerNumbers.split("");
-		if(answerNumbers.indexOf(guessNumber) < 0){
+		String answerNumber = answerNumbers.substring(index,index+1) ;
+		if(isNothing(guessNumber, answerNumbers)){
 			return count;
 		}
+		return checkNumber(guessNumber, answerNumber, count);
+	}
 
-		if(guessNumber.equals(splitedAnswerNumbers[index])){
+	private boolean isNothing(String guessNumber, String answerNumbers){
+		return answerNumbers.indexOf(guessNumber) < 0;
+	}
+
+
+	private int[] checkNumber(String guessNumber, String answerNumber,int[] count){
+		if(guessNumber.equals(answerNumber)){
 			count[0]+=1;
 			return count;
 		}
 		count[1]+=1;
 		return count;
+
 	}
 
 }
