249ebdf46f78ee172256ce8f9d7cb90c5b2fea21
shin <shin@LAPTOP-MM4U4FF8>
2021-04-27 18:14:59 +0900

    feat: 볼 체크

--- src/main/java/play/CompareProcess.java
+++ src/main/java/play/CompareProcess.java
@@ -4,12 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CompareProcess {
-	
+
 	public static final int COMPARE_NUM_CNT = 4;
 	public static final String STRIKE_CNT = "STRIKE_CNT";
+	public static final String BALL_CNT = "BALL_CNT";
+	public static final String STRIKE_MSG = " 스트라이크 ";
 
 	public static boolean gameProcess(Map<Integer, Integer> ranNumMap, Map<Integer, Integer> inNumMap) {
 		Map<String, Integer> resultMap = new HashMap<String, Integer>();
+		resultMap.put(STRIKE_CNT, 0);
+		resultMap.put(BALL_CNT, 0);
 
 		int seatNum = 0;
 
@@ -65,7 +69,9 @@ public class CompareProcess {
 	public static void middleProceed(Map<Integer, Integer> ranNumMap, Map<Integer, Integer> inNumMap,
 		Map<String, Integer> resultMap, int seatNum) {
 
-		strikeCheck(ranNumMap, inNumMap, resultMap, seatNum);
+		if (!strikeCheck(ranNumMap, inNumMap, resultMap, seatNum)) {
+			ballCheck(ranNumMap, inNumMap, resultMap, seatNum);
+		}
 
 		compareProcess(ranNumMap, inNumMap, resultMap, seatNum);
 	}
@@ -80,5 +86,28 @@ public class CompareProcess {
 		}
 
 		return compareResult;
+	}
+
+	public static void ballCheck(Map<Integer, Integer> ranNumMap, Map<Integer, Integer> inNumMap,
+		Map<String, Integer> resultMap, int seatNum) {
+
+		Map<Integer, Integer> tmpRanNumMap = setTmpMap(ranNumMap, seatNum);
+
+		for (Integer key : tmpRanNumMap.keySet()) {
+			valueResultProcess(valueCompare(tmpRanNumMap.get(key), inNumMap.get(seatNum)),
+				ranNumMap, resultMap, key, BALL_CNT);
+		}
+	}
+
+	public static Map<Integer, Integer> setTmpMap(Map<Integer, Integer> ranNumMap, int seatNum) {
+		Map<Integer, Integer> tmpMap = new HashMap<Integer, Integer>();
+
+		for (Integer key : ranNumMap.keySet()) {
+			tmpMap.put(key, ranNumMap.get(key));
+		}
+
+		tmpMap.remove(seatNum);
+
+		return tmpMap;
 	}
 }
