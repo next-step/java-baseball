d1336884cb5f9a80e5e244e8b5a2cf6959fd7f7a
Eden Kang <kg0116@lycle.co.kr>
2021-04-27 20:39:08 +0900

    refactor : Separate playGame logic
    main 메서드 안에서 한번에 처리되던 게임 진행에 관한 로직을 각각 playGame, checkGame, endGame, stopGame 으로 분리함
    
    playGame : 유저의 answer를 입력받고 checkGame을 호출
    checkGame : 유저의 answer를 확인 하여 endGame 호출
    endGame : 게임종료 변수가 1인 경우, 게임 재실행/종료 여부 input 받고 stopGame 호출
    stopGame : 유저의 게임 종료 의사를 확인 후, 2인 경우 종료처리

--- src/main/java/BaseballProject.java
+++ src/main/java/BaseballProject.java
@@ -2,44 +2,68 @@ import java.util.Scanner;
 
 public class BaseballProject {
 
+    boolean isCorrect; // 정답확인 여부
+    boolean isContinue = true; // 계속진행 여부
+
+    // TODO : 게임 실행 메서드
+    public static void playGame(BaseballProject project, BaseBallGame baseBallGame){
+        while(!project.isCorrect){
+            Scanner scanner = new Scanner(System.in);
+            // TODO : 1. input user answer
+            System.out.println("숫자를 입력해주세요");
+            String userAnswer = scanner.next();
+
+            // TODO : 2. check user' Answer & show resultMessage
+            checkGame(baseBallGame, userAnswer, project);
+        }
+    }
+
+    // TODO : 게임 결과 확인 후 진행 로직
+    public static void checkGame(BaseBallGame baseBallGame, String userAnswer, BaseballProject project){
+
+        // TODO : 2. check user' Answer & show resultMessage
+        int result = baseBallGame.checkAnswer(userAnswer);
+        String resultMessage = baseBallGame.makeResultMessage(baseBallGame.checkResultModel);
+        System.out.println(resultMessage);
+        baseBallGame.resetModel(baseBallGame.checkResultModel);
+
+        // TODO : 3. control isContinue to restart or stop game
+        endGame(result, project);
+    }
+
+    // TODO : 게임 종료 여부 확인 후, 종료/재시작 조치
+    public static void endGame(int result, BaseballProject project){
+        if(result == 1){
+            project.isCorrect = true;
+            System.out.println("게임을 새로 시작하시려면 1, 아니면 2를 입력해주세요");
+            Scanner scanner = new Scanner(System.in);
+            String willContinue = scanner.next();
+            stopGame(willContinue, project);
+        }
+    }
+
+    // TODO : 게임 종료 의사 확인 후, 종료
+    public static void stopGame(String willContinue, BaseballProject project){
+        if(willContinue.equals("2") ){
+            project.isContinue = false;
+            System.out.println("게임이 종료되었습니다.");
+        }
+    }
+
     public static void main(String [] args){
 
-        boolean isCorrect; // 정답확인 여부
-        boolean isContinue = true; // 계속진행 여부
-
-        while(isContinue){
+        BaseballProject project = new BaseballProject();
+        project.isCorrect = false;
+        while(project.isContinue){
 
             System.out.println("게임을 시작합니다.");
 
             // TODO : 0. init
+            project.isCorrect = false;
             BaseBallGame baseBallGame = new BaseBallGame();
             baseBallGame.init();
-            isCorrect = false;
-//            System.out.println("target : " + baseBallGame.targetNumberArray.get(0) + baseBallGame.targetNumberArray.get(1) + baseBallGame.targetNumberArray.get(2));
-            Scanner scanner = new Scanner(System.in);
-            while(!isCorrect){
-
-                // TODO : 1. input user answer
-                System.out.println("숫자를 입력해주세요");
-                String userAnswer = scanner.next();
-
-                // TODO : 2. check user' Answer & show resultMessage
-                int result = baseBallGame.checkAnswer(userAnswer);
-                String resultMessage = baseBallGame.makeResultMessage(baseBallGame.checkResultModel);
-                System.out.println(resultMessage);
-                baseBallGame.resetModel(baseBallGame.checkResultModel);
-
-                // TODO : 3. control isContinue to restart or stop game
-                if(result == 1){
-                    isCorrect = true;
-                    System.out.println("게임을 새로 시작하시려면 1, 아니면 2를 입력해주세요");
-                    String willContinue = scanner.next();
-                    if(willContinue.equals("2") ){
-                        isContinue = false;
-                        System.out.println("게임이 종료되었습니다.");
-                    }
-                }
-            }
+            System.out.println("target : " + baseBallGame.targetNumberList.get(0) + baseBallGame.targetNumberList.get(1) + baseBallGame.targetNumberList.get(2));
+            playGame(project, baseBallGame);
         }
     }
 }
