368b4f6b26c96e31466ce37564795d5325acf849
bwowby <pring120@gmail.com>
2021-04-27 21:50:49 +0900

    test : Junit,AssertJ 학습
    -ParameterizedTest
    -CsvSource

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,49 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp(){
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set size()메소드 테스트")
+    @Test
+    void sizeTest(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    @DisplayName("contains() 메소드 테스트")
+    @Test
+    void containsTest(int input){
+        assertThat(numbers.contains(input)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true","2:true","3:true","4:false", "5:false"},delimiter = ':')
+    @DisplayName("contains() true/false 케이스 테스트")
+    void containsTrueFalseTest(int input,boolean expected){
+        assertEquals(numbers.contains(input),expected);
+    }
+}
