7a4e1cd47e18a648d3a0b40200a41bae94131b33
jonghaha <bgh9197@naver.com>
2021-04-27 19:27:49 +0900

    test: User 입력 숫자 중복 테스트

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -25,4 +25,21 @@ public class UserTest {
 //		}).withMessage("입력한 숫자는 %s자리 입니다.", testInput2.length())
 //				.withMessageContaining("세자리 숫자가 아닙니다.");
 	}
+
+	@Test
+	@DisplayName("유저가 입력한 숫자의 중복 테스트")
+	public void 유저_입력값_중복_체크() {
+//		String testInput1 = "486";
+//
+//		assertThatCode(() -> {
+//			user.validateNumber(testInput1);
+//		}).doesNotThrowAnyException();
+
+		String testInput2 = "446";
+
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			user.validateNumber(testInput2);
+		}).withMessage("입력 숫자 %s의 중복되는 수가 있습니다.", testInput2)
+				.withMessageContaining("중복되는 수가 있습니다.");
+	}
 }
