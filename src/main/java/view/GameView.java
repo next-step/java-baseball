6e9e785ede037a297abca09a2ba66fad9e0ba29a
JJuniJJuni <qkrgud305@naver.com>
2021-04-27 12:14:32 +0900

    feat: 사용자로부터 선택지 입력 구현(UI)
    사용자로부터 선택지 입력 구현(UI)
    - 입력 화면 구현
    - 입력한 수가 제시한 형식에 맞는 지 검증 로직 추가

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -2,6 +2,7 @@ package view;
 
 import java.util.Scanner;
 
+import constant.NumberValidation;
 import dto.GameResult;
 import validator.NumberValidator;
 
@@ -20,5 +21,24 @@ public class GameView {
 	public static boolean resultView(GameResult gameResult) {
 		System.out.println(gameResult.getMsg());
 		return gameResult.getSuccessFlag();
+	}
+
+	public static boolean endView() {
+		Scanner scanner = new Scanner(System.in);
+		String inputNumber;
+		do {
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+			inputNumber = scanner.nextLine();
+		} while (isNotValidEndViewInput(inputNumber));
+		return inputNumber.charAt(0) == '1';
+	}
+
+	private static boolean isNotValidEndViewInput(String inputNumber) {
+		return !NumberValidation.IsNotEmptyString.getValidate().test(inputNumber) ||
+			!validateEndViewNumber(inputNumber);
+	}
+
+	private static boolean validateEndViewNumber(String inputNumber) {
+		return inputNumber.matches("^[1-2]$");
 	}
 }
