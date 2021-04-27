3e47e087b8b156f2ae73eb56535e89e9780fb10c
최인수 <insu.choi@innowireless.com>
2021-04-27 19:11:59 +0900

    feat: 3자리 랜덤 숫자 생성 기능 추가

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -1,7 +1,24 @@
 import java.util.Random;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public class BaseBallGame {
 	public static int getRandomNumber() {
 		return new Random().nextInt(9) + 1;
+	}
+
+	public static int[] getRandomNumber3Length() {
+		int[] targetNumber = new int[3];
+
+		int count = 0;
+		while (count < 3) {
+			int number = getRandomNumber();
+			if (!ArrayUtils.contains(targetNumber, number)) {
+				targetNumber[count] = number;
+				count++;
+			}
+		}
+
+		return targetNumber;
 	}
 }
