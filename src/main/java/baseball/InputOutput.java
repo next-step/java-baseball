6d6d1f08c3b1ac14e65ee9cf730f0ad227636698
ShinHan <tlsgks8668@naver.com>
2021-04-27 15:47:18 +0900

    사용자가 세 자리수를 입력

--- src/main/java/baseball/InputOutput.java
+++ src/main/java/baseball/InputOutput.java
@@ -1,37 +1,43 @@
 package baseball;
 
-import java.io.*;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class Input {
-    private static BufferedReader br;
-    private static BufferedWriter bw;
-    private static final String pattern = "^[0-9]*$";
-
-    private static Output output = new Output();
+public class InputOutput {
+    private static Scanner scan = new Scanner(System.in);
     private static Message message = new Message();
 
-    public Input() {}
+    private static final String pattern = "^[0-9]*$";
 
-    public static int number() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
+    public InputOutput() {}
 
 
-        String input = br.readLine();
+    public static String input() {
+        String input = scan.nextLine();
 
-        while(!checkInputNumber(input)) {
-            print(message.INPUT_NUMBER_INVALID_VALUE);
-            input = br.readLine();
+        return input;
+    }
+
+    public static int inputNumber() {
+        String input = input();
+
+        while (!checkInputNumber(input)) {
+            println(message.INPUT_NUMBER_INVALID_VALUE);
+            print(message.INPUT_NUMBER);
+            input = input();
         }
-
-        int inputNumber = Integer.parseInt(input);
+        int inputNumber = parseInt(input);
 
         return inputNumber;
     }
 
-    public static boolean checkInputNumber(String input) throws IOException {
-        if (!Pattern.matches(pattern, input)) {
+    public static boolean checkInputNumber(String input) {
+        if (!Pattern.matches(pattern, input) || input.equals("")) {
             return false;
+        }
+
+        if (parseInt(input.trim()) == 9) {
+            return true;
         }
 
         if (input.length() != 3) {
@@ -41,22 +47,15 @@ public class Input {
         return true;
     }
 
-    public static void print(String str) throws IOException {
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(str);
-
-        bw.flush();
+    public static void print(String str) {
+        System.out.print(str);
     }
 
-    public static void print(int num) throws IOException {
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(num);
-
-        bw.flush();
+    public static void println(String str) {
+        System.out.println(str);
     }
 
-    public static void endInputOutput() throws IOException {
-        br.close();
-        bw.close();
+    private static int parseInt(String input) {
+        return Integer.parseInt(input);
     }
 }
