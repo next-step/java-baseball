5669b92dab56e6a30a36fffab171bc0bf75a0cd3
moon_ <m3252@naver.com>
2021-04-27 21:25:47 +0900

    refactor(InputDevice): 입력 값에 대한 유효성 추가 및 리턴 타입 변경

--- src/main/java/view/InputDevice.java
+++ src/main/java/view/InputDevice.java
@@ -3,33 +3,51 @@ package view;
 import baseball.BaseballNumber;
 import baseball.enumerator.GameProgress;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class InputDevice {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    public static int inputNumber() {
+    public static List<BaseballNumber> inputNumber() {
         String answerEntered = SCANNER.nextLine().trim();
-        checkInputValidationLength(answerEntered);
-        checkInputValidationValue(answerEntered);
-        return Integer.parseInt(answerEntered);
+        checkInputValidRange(answerEntered);
+        checkInputValidValue(answerEntered);
+        checkInputValidDuplicate(answerEntered);
+        return stringToBaseballNumbers(answerEntered);
     }
 
-    private static void checkInputValidationLength(String answerEntered){
-        if(answerEntered.length() != 3){
+    public static void checkInputValidRange(String answerEntered) {
+        if (answerEntered.length() != 3) {
             throw new IllegalArgumentException("3 자리 숫자만 입력 가능합니다.");
         }
     }
-    private static void checkInputValidationValue(String answerEntered){
-        try{
+
+    public static void checkInputValidValue(String answerEntered) {
+        try {
             Integer.parseInt(answerEntered);
-        }catch (NumberFormatException numberFormatException){
+        } catch (NumberFormatException numberFormatException) {
             numberFormatException.printStackTrace();
             throw new NumberFormatException("숫자만 입력 가능합니다.");
         }
+    }
+
+    public static void checkInputValidDuplicate(String answerEntered){
+        Set<Character> duplicateHelper = new HashSet<>();
+        for(int i = 0; i < answerEntered.length(); i++) {
+            duplicateHelper.add(answerEntered.charAt(i));
+        }
+        if(duplicateHelper.size() != answerEntered.length()){
+            throw new IllegalArgumentException("중복된 숫자는 입력할 수 없습니다.");
+        }
+    }
+
+    public static List<BaseballNumber> stringToBaseballNumbers(String answerEntered) {
+        List<BaseballNumber> baseballNumbers = new ArrayList<>();
+        for(int i = 0; i < answerEntered.length(); i++) {
+            baseballNumbers.add(new BaseballNumber(answerEntered.charAt(i)));
+        }
+        return baseballNumbers;
     }
 
     public void checkProcessCodeRange(int processCode) {
