6456801108ba92d014f4bedc1810bc4959446380
jaewanLee <ianlee9180@gmail.com>
2021-04-27 19:56:31 +0900

    feat: InputHandler Class 추가
    InputHandler Class
    - splitInput - 넘겨 받은 int값을 3개의 List로 반환합니다.
    - isValidateInputList - 넘겨 받은 inpustList에 0값이 존재하는지, 중복값이 존재하는 검사합니다.
    - isValidateInputRange - 넘겨받은 int값이 3자리 수인지 검사합니다.

---
+++ src/main/java/InputHandler.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class InputHandler {
+    private final RandomUtil randomUtil = new RandomUtil();
+
+    public List<Integer> splitInput(int input) {
+        return Arrays.asList(input / 100, input % 100 / 10, input % 10);
+    }
+
+    public boolean isValidateInputList(List<Integer> inputList) {
+        //0 검사
+        for (int v : inputList) {
+            if (v == 0) {
+                return false;
+            }
+        }
+        //중복수 검사
+        return !randomUtil.IsDuplicatedNumList(inputList);
+    }
+
+    public void isValidateInputRange(int input) {
+        if (input < 100 || input > 1000) throw new IllegalArgumentException("숫자는 3자리 수여야 합니다.");
+    }
+
+}
