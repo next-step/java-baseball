021e28ff3b282e189ebeb1606dd2512b9a5fd29d
Kim Heeseok <zxcasd12@gmail.com>
2021-04-27 22:20:52 +0900

    feat(test): Add missing file.
    Add missing file from c17ef0ed feat(domain): Add random number logic

--- src/test/java/baseball/domain/GameTest.java
+++ src/test/java/baseball/domain/GameTest.java
@@ -1,2 +1,34 @@
-package baseball.domain;public class GameTest {
+package baseball.domain;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameTest {
+
+	@RepeatedTest(100)
+	void testRandomIfNumberIsInRange() {
+		Game game = new Game();
+		game.start();
+
+		int[] numbers = game.getTarget();
+		for (int i: numbers) {
+			assert(i >= 0 && i <= 9);
+		}
+	}
+
+	@RepeatedTest(100)
+	void testRandomIfNumberDuplicated() {
+		Game game = new Game();
+		game.start();
+
+		int[] numbers = game.getTarget();
+		Set<Integer> set = new HashSet();
+		for (int i: numbers) {
+			assertThat(set).doesNotContain(i);
+		}
+	}
 }
