9ba089f46a9fcf7698dfca4bcda061029fd3e2d0
ascii92der <ascii92der@gmail.com>
2021-04-27 14:25:15 +0900

    기능 추가
    플레이어의 3개의 숫자 입력
    - 3개의 숫자 입력 기능
    - 3개의 숫자의 범위 유효성 체크
    - 3개의 숫자의 중복 유효성 체크
    게임 재시작/종료
    - 플레이어 입력 기능
    - 입력된 값 유효성 체크

---
+++ src/main/java/com/ascii92der/baseball/ui/Massage.java
@@ -0,0 +1,10 @@
+package com.ascii92der.baseball.ui;
+
+public class Massage {
+
+    public static final String REQUEST_INPUT_MUNER = "숫자를 입력해주세요 : ";
+    public static final String INVALID_INPUT ="잘못된 값이 입력되었습니다. 다시 입력해주세요 : ";
+    public static final String REQUEST_INPUT_COMMAND = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String INVALID_COMMAND ="잘못된 값이 입력되었습니다. 다시 입력해주세요. [1 or 2] : ";
+    public static final String DUPLICATED_INPUT = "중복된 숫자가 입력되었습니다. 다시 입력해주세요 : ";
+}
---
+++ src/main/java/com/ascii92der/baseball/ui/outputUI.java
@@ -0,0 +1,7 @@
+package com.ascii92der.baseball.ui;
+
+public class outputUI {
+    public void resultMassage(String massege){
+        System.out.println(massege);
+    }
+}
