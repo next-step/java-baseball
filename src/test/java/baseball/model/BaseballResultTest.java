1b10c68c11f2154ae0beb614d8c5062373b5fa96
Shinyoung Kim <rolroralra@gmail.com>
2021-04-27 20:42:29 +0900

    test: add test case for exception

--- src/test/java/baseball/model/BaseballResultTest.java
+++ src/test/java/baseball/model/BaseballResultTest.java
@@ -1,18 +1,22 @@
 package baseball.model;
 
+import baseball.constants.BaseballConstant;
+import baseball.exception.BaseballNumberFormatException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class BaseballResultTest {
+    @DisplayName(value = "BaseballResult calcResult Test_1_1 Success Case")
     @ParameterizedTest(name = "BaseballNumber.builder().("+ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER+").build()")
     @MethodSource(value = {
-            "baseball.model.TestParametersProvider#provideSizeAndRadix",
-            "baseball.model.TestParametersProvider#provideNonPositiveSizeOrNonPositiveSizeRadix",
-            "baseball.model.TestParametersProvider#provideTooMuchSizeOrTooMuchRadix"
+            "baseball.model.TestParametersProvider#provideSizeAndRadix"
     })
-    public void test1_BaseballResult_calcResult(int size, int radix) {
+    public void test1_1_BaseballResult_calcResult(int size, int radix) {
         BaseballNumber left = BaseballNumber.builder().size(size).radix(radix).build();
         BaseballNumber right = BaseballNumber.builder().size(size).radix(radix).build();
         BaseballResult baseballResult = BaseballResult.calcResult(left, right);
@@ -24,17 +28,27 @@ public class BaseballResultTest {
         assertThat(baseballResult.getBaseballNumbers()).hasSize(2);
         assertThat(baseballResult.getStrike()).isBetween(0, left.getSize());
         assertThat(baseballResult.getBall()).isBetween(0, left.getSize());
-        assertThat(baseballResult.getRadix()).isBetween(BaseballNumber.MIN_RADIX, BaseballNumber.MAX_RADIX);
-        assertThat(baseballResult.getSize()).isBetween(BaseballNumber.MIN_RADIX - 1, BaseballNumber.MAX_RADIX - 1);
+        assertThat(baseballResult.getRadix()).isBetween(BaseballConstant.MIN_RADIX, BaseballConstant.MAX_RADIX);
+        assertThat(baseballResult.getSize()).isBetween(BaseballConstant.MIN_SIZE, BaseballConstant.MAX_SIZE);
     }
 
+    @DisplayName(value = "BaseballResult calcResult Test_1_2 Exception Case")
     @ParameterizedTest(name = "BaseballNumber.builder().("+ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER+").build()")
     @MethodSource(value = {
-            "baseball.model.TestParametersProvider#provideSizeAndRadix",
             "baseball.model.TestParametersProvider#provideNonPositiveSizeOrNonPositiveSizeRadix",
             "baseball.model.TestParametersProvider#provideTooMuchSizeOrTooMuchRadix"
     })
-    public void test2_BaseballResult_create_failed_result(int size, int radix) {
+    public void test1_2_BaseballResult_calcResult_exception_case(int size, int radix) {
+        assertThatExceptionOfType(BaseballNumberFormatException.class).isThrownBy(() -> {
+            BaseballNumber left = BaseballNumber.builder().size(size).radix(radix).build();
+            BaseballNumber right = BaseballNumber.builder().size(size).radix(radix).build();
+            BaseballResult.calcResult(left, right);
+        });
+    }
+
+    @DisplayName(value = "BaseballResult create failed result Test_2 Success Case")
+    @Test
+    public void test2_BaseballResult_create_failed_result() {
         BaseballResult baseballResult = BaseballResult.createFailedResult();
 
         assertThat(baseballResult).isNotNull();
