1f9e5df178a2f061cfb08edcaa9a29c364d79960
cheol <mettlekc@gmail.com>
2021-04-27 19:03:50 +0900

    test: display name 추가

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -2,6 +2,7 @@ import baseball.domain.Score;
 import baseball.exception.IllegalPositionException;
 import baseball.service.Game;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
@@ -21,6 +22,7 @@ class GameTest {
     }
 
     @Test
+    @DisplayName("무작위 생성된 숫자가 세자리 수인지 확인")
     void random_three_digits() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method method = game.getClass().getDeclaredMethod("createRandomNumber");
         method.setAccessible(true);
@@ -31,53 +33,25 @@ class GameTest {
     }
 
     @Test
+    @DisplayName("첫번째 자리수가 0을 제외한 1-9 숫자를 포함하는지 확인")
     void third_number_range_between_one_and_nine() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         assertThat(getIntegers(3)).containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9).doesNotContain(0);
     }
 
     @Test
+    @DisplayName("두번째 자리수가 0-9 숫자를 포함하는지 확인")
     void second_number_range_between_zero_and_nine() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         assertThat(getIntegers(2)).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
     }
 
     @Test
+    @DisplayName("세번째 자리수가 0-9 숫자를 포함하는지 확인")
     void first_number_range_between_zero_and_nine() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         assertThat(getIntegers(1)).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
     }
 
     @Test
-    void all_strike() {
-        assertPlay(game.play(123), 3, 0);
-    }
-
-    @Test
-    void some_strike() {
-        assertPlay(game.play(145), 1, 0);
-        assertPlay(game.play(125), 2, 0);
-    }
-
-    @Test
-    void nothing() {
-        assertPlay(game.play(456), 0, 4);
-    }
-
-    @Test
-    void one_ball() {
-        assertPlay(game.play(451), 0, 1);
-    }
-
-    @Test
-    void many_ball() {
-        assertPlay(game.play(431), 0, 2);
-        assertPlay(game.play(231), 0, 3);
-    }
-
-    @Test
-    void one_strike_two_ball() {
-        assertPlay(game.play(132), 1, 2);
-    }
-
-    @Test
+    @DisplayName("중복된 숫자를 포함하는지 확인")
     void check_duplicate_random_number() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method method = game.getClass().getDeclaredMethod("createRandomNumber");
         method.setAccessible(true);
@@ -89,6 +63,44 @@ class GameTest {
             sets.add(number % 10);
             assertThat(sets).hasSize(3);
         }
+    }
+
+    @Test
+    @DisplayName("세자리 모두 스트라이크 처리 확인, 전체 자리와 숫자 모두 맞춘 경우")
+    void all_strike() {
+        assertPlay(game.play(123), 3, 0);
+    }
+
+    @Test
+    @DisplayName("일부 스트라이크 처리 확인, 일부 자리와 숫자 모두 맞춘 경우")
+    void some_strike() {
+        assertPlay(game.play(145), 1, 0);
+        assertPlay(game.play(125), 2, 0);
+    }
+
+    @Test
+    @DisplayName("낫싱 처리 확인, 모든 숫자 틀린 경우")
+    void nothing() {
+        assertPlay(game.play(456), 0, 4);
+    }
+
+    @Test
+    @DisplayName("원볼 처리 확인, 숫자 하나 맞은 경우")
+    void one_ball() {
+        assertPlay(game.play(451), 0, 1);
+    }
+
+    @Test
+    @DisplayName("여러개 볼 처리 확인, 숫자는 맞았지만 자리가 틀린 경우")
+    void many_ball() {
+        assertPlay(game.play(431), 0, 2);
+        assertPlay(game.play(231), 0, 3);
+    }
+
+    @Test
+    @DisplayName("스트라이크와 볼이 혼합되어 있는 경우 확인")
+    void one_strike_two_ball() {
+        assertPlay(game.play(132), 1, 2);
     }
 
     private Set<Integer> getIntegers(int pos) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
