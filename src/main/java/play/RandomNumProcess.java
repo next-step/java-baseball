b1414d46dcc16ba61a905153d04a43c912003fe1
shin <shin@LAPTOP-MM4U4FF8>
2021-04-27 17:04:04 +0900

    feat: 발생 된 난수 저장

--- src/main/java/play/RandomNumProcess.java
+++ src/main/java/play/RandomNumProcess.java
@@ -1,9 +1,15 @@
 package play;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 public class RandomNumProcess {
+
+	public static void setRandomNum(Map<Integer, Integer> ranNumMap) {
+		setRandomNumMap(randomNumOccur(), ranNumMap);
+	}
 
 	public static Set<Integer> randomNumOccur() {
 		Set<Integer> ranNumSet = new HashSet<Integer>();
@@ -13,5 +19,14 @@ public class RandomNumProcess {
 		}
 
 		return ranNumSet;
+	}
+
+	public static void setRandomNumMap(Set<Integer> ranNumSet, Map<Integer, Integer> ranNumMap) {
+		Iterator<Integer> iter = ranNumSet.iterator();
+		int nb = 0;
+		while (iter.hasNext()) {
+			nb++;
+			ranNumMap.put(nb, iter.next());
+		}
 	}
 }
