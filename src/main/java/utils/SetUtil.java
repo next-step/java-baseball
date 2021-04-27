714b9652e35192042b41cf46727a3d9084ec861d
전계원 <tony@jeongyewon-ui-MacBookPro.local>
2021-04-27 22:56:28 +0900

    feat: add Utils(InputUtil, RandomUtil, SetUtil)

---
+++ src/main/java/utils/SetUtil.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class SetUtil {
+
+    public static ArrayList<Integer> transferSetToList(Set<Integer> randomNumbers) {
+        return new ArrayList<>(randomNumbers);
+    }
+}
