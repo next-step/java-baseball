c58403990bd9b774989be800c9b8a958a7bd6571
apio1130 <apio1130@naver.com>
2021-04-27 23:36:44 +0900

    test: 3자리 수에 대한 스트라이크와 볼 판정에 대한 test case 작성

--- src/test/java/com/nextstep/baseball/utils/NumberUtilsTest.java
+++ src/test/java/com/nextstep/baseball/utils/NumberUtilsTest.java
@@ -34,4 +34,22 @@ class NumberUtilsTest {
 		assertThat(result).isTrue();
 	}
 
+	@Test
+	@DisplayName("3자리 수에 대한 3볼 판정 확인")
+	void checkBall() {
+		String computer = "123";
+		String input = "456";
+		boolean result = NumberUtils.checkStrike(computer, input);
+		assertThat(result).isFalse();
+	}
+
+	@Test
+	@DisplayName("3자리 수에 대한 스트라이크 및 볼 판정 확인")
+	void checkStrikeAndBall() {
+		String computer = "123";
+		String input = "321";
+		boolean result = NumberUtils.checkStrike(computer, input);
+		assertThat(result).isFalse();
+	}
+
 }
