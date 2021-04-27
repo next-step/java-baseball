638b068a8a21a778073dc4179b695665ec7d4fac
조원진 <>
2021-04-27 19:46:39 +0900

    feat(view, application) : create baseball game launcher
    야구게임을 진행하는 BaseballGame 클래스, 게임 계속 진행 여부 묻는 view 생성.
    - 플레이어 야구번호 입력을 검증한 값이 true일 때까지 야구번호 입력을 반복
    - 플레이어 야구번호 입력 값이 모두 스트라이크일 때까지 입력을 반복
    - 정답을 맞출 경우 다시 반복 시작 또는 종료할 수 있도록 처리

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,4 +1,26 @@
 package baseball;
 
+import baseball.domain.BaseballNumberGenerator;
+import baseball.domain.BaseballNumberValidator;
+import baseball.domain.GameChecker;
+import baseball.view.ReceivingBaseballNumber;
+import baseball.view.ReceivingPlayAgainAnswer;
+import baseball.view.ResultScreen;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        BaseballNumberGenerator baseballNumberGenerator = new BaseballNumberGenerator();
+        BaseballNumberValidator baseballNumberValidator = new BaseballNumberValidator();
+        GameChecker gameChecker = new GameChecker();
+        Scanner scanner = new Scanner(System.in);
+        ReceivingBaseballNumber receivingBaseballNumber = new ReceivingBaseballNumber(scanner);
+        ReceivingPlayAgainAnswer receivingPlayAgainAnswer = new ReceivingPlayAgainAnswer(scanner);
+        ResultScreen resultScreen = new ResultScreen();
+
+        BaseballGame baseballGame = new BaseballGame(baseballNumberGenerator, baseballNumberValidator, gameChecker,
+                receivingBaseballNumber, receivingPlayAgainAnswer, resultScreen);
+        baseballGame.play();
+    }
 }
