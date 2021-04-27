4dddbd09790265d4b61e9539c5fd75ad5988925c
htkim <kht2199@gmail.com>
2021-04-27 22:31:05 +0900

    feat: 3자리 숫자 2개를 비교해 자리와 숫자가 일치하는 갯수 반환 (Strike)

--- src/main/java/nextstep/kht2199/precourse/FBGameNumber.java
+++ src/main/java/nextstep/kht2199/precourse/FBGameNumber.java
@@ -8,7 +8,7 @@ import java.util.Random;
  */
 public final class FBGameNumber {
 
-	private final int computerNumber;
+	private int computerNumber;
 
 	private final static Random random = new Random();
 
@@ -24,14 +24,27 @@ public final class FBGameNumber {
 		);
 	}
 
-	private int countBall(int base, int target) {
+	protected int countBall(int base, int target) {
 		assert false;
 		return 0;
 	}
 
-	private int countStrike(int base, int target) {
-		assert false;
-		return 0;
+	protected int countStrike(int base, int target) {
+		assert base > 100 && base < 999;
+		assert target > 100 && target < 999;
+		String s1 = String.valueOf(base);
+		String s2 = String.valueOf(target);
+		int sum = 0;
+		char[] charArray = s1.toCharArray();
+		char[] charArray2 = s2.toCharArray();
+		for (int i = 0, charArrayLength = charArray.length; i < charArrayLength; i++) {
+			sum += (returnsOneIfMatched(charArray[i], charArray2[i]));
+		}
+		return sum;
+	}
+
+	private int returnsOneIfMatched(char c1, char c2) {
+		return c1 == c2 ? 1 : 0;
 	}
 
 	/**
@@ -47,5 +60,4 @@ public final class FBGameNumber {
 		}
 		return number;
 	}
-
 }
--- src/test/java/nextstep/kht2199/precourse/FBGameNumberTest.java
+++ src/test/java/nextstep/kht2199/precourse/FBGameNumberTest.java
@@ -4,9 +4,14 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class FBGameNumberTest {
 
@@ -25,4 +30,21 @@ class FBGameNumberTest {
 			.isEqualTo(3)
 		;
 	}
+
+	@ParameterizedTest
+	@DisplayName("3자리 숫자 2개를 비교해 자리와 숫자가 일치하는 갯수 반환 (Strike)")
+	@MethodSource("countStrikeSource")
+	void countStrikeTest(int base, int target, int match) {
+		assertThat(number.countStrike(base, target)).isEqualTo(match);
+	}
+
+	static Stream<Arguments> countStrikeSource() {
+		return Stream.of(
+			Arguments.of(123, 123, 3),
+			Arguments.of(123, 124, 2),
+			Arguments.of(123, 145, 1),
+			Arguments.of(123, 234, 0)
+		);
+	}
+
 }
