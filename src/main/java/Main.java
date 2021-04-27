99aaa0a5429cd13051244e79071fb9f12eada209
minkue <minkue0321@naver.com>
2021-04-27 22:44:03 +0900

    사용자 입력값 받은 후 처리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ public class Main {
     private static Random random = new Random();
 
     public static void main(String[] args) {
-        List<Integer> goalValue = generateGoalValue();
-        System.out.println("goalValue = " + goalValue);
+        Scanner in = new Scanner(System.in);
+
+        List<Integer> goalValues = generateGoalValue();
+        int inputValue = in.nextInt();
+        List<Integer> inputValues = getUserInputValue(inputValue);
     }
 
     private static List<Integer> generateGoalValue(){
@@ -19,6 +22,19 @@ public class Main {
             }
         } while (goalValueSet.size() < 3);
         return goalValueSet;
+    }
+
+    private static List<Integer> getUserInputValue(int userInput){
+        List<Integer> userInputList = new ArrayList<>();
+
+        int modifyNumber = 100;
+        do {
+            int value = userInput / modifyNumber;
+            userInput %= modifyNumber;
+            modifyNumber /= 10;
+            userInputList.add(value);
+        } while (modifyNumber != 0);
+        return userInputList;
     }
 
 }
