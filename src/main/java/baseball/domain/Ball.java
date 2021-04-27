806b0fbece73a034c4d40be108688bc64f481d6b
ejhwang <ejhwang@mygenomebox.com>
2021-04-27 19:17:10 +0900

    feat : 볼 유효성검사 생성

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import baseball.exception.BallException;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,9 +10,13 @@ public class Ball {
 
     public List<Integer> ball;
 
-
     public Ball(List<Integer> ball){
         setBall(ball);
+    }
+
+
+    public List<Integer> getBall(){
+        return ball;
     }
 
 
@@ -21,12 +27,20 @@ public class Ball {
 
 
     public void setBall(List<Integer> ball){
+        validChk(ball);
         this.ball = ball;
     }
 
 
-    public List<Integer> getBall(){
-        return ball;
+    public List<Integer> strToListBall(String ballStr){
+        chkNumeric(ballStr);
+
+        List<Integer> ballList = new ArrayList<>();
+        for(int i=0;i<ballStr.length();i++){
+            ballList.add(i, Integer.parseInt(ballStr.substring(i,i+1)));
+        }
+
+        return ballList;
     }
 
 
@@ -38,13 +52,58 @@ public class Ball {
         return catcherNum;
     }
 
-    public List<Integer> strToListBall(String ballStr){
 
-        List<Integer> ballList = new ArrayList<>();
+
+    public void validChk(List<Integer> ball){
+        String ballStr = ball.toString().replaceAll("[^0-9]","");
+        chkEmpty(ballStr);
+        chkNumeric(ballStr);
+        chkNumberSize(ballStr);
+        chkZero(ballStr);
+        chkDuplicate(ballStr);
+    }
+
+
+    public void chkEmpty(String ballStr){
+        if("".equals(ballStr)){
+            throw new BallException("숫자를 입력해 주세요.");
+        }
+    }
+
+
+    public void chkNumeric(String ballStr){
+        if (!ballStr.matches("[+-]?\\d*(\\.\\d+)?")){
+            throw new BallException("숫자만 입력해 주세요.");
+        }
+    }
+
+
+    public  void chkZero(String ballStr){
+        if(ballStr.contains("0")){
+            throw new BallException("1~9사이의 수만 입력해 주세요.");
+        }
+    }
+
+
+    public void chkDuplicate(String ballStr){
+        Set<String> unique  = new HashSet<>();
+        List<String> numStrList = new ArrayList<>();
+
         for(int i=0;i<ballStr.length();i++){
-            ballList.add(i, Integer.parseInt(ballStr.substring(i,i+1)));
+            numStrList.add(ballStr.substring(i,i+1));
         }
 
-        return ballList;
+        for(String str:numStrList){
+            if(!unique.add(str)){
+                throw new BallException("중복 된 값은 입력할 수 없습니다.");
+            }
+        }
+    }
+
+
+    public  void chkNumberSize(String ballStr){
+        if(ballStr.length()!=3){
+            throw new BallException("세자리의 수를 입력해주세요.");
+        }
     }
 }
