f1b392a9245db03a7cf10f1a44f997bfbc910593
JisuLim <sanola3@gmail.com>
2021-04-27 23:32:59 +0900

    test: String test 요구사항 3

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 	@Test
@@ -43,5 +44,35 @@ public class StringTest {
 
 		// then
 		assertThat(substringActual).isEqualTo("1,2");
+	}
+
+	@Test
+	@DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다")
+	void charAtTest() {
+		// given
+		String actual = "abc";
+
+		// when
+		char a = actual.charAt(0);
+		char b = actual.charAt(1);
+		char c = actual.charAt(2);
+
+		// then
+		assertThat(a).isEqualTo('a');
+		assertThat(b).isEqualTo('b');
+		assertThat(c).isEqualTo('c');
+	}
+
+	@Test
+	@DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다")
+	void charAtExceptionTest() {
+		// given
+		String actual = "abc";
+
+		// when, then
+		assertThatThrownBy(() -> actual.charAt(3))
+				.isInstanceOf(IndexOutOfBoundsException.class)
+				.hasMessageContaining("String index out of range: 3");
+
 	}
 }
