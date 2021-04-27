0e3b77c92b69d637c02225c0bc476e0dd9535104
DESKTOP-C1BP6S7\kim <jtkimwd@gmail.com>
2021-04-27 09:08:55 +0900

    프리코스 야구게임 함수(기능) 커밋 PlayBaseballUtilTest 커밋 내용 -PlayBaseballUtil를 단위 테스트를 위한 클래스
    [함수 내용]
    - checkNumberIdentifyTest(숫자의 동일성 테스트 - 3자리 숫자내 같은 숫자가 있는 지 확인)
    - getRandomNumberTest(램덤 숫자 테스트 - 램덤 숫자 생성시 자리수 맞는 지 확인)
    - getRandomNumberIdentifyTest(램덤 숫자 동일성 테스트 - 램덤 숫자 생성시 같은 숫자가 없는 지 확인)
    - getBattingResultTest(배팅 결과 테스트 - 2개의 3자리 숫자에 따라 스트라이크, 볼 카운트 결과가 맞는 지 확인)

--- src/test/java/SplitStringUtilTest.java
+++ src/test/java/SplitStringUtilTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class SplitStringUtilTest {
