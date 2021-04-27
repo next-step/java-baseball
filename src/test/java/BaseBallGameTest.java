df889e5d0fb90818a51f138fc25b5f0b0a163474
unho.shin <cs7998@gmail.com>
2021-04-27 21:38:28 +0900

    test: change test method name
    change test method name:
    - DisplayNameGeneration Annotation
    - DisplayName Annotation

--- src/test/java/BaseBallGameTest.java
+++ src/test/java/BaseBallGameTest.java
@@ -1,11 +1,12 @@
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class BaseBallGameTest {
 
     static BaseBallGame bbg;
@@ -17,7 +18,8 @@ class BaseBallGameTest {
     }
 
     @Test
-    void generateBaseBallNumber() {
+    @DisplayName("컴퓨터가 생성하는 숫자 확인")
+    void generate_baseBall_number() {
         // int[] range = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         System.out.println("Generated Numbers :" +Arrays.toString(bbg.generatedArray));
