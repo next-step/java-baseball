3036c35c024219a31c91e4f080c250582a9fcbd1
최홍희 <vvshinevv@nhntoast.com>
2021-04-27 17:52:18 +0900

    feat : User 비지니스 로직 구현
     - 사용자는 PlayBall을 갖는다.
     - 입력부분은 구현하지 않음 -> 추후 입력을 통해서 PlayBall 을 갖을 수 있도록 추가 수정예정.

---
+++ src/main/java/playball/domain/User.java
@@ -0,0 +1,18 @@
+package playball.domain;
+
+public class User {
+
+    private final Balls balls;
+
+    private User(Balls balls) {
+        this.balls = balls;
+    }
+
+    public static User of(Balls balls) {
+        return new User(balls);
+    }
+
+    public Balls getBalls() {
+        return balls;
+    }
+}
