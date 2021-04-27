67ebc3f68ab6611b8b58f3a95ced55e8ab53e8cf
bckim <bckim7065@gmail.com>
2021-04-27 12:13:15 +0900

    feat: receiveNumber function
    added function which receive user-input
      - receive user-input for game
      - verify that user-input is correct

--- src/main/java/com/woowacourse/BaseBallGame.java
+++ src/main/java/com/woowacourse/BaseBallGame.java
@@ -1,5 +1,9 @@
 package com.woowacourse;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Random;
@@ -30,5 +34,68 @@ public class BaseBallGame {
 		}
 
 		setRandomNumber(randomNumber);
+	}
+	
+	/* 게임시작 */
+	public void startGame(Map<Integer, Integer> randomScore) {
+		String inputString = receiveNumber();
+	}
+	
+	/* 사용자데이터 입력 */
+	public String receiveNumber() {
+		System.out.println("숫자를 입력해주세요 : ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inputString = "";
+		
+		try {
+			inputString = br.readLine();
+			
+			while(!validateNumber(inputString)) {
+				System.out.println("숫자를 입력해주세요 : ");
+				inputString = br.readLine();
+			}
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return inputString;
+	}
+	
+	/* 입력데이터 검증 */
+	public Boolean validateNumber(String input) {
+		if (!isNumber(input) || input.length() != 3 || containDupNumber(input)) {
+			System.out.println("아래 규칙을 확인해주세요.\r\n1.숫자만입력가능합니다.\r\n2.3자리의 숫자를 입력하셔야합니다.\r\n3.1~9까지 서로 다른 3가지 숫자를 입력하셔야 합니다.");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/* 숫자인지 검증 */
+	public Boolean isNumber(String inputString) {
+		try {
+			Integer.parseInt(inputString);
+			
+			return true;
+		} catch(NumberFormatException e) {
+			return false;
+		}
+	}
+	
+	/* 중복값 검증 */
+	public Boolean containDupNumber(String inputString) {
+		HashSet<Character> numbers = new HashSet<Character>();
+		
+		for (int i = 0; i < inputString.length(); i++) {
+			char temp = inputString.charAt(i);
+			
+			if (numbers.contains(temp)) {
+				return true; 
+			}
+			
+			numbers.add(temp);
+		}
+		
+		return false;
 	}
 }
