f038737ceb2ba58886d45dae9bc9284568750cab
lhm7877 <vkfdldh7>
2021-04-27 21:28:23 +0900

    test: Hints가 힌트를 잘 생성하고 잘 가져오는지 테스트

---
+++ src/test/java/com/hoomin/game/baseball/domain/HintsTest.java
@@ -0,0 +1,42 @@
+package com.hoomin.game.baseball.domain;
+
+import static org.junit.jupiter.params.provider.Arguments.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.hoomin.game.baseball.enums.HintState;
+
+public class HintsTest {
+
+	@ParameterizedTest
+	@MethodSource("getHintStateAndCount")
+	public void getStrikeCount_HaveHintState_GetHintCount(List<HintState> hintStates, Integer strikeCount, Integer ballCount) {
+		Hints hints = new Hints();
+		for (HintState hintState : hintStates) {
+			hints.add(hintState);
+		}
+		assertThat(hints.getStrikeCount()).isEqualTo(strikeCount);
+		assertThat(hints.getBallCount()).isEqualTo(ballCount);
+	}
+
+	private static Stream<Arguments> getHintStateAndCount() {
+		return Stream.of(
+			arguments(Collections.singletonList(HintState.STRIKE), 1, 0),
+			arguments(Arrays.asList(HintState.STRIKE, HintState.STRIKE), 2, 0),
+			arguments(Arrays.asList(HintState.STRIKE, HintState.STRIKE, HintState.STRIKE), 3, 0),
+			arguments(Arrays.asList(HintState.STRIKE, HintState.BALL), 1, 1),
+			arguments(Arrays.asList(HintState.BALL, HintState.BALL), 0, 2),
+			arguments(Collections.emptyList(), 0, 0)
+		);
+	}
+}
