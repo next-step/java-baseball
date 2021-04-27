9c2b5ed256082e0271afc72310b2b65129432a71
juudev <juudeveloper@gmail.com>
2021-04-27 21:08:33 +0900

    feat(getRandomNumbers): 임의의 수 3개 구하기

--- src/main/java/com/example/baseball/BaseballGame.java
+++ src/main/java/com/example/baseball/BaseballGame.java
@@ -1,6 +1,9 @@
 package com.example.baseball;
 
+import java.util.Random;
+
 public class BaseballGame {
+    private static final int MAX_NUMBER = 9;
 
     public BaseballGame() {
 
@@ -8,5 +11,35 @@ public class BaseballGame {
 
     public void start() {
 
+    }
+
+    public int[] getRandomNumbers() {
+        Random r = new Random();
+
+        int firstNumber = r.nextInt(MAX_NUMBER) + 1;
+        int secondNumber = getSecondRandomNumber(r, firstNumber);
+        int thirdNumber = getThirdRandomNumber(r, firstNumber, secondNumber);
+
+        return new int[] {firstNumber, secondNumber, thirdNumber};
+    }
+
+    public int getSecondRandomNumber(Random r, int firstNumber) {
+        int secondNumber;
+
+        do {
+            secondNumber = r.nextInt(MAX_NUMBER) + 1;
+        } while (firstNumber == secondNumber);
+
+        return secondNumber;
+    }
+
+    public int getThirdRandomNumber(Random r, int firstNumber, int secondNumber) {
+        int thirdNumber;
+
+        do {
+            thirdNumber = r.nextInt(MAX_NUMBER) + 1;
+        } while (firstNumber == thirdNumber || secondNumber == thirdNumber);
+
+        return thirdNumber;
     }
 }
--- src/test/java/com/example/baseball/BaseballGameTest.java
+++ src/test/java/com/example/baseball/BaseballGameTest.java
@@ -1,7 +1,22 @@
 package com.example.baseball;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class BaseballGameTest {
 
+    @Test
+    void getRandomNumbers() {
+        BaseballGame baseballGame = new BaseballGame();
+        int[] randomNumbers = baseballGame.getRandomNumbers();
+        assertThat(randomNumbers)
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(3);
+        assertThat(randomNumbers[0]).isBetween(1, 9);
+        assertThat(randomNumbers[1]).isBetween(1, 9);
+        assertThat(randomNumbers[2]).isBetween(1, 9);
+        assertThat(randomNumbers).doesNotHaveDuplicates();
+    }
 }
