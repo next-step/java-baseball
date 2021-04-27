1c9240beab37ace6b32f557b018bc5309b353520
silee <silee9019@gmail.com>
2021-04-27 21:02:33 +0900

    feat: implement repeatable play and handle exception

--- src/main/java/study/BaseBall.java
+++ src/main/java/study/BaseBall.java
@@ -11,17 +11,29 @@ public class BaseBall {
 		Batter batter = new Batter();
 
 		Scanner sc = new Scanner(System.in);
-		List<Integer> pitches = pitcher.pitch(3);
 
-		System.out.println("Play Ball!!!");
-		Score score = Score.nothing();
-		int count = 1;
-		while (!Score.threeStrike().equals(score)) {
-			System.out.printf("[%d] 숫자를 입력해주세요: ", count++);
-			List<Integer> swings = batter.swingBat(sc.next());
-			score = scoreBoard.countScore(swings, pitches);
-			System.out.println(score);
+		int playState = 1;
+		while (playState == 1) {
+			List<Integer> pitches = pitcher.pitch(3);
+			Score score = Score.nothing();
+			int count = 1;
+
+			System.out.println("Play Ball!!!");
+
+			while (!Score.threeStrike().equals(score)) {
+				try {
+					System.out.printf("[%d] 숫자를 입력해주세요: ", count);
+					List<Integer> swings = batter.swingBat(sc.next());
+					score = scoreBoard.countScore(swings, pitches);
+					System.out.println(score);
+					count++;
+				} catch (IllegalArgumentException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			playState = sc.nextInt();
 		}
-		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
 }
