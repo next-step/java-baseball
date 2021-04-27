9a6c675cb86c5a045449708c4fe884bff967ebf0
HYUNMINKIM <khyunm91@gmail.com>
2021-04-27 18:02:23 +0900

    gradle 설정 테스트 class 추가

--- src/main/java/JavaBaseballGame.java
+++ src/main/java/JavaBaseballGame.java
@@ -23,10 +23,15 @@ public class JavaBaseballGame {
      * 세자리 숫자를 구하는데 중복되거나 3자리가 넘으면 예외처리를 통하여 숫자를 구한다.
      * @return num
      */
-    private int getNumberOfThree() {
+    public int getNumberOfThree() {
         int num = scanner.nextInt();
 
         if(num > 999) {
+            System.out.println("3개의 숫자만 입력이 가능합니다. 다시 입력해주세요");
+            return getNumberOfThree();
+        }
+
+        if(num < 100){
             System.out.println("3개의 숫자만 입력이 가능합니다. 다시 입력해주세요");
             return getNumberOfThree();
         }
@@ -46,7 +51,7 @@ public class JavaBaseballGame {
      * @param num
      * @return list
      */
-    private List<String> convertStringToInt(int num) {
+    public List<String> convertStringToInt(int num) {
         String numString = Integer.toString(num);
         List<String> resultList = new ArrayList<>();
         for(int i = 0; i < numString.length(); i++){
@@ -63,7 +68,7 @@ public class JavaBaseballGame {
      * @param num
      * @return
      */
-    private StrikeBall getHintMap(int gameNum, int num) {
+    public StrikeBall getHintMap(int gameNum, int num) {
         if( gameNum == num){
             return new StrikeBall(THE_NUMBER_OF_NUM, 0);
         }
@@ -87,7 +92,7 @@ public class JavaBaseballGame {
      * @param n
      * @return
      */
-    private int getStrike(String x, String n) {
+    public int getStrike(String x, String n) {
         if(x.equalsIgnoreCase(n))
             return 1;
         return 0;
@@ -100,7 +105,7 @@ public class JavaBaseballGame {
      * @param index
      * @return
      */
-    private int getBall(List<String> xString, String n, int index) {
+    public int getBall(List<String> xString, String n, int index) {
         if(index != xString.indexOf(n) && xString.indexOf(n) >= 0)
             return 1;
         return 0;
@@ -110,7 +115,7 @@ public class JavaBaseballGame {
      * 임의의 세자리 숫자 구하는 method
      * @return 임의의 세자리 숫자
      */
-    private int createRandomNumber() {
+    public int createRandomNumber() {
         Random rn = new Random();
         Set<Integer> sSet = new HashSet<>();
         while(sSet.size() < THE_NUMBER_OF_NUM){
@@ -123,7 +128,7 @@ public class JavaBaseballGame {
 
     }
 
-    private int getRealNumber(Set<Integer> sSet) {
+    public int getRealNumber(Set<Integer> sSet) {
         List<Integer> list = new ArrayList<>(sSet);
         Collections.reverse(list);
         int result = list.get(0);
@@ -136,7 +141,7 @@ public class JavaBaseballGame {
     }
 
 
-    private boolean isGame() {
+    public boolean isGame() {
         int gameButton = scanner.nextInt();
         if (gameButton == 1) {
             return true;
@@ -145,7 +150,7 @@ public class JavaBaseballGame {
         return false;
     }
 
-    private void startGame(int x){
+    public void startGame(int x){
         boolean flag = true;
         while(flag){
             int num = getNumberOfThree();
@@ -155,7 +160,7 @@ public class JavaBaseballGame {
         }
     }
 
-    private String printSentence(int x, int num) {
+    public String printSentence(int x, int num) {
         StrikeBall strikeBall = getHintMap(x, num);
         if(strikeBall.getBall() == 0 && strikeBall.getStrike() == THE_NUMBER_OF_NUM) {
             return "3 스트라이크";
@@ -165,7 +170,7 @@ public class JavaBaseballGame {
         return strikeBall.getStrike() + "스트라이크 " + strikeBall.getBall() + "볼";
     }
 
-    private boolean isFinishGame(int x, int num) {
+    public boolean isFinishGame(int x, int num) {
         StrikeBall strikeBall = getHintMap(x, num);
         if(strikeBall.getBall() == 0 && strikeBall.getStrike() == THE_NUMBER_OF_NUM) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다!. 게임 종료");
