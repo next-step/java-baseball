6a93ab87e0d615aac8e489ce00eb866a893442fe
tmkwak <tmkwak@hmm21.com>
2021-04-27 13:12:06 +0900

    fix(exceptions) : mesaage 추가

--- src/main/java/baseball/exception/InvalidateBallNumberSizeError.java
+++ src/main/java/baseball/exception/InvalidateBallNumberSizeError.java
@@ -2,6 +2,6 @@ package baseball.exception;
 
 public class InvalidateBallNumberSizeError extends InvalidateBallNumberException {
 	public InvalidateBallNumberSizeError(final String message) {
-		super(message);
+		super("중복되지 않은 3자리 수를 입력하세요 : " + message);
 	}
 }
--- src/main/java/baseball/exception/InvalidateBallNumberValueError.java
+++ src/main/java/baseball/exception/InvalidateBallNumberValueError.java
@@ -2,6 +2,6 @@ package baseball.exception;
 
 public class InvalidateBallNumberValueError extends InvalidateBallNumberException {
 	public InvalidateBallNumberValueError(final String message) {
-		super(message);
+		super("1과 9사이의 값을 넣어주세요 : " + message);
 	}
 }
