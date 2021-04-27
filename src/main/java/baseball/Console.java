6c1ebe11808272e7854d5bf484b717eec8f63414
cheol <mettlekc@gmail.com>
2021-04-27 10:46:24 +0900

    feat: getNumber/result 메서드 추가
    getNumber
    - input number를 받는 메서드
    
    result
    - Score 결과로 게임 계속/종료 판단 메서드

--- src/main/java/baseball/Console.java
+++ src/main/java/baseball/Console.java
@@ -1,18 +1,26 @@
 package baseball;
 
+import baseball.domain.Score;
+
 import java.util.Scanner;
 
 public class Console {
 
-    public void start() {
+    public int getNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("숫자를 입력해주세요 : ");
-        System.out.print(scanner.nextInt());
+        int number = scanner.nextInt();
         scanner.close();
+        return number;
     }
 
-    public static void main(String[] args) {
-
+    public void play() {
     }
 
+    public boolean result(Score score) {
+        if (score.getStrike() == 3) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/ConsoleTest.java
+++ src/test/java/ConsoleTest.java
@@ -1,27 +1,45 @@
 import baseball.Console;
+import baseball.domain.Score;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ConsoleTest {
 
     @Test
-    void create_console() {
+    void input_number() {
         Console console = new Console();
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        String input = "123";
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(input.getBytes());
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("123".getBytes());
         System.setIn(byteArrayInputStream);
-        console.start();
 
-        assertThat(out.toString()).hasToString("숫자를 입력해주세요 : " + 123);
+        assertThat(console.getNumber()).isEqualTo(123);
+    }
+
+    @Test
+    void result_three_strike() {
+        Console console = new Console();
+        Score score = new Score(3, 0);
+        assertThat(console.result(score)).isTrue();
+    }
+
+    @Test
+    void result_one_strike_one_ball() {
+        Console console = new Console();
+        Score score = new Score(1, 1);
+        assertThat(console.result(score)).isFalse();
+    }
+
+    @Disabled
+    @Test
+    void start_game() {
+        Console console = new Console();
+
+        // TODO 3스트라이크 될때까지 숫자를 물어보는 기능
+        console.play();
     }
 
 }
