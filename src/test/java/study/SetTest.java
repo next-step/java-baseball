a0cfb4ef98b38f04401686038bbfebea2d19d133
JisuLim <sanola3@gmail.com>
2021-04-27 23:34:06 +0900

    test: Set test 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -29,5 +31,12 @@ public class SetTest {
 
 		// then
 		assertThat(size).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	@DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다")
+	void setContainsTest(int inputNumber) {
+		assertThat(numbers.contains(inputNumber)).isTrue();
 	}
 }
