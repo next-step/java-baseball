0a265315c110759f7a461994b69924e0d115c4e8
bckim <bckim7065@gmail.com>
2021-04-27 12:18:20 +0900

    feat: getScoreCount function test
    unit-test for getScoreCount function

--- src/test/java/com/woowacourse/BaseBallGameTest.java
+++ src/test/java/com/woowacourse/BaseBallGameTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -29,7 +30,7 @@ public class BaseBallGameTest {
 		/* 랜덤숫자는 null일 수 없다. */
 		assertNotNull(randomString);
 		
-		/* 랜덤숫자의 길이는D DEFAULT_LENGTH 길이이다. */
+		/* 랜덤숫자의 길이는 DEFAULT_LENGTH 길이이다. */
 		assertEquals(randomString.length(), DEFAULT_LENGTH);
 		
 		/* 랜덤숫자는 모두 1~9까지 숫자로 이루어져 있다. */
@@ -50,5 +51,50 @@ public class BaseBallGameTest {
 			int temp = Character.getNumericValue(randomString.charAt(i));
 			assertEquals(1, numbers[temp]);
 		}
+	}
+	
+	@Test
+	public void testGetScoreCount() {
+		BaseBallGame baseBallGame = new BaseBallGame();
+
+		Map<String, Integer> scores = new HashMap<String, Integer>();
+		Map<Integer, Integer> randomScore = new LinkedHashMap<Integer, Integer>();
+		randomScore.put(1, 0);
+		randomScore.put(2, 1);
+		randomScore.put(3, 2);
+		
+		/* 3스트라이크 */
+		scores = baseBallGame.getScoreCount(randomScore, "123");
+		assertEquals(3, (int)scores.get("strike"));
+		
+		/* 2스트라이크 */
+		scores = baseBallGame.getScoreCount(randomScore, "129");
+		assertEquals(2, (int)scores.get("strike"));
+		
+		/* 1스트라이크 */
+		scores = baseBallGame.getScoreCount(randomScore, "199");
+		assertEquals(1, (int)scores.get("strike"));
+		
+		/* 3볼 */
+		scores = baseBallGame.getScoreCount(randomScore, "312");
+		assertEquals(3, (int)scores.get("ball"));
+		
+		/* 2볼 */
+		scores = baseBallGame.getScoreCount(randomScore, "319");
+		assertEquals(2, (int)scores.get("ball"));
+		
+		/* 1볼 */
+		scores = baseBallGame.getScoreCount(randomScore, "399");
+		assertEquals(1, (int)scores.get("ball"));
+		
+		/* 1스트라이크 2볼 */
+		scores = baseBallGame.getScoreCount(randomScore, "132");
+		assertEquals(1, (int)scores.get("strike"));
+		assertEquals(2, (int)scores.get("ball"));
+		
+		/* 낫띵 */
+		scores = baseBallGame.getScoreCount(randomScore, "999");
+		assertEquals(0, (int)scores.get("strike"));
+		assertEquals(0, (int)scores.get("ball"));
 	}
 }
