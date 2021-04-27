c5017626124e661d6e04956a2db5465f4ffb5ab4
2carrot <changgeun.lee1@cj.net>
2021-04-27 15:17:24 +0900

    feat : 사용자 입력 받는 기능
    - 야구게임에 사용할 숫자(3개) 입력 함수
    - 정답 맞힌 경우 게임을 계속 할지 여부 입력 함수

---
+++ src/main/java/baseball/UserInput.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserInput {
+    public static String inputThreeNumbers() throws InputMismatchException {
+        Scanner sc = new Scanner(System.in);
+        int numbers;
+        System.out.print("숫자를 입해주세요 : ");
+        numbers = sc.nextInt();
+        return String.valueOf(numbers);
+    }
+
+    public static String inputContinueGame() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ");
+        return sc.next();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(UserInput.inputThreeNumbers());
+        System.out.println(UserInput.inputContinueGame());
+    }
+}
