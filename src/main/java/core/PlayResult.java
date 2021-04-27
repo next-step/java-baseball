2cf20e108f129ed1f7e48cadd23dacd719132940
ssj5638 <ssj5638@gmail.com>
2021-04-27 19:56:14 +0900

    style 공백 추가

--- src/main/java/core/PlayResult.java
+++ src/main/java/core/PlayResult.java
@@ -13,13 +13,13 @@ public class PlayResult {
         return isBall;
     }
 
-    public void countPlayResult(List<Integer> questions, int index, List<Integer> answer){
+    public void countPlayResult(List<Integer> questions, int index, List<Integer> answer) {
         if(questions.get(index).equals(answer.get(index))){
             this.isStrike++;
             return;
         }
-        
-        if(questions.contains(answer.get(index))){
+
+        if(questions.contains(answer.get(index))) {
             this.isBall++;
         }
     }
