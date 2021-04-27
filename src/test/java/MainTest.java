0b099d3c0b5d27b5f484c44aba678627d9f75a2c
이병재[Security Dev] <bj.lee@navercorp.com>
2021-04-27 21:26:14 +0900

    test($compile): renaming the test function
    Renaming the test function

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
@@ -8,7 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MainTest {
 
     @Test
-    void test() {
+    @DisplayName("Test Baseball Random Number")
+    void TEST_getRandomNumStr() {
         String randomNumStr = Main.getRandomNumStr();
         StringBuilder baseballNum = new StringBuilder();
         Set baseballNumSet = new HashSet();
