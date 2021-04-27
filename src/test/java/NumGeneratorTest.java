f31e0c719fc6a0a15c84ddcdff3b8f868f1da6ec
DESKTOP-OP3B5IB\kmyms <kmyms2@naver.com>
2021-04-27 21:55:09 +0900

    Add Junit Test
    - 기능 테스트 클래스 추가

---
+++ src/test/java/NumGeneratorTest.java
@@ -0,0 +1,34 @@
+import generator.NumGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class NumGeneratorTest {
+    @Test
+    public void generatorTest(){
+        String generateNumber = NumGenerator.numGenerator();
+        System.out.println("generateNumber : " + generateNumber);
+        assertThat(checkValid(generateNumber)).isTrue();
+        assertThat(generateNumber.length()).isEqualTo(3);
+
+    }
+
+    public boolean checkValid(String input){
+        if(!input.matches("[1-9]{3}"))
+            return false;
+        Set<Integer> chkNum = new HashSet<Integer>();
+        for(char c : input.toCharArray()){
+            int a = c - '0';
+            chkNum.add(a);
+        }
+
+        return chkNum.size() == 3 ? true : false;
+    }
+}
