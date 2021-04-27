168322fa3e7b20c356ddd9545f8832688a5a9a0d
jonghaha <bgh9197@naver.com>
2021-04-27 21:00:32 +0900

    test: CsvSource 을 이용 볼 매칭 테스트

--- src/test/java/ComputerTest.java
+++ src/test/java/ComputerTest.java
@@ -86,7 +86,7 @@ public class ComputerTest {
 	@DisplayName("유저가 입력한 여러 숫자 매칭 테스트")
 	@ParameterizedTest(name = "[{0}, {1}, {2}] is {3} strike")
 	@CsvSource({"1,2,3,0", "4,2,8,1", "3,8,7,1", "4,2,6,2", "4,8,1,2", "4,8,6,3"})
-	public void 매칭_결과_테스트(int num1, int num2, int num3, int expected) {
+	public void 스트라이크_매칭_결과_테스트(int num1, int num2, int num3, int expected) {
 		List<Integer> computerNumber = new ArrayList<>();
 		computerNumber.add(4);
 		computerNumber.add(8);
@@ -103,4 +103,23 @@ public class ComputerTest {
 		assertThat(computer.getStrikeCount()).isEqualTo(expected);
 	}
 
+	@DisplayName("유저가 입력한 여러 숫자 매칭 테스트")
+	@ParameterizedTest(name = "[{0}, {1}, {2}] is {3} ball")
+	@CsvSource({"8,6,4,3", "4,2,8,1", "4,8,6,0", "7,6,8,2", "8,6,7,2", "2,4,8,2"})
+	public void 볼_매칭_결과_테스트(int num1, int num2, int num3, int expected) {
+		List<Integer> computerNumber = new ArrayList<>();
+		computerNumber.add(4);
+		computerNumber.add(8);
+		computerNumber.add(6);
+		computer.setComputerNumbers(computerNumber);
+
+		List<Integer> userNumber = new ArrayList<>();
+		userNumber.add(num1);
+		userNumber.add(num2);
+		userNumber.add(num3);
+
+		computer.matchResult(userNumber);
+
+		assertThat(computer.getBallCount()).isEqualTo(expected);
+	}
 }
