fc90e6567790ce264a54686878c8468ed67d8ad1
shkwon <oper912@naver.com>
2021-04-27 09:45:15 +0900

    refactor: 매직넘버 상수화

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -6,8 +6,9 @@ public class ResultView {
     private static final String NOTHING_MESSAGE = "낫싱";
     private static final String STRIKE_MESSAGE = "%s스트라이크 ";
     private static final String BALL_MESSAGE = "%s볼";
-    private static final String BLANK_MESSAGE = "";
+    private static final String EMPTY_MESSAGE = "";
     private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final int ZERO_SCORE = 0;
 
     public void printScore(Computer computer) {
         System.out.println(makeScoreMessage(computer));
@@ -28,10 +29,14 @@ public class ResultView {
     }
 
     private String strikeToString(int strike) {
-        return strike != 0 ? String.format(STRIKE_MESSAGE, strike) : BLANK_MESSAGE;
+        return isZeroScore(strike) ? EMPTY_MESSAGE : String.format(STRIKE_MESSAGE, strike);
     }
 
     private String ballToString(int ball) {
-        return ball != 0 ? String.format(BALL_MESSAGE, ball) : BLANK_MESSAGE;
+        return isZeroScore(ball) ? EMPTY_MESSAGE : String.format(BALL_MESSAGE, ball);
+    }
+
+    private boolean isZeroScore(int score) {
+        return score == ZERO_SCORE;
     }
 }
