60f4d126b3de4bdb10e53af52ba35cff4aa39c3f
jessi68 <lynn312.unist.ac.kr>
2021-04-27 18:13:54 +0900

    feat:  효율성 위해 각 야구공의 숫자와 위치를 담고 있는 클래스 따로 생성

---
+++ src/main/java/domain/Info.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Info {
+    private int number;
+    private int location;
+
+    public Info(int number, int location) {
+        this.number = number;
+        this.location = location;
+    }
+
+}
