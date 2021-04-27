3609b5a8c1521be37007f657b6aa741464b02bf4
shin <shin@LAPTOP-MM4U4FF8>
2021-04-27 16:53:49 +0900

    feat: 공통 문구 등록

---
+++ src/main/java/common/MessagesCommon.java
@@ -0,0 +1,7 @@
+package common;
+
+public class MessagesCommon {
+	public static final String	INPUT_MSG = "숫자를 입력해주세요 : ";
+	public static final String	GAME_END_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	public static final String	GAME_REPLAY_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
