4a93640a28fffc2b06524ca5823336cf2d7a60fc
juniq <juniqlim@gmail.com>
2021-04-27 22:55:12 +0900

    repactor: 객체 캐시 사용

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,11 +1,18 @@
 package baseball.domain;
 
 import baseball.domain.exception.NoAllowNumberException;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Ball {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
+    private static final List<Ball> baseBalls = IntStream.range(1, 10)
+        .mapToObj(Ball::new)
+        .collect(Collectors.toList());
+
     private final int number;
 
     private Ball(int number) {
@@ -16,7 +23,11 @@ public class Ball {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new NoAllowNumberException("숫자는 " + MIN_NUMBER + "~" + MAX_NUMBER + " 사이어야 합니다.");
         }
-        return new Ball(number);
+        return makeBall(number);
+    }
+
+    private static Ball makeBall(int number) {
+        return baseBalls.get(number - 1);
     }
 
     public static Ball of(OneDigitNumber oneDigitNumber) {
