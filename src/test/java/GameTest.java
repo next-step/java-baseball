b5b596c928657ff2359e5e2129aa6229a5729f70
이유진 <ujin@deali.net>
2021-04-27 21:45:34 +0900

    test: 두가지의 숫자를 이용해 두번째 숫자의 각 자릿수가 첫번쨰 수에 포함되는 경우를 더한 값 검사 TEST

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class GameTest {
 
     @Test
-    @DisplayName("입력받은 2 숫자의 자릿수가 같은 스트라이크 count를 계산하여 맞는지 검사한다")
+    @DisplayName("입력받은 두 숫자의 자릿수가 같은 스트라이크 count를 계산하여 맞는지 검사한다")
     void strikeTest(){
         String computerNum = "246";
 
@@ -15,6 +15,18 @@ class GameTest {
 
         String inputNum2 = "285";
         assertThat(game.strike(computerNum, inputNum2)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("입력받은 두 숫자중 두번째 숫자의 각 자릿수가 첫번째 수의 문자에 포함되는 경우를 더한 값이 맞는지 검사한다")
+    void ball(){
+        String computerNum = "123";
+        String inputNum1 = "436";
+        Game game = new Game();
+        assertThat(game.ball(computerNum, inputNum1)).isEqualTo(1);
+
+        String inputNum2 = "231";
+        assertThat(game.ball(computerNum, inputNum2)).isEqualTo(3);
     }
 
 
