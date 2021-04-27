414fce5a84c00db39f416e33c2feb844b5f16cb3
DESKTOP-OP3B5IB\kmyms <kmyms2@naver.com>
2021-04-27 23:14:22 +0900

    Remove unnecessary import statements

--- src/test/java/NumGeneratorTest.java
+++ src/test/java/NumGeneratorTest.java
@@ -1,13 +1,11 @@
 import generator.NumGenerator;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 
 
 public class NumGeneratorTest {
