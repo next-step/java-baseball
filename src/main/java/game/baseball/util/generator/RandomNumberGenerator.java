00e7189a43aa7879e6ff179f0bd0bd22a4a1591e
fDevJc <fDevJc@gmail.com>
2021-04-27 19:16:38 +0900

    refactor: 기능구현 설계시 작성했던 주석제거

--- src/main/java/game/baseball/util/generator/RandomNumberGenerator.java
+++ src/main/java/game/baseball/util/generator/RandomNumberGenerator.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomNumberGenerator {
-/*
- * getRandomNumber - 랜덤 숫자 생성(1~9까지의 랜덤 숫자 생성)
- * getRandomNumberList - 사이즈에 맞는 랜덤 숫자 배열 생성
- * addNumberIntoList - 리스트에 숫자 추가
- * checkDuplication - 중복 체크
- */
     final int MAX_VALUE = 9;
     final int MIN_VALUE = 1;
 
