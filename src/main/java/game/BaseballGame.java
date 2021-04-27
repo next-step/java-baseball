1d52a7d412a49b3fd6229ba70837a416e55957b7
Hye-Rin-Lee <49551361+Hye-Rin-Lee@users.noreply.github.com>
2021-04-27 17:30:00 +0900

    feat : 메인클래스 커밋

---
+++ src/main/java/game/BaseballGame.java
@@ -0,0 +1,10 @@
+package game;
+
+import manager.GameManager;
+
+public class BaseballGame {
+	public static void main(String[] args) {
+		GameManager manager = new GameManager();
+		manager.play();
+	}
+}
