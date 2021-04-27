ed50616273b2ed36529bdcf739f67a73298f10d3
jonghaha <bgh9197@naver.com>
2021-04-27 21:28:03 +0900

    refactor: User 입력값 유효성 검사 테스트 수정

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -8,48 +8,45 @@ public class UserTest {
 	User user = new User();
 
 	@Test
-	@DisplayName("유저가 입력한 숫자의 길이 테스트(성공, 실패)")
-	public void 유저_입력값_길이_체크() {
-		/*
-		* 성공 - 메소드 캡슐화로 주석처리
-		* */
+	@DisplayName("유저의 입력 테스트(성공)")
+	public void 유저_입력값_성공() {
 		String testInput1 = "486";
 
-//		assertThatCode(() -> {
-//			user.validateNumber(testInput1);
-//		}).doesNotThrowAnyException();
-
-		/*
-		 * 실패 - 메소드 캡슐화로 주석처리
-		 * */
-		String testInput2 = "4868";
-
-//		assertThatIllegalArgumentException().isThrownBy(() -> {
-//			user.validateNumber(testInput2);
-//		}).withMessage("입력한 숫자는 %s자리 입니다.", testInput2.length())
-//				.withMessageContaining("세자리 숫자가 아닙니다.");
+		assertThatCode(() -> {
+			user.validateNumber(testInput1);
+		}).doesNotThrowAnyException();
 	}
 
 	@Test
-	@DisplayName("유저가 입력한 숫자의 중복 테스트(성공, 실패)")
+	@DisplayName("유저의 문자 입력 테스트(실패)")
+	public void 유저_입력값_문자열() {
+		String testInput = "고고고";
+
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			user.validateNumber(testInput);
+		}).withMessage("입력 값 '%s'에는 문자열이 포함되어 있습니다.", testInput)
+				.withMessageContaining("문자는 입력할 수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("유저가 입력한 숫자의 길이 테스트(실패)")
+	public void 유저_입력값_길이_체크() {
+		String testInput = "4868";
+
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			user.validateNumber(testInput);
+		}).withMessage("입력한 숫자는 %s자리 입니다.", testInput.length())
+				.withMessageContaining("세자리 숫자가 아닙니다.");
+	}
+
+	@Test
+	@DisplayName("유저가 입력한 숫자의 중복 테스트(실패)")
 	public void 유저_입력값_중복_체크() {
-		/*
-		 * 성공 - 메소드 캡슐화로 주석처리
-		 * */
-		String testInput1 = "486";
-//
-//		assertThatCode(() -> {
-//			user.validateNumber(testInput1);
-//		}).doesNotThrowAnyException();
+		String testInput = "446";
 
-		/*
-		 * 실패 - 메소드 캡슐화로 주석처리
-		 * */
-		String testInput2 = "446";
-
-//		assertThatIllegalArgumentException().isThrownBy(() -> {
-//			user.validateNumber(testInput2);
-//		}).withMessage("입력 숫자 %s의 중복되는 수가 있습니다.", testInput2)
-//				.withMessageContaining("중복되는 수가 있습니다.");
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			user.validateNumber(testInput);
+		}).withMessage("입력 숫자 %s의 중복되는 수가 있습니다.", testInput)
+				.withMessageContaining("중복되는 수가 있습니다.");
 	}
 }
