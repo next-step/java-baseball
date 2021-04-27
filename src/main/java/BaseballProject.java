f7920bad3cd2443919095e51f0fc37eaaa01edf1
root <edenkang5517@gmail.com>
2021-04-27 11:47:09 +0900

    feature : Add init logic
    게임 초기화 로직을 추가

---
+++ src/main/java/BaseballProject.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BaseballProject {
+
+    public static void main(String [] args){
+
+        boolean isCorrect; // 정답확인 여부
+        boolean isContinue = true; // 계속진행 여부
+
+        while(isContinue){
+
+            System.out.println("게임을 시작합니다.");
+
+            // TODO : 0. init
+            BaseBallGame baseBallGame = new BaseBallGame();
+        }
+    }
+}
