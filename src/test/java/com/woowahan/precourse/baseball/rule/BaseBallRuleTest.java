9e3ae752a4fe7ae0aa83941f9e50b8be4fc61427
junseok Oh <ixtears23@icloud.com>
2021-04-27 22:46:51 +0900

    refactor(test) : strike, ball test remove
    strike와 ball 규칙은 BaseBallGame 객체 설계상
    별도의 Rule 클래스로 분리하기에는 BaseBallGame 객체를 과도하게 의존하고 있어서
    BaseBallGame 객체에 strike 규칙 ball 규칙 로직을 포함시켰고
    내부적으로만 사용하도록 캡슐화 하여 우선은 테스트 코드에서 제외시킴

--- src/test/java/com/woowahan/precourse/baseball/rule/BaseBallRuleTest.java
+++ src/test/java/com/woowahan/precourse/baseball/rule/BaseBallRuleTest.java
@@ -16,30 +16,6 @@ class BaseBallRuleTest {
     }
 
     @Test
-    @DisplayName("동일한 자리의 gameNumber와 inputNumber가 같으면 isStrike는 true 다르면 isStrike는 false")
-    void isStrikeTest() {
-
-        String firstInputNumber = "7";
-        String secondInputNumber = "4";
-
-        assertThat(BaseBallRule.isStrike(this.gameNumbers[0], firstInputNumber)).isFalse();
-        assertThat(BaseBallRule.isStrike(this.gameNumbers[1], secondInputNumber)).isTrue();
-    }
-
-    @Test
-    @DisplayName("inputNumber가 strike가 아니고 gameNumbers에 inputNumber가 포함되어 있으면 ball")
-    void isBallTest() {
-
-        String firstInputNumber = "3";
-        String secondInputNumber = "8";
-        String thirdInputNumber = "4";
-
-        assertThat(BaseBallRule.isBall(this.gameNumbers, firstInputNumber, 0)).isTrue();
-        assertThat(BaseBallRule.isBall(this.gameNumbers, secondInputNumber, 1)).isFalse();
-        assertThat(BaseBallRule.isBall(this.gameNumbers, thirdInputNumber, 2)).isTrue();
-    }
-
-    @Test
     @DisplayName("strike가 3이면 게임 승리이므로 승리인경우 true 승리가 아닌경우 false 반환")
     void isVictoryTest() {
 
