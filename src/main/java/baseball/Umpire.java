5b6f6a0ca150306042d2aa9b49df1e42226ad6f2
Paul An <anpaul0615@gmail.com>
2021-04-27 21:24:47 +0900

    refactor(Umpire): 스트라이크/볼/포볼 판별 기능 구현

--- src/main/java/baseball/Umpire.java
+++ src/main/java/baseball/Umpire.java
@@ -1,15 +1,27 @@
 package baseball;
 
 public class Umpire {
-	private boolean isStrike() {
-		return true;
+	private boolean isStrike(char attackNumber, char defenseNumber) {
+		return attackNumber == defenseNumber;
 	}
 
-	private boolean isBall() {
-		return true;
+	private boolean isBall(String attackNumbers, char attackNumber, char defenseNumber) {
+		if (isStrike(attackNumber, defenseNumber)) {
+			return false;
+		}
+		return attackNumbers.contains(String.valueOf(defenseNumber));
 	}
 
 	public Score judge(String attackNumbers, String defenceNumbers) {
-		return new Score(0, 0);
+		int strikeCount = 0;
+		int ballCount = 0;
+		for (int i = 0; i < attackNumbers.length(); i++) {
+			char attackNumber = attackNumbers.charAt(i);
+			char defenceNumber = defenceNumbers.charAt(i);
+			strikeCount += isStrike(attackNumber, defenceNumber) ? 1 : 0;
+			ballCount += isBall(attackNumbers, attackNumber, defenceNumber) ? 1 : 0;
+		}
+
+		return new Score(strikeCount, ballCount);
 	}
 }
