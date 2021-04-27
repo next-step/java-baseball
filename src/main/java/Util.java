9de33de3737ecf7e18842376a1c8dcf77edff4ed
이유진 <ujin@deali.net>
2021-04-27 12:23:16 +0900

    feat: 두개의 character를 전달받아 같은지 비교하는 기능 구현
    컴퓨터 숫자와 입력받은 숫자의 각 위치의 숫자가 같은경우 스트라이크이므로, 비교하기 위해 사용될 기능이다

---
+++ src/main/java/Util.java
@@ -0,0 +1,11 @@
+public class Util {
+    private Util(){}
+
+    public static Integer checkEqualChar(char target, char comparison){
+        if(target == comparison){
+            return 1;
+        }
+        return 0;
+    }
+
+}
