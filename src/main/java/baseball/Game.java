2a91f4c8065004b054376df9155353b38ea27562
Paul An <anpaul0615@gmail.com>
2021-04-27 22:17:46 +0900

    feat(Game): 게임 실행기 구현
    기능 구현
    - 게임 실행흐름 구성
    - 게임 종료/재개 분기 처리

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,39 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Game {
 	public static void printMessage(String message) {
 		System.out.println(message);
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		do {
+			game.play();
+		} while (game.isFinish());
+	}
+
+	public void play() {
+		Umpire umpire = new Umpire();
+		AttackTeam attackTeam = new AttackTeam();
+		attackTeam.prepareAttackNumbers();
+
+		DefenseTeam defenseTeam = new DefenseTeam();
+		Score score = new Score(0, 0);
+		while (score.getStrikeCount() != 3) {
+			defenseTeam.prepareDefenseNumbers();
+			score = umpire.judge(attackTeam.getAttackNumbers(), defenseTeam.getDefenseNumbers());
+			Game.printMessage(score.getScoreMessage());
+		}
+	}
+
+	public boolean isFinish() {
+		Game.printMessage("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+
+		return input.equals("1");
 	}
 }
