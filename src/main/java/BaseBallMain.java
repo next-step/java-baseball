b9a4ec0f0ccc84edcbdc3268c9a3842fe4901ad6
heo.h <heo.h@legaltech.co.kr>
2021-04-27 22:09:53 +0900

    feat(main) : 램덤 숫자 생성 generator

--- src/main/java/BaseBallMain.java
+++ src/main/java/BaseBallMain.java
@@ -1,10 +1,20 @@
+import util.BaseBallUtil;
+
+import java.util.Arrays;
+
 public class BaseBallMain
 {
 
     // 메인메소드
     public static void main(String[] args){
-
-
+        // 게임 시작,종료 판단
+        boolean isContinue = true;
+        // 컴퓨터 랜덤값
+        BaseBallUtil baseBallUtil = new BaseBallUtil();
+        int[] computerNumbers = baseBallUtil.generatorNumbers();
+        System.out.println(Arrays.toString(computerNumbers));
+//        baseBallUtil.playGame(computerNumbers);
+//        isContinue = gameEndCheck();
 
     }
 
