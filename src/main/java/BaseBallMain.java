1e64fb926c74a92238d79ab5f5d4f1161627ae82
heo.h <heo.h@legaltech.co.kr>
2021-04-27 22:50:16 +0900

    fix : 게임 종료, 재시작 메서드 수정

--- src/main/java/BaseBallMain.java
+++ src/main/java/BaseBallMain.java
@@ -10,16 +10,19 @@ public class BaseBallMain
 {
     public static void main(String[] args){
         // 게임 시작,종료 판단
+        boolean isContinue = true;
         // 컴퓨터 랜덤값
-        BaseBallUtil baseBallUtil = new BaseBallUtil();
-        int[] computerNumbers = baseBallUtil.generatorNumbers();
-        System.out.println(Arrays.toString(computerNumbers));
-        baseBallUtil.playGame(computerNumbers);
-        gameEndCheck();
+        while (isContinue) {
+            BaseBallUtil baseBallUtil = new BaseBallUtil();
+            int[] computerNumbers = baseBallUtil.generatorNumbers();
+            System.out.println(Arrays.toString(computerNumbers));
+            baseBallUtil.playGame(computerNumbers);
+            isContinue = gameEndCheck();
+        }
 
     }
     // 게임 계속, 종료 메서드
-    private static void gameEndCheck() {
+    private static boolean gameEndCheck() {
         Scanner scanner = new Scanner(System.in);
         int gameFlag = 1;
         boolean gameStatus = false;
@@ -29,6 +32,7 @@ public class BaseBallMain
         if (gameFlag == input) {
             gameStatus = true;
         }
+        return gameStatus;
     }
 
 }
