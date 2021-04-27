16085370e610b35f988a64304a5b72c270d72f5a
Shinyoung Kim <rolroralra@gmail.com>
2021-04-27 18:40:07 +0900

    Create BaseballConstant.java
    refactor: segregation Baseball Constant

---
+++ src/main/java/baseball/constants/BaseballConstant.java
@@ -0,0 +1,10 @@
+package baseball.constants;
+
+public class BaseballConstant {
+    public static final int DEFAULT_NUMBERS_SIZE = 3;
+    public static final int DEFAULT_NUMBER_RADIX = 10;
+    public static final int MAX_RADIX = 'z' - 'a' + 10;
+    public static final int MIN_RADIX = 2;
+    public static final int MAX_SIZE = MAX_RADIX - 1;
+    public static final int MIN_SIZE = MIN_RADIX - 1;
+}
