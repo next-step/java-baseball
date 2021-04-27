9b3d19250172849388b8fd35e955ad96a83220a9
Eunjeong Choi <ldmswjdl@naver.com>
2021-04-27 21:08:03 +0900

    bug : 기본 3자리 게임으로 실행 되도록 수정

--- src/main/java/com/baseball/woowahan/GameManager.java
+++ src/main/java/com/baseball/woowahan/GameManager.java
@@ -35,7 +35,7 @@ public class GameManager {
 	}
 
 	private void start() throws IOException {
-		Game game = new Game(new RandomGenerator(), 2);
+		Game game = new Game(new RandomGenerator());
 		while (!game.isFinished()) {
 			playGame(game);
 			System.out.println(Message.RESTART_OR_END.getMessage());
