9de217bea83e141a5e1c23c88c74fa08ca0fcab3
Eunyung Song <eysong@autocrypt.io>
2021-04-27 12:17:53 +0900

    test: add unit test

---
+++ src/main/java/task/baseball/BaseballService.java
@@ -0,0 +1,53 @@
+package task.baseball;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BaseballService {
+
+	public List<Integer> generateRandomAnswer() {
+
+		return Collections.emptyList();
+	}
+
+	public int getStrikeCount(int input, List<Integer> answer) {
+
+		return 0;
+	}
+
+	public boolean isAnswer(int strikeCount) {
+
+		return false;
+	}
+
+	public int getBallCount(int input, List<Integer> answer) {
+
+		return 0;
+	}
+
+	public boolean isNotThing(int strikeCount, int ballCount) {
+
+		return false;
+	}
+
+	public String generateOutputMessage(int strikeCount, int ballCount) {
+
+		return "";
+	}
+
+	public String generateAnswerOutputMessage() {
+
+		return "";
+	}
+
+	public String generateNotThingOutputMessage() {
+
+		return "";
+	}
+
+	public int[] validateInputAndConvert(String input) {
+
+		return new int[3];
+	}
+
+}
