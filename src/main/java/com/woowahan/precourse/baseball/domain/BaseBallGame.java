7e73bbc1b5dd9101c913338ed656faa1a2172ce9
junseok Oh <ixtears23@icloud.com>
2021-04-27 17:59:59 +0900

    feat(game) : game 실행 기능
    BaseBall 게임 실행(execute) 메서드에서는
    게임 Number와 사용자가 입력한 Number를 비교해서
    Strike와 Ball Count를 기록(Record)하고
    Rule에 따라 사용자에게 GameMessage를 반환하는 기능 담당

--- src/main/java/com/woowahan/precourse/baseball/domain/BaseBallGame.java
+++ src/main/java/com/woowahan/precourse/baseball/domain/BaseBallGame.java
@@ -1,15 +1,21 @@
 package com.woowahan.precourse.baseball.domain;
 
+import com.woowahan.precourse.baseball.record.BaseBallRecord;
+import com.woowahan.precourse.baseball.rule.BaseBallRule;
+import com.woowahan.precourse.baseball.view.GameMessage;
+
 public class BaseBallGame {
 
-    private String[] gameNumber;  // 컴퓨터가 선택한 세자리 수 - Game 생성 시 같이 생성되어야 함.
+    private final String[] gameNumbers;  // 컴퓨터가 선택한 세자리 수 - Game 생성 시 같이 생성되어야 함.
+    private String[] inputNumbers;
+    private BaseBallRecord baseBallRecord;
 
     public BaseBallGame() {
-        this.gameNumber = createBallGameNumber();
+        this.gameNumbers = createBallGameNumber();
     }
 
     public String[] getGameNumber() {
-        return gameNumber;
+        return gameNumbers;
     }
 
     private String generateRandomNumber() {
@@ -33,4 +39,52 @@ public class BaseBallGame {
         return new String[] { firstNumber, secondNumber, thirdNumber };
     }
 
+    public String execute(String[] inputNumbers) {
+
+        baseBallRecord = new BaseBallRecord();
+
+        this.inputNumbers = inputNumbers;
+
+        for(int position = 0; position < 3; position++) {
+            countStrike(position);
+            countBall(position);
+        }
+
+        int strikeCount = baseBallRecord.getStrikeCount();
+        int ballCount = baseBallRecord.getBallCount();
+
+        if (BaseBallRule.isNothingAndFourBall(strikeCount, ballCount)) {
+            return GameMessage.NOTHING;
+        }
+
+        if (BaseBallRule.isVictory(strikeCount)) {
+            return GameMessage.VICTORY;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        if (strikeCount > 0) {
+            sb.append(strikeCount)
+                    .append(GameMessage.STRIKE);
+        }
+
+        if (ballCount > 0) {
+            sb.append(ballCount)
+                    .append(GameMessage.BALL);
+        }
+
+        return sb.toString();
+    }
+
+    private void countBall(int position) {
+        if (BaseBallRule.isBall(this.gameNumbers, this.inputNumbers[position], position)) {
+            baseBallRecord.doBall();
+        }
+    }
+
+    private void countStrike(int position) {
+        if (BaseBallRule.isStrike(this.gameNumbers[position], this.inputNumbers[position])) {
+            baseBallRecord.doStrike();
+        }
+    }
 }
