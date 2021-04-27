b30ad2e68394851dceefbe8c982969a8057000b0
이유진 <ujin@deali.net>
2021-04-27 10:01:46 +0900

    feat: 입력받은 숫자를 유효성 체크하여 사용 가능할 경우 입력받은 숫자를 반환
    - 유효성 체크 메서드들을 활용해 입력받은 수에 유효성 체크하고 사용 가능한경우 입력받은 숫자를 반환한다
    - 유효성 체크 메서드들의 반환 타입을 primitive 타입으로 수정

--- src/main/java/InputNum.java
+++ src/main/java/InputNum.java
@@ -8,24 +8,34 @@ import java.util.regex.Pattern;
 public class InputNum {
     private static final Integer NUM_SIZE = 3;
 
+    public String getInputNum(){
+        String scanNum = scanNum();
+        System.out.println("scanNum : "+scanNum);
+        if (checkNumSize(scanNum) && checkOneToNine(scanNum) && !checkDuplicateNum(scanNum)) {
+            return scanNum;
+        }
+        System.out.println("입력형식이 틀렸습니다.");
+        return getInputNum();
+    }
+
     public String scanNum(){
         Scanner scan = new Scanner(System.in);
         System.out.print("숫자를 입력해주세요 : ");
         return scan.next();
     }
 
-    public static Boolean checkNumSize(String num){
+    public static boolean checkNumSize(String num){
         if(num.length() != NUM_SIZE){ return false;}
         return true;
     }
 
-    public static Boolean checkOneToNine(String num){
+    public static boolean checkOneToNine(String num){
         Pattern pattern = Pattern.compile("(^[1-9]*$)");
         Matcher matcher = pattern.matcher(num);
         return matcher.find();
     }
 
-    public static Boolean checkDuplicateNum(String num){
+    public static boolean checkDuplicateNum(String num){
         String[] numList = num.split("");
         Set<?> numSet = new HashSet<>(Arrays.asList(numList));
 
