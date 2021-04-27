610cf5e0eee5b01431d8d93b505f33eaeeda535f
root <edenkang5517@gmail.com>
2021-04-27 11:49:48 +0900

    feature : Add baseballGame Class
    게임 클래스를 추가하여 기본세팅 구성

---
+++ src/main/java/BaseBallGame.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class BaseBallGame {
+
+    List<String> targetNumberList = new ArrayList<>();
+    ArrayList<Integer> numberPool;
+    CheckResultModel checkResultModel = new CheckResultModel();
+    int isEnd = 0;
+
+}
