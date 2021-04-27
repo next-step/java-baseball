4a5097097dc718ca99d93cf999067f0aff4561a3
lafin <lafin716@gmail.com>
2021-04-27 21:24:06 +0900

    test: 숫자 검사 기능 테스트 추가

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -2,16 +2,20 @@ package baseball;
 
 import baseball.model.Computer;
 import baseball.value.Result;
+import baseball.view.Monitor;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ComputerTest {
+    Monitor monitor;
     Computer computer;
 
     @BeforeEach
     public void init() {
+        monitor = new Monitor();
         computer = new Computer();
         computer.generateBalls();
     }
@@ -24,5 +28,13 @@ public class ComputerTest {
         for (int ball : balls) {
             assertThat(ball).isBetween(1, 9);
         }
+    }
+
+    @Test
+    public void checkValidate() {
+        int playerBalls = 123;
+        Result result = computer.validate(playerBalls);
+
+        monitor.showResult(result);
     }
 }
