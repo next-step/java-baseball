4c0a89fb886e085a8c65e84f973d46c4d6056363
lhm7877 <vkfdldh7>
2021-04-27 21:01:01 +0900

    test: 테스트 메소드명 변경

--- src/test/java/com/hoomin/game/baseball/BaseBallGameTest.java
+++ src/test/java/com/hoomin/game/baseball/BaseBallGameTest.java
@@ -3,7 +3,6 @@ package com.hoomin.game.baseball;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,28 +11,22 @@ import com.hoomin.game.baseball.enums.GameOption;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BaseBallGameTest {
-	private BaseBallGame baseBallGame;
-
-	@BeforeAll
-	public void beforeAll() {
-		baseBallGame = new BaseBallGame();
-	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"RESTART"})
-	public void isOnGoing_InputGameOption_ReturnTrue(String input) {
+	public void isOnGoing_RevivedRESTART_ReturnTrue(String input) {
 		assertTrue(GameOption.valueOf(input).isOnGoing());
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"QUIT"})
-	public void isOnGoing_InputGameOption_ReturnFalse(String input) {
+	public void isOnGoing_RevivedQUIT_ReturnFalse(String input) {
 		assertFalse(GameOption.valueOf(input).isOnGoing());
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"TEST"})
-	public void isOnGoing_InputGameOption_IllegalArgumentExceptionThrown(String input) {
+	public void isOnGoing_InvalidInput_IllegalArgumentExceptionThrown(String input) {
 		assertThatThrownBy(() -> GameOption.valueOf(input).isOnGoing())
 			.isInstanceOf(IllegalArgumentException.class);
 	}
