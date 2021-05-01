1b68adf9bb9905b1940feb865eb9d18e01bbb1a4
kyle <cupeanimus@naver.com>
2021-05-01 13:44:01 +0900

    야구 숫자 1~9 검증

---
+++ src/test/java/baseball/tdd/utils/ValidationUtilsTest.java
@@ -0,0 +1,20 @@
+package baseball.tdd.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidationUtilsTest {
+
+    @DisplayName("야구 숫자 1~9 검증")
+    @Test
+    void invalid () {
+        assertThat(ValidationUtils.validNo(9)).isTrue();
+        assertThat(ValidationUtils.validNo(1)).isTrue();
+        assertThat(ValidationUtils.validNo(0)).isFalse();
+        assertThat(ValidationUtils.validNo(10)).isFalse();
+    }
+
+}
