e431e21546bdeaec45c530d0e09977a47b9dbb6a
gyewon.jun <gyewon.jun@nhnsoft.com>
2021-06-15 10:54:38 +0900

    feat: InputView, OutputView 구현

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -1,4 +1,7 @@
 package ui;
 
 public class InputView {
+    public String inputString() {
+        return System.in.toString();
+    }
 }
--- src/main/java/ui/OutputView.java
+++ src/main/java/ui/OutputView.java
@@ -1,4 +1,7 @@
 package ui;
 
 public class OutputView {
+    public void output(String output) {
+        System.out.printf("출력 : %s", output);
+    }
 }
