2c256f4775b4722590446ce82da284e286d92682
gyewon.jun <gyewon.jun@nhnsoft.com>
2021-06-15 13:38:10 +0900

    fix : InputView 값 입력 기능 수정

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -1,7 +1,12 @@
 package ui;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static String inputString() {
-        return System.in.toString();
+        System.out.println("값을 입력해주세요.");
+        return scanner.next();
     }
 }
