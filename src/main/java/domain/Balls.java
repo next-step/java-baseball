51047e77beda017c4514719940e31a341a71f894
gyewon.jun <gyewon.jun@nhnsoft.com>
2021-06-15 10:40:16 +0900

    feat : Balls Test 구현
    - 중복 숫자 예외처리
    - 중복 포지션 예외처리
    - 정상 테스트

--- src/main/java/domain/Balls.java
+++ src/main/java/domain/Balls.java
@@ -1,10 +1,12 @@
 package domain;
 
 import domain.exception.BallsDuplicatedNumberException;
+import domain.exception.BallsDuplicatedPositionException;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Balls {
 
@@ -17,21 +19,38 @@ public class Balls {
 
     private void validate(List<Ball> balls) {
         duplicatedBallNumberCheck(balls);
-        duplicatedBallPositionCheck();
+        duplicatedBallPositionCheck(balls);
 
     }
 
-    private void duplicatedBallPositionCheck() {
+    private void duplicatedBallPositionCheck(List<Ball> balls) {
+        List<Integer> positions = toPositionList(balls);
+        if (isDuplicatedPosition(positions)) {
+            throw new BallsDuplicatedPositionException();
+        }
     }
 
     private void duplicatedBallNumberCheck(List<Ball> balls) {
-        if (isDuplicatedNumber(balls)) {
+        List<Integer> numbers = toNumberList(balls);
+        if (isDuplicatedNumber(numbers)) {
             throw new BallsDuplicatedNumberException();
         }
     }
 
-    private boolean isDuplicatedNumber(List<Ball> balls) {
-        return balls.size() > balls.stream().distinct().count();
+    private List<Integer> toNumberList(List<Ball> ballList) {
+        return ballList.stream().map(ball -> ball.number()).collect(Collectors.toList());
+    }
+
+    private List<Integer> toPositionList(List<Ball> ballList) {
+        return ballList.stream().map(ball -> ball.position()).collect(Collectors.toList());
+    }
+
+    private boolean isDuplicatedPosition(List<Integer> positionList) {
+        return positionList.size() > positionList.stream().distinct().count();
+    }
+
+    private boolean isDuplicatedNumber(List<Integer> numbers) {
+        return numbers.size() > numbers.stream().distinct().count();
     }
 
     public int size() {
